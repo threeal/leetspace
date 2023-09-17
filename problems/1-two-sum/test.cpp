80272389d5cbaf6704f647b628c93f11a12a4ca2
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-17 15:51:42 +0700

    style: fix C++ source files format

--- problems/1-two-sum/test.cpp
+++ problems/1-two-sum/test.cpp
@@ -2,32 +2,26 @@
 
 using namespace std;
 
-#include "solution.cpp"
-
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_vector.hpp>
+
+#include "solution.cpp"
 
 TEST_CASE("2235. Add Two Integers") {
   Solution solution;
 
   SECTION("Testcase 1") {
-    vector<int> vec = {2,7,11,15};
-    CHECK_THAT(
-      solution.twoSum(vec, 9),
-      Catch::Matchers::Equals<int>({0, 1}));
+    vector<int> vec = {2, 7, 11, 15};
+    CHECK_THAT(solution.twoSum(vec, 9), Catch::Matchers::Equals<int>({0, 1}));
   }
 
   SECTION("Testcase 2") {
-    vector<int> vec = {3,2,4,};
-    CHECK_THAT(
-      solution.twoSum(vec, 6),
-      Catch::Matchers::Equals<int>({1, 2}));
+    vector<int> vec = {3, 2, 4};
+    CHECK_THAT(solution.twoSum(vec, 6), Catch::Matchers::Equals<int>({1, 2}));
   }
 
   SECTION("Testcase 3") {
-    vector<int> vec = {3,3};
-    CHECK_THAT(
-      solution.twoSum(vec, 6),
-      Catch::Matchers::Equals<int>({0, 1}));
+    vector<int> vec = {3, 3};
+    CHECK_THAT(solution.twoSum(vec, 6), Catch::Matchers::Equals<int>({0, 1}));
   }
 }
