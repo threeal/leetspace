21f9d9f40597be69540d0d854072367080318c52
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-18 09:00:39 +0700

    feat: add problem 1337. The K Weakest Rows in a Matrix

---
+++ problems/1337-the-k-weakest-rows-in-a-matrix/test.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+using namespace std;
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_vector.hpp>
+
+#include "solution.cpp"
+
+TEST_CASE("1337. The K Weakest Rows in a Matrix") {
+  Solution solution;
+
+  SECTION("Testcase 1") {
+    vector<vector<int>> mat = {
+        {1, 1, 0, 0, 0},
+        {1, 1, 1, 1, 0},
+        {1, 0, 0, 0, 0},
+        {1, 1, 0, 0, 0},
+        {1, 1, 1, 1, 1}};
+    CHECK_THAT(solution.kWeakestRows(mat, 3), Catch::Matchers::Equals<int>({2, 0, 3}));
+  }
+
+  SECTION("Testcase 2") {
+    vector<vector<int>> mat = {
+        {1, 0, 0, 0},
+        {1, 1, 1, 1},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0}};
+    CHECK_THAT(solution.kWeakestRows(mat, 2), Catch::Matchers::Equals<int>({0, 2}));
+  }
+}
