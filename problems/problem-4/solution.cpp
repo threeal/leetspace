c5084d6c4f8e5b0f6ae5f27e1e316167ff744341
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-20 09:21:15 +0700

    feat: solve problem 4 if one of the array is empty

--- problems/problem-4/solution.cpp
+++ problems/problem-4/solution.cpp
@@ -1,6 +1,21 @@
 class Solution {
  public:
-  double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+  double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+    const auto ae = a.end();
+    const auto be = b.end();
+
+    auto ai = a.begin();
+    auto bi = b.begin();
+
+    const auto n = a.size() + b.size();
+    const auto n2 = n / 2;
+
+    if (ai == ae) {
+      return n % 2 == 0 ? (b[n2] + b[n2 - 1]) / 2.0 : b[n2];
+    } else if (bi == be) {
+      return n % 2 == 0 ? (a[n2] + a[n2 - 1]) / 2.0 : a[n2];
+    }
+
     return 0;
   }
 };
