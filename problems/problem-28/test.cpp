9d53b1f849ab8abb74b2951307e6475b2b4d1459
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-22 07:33:27 +0700

    test: use Catch2 Generators in problem 28 testing

--- problems/problem-28/test.cpp
+++ problems/problem-28/test.cpp
@@ -3,21 +3,24 @@
 using namespace std;
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 #include "solution.cpp"
+
+struct TestCase {
+  string title;
+  string haystack;
+  string needle;
+  int expected;
+};
 
 TEST_CASE("28. Find the Index of the First Occurrence in a String") {
   Solution solution;
 
-  SECTION("Testcase 1") {
-    INFO("haystack: sadbutsad");
-    INFO("needle: sad");
-    CHECK(solution.strStr("sadbutsad", "sad") == 0);
-  }
+  auto [title, haystack, needle, expected] = GENERATE(
+      TestCase{.title = "Example 1", .haystack = "sadbutsad", .needle = "sad", .expected = 0},
+      TestCase{.title = "Example 2", .haystack = "leetcode", .needle = "leeto", .expected = -1});
 
-  SECTION("Testcase 2") {
-    INFO("haystack: leetcode");
-    INFO("needle: leeto");
-    CHECK(solution.strStr("leetcode", "leeto") == -1);
-  }
+  INFO(title);
+  CHECK(solution.strStr(haystack, needle) == expected);
 }
