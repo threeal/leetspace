f5c6810b69373c028f17bd29c24dd5d9d5948fca
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-22 09:13:34 +0700

    test: use Catch2 Generators in problem 2235 testing

--- problems/problem-2235/test.cpp
+++ problems/problem-2235/test.cpp
@@ -1,11 +1,23 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <string>
 
 #include "solution.cpp"
+
+struct TestCase {
+  std::string title;
+  int num1;
+  int num2;
+  int expected;
+};
 
 TEST_CASE("2235. Add Two Integers") {
   Solution solution;
 
-  SECTION("Testcase 1") { CHECK(solution.sum(12, 5) == 17); }
+  auto [title, num1, num2, expected] = GENERATE(
+      TestCase{.title = "Example 1", .num1 = 12, .num2 = 5, .expected = 17},
+      TestCase{.title = "Example 2", .num1 = -10, .num2 = 4, .expected = -6});
 
-  SECTION("Testcase 2") { CHECK(solution.sum(-10, 4) == -6); }
+  INFO(title);
+  CHECK(solution.sum(num1, num2) == expected);
 }
