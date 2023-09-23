c9840205c8ceb131252ba0766d63fcee2c36413e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-23 08:37:27 +0700

    feat(problem-1658): try to solve by iterator shift until `x <= 0`

--- problems/problem-1658/cpp/solution.cpp
+++ problems/problem-1658/cpp/solution.cpp
@@ -1,6 +1,41 @@
 class Solution {
  public:
   int minOperations(vector<int>& nums, int x) {
-    return -1;
+    auto left = nums.begin();
+    auto right = nums.end();
+    --right;
+    return shiftLeftRight(left, right, x, 1);
+  }
+
+  int shiftLeftRight(vector<int>::const_iterator left, vector<int>::const_iterator right, int x, int count) {
+    const int left_res = shiftLeft(left, right, x, count);
+    const int right_res = shiftRight(left, right, x, count);
+    if (left_res > 0) {
+      if (right_res > 0) {
+        return min(left_res, right_res);
+      }
+      return left_res;
+    }
+    return right_res;
+  }
+
+  int shiftLeft(vector<int>::const_iterator left, vector<int>::const_iterator right, int x, int count) {
+    x -= *left;
+    if (x > 0) {
+      if (left == right) return -1;
+      ++left;
+      return shiftLeftRight(left, right, x, ++count);
+    }
+    return x == 0 ? count : -1;
+  }
+
+  int shiftRight(vector<int>::const_iterator left, vector<int>::const_iterator right, int x, int count) {
+    x -= *right;
+    if (x > 0) {
+      if (left == right) return -1;
+      --right;
+      return shiftLeftRight(left, right, x, ++count);
+    }
+    return x == 0 ? count : -1;
   }
 };
