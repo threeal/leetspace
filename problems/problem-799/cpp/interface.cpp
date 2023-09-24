de42a36dc51e5b7fe5ced27fe2745e05996e5398
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-24 20:18:32 +0700

    feat: add problem 799. Champagne Tower

---
+++ problems/problem-799/cpp/interface.cpp
@@ -0,0 +1,5 @@
+#include "solution.cpp"
+
+double solution_cpp(int poured, int query_row, int query_glass) {
+  return Solution().champagneTower(poured, query_row, query_glass);
+}
---
+++ problems/problem-799/test.cpp
@@ -0,0 +1,23 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <string>
+
+double solution_cpp(int poured, int query_row, int query_glass);
+
+struct TestCase {
+  std::string title;
+  int poured;
+  int query_row;
+  int query_glass;
+  double expected;
+};
+
+TEST_CASE("799. Champagne Tower") {
+  auto [title, poured, query_row, query_glass, expected] = GENERATE(
+      TestCase{.title = "Example 1", .poured = 1, .query_row = 1, .query_glass = 1, .expected = 0.0},
+      TestCase{.title = "Example 2", .poured = 2, .query_row = 1, .query_glass = 1, .expected = 0.5},
+      TestCase{.title = "Example 3", .poured = 100000009, .query_row = 33, .query_glass = 17, .expected = 1.0});
+
+  INFO(title);
+  CHECK(solution_cpp(poured, query_row, query_glass) == expected);
+}
