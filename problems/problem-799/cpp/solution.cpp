5d6c27c439aaf62995d841623da0a20954ebb327
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-24 20:48:20 +0700

    feat(problem-799): try to solve by comparing total of the tower

--- problems/problem-799/cpp/solution.cpp
+++ problems/problem-799/cpp/solution.cpp
@@ -1,6 +1,21 @@
 class Solution {
  public:
   double champagneTower(int poured, int query_row, int query_glass) {
-    return 0.0;
+    int total = calculateTotal(query_row);
+    if (poured <= total) {
+      total -= query_row + 1;
+      if (poured <= total) return 0.0;
+      return (query_glass == 0 || query_glass == query_row)
+          ? (poured - total) / (2.0 * query_row)
+          : (poured - total) / (double)query_row;
+    }
+    return 1.0;
+  }
+
+  int calculateTotal(int row) {
+    ++row;
+    return row % 2 == 0
+        ? (row + 1) * (row / 2)
+        : (row + 1) * (row / 2) + (row / 2 + 1);
   }
 };
