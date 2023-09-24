d725d4f4cee8ed6852b351e0be7dfbcd5f511ee9
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-24 17:05:20 +0700

    fix(problem-8): handle case if start with word

--- problems/problem-8/cpp/solution.cpp
+++ problems/problem-8/cpp/solution.cpp
@@ -6,6 +6,15 @@ class Solution {
     int i;
     for (i = 0; i < n; ++i) {
       if (s[i] >= '0' && s[i] <= '9') break;
+      switch (s[i]) {
+        case ' ':
+        case '+':
+        case '-':
+          continue;
+
+        default:
+          return 0;
+      }
     }
 
     const bool neg = i > 0 && s[i - 1] == '-';
--- problems/problem-8/test.cpp
+++ problems/problem-8/test.cpp
@@ -14,7 +14,8 @@ TEST_CASE("8. String to Integer (atoi)") {
   auto [title, s, expected] = GENERATE(
       TestCase{.title = "Example 1", .s = "42", .expected = 42},
       TestCase{.title = "Example 2", .s = "   -42", .expected = -42},
-      TestCase{.title = "Example 3", .s = "4193 with words", .expected = 4193});
+      TestCase{.title = "Example 3", .s = "4193 with words", .expected = 4193},
+      TestCase{.title = "Starts with word", .s = "words and 987", .expected = 0});
 
   INFO(title);
   CHECK(solution_cpp(s) == expected);
