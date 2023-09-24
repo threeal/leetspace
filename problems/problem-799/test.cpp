88f418e3fc37316d23c4333aad0e831f305a3919
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-24 20:50:42 +0700

    test(problem-799): add failed test case 1

--- problems/problem-799/test.cpp
+++ problems/problem-799/test.cpp
@@ -16,7 +16,8 @@ TEST_CASE("799. Champagne Tower") {
   auto [title, poured, query_row, query_glass, expected] = GENERATE(
       TestCase{.title = "Example 1", .poured = 1, .query_row = 1, .query_glass = 1, .expected = 0.0},
       TestCase{.title = "Example 2", .poured = 2, .query_row = 1, .query_glass = 1, .expected = 0.5},
-      TestCase{.title = "Example 3", .poured = 100000009, .query_row = 33, .query_glass = 17, .expected = 1.0});
+      TestCase{.title = "Example 3", .poured = 100000009, .query_row = 33, .query_glass = 17, .expected = 1.0},
+      TestCase{.title = "Failed test case 1", .poured = 25, .query_row = 6, .query_glass = 1, .expected = 0.18750});
 
   INFO(title);
   CHECK(solution_cpp(poured, query_row, query_glass) == expected);
