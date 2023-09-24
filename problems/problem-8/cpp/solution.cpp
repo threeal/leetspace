9183a659edcc5c10e05afbc3aee495a091a3c3a4
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-24 17:04:01 +0700

    feat(problem-8): try to solve by parsing number

--- problems/problem-8/cpp/solution.cpp
+++ problems/problem-8/cpp/solution.cpp
@@ -1,6 +1,22 @@
 class Solution {
  public:
   int myAtoi(string s) {
-    return 0;
+    const int n = s.size();
+
+    int i;
+    for (i = 0; i < n; ++i) {
+      if (s[i] >= '0' && s[i] <= '9') break;
+    }
+
+    const bool neg = i > 0 && s[i - 1] == '-';
+
+    int total = 0;
+    int mul = 1;
+    for (; i < n; ++i) {
+      if (s[i] < '0' || s[i] > '9') break;
+      total = total * 10 + s[i] - '0';
+    }
+
+    return neg ? -total : total;
   }
 };
