90ed80de6224e4d8fcc8ca928a102ee5afe9a1d6
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-24 17:03:38 +0700

    feat: add problem 8. String to Integer (atoi)

---
+++ problems/problem-8/test.cpp
@@ -0,0 +1,21 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <string>
+
+int solution_cpp(std::string s);
+
+struct TestCase {
+  std::string title;
+  std::string s;
+  int expected;
+};
+
+TEST_CASE("8. String to Integer (atoi)") {
+  auto [title, s, expected] = GENERATE(
+      TestCase{.title = "Example 1", .s = "42", .expected = 42},
+      TestCase{.title = "Example 2", .s = "   -42", .expected = -42},
+      TestCase{.title = "Example 3", .s = "4193 with words", .expected = 4193});
+
+  INFO(title);
+  CHECK(solution_cpp(s) == expected);
+}
