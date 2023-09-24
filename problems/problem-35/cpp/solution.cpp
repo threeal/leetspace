a9da265357b235d955f728a7b8961c1ab91910c6
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-24 17:32:58 +0700

    feat(problem-35): try to solve by binary search

--- problems/problem-35/cpp/solution.cpp
+++ problems/problem-35/cpp/solution.cpp
@@ -1,6 +1,22 @@
 class Solution {
  public:
   int searchInsert(vector<int>& nums, int target) {
-    return 0;
+    int left = 0;
+    int right = nums.size() - 1;
+
+    if (target < nums[left]) return left;
+    if (target > nums[right]) return right + 1;
+
+    while (right - left > 1) {
+      int x = left + (right - left) / 2;
+      if (nums[x] == target) return x;
+      if (nums[x] < target) {
+        left = x;
+      } else {
+        right = x;
+      }
+    }
+
+    return right;
   }
 };
