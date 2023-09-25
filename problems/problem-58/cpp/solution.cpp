eec854c8be7e95f6e443e0396340483d3b8f49ec
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-25 15:05:33 +0700

    feat(problem-58): solve by iterating backward

--- problems/problem-58/cpp/solution.cpp
+++ problems/problem-58/cpp/solution.cpp
@@ -1,6 +1,17 @@
 class Solution {
  public:
   int lengthOfLastWord(string s) {
-    return 0;
+    const int n = s.size();
+    int b = n - 1;
+    while (b >= 0) {
+      if (s[b] != ' ') break;
+      b--;
+    }
+    int a = b;
+    while (a >= 0) {
+      if (s[a] == ' ') break;
+      a--;
+    }
+    return b - a;
   }
 };
