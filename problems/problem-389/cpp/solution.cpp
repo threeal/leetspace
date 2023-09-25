467cf4b027244584807cf74de027d2dcae796254
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-25 10:32:15 +0700

    feat(problem-389): solve by using array of characters

--- problems/problem-389/cpp/solution.cpp
+++ problems/problem-389/cpp/solution.cpp
@@ -1,6 +1,29 @@
 class Solution {
  public:
   char findTheDifference(string s, string t) {
-    return 0;
+    char maps[26];
+    for (int i = 0; i < 26; ++i) {
+      maps[i] = 0;
+    }
+
+    auto it = s.begin();
+    auto end = s.end();
+    while (it != end) {
+      ++maps[*it - 'a'];
+      ++it;
+    }
+
+    it = t.begin();
+    end = t.end();
+    while (it != end) {
+      --maps[*it - 'a'];
+      ++it;
+    }
+
+    for (int i = 0; i < 26; ++i) {
+      if (maps[i] != 0) return 'a' + i;
+    }
+
+    return 'a';
   }
 };
