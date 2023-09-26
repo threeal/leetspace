9fa8537f5d6b5fcfde355f46e44b7aa75b52c997
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-26 14:12:24 +0700

    feat: add problem 1081. Smallest Subsequence of Distinct Characters

---
+++ problems/problem-1081/test.cpp
@@ -0,0 +1,20 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <string>
+
+std::string solution_cpp(std::string s);
+
+struct TestCase {
+  std::string title;
+  std::string s;
+  std::string expected;
+};
+
+TEST_CASE("1081. Smallest Subsequence of Distinct Characters") {
+  auto [title, s, expected] = GENERATE(
+      TestCase{.title = "Example 1", .s = "bcabc", .expected = "abc"},
+      TestCase{.title = "Example 2", .s = "cbacdcbc", .expected = "acdb"});
+
+  INFO(title);
+  CHECK(solution_cpp(s) == expected);
+}
