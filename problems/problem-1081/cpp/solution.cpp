fd24601f778d78b06d53ac9a77ed73a234f74f1b
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-26 14:13:45 +0700

    feat(problem-1081): solve by using stack and iterating the characters

--- problems/problem-1081/cpp/solution.cpp
+++ problems/problem-1081/cpp/solution.cpp
@@ -1,6 +1,33 @@
 class Solution {
  public:
   string smallestSubsequence(string s) {
-    return s;
+    unordered_map<char, size_t> counts;
+    for (const auto c : s) {
+      ++counts[c];
+    }
+
+    unordered_set<char> existed;
+    stack<char> orders;
+
+    for (const auto c : s) {
+      --counts[c];
+      if (existed.find(c) != existed.end()) continue;
+
+      while (!orders.empty() && orders.top() > c && counts[orders.top()] > 0) {
+        existed.erase(orders.top());
+        orders.pop();
+      }
+
+      orders.push(c);
+      existed.insert(c);
+    }
+
+    string res;
+    while (!orders.empty()) {
+      res = orders.top() + res;
+      orders.pop();
+    }
+
+    return res;
   }
 };
