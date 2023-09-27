358608448a5795bf4994f73de3ad1a8530f5ca92
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-27 12:43:43 +0700

    feat(problem-0880): try to solve in C using recursive function

--- problems/0880/c/solution.c
+++ problems/0880/c/solution.c
@@ -1,3 +1,28 @@
+char decode(char* s, unsigned int k);
+
 char* decodeAtIndex(char* s, int k) {
+  s[0] = decode(s, k - 1);
+  s[1] = 0;
   return s;
+}
+
+char decode(char* s, unsigned int k) {
+  unsigned int i = 0;
+  char* c = s;
+  while (*c != 0) {
+    if (*c > '9') {
+      if (i++ == k) {
+        return *c;
+      }
+    } else {
+      const unsigned int repeat = *c - '0';
+      if (k < i * repeat) {
+        return decode(s, k % i);
+      } else {
+        i *= repeat;
+      }
+    }
+    ++c;
+  }
+  return 0;
 }
