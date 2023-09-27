efb18b344f49a12f5cd61f6021b1806ce1a294c2
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-27 11:55:09 +0700

    feat(problem-0880): try to solve by using recursive function

--- problems/0880/cpp/solution.cpp
+++ problems/0880/cpp/solution.cpp
@@ -1,44 +1,29 @@
-struct Sub {
-  size_t total;
-  size_t a;
-  size_t b;
-  int repeat;
-};
-
 class Solution {
+ private:
+  string str;
+
  public:
   string decodeAtIndex(string s, int k) {
-    stack<Sub> subs;
+    str = s;
+    return string() + decode(k - 1);
+  }
 
-    Sub sub{.total = 0, .a = 0};
-
-    int j = 0;
-    const size_t n = s.size();
-    for (size_t i = 0; i < n; ++i) {
-      if (s[i] > '9') {
-        if (++j == k) {
-          return string() + s[i];
+  char decode(int k) {
+    int i = 0;
+    for (const auto c : str) {
+      if (c > '9') {
+        if (i++ == k) {
+          return c;
         }
       } else {
-        sub.b = i;
-        sub.repeat = s[i] - '0';
-        j += (sub.total + sub.b - sub.a) * (sub.repeat - 1);
-        if (k > j) {
-          subs.push(sub);
-          sub.total = j;
-          sub.a = sub.b + 1;
+        const int repeat = c - '0';
+        if (k < i * repeat) {
+          return decode(k % i);
         } else {
-          --k;
-          while (sub.b - sub.a == 0) {
-            k %= sub.total;
-            sub = subs.top();
-            subs.pop();
-          }
-          k %= sub.b - sub.a;
-          return string() + s[sub.a + k];
+          i *= repeat;
         }
       }
     }
-    return s;
+    return '?';
   }
 };
