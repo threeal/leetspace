bb0dc231c33ee787b8a48908b4a6e7444a56e1ec
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-28 19:46:44 +0700

    feat(problem-6): solve in C++ by iterating the characters

--- problems/0006/cpp/solution.cpp
+++ problems/0006/cpp/solution.cpp
@@ -1,6 +1,32 @@
 class Solution {
  public:
   string convert(string s, int numRows) {
-    return s;
+    if (numRows == 1) return s;
+
+    auto res = s;
+    int n = s.size();
+    int rn = (numRows - 1) * 2;
+
+    int i = -1;
+
+    for (int j = 0; j < n; j += rn) {
+      res[++i] = s[j];
+    }
+
+    for (int r = 1; r < numRows - 1; ++r) {
+      auto rj = rn - (2 * r);
+      for (int j = r; j < n; j += rn) {
+        res[++i] = s[j];
+        if (j + rj < n) {
+          res[++i] = s[j + rj];
+        }
+      }
+    }
+
+    for (int j = numRows - 1; j < n; j += rn) {
+      res[++i] = s[j];
+    }
+
+    return res;
   }
 };
