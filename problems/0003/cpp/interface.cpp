6038d18c04b4dbb8e75523a4db7ec2b11e02c1cb
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-28 22:47:56 +0700

    feat(problem-003): solve in C++ by using recursive function and set container

--- problems/0003/cpp/interface.cpp
+++ problems/0003/cpp/interface.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <unordered_set>
 
 using namespace std;
 
--- problems/0003/cpp/solution.cpp
+++ problems/0003/cpp/solution.cpp
@@ -1,6 +1,20 @@
 class Solution {
  public:
   int lengthOfLongestSubstring(string s) {
-    return 0;
+    return longest(s.c_str(), s.size());
+  }
+
+  int longest(const char* str, int n) {
+    unordered_set<char> cs;
+    int i = 0;
+    while (i < n) {
+      if (cs.contains(str[i])) break;
+      cs.insert(str[i]);
+      ++i;
+    }
+    if (n <= 1) return i;
+    auto j = longest(str + 1, n - 1);
+
+    return max(i, j);
   }
 };
