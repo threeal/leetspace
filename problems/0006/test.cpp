74b11a14a587f9d28ee3aff5728348565e37461e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-28 19:45:06 +0700

    feat: add problem 6. Zigzag Conversion

---
+++ problems/0006/test.cpp
@@ -0,0 +1,26 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <string>
+
+std::string solution_cpp(const std::string& s, int numRows);
+
+struct TestCase {
+  std::string name;
+  std::string s;
+  int numRows;
+  std::string expected;
+};
+
+TEST_CASE("6. Zigzag Conversion") {
+  const auto solution = GENERATE(solution_cpp);
+  const auto [name, s, numRows, expected] = GENERATE(
+      TestCase{.name = "Example 1", .s = "PAYPALISHIRING", .numRows = 3, .expected = "PAHNAPLSIIGYIR"},
+      TestCase{.name = "Example 2", .s = "PAYPALISHIRING", .numRows = 4, .expected = "PINALSIGYAHRPI"},
+      TestCase{.name = "Example 3", .s = "A", .numRows = 1, .expected = "A"});
+
+  INFO("name: " << name);
+  INFO("s: " << s);
+  INFO("numRows: " << numRows);
+
+  CHECK(solution(s, numRows) == expected);
+}
