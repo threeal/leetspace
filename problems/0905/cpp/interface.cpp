7289ab3d281bfc3afcf57f073d0c105ec7118d89
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-28 15:30:17 +0700

    feat: add problem 905. Sort Array By Parity

---
+++ problems/0905/cpp/interface.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+#include "solution.cpp"
+
+vector<int> solution_cpp(const vector<int>& nums) {
+  Solution solution;
+  auto nums_copy = nums;
+  return solution.sortArrayByParity(nums_copy);
+}
