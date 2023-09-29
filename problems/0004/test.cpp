bde1605c3e9d6c04978dcc31bc8bd70c7ea6662c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-29 09:18:14 +0700

    test(problem-4): specify test cases using `test_cases.yaml`

--- problems/0004/test.cpp
+++ problems/0004/test.cpp
@@ -1,71 +1,20 @@
+#include <yaml-cpp/yaml.h>
+
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/generators/catch_generators.hpp>
-#include <catch2/matchers/catch_matchers_vector.hpp>
 #include <string>
 #include <vector>
 
 double solution_cpp(std::vector<int>&, std::vector<int>&);
 
-struct TestCase {
-  std::string title;
-  std::vector<int> nums1;
-  std::vector<int> nums2;
-  double expected;
-};
-
 TEST_CASE("4. Median of Two Sorted Arrays") {
-  auto [title, nums1, nums2, expected] = GENERATE(
-      TestCase{
-          .title = "Odd total size",
-          .nums1 = {1, 3},
-          .nums2 = {2},
-          .expected = 2.0},
-      TestCase{
-          .title = "Even total size",
-          .nums1 = {1, 3},
-          .nums2 = {2, 4},
-          .expected = 2.5},
-      TestCase{
-          .title = "Lower first array odd total size",
-          .nums1 = {1},
-          .nums2 = {2, 3},
-          .expected = 2.0},
-      TestCase{
-          .title = "Lower first array even total size",
-          .nums1 = {1, 2},
-          .nums2 = {3, 4},
-          .expected = 2.5},
-      TestCase{
-          .title = "Lower second array odd total size",
-          .nums1 = {2, 3},
-          .nums2 = {1},
-          .expected = 2.0},
-      TestCase{
-          .title = "Lower second array even total size",
-          .nums1 = {3, 4},
-          .nums2 = {1, 2},
-          .expected = 2.5},
-      TestCase{
-          .title = "Empty first array odd total size",
-          .nums1 = {},
-          .nums2 = {2, 3, 6},
-          .expected = 3.0},
-      TestCase{
-          .title = "Empty first array even total size",
-          .nums1 = {},
-          .nums2 = {2, 3, 6, 7},
-          .expected = 4.5},
-      TestCase{
-          .title = "Empty second array odd total size",
-          .nums1 = {2, 3, 6},
-          .nums2 = {},
-          .expected = 3.0},
-      TestCase{
-          .title = "Empty second array even total size",
-          .nums1 = {2, 3, 6, 7},
-          .nums2 = {},
-          .expected = 4.5});
+  auto test_cases = YAML::LoadFile("test_cases.yaml");
+  for (const auto& test_case : test_cases) {
+    auto name = test_case["name"].as<std::string>();
+    auto nums1 = test_case["nums1"].as<std::vector<int>>();
+    auto nums2 = test_case["nums2"].as<std::vector<int>>();
+    auto expected = test_case["expected"].as<double>();
 
-  INFO(title);
-  CHECK(solution_cpp(nums1, nums2) == expected);
+    CAPTURE(name);
+    CHECK(solution_cpp(nums1, nums2) == expected);
+  }
 }
