1454dce1ff7125f43e28b8fe1d57f2e4237bc1c6
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-30 17:06:31 +0700

    feat(problem-456): try to solve in C++ by prevent for looping on j

--- problems/0456/cpp/solution.cpp
+++ problems/0456/cpp/solution.cpp
@@ -4,11 +4,12 @@ class Solution {
     if (nums.size() < 3) return false;
     const size_t n = nums.size();
     for (size_t i = 0; i < n - 2; ++i) {
+      int highest = nums[i + 1];
       for (size_t k = i + 2; k < n; ++k) {
-        if (nums[i] >= nums[k]) continue;
-        for (size_t j = i + 1; j < k; ++j) {
-          if (nums[k] < nums[j]) return true;
+        if (nums[i] < nums[k] && nums[k] < highest) {
+          return true;
         }
+        highest = max(highest, nums[k]);
       }
     }
     return false;
