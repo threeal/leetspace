321c7d89148ee8e364713b34aa664b5a8e29e6fd
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-30 16:51:27 +0700

    feat(problem-456): try to solve in C++ by using for loop

--- problems/0456/cpp/solution.cpp
+++ problems/0456/cpp/solution.cpp
@@ -1,6 +1,16 @@
 class Solution {
  public:
   bool find132pattern(vector<int>& nums) {
+    if (nums.size() < 3) return false;
+    const size_t n = nums.size();
+    for (size_t i = 0; i < n - 2; ++i) {
+      for (size_t k = i + 2; k < n; ++k) {
+        if (nums[i] >= nums[k]) continue;
+        for (size_t j = i + 1; j < k; ++j) {
+          if (nums[k] < nums[j]) return true;
+        }
+      }
+    }
     return false;
   }
 };
