6e75c8cab2a50244b4f6988db29893750074f0ef
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-01 22:39:50 +0700

    feat(problem-1): solve in C by iterating each elements

--- problems/0001/c/solution.c
+++ problems/0001/c/solution.c
@@ -1,4 +1,15 @@
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-  *returnSize = numsSize;
+  *returnSize = 2;
+  int* bn = nums + numsSize - 1;
+  int* an = bn - 1;
+  for (int* a = nums; a <= an; ++a) {
+    for (int* b = a + 1; b <= bn; ++b) {
+      if (*a + *b == target) {
+        nums[0] = a - nums;
+        nums[1] = b - nums;
+        return nums;
+      }
+    }
+  }
   return nums;
 }
