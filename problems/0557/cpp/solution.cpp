d518b8f21ffabcb8c289b41d151d75a353359852
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-01 21:58:11 +0700

    feat(problem-557): solve in C++ by iterating and swaping elements

--- problems/0557/cpp/solution.cpp
+++ problems/0557/cpp/solution.cpp
@@ -1,6 +1,27 @@
 class Solution {
  public:
   string reverseWords(string s) {
+    const int n = s.size();
+    int a = 0;
+    for (int i = 0; i < n; ++i) {
+      if (s[i] == ' ') {
+        swap(s, a, i - 1);
+        a = i + 1;
+      }
+    }
+    if (s[n - 1] != ' ') {
+      swap(s, a, n - 1);
+    }
     return s;
+  }
+
+  void swap(string& s, int a, int b) {
+    while (b > a) {
+      auto tmp = s[a];
+      s[a] = s[b];
+      s[b] = tmp;
+      ++a;
+      --b;
+    }
   }
 };
