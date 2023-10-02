c3e7c2eadc49dfe849e08457b1647533858ab085
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-02 10:39:17 +0700

    feat(problem-2038): try to solve in C++ by counting the characters

--- problems/2038/cpp/solution.cpp
+++ problems/2038/cpp/solution.cpp
@@ -1,6 +1,24 @@
 class Solution {
  public:
   bool winnerOfGame(string colors) {
-    return false;
+    size_t a = 0;
+    size_t b = 0;
+
+    size_t start = 0;
+    const size_t n = colors.size();
+    for (size_t i = 1; i < n; ++i) {
+      if (colors[i] != colors[start]) {
+        if (i - start > 2) {
+          if (colors[start] == 'A') {
+            a += i - start - 2;
+          } else {
+            b += i - start - 2;
+          }
+        }
+        start = i;
+      }
+    }
+
+    return a > b;
   }
 };
