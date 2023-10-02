d6ac370df68490b295257b215dda5b61f66c8b4f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-02 21:04:50 +0700

    feat(problem-2038): solve in C by counting duplicate elements

--- problems/2038/c/solution.c
+++ problems/2038/c/solution.c
@@ -1,4 +1,30 @@
-
 bool winnerOfGame(char* colors) {
-  return false;
+  int a = 0;
+  int b = 0;
+
+  int start = 0;
+  int i = 0;
+  while (colors[i] != 0) {
+    if (colors[i] != colors[start]) {
+      if (i - start > 2) {
+        if (colors[start] == 'A') {
+          a += i - start - 2;
+        } else {
+          b += i - start - 2;
+        }
+      }
+      start = i;
+    }
+    ++i;
+  }
+
+  if (i - start > 2) {
+    if (colors[start] == 'A') {
+      a += i - start - 2;
+    } else {
+      b += i - start - 2;
+    }
+  }
+
+  return a > b;
 }
