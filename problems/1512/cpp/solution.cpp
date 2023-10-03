5221773c360ee5c09ac601096b47dbd41f12079d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-03 13:16:27 +0700

    feat(problem-1512): solve by iterating and comparing elements

--- problems/1512/cpp/solution.cpp
+++ problems/1512/cpp/solution.cpp
@@ -1,6 +1,13 @@
 class Solution {
  public:
   int numIdenticalPairs(vector<int>& nums) {
-    return nums.size();
+    size_t count = 0;
+    const size_t n = nums.size();
+    for (size_t i = 0; i < n; ++i) {
+      for (size_t j = i + 1; j < n; ++j) {
+        if (nums[i] == nums[j]) ++count;
+      }
+    }
+    return count;
   }
 };
