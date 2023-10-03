681129e1f15b64dd25c181062c09e9f3b69e0f08
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-03 15:51:45 +0700

    test(problem-1512): specify test cases using `test_cases.yaml`

--- problems/1512/test.cpp
+++ problems/1512/test.cpp
@@ -1,3 +1,5 @@
+#include <yaml-cpp/yaml.h>
+
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
 #include <string>
@@ -6,19 +8,16 @@
 int solution_c(const std::vector<int>& nums);
 int solution_cpp(const std::vector<int>& nums);
 
-struct TestCase {
-  std::string name;
-  std::vector<int> nums;
-  int expected;
-};
-
 TEST_CASE("1512. Number of Good Pairs") {
   const auto solution = GENERATE(solution_c, solution_cpp);
-  const auto [name, nums, expected] = GENERATE(
-      TestCase{.name = "Example 1", .nums = {1, 2, 3, 1, 1, 3}, .expected = 4},
-      TestCase{.name = "Example 2", .nums = {1, 1, 1, 1}, .expected = 6},
-      TestCase{.name = "Example 3", .nums = {1, 2, 3}, .expected = 0});
 
-  CAPTURE(name, nums);
-  CHECK(solution(nums) == expected);
+  const auto test_cases = YAML::LoadFile("test_cases.yaml");
+  for (const auto& test_case : test_cases) {
+    const auto name = test_case["name"].as<std::string>();
+    const auto nums = test_case["nums"].as<std::vector<int>>();
+    const auto expected = test_case["expected"].as<int>();
+
+    CAPTURE(name, nums);
+    CHECK(solution(nums) == expected);
+  }
 }
