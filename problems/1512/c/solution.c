c37a3dcde170c97a811e12ef09620f4c28150969
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-03 13:31:18 +0700

    feat(problem-1512): solve in C by iterating and comparing elements

--- problems/1512/c/solution.c
+++ problems/1512/c/solution.c
@@ -1,4 +1,9 @@
 int numIdenticalPairs(int* nums, int numsSize) {
-  (void)nums;
-  return numsSize;
+  int count = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    for (int j = i + 1; j < numsSize; ++j) {
+      if (nums[i] == nums[j]) ++count;
+    }
+  }
+  return count;
 }
