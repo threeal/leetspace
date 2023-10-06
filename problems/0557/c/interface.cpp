6e0a001b71af701bb07dac9ffa7aa043a213caee
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-06 11:50:35 +0700

    refactor(problem-557): use generic solution function

--- problems/0557/c/interface.cpp
+++ problems/0557/c/interface.cpp
@@ -1,9 +1,12 @@
+#include <interface.hpp>
 #include <string>
 
 extern "C" {
 char* reverseWords(char* s);
 }
 
+template <>
 std::string solution_c(const std::string& s) {
-  return reverseWords(const_cast<char*>(s.c_str()));
+  auto s_copy = s;
+  return reverseWords(s_copy.data());
 }
--- problems/0557/cpp/interface.cpp
+++ problems/0557/cpp/interface.cpp
@@ -1,9 +1,11 @@
+#include <interface.hpp>
 #include <string>
 
 using namespace std;
 
 #include "solution.cpp"
 
+template <>
 string solution_cpp(const string& s) {
   Solution solution;
   return solution.reverseWords(s);
