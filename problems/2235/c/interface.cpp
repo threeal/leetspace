2126e6954a0417cdd6f53ff7a407bfdc4d469106
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-06 11:50:46 +0700

    refactor(problem-2235): use generic solution function

--- problems/2235/c/interface.cpp
+++ problems/2235/c/interface.cpp
@@ -1,7 +1,10 @@
+#include <interface.hpp>
+
 extern "C" {
 int sum(int num1, int num2);
 }
 
-int solution_c(int num1, int num2) {
+template <>
+int solution_c(const int& num1, const int& num2) {
   return sum(num1, num2);
 }
