2f165a900d23f2dcea8693ae1a5a606c19a982be
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-07 13:36:57 +0700

    feat: add problem 15. 3Sum

---
+++ problems/0015/test.cpp
@@ -0,0 +1,22 @@
+#include <yaml-cpp/yaml.h>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_vector.hpp>
+#include <string>
+#include <vector>
+
+std::vector<std::vector<int>> solution(const std::vector<int>& nums) {
+  return {nums};
+}
+
+TEST_CASE("15. 3Sum") {
+  const auto test_cases = YAML::LoadFile("test_cases.yaml");
+  for (const auto& test_case : test_cases) {
+    auto name = test_case["name"].as<std::string>();
+    auto nums = test_case["nums"].as<std::vector<int>>();
+    auto expected = test_case["expected"].as<std::vector<std::vector<int>>>();
+
+    CAPTURE(name, nums);
+    CHECK_THAT(solution(nums), Catch::Matchers::UnorderedEquals(expected));
+  }
+}
