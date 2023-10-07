69851ffe045f221e8f62fe5b3a2b2842f6f59d6b
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-07 14:30:53 +0700

    feat: add problem 229. Majority Element II

---
+++ problems/0229/test.cpp
@@ -0,0 +1,21 @@
+#include <yaml-cpp/yaml.h>
+
+#include <catch2/catch_test_macros.hpp>
+#include <string>
+#include <vector>
+
+std::vector<int> solution(const std::vector<int>& nums) {
+  return nums;
+}
+
+TEST_CASE("229. Majority Element II") {
+  const auto test_cases = YAML::LoadFile("test_cases.yaml");
+  for (const auto& test_case : test_cases) {
+    const auto name = test_case["name"].as<std::string>();
+    const auto nums = test_case["nums"].as<std::vector<int>>();
+    const auto expected = test_case["expected"].as<std::vector<int>>();
+
+    CAPTURE(name, nums);
+    CHECK(solution(nums) == expected);
+  }
+}
