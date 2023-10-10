c4f90ff4457919b4bb0066c97be5de001d812a4d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-10 17:14:20 +0700

    feat: modify test cases to be specified as map

--- internal/generators/generate_test.py
+++ internal/generators/generate_test.py
@@ -9,33 +9,39 @@ with open(sys.argv[1], 'r') as config_file:
         "<catch2/catch_test_macros.hpp>",
         "<yaml-cpp/yaml.h>",
         "<interface.hpp>",
-        "<string>"
+        "<string>",
+        "<vector>"
     }
-
-    types = list(config["types"]["inputs"].values())
-    types.append(config["types"]["output"])
-    for t in types:
-        if "std::vector" in t:
-            headers.add("<vector>")
 
     with open(sys.argv[2], "w") as output:
         for header in headers:
             output.write("#include %s\n" % header)
 
-        output.write("\nconst auto test_cases = YAML::Load(R\"(\n")
-        output.write(yaml.dump(config["test_cases"]))
-        output.write(")\");\n")
+        output.write("\nstruct TestCase {\n")
+        output.write("  std::string name;\n")
+        output.write("  YAML::Node data;\n")
+        output.write("};\n")
+
+        output.write("\nconst std::vector<TestCase> test_cases = {\n")
+        for name in config["test_cases"]:
+            output.write("  {\n")
+            output.write("    .name = \"%s\",\n" % name)
+            output.write("    .data = YAML::Load(R\"(\n")
+            output.write(yaml.dump(config["test_cases"][name]))
+            output.write("    )\")")
+            output.write("  },\n")
+        output.write("};\n")
 
         output.write("\nTEST_CASE(\"%s\") {\n" % name)
-        output.write("  for (const auto& test_case : test_cases) {\n")
+        output.write("  for (const auto& [name, data] : test_cases) {\n")
 
-        variables = {"name": "std::string"}
+        variables = {}
         for v, t in config["types"]["inputs"].items():
             variables[v] = t
         variables["expected"] = config["types"]["output"]
 
         for v, t in variables.items():
-            output.write("    const auto %s = test_case[\"%s\"].as<%s>();\n" % (v, v, t))
+            output.write("    const auto %s = data[\"%s\"].as<%s>();\n" % (v, v, t))
 
         inputs = ", ".join(config["types"]["inputs"])
         output.write("\n    CAPTURE(name, %s);\n" % inputs)
