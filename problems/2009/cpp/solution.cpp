ee9e72abcdf7b13a29ed0bb26c9d31e5bd6c81e4
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-10 10:39:11 +0700

    feat(problem-2009): try to solve in C++ by iterating possible first value

--- problems/2009/cpp/solution.cpp
+++ problems/2009/cpp/solution.cpp
@@ -3,25 +3,27 @@ class Solution {
   int minOperations(vector<int>& nums) {
     sort(nums.begin(), nums.end());
 
-    bool prev_counted = false;
-    int count = 0;
+    int res = 0;
 
     const int n = nums.size();
-    for (int i = 1; i < n; ++i) {
-      if (nums[i] - nums[i - 1] == 1) {
-        prev_counted = false;
-        continue;
+    for (int i = 0; i < n; ++i) {
+      int unchanged = 1;
+      int need_changed = n - 1;
+
+      int j = i + 1;
+      while (j < n && need_changed > 0) {
+        if (nums[j] != nums[j - 1]) {
+          need_changed -= nums[j] - nums[j - 1];
+          if (need_changed >= 0) {
+            ++unchanged;
+          }
+        }
+        ++j;
       }
-      if (nums[i] - nums[i - 1] > 1) {
-        prev_counted = true;
-        ++count;
-        continue;
-      }
-      if (prev_counted) {
-        ++count;
-      }
+
+      res = max(res, unchanged);
     }
 
-    return count;
+    return n - res;
   }
 };
