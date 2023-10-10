6cc3c38bb214f9264dc373caa142b17e0f142d40
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-10 10:14:18 +0700

    feat(problem-2009): try to solve in C++ by handle case on duplicates

--- problems/2009/cpp/solution.cpp
+++ problems/2009/cpp/solution.cpp
@@ -3,11 +3,23 @@ class Solution {
   int minOperations(vector<int>& nums) {
     sort(nums.begin(), nums.end());
 
+    bool prev_counted = false;
     int count = 0;
 
     const int n = nums.size();
     for (int i = 1; i < n; ++i) {
-      if (nums[i] - nums[i - 1] > 1) ++count;
+      if (nums[i] - nums[i - 1] == 1) {
+        prev_counted = false;
+        continue;
+      }
+      if (nums[i] - nums[i - 1] > 1) {
+        prev_counted = true;
+        ++count;
+        continue;
+      }
+      if (prev_counted) {
+        ++count;
+      }
     }
 
     return count;
