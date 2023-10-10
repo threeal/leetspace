1b5f376f6f374d30d4df2ffdec76c608874b756c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-10 17:35:47 +0700

    refactor: modify structure of inputs and output of test cases in configuration

--- internal/generators/generate_test.py
+++ internal/generators/generate_test.py
@@ -35,18 +35,14 @@ with open(sys.argv[1], 'r') as config_file:
         output.write("\nTEST_CASE(\"%s\") {\n" % name)
         output.write("  for (const auto& [name, data] : test_cases) {\n")
 
-        variables = {}
         for v, t in config["types"]["inputs"].items():
-            variables[v] = t
-        variables["expected"] = config["types"]["output"]
-
-        for v, t in variables.items():
-            output.write("    const auto %s = data[\"%s\"].as<%s>();\n" % (v, v, t))
+            output.write("    const auto %s = data[\"inputs\"][\"%s\"].as<%s>();\n" % (v, v, t))
+        output.write("    const auto output = data[\"output\"].as<%s>();\n" % config["types"]["output"])
 
         inputs = ", ".join(config["types"]["inputs"])
         output.write("\n    CAPTURE(name, %s);\n" % inputs)
         for lang in config["solutions"]:
-            output.write("    CHECK(solution_%s<%s>(%s) == expected);\n" % (lang, config["types"]["output"], inputs))
+            output.write("    CHECK(solution_%s<%s>(%s) == output);\n" % (lang, config["types"]["output"], inputs))
 
         output.write("  }\n")
         output.write("}\n")
