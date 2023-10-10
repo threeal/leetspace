18ca4641f3100699c36fa382d23aac52549ad159
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-10 10:02:02 +0700

    feat(problem-2009): try to solve in C++ by sorting and calculating differences

--- problems/2009/cpp/interface.cpp
+++ problems/2009/cpp/interface.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <interface.hpp>
 #include <vector>
 
--- problems/2009/cpp/solution.cpp
+++ problems/2009/cpp/solution.cpp
@@ -1,6 +1,15 @@
 class Solution {
  public:
-  int minOperations(vector<int>&) {
-    return 0;
+  int minOperations(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+
+    int count = 0;
+
+    const int n = nums.size();
+    for (int i = 1; i < n; ++i) {
+      if (nums[i] - nums[i - 1] > 1) ++count;
+    }
+
+    return count;
   }
 };
