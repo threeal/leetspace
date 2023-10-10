76e692909d622aa58091a17fb07273f4bd1c66d7
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-10 17:53:47 +0700

    perf(generators): dump the test cases output directly as C++ value

--- internal/generators/generate_test.py
+++ internal/generators/generate_test.py
@@ -1,5 +1,5 @@
-import yaml
 import sys
+import yaml
 
 with open(sys.argv[1], 'r') as config_file:
     config = yaml.safe_load(config_file)
@@ -20,24 +20,31 @@ with open(sys.argv[1], 'r') as config_file:
         output.write("\nstruct TestCase {\n")
         output.write("  std::string name;\n")
         output.write("  YAML::Node data;\n")
+        output.write("  %s output;\n" % config["types"]["output"])
         output.write("};\n")
+
+        def dump(data):
+            import json
+            s = json.dumps(data)
+            return s.replace("[", "{").replace("]", "}")
 
         output.write("\nconst std::vector<TestCase> test_cases = {\n")
         for name in config["test_cases"]:
+            cfg = config["test_cases"][name]
             output.write("  {\n")
             output.write("    .name = \"%s\",\n" % name)
             output.write("    .data = YAML::Load(R\"(\n")
-            output.write(yaml.dump(config["test_cases"][name]))
-            output.write("    )\")")
+            output.write(yaml.dump(cfg))
+            output.write("    )\"),\n")
+            output.write("    .output = %s\n" % dump(cfg["output"]))
             output.write("  },\n")
         output.write("};\n")
 
         output.write("\nTEST_CASE(\"%s\") {\n" % name)
-        output.write("  for (const auto& [name, data] : test_cases) {\n")
+        output.write("  for (const auto& [name, data, output] : test_cases) {\n")
 
         for v, t in config["types"]["inputs"].items():
             output.write("    const auto %s = data[\"inputs\"][\"%s\"].as<%s>();\n" % (v, v, t))
-        output.write("    const auto output = data[\"output\"].as<%s>();\n" % config["types"]["output"])
 
         inputs = ", ".join(config["types"]["inputs"])
         output.write("\n    CAPTURE(name, %s);\n" % inputs)
