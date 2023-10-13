4ac095695454af0b99c257ac6fa6d8427eb0ce73
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-13 10:37:46 +0700

    feat(problem-746): solved by caching the computation result

--- problems/0746/cpp/solution.cpp
+++ problems/0746/cpp/solution.cpp
@@ -2,9 +2,14 @@ class Solution {
  public:
   int minCostClimbingStairs(vector<int>& cost) {
     const auto n = cost.size();
+    vector<int> cache(n, -1);
+
     function<int(size_t)> fn = [&](size_t i) {
       if (i >= n) return 0;
-      return cost[i] + min(fn(i + 1), fn(i + 2));
+      if (cache[i] < 0) {
+        cache[i] = cost[i] + min(fn(i + 1), fn(i + 2));
+      }
+      return cache[i];
     };
 
     return min(fn(0), fn(1));
