0453a87fa92fac69956bf558fedc53c3aba9bc95
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-13 10:33:55 +0700

    feat(problem-746): try to solve by using recursive function

--- problems/0746/cpp/solution.cpp
+++ problems/0746/cpp/solution.cpp
@@ -1,6 +1,12 @@
 class Solution {
  public:
-  int minCostClimbingStairs(vector<int>& /*cost*/) {
-    return 0;
+  int minCostClimbingStairs(vector<int>& cost) {
+    const auto n = cost.size();
+    function<int(size_t)> fn = [&](size_t i) {
+      if (i >= n) return 0;
+      return cost[i] + min(fn(i + 1), fn(i + 2));
+    };
+
+    return min(fn(0), fn(1));
   }
 };
