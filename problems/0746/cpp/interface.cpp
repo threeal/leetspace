f563eaa791d0e0d25f69b0a2b4ea142cda8bd16a
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-13 10:26:24 +0700

    feat: add problem 746. Min Cost Climbing Stairs

---
+++ problems/0746/cpp/interface.cpp
@@ -0,0 +1,12 @@
+#include <interface.hpp>
+#include <vector>
+
+using namespace std;
+
+#include "solution.cpp"
+
+template <>
+int solution_cpp(const std::vector<int>& cost) {
+  auto cost_copy = cost;
+  return Solution().minCostClimbingStairs(cost_copy);
+}
