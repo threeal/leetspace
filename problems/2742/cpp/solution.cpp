c09843c222dca149ceef30114d4e178a925e3cee
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-14 22:37:49 +0700

    feat(problem-2742): solve by using cache on large inputs

--- problems/2742/cpp/solution.cpp
+++ problems/2742/cpp/solution.cpp
@@ -3,12 +3,14 @@ class Solution {
   int paintWalls(vector<int>& cost, vector<int>& time) {
     const int n = min(cost.size(), time.size());
 
+    auto cache = vector(n + 1, vector(n + 1, -1));
     const function<int(int, int)> fn = [&](int i, int left) -> int {
       if (left <= 0) return 0;
       if (i >= n) return 1'000'000'000;
-      return min(
-          cost[i] + fn(i + 1, left - time[i] - 1),
-          fn(i + 1, left));
+      if (cache[i][left] >= 0) return cache[i][left];
+      return cache[i][left] = min(
+                 cost[i] + fn(i + 1, left - time[i] - 1),
+                 fn(i + 1, left));
     };
 
     return fn(0, n);
