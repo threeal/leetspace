62539765e26dd8c332b29d0002d98bcf17b6153b
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-16 12:29:46 +0700

    feat(generators): merge `generate_solution_cpp.py` to `generate_test.py`

--- internal/generators/generate_test.py
+++ internal/generators/generate_test.py
@@ -5,18 +5,38 @@ import yaml
 with open(sys.argv[1], 'r') as config_file:
     config = yaml.safe_load(config_file)
 
-    headers = {
-        "<catch2/catch_test_macros.hpp>",
-        "<string>",
-        "<vector>"
-    }
-
     if not os.path.exists(sys.argv[2]):
         os.makedirs(sys.argv[2])
 
+    with open(os.path.join(sys.argv[2], "solution_cpp.cpp"), "w") as output:
+        if "cpp" in config["solutions"]:
+            cfg = config["solutions"]["cpp"]
+
+            if cfg is not None and "includes" in cfg:
+                for include in cfg["includes"]:
+                    output.write("#include <%s>\n" % include)
+                output.write("\nusing namespace std;\n\n")
+
+            with open(os.path.join(os.path.dirname(sys.argv[1]), "solution.cpp"), "r") as source:
+                for line in source.readlines():
+                    output.write(line)
+
+            output.write("\n%s solution_cpp(" % config["types"]["output"])
+            output.write(", ".join([("%s %s" % (t, v)) for v, t in config["types"]["inputs"].items()]))
+            output.write(") {\n")
+
+            output.write("  auto output = Solution().%s(" % cfg["function"])
+            output.write(", ".join(config["types"]["inputs"]))
+            output.write(");\n")
+
+            output.write("  return %s;\n" % (cfg["output"] if "output" in cfg else "output"))
+
+            output.write("}\n")
+
     with open(os.path.join(sys.argv[2], "test.cpp"), "w") as output:
-        for header in headers:
-            output.write("#include %s\n" % header)
+        output.write("#include <catch2/catch_test_macros.hpp>\n")
+        output.write("#include <string>\n")
+        output.write("#include <vector>\n")
 
         output.write("\n")
         for lang in config["solutions"]:
