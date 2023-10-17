711d15908f5230a605868db94c1015b9f3152a2e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-17 15:27:13 +0700

    feat(problem-1361): solve in C++ by counting children from the root

--- problems/1361/solution.cpp
+++ problems/1361/solution.cpp
@@ -12,6 +12,18 @@ class Solution {
       ++count;
     }
 
-    return count == n - 1;
+    if (count != n - 1) return false;
+
+    count = 0;
+    const function<void(int)> sweep = [&](int root) {
+      ++count;
+      if (leftChild[root] >= 0) sweep(leftChild[root]);
+      if (rightChild[root] >= 0) sweep(rightChild[root]);
+    };
+
+    const int root = distance(exists.begin(), find(exists.begin(), exists.end(), false));
+    sweep(root);
+
+    return count == n;
   }
 };
