8609c7b15ea0e22a89220a081836b06bb4e28d69
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-17 10:59:20 +0700

    feat: add problem 1361. Validate Binary Tree Nodes

---
+++ problems/1361/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+ public:
+  bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+    (void)n;
+    (void)leftChild;
+    (void)rightChild;
+    return false;
+  }
+};
