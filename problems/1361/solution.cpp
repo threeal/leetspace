06650fdd689a745e5c66eb0c1b26c01be26e6ed5
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-17 11:29:07 +0700

    feat(problem-1361): try to solve in C++ by counting for duplicates

--- problems/1361/solution.cpp
+++ problems/1361/solution.cpp
@@ -1,9 +1,17 @@
 class Solution {
  public:
   bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-    (void)n;
-    (void)leftChild;
-    (void)rightChild;
-    return false;
+    vector<bool> exists(n, false);
+    int count = 0;
+
+    leftChild.insert(leftChild.end(), rightChild.begin(), rightChild.end());
+    for (auto val : leftChild) {
+      if (val < 0) continue;
+      if (exists[val]) return false;
+      exists[val] = true;
+      ++count;
+    }
+
+    return count == n - 1;
   }
 };
