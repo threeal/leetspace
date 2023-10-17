b50afd6ace53ef1711ada1f8c30426a47f51e798
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-17 22:40:22 +0700

    feat(problem-1361): add support to solve in C

---
+++ problems/1361/c/solution.c
@@ -0,0 +1,8 @@
+bool validateBinaryTreeNodes(int n, int* leftChild, int leftChildSize, int* rightChild, int rightChildSize) {
+  (void)n;
+  (void)leftChild;
+  (void)leftChildSize;
+  (void)rightChild;
+  (void)rightChildSize;
+  return false;
+}
