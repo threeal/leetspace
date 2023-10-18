f0664df6b7ac58e79db05bdce10225c531a6b1cb
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-18 11:30:24 +0700

    feat: add problem 2050. Parallel Courses III

---
+++ problems/2050/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+ public:
+  int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+    (void)n;
+    (void)relations;
+    (void)time;
+    return 0;
+  }
+};
