acb9fcf2d5e173eb63782650911cad7e05ac94c3
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-19 15:08:51 +0700

    feat(problem-844): try to solve in C++ by iterating backward

--- problems/0844/solution.cpp
+++ problems/0844/solution.cpp
@@ -1,8 +1,44 @@
 class Solution {
  public:
   bool backspaceCompare(string s, string t) {
-    (void)s;
-    (void)t;
-    return false;
+    auto ss = s.rbegin();
+    auto tt = t.rbegin();
+
+    const auto se = s.rend();
+    const auto te = t.rend();
+
+    // Iterating through each string backwards.
+    while (ss != se && tt != te) {
+      // Shift both iterators to skip the backspace character.
+      shift_backspace(ss, se);
+      shift_backspace(tt, te);
+
+      if (ss == se || tt == te) break;
+
+      // Both strings are unequal if these iterators contain different characters.
+      if (*ss != *tt) return false;
+
+      ++ss;
+      ++tt;
+    }
+
+    // Both strings are equal if both iterators have reached the end.
+    return ss == se && tt == te;
+  }
+
+  // This function will increment the iterator as long as it still contains the backspace character (`#`).
+  void shift_backspace(string::reverse_iterator& it, string::reverse_iterator end) {
+    while (it != end && *it == '#') {
+      int skip = 0;
+      while (it != end && *it == '#') {
+        ++skip;
+        ++it;
+      }
+
+      while (it != end && skip > 0) {
+        --skip;
+        ++it;
+      }
+    }
   }
 };
