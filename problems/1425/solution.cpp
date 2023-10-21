3fa6d3ca5b77882c3026dbf47f8f198c9e87f5be
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-21 22:47:48 +0700

    fea(problem-1425): solve in C++ by using deque

--- problems/1425/solution.cpp
+++ problems/1425/solution.cpp
@@ -1,8 +1,22 @@
 class Solution {
  public:
   int constrainedSubsetSum(vector<int>& nums, int k) {
-    (void)nums;
-    (void)k;
-    return 0;
+    deque<int> dq;
+
+    const int n = nums.size();
+    for (int i = 0; i < n; ++i) {
+      if (!dq.empty()) nums[i] += nums[dq.front()];
+
+      while (!dq.empty() && (i - dq.front() >= k || nums[i] >= nums[dq.back()])) {
+        if (nums[i] >= nums[dq.back()])
+          dq.pop_back();
+        else
+          dq.pop_front();
+      }
+
+      if (nums[i] > 0) dq.push_back(i);
+    }
+
+    return *max_element(nums.begin(), nums.end());
   }
 };
