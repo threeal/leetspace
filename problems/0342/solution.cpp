3337da0956c254ca5649f612df0a3d08818e7593
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-23 14:31:27 +0700

    feat(problem-342): solve in C++ by fixing the logic

--- problems/0342/solution.cpp
+++ problems/0342/solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
  public:
   bool isPowerOfFour(int n) {
-    if (n < 4) return n == 1;
+    if (n <= 0) return false;
 
-    while (n > 4) {
+    while (n >= 4) {
       if (n % 4 > 0) return false;
       n /= 4;
     }
 
-    return true;
+    return n == 1;
   }
 };
