b36290bcfc65a154255ba6fef4e7438806a4b8c7
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-23 14:23:18 +0700

    feat(problem-342): try to solve in C++ by using repeating modulo operation

--- problems/0342/solution.cpp
+++ problems/0342/solution.cpp
@@ -1,7 +1,13 @@
 class Solution {
  public:
   bool isPowerOfFour(int n) {
-    (void)n;
-    return false;
+    if (n < 4) return n == 1;
+
+    while (n > 4) {
+      if (n % 4 > 0) return false;
+      n /= 4;
+    }
+
+    return true;
   }
 };
