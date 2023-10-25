4f2e29224d19debb5287b7506e9ca10192a8f766
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-25 18:38:17 +0700

    feat(problem-779): solve in C by using recursive logic

--- problems/0779/c/solution.c
+++ problems/0779/c/solution.c
@@ -1,5 +1,11 @@
-int kthGrammar(int n, int k){
-  (void)n;
-  (void)k;
-  return 0;
+int kthGrammar(int n, int k) {
+  // Base case: The root value is zero.
+  if (n == 0) return 0;
+
+  // Recursive logic:
+  // - If the parent value is zero, children will be zero and one.
+  // - If the parent value is one, children will be one and zero.
+  return kthGrammar(n - 1, (k + 1) / 2) == 0
+      ? (k - 1) % 2
+      : k % 2;
 }
