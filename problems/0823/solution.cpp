c961dc535647baa0cee93781e30eb64325731fb7
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-26 10:28:11 +0700

    feat(problem-823): try to solve in C++ by sorting and using cache

--- problems/0823/solution.cpp
+++ problems/0823/solution.cpp
@@ -1,7 +1,28 @@
 class Solution {
  public:
   int numFactoredBinaryTrees(vector<int>& arr) {
-    (void)arr;
-    return 0;
+    int res = 0;
+
+    // Container to track the number of combinations for each value.
+    unordered_map<int, int> comb_of;
+
+    // Iterate from lower values to higher values.
+    sort(arr.begin(), arr.end());
+    for (size_t i = 0; i < arr.size(); ++i) {
+      // A value can be its own node.
+      int comb = 1;
+
+      // Find values that could serve as child nodes.
+      for (size_t j = 0; j < i; ++j) {
+        if (arr[i] % arr[j] > 0) continue;
+        comb += comb_of[arr[j]] * comb_of[arr[i] / arr[j]];
+      }
+
+      // Track the number of combinations and add it to the result.
+      comb_of[arr[i]] = comb;
+      res += comb;
+    }
+
+    return res;
   }
 };
