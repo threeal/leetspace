1dca7ab042ae0972f9a8a49ce6316f2f3ede44ea
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-28 16:17:43 +0700

    feat(problem-5): update to use local function in C++ solution

--- problems/0005/solution.cpp
+++ problems/0005/solution.cpp
@@ -1,31 +1,30 @@
 class Solution {
  public:
   string longestPalindrome(string s) {
-    string res = s.substr(0, 1);
     const int n = s.size();
+
+    int res_n = 1;
+    string res = s.substr(0, res_n);
+
+    const function<void(int, int)> fn = [&](int a, int b) {
+      if (s[a] != s[b]) return;
+      while (a >= 0 && b < n) {
+        if (s[a] != s[b]) break;
+        --a;
+        ++b;
+      }
+      if (b - a - 1 > res_n) {
+        res_n = b - a - 1;
+        res = s.substr(a + 1, res_n);
+      }
+    };
+
     for (int i = 1; i < n; ++i) {
-      auto ss = palindromeEven(s, i - 1, i);
-      if (ss.size() > res.size()) res = ss;
+      fn(i - 1, i);
       if (i < n - 1) {
-        ss = palindromeOdd(s, i);
-        if (ss.size() > res.size()) res = ss;
+        fn(i - 1, i + 1);
       }
     }
     return res;
-  }
-
-  string palindromeEven(const string& s, int a, int b) {
-    if (s[a] != s[b]) return "";
-    const int n = s.size();
-    while (a >= 0 && b < n) {
-      if (s[a] != s[b]) break;
-      --a;
-      ++b;
-    }
-    return s.substr(a + 1, b - a - 1);
-  }
-
-  string palindromeOdd(const string& s, int i) {
-    return palindromeEven(s, i - 1, i + 1);
   }
 };
