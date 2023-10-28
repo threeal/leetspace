1c62eedee494649f2eb227347394054704b09156
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-28 17:39:00 +0700

    feat(problem-1220/cpp): solve by using while loop and addition

--- problems/1220/solution.cpp
+++ problems/1220/solution.cpp
@@ -1,7 +1,20 @@
+const unsigned int mod = 1'000'000'007;
+
 class Solution {
  public:
   int countVowelPermutation(int n) {
-    (void)n;
-    return 0;
+    unsigned int a = 1, e = 1, i = 1, o = 1, u = 1;
+
+    while (--n > 0) {
+      const unsigned int aa = a, ee = e, ii = i, oo = o, uu = u;
+
+      a = (ee + ii + uu) % mod;
+      e = (aa + ii) % mod;
+      i = (ee + oo) % mod;
+      o = ii;
+      u = (ii + oo) % mod;
+    }
+
+    return (a + e + i + o + u) % mod;
   }
 };
