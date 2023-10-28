7e769c6ce3a75f4f98e86300c260a969745522f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-28 18:50:03 +0700

    refactor(internal/python): add `generate_solution_cpp_src` function from lines in `__main__.py`

---
+++ internal/python/lib/generate_test_src/generators/solution_cpp_src.py
@@ -0,0 +1,36 @@
+import os
+
+
+def generate_solution_cpp_src(source_dir: str, out_dir: str, config: any):
+    with open(os.path.join(out_dir, "solution_cpp.cpp"), "w") as output:
+        if "cpp" not in config["solutions"]:
+            return
+
+        cfg = config["solutions"]["cpp"]
+
+        if cfg is not None and "includes" in cfg:
+            for include in cfg["includes"]:
+                output.write("#include <%s>\n" % include)
+            output.write("\nusing namespace std;\n\n")
+
+        with open(os.path.join(source_dir, "solution.cpp"), "r") as source:
+            for line in source.readlines():
+                output.write(line)
+
+        output.write("\n%s solution_cpp(" % config["types"]["output"])
+        output.write(
+            ", ".join(
+                [("%s %s" % (t, v)) for v, t in config["types"]["inputs"].items()]
+            )
+        )
+        output.write(") {\n")
+
+        output.write("  auto output = Solution().%s(" % cfg["function"])
+        output.write(", ".join(config["types"]["inputs"]))
+        output.write(");\n")
+
+        output.write(
+            "  return %s;\n" % (cfg["output"] if "output" in cfg else "output")
+        )
+
+        output.write("}\n")
