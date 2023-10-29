aac65d13b89d2fbb4d4b71c58131368411637437
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-29 17:34:04 +0700

    feat(problem-458/cpp): solve by using logarithmic

--- problems/0458/solution.cpp
+++ problems/0458/solution.cpp
@@ -1,9 +1,6 @@
 class Solution {
  public:
   int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-    (void)buckets;
-    (void)minutesToDie;
-    (void)minutesToTest;
-    return 0;
+    return ceil(log2(buckets) / log2(minutesToTest / minutesToDie + 1));
   }
 };
