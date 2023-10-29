cb8600fbc5aa2201311dda63182da94275840176
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-29 17:29:46 +0700

    feat: add problem 458. Poor Pigs

---
+++ problems/0458/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+ public:
+  int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+    (void)buckets;
+    (void)minutesToDie;
+    (void)minutesToTest;
+    return 0;
+  }
+};
