31aea0e43a16fd2b9da7477653d26d898dc1b415
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-30 10:26:08 +0700

    feat(problem-1356/cpp): use `__builtin_popcount` for faster bit counting

--- problems/1356/solution.cpp
+++ problems/1356/solution.cpp
@@ -2,16 +2,8 @@ class Solution {
  public:
   vector<int> sortByBits(vector<int>& arr) {
     sort(arr.begin(), arr.end(), [](int a, int b) {
-      int ai = 0;
-      for (int i = 1; i <= a; i *= 2) {
-        if ((a & i) > 0) ++ai;
-      }
-
-      int bi = 0;
-      for (int i = 1; i <= b; i *= 2) {
-        if ((b & i) > 0) ++bi;
-      }
-
+      int ai = __builtin_popcount(a);
+      int bi = __builtin_popcount(b);
       return ai == bi ? a < b : ai < bi;
     });
     return arr;
