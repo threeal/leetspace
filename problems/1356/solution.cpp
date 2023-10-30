19f2d94d895f74e02034860019d05dd6eb9930b9
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-30 08:55:40 +0700

    feat(problem-1356/cpp): solve by using `std::sort` function

--- problems/1356/solution.cpp
+++ problems/1356/solution.cpp
@@ -1,6 +1,19 @@
 class Solution {
  public:
   vector<int> sortByBits(vector<int>& arr) {
+    sort(arr.begin(), arr.end(), [](int a, int b) {
+      int ai = 0;
+      for (int i = 1; i <= a; i *= 2) {
+        if ((a & i) > 0) ++ai;
+      }
+
+      int bi = 0;
+      for (int i = 1; i <= b; i *= 2) {
+        if ((b & i) > 0) ++bi;
+      }
+
+      return ai == bi ? a < b : ai < bi;
+    });
     return arr;
   }
 };
