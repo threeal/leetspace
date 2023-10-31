2b4d4aa636cbbca511e3e02c666b2cdd902576e1
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-10-31 12:22:22 +0700

    feat(problem-2433/cpp): solve by using simple for loop

--- problems/2433/solution.cpp
+++ problems/2433/solution.cpp
@@ -1,6 +1,10 @@
 class Solution {
  public:
   vector<int> findArray(vector<int>& pref) {
-    return pref;
+    vector<int> res = pref;
+    for (size_t i = 1; i < res.size(); ++i) {
+      res[i] = pref[i - 1] ^ pref[i];
+    }
+    return res;
   }
 };
