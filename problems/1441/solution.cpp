ce175d352974719a3fad2cd24cd522c65d0cd2d2
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-03 13:08:28 +0700

    feat(problem-1441): solve in C++ by using for loop

--- problems/1441/solution.cpp
+++ problems/1441/solution.cpp
@@ -1,8 +1,19 @@
 class Solution {
  public:
   vector<string> buildArray(vector<int>& target, int n) {
-    (void)target;
-    (void)n;
-    return {};
+    vector<string> cmds;
+
+    auto ti = target.begin();
+    for (int i = 1; i <= n && ti != target.end(); ++i) {
+      if (*ti == i) {
+        cmds.push_back("Push");
+        ++ti;
+      } else {
+        cmds.push_back("Push");
+        cmds.push_back("Pop");
+      }
+    }
+
+    return cmds;
   }
 };
