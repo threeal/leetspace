96612a3371acc698f31d41ba2a05c8a7ccb7184f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-04 10:43:09 +0700

    docs(problem-1503/cpp); add comment that describe the solution

--- problems/1503/solution.cpp
+++ problems/1503/solution.cpp
@@ -1,3 +1,7 @@
+// The solution is straightforward. Despite collisions between two ants, both ants eventually end up in the same starting place.
+// Conceptually, we can ignore the collisions, and ant movement is as if they pass through each other.
+// Hence, the time required is the same as the position of the leftmost ant moving right or the rightmost ant moving left.
+
 class Solution {
  public:
   int getLastMoment(int n, vector<int>& left, vector<int>& right) {
