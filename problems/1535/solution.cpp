0c65ac3485a6db972c56a2c06a8a365906b636fa
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-05 20:02:37 +0700

    feat(problem-1535/cpp): solve by handling break case

--- problems/1535/solution.cpp
+++ problems/1535/solution.cpp
@@ -3,12 +3,14 @@ class Solution {
   int getWinner(vector<int>& arr, int k) {
     int win = 0;
     size_t ref = 0;
+
     for (size_t i = 1; i < arr.size(); ++i) {
+      if (win >= k) break;
       if (arr[i] > arr[ref]) {
         win = 1;
         ref = i;
-      } else if (++win >= k) {
-        break;
+      } else {
+        ++win;
       }
     }
 
