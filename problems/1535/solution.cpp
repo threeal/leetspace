3ec7897d643757f7cd171499321cca34a773c43c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-05 19:46:18 +0700

    feat(problem-1535/cpp): try to solve by using simple for loop

--- problems/1535/solution.cpp
+++ problems/1535/solution.cpp
@@ -1,8 +1,17 @@
 class Solution {
  public:
   int getWinner(vector<int>& arr, int k) {
-    (void)arr;
-    (void)k;
-    return 0;
+    int win = 0;
+    size_t ref = 0;
+    for (size_t i = 1; i < arr.size(); ++i) {
+      if (arr[i] > arr[ref]) {
+        win = 1;
+        ref = i;
+      } else if (++win >= k) {
+        break;
+      }
+    }
+
+    return arr[ref];
   }
 };
