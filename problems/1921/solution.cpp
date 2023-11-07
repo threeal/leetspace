9b4b95f9c1dd577b784e1d00d9f818145d49e609
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-07 10:10:39 +0700

    feat(problem-1921/cpp): try to solve by using simple for loop

--- problems/1921/solution.cpp
+++ problems/1921/solution.cpp
@@ -1,8 +1,26 @@
 class Solution {
  public:
   int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
-    (void)dist;
-    (void)speed;
-    return 0;
+    int n = min(dist.size(), speed.size());
+    int kills = 0;
+    int damage = 0;
+    while (damage == 0 && kills < n) {
+      int nearest = 0;
+      while (dist[nearest] < 0) ++nearest;
+      dist[nearest] -= speed[nearest];
+      if (dist[nearest] <= 0) ++damage;
+
+      for (int i = nearest + 1; i < n; ++i) {
+        if (dist[i] < 0) continue;
+        dist[i] -= speed[i];
+        if (dist[i] <= 0) ++damage;
+        if (dist[i] < dist[nearest]) nearest = i;
+      }
+
+      if (dist[nearest] <= 0) --damage;
+      dist[nearest] = -1;
+      ++kills;
+    }
+    return kills;
   }
 };
