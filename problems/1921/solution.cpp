7f607c6f50d3d6365550a935738adb378b6218e4
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-07 10:31:45 +0700

    feat(problem-1921/cpp): solve in C++ by calculating required time first and sort it later

--- problems/1921/solution.cpp
+++ problems/1921/solution.cpp
@@ -2,25 +2,18 @@ class Solution {
  public:
   int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
     int n = min(dist.size(), speed.size());
+    for (int i = 0; i < n; ++i) {
+      dist[i] = dist[i] / speed[i] + (dist[i] % speed[i] > 0 ? 1 : 0);
+    }
+
+    sort(dist.begin(), dist.end());
+
     int kills = 0;
-    int damage = 0;
-    while (damage == 0 && kills < n) {
-      int nearest = 0;
-      while (dist[nearest] < 0) ++nearest;
-      dist[nearest] -= speed[nearest];
-      if (dist[nearest] <= 0) ++damage;
-
-      for (int i = nearest + 1; i < n; ++i) {
-        if (dist[i] < 0) continue;
-        dist[i] -= speed[i];
-        if (dist[i] <= 0) ++damage;
-        if (dist[i] < dist[nearest]) nearest = i;
-      }
-
-      if (dist[nearest] <= 0) --damage;
-      dist[nearest] = -1;
+    for (const auto val : dist) {
+      if (kills + 1 > val) break;
       ++kills;
     }
+
     return kills;
   }
 };
