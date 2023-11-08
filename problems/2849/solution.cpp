268c00b6b3903bec90b3dbeb573076540337b002
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-08 09:53:12 +0700

    feat: add problem 2849. Determine if a Cell Is Reachable at a Given Time

---
+++ problems/2849/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+ public:
+  bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+    (void)sx;
+    (void)sy;
+    (void)fx;
+    (void)fy;
+    (void)t;
+    return false;
+  }
+};
