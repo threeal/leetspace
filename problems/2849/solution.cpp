c9410304280f39e42f2325c626197bf731bb8abe
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-08 10:24:49 +0700

    test(problem-2849/cpp): solve by handling case if start and finish is equal

--- problems/2849/solution.cpp
+++ problems/2849/solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
  public:
   bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+    if (sx == fx && sy == fy && t == 1) return false;
     return max(abs(fx - sx), abs(fy - sy)) <= t;
   }
 };
