5150fe6fbecbfb9f0e80b0da6959c348538e4ecf
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-09 10:35:37 +0700

    feat(problem-1759/cpp): solve by summing series of consecutive letters

--- problems/1759/solution.cpp
+++ problems/1759/solution.cpp
@@ -1,7 +1,27 @@
+const long long mod = 1'000'000'007;
+
 class Solution {
  public:
   int countHomogenous(string s) {
-    (void)s;
-    return 0;
+    long long total = 0;
+
+    char prev = 0;
+    long long n = 0;
+    for (const auto c : s) {
+      if (prev == c) {
+        ++n;
+      } else {
+        total = (total + sumHomogenous(n)) % mod;
+        prev = c;
+        n = 1;
+      }
+    }
+
+    return (total + sumHomogenous(n)) % mod;
+  }
+
+  long long sumHomogenous(long long n) {
+    if (n <= 1) return n;
+    return ((n + 1) * n) / 2;
   }
 };
