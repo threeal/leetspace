0fc462233a1a8fbfa0a856d790f07d0afb52cb0a
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-10 09:32:51 +0700

    feat(problem-1743/cpp): solve by using hash map to map adjacents

--- problems/1743/solution.cpp
+++ problems/1743/solution.cpp
@@ -1,7 +1,33 @@
 class Solution {
  public:
   vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
-    (void)adjacentPairs;
-    return {};
+    unordered_map<int, list<int>> adjacents;
+    for (const auto& pair : adjacentPairs) {
+      adjacents[pair[0]].push_back(pair[1]);
+      adjacents[pair[1]].push_back(pair[0]);
+    }
+
+    const auto begin = find_if(adjacents.begin(), adjacents.end(), [](const auto& pair) {
+      return pair.second.size() == 1;
+    });
+
+    vector<int> res(adjacentPairs.size() + 1);
+    res[0] = begin->first;
+
+    int prev = begin->first;
+    int current = begin->second.front();
+
+    for (size_t i = 1; i < res.size() - 1; ++i) {
+      for (auto adjacent : adjacents[current]) {
+        if (adjacent == prev) continue;
+        res[i] = prev = current;
+        current = adjacent;
+        break;
+      }
+    }
+
+    res[res.size() - 1] = current;
+
+    return res;
   }
 };
