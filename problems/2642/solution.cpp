1963afdb02e6b3e2eadae7f1a2db3e7b7e62186d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-11 12:49:26 +0700

    feat: add problem 2642. Design Graph With Shortest Path Calculator

---
+++ problems/2642/solution.cpp
@@ -0,0 +1,17 @@
+class Graph {
+ public:
+  Graph(int n, vector<vector<int>>& edges) {
+    (void)n;
+    (void)edges;
+  }
+
+  void addEdge(vector<int> edge) {
+    (void)edge;
+  }
+
+  int shortestPath(int node1, int node2) {
+    (void)node1;
+    (void)node2;
+    return 0;
+  }
+};
