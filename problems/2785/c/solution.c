4f444241c8e0a3702bb587d299dc3a83cb7a9bd9
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-13 10:45:56 +0700

    feat(problem-2785/c): solve by counting vowels using switch case

--- problems/2785/c/solution.c
+++ problems/2785/c/solution.c
@@ -1,3 +1,63 @@
+// clang-format off
+enum { A = 0, E, I, O, U, a, e, i, o, u };
+// clang-format on
+
 char* sortVowels(char* s) {
+  int vowelsCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+  char* c = s;
+  while (*c != 0) {
+    switch (*c) {
+      // clang-format off
+      case 'A': ++vowelsCount[A]; break;
+      case 'E': ++vowelsCount[E]; break;
+      case 'I': ++vowelsCount[I]; break;
+      case 'O': ++vowelsCount[O]; break;
+      case 'U': ++vowelsCount[U]; break;
+      case 'a': ++vowelsCount[a]; break;
+      case 'e': ++vowelsCount[e]; break;
+      case 'i': ++vowelsCount[i]; break;
+      case 'o': ++vowelsCount[o]; break;
+      case 'u': ++vowelsCount[u]; break;
+        // clang-format on
+    }
+    ++c;
+  }
+
+  int vowel = A;
+  c = s;
+  while (*c != 0) {
+    switch (*c) {
+      // clang-format off
+      case 'A':
+      case 'E':
+      case 'I':
+      case 'O':
+      case 'U':
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+        while (vowelsCount[vowel] <= 0) ++vowel;
+        switch (vowel) {
+          case A: *c = 'A'; break;
+          case E: *c = 'E'; break;
+          case I: *c = 'I'; break;
+          case O: *c = 'O'; break;
+          case U: *c = 'U'; break;
+          case a: *c = 'a'; break;
+          case e: *c = 'e'; break;
+          case i: *c = 'i'; break;
+          case o: *c = 'o'; break;
+          case u: *c = 'u'; break;
+        }
+        --vowelsCount[vowel];
+        break;
+        // clang-format on
+    }
+    ++c;
+  }
+
   return s;
 }
