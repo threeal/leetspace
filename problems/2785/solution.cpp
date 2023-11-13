1a19904b878d72e16cab535a6efb236c4dd4a123
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-13 09:23:09 +0700

    feat(problem-2785/cpp): solve by using hash map to count vowels

--- problems/2785/solution.cpp
+++ problems/2785/solution.cpp
@@ -1,6 +1,22 @@
+const set<char> vowels = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
+
 class Solution {
  public:
   string sortVowels(string s) {
+    map<char, int> vowelsCount;
+    for (const auto c : s) {
+      if (!vowels.contains(c)) continue;
+      ++vowelsCount[c];
+    }
+
+    auto it = vowelsCount.begin();
+    for (auto& c : s) {
+      if (!vowels.contains(c)) continue;
+      while (it->second <= 0) ++it;
+      c = it->first;
+      --(it->second);
+    }
+
     return s;
   }
 };
