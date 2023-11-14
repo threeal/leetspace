534bd93088bddc1cd4bb3ea3183c5c12e6d18552
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-14 12:00:21 +0700

    perf(problem-1930/cpp): use single map for position tracking

--- problems/1930/solution.cpp
+++ problems/1930/solution.cpp
@@ -1,20 +1,19 @@
 class Solution {
  public:
   int countPalindromicSubsequence(string s) {
-    unordered_map<char, size_t> charsLeft;
-    unordered_map<char, size_t> charsRight;
+    unordered_map<char, pair<size_t, size_t>> charsPos;
     for (size_t i = 0; i < s.size(); ++i) {
-      if (!charsLeft.contains(s[i])) charsLeft[s[i]] = i;
-      charsRight[s[i]] = i;
+      const auto it = charsPos.find(s[i]);
+      if (it == charsPos.end())
+        charsPos[s[i]] = {i, i};
+      else
+        it->second.second = i;
     }
 
     int count = 0;
-    for (const auto [c, left] : charsLeft) {
-      if (!charsRight.contains(c)) continue;
-      auto right = charsRight[c];
-
+    for (const auto& [c, pos] : charsPos) {
       unordered_set<char> chars;
-      for (auto i = left + 1; i < right; ++i) {
+      for (auto i = pos.first + 1; i < pos.second; ++i) {
         chars.insert(s[i]);
       }
       count += chars.size();
