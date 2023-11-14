817a0671c0bc6a5856857b9d1b554dec1dc073ee
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-14 12:24:03 +0700

    docs(problem-1930/cpp): add comment that describe the solution

--- problems/1930/solution.cpp
+++ problems/1930/solution.cpp
@@ -1,6 +1,10 @@
+// The problem can be solved by finding the left and right pointers of each character
+// and then counting the unique characters between those pointers.
+
 class Solution {
  public:
   int countPalindromicSubsequence(string s) {
+    // Find the left and right pointers of each character.
     unordered_map<char, pair<size_t, size_t>> charsPos;
     for (size_t i = 0; i < s.size(); ++i) {
       const auto it = charsPos.find(s[i]);
@@ -10,6 +14,7 @@ class Solution {
         it->second.second = i;
     }
 
+    // Count the number of unique characters between each pair of pointers.
     int count = 0;
     for (const auto& [c, pos] : charsPos) {
       unordered_set<char> chars;
