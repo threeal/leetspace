cf2d71cf1077dc889e59543d787c8302b443c64f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-14 11:24:43 +0700

    feat(problem-1930/cpp): solve by using left and right pointers

--- problems/1930/solution.cpp
+++ problems/1930/solution.cpp
@@ -1,7 +1,25 @@
 class Solution {
  public:
   int countPalindromicSubsequence(string s) {
-    (void)s;
-    return 0;
+    unordered_map<char, size_t> charsLeft;
+    unordered_map<char, size_t> charsRight;
+    for (size_t i = 0; i < s.size(); ++i) {
+      if (!charsLeft.contains(s[i])) charsLeft[s[i]] = i;
+      charsRight[s[i]] = i;
+    }
+
+    int count = 0;
+    for (const auto [c, left] : charsLeft) {
+      if (!charsRight.contains(c)) continue;
+      auto right = charsRight[c];
+
+      unordered_set<char> chars;
+      for (auto i = left + 1; i < right; ++i) {
+        chars.insert(s[i]);
+      }
+      count += chars.size();
+    }
+
+    return count;
   }
 };
