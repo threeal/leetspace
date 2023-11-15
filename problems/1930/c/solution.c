52e7556ad86dc0c3f6846b7c573a4947a5c8570c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-15 10:24:50 +0700

    perf(problem-1930/c): map string characters so `a` be `0`

--- problems/1930/c/solution.c
+++ problems/1930/c/solution.c
@@ -7,11 +7,11 @@ int countPalindromicSubsequence(char* s) {
   // Find the left and right pointers of each character.
   char* charsIt[26][2] = {0};
   while (*s != 0) {
-    int c = *s - 'a';
-    if (charsIt[c][0] == 0) {
-      charsIt[c][0] = charsIt[c][1] = s;
+    *s -= 'a';
+    if (charsIt[*s][0] == 0) {
+      charsIt[*s][0] = charsIt[*s][1] = s;
     } else {
-      charsIt[c][1] = s;
+      charsIt[*s][1] = s;
     }
     ++s;
   }
@@ -20,10 +20,9 @@ int countPalindromicSubsequence(char* s) {
   int count = 0;
   for (int i = 0; i < 26; ++i) {
     bool charsExist[26] = {false};
-    for (char* ci = charsIt[i][0] + 1; ci < charsIt[i][1]; ++ci) {
-      int c = *ci - 'a';
-      if (charsExist[c]) continue;
-      charsExist[c] = true;
+    for (char* c = charsIt[i][0] + 1; c < charsIt[i][1]; ++c) {
+      if (charsExist[*c]) continue;
+      charsExist[*c] = true;
       ++count;
     }
   }
