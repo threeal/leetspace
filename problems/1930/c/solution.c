79de551cbec9f2a993dd87eb3f1c6aa339dbad81
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-15 10:15:24 +0700

    feat(problem-1930/c): solve by using left and right pointers

--- problems/1930/c/solution.c
+++ problems/1930/c/solution.c
@@ -1,4 +1,27 @@
+#include <stdbool.h>
+
 int countPalindromicSubsequence(char* s) {
-  (void)s;
-  return 0;
+  char* charsIt[26][2] = {0};
+  while (*s != 0) {
+    int c = *s - 'a';
+    if (charsIt[c][0] == 0) {
+      charsIt[c][0] = charsIt[c][1] = s;
+    } else {
+      charsIt[c][1] = s;
+    }
+    ++s;
+  }
+
+  int count = 0;
+  for (int i = 0; i < 26; ++i) {
+    bool charsExist[26] = {false};
+    for (char* ci = charsIt[i][0] + 1; ci < charsIt[i][1]; ++ci) {
+      int c = *ci - 'a';
+      if (charsExist[c]) continue;
+      charsExist[c] = true;
+      ++count;
+    }
+  }
+
+  return count;
 }
