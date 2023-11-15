a71e5e274a1db192adde2378271e1ca4a7c2ba7a
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-15 10:17:33 +0700

    docs(problem-1930/c): add comment that describe the solution

--- problems/1930/c/solution.c
+++ problems/1930/c/solution.c
@@ -1,6 +1,10 @@
+// The problem can be solved by finding the left and right pointers of each character
+// and then counting the unique characters between those pointers.
+
 #include <stdbool.h>
 
 int countPalindromicSubsequence(char* s) {
+  // Find the left and right pointers of each character.
   char* charsIt[26][2] = {0};
   while (*s != 0) {
     int c = *s - 'a';
@@ -12,6 +16,7 @@ int countPalindromicSubsequence(char* s) {
     ++s;
   }
 
+  // Count the number of unique characters between each pair of pointers.
   int count = 0;
   for (int i = 0; i < 26; ++i) {
     bool charsExist[26] = {false};
