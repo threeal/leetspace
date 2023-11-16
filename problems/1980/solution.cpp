df5170e4770989b749efa2c407ce8d9ec72603f7
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-16 10:59:45 +0700

    feat(problem-1980/cpp): solve by sorting and iterating

--- problems/1980/solution.cpp
+++ problems/1980/solution.cpp
@@ -1,7 +1,21 @@
 class Solution {
  public:
   string findDifferentBinaryString(vector<string>& nums) {
-    (void)nums;
-    return "";
+    sort(nums.begin(), nums.end());
+
+    string res(nums.size(), '0');
+    for (const auto& num : nums) {
+      if (num != res) break;
+
+      auto c = res.rbegin();
+      *c += 1;
+      while (*c == '2') {
+        *c = '0';
+        ++c;
+        *c += 1;
+      }
+    }
+
+    return res;
   }
 };
