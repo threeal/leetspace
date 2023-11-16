b3a879cd9d3502c8049f39b3ef34e4623d92a08c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-16 11:05:47 +0700

    docs(problem-1980/cpp): add comments that describe the solution

--- problems/1980/solution.cpp
+++ problems/1980/solution.cpp
@@ -1,12 +1,23 @@
+// The solution can be done as follows:
+// - First, sort the binary numbers.
+// - Declare a binary number 0 as an iterator.
+// - For each binary number, compare it with the iterator.
+// - If it is equal, increment the iterator; otherwise, return the iterator.
+
 class Solution {
  public:
   string findDifferentBinaryString(vector<string>& nums) {
+    // Sort the binary numbers.
     sort(nums.begin(), nums.end());
 
+    // Declare a binary number 0 as an iterator.
     string res(nums.size(), '0');
+
+    // For each binary number, compare it with the iterator.
     for (const auto& num : nums) {
       if (num != res) break;
 
+      // Increment the iterator.
       auto c = res.rbegin();
       *c += 1;
       while (*c == '2') {
