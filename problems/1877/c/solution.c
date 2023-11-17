6f73db7b23a79bbad7a2df282ce98195c9eb0ac3
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-17 09:33:31 +0700

    feat(problem-1877/c): solve by implementing quick sort and two pointers

--- problems/1877/c/solution.c
+++ problems/1877/c/solution.c
@@ -1,5 +1,39 @@
+void quickSort(int* nums, int low, int high) {
+  if (low >= high) return;
+
+  int pivot = nums[high];
+  int i = (low - 1);
+
+  for (int j = low; j < high; ++j) {
+    if (nums[j] >= pivot) continue;
+    ++i;
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+
+  ++i;
+  int temp = nums[i];
+  nums[i] = nums[high];
+  nums[high] = temp;
+
+  quickSort(nums, low, i - 1);
+  quickSort(nums, i + 1, high);
+}
+
 int minPairSum(int* nums, int numsSize) {
-  (void)nums;
-  (void)numsSize;
-  return 0;
+  quickSort(nums, 0, numsSize - 1);
+
+  int left = 0;
+  int right = numsSize - 1;
+
+  int res = 0;
+  while (left < right) {
+    if (nums[left] + nums[right] > res)
+      res = nums[left] + nums[right];
+    ++left;
+    --right;
+  }
+
+  return res;
 }
