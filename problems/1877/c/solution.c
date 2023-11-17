65a0f43367f77b1c45143d2b270fc40a3a4f61ce
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-17 09:50:58 +0700

    perf(problem-1877/c): use pointers in the quicksort

--- problems/1877/c/solution.c
+++ problems/1877/c/solution.c
@@ -1,28 +1,27 @@
-void quickSort(int* nums, int low, int high) {
+void quickSort(int* low, int* high) {
   if (low >= high) return;
 
-  int pivot = nums[high];
-  int i = (low - 1);
+  int* i = low - 1;
 
-  for (int j = low; j < high; ++j) {
-    if (nums[j] >= pivot) continue;
+  for (int* j = low; j < high; ++j) {
+    if (*j >= *high) continue;
     ++i;
-    int temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
+    const int temp = *i;
+    *i = *j;
+    *j = temp;
   }
 
   ++i;
-  int temp = nums[i];
-  nums[i] = nums[high];
-  nums[high] = temp;
+  const int temp = *i;
+  *i = *high;
+  *high = temp;
 
-  quickSort(nums, low, i - 1);
-  quickSort(nums, i + 1, high);
+  quickSort(low, i - 1);
+  quickSort(i + 1, high);
 }
 
 int minPairSum(int* nums, int numsSize) {
-  quickSort(nums, 0, numsSize - 1);
+  quickSort(nums, nums + numsSize - 1);
 
   int left = 0;
   int right = numsSize - 1;
