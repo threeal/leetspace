4266e9dec9d0df72d253d4ff6a469255517048a5
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-18 20:40:07 +0700

    feat(problem-1838/cpp): solve by using sort and sliding window

--- problems/1838/solution.cpp
+++ problems/1838/solution.cpp
@@ -1,8 +1,18 @@
 class Solution {
  public:
   int maxFrequency(vector<int>& nums, int k) {
-    (void)nums;
-    (void)k;
-    return 0;
+    sort(nums.begin(), nums.end());
+
+    size_t left = 0;
+    long curr = 0;
+    for (size_t right = 0; right < nums.size(); ++right) {
+      curr += nums[right];
+      if (long(right - left + 1) * nums[right] - curr > k) {
+        curr -= nums[left];
+        ++left;
+      }
+    }
+
+    return nums.size() - left;
   }
 };
