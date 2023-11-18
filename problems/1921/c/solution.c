97d4469bc2dca2aca84816dde848befe68c22ac8
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-18 22:26:22 +0700

    feat(problem-1921/c): try to solve by sorting and iterating

--- problems/1921/c/solution.c
+++ problems/1921/c/solution.c
@@ -1,7 +1,46 @@
+void quickSort(int* low, int* high);
+
 int eliminateMaximum(int* dist, int distSize, int* speed, int speedSize) {
-  (void)dist;
-  (void)distSize;
-  (void)speed;
+  // Calculate the time required before enemies reach distance zero.
   (void)speedSize;
-  return 0;
+
+  for (int i = 0; i < distSize; ++i) {
+    dist[i] = dist[i] / speed[i] + (dist[i] % speed[i] > 0 ? 1 : 0);
+  }
+
+  // Sort the time required from lowest to highest.
+  quickSort(dist, dist + distSize - 1);
+
+  // Time equals to kills. Iterate through the time required as long as the current time is not greater than the time required.
+  int kills = 0;
+  for (int i = 0; i < distSize; ++i) {
+    if (kills + 1 > dist[i]) break;
+    ++kills;
+  }
+
+  return kills;
+}
+
+// This function implements quick sort to sort the given range of arrays.
+// See: https://www.geeksforgeeks.org/quick-sort/
+void quickSort(int* low, int* high) {
+  if (low >= high) return;
+
+  int* i = low - 1;
+
+  for (int* j = low; j < high; ++j) {
+    if (*j >= *high) continue;
+    ++i;
+    const int temp = *i;
+    *i = *j;
+    *j = temp;
+  }
+
+  ++i;
+  const int temp = *i;
+  *i = *high;
+  *high = temp;
+
+  quickSort(low, i - 1);
+  quickSort(i + 1, high);
 }
