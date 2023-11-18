2445c882c8e8d13d5ec9fc62466aa81e6bb6a620
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-18 20:54:58 +0700

    docs(problem-1838): add comment on how i got the solution

--- problems/1838/solution.cpp
+++ problems/1838/solution.cpp
@@ -1,3 +1,6 @@
+// TLDR: I don't know how they derived this solution, I just found it in the editorial:
+// https://leetcode.com/problems/frequency-of-the-most-frequent-element/editorial/
+
 class Solution {
  public:
   int maxFrequency(vector<int>& nums, int k) {
