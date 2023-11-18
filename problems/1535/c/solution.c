e9ec94b038b19a56b902c77566d4dbf8e81cfba4
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-18 22:02:54 +0700

    feat(problem-1535/c): solve by using for loop

--- problems/1535/c/solution.c
+++ problems/1535/c/solution.c
@@ -1,6 +1,16 @@
 int getWinner(int* arr, int arrSize, int k) {
-  (void)arr;
-  (void)arrSize;
-  (void)k;
-  return 0;
+  int win = 0;
+  int ref = 0;
+
+  for (int i = 1; i < arrSize; ++i) {
+    if (win >= k) break;
+    if (arr[i] > arr[ref]) {
+      win = 1;
+      ref = i;
+    } else {
+      ++win;
+    }
+  }
+
+  return arr[ref];
 }
