43b71af9082e96512059473735686bb7848ba057
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-18 21:12:28 +0700

    feat(problem-1503/c): solve by calculating min and max elements

--- problems/1503/c/solution.c
+++ problems/1503/c/solution.c
@@ -1,8 +1,15 @@
 int getLastMoment(int n, int* left, int leftSize, int* right, int rightSize) {
-  (void)n;
-  (void)left;
-  (void)leftSize;
-  (void)right;
-  (void)rightSize;
-  return 0;
+  int leftMax = 0;
+  for (int i = 0; i < leftSize; ++i) {
+    if (left[i] > leftMax) leftMax = left[i];
+  }
+
+  int rightMin = 10000;
+  for (int i = 0; i < rightSize; ++i) {
+    if (right[i] < rightMin) rightMin = right[i];
+  }
+
+  if (leftSize == 0) return n - rightMin;
+  if (rightSize == 0) return leftMax;
+  return (leftMax > n - rightMin) ? leftMax : n - rightMin;
 }
