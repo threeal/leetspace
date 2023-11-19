e4001d5f1eaeb38e12b85e14a20ddbbe55c34c70
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-19 17:42:03 +0700

    feat(problem-1887/cpp): try to solve by counting frequencies

--- problems/1887/solution.cpp
+++ problems/1887/solution.cpp
@@ -1,7 +1,21 @@
+#include <map>
+#include <vector>
+
 class Solution {
  public:
-  int reductionOperations(vector<int>& nums) {
-    (void)nums;
-    return 0;
+  int reductionOperations(std::vector<int>& nums) {
+    std::map<int, int, std::greater<int>> frequencies;
+    for (const auto num : nums) {
+      ++frequencies[num];
+    }
+
+    frequencies.erase(std::prev(frequencies.end()));
+
+    int operations = 0;
+    for (const auto [num, freq] : frequencies) {
+      operations += operations + freq;
+    }
+
+    return operations;
   }
 };
