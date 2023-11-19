e59cecf7180553729bfda2f910c73da30a71c31c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-19 20:29:56 +0700

    feat(problem-1846/c): solve by using merge sort algorithm

--- problems/1846/c/solution.c
+++ problems/1846/c/solution.c
@@ -1,10 +1,12 @@
-void quickSort(int* low, int* high);
+#include <stdlib.h>
+
+void mergeSort(int* begin, int* end);
 
 int maximumElementAfterDecrementingAndRearranging(int* arr, int arrSize) {
   // Sort the time required from lowest to highest (don't sort if already sorted).
   for (int i = 1; i < arrSize; ++i) {
     if (arr[i - 1] > arr[i]) {
-      quickSort(arr, arr + arrSize - 1);
+      mergeSort(arr, arr + arrSize - 1);
       break;
     }
   }
@@ -17,26 +19,54 @@ int maximumElementAfterDecrementingAndRearranging(int* arr, int arrSize) {
   return res;
 }
 
-// This function implements quick sort to sort the given range of arrays.
-// See: https://www.geeksforgeeks.org/quick-sort/
-void quickSort(int* low, int* high) {
-  if (low >= high) return;
+// This function implements merge sort to sort the given range of arrays.
+// See: https://www.geeksforgeeks.org/merge-sort/
+void mergeSort(int* begin, int* end) {
+  if (begin >= end) return;
 
-  int* i = low - 1;
+  int* mid = begin + (end - begin) / 2;
+  mergeSort(begin, mid);
+  mergeSort(mid + 1, end);
 
-  for (int* j = low; j < high; ++j) {
-    if (*j >= *high) continue;
-    ++i;
-    const int temp = *i;
-    *i = *j;
-    *j = temp;
+  const int leftSize = mid - begin + 1;
+  int* left = malloc(sizeof(int) * leftSize);
+  for (int i = 0; i < leftSize; ++i) {
+    left[i] = begin[i];
   }
 
-  ++i;
-  const int temp = *i;
-  *i = *high;
-  *high = temp;
+  const int rightSize = end - mid;
+  int* right = malloc(sizeof(int) * rightSize);
+  for (int i = 0; i < rightSize; ++i) {
+    right[i] = mid[i + 1];
+  }
 
-  quickSort(low, i - 1);
-  quickSort(i + 1, high);
+  int leftIdx = 0;
+  int rightIdx = 0;
+  int* it = begin;
+
+  while (leftIdx < leftSize && rightIdx < rightSize) {
+    if (left[leftIdx] <= right[rightIdx]) {
+      *it = left[leftIdx];
+      ++leftIdx;
+    } else {
+      *it = right[rightIdx];
+      ++rightIdx;
+    }
+    ++it;
+  }
+
+  while (leftIdx < leftSize) {
+    *it = left[leftIdx];
+    ++leftIdx;
+    ++it;
+  }
+
+  while (rightIdx < rightSize) {
+    *it = right[rightIdx];
+    ++rightIdx;
+    ++it;
+  }
+
+  free(left);
+  free(right);
 }
