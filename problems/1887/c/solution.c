9a83379ded5a999fad2d85d14e1826beca1ac5bf
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-19 18:16:10 +0700

    feat(problem-1887/c): solve by counting frequencies

--- problems/1887/c/solution.c
+++ problems/1887/c/solution.c
@@ -1,5 +1,21 @@
 int reductionOperations(int* nums, int numsSize) {
-  (void)nums;
-  (void)numsSize;
-  return 0;
+  int maxNum = 1;
+  int minNum = 5000;
+
+  int frequencies[50001] = {0};
+  for (int i = 0; i < numsSize; ++i) {
+    ++frequencies[nums[i]];
+    if (nums[i] > maxNum) maxNum = nums[i];
+    if (nums[i] < minNum) minNum = nums[i];
+  }
+
+  int frequencySum = 0;
+  int operations = 0;
+  for (int num = maxNum; num > minNum; --num) {
+    if (frequencies[num] == 0) continue;
+    frequencySum += frequencies[num];
+    operations += frequencySum;
+  }
+
+  return operations;
 }
