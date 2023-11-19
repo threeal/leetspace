1cb6c5be931da0a00652296be53c458b8684ce59
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-19 20:29:37 +0700

    feat(problem-1846/c): try to solve by sorting and iterating

--- problems/1846/c/solution.c
+++ problems/1846/c/solution.c
@@ -1,5 +1,36 @@
+void quickSort(int* low, int* high);
+
 int maximumElementAfterDecrementingAndRearranging(int* arr, int arrSize) {
-  (void)arr;
-  (void)arrSize;
-  return 0;
+  quickSort(arr, arr + arrSize - 1);
+
+  int res = 0;
+  for (int i = 0; i < arrSize; ++i) {
+    res = res + 1 < arr[i] ? res + 1 : arr[i];
+  }
+
+  return res;
+}
+
+// This function implements quick sort to sort the given range of arrays.
+// See: https://www.geeksforgeeks.org/quick-sort/
+void quickSort(int* low, int* high) {
+  if (low >= high) return;
+
+  int* i = low - 1;
+
+  for (int* j = low; j < high; ++j) {
+    if (*j >= *high) continue;
+    ++i;
+    const int temp = *i;
+    *i = *j;
+    *j = temp;
+  }
+
+  ++i;
+  const int temp = *i;
+  *i = *high;
+  *high = temp;
+
+  quickSort(low, i - 1);
+  quickSort(i + 1, high);
 }
