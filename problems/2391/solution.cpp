51f95d6b08eee902a78b1a5cf002efe6985d79c6
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-20 10:02:17 +0700

    refactor(problem-2391/cpp): use enum for simplicity

--- problems/2391/solution.cpp
+++ problems/2391/solution.cpp
@@ -1,49 +1,42 @@
 #include <string>
 #include <vector>
 
+// clang-format off
+enum GarbageType { Metal = 0, Paper, Glass };
+// clang-format on
+
 class Solution {
  public:
   int garbageCollection(std::vector<std::string>& garbage, std::vector<int>& travel) {
-    int metalTotalTime = 0, paperTotalTime = 0, glassTotalTime = 0;
-    int metalTravelTime = 0, paperTravelTime = 0, glassTravelTime = 0;
+    int totalTime[3] = {0};
+    int travelTime[3] = {0};
+
     for (size_t i = 0; i < garbage.size(); ++i) {
       if (i > 0) {
-        metalTravelTime += travel[i - 1];
-        paperTravelTime += travel[i - 1];
-        glassTravelTime += travel[i - 1];
-      }
-
-      int metalTime = 0, paperTime = 0, glassTime = 0;
-      for (const auto c : garbage[i]) {
-        switch (c) {
-          case 'M':
-            ++metalTime;
-            break;
-          case 'P':
-            ++paperTime;
-            break;
-          case 'G':
-            ++glassTime;
-            break;
+        for (size_t j = 0; j < 3; ++j) {
+          travelTime[j] += travel[i - 1];
         }
       }
 
-      if (metalTime > 0) {
-        metalTotalTime += metalTravelTime + metalTime;
-        metalTravelTime = 0;
+      int time[3] = {0};
+      for (const auto c : garbage[i]) {
+        switch (c) {
+          // clang-format off
+          case 'M': ++time[Metal]; break;
+          case 'P': ++time[Paper]; break;
+          case 'G': ++time[Glass]; break;
+            // clang-format on
+        }
       }
 
-      if (paperTime > 0) {
-        paperTotalTime += paperTravelTime + paperTime;
-        paperTravelTime = 0;
-      }
-
-      if (glassTime > 0) {
-        glassTotalTime += glassTravelTime + glassTime;
-        glassTravelTime = 0;
+      for (size_t j = 0; j < 3; ++j) {
+        if (time[j] > 0) {
+          totalTime[j] += travelTime[j] + time[j];
+          travelTime[j] = 0;
+        }
       }
     }
 
-    return metalTotalTime + paperTotalTime + glassTotalTime;
+    return totalTime[Metal] + totalTime[Paper] + totalTime[Glass];
   }
 };
