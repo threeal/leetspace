a134a0b35b61c44bf8d88445d6654dbf4f903e08
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-21 10:18:37 +0700

    feat(problem-1814/cpp): try to solve by mapping differences

--- problems/1814/solution.cpp
+++ problems/1814/solution.cpp
@@ -1,9 +1,31 @@
+#include <unordered_map>
 #include <vector>
+
+int rev(int val);
 
 class Solution {
  public:
   int countNicePairs(std::vector<int>& nums) {
-    (void)nums;
-    return 0;
+    std::unordered_map<int, int> diffsCounts;
+    for (const auto num : nums) {
+      ++diffsCounts[num - rev(num)];
+    }
+
+    int totalCount = 0;
+    for (const auto [diff, count] : diffsCounts) {
+      totalCount += count * (count - 1) / 2;
+    }
+
+    return totalCount;
   }
 };
+
+int rev(int num) {
+  int revNum = 0;
+  while (num > 0) {
+    revNum = revNum * 10 + num % 10;
+    num /= 10;
+  }
+
+  return revNum;
+}
