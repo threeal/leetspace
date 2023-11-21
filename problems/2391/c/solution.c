eed0fac37e673a63506c5b5a1effb1b421b9288c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-21 10:57:45 +0700

    docs(problem-2391/cpp): add comment that describe the solution

--- problems/2391/c/solution.c
+++ problems/2391/c/solution.c
@@ -1,3 +1,6 @@
+// The solution can simply be done by iterating over the garbage to sum the required times.
+// To calculate the travel time, one should only add it to the total time if there's some garbage in a house.
+
 // clang-format off
 enum GarbageType { Metal = 0, Paper, Glass };
 // clang-format on
@@ -8,13 +11,16 @@ int garbageCollection(char** garbage, int garbageSize, int* travel, int travelSi
   int totalTime[3] = {0};
   int travelTime[3] = {0};
 
+  // Iterating over the garbage in every house.
   for (int i = 0; i < garbageSize; ++i) {
+    // Remember the travel time, don't add it to the total time yet.
     if (i > 0) {
       for (int j = 0; j < 3; ++j) {
         travelTime[j] += travel[i - 1];
       }
     }
 
+    // Calculate garbage in the house.
     int time[3] = {0};
     char* ci = garbage[i];
     while (*ci != 0) {
@@ -28,6 +34,7 @@ int garbageCollection(char** garbage, int garbageSize, int* travel, int travelSi
       ++ci;
     }
 
+    // Add garbage count and travel time if there's some garbage in the house.
     for (int j = 0; j < 3; ++j) {
       if (time[j] > 0) {
         totalTime[j] += travelTime[j] + time[j];
@@ -36,5 +43,6 @@ int garbageCollection(char** garbage, int garbageSize, int* travel, int travelSi
     }
   }
 
+  // Return the total time required by all types of garbage.
   return totalTime[Metal] + totalTime[Paper] + totalTime[Glass];
 }
