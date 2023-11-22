c02e6fdaf8cfb3bc82d267a1e942cc8ecf7c9b64
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-22 11:03:08 +0700

    feat(problem-1424/cpp): try to solve by simply iterating elements

--- problems/1424/solution.cpp
+++ problems/1424/solution.cpp
@@ -3,7 +3,24 @@
 class Solution {
  public:
   std::vector<int> findDiagonalOrder(std::vector<std::vector<int>>& nums) {
-    (void)nums;
-    return {};
+    size_t numsCount = 0;
+    for (const auto& subnums : nums) {
+      numsCount += subnums.size();
+    }
+
+    std::vector<int> result(numsCount);
+
+    size_t i = 0;
+    size_t y = 0;
+    while (i < numsCount) {
+      for (size_t x = 0; x <= y; ++x) {
+        if (y - x >= nums.size()) continue;
+        if (x >= nums[y - x].size()) continue;
+        result[i++] = nums[y - x][x];
+      }
+      ++y;
+    }
+
+    return result;
   }
 };
