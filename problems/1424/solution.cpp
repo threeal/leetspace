6b3ecf1d9ba44d961bdffa4d83df4a47ae2499b6
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-22 11:50:19 +0700

    feat: solve problem 1424. Diagonal Traverse II (#204)
    * test: add test for problem 1424. Diagonal Traverse II
    
    * feat(problem-1424/cpp): try to solve by simply iterating elements
    
    * feat(problem-1424/cpp): solve by pushing to diagonals
    
    * docs(problem-1424/cpp): add comment that describe the solution

---
+++ problems/1424/solution.cpp
@@ -0,0 +1,35 @@
+// The solution can be done by categorizing each number to its diagonal.
+// After that, merge all numbers in the diagonals into a single array.
+
+#include <algorithm>
+#include <list>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> findDiagonalOrder(std::vector<std::vector<int>>& nums) {
+    // Calculate the maximum diagonal count.
+    size_t diagonalCount = 0;
+    for (size_t y = 0; y < nums.size(); ++y) {
+      diagonalCount = std::max(diagonalCount, y + nums[y].size());
+    }
+
+    // Push each number into its diagonal.
+    std::vector<std::list<int>> diagonals(diagonalCount);
+    for (size_t y = 0; y < nums.size(); ++y) {
+      for (size_t x = 0; x < nums[y].size(); ++x) {
+        diagonals[y + x].push_front(nums[y][x]);
+      }
+    }
+
+    // Merge all numbers in the diagonals into a single array.
+    std::vector<int> result;
+    for (const auto& diagonal : diagonals) {
+      for (const auto num : diagonal) {
+        result.push_back(num);
+      }
+    }
+
+    return result;
+  }
+};
