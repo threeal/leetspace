e4283742dd44c8b84529e0812afdb39b0cecec2e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-22 11:31:55 +0700

    feat(problem-1424/cpp): solve by pushing to diagonals

--- problems/1424/solution.cpp
+++ problems/1424/solution.cpp
@@ -1,24 +1,27 @@
+#include <algorithm>
+#include <list>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> findDiagonalOrder(std::vector<std::vector<int>>& nums) {
-    size_t numsCount = 0;
-    for (const auto& subnums : nums) {
-      numsCount += subnums.size();
+    size_t diagonalCount = 0;
+    for (size_t y = 0; y < nums.size(); ++y) {
+      diagonalCount = std::max(diagonalCount, y + nums[y].size());
     }
 
-    std::vector<int> result(numsCount);
-
-    size_t i = 0;
-    size_t y = 0;
-    while (i < numsCount) {
-      for (size_t x = 0; x <= y; ++x) {
-        if (y - x >= nums.size()) continue;
-        if (x >= nums[y - x].size()) continue;
-        result[i++] = nums[y - x][x];
+    std::vector<std::list<int>> diagonals(diagonalCount);
+    for (size_t y = 0; y < nums.size(); ++y) {
+      for (size_t x = 0; x < nums[y].size(); ++x) {
+        diagonals[y + x].push_front(nums[y][x]);
       }
-      ++y;
+    }
+
+    std::vector<int> result;
+    for (const auto& diagonal : diagonals) {
+      for (const auto num : diagonal) {
+        result.push_back(num);
+      }
     }
 
     return result;
