68c2cb67927a8484d877f8ede159ca264b57389d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-23 20:23:09 +0700

    test: add test for problem 1630. Arithmetic Subarrays

---
+++ problems/1630/solution.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<bool> checkArithmeticSubarrays(std::vector<int>& nums, std::vector<int>& l, std::vector<int>& r) {
+    (void)nums;
+    (void)l;
+    (void)r;
+    return {};
+  }
+};
