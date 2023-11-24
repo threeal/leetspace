d19361df549a3721acf05f06bdcceffada5a2761
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-24 09:50:54 +0700

    feat(problem-1561/cpp): solve by sorting and iterating

--- problems/1561/solution.cpp
+++ problems/1561/solution.cpp
@@ -1,9 +1,16 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maxCoins(std::vector<int>& piles) {
-    (void)piles;
-    return 0;
+    std::sort(piles.begin(), piles.end());
+
+    int total = 0;
+    for (size_t i = 1; i <= piles.size() / 3; ++i) {
+      total += piles[piles.size() - 2 * i];
+    }
+
+    return total;
   }
 };
