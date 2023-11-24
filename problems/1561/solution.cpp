479fb91f0edbd0191977cd0108636f5066d8a5ec
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-24 09:55:48 +0700

    docs(problem-1561/cpp): add comments that describe the solution

--- problems/1561/solution.cpp
+++ problems/1561/solution.cpp
@@ -1,11 +1,17 @@
+// The solution to maximize the number of coins is by always picking the second-highest pile.
+// To do this, one can sort the piles and then iterate over the piles n / 3 times
+// to get the second-highest pile in each iteration.
+
 #include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maxCoins(std::vector<int>& piles) {
+    // Sort each pile from lowest to highest.
     std::sort(piles.begin(), piles.end());
 
+    // Iterate n / 3 times to get the second-highest pile in each iteration.
     int total = 0;
     for (size_t i = 1; i <= piles.size() / 3; ++i) {
       total += piles[piles.size() - 2 * i];
