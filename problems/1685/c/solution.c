e2f83b887457a37b4908ca8750f99d6195b10024
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-25 22:20:41 +0800

    feat: solve problem 1685. Sum of Absolute Differences in a Sorted Array in C language (#215)
    * test(problem-1685): add support to solve in C language
    
    * feat(problem-1685/c): solve by calculating total
    
    * docs(problem-1685/c): add comment that describe the solution

---
+++ problems/1685/c/solution.c
@@ -0,0 +1,48 @@
+// Here's the clue from the example:
+//
+// nums: [1, 4, 6, 8, 10]
+// output: [24, 15, 13, 15, 21]
+//
+// The output[2] can be calculated as follows:
+//
+// output[2] = abs(1 - 6) + abs(4 - 6) + abs(6 - 6) + abs(8 - 6) + abs(10 - 6)
+// output[2] = 5 + 2 + 2 + 4
+// output[2] = 13
+//
+// But since we know the array is sorted, we can ignore absolute here by adjusting lhs and rhs accordingly:
+//
+// output[2] = (6 - 1) + (6 - 4) + (6 - 6) + (8 - 6) + (10 - 6)
+//
+// Or we can just simplify it like this:
+//
+// output[2] = (6 * 2) - (1 + 4) + (8 + 10) - (6 * 2)
+// output[i] = (nums[i] * i) - leftTotal + rightTotal - (nums[i] * (n - i - 1))
+//
+// From that clue, we can solve this problem as follows:
+// - Calculate the total, save it as the right total.
+// - Iterate each element, shift the number from the right total to the left total.
+// - Modify the element according to the above equation.
+// - Return the modified array.
+
+int* getSumAbsoluteDifferences(int* nums, int numsSize, int* returnSize) {
+  // Calculate the right total.
+  int rightTotal = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    rightTotal += nums[i];
+  }
+
+  // Iterate each element to update the output.
+  int leftTotal = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    // Shift value from the right total to the left total.
+    leftTotal += nums[i];
+    rightTotal -= nums[i];
+
+    // Update the output.
+    nums[i] = (nums[i] * (i + 1) - leftTotal) + (rightTotal - nums[i] * (numsSize - i - 1));
+  }
+
+  // Return the modified array.
+  *returnSize = numsSize;
+  return nums;
+}
