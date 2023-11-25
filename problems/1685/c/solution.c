15046b68f4c1bc174211a3abcc4d2f5643e71116
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-25 21:15:52 +0700

    feat(problem-1685/c): solve by calculating total

--- problems/1685/c/solution.c
+++ problems/1685/c/solution.c
@@ -1,4 +1,17 @@
 int* getSumAbsoluteDifferences(int* nums, int numsSize, int* returnSize) {
+  int rightTotal = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    rightTotal += nums[i];
+  }
+
+  int leftTotal = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    leftTotal += nums[i];
+    rightTotal -= nums[i];
+
+    nums[i] = (nums[i] * (i + 1) - leftTotal) + (rightTotal - nums[i] * (numsSize - i - 1));
+  }
+
   *returnSize = numsSize;
   return nums;
 }
