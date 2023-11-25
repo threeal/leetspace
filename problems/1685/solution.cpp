d98776c4b0c16bf92458a81ec7bf12b13e85b993
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-25 20:36:28 +0700

    feat(problem-1685/cpp): solve by calculating total

--- problems/1685/solution.cpp
+++ problems/1685/solution.cpp
@@ -1,19 +1,20 @@
-#include <cmath>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> getSumAbsoluteDifferences(std::vector<int>& nums) {
-    std::vector<int> res(nums.size(), 0);
-
-    for (size_t i = 0; i < nums.size(); ++i) {
-      for (size_t j = i + 1; j < nums.size(); ++j) {
-        const int diff = std::abs(nums[j] - nums[i]);
-        res[i] += diff;
-        res[j] += diff;
-      }
+    int rightTotal = 0;
+    for (const auto num : nums) {
+      rightTotal += num;
     }
 
-    return res;
+    int leftTotal = 0;
+    for (size_t i = 0; i < nums.size(); ++i) {
+      leftTotal += nums[i];
+      rightTotal -= nums[i];
+      nums[i] = (nums[i] * (i + 1) - leftTotal) + (rightTotal - nums[i] * (nums.size() - i - 1));
+    }
+
+    return nums;
   }
 };
