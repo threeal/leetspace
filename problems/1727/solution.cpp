d9d0e57fda7a0f6b1a733a1fd9bf5f607c599182
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-26 19:14:04 +0700

    feat(problem-1727/cpp): solve by modify matrix and for loop

--- problems/1727/solution.cpp
+++ problems/1727/solution.cpp
@@ -1,9 +1,25 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int largestSubmatrix(std::vector<std::vector<int>>& matrix) {
-    (void)matrix;
-    return 0;
+    for (size_t y = 1; y < matrix.size(); ++y) {
+      for (size_t x = 0; x < matrix[y].size(); ++x) {
+        if (matrix[y][x] == 0) continue;
+        matrix[y][x] = matrix[y - 1][x] + 1;
+      }
+    }
+
+    int maxArea = 0;
+    for (auto& rows : matrix) {
+      sort(rows.begin(), rows.end(), std::greater<int>());
+
+      for (size_t i = 0; i < rows.size(); ++i) {
+        maxArea = std::max<int>(maxArea, (i + 1) * rows[i]);
+      }
+    }
+
+    return maxArea;
   }
 };
