d953d5028a1d7ba1d0c981bd460ca9033c506cb1
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-27 21:08:22 +0700

    docs(problem-935/cpp): add comment that describe the solution

--- problems/0935/solution.cpp
+++ problems/0935/solution.cpp
@@ -1,3 +1,26 @@
+// The solution can simply be done by using a for loop.
+//
+// Think about it as an inheritance:
+//
+// - A number ending with 0 will be inherited to a number ending with 4 and 6
+// - A number ending with 1 will be inherited to a number ending with 6 and 8
+// - And so on...
+//
+// For example:
+//
+// - 0 => 04, 06
+// - 1 => 16, 18
+// - And so on...
+//
+// So to count the number, we can do the following:
+//
+// - endedWith6 = endedWith0 + endedWith1
+// - endedWith7 = endedWith2 + endedWith6
+// - And so on...
+//
+// At the end of the loop, we can just total the number count.
+// And don't forget to handle the mod 10e9+7.
+
 #include <array>
 
 const unsigned int mod = 1'000'000'007;
@@ -7,9 +30,12 @@ class Solution {
   int knightDialer(int n) {
     if (n == 1) return 10;
 
+    // Count of numbers ending with i (0 is 2, 1 is 2, ... 4 is 3, 5 is 0, ...)
     std::array<unsigned int, 10> nums = {2, 2, 2, 2, 3, 0, 3, 2, 2, 2};
 
+    // Repeat n - 2 times.
     while (--n > 1) {
+      // 0 depends on 4 and 6, 1 depends on 6 and 8, ...
       nums = {
           (nums[4] + nums[6]) % mod,
           (nums[6] + nums[8]) % mod,
@@ -23,6 +49,7 @@ class Solution {
           (nums[2] + nums[4]) % mod};
     }
 
+    // Calculate total count.
     unsigned int total = 0;
     for (unsigned int i = 0; i < 10; ++i) {
       total = (total + nums[i]) % mod;
