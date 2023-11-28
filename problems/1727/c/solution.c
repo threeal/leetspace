77b65600f438d73a9f65355232b7bb766dcf274e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-28 11:06:05 +0700

    docs(problem-1727/c): add comment that describe the solution

--- problems/1727/c/solution.c
+++ problems/1727/c/solution.c
@@ -1,3 +1,37 @@
+// We will do a brute force search here to find the maximum area of the submatrix.
+// But before that, we need to adjust each element of the matrix so it contains
+// 1 + the number of the above elements whose value is 1.
+//
+// Given the example input:
+//
+//   [ 0 0 1 ]
+//   [ 1 1 1 ]
+//   [ 1 0 1 ]
+//
+// We will adjust it to be:
+//
+//   [ 0 0 1 ]
+//   [ 1 1 2 ]
+//   [ 2 0 3 ]
+//
+// And then we can just iterate each row to find the maximum area.
+// To calculate each row, we can sort the row first from highest to lowest
+// and then calculate the area by multiplying the element value with the element index + 1.
+//
+// Given the second row of the adjusted example matrix:
+//
+//   [ 1 2 2 ]
+//
+// We can sort it to be:
+//
+//   [ 2 2 1 ]
+//
+// And we can just choose the largest area as follows:
+//
+//   2 * 1 = 2
+//   2 * 2 = 4 (largest)
+//   1 * 3 = 3
+
 void reversedQuickSort(int* low, int* high);
 
 int largestSubmatrix(int** matrix, int matrixSize, int* matrixColSize) {
