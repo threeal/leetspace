a5db7800d8898f4f9c77857ef4325759e76fc606
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-28 11:06:03 +0700

    test(problem-1727): add support to solve in C language

---
+++ problems/1727/c/interface.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+extern "C" {
+int largestSubmatrix(int** matrix, int matrixSize, int* matrixColSize);
+}
+
+int solution_c(std::vector<std::vector<int>> matrix) {
+  std::vector<int*> matrixData(matrix.size());
+  std::vector<int> matrixColSize(matrix.size());
+  for (size_t i = 0; i < matrix.size(); ++i) {
+    matrixData[i] = matrix[i].data();
+    matrixColSize[i] = matrix[i].size();
+  }
+
+  return largestSubmatrix(matrixData.data(), matrix.size(), matrixColSize.data());
+}
---
+++ problems/1727/c/solution.c
@@ -0,0 +1,6 @@
+int largestSubmatrix(int** matrix, int matrixSize, int* matrixColSize) {
+  (void)matrix;
+  (void)matrixSize;
+  (void)matrixColSize;
+  return 0;
+}
