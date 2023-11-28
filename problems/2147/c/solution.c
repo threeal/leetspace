d1a7790db95d22503db48ab3fd2512b3acd0e263
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-28 20:30:47 +0700

    docs(problem-2147/c): add comment that describe the solution

--- problems/2147/c/solution.c
+++ problems/2147/c/solution.c
@@ -1,3 +1,6 @@
+// The solution is simple, just count the number of plants between two seats and multiply each of them.
+// Tidak layak dikategorikan hard wkwk
+
 const unsigned long mod = 1000000007;
 
 int numberOfWays(char* corridor) {
@@ -6,18 +9,26 @@ int numberOfWays(char* corridor) {
   unsigned char seats = 0;
   unsigned long plants = 0;
 
+  // Iterate through each character of the corridor string.
   while (*corridor != 0) {
+    // Repeat until it finds 2 seats.
     if (seats < 2) {
       if (*corridor == 'S') ++seats;
     } else {
+      // If 2 seats are found, check the next character.
       switch (*corridor) {
         case 'S':
+          // If it is a seat, calculate the total count by multiplying it
+          // with the number of plants + 1.
           if (plants > 0) count = (count * ++plants) % mod;
+
+          // Reset the plants and seats counting.
           plants = 0;
           seats = 1;
           break;
 
         case 'P':
+          // Count each next consecutive plant.
           ++plants;
           break;
       }
@@ -25,6 +36,9 @@ int numberOfWays(char* corridor) {
     ++corridor;
   }
 
+  // If the previous seats counting is not 2
+  // then the total counting does not matter.
   if (seats != 2) return 0;
+
   return count;
 }
