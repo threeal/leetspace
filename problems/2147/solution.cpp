2dbb29eaf56a150af43863e372aab0c56aff1ced
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-28 19:43:32 +0700

    feat(problem-2147/cpp): try to solve by for looping and counting

--- problems/2147/solution.cpp
+++ problems/2147/solution.cpp
@@ -1,9 +1,34 @@
 #include <string>
 
+const unsigned long mod = 1'000'000'009;
+
 class Solution {
  public:
   int numberOfWays(std::string corridor) {
-    (void)corridor;
-    return 0;
+    unsigned long count = 1;
+
+    unsigned char seats = 0;
+    unsigned long plants = 0;
+
+    for (const auto c : corridor) {
+      if (seats < 2) {
+        if (c == 'S') ++seats;
+      } else {
+        switch (c) {
+          case 'S':
+            if (plants > 0) count = (count * ++plants) % mod;
+            plants = 0;
+            seats = 1;
+            break;
+
+          case 'P':
+            ++plants;
+            break;
+        }
+      }
+    }
+
+    if (seats == 1) return 0;
+    return count;
   }
 };
