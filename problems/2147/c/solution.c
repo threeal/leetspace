ea8ed5bd6b26ed00317419270c2eeeb837cd91ed
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-28 20:30:42 +0700

    feat(problem-2147/c): solve by iterating and counting

--- problems/2147/c/solution.c
+++ problems/2147/c/solution.c
@@ -1,3 +1,30 @@
+const unsigned long mod = 1000000007;
+
 int numberOfWays(char* corridor) {
-  return 0;
+  unsigned long count = 1;
+
+  unsigned char seats = 0;
+  unsigned long plants = 0;
+
+  while (*corridor != 0) {
+    if (seats < 2) {
+      if (*corridor == 'S') ++seats;
+    } else {
+      switch (*corridor) {
+        case 'S':
+          if (plants > 0) count = (count * ++plants) % mod;
+          plants = 0;
+          seats = 1;
+          break;
+
+        case 'P':
+          ++plants;
+          break;
+      }
+    }
+    ++corridor;
+  }
+
+  if (seats != 2) return 0;
+  return count;
 }
