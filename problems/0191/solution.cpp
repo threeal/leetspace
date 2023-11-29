4ad17c50aaa5787a11e674b0501c1de7c74c79d8
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-29 12:31:57 +0700

    docs(problem-191/cpp): add comment that describe the solution

--- problems/0191/solution.cpp
+++ problems/0191/solution.cpp
@@ -3,6 +3,7 @@
 class Solution {
  public:
   int hammingWeight(uint32_t n) {
+    // See this: https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html#index-_005f_005fbuiltin_005fpopcount.
     return __builtin_popcount(n);
   }
 };
