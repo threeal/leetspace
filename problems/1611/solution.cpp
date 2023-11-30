6426848434b0760b874529cfe9425d0e7af6611c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-30 20:14:10 +0800

    feat: solve problem 1611. Minimum One Bit Operations to Make Integers Zero (#236)
    * test: add test for problem 1611. Minimum One Bit Operations to Make Integers Zero
    
    * feat(problem-1611/cpp): solve by using Gray code
    
    * docs(problem-1611/cpp): add comment that describe the solution

---
+++ problems/1611/solution.cpp
@@ -0,0 +1,13 @@
+// IDK man, see this: https://en.wikipedia.org/wiki/Gray_code#Converting_to_and_from_Gray_code
+
+class Solution {
+ public:
+  int minimumOneBitOperations(int n) {
+    n ^= n >> 16;
+    n ^= n >> 8;
+    n ^= n >> 4;
+    n ^= n >> 2;
+    n ^= n >> 1;
+    return n;
+  }
+};
