7c93a26a973099c866c68577c5d8c8ba0c99285b
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-01 11:42:21 +0700

    feat(problem-1662/cpp): solve by using iterator and while loop

--- problems/1662/solution.cpp
+++ problems/1662/solution.cpp
@@ -4,8 +4,18 @@
 class Solution {
  public:
   bool arrayStringsAreEqual(std::vector<std::string>& word1, std::vector<std::string>& word2) {
-    (void)word1;
-    (void)word2;
-    return false;
+    auto w1 = word1.begin();
+    auto w2 = word2.begin();
+
+    auto c1 = w1->begin();
+    auto c2 = w2->begin();
+
+    while (w1 != word1.end() && w2 != word2.end()) {
+      if (*c1 != *c2) return false;
+      if (++c1 == w1->end() && ++w1 != word1.end()) c1 = w1->begin();
+      if (++c2 == w2->end() && ++w2 != word2.end()) c2 = w2->begin();
+    }
+
+    return w1 == word1.end() && w2 == word2.end();
   }
 };
