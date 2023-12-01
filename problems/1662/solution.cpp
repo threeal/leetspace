4adac8ef5cf9995b93ab93b7bf442afb3638ec6d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-01 11:27:58 +0700

    test: add test for problem 1662. Check If Two String Arrays are Equivalent

---
+++ problems/1662/solution.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  bool arrayStringsAreEqual(std::vector<std::string>& word1, std::vector<std::string>& word2) {
+    (void)word1;
+    (void)word2;
+    return false;
+  }
+};
