48ad51a88fc08528892da49087dee7f12bc0e0c5
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-02 14:47:52 +0700

    feat(problem-1160/cpp): solve by mapping the characters

--- problems/1160/solution.cpp
+++ problems/1160/solution.cpp
@@ -4,8 +4,31 @@
 class Solution {
  public:
   int countCharacters(std::vector<std::string>& words, std::string chars) {
-    (void)words;
-    (void)chars;
-    return 0;
+    std::vector<unsigned char> charCounts(26, 0);
+
+    for (const auto c : chars) {
+      ++charCounts[c - 'a'];
+    }
+
+    int sum = 0;
+    for (const auto& word : words) {
+      std::vector<unsigned char> wordCounts(26, 0);
+
+      bool isGood = true;
+      for (const auto c : word) {
+        if (++wordCounts[c - 'a'] > charCounts[c - 'a']) {
+          isGood = false;
+          break;
+        }
+      }
+
+      if (isGood) {
+        for (const auto count : wordCounts) {
+          sum += count;
+        }
+      }
+    }
+
+    return sum;
   }
 };
