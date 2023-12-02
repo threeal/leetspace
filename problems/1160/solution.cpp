54714249e7b22dd1e4051c9e4b1b893340d889c9
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-02 14:25:36 +0700

    test: add test for problem 1160. Find Words That Can Be Formed by Characters

---
+++ problems/1160/solution.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  int countCharacters(std::vector<std::string>& words, std::string chars) {
+    (void)words;
+    (void)chars;
+    return 0;
+  }
+};
