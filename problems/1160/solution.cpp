9c05bc9b5fed745edfb8ee56b9d446efe9b244c5
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-02 15:11:15 +0700

    docs(problem-1160/cpp): add comment that describe the solution

--- problems/1160/solution.cpp
+++ problems/1160/solution.cpp
@@ -1,3 +1,9 @@
+// The solution can be done as follows:
+// - Count each character in the `chars` string.
+// - For each string in `words`, it is good if each character count
+//   is equal to or less than the count in the `chars` string.
+// - Return the total length of all good strings.
+
 #include <string>
 #include <vector>
 
@@ -6,13 +12,17 @@ class Solution {
   int countCharacters(std::vector<std::string>& words, std::string chars) {
     std::vector<int> charCounts(26, 0);
 
+    // Count each character in the `chars` string.
     for (const auto c : chars) {
       ++charCounts[c - 'a'];
     }
 
+    // Check if each string in `words` is good.
     int totalSum = 0;
     for (const auto& word : words) {
       bool isGood = true;
+
+      // Copy the counting array and decrement it to check if the string is good.
       auto counts = charCounts;
       for (const auto c : word) {
         if (--counts[c - 'a'] < 0) {
@@ -21,6 +31,7 @@ class Solution {
         }
       }
 
+      // Sum the length of the string if it is good.
       if (isGood) totalSum += word.size();
     }
 
