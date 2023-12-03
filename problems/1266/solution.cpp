d217af39366cb4920b15ff25f58d399f7af894fb
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-03 20:26:18 +0700

    docs(problem-1266/cpp): add comment that describe the solution

--- problems/1266/solution.cpp
+++ problems/1266/solution.cpp
@@ -6,7 +6,10 @@ class Solution {
  public:
   int minTimeToVisitAllPoints(std::vector<std::vector<int>>& points) {
     int time = 0;
+
+    // Calculate the distance between each pair of points.
     for (std::size_t i = 1; i < points.size(); ++i) {
+      // The distance between two points is equal to the maximum distance between x and y axes.
       time += std::max(
           std::abs(points[i][0] - points[i - 1][0]),
           std::abs(points[i][1] - points[i - 1][1]));
