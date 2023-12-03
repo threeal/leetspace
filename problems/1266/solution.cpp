545d8a3179f1927e7fbb9e9d934c743e670cce2d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-03 20:21:42 +0700

    feat(problem-1266/cpp): solve by calculating distance between points

--- problems/1266/solution.cpp
+++ problems/1266/solution.cpp
@@ -1,9 +1,17 @@
+#include <algorithm>
+#include <cstdlib>
 #include <vector>
 
 class Solution {
  public:
   int minTimeToVisitAllPoints(std::vector<std::vector<int>>& points) {
-    (void)points;
-    return 0;
+    int time = 0;
+    for (std::size_t i = 1; i < points.size(); ++i) {
+      time += std::max(
+          std::abs(points[i][0] - points[i - 1][0]),
+          std::abs(points[i][1] - points[i - 1][1]));
+    }
+
+    return time;
   }
 };
