28d5107ee560e7257254f07d3b59ba4129c660a0
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-03 20:43:32 +0700

    feat(problem-1266/c): solve by calculating distance between points

--- problems/1266/c/solution.c
+++ problems/1266/c/solution.c
@@ -1,6 +1,16 @@
+#include <stdlib.h>
+
 int minTimeToVisitAllPoints(int** points, int pointsSize, int* pointsColSize) {
-  (void)points;
-  (void)pointsSize;
   (void)pointsColSize;
-  return 0;
+  int time = 0;
+
+  // Calculate the distance between each pair of points.
+  for (int i = 1; i < pointsSize; ++i) {
+    // The distance between two points is equal to the maximum distance between x and y axes.
+    const int dx = abs(points[i][0] - points[i - 1][0]);
+    const int dy = abs(points[i][1] - points[i - 1][1]);
+    time += dx > dy ? dx : dy;
+  }
+
+  return time;
 }
