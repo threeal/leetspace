63734eef8181111496060157c026389814e09eb7
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-03 20:59:26 +0700

    feat(problem-1160/c): solve by mapping the characters

--- problems/1160/c/solution.c
+++ problems/1160/c/solution.c
@@ -1,6 +1,37 @@
+#include <stdbool.h>
+#include <string.h>
+
 int countCharacters(char** words, int wordsSize, char* chars) {
-  (void)words;
-  (void)wordsSize;
-  (void)chars;
-  return 0;
+  int charCounts[26] = {0};
+
+  // Count each character in the `chars` string.
+  while (*chars != 0) {
+    ++charCounts[*chars - 'a'];
+    ++chars;
+  }
+
+  // Check if each string in `words` is good.
+  int totalSum = 0;
+  for (int i = 0; i < wordsSize; ++i) {
+    bool isGood = true;
+
+    // Copy the counting array
+    int counts[26];
+    memcpy(counts, charCounts, 26 * sizeof(int));
+
+    // Decrement the counting array to check if the string is good.
+    int j = 0;
+    while (words[i][j] != 0) {
+      if (--counts[words[i][j] - 'a'] < 0) {
+        isGood = false;
+        break;
+      }
+      ++j;
+    }
+
+    // Sum the length of the string if it is good.
+    if (isGood) totalSum += j;
+  }
+
+  return totalSum;
 }
