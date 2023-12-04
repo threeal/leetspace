80c65a7d142561cb68ed6233917fc30f3df8d186
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-04 21:40:32 +0700

    feat(problem-2264/cpp): solve by using for loop

--- problems/2264/solution.cpp
+++ problems/2264/solution.cpp
@@ -3,7 +3,14 @@
 class Solution {
  public:
   std::string largestGoodInteger(std::string num) {
-    (void)num;
-    return "";
+    char largestCh = 0;
+    for (std::size_t i = 2; i < num.size(); ++i) {
+      if (num[i] <= largestCh) continue;
+      if (num[i] == num[i - 1] && num[i] == num[i - 2]) {
+        largestCh = num[i];
+      }
+    }
+
+    return largestCh > 0 ? std::string(3, largestCh) : "";
   }
 };
