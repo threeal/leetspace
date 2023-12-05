e713d9def2052de6a2e359c8052fa60ab42114a1
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-05 13:17:12 +0700

    feat(problem-2264/c): solve by using for loop

--- problems/2264/c/solution.c
+++ problems/2264/c/solution.c
@@ -1,3 +1,20 @@
 char* largestGoodInteger(char* num) {
+  char largestCh = 0;
+  for (int i = 2; num[i] != 0; ++i) {
+    if (num[i] <= largestCh) continue;
+    if (num[i] == num[i - 1] && num[i] == num[i - 2]) {
+      largestCh = num[i];
+    }
+  }
+
+  if (largestCh > 0) {
+    num[0] = largestCh;
+    num[1] = largestCh;
+    num[2] = largestCh;
+    num[3] = 0;
+  } else {
+    num[0] = 0;
+  }
+
   return num;
 }
