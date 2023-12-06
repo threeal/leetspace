59ad0cf0cdd8c21c809b2b71f38f7af774a3d671
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-06 10:57:22 +0700

    feat: solve problem 1716. Calculate Money in Leetcode Bank (#264)
    * test: add test for problem 1716. Calculate Money in Leetcode Bank
    
    * feat(problem-1716/cpp): solve by using series formula

---
+++ problems/1716/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+ public:
+  int totalMoney(int n) {
+    const int div = n / 7;
+    const int mod = n % 7;
+    return (div * (49 + 7 * div) + mod * (2 * div + mod + 1)) / 2;
+  }
+};
