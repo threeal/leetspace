5898caf5c312ffdafdba70fbbe92c40a3e2b6fb3
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-07 14:58:55 +0700

    feat(problem-1903/cpp): solve by iterating from back to find odd number

--- problems/1903/solution.cpp
+++ problems/1903/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   std::string largestOddNumber(std::string num) {
-    return num;
+    for (size_t i = num.size(); i > 0; --i) {
+      if ((num[i - 1] - '0') % 2 != 1) continue;
+      return num.substr(0, i);
+    }
+    return "";
   }
 };
