8c9f729c57a4e698e190a84ae09db5fb8037ca8e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-07 15:04:40 +0700

    docs(problem-1903/cpp): add comment that describe the solution

--- problems/1903/solution.cpp
+++ problems/1903/solution.cpp
@@ -1,12 +1,18 @@
+// A number is odd if the last digit is odd, that is why we iterate
+// from the last element to find the first occurrence of an odd digit.
+
 #include <string>
 
 class Solution {
  public:
   std::string largestOddNumber(std::string num) {
-    for (size_t i = num.size(); i > 0; --i) {
+    // Iterating from the last element to find an odd digit.
+    for (std::size_t i = num.size(); i > 0; --i) {
       if ((num[i - 1] - '0') % 2 != 1) continue;
       return num.substr(0, i);
     }
+
+    // No odd digit found.
     return "";
   }
 };
