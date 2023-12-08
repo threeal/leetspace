4bcff5fe73a14af106e0d5eb31d05bf50ea7b85c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-08 09:56:53 +0700

    feat(problem-1903/c): solve by iterating from back to find odd number

--- problems/1903/c/solution.c
+++ problems/1903/c/solution.c
@@ -1,3 +1,14 @@
+#include <string.h>
+
 char* largestOddNumber(char* num) {
+  // Iterating from the last element to find an odd digit.
+  for (int i = strlen(num); i > 0; --i) {
+    if ((num[i - 1] - '0') % 2 != 1) continue;
+    num[i] = 0;
+    return num;
+  }
+
+  // No odd digit found.
+  num[0] = 0;
   return num;
 }
