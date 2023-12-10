8f9b40da252e5b0592eb7c716207d4aba7ec4a9a
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-10 15:17:20 +0700

    feat(problem-867/cpp): solve by constructing vector with reversed `m` and `n`

--- problems/0867/solution.cpp
+++ problems/0867/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   std::vector<std::vector<int>> transpose(std::vector<std::vector<int>>& matrix) {
-    return matrix;
+    const auto m = matrix.size();
+    const auto n = matrix[0].size();
+
+    auto output = std::vector(n, std::vector<int>(m));
+    for (std::size_t i = 0; i < m; ++i) {
+      for (std::size_t j = 0; j < n; ++j) {
+        output[j][i] = matrix[i][j];
+      }
+    }
+
+    return output;
   }
 };
