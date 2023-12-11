9b1caa4b1c086bdb78709e5e44d14027906fbe1b
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-11 15:02:18 +0700

    feat(problem-1287/cpp): solve by using for loop

--- problems/1287/solution.cpp
+++ problems/1287/solution.cpp
@@ -3,7 +3,9 @@
 class Solution {
  public:
   int findSpecialInteger(std::vector<int>& arr) {
-    (void)arr;
+    for (std::size_t i = 0; i < arr.size(); ++i) {
+      if (arr[i] == arr[i + arr.size() / 4]) return arr[i];
+    }
     return 0;
   }
 };
