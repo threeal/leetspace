4f9fecab93b044a237eb4c699f8a10bef9bbec54
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-12 12:18:50 +0700

    docs(problem-1464/cpp): add comment that describe the solution

--- problems/1464/solution.cpp
+++ problems/1464/solution.cpp
@@ -1,11 +1,16 @@
+// The solution can simply be accomplished by finding the two largest elements and then multiplying them.
+
 #include <vector>
 
 class Solution {
  public:
   int maxProduct(std::vector<int>& nums) {
+    // Prepare containers for storing two largest elements.
+    // a should be smaller than b.
     auto a = nums[0] < nums[1] ? nums[0] : nums[1];
     auto b = nums[0] < nums[1] ? nums[1] : nums[0];
 
+    // Iterating over the elements to find the two largest elements.
     for (std::size_t i = 2; i < nums.size(); ++i) {
       if (nums[i] < a) continue;
 
@@ -17,6 +22,7 @@ class Solution {
       }
     }
 
+    // Multiply them after decrementing each by 1.
     return --a * --b;
   }
 };
