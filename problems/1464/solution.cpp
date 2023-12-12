6c91786c177186e710db397bb5561851f9168acf
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-12 12:13:32 +0700

    perf(problem-1464/cpp): sort `a` and `b` values

--- problems/1464/solution.cpp
+++ problems/1464/solution.cpp
@@ -3,21 +3,16 @@
 class Solution {
  public:
   int maxProduct(std::vector<int>& nums) {
-    auto a = nums[0];
-    auto b = nums[1];
+    auto a = nums[0] < nums[1] ? nums[0] : nums[1];
+    auto b = nums[0] < nums[1] ? nums[1] : nums[0];
 
     for (std::size_t i = 2; i < nums.size(); ++i) {
-      if (nums[i] > a) {
-        if (nums[i] > b) {
-          if (a > b) {
-            b = nums[i];
-          } else {
-            a = nums[i];
-          }
-        } else {
-          a = nums[i];
-        }
-      } else if (nums[i] > b) {
+      if (nums[i] < a) continue;
+
+      if (nums[i] < b) {
+        a = nums[i];
+      } else {
+        a = b;
         b = nums[i];
       }
     }
