60eb478b63ead5e369fc7c5b232206e1e4bc2a3e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-12 12:32:39 +0700

    feat(problem-1464/c): solve by finding two largest values

--- problems/1464/c/solution.c
+++ problems/1464/c/solution.c
@@ -1,5 +1,21 @@
 int maxProduct(int* nums, int numsSize) {
-  (void)nums;
-  (void)numsSize;
-  return 0;
+  // Prepare containers for storing two largest elements.
+  // a should be smaller than b.
+  int a = nums[0] < nums[1] ? nums[0] : nums[1];
+  int b = nums[0] < nums[1] ? nums[1] : nums[0];
+
+  // Iterating over the elements to find the two largest elements.
+  for (int i = 2; i < numsSize; ++i) {
+    if (nums[i] < a) continue;
+
+    if (nums[i] < b) {
+      a = nums[i];
+    } else {
+      a = b;
+      b = nums[i];
+    }
+  }
+
+  // Multiply them after decrementing each by 1.
+  return --a * --b;
 }
