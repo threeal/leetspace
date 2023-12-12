63f6289c17e5594e8405d6d082b2ae58171a9b31
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-12 12:05:08 +0700

    feat(problem-1464/cpp): solve by using brute force

--- problems/1464/solution.cpp
+++ problems/1464/solution.cpp
@@ -3,7 +3,16 @@
 class Solution {
  public:
   int maxProduct(std::vector<int>& nums) {
-    (void)nums;
-    return 0;
+    for (auto& num : nums) {
+      --num;
+    }
+
+    int max = 0;
+    for (std::size_t i = 0; i < nums.size(); ++i) {
+      for (std::size_t j = i + 1; j < nums.size(); ++j) {
+        max = std::max(max, nums[i] * nums[j]);
+      }
+    }
+    return max;
   }
 };
