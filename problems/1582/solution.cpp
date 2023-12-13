99f4f93b106eab1742f8cd63416c72e2d4f2fbed
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-13 14:57:42 +0700

    feat(problem-1582/cpp): solve by saving coordinate of the special position

--- problems/1582/solution.cpp
+++ problems/1582/solution.cpp
@@ -6,27 +6,22 @@ class Solution {
     const int m = mat.size();
     const int n = mat[0].size();
 
-    std::vector<int> rows(m, 0);
-    std::vector<int> cols(n, 0);
+    std::vector<int> rows(m, -1);
+    std::vector<int> cols(n, -1);
     for (int i = 0; i < m; ++i) {
       for (int j = 0; j < n; ++j) {
         if (mat[i][j] == 1) {
-          ++rows[i];
-          ++cols[j];
+          rows[i] = rows[i] == -1 ? j : -2;
+          cols[j] = cols[j] == -1 ? i : -2;
         }
       }
     }
 
-    int sumRow = 0;
-    for (const auto row : rows) {
-      if (row == 1) ++sumRow;
+    int specials = 0;
+    for (int i = 0; i < m; ++i) {
+      if (rows[i] >= 0 && cols[rows[i]] == i) ++specials;
     }
 
-    int sumCol = 0;
-    for (const auto col : cols) {
-      if (col == 1) ++sumCol;
-    }
-
-    return std::min(sumRow, sumCol);
+    return specials;
   }
 };
