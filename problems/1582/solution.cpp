abaa767e5340f7d27da8cd95b86153f160a9ae31
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-13 14:40:38 +0700

    feat(problem-1582/cpp): try to solve by counting `1` in each rows and cols

--- problems/1582/solution.cpp
+++ problems/1582/solution.cpp
@@ -1,9 +1,32 @@
 #include <vector>
 
 class Solution {
-public:
+ public:
   int numSpecial(std::vector<std::vector<int>>& mat) {
-    (void)mat;
-    return 0;
+    const int m = mat.size();
+    const int n = mat[0].size();
+
+    std::vector<int> rows(m, 0);
+    std::vector<int> cols(n, 0);
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (mat[i][j] == 1) {
+          ++rows[i];
+          ++cols[j];
+        }
+      }
+    }
+
+    int sumRow = 0;
+    for (const auto row : rows) {
+      if (row == 1) ++sumRow;
+    }
+
+    int sumCol = 0;
+    for (const auto col : cols) {
+      if (col == 1) ++sumCol;
+    }
+
+    return std::min(sumRow, sumCol);
   }
 };
