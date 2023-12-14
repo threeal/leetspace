52fdb010465ad21fc252a7f86e57bd21acae37fc
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-14 17:41:45 +0700

    docs(problem-2842/cpp): add comment that describe the solution

--- problems/2482/solution.cpp
+++ problems/2482/solution.cpp
@@ -6,6 +6,7 @@ class Solution {
     const int m = grid.size();
     const int n = grid[0].size();
 
+    // Calculate the total count of elements with a value of one in each row and column.
     std::vector<int> oneRow(m);
     std::vector<int> oneCol(n);
     for (int i = 0; i < m; ++i) {
@@ -17,8 +18,10 @@ class Solution {
       }
     }
 
+    // Modify the given grid.
     for (int i = 0; i < m; ++i) {
       for (int j = 0; j < n; ++j) {
+        // Given that zeroRow[i] + zeroCol[j] = m + n - oneRow[i] - oneCol[j].
         grid[i][j] = 2 * (oneRow[i] + oneCol[j]) - m - n;
       }
     }
