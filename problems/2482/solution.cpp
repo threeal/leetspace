798eef056c329079c70b05337827ec3fbe126089
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-14 17:21:44 +0700

    feat(problem-2482/cpp): try to solve by incrementing/decrementing value

--- problems/2482/solution.cpp
+++ problems/2482/solution.cpp
@@ -3,6 +3,30 @@
 class Solution {
  public:
   std::vector<std::vector<int>> onesMinusZeros(std::vector<std::vector<int>>& grid) {
-    return grid;
+    const int m = grid.size();
+    const int n = grid[0].size();
+
+    auto res = std::vector(m, std::vector(n, 0));
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (grid[i][j] == 1) {
+          for (int ii = 0; ii < m; ++ii) {
+            ++res[ii][j];
+          }
+          for (int jj = 0; jj < n; ++jj) {
+            ++res[i][jj];
+          }
+        } else {
+          for (int ii = 0; ii < m; ++ii) {
+            --res[ii][j];
+          }
+          for (int jj = 0; jj < n; ++jj) {
+            --res[i][jj];
+          }
+        }
+      }
+    }
+
+    return res;
   }
 };
