a7eefc65c5d3c6910e39bc99983a3e8b92f9c162
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-15 12:43:53 +0700

    feat(problem-1436/cpp): solve by inserting and erasing set

--- problems/1436/solution.cpp
+++ problems/1436/solution.cpp
@@ -1,10 +1,26 @@
+#include <set>
 #include <string>
 #include <vector>
 
 class Solution {
  public:
   std::string destCity(std::vector<std::vector<std::string>>& paths) {
-    (void)paths;
-    return "";
+    std::set<std::string> starts;
+    std::set<std::string> ends;
+
+    for (const auto& path : paths) {
+      if (!starts.contains(path[0])) {
+        starts.insert(path[0]);
+        if (ends.contains(path[0])) {
+          ends.erase(path[0]);
+        }
+      }
+
+      if (!starts.contains(path[1]) && !ends.contains(path[1])) {
+        ends.insert(path[1]);
+      }
+    }
+
+    return *ends.begin();
   }
 };
