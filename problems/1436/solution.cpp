858d84b6ef013aee400013f9aeb69f3c9e402fde
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-15 12:55:11 +0700

    docs(problem-1436/cpp): add comment that describe the solution

--- problems/1436/solution.cpp
+++ problems/1436/solution.cpp
@@ -1,3 +1,5 @@
+// A destination city is defined as an end city that is not a start city.
+
 #include <string>
 #include <unordered_set>
 #include <vector>
@@ -5,14 +7,15 @@
 class Solution {
  public:
   std::string destCity(std::vector<std::vector<std::string>>& paths) {
+    // Store each start and end city from a path into sets.
     std::unordered_set<std::string> starts;
     std::unordered_set<std::string> ends;
-
     for (const auto& path : paths) {
       starts.insert(path[0]);
       ends.insert(path[1]);
     }
 
+    // Find an end city that is not a start city.
     for (const auto& end : ends) {
       if (!starts.contains(end)) return end;
     }
