53b4205ad4e8eb387be36b6fc313f838c997422f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-16 19:00:44 +0700

    feat(problem-242/cpp): try to solve by counting characters

--- problems/0242/solution.cpp
+++ problems/0242/solution.cpp
@@ -1,8 +1,18 @@
 #include <string>
+#include <vector>
 
 class Solution {
  public:
   bool isAnagram(std::string s, std::string t) {
-    return s == t;
+    std::vector<int> counts('z' - 'a' + 1, 0);
+    for (const auto c : s) {
+      ++counts[c - 'a'];
+    }
+
+    for (const auto c : t) {
+      if (--counts[c - 'a'] < 0) return false;
+    }
+
+    return true;
   }
 };
