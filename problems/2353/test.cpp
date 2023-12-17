9839036147f95141a5351d3e297fb58f431a78d6
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-17 22:50:27 +0700

    test(problem-2353): add `Case 1` test case

--- problems/2353/test.cpp
+++ problems/2353/test.cpp
@@ -16,4 +16,17 @@ TEST_CASE("2353. Design a Food Rating System (cpp)") {
     foodRatings.changeRating("ramen", 16);
     REQUIRE(foodRatings.highestRated("japanese") == "ramen");
   }
+
+  SECTION("Case 1") {
+    std::vector<std::string> foogs = {"czopaaeyl", "lxoozsbh", "kbaxapl"};
+    std::vector<std::string> cuisines = {"dmnuqeatj", "dmnuqeatj", "dmnuqeatj"};
+    std::vector<int> ratings = {11, 2, 15};
+    FoodRatings foodRatings(foogs, cuisines, ratings);
+
+    foodRatings.changeRating("czopaaeyl", 12);
+    REQUIRE(foodRatings.highestRated("dmnuqeatj") == "kbaxapl");
+    foodRatings.changeRating("kbaxapl", 8);
+    foodRatings.changeRating("lxoozsbh", 5);
+    REQUIRE(foodRatings.highestRated("dmnuqeatj") == "czopaaeyl");
+  }
 }
