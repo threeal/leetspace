47c8e237b5fd7bdfbcf484fe2ac51ec58bb7cdae
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-17 21:26:12 +0700

    feat(problem-2353/cpp): try to solve by storing to map

--- problems/2353/solution.cpp
+++ problems/2353/solution.cpp
@@ -1,21 +1,36 @@
+#include <algorithm>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 class FoodRatings {
+ private:
+  std::unordered_map<std::string, std::string> cuisines_best_foods;
+  std::unordered_map<std::string, std::string> food_cuisines;
+  std::unordered_map<std::string, int> food_ratings;
+
  public:
   FoodRatings(std::vector<std::string>& foods, std::vector<std::string>& cuisines, std::vector<int>& ratings) {
-    (void)foods;
-    (void)cuisines;
-    (void)ratings;
+    const auto n = std::min({foods.size(), cuisines.size(), ratings.size()});
+    for (std::size_t i = 0; i < n; ++i) {
+      food_cuisines[foods[i]] = cuisines[i];
+      changeRating(foods[i], ratings[i]);
+    }
   }
 
   void changeRating(std::string food, int newRating) {
-    (void)food;
-    (void)newRating;
+    food_ratings[food] = newRating;
+    const auto& cuisine = food_cuisines[food];
+    const auto& best_food = cuisines_best_foods[cuisine];
+    if (best_food != food) {
+      const auto rating = food_ratings[best_food];
+      if (newRating > rating || (newRating == rating && food.compare(best_food) < 0)) {
+        cuisines_best_foods[cuisine] = food;
+      }
+    }
   }
 
   std::string highestRated(std::string cuisine) {
-    (void)cuisine;
-    return "";
+    return cuisines_best_foods[cuisine];
   }
 };
