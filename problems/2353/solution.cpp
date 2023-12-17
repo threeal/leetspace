ebae4bc2762a22418d7944fb7f100ca5086667dd
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-17 23:08:29 +0700

    feat(problem-2353/cpp): solve by storing food rating in ordered set

--- problems/2353/solution.cpp
+++ problems/2353/solution.cpp
@@ -1,11 +1,22 @@
 #include <algorithm>
+#include <set>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
+struct FoodRating {
+  int rating;
+  std::string food;
+
+  bool operator>(const FoodRating& other) const {
+    if (rating == other.rating) return food < other.food;
+    return rating > other.rating;
+  }
+};
+
 class FoodRatings {
  private:
-  std::unordered_map<std::string, std::string> cuisines_best_foods;
+  std::unordered_map<std::string, std::set<FoodRating, std::greater<FoodRating>>> cuisine_food_ratings;
   std::unordered_map<std::string, std::string> food_cuisines;
   std::unordered_map<std::string, int> food_ratings;
 
@@ -13,24 +24,24 @@ class FoodRatings {
   FoodRatings(std::vector<std::string>& foods, std::vector<std::string>& cuisines, std::vector<int>& ratings) {
     const auto n = std::min({foods.size(), cuisines.size(), ratings.size()});
     for (std::size_t i = 0; i < n; ++i) {
+      cuisine_food_ratings[cuisines[i]].insert({.food = foods[i], .rating = ratings[i]});
       food_cuisines[foods[i]] = cuisines[i];
-      changeRating(foods[i], ratings[i]);
+      food_ratings[foods[i]] = ratings[i];
     }
   }
 
   void changeRating(std::string food, int newRating) {
-    food_ratings[food] = newRating;
     const auto& cuisine = food_cuisines[food];
-    const auto& best_food = cuisines_best_foods[cuisine];
-    if (best_food != food) {
-      const auto rating = food_ratings[best_food];
-      if (newRating > rating || (newRating == rating && food.compare(best_food) < 0)) {
-        cuisines_best_foods[cuisine] = food;
-      }
-    }
+    const auto rating = food_ratings[food];
+
+    cuisine_food_ratings[cuisine].erase({.food = food, .rating = rating});
+    cuisine_food_ratings[cuisine].insert({.food = food, .rating = newRating});
+
+    food_ratings[food] = newRating;
   }
 
   std::string highestRated(std::string cuisine) {
-    return cuisines_best_foods[cuisine];
+    return cuisine_food_ratings[cuisine].begin()->food;
+    ;
   }
 };
