04e051b0653bd5daafa4c8484c5c6ce3751c5ae3
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-18 17:21:40 +0700

    docs(problem-1913/cpp): add comments to describe solution

--- problems/1913/solution.cpp
+++ problems/1913/solution.cpp
@@ -3,13 +3,21 @@
 class Solution {
  public:
   int maxProductDifference(std::vector<int>& nums) {
+    // Sort the first 4 elements.
     std::sort(nums.begin(), nums.begin() + 4);
 
+    // Store the 2 largest elements in `a` and `b`.
+    // `b` should be larger than `a`.
     int a = nums[2];
     int b = nums[3];
+
+    // Store the 2 smallest elements in `c` and `d`.
+    // `d` should be smaller than `c`.
     int c = nums[1];
     int d = nums[0];
 
+    // Iterate over the rest of the elements to update
+    // the largest and smallest elements.
     for (std::size_t i = 4; i < nums.size(); ++i) {
       if (nums[i] > a) {
         if (nums[i] > b) {
@@ -28,6 +36,7 @@ class Solution {
       }
     }
 
+    // Calculate the maximum product difference.
     return (a * b) - (c * d);
   }
 };
