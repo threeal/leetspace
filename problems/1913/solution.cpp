8dc1aab2184fcbe2a0793835f0223d5daba5d328
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-18 17:11:48 +0700

    feat(problem-1913/cpp): solve by finding largest and smallest elements

--- problems/1913/solution.cpp
+++ problems/1913/solution.cpp
@@ -3,7 +3,31 @@
 class Solution {
  public:
   int maxProductDifference(std::vector<int>& nums) {
-    (void)nums;
-    return 0;
+    std::sort(nums.begin(), nums.begin() + 4);
+
+    int a = nums[2];
+    int b = nums[3];
+    int c = nums[1];
+    int d = nums[0];
+
+    for (std::size_t i = 4; i < nums.size(); ++i) {
+      if (nums[i] > a) {
+        if (nums[i] > b) {
+          a = b;
+          b = nums[i];
+        } else {
+          a = nums[i];
+        }
+      } else if (nums[i] < c) {
+        if (nums[i] < d) {
+          c = d;
+          d = nums[i];
+        } else {
+          c = nums[i];
+        }
+      }
+    }
+
+    return (a * b) - (c * d);
   }
 };
