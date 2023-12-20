3e18dfc17b410024edc25da328eea05cd05761a1
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-20 18:36:55 +0700

    feat(problem-2706/cpp): solve by finding two smallest elements

--- problems/2706/solution.cpp
+++ problems/2706/solution.cpp
@@ -3,7 +3,19 @@
 class Solution {
  public:
   int buyChoco(std::vector<int>& prices, int money) {
-    (void)prices;
-    return money;
+    int a = prices[0] < prices[1] ? prices[0] : prices[1];
+    int b = prices[0] < prices[1] ? prices[1] : prices[0];
+
+    for (std::size_t i = 2; i < prices.size(); ++i) {
+      if (prices[i] >= b) continue;
+      if (prices[i] < a) {
+        b = a;
+        a = prices[i];
+      } else {
+        b = prices[i];
+      }
+    }
+
+    return a + b > money ? money : money - a - b;
   }
 };
