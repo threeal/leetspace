3763572e7aae49db03e4b80776018b9d7ab9407f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-20 18:36:56 +0700

    docs(problem-2706/cpp): add comments to describe solution

--- problems/2706/solution.cpp
+++ problems/2706/solution.cpp
@@ -3,9 +3,11 @@
 class Solution {
  public:
   int buyChoco(std::vector<int>& prices, int money) {
+    // Determine the smallest between the first and the second price.
     int a = prices[0] < prices[1] ? prices[0] : prices[1];
     int b = prices[0] < prices[1] ? prices[1] : prices[0];
 
+    // Iterate throught the rest of the prices two update the two smallest prices.
     for (std::size_t i = 2; i < prices.size(); ++i) {
       if (prices[i] >= b) continue;
       if (prices[i] < a) {
@@ -16,6 +18,7 @@ class Solution {
       }
     }
 
+    // If the total price is higher than the money, return the money instead.
     return a + b > money ? money : money - a - b;
   }
 };
