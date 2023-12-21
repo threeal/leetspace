bfffc443b2c89182b735cfd474be72f32804a5d4
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-21 19:25:45 +0700

    docs(problem-1637/cp): add comment that describe the solution

--- problems/1637/solution.cpp
+++ problems/1637/solution.cpp
@@ -4,13 +4,16 @@
 class Solution {
  public:
   int maxWidthOfVerticalArea(std::vector<std::vector<int>>& points) {
+    // Convert list of points into a list of x coordinates.
     std::vector<int> xs(points.size());
     for (std::size_t i = 0; i < points.size(); ++i) {
       xs[i] = points[i][0];
     }
 
+    // Sort the x coordinates.
     std::sort(xs.begin(), xs.end());
 
+    // Find the maximum difference between two different x coordinates.
     int prev_x = xs[0];
     int max_width = 0;
     for (std::size_t i = 1; i < xs.size(); ++i) {
