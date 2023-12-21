7c65afc7254612d388e07ddb24377e923b5c715e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-21 19:18:13 +0700

    perf(problem-1637/cpp): just sort without finding for uniqueness

--- problems/1637/solution.cpp
+++ problems/1637/solution.cpp
@@ -1,26 +1,22 @@
 #include <algorithm>
-#include <unordered_set>
 #include <vector>
 
 class Solution {
  public:
   int maxWidthOfVerticalArea(std::vector<std::vector<int>>& points) {
-    std::unordered_set<int> unique_xs;
-    for (const auto& point : points) {
-      unique_xs.insert(point[0]);
-    }
-
-    std::vector<int> xs(unique_xs.size());
-    int i = -1;
-    for (const auto& x : unique_xs) {
-      xs[++i] = x;
+    std::vector<int> xs(points.size());
+    for (std::size_t i = 0; i < points.size(); ++i) {
+      xs[i] = points[i][0];
     }
 
     std::sort(xs.begin(), xs.end());
 
+    int prev_x = xs[0];
     int max_width = 0;
     for (std::size_t i = 1; i < xs.size(); ++i) {
-      max_width = std::max(max_width, xs[i] - xs[i - 1]);
+      if (xs[i] == prev_x) continue;
+      max_width = std::max(max_width, xs[i] - prev_x);
+      prev_x = xs[i];
     }
 
     return max_width;
