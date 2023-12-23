1debe24eda985f9a3c102e12523a1a33463ed255
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-23 12:49:51 +0700

    feat(problem-1496/cpp): solve by storing path on a hash map

--- problems/1496/solution.cpp
+++ problems/1496/solution.cpp
@@ -1,9 +1,39 @@
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 
 class Solution {
  public:
   bool isPathCrossing(std::string path) {
-    (void)path;
+    std::unordered_map<int, std::unordered_set<int>> visited;
+    visited[0].insert(0);
+
+    int x = 0;
+    int y = 0;
+
+    for (const auto dir : path) {
+      switch (dir) {
+        case 'N':
+          --y;
+          break;
+
+        case 'S':
+          ++y;
+          break;
+
+        case 'E':
+          ++x;
+          break;
+
+        case 'W':
+          --x;
+          break;
+      }
+
+      if (visited[x].contains(y)) return true;
+      visited[x].insert(y);
+    }
+
     return false;
   }
 };
