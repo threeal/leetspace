3e93a9c3881e08f8686319455ebaf552e063654a
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-24 16:31:18 +0700

    feat(problem-1758/cpp): solve by comparing two possibility

--- problems/1758/solution.cpp
+++ problems/1758/solution.cpp
@@ -1,9 +1,28 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   int minOperations(std::string s) {
-    (void)s;
-    return 0;
+    int zeroFirst = 0;
+    int oneFirst = 0;
+
+    for (std::size_t i = 0; i < s.size(); ++i) {
+      if (i % 2 == 0) {
+        if (s[i] == '0') {
+          ++oneFirst;
+        } else {
+          ++zeroFirst;
+        }
+      } else {
+        if (s[i] == '0') {
+          ++zeroFirst;
+        } else {
+          ++oneFirst;
+        }
+      }
+    }
+
+    return std::min(zeroFirst, oneFirst);
   }
 };
