8d6dee73c656f6a565624245cb6c5e3c963370d5
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-25 09:33:10 +0700

    feat(problem-91/cpp): solve by using 2 variables instead of array

--- problems/0091/solution.cpp
+++ problems/0091/solution.cpp
@@ -1,35 +1,30 @@
 #include <string>
-#include <vector>
 
 class Solution {
  public:
   int numDecodings(std::string s) {
-    const int n = s.size();
+    int prev_ways = 0;
+    int ways = 1;
+
     s += '9';
-
-    std::vector<int> ways(n + 1, 0);
-    ways[n] = 1;
-
-    for (int i = n - 1; i >= 0; --i) {
+    for (int i = s.size() - 2; i >= 0; --i) {
+      const int temp_ways = ways;
       switch (s[i]) {
         case '0':
-          ways[i] = 0;
+          ways = 0;
           break;
 
         case '1':
-          ways[i] = ways[i + 1] + ways[i + 2];
+          ways += prev_ways;
           break;
 
         case '2':
-          ways[i] = ways[i + 1];
-          if (s[i + 1] <= '6') ways[i] += ways[i + 2];
+          if (s[i + 1] <= '6') ways += prev_ways;
           break;
-
-        default:
-          ways[i] = ways[i + 1];
       }
+      prev_ways = temp_ways;
     }
 
-    return ways[0];
+    return ways;
   }
 };
