075589d329c504da8c3af5f331e99fd60516d9a9
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-25 09:27:15 +0700

    feat(problem-91/cpp): try to solve by calculating possible ways

--- problems/0091/solution.cpp
+++ problems/0091/solution.cpp
@@ -1,9 +1,35 @@
 #include <string>
+#include <vector>
 
 class Solution {
  public:
   int numDecodings(std::string s) {
-    (void)s;
-    return 0;
+    const int n = s.size();
+    s += '9';
+
+    std::vector<int> ways(n + 1, 0);
+    ways[n] = 1;
+
+    for (int i = n - 1; i >= 0; --i) {
+      switch (s[i]) {
+        case '0':
+          ways[i] = 0;
+          break;
+
+        case '1':
+          ways[i] = ways[i + 1] + ways[i + 2];
+          break;
+
+        case '2':
+          ways[i] = ways[i + 1];
+          if (s[i + 1] <= '6') ways[i] += ways[i + 2];
+          break;
+
+        default:
+          ways[i] = ways[i + 1];
+      }
+    }
+
+    return ways[0];
   }
 };
