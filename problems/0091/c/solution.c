44dd6181b6ee021ff21534d26e32baee5be4c904
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-27 21:56:14 +0700

    feat(problem-91/c): solve by calculating possible ways

--- problems/0091/c/solution.c
+++ problems/0091/c/solution.c
@@ -1,4 +1,28 @@
+#include <string.h>
+
 int numDecodings(char* s) {
-  (void)s;
-  return 0;
+  int prev_ways = 0;
+  int ways = 1;
+
+  const int n = strlen(s);
+  s[n] = '9';
+  for (int i = n - 1; i >= 0; --i) {
+    const int temp_ways = ways;
+    switch (s[i]) {
+      case '0':
+        ways = 0;
+        break;
+
+      case '1':
+        ways += prev_ways;
+        break;
+
+      case '2':
+        if (s[i + 1] <= '6') ways += prev_ways;
+        break;
+    }
+    prev_ways = temp_ways;
+  }
+
+  return ways;
 }
