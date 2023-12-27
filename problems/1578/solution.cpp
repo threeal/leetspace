9a03aaccf505de10f3c535b3babc029e984472fe
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-27 20:27:59 +0700

    perf(problem-1578/cpp): calculate twice instead of appending string

--- problems/1578/solution.cpp
+++ problems/1578/solution.cpp
@@ -4,12 +4,12 @@
 class Solution {
  public:
   int minCost(std::string colors, std::vector<int>& neededTime) {
-    colors += ' ';
-
     int total = 0;
 
     std::size_t left = 0;
-    for (std::size_t right = 1; right < colors.size(); ++right) {
+    std::size_t right = 1;
+
+    for (; right < colors.size(); ++right) {
       if (colors[left] != colors[right]) {
         if (right - left > 1) {
           int largest = 0;
@@ -21,6 +21,15 @@ class Solution {
         }
         left = right;
       }
+    }
+
+    if (right - left > 1) {
+      int largest = 0;
+      for (std::size_t i = left; i < right; ++i) {
+        largest = std::max(largest, neededTime[i]);
+        total += neededTime[i];
+      }
+      total -= largest;
     }
 
     return total;
