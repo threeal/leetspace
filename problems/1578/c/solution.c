0340eaf638e13cf68d0fcff598e059918df6ea6b
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-27 21:41:21 +0700

    feat(problem-1578/c): solve by counting all except max of duplicate colors

--- problems/1578/c/solution.c
+++ problems/1578/c/solution.c
@@ -1,6 +1,29 @@
 int minCost(char* colors, int* neededTime, int neededTimeSize) {
-  (void)colors;
-  (void)neededTime;
   (void)neededTimeSize;
-  return 0;
+  int total = 0;
+
+  int left = 0;
+  int right = 1;
+
+  while (colors[right] != 0) {
+    if (colors[left] != colors[right]) {
+      int largest = 0;
+      for (int i = left; i < right; ++i) {
+        if (neededTime[i] > largest) largest = neededTime[i];
+        total += neededTime[i];
+      }
+      total -= largest;
+      left = right;
+    }
+    ++right;
+  }
+
+  int largest = 0;
+  for (int i = left; i < right; ++i) {
+    if (neededTime[i] > largest) largest = neededTime[i];
+    total += neededTime[i];
+  }
+  total -= largest;
+
+  return total;
 }
