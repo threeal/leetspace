7db80aefabe8d6e4bdb2eb58e95c5f051692948f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-27 20:31:20 +0700

    perf(problem-1578/cpp): simplify if check

--- problems/1578/solution.cpp
+++ problems/1578/solution.cpp
@@ -10,27 +10,22 @@ class Solution {
     std::size_t right = 1;
 
     for (; right < colors.size(); ++right) {
-      if (colors[left] != colors[right]) {
-        if (right - left > 1) {
-          int largest = 0;
-          for (std::size_t i = left; i < right; ++i) {
-            largest = std::max(largest, neededTime[i]);
-            total += neededTime[i];
-          }
-          total -= largest;
-        }
-        left = right;
-      }
-    }
-
-    if (right - left > 1) {
+      if (colors[left] == colors[right]) continue;
       int largest = 0;
       for (std::size_t i = left; i < right; ++i) {
         largest = std::max(largest, neededTime[i]);
         total += neededTime[i];
       }
       total -= largest;
+      left = right;
     }
+
+    int largest = 0;
+    for (std::size_t i = left; i < right; ++i) {
+      largest = std::max(largest, neededTime[i]);
+      total += neededTime[i];
+    }
+    total -= largest;
 
     return total;
   }
