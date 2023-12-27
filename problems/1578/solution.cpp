6965bf2f1057c03fc5b017a5897451c3395b7d63
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-27 20:16:37 +0700

    test: add test for problem 1578. Minimum Time to Make Rope Colorful

---
+++ problems/1578/solution.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  int minCost(std::string colors, std::vector<int>& neededTime) {
+    (void)colors;
+    (void)neededTime;
+    return 0;
+  }
+};
