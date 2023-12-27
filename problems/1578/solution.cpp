d0f80f4c8203d622da0c630fc2800b86358a0369
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-27 20:25:31 +0700

    feat(problem-1578/cpp): solve by counting all except max of duplicate colors

--- problems/1578/solution.cpp
+++ problems/1578/solution.cpp
@@ -4,8 +4,25 @@
 class Solution {
  public:
   int minCost(std::string colors, std::vector<int>& neededTime) {
-    (void)colors;
-    (void)neededTime;
-    return 0;
+    colors += ' ';
+
+    int total = 0;
+
+    std::size_t left = 0;
+    for (std::size_t right = 1; right < colors.size(); ++right) {
+      if (colors[left] != colors[right]) {
+        if (right - left > 1) {
+          int largest = 0;
+          for (std::size_t i = left; i < right; ++i) {
+            largest = std::max(largest, neededTime[i]);
+            total += neededTime[i];
+          }
+          total -= largest;
+        }
+        left = right;
+      }
+    }
+
+    return total;
   }
 };
