b42be34369073fe3d9dc6f75628e03e9ec1aba8c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-30 11:19:22 +0700

    feat(problem-1897/cpp): solve by counting letters

--- problems/1897/solution.cpp
+++ problems/1897/solution.cpp
@@ -1,10 +1,21 @@
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   bool makeEqual(std::vector<std::string>& words) {
-    (void)words;
-    return false;
+    std::unordered_map<char, int> letter_counts;
+    for (const auto& word : words) {
+      for (const auto& letter : word) {
+        ++letter_counts[letter];
+      }
+    }
+
+    for (const auto [letter, count] : letter_counts) {
+      if (count % words.size() != 0) return false;
+    }
+
+    return true;
   }
 };
