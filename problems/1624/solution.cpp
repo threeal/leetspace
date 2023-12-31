5a3e0b9cf4fc64d3457dc4744176e5c406353e0d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-31 19:39:44 +0700

    feat(problem-1624/cpp): solve by saving index of first occurences

--- problems/1624/solution.cpp
+++ problems/1624/solution.cpp
@@ -1,9 +1,20 @@
+#include <algorithm>
 #include <string>
+#include <unordered_map>
 
 class Solution {
  public:
   int maxLengthBetweenEqualCharacters(std::string s) {
-    (void)s;
-    return 0;
+    int maxLength = -1;
+    std::unordered_map<char, std::size_t> lefts;
+    for (std::size_t i = 0; i < s.size(); ++i) {
+      const auto it = lefts.find(s[i]);
+      if (it == lefts.end()) {
+        lefts.emplace(s[i], i);
+      } else {
+        maxLength = std::max<int>(maxLength, i - it->second - 1);
+      }
+    }
+    return maxLength;
   }
 };
