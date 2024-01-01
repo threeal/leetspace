f85c6703230f3b88cb4628429e5ec5be6cf48e64
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-01 21:02:02 +0700

    feat(problem-0455/cpp): try to solve by sorting `g` and `s`

--- problems/0455/solution.cpp
+++ problems/0455/solution.cpp
@@ -1,10 +1,19 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int findContentChildren(std::vector<int>& g, std::vector<int>& s) {
-    (void)g;
-    (void)s;
-    return 0;
+    sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+
+    int i = 0;
+    const int n = std::min(g.size(), s.size());
+    while (i < n) {
+      if (g[i] > s[i]) break;
+      ++i;
+    }
+
+    return i;
   }
 };
