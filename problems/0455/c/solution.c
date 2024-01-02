f8b2121e64d09d72d9db534638f1a6b4c6f95efd
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-02 22:57:59 +0700

    feat(problem-455/c): solve by iterating sorted `g` and `s`

--- problems/0455/c/solution.c
+++ problems/0455/c/solution.c
@@ -1,7 +1,39 @@
+void quickSort(int* low, int* high);
+
 int findContentChildren(int* g, int gSize, int* s, int sSize) {
-  (void)g;
-  (void)gSize;
-  (void)s;
-  (void)sSize;
-  return 0;
+  quickSort(g, g + gSize - 1);
+  quickSort(s, s + sSize - 1);
+
+  int gi = 0;
+  int si = 0;
+  while (gi < gSize && si < sSize) {
+    if (g[gi] <= s[si]) ++gi;
+    ++si;
+  }
+
+  return gi;
+}
+
+// This function implements quick sort to sort the given range of arrays.
+// See: https://www.geeksforgeeks.org/quick-sort/
+void quickSort(int* low, int* high) {
+  if (low >= high) return;
+
+  int* i = low - 1;
+
+  for (int* j = low; j < high; ++j) {
+    if (*j >= *high) continue;
+    ++i;
+    const int temp = *i;
+    *i = *j;
+    *j = temp;
+  }
+
+  ++i;
+  const int temp = *i;
+  *i = *high;
+  *high = temp;
+
+  quickSort(low, i - 1);
+  quickSort(i + 1, high);
 }
