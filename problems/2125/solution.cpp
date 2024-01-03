c6b165dfc8b9df502f08adf466ed024fcfce7330
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-03 20:54:01 +0700

    feat(problem-2125/cpp): solve by multiplying beams in each row

--- problems/2125/solution.cpp
+++ problems/2125/solution.cpp
@@ -4,7 +4,21 @@
 class Solution {
  public:
   int numberOfBeams(std::vector<std::string>& bank) {
-    (void)bank;
-    return 0;
+    int lasers = 0;
+    int prevBeams = 0;
+
+    for (const auto& row : bank) {
+      int beams = 0;
+      for (const auto c : row) {
+        if (c == '1') ++beams;
+      }
+
+      if (beams > 0) {
+        lasers += prevBeams * beams;
+        prevBeams = beams;
+      }
+    }
+
+    return lasers;
   }
 };
