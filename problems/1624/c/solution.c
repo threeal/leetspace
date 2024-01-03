958af02b4ed0b8eef26ba73319c6b5a367da2e4c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-03 21:08:48 +0700

    feat(problem-1624/c): solve by saving index of first occurences

--- problems/1624/c/solution.c
+++ problems/1624/c/solution.c
@@ -1,4 +1,14 @@
 int maxLengthBetweenEqualCharacters(char* s) {
-  (void)s;
-  return 0;
+  int maxLength = -1;
+  int lefts[26] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+  for (int i = 0; s[i] != 0; ++i) {
+    int left_id = s[i] - 'a';
+    if (lefts[left_id] == -1) {
+      lefts[left_id] = i;
+    } else {
+      const int length = i - lefts[left_id] - 1;
+      if (length > maxLength) maxLength = length;
+    }
+  }
+  return maxLength;
 }
