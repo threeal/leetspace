40e22ad16c91334c68b7df3725424f3a5a861c66
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-04 21:31:01 +0700

    feat(problem-1758/c): solve by comparing two possibilities

--- problems/1758/c/solution.c
+++ problems/1758/c/solution.c
@@ -1,4 +1,14 @@
 int minOperations(char* s) {
-  (void)s;
-  return 0;
+  int cost01 = 0;
+
+  int i = 0;
+  while (s[i] != 0) {
+    if (s[i] - '0' != i % 2) {
+      ++cost01;
+    }
+    ++i;
+  }
+
+  const int cost10 = i - cost01;
+  return cost01 < cost10 ? cost01 : cost10;
 }
