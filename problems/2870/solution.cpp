3d323d7c7d9e35ec326e49e134a9fabc2f885e44
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-04 20:59:36 +0700

    feat(problem-2870/cpp): solve by counting number in hash map

--- problems/2870/solution.cpp
+++ problems/2870/solution.cpp
@@ -1,9 +1,22 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int minOperations(std::vector<int>& nums) {
-    (void)nums;
-    return 0;
+    std::unordered_map<int, int> counts;
+
+    for (const auto num : nums) {
+      ++counts[num];
+    }
+
+    int operations = 0;
+    for (const auto [num, count] : counts) {
+      if (count < 2) return -1;
+      operations += count / 3;
+      if (count % 3 > 0) ++operations;
+    }
+
+    return operations;
   }
 };
