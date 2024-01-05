9239cd2af0b94513390278f92e0ee576be61d71a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-05 16:30:20 +0700

    feat(problem-1422/c): solve by iterating twice

--- problems/1422/c/solution.c
+++ problems/1422/c/solution.c
@@ -1,4 +1,26 @@
 int maxScore(char* s) {
-  (void)s;
-  return 0;
+  int zeros = s[0] == '0' ? 1 : 0;
+
+  int ones = 0;
+  for (char* c = s + 1; *c != 0; ++c) {
+    if (*c == '1') ++ones;
+  }
+
+  int score = zeros + ones;
+  for (char* c = s + 1; *(c + 1) != 0; ++c) {
+    switch (*c) {
+      case '0':
+        ++zeros;
+        break;
+
+      case '1':
+        --ones;
+        break;
+    }
+
+    if (zeros + ones > score)
+      score = zeros + ones;
+  }
+
+  return score;
 }
