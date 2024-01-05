00efcc878f5a9e1e0ca47084f775932c1bed0756
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-05 16:13:16 +0700

    feat(problem-300/cpp): try to solve using dynamic programming

--- problems/0300/solution.cpp
+++ problems/0300/solution.cpp
@@ -1,9 +1,27 @@
+#include <functional>
 #include <vector>
 
 class Solution {
  public:
   int lengthOfLIS(std::vector<int>& nums) {
-    (void)nums;
-    return 0;
+    const int n = nums.size();
+    std::vector<int> cache(n, -1);
+
+    const std::function<int(int)> fn = [&](int start) {
+      if (start >= n) return 0;
+      if (cache[start] >= 0) {
+        return cache[start];
+      }
+
+      int length = 0;
+      const int num = start > 0 ? nums[start - 1] : -1;
+      for (int i = start; i < n; ++i) {
+        if (num >= nums[i]) continue;
+        length = std::max(length, 1 + fn(i + 1));
+      }
+      return cache[start] = length;
+    };
+
+    return fn(0);
   }
 };
