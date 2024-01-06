f1b8cf2a891e8edaf6e1e8d5ef7be6a24772c9f0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-06 21:20:37 +0700

    feat(problem-1235/cpp): solve by sorting jobs and decide to take or not

--- problems/1235/solution.cpp
+++ problems/1235/solution.cpp
@@ -1,27 +1,43 @@
+#include <algorithm>
 #include <functional>
 #include <vector>
+
+struct Job {
+  int startTime;
+  int endTime;
+  int profit;
+  int maxProfit = -1;
+};
 
 class Solution {
  public:
   int jobScheduling(std::vector<int>& startTime, std::vector<int>& endTime, std::vector<int>& profit) {
-    std::vector<int> cache(profit.size(), -1);
-    const std::function<int(int)> fn = [&](std::size_t i) {
-      if (cache[i] >= 0) return cache[i];
-
-      int maxProfit = profit[i];
-      for (std::size_t j = 0; j < profit.size(); ++j) {
-        if (startTime[j] < endTime[i]) continue;
-        maxProfit = std::max(maxProfit, profit[i] + fn(j));
-      }
-
-      return cache[i] = maxProfit;
-    };
-
-    int maxProfit = 0;
-    for (std::size_t i = 0; i < profit.size(); ++i) {
-      maxProfit = std::max(maxProfit, fn(i));
+    std::vector<Job> jobs(profit.size());
+    for (std::size_t i = 0; i < jobs.size(); ++i) {
+      jobs[i].startTime = startTime[i];
+      jobs[i].endTime = endTime[i];
+      jobs[i].profit = profit[i];
     }
 
-    return maxProfit;
+    std::sort(jobs.begin(), jobs.end(), [](const Job& a, const Job& b) {
+      return a.startTime < b.startTime;
+    });
+
+    const std::function<int(std::vector<Job>::iterator)> fn = [&](std::vector<Job>::iterator it) {
+      if (it == jobs.end()) return 0;
+      if (it->maxProfit >= 0) return it->maxProfit;
+
+      const int profitIfNotTake = fn(it + 1);
+
+      const auto nextIfTake = std::lower_bound(it + 1, jobs.end(), it->endTime, [](const Job& job, int endTime) {
+        return job.startTime < endTime;
+      });
+
+      const int profitIfTake = it->profit + fn(nextIfTake);
+
+      return it->maxProfit = std::max(profitIfTake, profitIfNotTake);
+    };
+
+    return fn(jobs.begin());
   }
 };
