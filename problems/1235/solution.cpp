ad171f4431a75c4ea4c4e34ee21d62ddc31b8852
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-06 20:30:11 +0700

    feat(problem-1235/cpp): try to solve by using dynamic programming

--- problems/1235/solution.cpp
+++ problems/1235/solution.cpp
@@ -1,11 +1,27 @@
+#include <functional>
 #include <vector>
 
 class Solution {
  public:
   int jobScheduling(std::vector<int>& startTime, std::vector<int>& endTime, std::vector<int>& profit) {
-    (void)startTime;
-    (void)endTime;
-    (void)profit;
-    return 0;
+    std::vector<int> cache(profit.size(), -1);
+    const std::function<int(int)> fn = [&](std::size_t i) {
+      if (cache[i] >= 0) return cache[i];
+
+      int maxProfit = profit[i];
+      for (std::size_t j = 0; j < profit.size(); ++j) {
+        if (startTime[j] < endTime[i]) continue;
+        maxProfit = std::max(maxProfit, profit[i] + fn(j));
+      }
+
+      return cache[i] = maxProfit;
+    };
+
+    int maxProfit = 0;
+    for (std::size_t i = 0; i < profit.size(); ++i) {
+      maxProfit = std::max(maxProfit, fn(i));
+    }
+
+    return maxProfit;
   }
 };
