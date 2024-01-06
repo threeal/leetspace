647f81021c4ac108a043692d38804815495da4bc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-06 14:52:58 +0800

    feat: solve problem 2870. Minimum Number of Operations to Make Array Empty with sorting (#366)

--- problems/2870/solution.cpp
+++ problems/2870/solution.cpp
@@ -1,21 +1,31 @@
-#include <unordered_map>
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int minOperations(std::vector<int>& nums) {
-    std::unordered_map<int, int> counts;
-
-    for (const auto num : nums) {
-      ++counts[num];
-    }
+    sort(nums.begin(), nums.end());
 
     int operations = 0;
-    for (const auto [num, count] : counts) {
+    int num = nums[0];
+    int count = 1;
+    for (std::size_t i = 1; i < nums.size(); ++i) {
+      if (nums[i] == num) {
+        ++count;
+        continue;
+      }
+
       if (count < 2) return -1;
       operations += count / 3;
       if (count % 3 > 0) ++operations;
+
+      num = nums[i];
+      count = 1;
     }
+
+    if (count < 2) return -1;
+    operations += count / 3;
+    if (count % 3 > 0) ++operations;
 
     return operations;
   }
