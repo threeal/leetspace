7488f43a57816fc154398391808c75853d788be5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-06 20:16:28 +0700

    test: add test for problem 1235. Maximum Profit in Job Scheduling

---
+++ problems/1235/solution.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+class Solution {
+ public:
+  int jobScheduling(std::vector<int>& startTime, std::vector<int>& endTime, std::vector<int>& profit) {
+    (void)startTime;
+    (void)endTime;
+    (void)profit;
+    return 0;
+  }
+};
