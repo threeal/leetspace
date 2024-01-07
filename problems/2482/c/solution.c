4ac6ba5657ba128b2b10a0cb730b061a4427f457
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-07 21:09:39 +0700

    feat(problem-2482/c): solve by collect in each rows and cols

--- problems/2482/c/solution.c
+++ problems/2482/c/solution.c
@@ -1,5 +1,31 @@
+#include <stdlib.h>
+
 int** onesMinusZeros(int** grid, int gridSize, int* gridColSize, int* returnSize, int** returnColumnSizes) {
   *returnSize = gridSize;
   *returnColumnSizes = gridColSize;
+
+  // Calculate the total count of elements with a value of one in each row and column.
+  int* oneRow = calloc(gridSize, sizeof(int));
+  int* oneCol = calloc(gridColSize[0], sizeof(int));
+  for (int i = 0; i < gridSize; ++i) {
+    for (int j = 0; j < gridColSize[i]; ++j) {
+      if (grid[i][j] == 1) {
+        ++oneRow[i];
+        ++oneCol[j];
+      }
+    }
+  }
+
+  // Modify the given grid.
+  for (int i = 0; i < gridSize; ++i) {
+    for (int j = 0; j < gridColSize[i]; ++j) {
+      // Given that zeroRow[i] + zeroCol[j] = gridSize + gridColSize[i] - oneRow[i] - oneCol[j].
+      grid[i][j] = 2 * (oneRow[i] + oneCol[j]) - gridSize - gridColSize[i];
+    }
+  }
+
+  free(oneRow);
+  free(oneCol);
+
   return grid;
 }
