cbe7d2a6d525c08d5d17c23b44a8ada37f6553ba
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-07 17:37:39 +0700

    feat(problem-446/cpp): try to solve by using dynamic programming

--- problems/0446/solution.cpp
+++ problems/0446/solution.cpp
@@ -1,9 +1,25 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int numberOfArithmeticSlices(std::vector<int>& nums) {
-    (void)nums;
-    return 0;
+    int total = 0;
+
+    std::vector<std::unordered_map<int, int>> dp(nums.size());
+    for (std::size_t j = 1; j < nums.size(); ++j) {
+      for (std::size_t i = 0; i < j; ++i) {
+        const int diff = nums[j] - nums[i];
+        dp[j][diff] += 1;
+
+        const auto it = dp[i].find(diff);
+        if (it != dp[i].end()) {
+          dp[j][diff] += dp[i][diff];
+          total += dp[i][diff];
+        }
+      }
+    }
+
+    return total;
   }
 };
