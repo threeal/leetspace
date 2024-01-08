f960f706e7fb4fd59947dc9dfb0bcad6de693f97
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-08 21:04:43 +0700

    feat(problem-867/c): solve by constructing arrays with reversed cols and rows

--- problems/0867/c/solution.c
+++ problems/0867/c/solution.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
 
 int** transpose(int** matrix, int matrixSize, int* matrixColSize, int* returnSize, int** returnColumnSizes) {
-  (void)matrix;
-
   *returnSize = matrixColSize[0];
   *returnColumnSizes = malloc(sizeof(int) * (*returnSize));
   int** returnMatrix = malloc(sizeof(int*) * (*returnSize));
   for (int i = 0; i < *returnSize; ++i) {
     (*returnColumnSizes)[i] = matrixSize;
     returnMatrix[i] = malloc(sizeof(int) * (*returnColumnSizes)[i]);
+  }
+
+  for (int i = 0; i < matrixSize; ++i) {
+    for (int j = 0; j < matrixColSize[i]; ++j) {
+      returnMatrix[j][i] = matrix[i][j];
+    }
   }
 
   return returnMatrix;
