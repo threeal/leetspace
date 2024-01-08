359280665481b30be9895402b35a2f061a746424
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-08 20:18:11 +0700

    feat(problem-1582/c): solve by saving coordinate of the special position

--- problems/1582/c/solution.c
+++ problems/1582/c/solution.c
@@ -1,6 +1,32 @@
+#include <stdlib.h>
+
 int numSpecial(int** mat, int matSize, int* matColSize) {
-  (void)mat;
-  (void)matSize;
-  (void)matColSize;
-  return 0;
+  int* rows = malloc(matSize * sizeof(int));
+  for (int i = 0; i < matSize; ++i) {
+    rows[i] = -1;
+  }
+
+  int* cols = malloc(matColSize[0] * sizeof(int));
+  for (int i = 0; i < matColSize[0]; ++i) {
+    cols[i] = -1;
+  }
+
+  for (int i = 0; i < matSize; ++i) {
+    for (int j = 0; j < matColSize[i]; ++j) {
+      if (mat[i][j] == 1) {
+        rows[i] = rows[i] == -1 ? j : -2;
+        cols[j] = cols[j] == -1 ? i : -2;
+      }
+    }
+  }
+
+  int specials = 0;
+  for (int i = 0; i < matSize; ++i) {
+    if (rows[i] >= 0 && cols[rows[i]] == i) ++specials;
+  }
+
+  free(rows);
+  free(cols);
+
+  return specials;
 }
