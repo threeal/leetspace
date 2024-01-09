05ba10c90959f0993a4d3220eac87ea75d1b85f0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-09 19:22:54 +0700

    feat(problem-94/cpp): solve by using recursive function

--- problems/0094/solution.cpp
+++ problems/0094/solution.cpp
@@ -3,7 +3,15 @@
 class Solution {
  public:
   std::vector<int> inorderTraversal(TreeNode* root) {
-    (void)root;
-    return {};
+    std::vector<int> output;
+    push_traversal(output, root);
+    return output;
+  }
+
+  void push_traversal(std::vector<int>& traversal, TreeNode* node) {
+    if (node == nullptr) return;
+    push_traversal(traversal, node->left);
+    traversal.push_back(node->val);
+    push_traversal(traversal, node->right);
   }
 };
