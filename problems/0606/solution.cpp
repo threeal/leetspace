e6e6c77d8376ea9068b7fe5d36c8f0c41b3a0302
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-09 19:48:40 +0700

    feat(problem-606/cpp): solve by implementing ostream operator

--- problems/0606/solution.cpp
+++ problems/0606/solution.cpp
@@ -1,9 +1,27 @@
+#include <sstream>
 #include <string>
+
+std::ostream& operator<<(std::ostream& out, TreeNode* node);
 
 class Solution {
  public:
   std::string tree2str(TreeNode* root) {
-    (void)root;
-    return "";
+    std::stringstream ss;
+    ss << root;
+    return ss.str();
   }
 };
+
+std::ostream& operator<<(std::ostream& out, TreeNode* node) {
+  out << node->val;
+  if (node->right != nullptr) {
+    if (node->left != nullptr) {
+      out << "(" << node->left << ")(" << node->right << ")";
+    } else {
+      out << "()(" << node->right << ")";
+    }
+  } else if (node->left != nullptr) {
+    out << "(" << node->left << ")";
+  }
+  return out;
+}
