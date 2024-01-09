6c0dd0c8e8c3a026e622ac6e9fddb1628f24443c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-09 19:25:23 +0700

    feat(problem-872/cpp): solve by pushing leaves to list

--- problems/0872/solution.cpp
+++ problems/0872/solution.cpp
@@ -1,8 +1,35 @@
+#include <list>
+
 class Solution {
  public:
   bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-    (void)root1;
-    (void)root2;
-    return false;
+    std::list<int> leaves1, leaves2;
+    pushLeaves(leaves1, root1);
+    pushLeaves(leaves2, root2);
+
+    auto it1 = leaves1.begin();
+    auto it2 = leaves2.begin();
+    while (it1 != leaves1.end() && it2 != leaves2.end()) {
+      if (*it1 != *it2) return false;
+      ++it1;
+      ++it2;
+    }
+
+    return it1 == leaves1.end() && it2 == leaves2.end();
+  }
+
+  void pushLeaves(std::list<int>& leaves, TreeNode* node) {
+    if (node->left != nullptr) {
+      pushLeaves(leaves, node->left);
+      if (node->right != nullptr) {
+        pushLeaves(leaves, node->right);
+      }
+    } else {
+      if (node->right != nullptr) {
+        pushLeaves(leaves, node->right);
+      } else {
+        leaves.push_back(node->val);
+      }
+    }
   }
 };
