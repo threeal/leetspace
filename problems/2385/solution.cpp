d909965f8a58ee5acf1a29a4f8e55d8bf8087c14
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-10 23:41:03 +0700

    feat(problem-2385/cpp): solve by using recursive function

--- problems/2385/solution.cpp
+++ problems/2385/solution.cpp
@@ -1,8 +1,66 @@
+struct Distance {
+  int val;
+  int rev;
+};
+
 class Solution {
  public:
   int amountOfTime(TreeNode* root, int start) {
-    (void)root;
-    (void)start;
-    return 0;
+    const auto distance = largestDistance(root, start);
+    return std::max(distance.val, distance.rev);
+  }
+
+  Distance largestDistance(TreeNode* node, int start) {
+    if (node->val == start) {
+      if (node->left != nullptr) {
+        if (node->right != nullptr) {
+          const auto left = largestDistance(node->left, start);
+          const auto right = largestDistance(node->right, start);
+          return {
+              .val = std::max(left.val, right.val),
+              .rev = 0};
+        } else {
+          auto left = largestDistance(node->left, start);
+          left.rev = 0;
+          return left;
+        }
+      } else if (node->right != nullptr) {
+        auto right = largestDistance(node->right, start);
+        right.rev = 0;
+        return right;
+      } else {
+        return {.val = 0, .rev = 0};
+      }
+    } else {
+      if (node->left != nullptr) {
+        if (node->right != nullptr) {
+          const auto left = largestDistance(node->left, start);
+          const auto right = largestDistance(node->right, start);
+          if (left.rev >= 0) {
+            return {
+                .val = std::max(left.val, 1 + right.val + left.rev),
+                .rev = left.rev + 1};
+          } else if (right.rev >= 0) {
+            return {
+                .val = std::max(1 + left.val + right.rev, right.val),
+                .rev = right.rev + 1};
+          } else {
+            return {
+                .val = 1 + std::max(left.val, right.val),
+                .rev = -1};
+          }
+        } else {
+          auto left = largestDistance(node->left, start);
+          left.rev >= 0 ? ++left.rev : ++left.val;
+          return left;
+        }
+      } else if (node->right != nullptr) {
+        auto right = largestDistance(node->right, start);
+        right.rev >= 0 ? ++right.rev : ++right.val;
+        return right;
+      } else {
+        return {.val = 1, .rev = -1};
+      }
+    }
   }
 };
