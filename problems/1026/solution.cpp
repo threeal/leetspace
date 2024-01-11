273793f6ddec88f0d8de0e8acd0d63d231ad188c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-11 22:31:48 +0700

    feat(problem-1026/cpp): solve by storing ancestors in linked list

--- problems/1026/solution.cpp
+++ problems/1026/solution.cpp
@@ -1,7 +1,38 @@
+#include <list>
+
 class Solution {
  public:
   int maxAncestorDiff(TreeNode* root) {
-    (void)root;
-    return 0;
+    std::list<int> ancestors{root->val};
+    return maxDifferences(ancestors, root);
+  }
+
+  int maxDifferences(std::list<int>& ancestors, TreeNode* node) {
+    int differences = std::max(
+        std::abs(ancestors.front() - node->val),
+        std::abs(ancestors.back() - node->val));
+
+    if (node->left == nullptr && node->right == nullptr) {
+      return differences;
+    }
+
+    ancestors.insert(std::lower_bound(ancestors.begin(), ancestors.end(), node->val), node->val);
+
+    if (node->left != nullptr) {
+      if (node->right != nullptr) {
+        differences = std::max({
+            differences,
+            maxDifferences(ancestors, node->left),
+            maxDifferences(ancestors, node->right)});
+      } else {
+        differences = std::max(differences, maxDifferences(ancestors, node->left));
+      }
+    } else if (node->right != nullptr) {
+      differences = std::max(differences, maxDifferences(ancestors, node->right));
+    }
+
+    ancestors.erase(std::find(ancestors.begin(), ancestors.end(), node->val));
+
+    return differences;
   }
 };
