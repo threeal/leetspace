a0096cc0f830a49b2cacbf96c923cad1aca90ac7
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-13 19:55:11 +0700

    feat(problem-1347/cpp): solve by counting characters

--- problems/1347/solution.cpp
+++ problems/1347/solution.cpp
@@ -1,10 +1,24 @@
 #include <string>
+#include <vector>
 
 class Solution {
  public:
   int minSteps(std::string s, std::string t) {
-    (void)s;
-    (void)t;
-    return 0;
+    std::vector<int> counts(26, 0);
+
+    for (const auto c : s) {
+      ++counts[c - 'a'];
+    }
+
+    for (const auto c : t) {
+      --counts[c - 'a'];
+    }
+
+    int diff = 0;
+    for (const auto count : counts) {
+      diff += std::abs(count);
+    }
+
+    return diff / 2;
   }
 };
