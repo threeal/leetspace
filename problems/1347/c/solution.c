066d129e24c2337868c5ef183042703c3056d76f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-13 20:56:50 +0700

    feat(problem-1347/c): solve by counting characters

--- problems/1347/c/solution.c
+++ problems/1347/c/solution.c
@@ -1,5 +1,25 @@
+#include <stdlib.h>
+
 int minSteps(char* s, char* t) {
-  (void)t;
+  int counts[26] = {0};
+
   (void)s;
-  return 0;
+  (void)t;
+
+  while (*s != 0) {
+    ++counts[*s - 'a'];
+    ++s;
+  }
+
+  while (*t != 0) {
+    --counts[*t - 'a'];
+    ++t;
+  }
+
+  int diff = 0;
+  for (int i = 0; i < 26; ++i) {
+    diff += abs(counts[i]);
+  }
+
+  return diff / 2;
 }
