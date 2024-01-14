22b8196760ddaab620ff969e8a5af430d3d696c7
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-14 15:18:59 +0700

    feat(problem-1657/cpp): try to solve by counting characters

--- problems/1657/solution.cpp
+++ problems/1657/solution.cpp
@@ -1,10 +1,32 @@
 #include <string>
+#include <unordered_map>
 
 class Solution {
  public:
   bool closeStrings(std::string word1, std::string word2) {
-    (void)word1;
-    (void)word2;
-    return false;
+    std::unordered_map<char, int> word1Counts;
+    for (const auto c : word1) {
+      ++word1Counts[c];
+    }
+
+    std::unordered_map<char, int> word2Counts;
+    for (const auto c : word2) {
+      ++word2Counts[c];
+    }
+
+    std::unordered_map<int, int> unique_counts;
+    for (const auto& [c, count] : word1Counts) {
+      ++unique_counts[count];
+    }
+
+    for (const auto& [c, count] : word2Counts) {
+      --unique_counts[count];
+    }
+
+    for (const auto& [count, val] : unique_counts) {
+      if (val != 0) return false;
+    }
+
+    return true;
   }
 };
