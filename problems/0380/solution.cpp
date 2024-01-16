1c8b3b148b2e9e13cc411933d3cd1c351a570c9e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-16 13:30:57 +0700

    feat(problem-380/cpp): solve by using map and list

--- problems/0380/solution.cpp
+++ problems/0380/solution.cpp
@@ -1,17 +1,42 @@
+#include <unordered_map>
+
 class RandomizedSet {
+ private:
+  std::unordered_map<int, std::size_t> valuesPositions;
+  std::vector<int> values;
+  std::size_t lastPosition;
+
  public:
-  RandomizedSet() {
-  }
+  RandomizedSet() : lastPosition(0) {}
 
   bool insert(int val) {
-    return val == 0;
+    const auto it = valuesPositions.find(val);
+    if (it != valuesPositions.end()) {
+      return false;
+    }
+    valuesPositions.emplace(val, lastPosition);
+    if (lastPosition >= values.size()) {
+      values.push_back(val);
+    } else {
+      values[lastPosition] = val;
+    }
+    ++lastPosition;
+    return true;
   }
 
   bool remove(int val) {
-    return val == 0;
+    const auto it = valuesPositions.find(val);
+    if (it == valuesPositions.end()) {
+      return false;
+    }
+    --lastPosition;
+    values[it->second] = values[lastPosition];
+    valuesPositions[values[it->second]] = it->second;
+    valuesPositions.erase(it);
+    return true;
   }
 
   int getRandom() {
-    return 0;
+    return values[std::rand() % lastPosition];
   }
 };
