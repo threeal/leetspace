39f8cbcc4248167d8117d96fecda93c5728c244d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-18 14:28:38 +0700

    perf(problem-70/cpp): use two variables instead of array

--- problems/0070/solution.cpp
+++ problems/0070/solution.cpp
@@ -1,18 +1,17 @@
-#include <vector>
-
 class Solution {
  public:
   int climbStairs(int n) {
     if (n <= 2) return n;
 
-    std::vector<int> mem(n + 1);
-    mem[1] = 1;
-    mem[2] = 2;
+    int a = 1;
+    int b = 2;
 
     for (int i = 3; i <= n; ++i) {
-      mem[i] = mem[i - 1] + mem[i - 2];
+      const int val = a + b;
+      a = b;
+      b = val;
     }
 
-    return mem[n];
+    return b;
   }
 };
