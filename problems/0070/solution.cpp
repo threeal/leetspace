a6fd42f9213705b7bd33e4ab74c5240fdb5c74c0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-18 10:45:19 +0700

    feat(problem-70/cpp): solve by using for loop

--- problems/0070/solution.cpp
+++ problems/0070/solution.cpp
@@ -1,6 +1,18 @@
+#include <vector>
+
 class Solution {
  public:
   int climbStairs(int n) {
-    return n;
+    if (n <= 2) return n;
+
+    std::vector<int> mem(n + 1);
+    mem[1] = 1;
+    mem[2] = 2;
+
+    for (int i = 3; i <= n; ++i) {
+      mem[i] = mem[i - 1] + mem[i - 2];
+    }
+
+    return mem[n];
   }
 };
