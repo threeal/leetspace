caf2a7bdc9f78e9c67c80524e1b2d26234e659b0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-19 10:34:22 +0700

    feat: solve problem 70. Climbing Stairs (#424)
    * test: add test for problem 70. Climbing Stairs
    
    * feat(problem-70/cpp): solve by using for loop
    
    * perf(problem-70/cpp): use two variables instead of array

---
+++ problems/0070/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  int climbStairs(int n) {
+    if (n <= 2) return n;
+
+    int a = 1;
+    int b = 2;
+
+    for (int i = 3; i <= n; ++i) {
+      const int val = a + b;
+      a = b;
+      b = val;
+    }
+
+    return b;
+  }
+};
