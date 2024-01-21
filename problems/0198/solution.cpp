62551b425548b2deb4ee8fab5eb7f110ebea9348
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-21 19:15:34 +0700

    feat(problem-198/cpp): solve using recursive function

--- problems/0198/solution.cpp
+++ problems/0198/solution.cpp
@@ -1,8 +1,22 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int rob(std::vector<int>& nums) {
-    return nums[0];
+    std::vector<int> plans(nums.size(), -1);
+    return planRob(plans, nums, 0);
+  }
+
+  int planRob(std::vector<int>& plans, std::vector<int>& nums, std::size_t i) {
+    if (i >= nums.size()) return 0;
+
+    if (plans[i] < 0) {
+      plans[i] = std::max(
+          nums[i] + planRob(plans, nums, i + 2),
+          planRob(plans, nums, i + 1));
+    }
+
+    return plans[i];
   }
 };
