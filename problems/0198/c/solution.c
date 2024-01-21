7ee934dd58d03525b64527b10d3dc96fe9b4178c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-21 19:35:51 +0700

    feat(problem-198/c): solve by using for loop

--- problems/0198/c/solution.c
+++ problems/0198/c/solution.c
@@ -1,3 +1,11 @@
 int rob(int* nums, int numsSize) {
+  if (numsSize >= 2) {
+    if (nums[0] > nums[1]) nums[1] = nums[0];
+    for (int i = 2; i < numsSize; ++i) {
+      nums[i] += nums[i - 2];
+      if (nums[i - 1] > nums[i]) nums[i] = nums[i - 1];
+    }
+  }
+
   return nums[numsSize - 1];
 }
