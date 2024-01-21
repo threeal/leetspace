89a311c48b79d1d40598a2eafce21a9f82ab1933
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-21 19:24:14 +0700

    perf(problem-198/cpp): use for loop instead

--- problems/0198/solution.cpp
+++ problems/0198/solution.cpp
@@ -4,19 +4,15 @@
 class Solution {
  public:
   int rob(std::vector<int>& nums) {
-    std::vector<int> plans(nums.size(), -1);
-    return planRob(plans, nums, 0);
-  }
-
-  int planRob(std::vector<int>& plans, std::vector<int>& nums, std::size_t i) {
-    if (i >= nums.size()) return 0;
-
-    if (plans[i] < 0) {
-      plans[i] = std::max(
-          nums[i] + planRob(plans, nums, i + 2),
-          planRob(plans, nums, i + 1));
+    if (nums.size() >= 2) {
+      nums[1] = std::max(nums[0], nums[1]);
+      for (std::size_t i = 2; i < nums.size(); ++i) {
+        nums[i] = std::max(
+            nums[i] + nums[i - 2],
+            nums[i - 1]);
+      }
     }
 
-    return plans[i];
+    return nums.back();
   }
 };
