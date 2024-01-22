0636816d3e85eda825fd0d78d917da09e41175a2
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-22 19:22:37 +0700

    feat(problem-645/c): solve by using for loop

--- problems/0645/c/solution.c
+++ problems/0645/c/solution.c
@@ -4,8 +4,23 @@ int* findErrorNums(int* nums, int numsSize, int* returnSize) {
   *returnSize = 2;
   int* returnData = malloc(*returnSize * sizeof(int));
 
-  returnData[0] = nums[0];
-  returnData[1] = nums[numsSize - 1];
+  for (int i = 0; i < numsSize; ++i) {
+    int num = nums[i];
+    if (num < 0) num *= -1;
+
+    if (nums[num - 1] < 0) {
+      returnData[0] = num;
+    } else {
+      nums[num - 1] *= -1;
+    }
+  }
+
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] > 0) {
+      returnData[1] = i + 1;
+      break;
+    }
+  }
 
   return returnData;
 }
