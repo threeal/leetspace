e665a396c331543ed30588cb01d780b797358a51
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-22 18:47:33 +0700

    feat(problem-645/cpp): solve by using for loop

--- problems/0645/solution.cpp
+++ problems/0645/solution.cpp
@@ -3,6 +3,26 @@
 class Solution {
  public:
   std::vector<int> findErrorNums(std::vector<int>& nums) {
-    return nums;
+    int twice = 0;
+    int missing = 0;
+
+    const int n = nums.size();
+    std::vector<bool> flags(n + 1, false);
+
+    for (const auto num : nums) {
+      if (flags[num])
+        twice = num;
+      else
+        flags[num] = true;
+    }
+
+    for (int i = 1; i <= n; ++i) {
+      if (!flags[i]) {
+        missing = i;
+        break;
+      }
+    }
+
+    return {twice, missing};
   }
 };
