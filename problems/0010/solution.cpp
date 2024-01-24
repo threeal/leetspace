149088be8bd002cce5822d22ac46a7a83a38c5c9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-24 23:23:22 +0700

    feat(problem-10/cpp): try to solve by using recursive function

--- problems/0010/solution.cpp
+++ problems/0010/solution.cpp
@@ -3,6 +3,26 @@
 class Solution {
  public:
   bool isMatch(std::string s, std::string p) {
-    return s == p;
+    if (p.empty()) {
+      return s.empty();
+    } else if (s.empty()) {
+      return p.size() == 2 && p[1] == '*';
+    } else if (p.size() >= 2 && p[1] == '*') {
+      if (p[0] == '.') {
+        if (isMatch(s, p.substr(2))) {
+          return true;
+        } else {
+          return isMatch(s.substr(1), p);
+        }
+      } else if (s[0] == p[0]) {
+        return isMatch(s.substr(1), p);
+      } else {
+        return isMatch(s, p.substr(2));
+      }
+    } else if (s[0] == p[0]) {
+      return isMatch(s.substr(1), p.substr(1));
+    } else {
+      return false;
+    }
   }
 };
