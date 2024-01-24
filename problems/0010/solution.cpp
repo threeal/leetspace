ca7352cd93bd08408f47a82062c8c588e120858b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-24 23:34:15 +0700

    feat(problem-10/cpp): try to solve by handling edge case

--- problems/0010/solution.cpp
+++ problems/0010/solution.cpp
@@ -6,20 +6,22 @@ class Solution {
     if (p.empty()) {
       return s.empty();
     } else if (s.empty()) {
-      return p.size() == 2 && p[1] == '*';
+      if (p.size() >= 2 && p[1] == '*') {
+        return isMatch(s, p.substr(2));
+      } else {
+        return false;
+      }
     } else if (p.size() >= 2 && p[1] == '*') {
-      if (p[0] == '.') {
-        if (isMatch(s, p.substr(2))) {
+      if (s[0] == p[0] || p[0] == '.') {
+        if (isMatch(s.substr(1), p)) {
           return true;
         } else {
-          return isMatch(s.substr(1), p);
+          return isMatch(s, p.substr(2));
         }
-      } else if (s[0] == p[0]) {
-        return isMatch(s.substr(1), p);
       } else {
         return isMatch(s, p.substr(2));
       }
-    } else if (s[0] == p[0]) {
+    } else if (s[0] == p[0] || p[0] == '.') {
       return isMatch(s.substr(1), p.substr(1));
     } else {
       return false;
