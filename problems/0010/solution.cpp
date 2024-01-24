8c29653c5bf9efb3acabe3113506b12777a8048a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-24 23:41:07 +0700

    feat(problem-10/cpp): solve by passing char array instead of string

--- problems/0010/solution.cpp
+++ problems/0010/solution.cpp
@@ -3,26 +3,30 @@
 class Solution {
  public:
   bool isMatch(std::string s, std::string p) {
-    if (p.empty()) {
-      return s.empty();
-    } else if (s.empty()) {
-      if (p.size() >= 2 && p[1] == '*') {
-        return isMatch(s, p.substr(2));
+    return isMatch(s.c_str(), p.c_str());
+  }
+
+  bool isMatch(const char* s, const char* p) {
+    if (p[0] == 0) {
+      return s[0] == 0;
+    } else if (s[0] == 0) {
+      if (p[0] != 0 && p[1] == '*') {
+        return isMatch(s, p + 2);
       } else {
         return false;
       }
-    } else if (p.size() >= 2 && p[1] == '*') {
+    } else if (p[0] != 0 && p[1] == '*') {
       if (s[0] == p[0] || p[0] == '.') {
-        if (isMatch(s.substr(1), p)) {
+        if (isMatch(s + 1, p)) {
           return true;
         } else {
-          return isMatch(s, p.substr(2));
+          return isMatch(s, p + 2);
         }
       } else {
-        return isMatch(s, p.substr(2));
+        return isMatch(s, p + 2);
       }
     } else if (s[0] == p[0] || p[0] == '.') {
-      return isMatch(s.substr(1), p.substr(1));
+      return isMatch(s + 1, p + 1);
     } else {
       return false;
     }
