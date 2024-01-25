f36fe87a965534e9c4a9038038a68fa80a275a54
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-26 01:33:52 +0700

    feat(problem-10/c): solve by using recursive function

--- problems/0010/c/solution.c
+++ problems/0010/c/solution.c
@@ -1,5 +1,27 @@
 #include <stdbool.h>
 
 bool isMatch(char* s, char* p) {
-  return *s == *p;
+  if (p[0] == 0) {
+    return s[0] == 0;
+  } else if (s[0] == 0) {
+    if (p[0] != 0 && p[1] == '*') {
+      return isMatch(s, p + 2);
+    } else {
+      return false;
+    }
+  } else if (p[0] != 0 && p[1] == '*') {
+    if (s[0] == p[0] || p[0] == '.') {
+      if (isMatch(s + 1, p)) {
+        return true;
+      } else {
+        return isMatch(s, p + 2);
+      }
+    } else {
+      return isMatch(s, p + 2);
+    }
+  } else if (s[0] == p[0] || p[0] == '.') {
+    return isMatch(s + 1, p + 1);
+  } else {
+    return false;
+  }
 }
