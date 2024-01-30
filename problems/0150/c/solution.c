db736c913548eb7bbf023cbc1bd8c14170543c8d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-30 22:06:10 +0700

    feat(problem-150/c): solve by using array as the stack

--- problems/0150/c/solution.c
+++ problems/0150/c/solution.c
@@ -1,5 +1,45 @@
+#include <stdlib.h>
+
 int evalRPN(char** tokens, int tokensSize) {
-  (void)tokens;
-  (void)tokensSize;
-  return 0;
+  int stacksIt = 0;
+  int* stacks = malloc(sizeof(int) * (tokensSize + 1) / 2);
+
+  for (int i = 0; i < tokensSize; ++i) {
+    if (tokens[i][1] == 0) {
+      switch (tokens[i][0]) {
+        case '+':
+          stacks[stacksIt - 2] += stacks[stacksIt - 1];
+          --stacksIt;
+          break;
+
+        case '-':
+          stacks[stacksIt - 2] -= stacks[stacksIt - 1];
+          --stacksIt;
+          break;
+
+        case '*':
+          stacks[stacksIt - 2] *= stacks[stacksIt - 1];
+          --stacksIt;
+          break;
+
+        case '/':
+          stacks[stacksIt - 2] /= stacks[stacksIt - 1];
+          --stacksIt;
+          break;
+
+        default:
+          stacks[stacksIt] = tokens[i][0] - '0';
+          ++stacksIt;
+          break;
+      }
+    } else {
+      stacks[stacksIt] = atoi(tokens[i]);
+      ++stacksIt;
+    }
+  }
+
+  int res = stacks[stacksIt - 1];
+  free(stacks);
+
+  return res;
 }
