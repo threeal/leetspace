d27ae1dcacf01cde13e15b3b7cb5d05043c29058
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-31 18:48:52 +0700

    feat(problem-739/cpp): solve by using array of future days

--- problems/0739/solution.cpp
+++ problems/0739/solution.cpp
@@ -1,8 +1,17 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> dailyTemperatures(std::vector<int>& temperatures) {
+    std::vector<int> futures(101, -1);
+
+    for (int i = temperatures.size() - 1; i >= 0; --i) {
+      const auto temperature = temperatures[i];
+      temperatures[i] = std::max(futures[temperature] - i, 0);
+      std::fill_n(futures.begin(), temperature, i);
+    }
+
     return temperatures;
   }
 };
