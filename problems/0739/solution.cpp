bf411a0a3442da214437eb95a3ec500e0c385742
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-31 18:53:55 +0700

    perf(problem-739/cpp): optimize `futures` size

--- problems/0739/solution.cpp
+++ problems/0739/solution.cpp
@@ -4,10 +4,10 @@
 class Solution {
  public:
   std::vector<int> dailyTemperatures(std::vector<int>& temperatures) {
-    std::vector<int> futures(101, -1);
+    std::vector<std::int32_t> futures(71, -1);
 
-    for (int i = temperatures.size() - 1; i >= 0; --i) {
-      const auto temperature = temperatures[i];
+    for (std::int32_t i = temperatures.size() - 1; i >= 0; --i) {
+      const auto temperature = temperatures[i] - 30;
       temperatures[i] = std::max(futures[temperature] - i, 0);
       std::fill_n(futures.begin(), temperature, i);
     }
