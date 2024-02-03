75bcec4657dd3081ed0aa0ec9034cf905183388b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-03 10:16:12 +0700

    perf(problem-1043/cpp): optimize function calls

--- problems/1043/solution.cpp
+++ problems/1043/solution.cpp
@@ -1,35 +1,33 @@
-#include <functional>
 #include <vector>
 
 class Solution {
  public:
   int maxSumAfterPartitioning(std::vector<int>& arr, int k) {
     std::vector<int> cache(arr.size(), -1);
+    return recursive(arr, k, cache, 1, arr[0], 1);
+  }
 
-    const std::function<int(std::size_t, int, int)> fn = [&](std::size_t i, int maxVal, int len) -> int {
-      if (i >= arr.size()) {
-        return maxVal * len;
-      }
+  int recursive(const std::vector<int>& arr, int k, std::vector<int>& cache, std::size_t i, int maxVal, int len) {
+    if (i >= arr.size()) {
+      return maxVal * len;
+    }
 
-      if (len >= k) {
-        return maxVal * len + fn(i + 1, arr[i], 1);
-      }
+    if (len >= k) {
+      return maxVal * len + recursive(arr, k, cache, i + 1, arr[i], 1);
+    }
 
-      if (len == 1 && cache[i] > 0) {
-        return cache[i];
-      }
+    if (len == 1 && cache[i] > 0) {
+      return cache[i];
+    }
 
-      const auto res = std::max(
-          fn(i + 1, std::max(maxVal, arr[i]), len + 1),
-          maxVal * len + fn(i + 1, arr[i], 1));
+    const auto res = std::max(
+        recursive(arr, k, cache, i + 1, std::max(maxVal, arr[i]), len + 1),
+        maxVal * len + recursive(arr, k, cache, i + 1, arr[i], 1));
 
-      if (len == 1) {
-        cache[i] = res;
-      }
+    if (len == 1) {
+      cache[i] = res;
+    }
 
-      return res;
-    };
-
-    return fn(1, arr[0], 1);
+    return res;
   }
 };
