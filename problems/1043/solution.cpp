41903c5cc3695039aeeb4719adf80f02f2e0a7b4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-03 10:02:33 +0700

    feat(problem-1043/cpp): solve by cache the calculation

--- problems/1043/solution.cpp
+++ problems/1043/solution.cpp
@@ -4,6 +4,8 @@
 class Solution {
  public:
   int maxSumAfterPartitioning(std::vector<int>& arr, int k) {
+    std::vector<int> cache(arr.size(), -1);
+
     const std::function<int(std::size_t, int, int)> fn = [&](std::size_t i, int maxVal, int len) -> int {
       if (i >= arr.size()) {
         return maxVal * len;
@@ -13,9 +15,19 @@ class Solution {
         return maxVal * len + fn(i + 1, arr[i], 1);
       }
 
-      return std::max(
+      if (len == 1 && cache[i] > 0) {
+        return cache[i];
+      }
+
+      const auto res = std::max(
           fn(i + 1, std::max(maxVal, arr[i]), len + 1),
           maxVal * len + fn(i + 1, arr[i], 1));
+
+      if (len == 1) {
+        cache[i] = res;
+      }
+
+      return res;
     };
 
     return fn(1, arr[0], 1);
