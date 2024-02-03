b7ae5b7c1686d789d976f9c0781cc05ef6178612
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-03 09:50:42 +0700

    feat(problem-1043/cpp): try to solve by using recursive function

--- problems/1043/solution.cpp
+++ problems/1043/solution.cpp
@@ -1,8 +1,23 @@
+#include <functional>
 #include <vector>
 
 class Solution {
  public:
   int maxSumAfterPartitioning(std::vector<int>& arr, int k) {
-    return arr[0] + k;
+    const std::function<int(std::size_t, int, int)> fn = [&](std::size_t i, int maxVal, int len) -> int {
+      if (i >= arr.size()) {
+        return maxVal * len;
+      }
+
+      if (len >= k) {
+        return maxVal * len + fn(i + 1, arr[i], 1);
+      }
+
+      return std::max(
+          fn(i + 1, std::max(maxVal, arr[i]), len + 1),
+          maxVal * len + fn(i + 1, arr[i], 1));
+    };
+
+    return fn(1, arr[0], 1);
   }
 };
