df6f95ffb745d9e5388c013c6b4e40e4cc172e30
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-06 10:56:33 +0700

    test: add test for problem 49. Group Anagrams

---
+++ problems/0049/solution.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+    return {strs};
+  }
+};
