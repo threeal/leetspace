888ab2e9878b28ce2df89b000dcee29897bf4a52
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-06 11:06:49 +0700

    feat(problem-49/cpp): solve by group by sorted string

--- problems/0049/solution.cpp
+++ problems/0049/solution.cpp
@@ -1,9 +1,24 @@
+#include <algorithm>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
-    return {strs};
+    std::unordered_map<std::string, std::vector<std::string>> groupStrs;
+
+    for (const auto& str : strs) {
+      auto key = str;
+      std::sort(key.begin(), key.end());
+      groupStrs[key].push_back(str);
+    }
+
+    std::vector<std::vector<std::string>> output;
+    for (const auto& [key, strs] : groupStrs) {
+      output.push_back(strs);
+    }
+
+    return output;
   }
 };
