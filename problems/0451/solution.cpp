26764194c94fa9b43b43a356b37c0790a84845f2
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-07 12:05:04 +0700

    feat(problem-451/cpp): solve by using hash map and sorting

--- problems/0451/solution.cpp
+++ problems/0451/solution.cpp
@@ -1,8 +1,33 @@
+#include <algorithm>
+#include <cstring>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 class Solution {
  public:
   std::string frequencySort(std::string s) {
+    std::unordered_map<char, int> counts{};
+    for (const auto c : s) {
+      ++counts[c];
+    }
+
+    std::vector<char> order{};
+    order.reserve(counts.size());
+    for (const auto [c, count] : counts) {
+      order.push_back(c);
+    }
+
+    std::sort(order.begin(), order.end(), [&](char a, char b) {
+      return counts[a] > counts[b];
+    });
+
+    auto ptr = s.data();
+    for (const auto c : order) {
+      std::memset(ptr, c, counts[c]);
+      ptr += counts[c];
+    }
+
     return s;
   }
 };
