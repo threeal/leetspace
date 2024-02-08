b652c4a8a071cc6123cb2d03ca1d75c64bec051b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-08 22:47:36 +0700

    perf(problem-1921/c): use `qsort` function

--- problems/1921/c/solution.c
+++ problems/1921/c/solution.c
@@ -1,4 +1,8 @@
-void quickSort(int* low, int* high);
+#include <stdlib.h>
+
+int comp(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
 
 int eliminateMaximum(int* dist, int distSize, int* speed, int speedSize) {
   // Calculate the time required before enemies reach distance zero.
@@ -8,13 +12,8 @@ int eliminateMaximum(int* dist, int distSize, int* speed, int speedSize) {
     dist[i] = dist[i] / speed[i] + (dist[i] % speed[i] > 0 ? 1 : 0);
   }
 
-  // Sort the time required from lowest to highest (don't sort if already sorted).
-  for (int i = 1; i < distSize; ++i) {
-    if (dist[i - 1] > dist[i]) {
-      quickSort(dist, dist + distSize - 1);
-      break;
-    }
-  }
+  // Sort the time required from lowest to highest.
+  qsort(dist, distSize, sizeof(int), comp);
 
   // Time equals to kills. Iterate through the time required as long as the current time is not greater than the time required.
   int kills = 0;
@@ -24,28 +23,4 @@ int eliminateMaximum(int* dist, int distSize, int* speed, int speedSize) {
   }
 
   return kills;
-}
-
-// This function implements quick sort to sort the given range of arrays.
-// See: https://www.geeksforgeeks.org/quick-sort/
-void quickSort(int* low, int* high) {
-  if (low >= high) return;
-
-  int* i = low - 1;
-
-  for (int* j = low; j < high; ++j) {
-    if (*j >= *high) continue;
-    ++i;
-    const int temp = *i;
-    *i = *j;
-    *j = temp;
-  }
-
-  ++i;
-  const int temp = *i;
-  *i = *high;
-  *high = temp;
-
-  quickSort(low, i - 1);
-  quickSort(i + 1, high);
 }
