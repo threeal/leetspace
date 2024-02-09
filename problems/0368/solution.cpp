2196dc0afc916f670106308a913439426fdd515b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-09 10:58:09 +0700

    feat(problem-368/cpp): solve by sorting and comparing

--- problems/0368/solution.cpp
+++ problems/0368/solution.cpp
@@ -1,8 +1,35 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> largestDivisibleSubset(std::vector<int>& nums) {
-    return nums;
+    const int n = nums.size();
+    std::sort(nums.begin(), nums.end());
+
+    std::vector<int> prevOf(n, -1);
+    std::vector<int> countOf(n, 1);
+
+    int candidate = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int j = i - 1; j >= 0; --j) {
+        if (nums[i] % nums[j] == 0 && countOf[j] + 1 > countOf[i]) {
+          prevOf[i] = j;
+          countOf[i] = countOf[j] + 1;
+        }
+      }
+
+      if (countOf[i] > countOf[candidate]) {
+        candidate = i;
+      }
+    }
+
+    std::vector<int> output{};
+    output.reserve(countOf[candidate]);
+    for (int i = candidate; i >= 0; i = prevOf[i]) {
+      output.push_back(nums[i]);
+    }
+
+    return output;
   }
 };
