88a14c29b0e20ca3d74603a78e9eaa5384af2d6b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-10 12:39:16 +0700

    feat(problem-647/cpp): solve by using while loop

--- problems/0647/solution.cpp
+++ problems/0647/solution.cpp
@@ -3,6 +3,25 @@
 class Solution {
  public:
   int countSubstrings(std::string s) {
-    return s.size();
+    const int n = s.size();
+
+    int count = 0;
+    for (int i = 0; i < n; ++i) {
+      int len = 1;
+      while (i - len >= 0 && i + len < n) {
+        if (s[i - len] != s[i + len]) break;
+        ++len;
+      }
+      count += len;
+
+      len = 0;
+      while (i - 1 - len >= 0 && i + len < n) {
+        if (s[i - 1 - len] != s[i + len]) break;
+        ++len;
+      }
+      count += len;
+    }
+
+    return count;
   }
 };
