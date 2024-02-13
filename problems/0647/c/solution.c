b8a3ab09800cc245b018ecaabd3c6565bbd4aaad
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-13 10:48:53 +0700

    feat(problem-647/c): solve by using while loop

--- problems/0647/c/solution.c
+++ problems/0647/c/solution.c
@@ -1,4 +1,22 @@
 int countSubstrings(char* s) {
-  (void)s;
-  return 0;
+  int count = 0;
+  for (char* c = s; *c != 0; ++c) {
+    char* l = c - 1;
+    char* r = c + 1;
+    while (l >= s && *l == *r) {
+      --l;
+      ++r;
+    }
+    count += r - c;
+
+    l = c - 1;
+    r = c;
+    while (l >= s && *l == *r) {
+      --l;
+      ++r;
+    }
+    count += r - c;
+  }
+
+  return count;
 }
