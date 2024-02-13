c280f6a02a1209ada3df68f51ccaeafe5d1ae5f8
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-13 10:35:14 +0700

    feat(problem-2108/cpp): solve by using iteration

--- problems/2108/solution.cpp
+++ problems/2108/solution.cpp
@@ -4,6 +4,15 @@
 class Solution {
  public:
   std::string firstPalindrome(std::vector<std::string>& words) {
-    return words.front();
+    for (const auto& word : words) {
+      int l = 0;
+      int r = word.size() - 1;
+      while (l < r && word[l] == word[r]) {
+        ++l;
+        --r;
+      }
+      if (l >= r) return word;
+    }
+    return "";
   }
 };
