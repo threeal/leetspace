403756afa286e7b77a90a09d6947d5a45ed200e9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-14 19:43:36 +0700

    perf(problem-169/c): optimize if condition in for loop

--- problems/0169/c/solution.c
+++ problems/0169/c/solution.c
@@ -2,14 +2,15 @@ int majorityElement(int* nums, int numsSize) {
   int num = nums[0];
   int count = 1;
   for (int i = 1; i < numsSize; ++i) {
-    if (count == 0) {
-      num = nums[i];
-      count = 1;
+    if (nums[i] == num) {
+      ++count;
     } else {
-      if (nums[i] == num)
-        ++count;
-      else
-        --count;
+      --count;
+      if (count == 0) {
+        ++i;
+        num = nums[i];
+        count = 1;
+      }
     }
   }
   return num;
