068e4268690352c7214ebb317e40f47993542d60
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-14 18:10:41 +0700

    feat(problem-2149/cpp): solve by filling arrays with iteration

--- problems/2149/solution.cpp
+++ problems/2149/solution.cpp
@@ -3,6 +3,20 @@
 class Solution {
  public:
   std::vector<int> rearrangeArray(std::vector<int>& nums) {
-    return nums;
+    std::vector<int> output(nums.size());
+
+    std::size_t positive = 0;
+    std::size_t negative = 1;
+    for (const auto num : nums) {
+      if (num >= 0) {
+        output[positive] = num;
+        positive += 2;
+      } else {
+        output[negative] = num;
+        negative += 2;
+      }
+    }
+
+    return output;
   }
 };
