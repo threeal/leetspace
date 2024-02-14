5e31dd3e434ca2fa4cbe184a5621d5d07429fd87
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-14 20:17:00 +0700

    feat(problem-2149/c): solve by filling arrays with iteration

--- problems/2149/c/solution.c
+++ problems/2149/c/solution.c
@@ -1,4 +1,20 @@
+#include <stdlib.h>
+
 int* rearrangeArray(int* nums, int numsSize, int* returnSize) {
   *returnSize = numsSize;
-  return nums;
+  int* output = malloc(numsSize * sizeof(int));
+
+  int* positive = output;
+  int* negative = output + 1;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] >= 0) {
+      *positive = nums[i];
+      positive += 2;
+    } else {
+      *negative = nums[i];
+      negative += 2;
+    }
+  }
+
+  return output;
 }
