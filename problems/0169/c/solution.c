b295af8f90f033fe6410c8ed2be616b50f5d9c29
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-14 19:40:50 +0700

    test(problem-169/c): solve by using iteration

--- problems/0169/c/solution.c
+++ problems/0169/c/solution.c
@@ -1,3 +1,16 @@
 int majorityElement(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int num = nums[0];
+  int count = 1;
+  for (int i = 1; i < numsSize; ++i) {
+    if (count == 0) {
+      num = nums[i];
+      count = 1;
+    } else {
+      if (nums[i] == num)
+        ++count;
+      else
+        --count;
+    }
+  }
+  return num;
 }
