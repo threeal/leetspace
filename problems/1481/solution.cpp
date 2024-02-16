aea6ad9ee8daf74381429945f7298047b1ccb4c4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-16 10:30:43 +0700

    feat(problem-1481/cpp): solve by counting uniques and sort

--- problems/1481/solution.cpp
+++ problems/1481/solution.cpp
@@ -1,8 +1,29 @@
+#include <algorithm>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int findLeastNumOfUniqueInts(std::vector<int>& arr, int k) {
-    return arr.size() - k;
+    std::unordered_map<int, int> uniqueCounts;
+    for (const auto val : arr) {
+      ++uniqueCounts[val];
+    }
+
+    std::vector<int> counts;
+    counts.reserve(uniqueCounts.size());
+    for (const auto [val, count] : uniqueCounts) {
+      counts.push_back(count);
+    }
+
+    std::sort(counts.begin(), counts.end());
+
+    auto it = counts.begin();
+    while (it != counts.end() && k >= *it) {
+      k -= *it;
+      ++it;
+    }
+
+    return std::distance(it, counts.end());
   }
 };
