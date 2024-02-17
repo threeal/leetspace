ab90b0a8df2b22c81782607439fe18b25fc39da7
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-17 16:07:41 +0700

    feat(problem-2971/c): solve by sorting and iterating from largest

--- problems/2971/c/solution.c
+++ problems/2971/c/solution.c
@@ -1,3 +1,23 @@
+#include <stdlib.h>
+
+int comp(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 long long largestPerimeter(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  qsort(nums, numsSize, sizeof(int), comp);
+
+  long long sum = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    sum += nums[i];
+  }
+
+  for (int i = numsSize - 1; i >= 2; --i) {
+    sum -= nums[i];
+    if (sum > nums[i]) {
+      return sum + nums[i];
+    }
+  }
+
+  return -1;
 }
