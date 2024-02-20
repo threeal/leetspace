6736a3c641dee1cb5ecb5c58298c6ebab869d172
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-20 09:49:50 +0700

    feat(problem-268/cpp): solve by using sort and iteration

--- problems/0268/solution.cpp
+++ problems/0268/solution.cpp
@@ -1,8 +1,13 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int missingNumber(std::vector<int>& nums) {
-    return nums[0];
+    std::sort(nums.begin(), nums.end());
+    for (int i = 0; i < static_cast<int>(nums.size()); ++i) {
+      if (nums[i] != i) return i;
+    }
+    return nums.size();
   }
 };
