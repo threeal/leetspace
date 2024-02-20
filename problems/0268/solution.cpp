16fc1ba668c78ea8ca75497d33066e4d3790b331
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-20 09:59:23 +0700

    perf(problem-268/cpp): simplify diff calculation

--- problems/0268/solution.cpp
+++ problems/0268/solution.cpp
@@ -1,12 +1,15 @@
-#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   int missingNumber(std::vector<int>& nums) {
     const int n = nums.size();
-    const auto expected = (n * n + n) / 2;
-    const auto actual = std::accumulate(nums.begin(), nums.end(), 0, std::plus<int>());
-    return expected - actual;
+
+    auto total = (n * n + n) / 2;
+    for (const auto num : nums) {
+      total -= num;
+    }
+
+    return total;
   }
 };
