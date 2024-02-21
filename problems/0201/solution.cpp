fdecf32c982afbde521abfeedfee1c6529fb9b95
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-21 13:28:15 +0700

    feat(problem-201/cpp): solve by finding common prefix bit

--- problems/0201/solution.cpp
+++ problems/0201/solution.cpp
@@ -1,6 +1,12 @@
 class Solution {
  public:
   int rangeBitwiseAnd(int left, int right) {
-    return left & right;
+    int shift = 0;
+    while (left != right) {
+      left >>= 1;
+      right >>= 1;
+      ++shift;
+    }
+    return left << shift;
   }
 };
