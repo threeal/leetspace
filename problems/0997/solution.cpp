8fefb2ca4539a2ed2f2d6d7b7c56ccc99221a0de
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-22 12:31:06 +0700

    feat(problem-997/cpp): solve by checking trust count

--- problems/0997/solution.cpp
+++ problems/0997/solution.cpp
@@ -3,7 +3,20 @@
 class Solution {
  public:
   int findJudge(int n, std::vector<std::vector<int>>& trust) {
-    (void)trust;
-    return n - 1;
+    std::vector<int> trustCount(n, 0);
+    std::vector<bool> trustAnyone(n, false);
+
+    for (const auto& t : trust) {
+      trustAnyone[t[0] - 1] = true;
+      ++trustCount[t[1] - 1];
+    }
+
+    for (int i = 0; i < n; ++i) {
+      if (trustCount[i] == n - 1 && !trustAnyone[i]) {
+        return i + 1;
+      }
+    }
+
+    return -1;
   }
 };
