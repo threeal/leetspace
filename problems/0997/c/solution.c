145ea2ad6de22b82aaa414d181a50feeb3752874
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-24 20:51:53 +0700

    feat(problem-997/c): solve by checking trust count

--- problems/0997/c/solution.c
+++ problems/0997/c/solution.c
@@ -1,7 +1,27 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
 int findJudge(int n, int** trust, int trustSize, int* trustColSize) {
-  (void)n;
-  (void)trust;
-  (void)trustSize;
   (void)trustColSize;
+
+  int* trustCount = malloc(n * sizeof(int));
+  bool* trustAnyone = malloc(n * sizeof(bool));
+
+  for (int i = 0; i < n; ++i) {
+    trustCount[i] = 0;
+    trustAnyone[i] = false;
+  }
+
+  for (int i = 0; i < trustSize; ++i) {
+    trustAnyone[trust[i][0] - 1] = true;
+    ++trustCount[trust[i][1] - 1];
+  }
+
+  for (int i = 0; i < n; ++i) {
+    if (trustCount[i] == n - 1 && !trustAnyone[i]) {
+      return i + 1;
+    }
+  }
+
   return -1;
 }
