24c2ed37a69f8985da629c5d81d417b741d5ffd4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-26 10:18:43 +0700

    feat(problem-100/cpp): solve by calling function recursively

--- problems/0100/solution.cpp
+++ problems/0100/solution.cpp
@@ -1,6 +1,13 @@
 class Solution {
  public:
   bool isSameTree(TreeNode* p, TreeNode* q) {
-    return p == q;
+    if (p == nullptr || q == nullptr) return p == q;
+
+    if (p->val != q->val) return false;
+
+    if (!isSameTree(p->left, q->left)) return false;
+    if (!isSameTree(p->right, q->right)) return false;
+
+    return true;
   }
 };
