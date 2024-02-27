f4e3c8d7e45f4ffde34ccf2f50c0366150db6809
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-27 11:32:07 +0700

    feat(problem-543/cpp): solve by finding diameter recursively

--- problems/0543/solution.cpp
+++ problems/0543/solution.cpp
@@ -1,7 +1,16 @@
 class Solution {
  public:
   int diameterOfBinaryTree(TreeNode* root) {
-    (void)root;
-    return 0;
+    int max = 0;
+    findDiameter(root, max);
+    return max;
+  }
+
+  int findDiameter(TreeNode* node, int& max) {
+    if (node == nullptr) return 0;
+    const int left = findDiameter(node->left, max);
+    const int right = findDiameter(node->right, max);
+    if (left + right > max) max = left + right;
+    return 1 + (left > right ? left : right);
   }
 };
