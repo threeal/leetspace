01d205d809a13cb7f970dc4239726278f873465c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-29 10:09:43 +0700

    feat(problem-1609/cpp): solve by iterating node per layer

--- problems/1609/solution.cpp
+++ problems/1609/solution.cpp
@@ -1,6 +1,34 @@
+#include <list>
+#include <utility>
+
 class Solution {
  public:
   bool isEvenOddTree(TreeNode* root) {
-    return root->val % 2 == 0;
+    std::list<TreeNode*> nodes{root};
+    std::list<TreeNode*> prevNodes;
+
+    bool isOdd = true;
+    while (!nodes.empty()) {
+      std::swap(prevNodes, nodes);
+      nodes.clear();
+
+      int prevVal = isOdd ? 0 : 1000001;
+      for (const auto node : prevNodes) {
+        if (isOdd) {
+          if (node->val <= prevVal || node->val % 2 != 1)
+            return false;
+        } else {
+          if (node->val >= prevVal || node->val % 2 != 0)
+            return false;
+        }
+        prevVal = node->val;
+        if (node->left != nullptr) nodes.push_back(node->left);
+        if (node->right != nullptr) nodes.push_back(node->right);
+      }
+
+      isOdd = !isOdd;
+    }
+
+    return true;
   }
 };
