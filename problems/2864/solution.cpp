9409f5e6a328892ca645213c769e9d8210cfbecd
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-01 12:03:02 +0700

    feat(problem-2864/cpp): solve by counting `1`'s

--- problems/2864/solution.cpp
+++ problems/2864/solution.cpp
@@ -3,6 +3,27 @@
 class Solution {
  public:
   std::string maximumOddBinaryNumber(std::string s) {
+    int count = 0;
+    for (const auto c : s) {
+      if (c == '1') ++count;
+    }
+
+    if (count == 0) {
+      return s;
+    }
+
+    --count;
+    s[s.size() - 1] = '1';
+
+    for (std::size_t i = 0; i < s.size() - 1; ++i) {
+      if (count > 0) {
+        s[i] = '1';
+        --count;
+      } else {
+        s[i] = '0';
+      }
+    }
+
     return s;
   }
 };
