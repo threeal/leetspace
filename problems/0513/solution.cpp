2490e61d912cbc971e92caee79d80d1713f3b57e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-01 15:21:54 +0700

    feat(problem-513/cpp): solve by iterating node per layer

--- problems/0513/solution.cpp
+++ problems/0513/solution.cpp
@@ -1,6 +1,21 @@
+#include <list>
+
 class Solution {
  public:
   int findBottomLeftValue(TreeNode* root) {
-    return root->val;
+    std::list<TreeNode*> nodes{root};
+    std::list<TreeNode*> prevNodes{};
+
+    while (!nodes.empty()) {
+      prevNodes = nodes;
+      nodes.clear();
+
+      for (const auto node : prevNodes) {
+        if (node->left != nullptr) nodes.push_back(node->left);
+        if (node->right != nullptr) nodes.push_back(node->right);
+      }
+    }
+
+    return prevNodes.front()->val;
   }
 };
