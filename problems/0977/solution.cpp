722709e2ac15f3c45af7c816e5304b1c5c1d5f28
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-02 09:46:48 +0700

    feat(problem-977/cpp): solve by using stack

--- problems/0977/solution.cpp
+++ problems/0977/solution.cpp
@@ -1,8 +1,31 @@
+#include <stack>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> sortedSquares(std::vector<int>& nums) {
-    return nums;
+    std::vector<int> outputs;
+    outputs.reserve(nums.size());
+
+    std::stack<int> negs;
+    for (const auto num : nums) {
+      const auto sqr = num * num;
+      if (num < 0) {
+        negs.push(sqr);
+      } else {
+        while (!negs.empty() && negs.top() < sqr) {
+          outputs.push_back(negs.top());
+          negs.pop();
+        }
+        outputs.push_back(sqr);
+      }
+    }
+
+    while (!negs.empty()) {
+      outputs.push_back(negs.top());
+      negs.pop();
+    }
+
+    return outputs;
   }
 };
