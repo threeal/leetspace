4b651fc05fc4c05ded68e9332395c5fca54a731a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-03 12:58:35 +0700

    feat(problem-19/cpp): solve by using sliding windows

--- problems/0019/solution.cpp
+++ problems/0019/solution.cpp
@@ -1,7 +1,22 @@
 class Solution {
  public:
   ListNode* removeNthFromEnd(ListNode* head, int n) {
-    (void)n;
+    auto tail = head;
+    while (n > 0) {
+      tail = tail->next;
+      --n;
+    }
+
+    if (tail == nullptr) {
+      return head->next;
+    }
+
+    auto node = head;
+    while (tail->next != nullptr) {
+      node = node->next;
+      tail = tail->next;
+    }
+    node->next = node->next->next;
     return head;
   }
 };
