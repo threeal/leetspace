6ebe35312be4c90ba8968b9a5a97ff3a1c46c2f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-04 09:53:05 +0700

    feat(problem-948/cpp): solve by using sliding windows

--- problems/0948/solution.cpp
+++ problems/0948/solution.cpp
@@ -1,10 +1,29 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int bagOfTokensScore(std::vector<int>& tokens, int power) {
-    (void)tokens;
-    (void)power;
-    return 0;
+    int score = 0;
+
+    std::sort(tokens.begin(), tokens.end());
+
+    int head = 0;
+    int tail = tokens.size() - 1;
+    while (head <= tail) {
+      const int prev_score = score;
+      while (tokens[head] > power) {
+        if (score == 0 || head == tail) return prev_score;
+        power += tokens[tail];
+        --score;
+        --tail;
+      }
+
+      power -= tokens[head];
+      ++score;
+      ++head;
+    }
+
+    return score;
   }
 };
