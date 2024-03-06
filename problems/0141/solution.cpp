48f7c71af0332faf9c0b9b4c8a68937932b0e968
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-06 10:35:58 +0700

    feat(problem-141/cpp): solve by altering head

--- problems/0141/solution.cpp
+++ problems/0141/solution.cpp
@@ -1,6 +1,13 @@
 class Solution {
  public:
   bool hasCycle(ListNode *head) {
-    return head != nullptr;
+    auto node = head;
+    while (node != nullptr) {
+      if (node->next == head) return true;
+      auto prevNode = node;
+      node = node->next;
+      prevNode->next = head;
+    }
+    return false;
   }
 };
