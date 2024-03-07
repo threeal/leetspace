06f99c4e5b1cd76444f15ada6fa4defb7efc9001
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-07 12:51:29 +0700

    feat(problem-876/cpp): solve by iterating with different speed

--- problems/0876/solution.cpp
+++ problems/0876/solution.cpp
@@ -1,6 +1,15 @@
 class Solution {
  public:
   ListNode* middleNode(ListNode* head) {
-    return head;
+    auto middle = head;
+
+    while (head->next != nullptr) {
+      head = head->next;
+      middle = middle->next;
+      if (head->next == nullptr) break;
+      head = head->next;
+    }
+
+    return middle;
   }
 };
