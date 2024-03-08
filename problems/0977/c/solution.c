4521a6fdf782bb627f6e0ded9043fc7910a3349f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-08 17:25:34 +0700

    feat(problem-977/c): solve by using stack in array

--- problems/0977/c/solution.c
+++ problems/0977/c/solution.c
@@ -1,4 +1,34 @@
+#include <stdlib.h>
+
 int* sortedSquares(int* nums, int numsSize, int* returnSize) {
   *returnSize = numsSize;
-  return nums;
+  int* outputs = malloc(sizeof(int) * (*returnSize));
+  int* outIt = outputs;
+
+  int i = 0;
+  while (i < numsSize && nums[i] < 0) {
+    nums[i] *= nums[i];
+    ++i;
+  }
+
+  int j = i - 1;
+  while (i < numsSize) {
+    nums[i] *= nums[i];
+    while (j >= 0 && nums[j] < nums[i]) {
+      *outIt = nums[j];
+      ++outIt;
+      --j;
+    }
+    *outIt = nums[i];
+    ++outIt;
+    ++i;
+  }
+
+  while (j >= 0) {
+    *outIt = nums[j];
+    ++outIt;
+    --j;
+  }
+
+  return outputs;
 }
