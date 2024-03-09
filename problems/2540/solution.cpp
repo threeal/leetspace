cc22f695b665bf9225abb6074100488af3de8aaf
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-09 12:57:41 +0700

    feat(problem-2540/cpp): solve by iterating both array

--- problems/2540/solution.cpp
+++ problems/2540/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   int getCommon(std::vector<int>& nums1, std::vector<int>& nums2) {
-    return nums1[0] < nums2[0] ? nums1[0] : nums2[0];
+    auto it1 = nums1.begin();
+    auto it2 = nums2.begin();
+    while (it1 != nums1.end() && it2 != nums2.end()) {
+      if (*it1 == *it2) return *it1;
+      *it1 < *it2 ? ++it1 : ++it2;
+    }
+    return -1;
   }
 };
