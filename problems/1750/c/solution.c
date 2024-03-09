3ab1e39d2bc732021d9bf16a9d5a9adb27dea15c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-09 20:39:35 +0700

    feat(problem-1750/c): solve by using two pointers

--- problems/1750/c/solution.c
+++ problems/1750/c/solution.c
@@ -1,4 +1,16 @@
+#include <string.h>
+
 int minimumLength(char* s) {
-  (void)s;
-  return 0;
+  int left = 0;
+  int right = strlen(s) - 1;
+
+  while (left < right) {
+    if (s[left] != s[right]) break;
+
+    const char c = s[left];
+    while (s[left] == c && left <= right) ++left;
+    while (s[right] == c && left <= right) --right;
+  }
+
+  return right - left + 1;
 }
