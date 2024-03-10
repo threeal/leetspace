0c5c15587ceeb2e83ab0f4d67f37af84f6ccbe48
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-10 16:14:56 +0700

    feat(problem-0349/cpp): solve by using two hash sets

--- problems/0349/solution.cpp
+++ problems/0349/solution.cpp
@@ -1,10 +1,26 @@
+#include <unordered_set>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2) {
-    (void)nums1;
-    (void)nums2;
-    return {};
+    std::unordered_set<int> uniques1{};
+    for (const auto num : nums1) {
+      uniques1.insert(num);
+    }
+
+    std::unordered_set<int> uniques2{};
+    for (const auto num : nums2) {
+      uniques2.insert(num);
+    }
+
+    std::vector<int> intersections{};
+    for (const auto unique : uniques1) {
+      if (uniques2.contains(unique)) {
+        intersections.push_back(unique);
+      }
+    }
+
+    return intersections;
   }
 };
