5023862188d97757827852b9299341bd3c951d19
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-11 14:08:53 +0700

    feat(problem-791/cpp): solve by storing order in array

--- problems/0791/solution.cpp
+++ problems/0791/solution.cpp
@@ -1,9 +1,18 @@
 #include <string>
+#include <vector>
 
 class Solution {
  public:
   std::string customSortString(std::string order, std::string s) {
-    (void)order;
+    std::vector<std::uint_fast8_t> orderOf(26, 0);
+    for (std::uint_fast8_t i = 0; i < order.size(); ++i) {
+      orderOf[order[i] - 'a'] = i;
+    }
+
+    std::sort(s.begin(), s.end(), [&](const char a, const char b) {
+      return orderOf[a - 'a'] < orderOf[b - 'a'];
+    });
+
     return s;
   }
 };
