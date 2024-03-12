2895277952fa53482d938f27977f4122cec51826
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-12 21:30:52 +0700

    feat(problem-791/c): solve by storing order in array

--- problems/0791/c/solution.c
+++ problems/0791/c/solution.c
@@ -1,4 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+
+int orderOf[26];
+
+int comp(const void* a, const void* b) {
+  return orderOf[*(char*)a - 'a'] - orderOf[*(char*)b - 'a'];
+}
+
 char* customSortString(char* order, char* s) {
-  (void)order;
+  for (int i = 0; order[i] != 0; ++i) {
+    orderOf[order[i] - 'a'] = i;
+  }
+
+  qsort(s, strlen(s), sizeof(char), comp);
+
   return s;
 }
