65d17ee37318cada27c1b89d838e18678ca16a04
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-14 10:57:35 +0700

    feat(problem-930/cpp): solve by storing count in hash map

--- problems/0930/solution.cpp
+++ problems/0930/solution.cpp
@@ -1,9 +1,21 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int numSubarraysWithSum(std::vector<int>& nums, int goal) {
-    (void)goal;
-    return nums.size();
+    std::unordered_map<int, int> counts;
+    counts[0] = 1;
+
+    int sum = 0;
+    int subarrays = 0;
+
+    for (int num : nums) {
+      sum += num;
+      subarrays += counts[sum - goal];
+      ++counts[sum];
+    }
+
+    return subarrays;
   }
 };
