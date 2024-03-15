99789c03124cdf6a81feb0a6edbd1c32b5a13fcd
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-15 12:06:24 +0700

    feat(problem-238/cpp): solve by accumulating product first

--- problems/0238/solution.cpp
+++ problems/0238/solution.cpp
@@ -3,6 +3,39 @@
 class Solution {
  public:
   std::vector<int> productExceptSelf(std::vector<int>& nums) {
+    int zeros = 0;
+    int product = 1;
+    for (const auto num : nums) {
+      if (num == 0) {
+        ++zeros;
+      } else {
+        product *= num;
+      }
+    }
+
+    switch (zeros) {
+      case 0:
+        for (auto& num : nums) {
+          num = product / num;
+        }
+        break;
+
+      case 1:
+        for (auto& num : nums) {
+          if (num == 0) {
+            num = product;
+          } else {
+            num = 0;
+          }
+        }
+        break;
+
+      default:
+        for (auto& num : nums) {
+          num = 0;
+        }
+    }
+
     return nums;
   }
 };
