c8e0e2c61751cff06415b12e1497ea06c95189b1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-17 13:58:12 +0700

    test: add test for problem 57. Insert Interval

---
+++ problems/0057/solution.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> insert(std::vector<std::vector<int>>& intervals, std::vector<int>& newInterval) {
+    (void)newInterval;
+    return intervals;
+  }
+};
