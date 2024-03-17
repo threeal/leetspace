7e760cfab585d6807596e2bf6c36ff7603f46cea
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-17 20:30:06 +0700

    feat(problem-2485/cpp): solve by using binary search

--- problems/2485/solution.cpp
+++ problems/2485/solution.cpp
@@ -1,6 +1,25 @@
 class Solution {
  public:
   int pivotInteger(int n) {
-    return n;
+    if (n == 1) return 1;
+
+    int left = 1;
+    int right = n;
+
+    while (left + 1 < right) {
+      const int pivot = (left + right) / 2;
+      const int leftSum = pivot * (1 + pivot);
+      const int rightSum = (n + 1 - pivot) * (pivot + n);
+
+      if (leftSum == rightSum) return pivot;
+
+      if (leftSum < rightSum) {
+        left = pivot;
+      } else {
+        right = pivot;
+      }
+    }
+
+    return -1;
   }
 };
