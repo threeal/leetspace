dac26580f731a35c3362790d45ea549528d4ec54
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-23 14:05:26 +0700

    feat(problem-143/cpp): solve by reversing last half of the list first

--- problems/0143/solution.cpp
+++ problems/0143/solution.cpp
@@ -1,6 +1,29 @@
 class Solution {
  public:
   void reorderList(ListNode* head) {
-    (void)head;
+    if (head->next == nullptr) return;
+
+    auto mid = head;
+    auto fast = head;
+    while (fast != nullptr && fast->next != nullptr) {
+      mid = mid->next;
+      fast = fast->next->next;
+    }
+
+    auto prev = mid;
+    auto it = mid->next;
+    mid->next = nullptr;
+    while (it != nullptr) {
+      std::swap(prev, it->next);
+      std::swap(prev, it);
+    }
+
+    it = prev;
+    auto next = head->next;
+    head->next = prev;
+    while (it != next) {
+      std::swap(next, it->next);
+      it = it->next;
+    }
   }
 };
