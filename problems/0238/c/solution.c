721246d08c6de661ab143e0e5caaed3a6a1663f4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-23 21:43:24 +0700

    feat(problem-238/c): solve by multiplies suffixes and prefixes

--- problems/0238/c/solution.c
+++ problems/0238/c/solution.c
@@ -1,4 +1,19 @@
+#include <stdlib.h>
+
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
   *returnSize = numsSize;
-  return nums;
+  int* res = malloc(sizeof(int) * numsSize);
+
+  res[0] = 1;
+  for (int i = 1; i < numsSize; ++i) {
+    res[i] = res[i - 1] * nums[i - 1];
+  }
+
+  int temp = nums[numsSize - 1];
+  for (int i = numsSize - 2; i >= 0; --i) {
+    res[i] *= temp;
+    temp *= nums[i];
+  }
+
+  return res;
 }
