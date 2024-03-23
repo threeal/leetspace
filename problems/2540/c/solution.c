35581b4b85d444ec5786729636fd985ea8b2a944
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-23 14:30:22 +0700

    feat(problem-2540/c): solve by iterating both arrays

--- problems/2540/c/solution.c
+++ problems/2540/c/solution.c
@@ -1,7 +1,14 @@
 int getCommon(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-  (void)nums1;
-  (void)nums1Size;
-  (void)nums2;
-  (void)nums2Size;
-  return 0;
+  int* it1 = nums1;
+  int* it2 = nums2;
+
+  nums1 += nums1Size;
+  nums2 += nums2Size;
+
+  while (it1 < nums1 && it2 < nums2) {
+    if (*it1 == *it2) return *it1;
+    *it1 < *it2 ? ++it1 : ++it2;
+  }
+
+  return -1;
 }
