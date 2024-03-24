33a68b8d5d1bce97de3250b2e3cbc04021db707a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-24 20:28:07 +0700

    test(problem-621/c): solve by counting and sorting tasks

--- problems/0621/c/solution.c
+++ problems/0621/c/solution.c
@@ -1,4 +1,22 @@
+#include <stdlib.h>
+
+int comp(const void* a, const void* b) {
+  return *(int*)b - *(int*)a;
+}
+
 int leastInterval(char* tasks, int tasksSize, int n) {
-  (void)tasks;
-  return tasksSize + n;
+  int counts[26] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (int i = 0; i < tasksSize; ++i) {
+    ++counts[tasks[i] - 'A'];
+  }
+
+  qsort(counts, 26, sizeof(int), comp);
+
+  int maxIntervals = counts[0] - 1;
+  int intervals = maxIntervals * n;
+  for (int i = 1; i < 26; ++i) {
+    intervals -= maxIntervals < counts[i] ? maxIntervals : counts[i];
+  }
+
+  return tasksSize + (intervals > 0 ? intervals : 0);
 }
