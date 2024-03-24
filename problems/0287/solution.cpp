6066102fd80e3092c08a4be16a11b35c20636f6a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-24 20:04:05 +0700

    feat(problem-287/cpp): solve by using slow and fast pointers

--- problems/0287/solution.cpp
+++ problems/0287/solution.cpp
@@ -3,6 +3,20 @@
 class Solution {
  public:
   int findDuplicate(std::vector<int>& nums) {
-    return nums[0];
+    auto slow = nums[nums[0]];
+    auto fast = nums[nums[nums[0]]];
+
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    }
+
+    slow = nums[0];
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[fast];
+    }
+
+    return slow;
   }
 };
