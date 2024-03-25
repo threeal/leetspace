fded7ad5b8aa0b9960c79a3d5cd4992f35e97288
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-25 10:39:04 +0700

    feat(problem-442/cpp): solve by negating nums to track duplicates

--- problems/0442/solution.cpp
+++ problems/0442/solution.cpp
@@ -1,8 +1,20 @@
+#include <cstdlib>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> findDuplicates(std::vector<int>& nums) {
-    return nums;
+    std::vector<int> output;
+
+    // Negates the numbers to track duplicates.
+    for (const auto num : nums) {
+      if (nums[std::abs(num) - 1] > 0) {
+        nums[std::abs(num) - 1] *= -1;
+      } else {
+        output.push_back(std::abs(num));
+      }
+    }
+
+    return output;
   }
 };
