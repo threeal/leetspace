4e0bbe890a960904fefe8e0ecd9c6a851c835f86
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-26 12:39:40 +0700

    feat(problem-41/cpp): solve by marking array as negative for tracking

--- problems/0041/solution.cpp
+++ problems/0041/solution.cpp
@@ -1,9 +1,25 @@
+#include <cstdlib>
 #include <vector>
 
 class Solution {
  public:
   int firstMissingPositive(std::vector<int>& nums) {
-    (void)nums;
-    return 0;
+    const int n = nums.size();
+    for (auto& num : nums) {
+      if (num <= 0) num = n + 1;
+    }
+
+    for (const auto num : nums) {
+      const auto i = std::abs(num);
+      if (i <= n && nums[i - 1] > 0) {
+        nums[i - 1] *= -1;
+      }
+    }
+
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] > 0) return i + 1;
+    }
+
+    return n + 1;
   }
 };
