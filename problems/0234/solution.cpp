b9f4b8d6b02bc322b067797e250e0606658e2536
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-27 22:32:21 +0700

    feat(problem-234/cpp): solve by reversing first half

--- problems/0234/solution.cpp
+++ problems/0234/solution.cpp
@@ -1,6 +1,28 @@
 class Solution {
  public:
   bool isPalindrome(ListNode* head) {
-    return head == nullptr;
+    auto mid = head;
+    auto fast = head;
+    while (fast != nullptr && fast->next != nullptr) {
+      mid = mid->next;
+      fast = fast->next->next;
+    }
+
+    auto prev = mid;
+    auto it = mid->next;
+    mid->next = nullptr;
+    while (it != nullptr) {
+      std::swap(prev, it->next);
+      std::swap(prev, it);
+    }
+
+    auto tail = prev;
+    while (head != nullptr && tail != nullptr) {
+      if (head->val != tail->val) return false;
+      head = head->next;
+      tail = tail->next;
+    }
+
+    return true;
   }
 };
