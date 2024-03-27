5faf15b324e9099998e75ff01589dd09e0427b02
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-27 13:42:44 +0700

    feat(problem-713/cpp): solve by using sliding window

--- problems/0713/solution.cpp
+++ problems/0713/solution.cpp
@@ -3,7 +3,24 @@
 class Solution {
  public:
   int numSubarrayProductLessThanK(std::vector<int>& nums, int k) {
-    (void)k;
-    return nums.size();
+    if (k < 1) return 0;
+
+    int count = 0;
+
+    std::size_t left = 0;
+    std::size_t right = 0;
+    int product = 1;
+
+    while (right < nums.size()) {
+      product *= nums[right];
+      while (product >= k) {
+        product /= nums[left];
+        ++left;
+      }
+      count += right + 1 - left;
+      ++right;
+    }
+
+    return count;
   }
 };
