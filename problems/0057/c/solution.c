30286536db0170acb637835d68124828bba33e73
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-27 14:43:59 +0700

    feat(problem-57/c): solve by pushing intervals

--- problems/0057/c/solution.c
+++ problems/0057/c/solution.c
@@ -1,17 +1,35 @@
 #include <stdlib.h>
 
 int** insert(int** intervals, int intervalsSize, int* intervalsColSize, int* newInterval, int newIntervalSize, int* returnSize, int** returnColumnSizes) {
-  int** newIntervals = malloc(sizeof(int*) * intervalsSize + 1);
-
-  (void)newInterval;
+  (void)intervalsColSize;
   (void)newIntervalSize;
 
-  for (int i = 0; i < intervalsSize; ++i) {
-    newIntervals[i] = intervals[i];
+  int** newIntervals = malloc(sizeof(int*) * (intervalsSize + 1));
+
+  *returnSize = 0;
+
+  int i = 0;
+  while (i < intervalsSize && intervals[i][1] < newInterval[0]) {
+    newIntervals[(*returnSize)++] = intervals[i];
+    ++i;
   }
 
-  *returnSize = intervalsSize;
-  *returnColumnSizes = intervalsColSize;
+  while (i < intervalsSize && intervals[i][0] <= newInterval[1]) {
+    if (intervals[i][0] < newInterval[0]) newInterval[0] = intervals[i][0];
+    if (intervals[i][1] > newInterval[1]) newInterval[1] = intervals[i][1];
+    ++i;
+  }
+  newIntervals[(*returnSize)++] = newInterval;
 
-  return intervals;
+  while (i < intervalsSize) {
+    newIntervals[(*returnSize)++] = intervals[i];
+    ++i;
+  }
+
+  *returnColumnSizes = malloc(sizeof(int*) * (*returnSize));
+  for (int i = 0; i <= *returnSize; ++i) {
+    (*returnColumnSizes)[i] = 2;
+  }
+
+  return newIntervals;
 }
