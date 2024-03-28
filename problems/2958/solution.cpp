965b7948f13388af8a842f012a5e31048168b722
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-28 12:01:20 +0700

    feat(problem-2958/cpp): solve by using sliding window

--- problems/2958/solution.cpp
+++ problems/2958/solution.cpp
@@ -1,9 +1,25 @@
+#include <algorithm>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int maxSubarrayLength(std::vector<int>& nums, int k) {
-    (void)k;
-    return nums.size();
+    std::unordered_map<int, int> frequencies;
+    int maxLength = 0;
+
+    int left = 0;
+    int right = 0;
+    while (right < static_cast<int>(nums.size())) {
+      ++frequencies[nums[right]];
+      while (frequencies[nums[right]] > k) {
+        --frequencies[nums[left++]];
+      }
+
+      maxLength = std::max(maxLength, right - left + 1);
+      ++right;
+    }
+
+    return maxLength;
   }
 };
