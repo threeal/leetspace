f70c86b155525a46d105cef2a7b9e622c76a4482
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-30 13:06:42 +0700

    feat(problem-992/cpp): solve by using sliding window

--- problems/0992/solution.cpp
+++ problems/0992/solution.cpp
@@ -1,8 +1,28 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int subarraysWithKDistinct(std::vector<int>& nums, int k) {
-    return nums.size() * k;
+    return subarraysWithAtMostKDistinct(nums, k) - subarraysWithAtMostKDistinct(nums, k - 1);
+  }
+
+  int subarraysWithAtMostKDistinct(const std::vector<int>& nums, std::size_t k) {
+    int subarraysCount = 0;
+    std::unordered_map<int, int> numsCounts;
+
+    std::size_t left = 0;
+    std::size_t right = 0;
+    while (right < nums.size()) {
+      ++numsCounts[nums[right]];
+      while (numsCounts.size() > k && left < right) {
+        if (--numsCounts[nums[left]] == 0) numsCounts.erase(nums[left]);
+        ++left;
+      }
+      subarraysCount += right - left + 1;
+      ++right;
+    }
+
+    return subarraysCount;
   }
 };
