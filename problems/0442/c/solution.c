da599da532ae1d76728de475f32c5fda5339d25b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-30 20:22:11 +0700

    test(problem-442/c): solve by negating nums to track duplicates

--- problems/0442/c/solution.c
+++ problems/0442/c/solution.c
@@ -1,5 +1,17 @@
+#include <stdlib.h>
+
 int* findDuplicates(int* nums, int numsSize, int* returnSize) {
-  (void)nums;
-  *returnSize = numsSize;
-  return nums;
+  int* output = malloc(sizeof(int) * numsSize);
+  *returnSize = 0;
+
+  // Negates the numbers to track duplicates.
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[abs(nums[i]) - 1] > 0) {
+      nums[abs(nums[i]) - 1] *= -1;
+    } else {
+      output[(*returnSize)++] = abs(nums[i]);
+    }
+  }
+
+  return output;
 }
