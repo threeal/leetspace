b2f51944f726d6c5dafcba6fa73e764888e1a672
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-31 18:49:33 +0700

    feat(problem-2444/c): solve by using a sliding window

--- problems/2444/c/solution.c
+++ problems/2444/c/solution.c
@@ -1,6 +1,23 @@
 long long countSubarrays(int* nums, int numsSize, int minK, int maxK) {
-  (void)nums;
-  (void)minK;
-  (void)maxK;
-  return numsSize;
+  long long count = 0;
+
+  int left = 0;
+  int right = 0;
+
+  int minPos = -1;
+  int maxPos = -1;
+
+  while (right < numsSize) {
+    if (nums[right] < minK || nums[right] > maxK) {
+      left = right + 1;
+    } else {
+      if (nums[right] == minK) minPos = right;
+      if (nums[right] == maxK) maxPos = right;
+      const int pos = minPos < maxPos ? minPos : maxPos;
+      if (pos >= left) count += pos - left + 1;
+    }
+    ++right;
+  }
+
+  return count;
 }
