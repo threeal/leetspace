5af355c2590b1684d5721fee051f007f1a6c41fb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-31 12:13:51 +0700

    test: add test for problem 2444. Count Subarrays With Fixed Bounds

---
+++ problems/2444/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  long long countSubarrays(std::vector<int>& nums, int minK, int maxK) {
+    (void)minK;
+    (void)maxK;
+    return nums.size();
+  }
+};
