9203179810c9354e323fa4f9e7108fef1b218938
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-31 13:08:56 +0700

    feat(problem-525/cpp): solve by using map to store previous diff

--- problems/0525/solution.cpp
+++ problems/0525/solution.cpp
@@ -1,8 +1,26 @@
+#include <algorithm>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int findMaxLength(std::vector<int>& nums) {
-    return nums.size();
+    int maxLength{0};
+
+    int diff{0};
+    std::unordered_map<int, int> posOfDiff{{0, -1}};
+
+    for (int i{0}; i < static_cast<int>(nums.size()); ++i) {
+      nums[i] == 0 ? --diff : ++diff;
+
+      const auto it{posOfDiff.find(diff)};
+      if (it != posOfDiff.end()) {
+        maxLength = std::max(maxLength, i - it->second);
+      } else {
+        posOfDiff.emplace(diff, i);
+      }
+    }
+
+    return maxLength;
   }
 };
