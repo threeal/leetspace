743ba6d15e5796674522cb0e2140af01db5d0080
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-02 10:19:45 +0700

    feat(problem-205/cpp): solve by mapping characters between string

--- old-problems/0205/solution.cpp
+++ old-problems/0205/solution.cpp
@@ -1,8 +1,26 @@
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 
 class Solution {
  public:
   bool isIsomorphic(std::string s, std::string t) {
-    return s == t;
+    std::unordered_map<char, char> map{};
+    std::unordered_set<char> exist{};
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      const auto it = map.find(s[i]);
+      if (it == map.end()) {
+        if (exist.contains(t[i])) {
+          return false;
+        } else {
+          map.emplace(s[i], t[i]);
+          exist.insert(t[i]);
+        }
+      } else if (t[i] != it->second) {
+        return false;
+      }
+    }
+
+    return true;
   }
 };
