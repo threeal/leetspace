60146d5a7b2b40c53502dfdbc3bf709d87314d6e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-03 11:05:50 +0700

    test: add test for problem 79. Word Search

---
+++ old-problems/0079/solution.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  bool exist(std::vector<std::vector<char>>& board, std::string word) {
+    (void)board;
+    (void)word;
+    return false;
+  }
+};
