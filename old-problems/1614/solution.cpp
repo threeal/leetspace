255bf39a1d5c8f7ef61535782a0303c233fddcdb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-04 12:00:50 +0700

    feat(problem-1614/cpp): solve by iterating characters

--- old-problems/1614/solution.cpp
+++ old-problems/1614/solution.cpp
@@ -3,6 +3,24 @@
 class Solution {
  public:
   int maxDepth(std::string s) {
-    return s.size();
+    int maxCount{0};
+    int count{0};
+
+    char* c{s.data()};
+    while (*c != 0) {
+      switch (*c) {
+        case '(':
+          ++count;
+          if (count > maxCount) maxCount = count;
+          break;
+
+        case ')':
+          --count;
+          break;
+      }
+      ++c;
+    }
+
+    return maxCount;
   }
 };
