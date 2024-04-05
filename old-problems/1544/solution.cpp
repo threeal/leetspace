ec1755b835ab2f2f6b38104090edd21c06e433ed
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-05 14:27:58 +0700

    feat(problem-1544/cpp): solve by using two pointers

--- old-problems/1544/solution.cpp
+++ old-problems/1544/solution.cpp
@@ -3,6 +3,22 @@
 class Solution {
  public:
   std::string makeGood(std::string s) {
+    std::size_t l{0};
+    for (std::size_t r{1}; r < s.size(); ++r) {
+      if (s[l] < s[r] ? s[l] + 32 == s[r] : s[l] == s[r] + 32) {
+        if (l > 0) {
+          --l;
+        } else if (++r < s.size()) {
+          s[l] = s[r];
+        } else {
+          return "";
+        }
+      } else {
+        s[++l] = s[r];
+      }
+    }
+
+    s.resize(l + 1);
     return s;
   }
 };
