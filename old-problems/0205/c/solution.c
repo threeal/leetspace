ced839a4deeb2dbc5dbf1af37338c6230f910e3d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-06 20:52:57 +0700

    feat(problem-205/c): solve by mapping character between strings

--- old-problems/0205/c/solution.c
+++ old-problems/0205/c/solution.c
@@ -1,5 +1,18 @@
 #include <stdbool.h>
+#include <string.h>
+
+int sMap[128];
+int tMap[128];
 
 bool isIsomorphic(char* s, char* t) {
-  return *s == *t;
+  memset(sMap, 0, 128 * sizeof(int));
+  memset(tMap, 0, 128 * sizeof(int));
+
+  for (int i = 0; s[i] != 0; ++i) {
+    if (*(sMap + s[i]) != *(tMap + t[i])) return false;
+    *(sMap + s[i]) = i + 1;
+    *(tMap + t[i]) = i + 1;
+  }
+
+  return true;
 }
