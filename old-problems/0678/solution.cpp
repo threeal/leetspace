b7a299d2671c79cee140cbd12a4efda14ecf246c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-07 13:08:23 +0700

    feat(problem-678/cpp): try to solve by iterating recursively

--- old-problems/0678/solution.cpp
+++ old-problems/0678/solution.cpp
@@ -1,8 +1,29 @@
 #include <string>
+#include <string_view>
 
 class Solution {
  public:
   bool checkValidString(std::string s) {
-    return s.empty();
+    return iterateIfStringValid(s, 0);
+  }
+
+ public:
+  bool iterateIfStringValid(std::string_view v, int level) {
+    if (v.empty()) return level == 0;
+
+    switch (v[0]) {
+      case '(':
+        return iterateIfStringValid(v.substr(1), level + 1);
+
+      case ')':
+        if (level < 1) return false;
+        return iterateIfStringValid(v.substr(1), level - 1);
+
+      case '*':
+        const bool res = iterateIfStringValid(v.substr(1), level) || iterateIfStringValid(v.substr(1), level + 1);
+        return (!res && level > 0) ? iterateIfStringValid(v.substr(1), level - 1) : res;
+    }
+
+    return false;
   }
 };
