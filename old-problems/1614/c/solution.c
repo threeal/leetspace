65bd877cc55d75737f34c2590ea92189791e72ad
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-07 21:12:28 +0700

    feat(problem-1614/c): solve by iterating characters

--- old-problems/1614/c/solution.c
+++ old-problems/1614/c/solution.c
@@ -1,4 +1,20 @@
 int maxDepth(char* s) {
-  (void)s;
-  return 0;
+  int maxCount = 0;
+  int count = 0;
+
+  while (*s != 0) {
+    switch (*s) {
+      case '(':
+        ++count;
+        if (count > maxCount) maxCount = count;
+        break;
+
+      case ')':
+        --count;
+        break;
+    }
+    ++s;
+  }
+
+  return maxCount;
 }
