fd61cb943ecd44a94ca61c85948f4d86ade65668
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-08 15:01:55 +0700

    feat(problem-1700/cpp): solve by iterating and counting preferences

--- old-problems/1700/solution.cpp
+++ old-problems/1700/solution.cpp
@@ -3,6 +3,20 @@
 class Solution {
  public:
   int countStudents(std::vector<int>& students, std::vector<int>& sandwiches) {
-    return students.size() - sandwiches.size();
+    int preferCircular{0};
+    int preferSquare{0};
+    for (const auto student : students) {
+      student == 0 ? ++preferCircular : ++preferSquare;
+    }
+
+    for (const auto sandwich : sandwiches) {
+      if (sandwich == 0) {
+        if (--preferCircular < 0) return preferSquare;
+      } else {
+        if (--preferSquare < 0) return preferCircular;
+      }
+    }
+
+    return 0;
   }
 };
