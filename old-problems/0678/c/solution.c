71a56ec5493ca696642d668bec6a5fe1f94a370c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-09 13:55:20 +0700

    feat(problem-678/c): solve by iterating and tracking min max level

--- old-problems/0678/c/solution.c
+++ old-problems/0678/c/solution.c
@@ -1,5 +1,31 @@
 #include <stdbool.h>
 
 bool checkValidString(char* s) {
-  return s != 0;
+  int minLevel = 0;
+  int maxLevel = 0;
+
+  while (*s != 0) {
+    switch (*s) {
+      case '(':
+        ++minLevel;
+        ++maxLevel;
+        break;
+
+      case ')':
+        --minLevel;
+        --maxLevel;
+        break;
+
+      case '*':
+        --minLevel;
+        ++maxLevel;
+        break;
+    }
+
+    if (maxLevel < 0) return false;
+    if (minLevel < 0) minLevel = 0;
+    ++s;
+  }
+
+  return minLevel == 0;
 }
