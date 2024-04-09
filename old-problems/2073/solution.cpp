8f344c7075e7c7d6433e54cf8bb89b9ad7251dd8
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-09 13:32:44 +0700

    feat(problem-2073/cpp): solve by summing all elements

--- old-problems/2073/solution.cpp
+++ old-problems/2073/solution.cpp
@@ -1,8 +1,19 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int timeRequiredToBuy(std::vector<int>& tickets, int k) {
-    return tickets[k];
+    int sum{tickets[k]};
+
+    for (int i{0}; i < k; ++i) {
+      sum += std::min(tickets[i], tickets[k]);
+    }
+
+    for (int i{k + 1}; i < static_cast<int>(tickets.size()); ++i) {
+      sum += std::min(tickets[i], tickets[k] - 1);
+    }
+
+    return sum;
   }
 };
