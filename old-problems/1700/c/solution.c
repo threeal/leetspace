708e921a538ec94c1068825c126a43b5e526b807
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-12 20:44:38 +0700

    feat(problem-1700/c): solve by iterating and counting preferences

--- old-problems/1700/c/solution.c
+++ old-problems/1700/c/solution.c
@@ -1,7 +1,17 @@
 int countStudents(int* students, int studentsSize, int* sandwiches, int sandwichesSize) {
-  (void)students;
-  (void)studentsSize;
-  (void)sandwiches;
-  (void)sandwichesSize;
+  int preferCircular = 0;
+  int preferSquare = 0;
+  for (int i = 0; i < studentsSize; ++i) {
+    students[i] == 0 ? ++preferCircular : ++preferSquare;
+  }
+
+  for (int i = 0; i < sandwichesSize; ++i) {
+    if (sandwiches[i] == 0) {
+      if (--preferCircular < 0) return preferSquare;
+    } else {
+      if (--preferSquare < 0) return preferCircular;
+    }
+  }
+
   return 0;
 }
