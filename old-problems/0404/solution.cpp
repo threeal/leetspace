aa0182e30c46a4107abae28d4f0650f2ba51f13c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-14 16:11:17 +0700

    feat(problem-404/cpp): solve by using recursive function

--- old-problems/0404/solution.cpp
+++ old-problems/0404/solution.cpp
@@ -1,6 +1,20 @@
 class Solution {
  public:
   int sumOfLeftLeaves(TreeNode* root) {
-    return root->val;
+    int sum{0};
+
+    if (root->left != nullptr) {
+      if (root->left->left == nullptr && root->left->right == nullptr) {
+        sum += root->left->val;
+      } else {
+        sum += sumOfLeftLeaves(root->left);
+      }
+    }
+
+    if (root->right != nullptr) {
+      sum += sumOfLeftLeaves(root->right);
+    }
+
+    return sum;
   }
 };
