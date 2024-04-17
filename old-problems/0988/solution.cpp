a4e29ca772e3cdcc3f5b8165f0388ea6542e34dc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-17 13:32:35 +0700

    feat(problem-988/cpp): solve by using recursive function

--- old-problems/0988/solution.cpp
+++ old-problems/0988/solution.cpp
@@ -1,58 +1,25 @@
-#include <queue>
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   std::string smallestFromLeaf(TreeNode* root) {
-    std::queue<std::string> strs{};
-    std::queue<TreeNode*> nodes{};
+    return smallestFromLeaf(root, "");
+  }
 
-    strs.push({static_cast<char>('a' + root->val)});
-    nodes.push(root);
-
-    while (true) {
-      std::size_t n{nodes.size()};
-      for (std::size_t i{0}; i < n; ++i) {
-        if (nodes.front()->left != nullptr) {
-          if (nodes.front()->right != nullptr) {
-            strs.push(static_cast<char>('a' + nodes.front()->left->val) + strs.front());
-            nodes.push(nodes.front()->left);
-
-            strs.push(static_cast<char>('a' + nodes.front()->right->val) + strs.front());
-            nodes.push(nodes.front()->right);
-          } else {
-            strs.push(static_cast<char>('a' + nodes.front()->left->val) + strs.front());
-            nodes.push(nodes.front()->left);
-          }
-        } else if (nodes.front()->right != nullptr) {
-          strs.push(static_cast<char>('a' + nodes.front()->right->val) + strs.front());
-          nodes.push(nodes.front()->right);
-        } else {
-          std::queue<std::string> candidates{};
-          while (i < n) {
-            if (nodes.front()->left == nullptr && nodes.front()->right == nullptr) {
-              candidates.push(strs.front());
-            }
-            strs.pop();
-            nodes.pop();
-            ++i;
-          }
-
-          std::string str{candidates.front()};
-          candidates.pop();
-
-          while (!candidates.empty()) {
-            if (candidates.front() < str) {
-              str = candidates.front();
-            }
-            candidates.pop();
-          }
-
-          return str;
-        }
-        strs.pop();
-        nodes.pop();
+  std::string smallestFromLeaf(TreeNode* node, const std::string& str) {
+    if (node->left != nullptr) {
+      if (node->right != nullptr) {
+        return std::min(
+            smallestFromLeaf(node->left, static_cast<char>('a' + node->val) + str),
+            smallestFromLeaf(node->right, static_cast<char>('a' + node->val) + str));
+      } else {
+        return smallestFromLeaf(node->left, static_cast<char>('a' + node->val) + str);
       }
+    } else if (node->right != nullptr) {
+      return smallestFromLeaf(node->right, static_cast<char>('a' + node->val) + str);
+    } else {
+      return static_cast<char>('a' + node->val) + str;
     }
   }
 };
