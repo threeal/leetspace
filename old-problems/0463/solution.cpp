6dc132d3bfe8a8e141bffe92891adfbc1beed0c5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-18 11:10:59 +0700

    perf(problem-463/cpp): calculate perimeter globally

--- old-problems/0463/solution.cpp
+++ old-problems/0463/solution.cpp
@@ -3,48 +3,35 @@
 class Solution {
  public:
   int islandPerimeter(std::vector<std::vector<int>>& grid) {
-    const auto rows = grid.size();
-    const auto cols = grid[0].size();
-
     int perimeter{0};
 
-    if (rows == 1) {
-      for (std::size_t col{0}; col < cols; ++col) {
-        if (grid[0][col] == 1) perimeter += 2;
-      }
-    } else {
-      for (std::size_t col{0}; col < cols; ++col) {
-        if (grid[0][col] == 1) ++perimeter;
-      }
+    if (grid[0][0] == 1) {
+      perimeter += 4;
+    }
 
-      for (std::size_t row{1}; row < rows; ++row) {
-        for (std::size_t col{0}; col < cols; ++col) {
-          if (grid[row - 1][col] != grid[row][col]) ++perimeter;
-        }
-      }
-
-      for (std::size_t col{0}; col < cols; ++col) {
-        if (grid[rows - 1][col] == 1) ++perimeter;
+    for (std::size_t j{1}; j < grid[0].size(); ++j) {
+      if (grid[0][j] == 1) {
+        perimeter += grid[0][j - 1] == 0 ? 4 : 2;
       }
     }
 
-    if (cols == 1) {
-      for (std::size_t row{0}; row < rows; ++row) {
-        if (grid[row][0] == 1) perimeter += 2;
-      }
-    } else {
-      for (std::size_t row{0}; row < rows; ++row) {
-        if (grid[row][0] == 1) ++perimeter;
+    for (std::size_t i{1}; i < grid.size(); ++i) {
+      if (grid[i][0] == 1) {
+        perimeter += grid[i - 1][0] == 0 ? 4 : 2;
       }
 
-      for (std::size_t col{1}; col < cols; ++col) {
-        for (std::size_t row{0}; row < rows; ++row) {
-          if (grid[row][col - 1] != grid[row][col]) ++perimeter;
+      for (std::size_t j{1}; j < grid[i].size(); ++j) {
+        if (grid[i][j] == 1) {
+          if (grid[i - 1][j] == 0) {
+            if (grid[i][j - 1] == 0) {
+              perimeter += 4;
+            } else {
+              perimeter += 2;
+            }
+          } else if (grid[i][j - 1] == 0) {
+            perimeter += 2;
+          }
         }
-      }
-
-      for (std::size_t row{0}; row < rows; ++row) {
-        if (grid[row][cols - 1] == 1) ++perimeter;
       }
     }
 
