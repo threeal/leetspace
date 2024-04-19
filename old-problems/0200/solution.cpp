23f02dbc13b94edd0a158a4485d261c23fee4f40
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-19 10:36:30 +0700

    feat(problem-200/cpp): try to solve by submerging islands

--- old-problems/0200/solution.cpp
+++ old-problems/0200/solution.cpp
@@ -3,6 +3,24 @@
 class Solution {
  public:
   int numIslands(std::vector<std::vector<char>>& grid) {
-    return grid.size();
+    int islands{0};
+
+    for (std::size_t y{0}; y < grid.size(); ++y) {
+      for (std::size_t x{0}; x < grid[y].size(); ++x) {
+        if (grid[y][x] == '1') {
+          ++islands;
+          submergeIsland(grid, y, x);
+        }
+      }
+    }
+
+    return islands;
+  }
+
+  void submergeIsland(std::vector<std::vector<char>>& grid, std::size_t y, std::size_t x) {
+    grid[y][x] = '0';
+    if (x > 0 && grid[y][x - 1] == '1') submergeIsland(grid, y, x - 1);
+    if (x < grid[y].size() - 1 && grid[y][x + 1] == '1') submergeIsland(grid, y, x + 1);
+    if (y < grid.size() - 1 && grid[y + 1][x] == '1') submergeIsland(grid, y + 1, x);
   }
 };
