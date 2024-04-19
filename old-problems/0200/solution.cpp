39bfe1ca5a105b74f0090acf13442e001c1eeddc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-19 10:42:06 +0700

    feat(problem-200/cpp): solve by handling upper neighbor search

--- old-problems/0200/solution.cpp
+++ old-problems/0200/solution.cpp
@@ -21,6 +21,7 @@ class Solution {
     grid[y][x] = '0';
     if (x > 0 && grid[y][x - 1] == '1') submergeIsland(grid, y, x - 1);
     if (x < grid[y].size() - 1 && grid[y][x + 1] == '1') submergeIsland(grid, y, x + 1);
+    if (y > 0 && grid[y - 1][x] == '1') submergeIsland(grid, y - 1, x);
     if (y < grid.size() - 1 && grid[y + 1][x] == '1') submergeIsland(grid, y + 1, x);
   }
 };
