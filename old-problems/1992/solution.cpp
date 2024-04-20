2437aa0443db80d6016f6e5015037ba68f74d467
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-20 14:33:02 +0700

    feat(problem-1992/cpp): try to solve by searching for limit and clearing the farmland

--- old-problems/1992/solution.cpp
+++ old-problems/1992/solution.cpp
@@ -3,7 +3,36 @@
 class Solution {
  public:
   std::vector<std::vector<int>> findFarmland(std::vector<std::vector<int>>& land) {
-    (void)land;
-    return {};
+    std::vector<std::vector<int>> coordinates{};
+    for (int y1{0}; y1 < static_cast<int>(land.size()); ++y1) {
+      for (int x1{0}; x1 < static_cast<int>(land[y1].size()); ++x1) {
+        if (land[y1][x1] != 1) continue;
+
+        // Search for the bottom limit.
+        int y2{y1 + 1};
+        while (y2 < static_cast<int>(land.size())) {
+          if (land[y2][x1] != 1) break;
+          ++y2;
+        }
+
+        // Search for the right limit.
+        int x2{x1 + 1};
+        while (x2 < static_cast<int>(land[y1].size())) {
+          if (land[y1][x2] != 1) break;
+          ++x2;
+        }
+
+        coordinates.push_back({x1, y1, x2 - 1, y2 - 1});
+
+        // Clear the farmland, preventing it from being counted twice.
+        for (int y{y1}; y < y2; ++y) {
+          for (int x{x1}; x < x2; ++x) {
+            land[y][x] = 0;
+          }
+        }
+      }
+    }
+
+    return coordinates;
   }
 };
