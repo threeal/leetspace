954daf469366507cf4cc4360ed6a9814a61ad9a8
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-26 20:24:29 +0700

    feat(problem-1971/c): solve by iterating non-visited edges

--- old-problems/1971/c/solution.c
+++ old-problems/1971/c/solution.c
@@ -1,11 +1,31 @@
 #include <stdbool.h>
+#include <stdlib.h>
 
 bool validPath(int n, int** edges, int edgesSize, int* edgesColSize, int source, int destination) {
-  (void)n;
-  (void)edges;
-  (void)edgesSize;
   (void)edgesColSize;
-  (void)source;
-  (void)destination;
+
+  if (source == destination) return true;
+
+  bool* visited = malloc(sizeof(bool) * n);
+  for (int i = 0; i < n; ++i) {
+    visited[i] = false;
+  }
+  visited[source] = true;
+
+  int prevEdgesSize = edgesSize + 1;
+  while (edgesSize != prevEdgesSize) {
+    prevEdgesSize = edgesSize;
+    for (int i = 0; i < edgesSize; ++i) {
+      if (visited[edges[i][0]] != visited[edges[i][1]]) {
+        visited[edges[i][0]] = true;
+        visited[edges[i][1]] = true;
+
+        edges[i] = edges[--edgesSize];
+        --i;
+      }
+    }
+    if (visited[destination]) return true;
+  }
+
   return false;
 }
