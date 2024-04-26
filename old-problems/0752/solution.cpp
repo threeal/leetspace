033fdde5794c50457637373ae857593cd8c08fb6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-26 19:32:06 +0700

    test: add test for problem 752. Open the Lock

---
+++ old-problems/0752/solution.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  int openLock(std::vector<std::string>& deadends, std::string target) {
+    (void)deadends;
+    (void)target;
+    return 0;
+  }
+};
