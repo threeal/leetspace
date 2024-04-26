cc33881346d89bbbae47483a9b4cd8869fdfcd2d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-26 19:29:24 +0700

    perf(problem-1971/cpp): replace `std::unordered_map` and `std::unordered_set` with `std::vector`

--- old-problems/1971/solution.cpp
+++ old-problems/1971/solution.cpp
@@ -1,22 +1,20 @@
 #include <list>
 #include <queue>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 class Solution {
  public:
-  bool validPath(int /*n*/, std::vector<std::vector<int>>& edges, int source, int destination) {
+  bool validPath(int n, std::vector<std::vector<int>>& edges, int source, int destination) {
     if (source == destination) return true;
 
-    std::unordered_map<int, std::list<int>> connections{};
+    std::vector<std::list<int>> connections(n);
     for (const auto& edge : edges) {
       connections[edge[0]].push_back(edge[1]);
       connections[edge[1]].push_back(edge[0]);
     }
 
-    std::unordered_set<int> visited{};
-    visited.insert(source);
+    std::vector<bool> visited(n, false);
+    visited[source] = true;
 
     std::queue<int> queue;
     queue.push(source);
@@ -24,8 +22,8 @@ class Solution {
     while (!queue.empty()) {
       for (const auto next : connections[queue.front()]) {
         if (next == destination) return true;
-        if (!visited.contains(next)) {
-          visited.insert(next);
+        if (!visited[next]) {
+          visited[next] = true;
           queue.push(next);
         }
       }
