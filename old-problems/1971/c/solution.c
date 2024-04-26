d5aed2fb3d4466709db6c60eee96610cb0ec886b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-26 20:56:50 +0700

    perf(problem-1971/c): store visited in bytes

--- old-problems/1971/c/solution.c
+++ old-problems/1971/c/solution.c
@@ -1,30 +1,30 @@
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 bool validPath(int n, int** edges, int edgesSize, int* edgesColSize, int source, int destination) {
   (void)edgesColSize;
 
   if (source == destination) return true;
 
-  bool* visited = malloc(sizeof(bool) * n);
-  for (int i = 0; i < n; ++i) {
-    visited[i] = false;
-  }
-  visited[source] = true;
+  n = n / 8 + (n % 8 == 0 ? 0 : 1);
+  uint8_t* visited = malloc(n);
+  memset(visited, 0, n);
+  visited[source / 8] |= 1 << source % 8;
 
   int prevEdgesSize = edgesSize + 1;
   while (edgesSize != prevEdgesSize) {
     prevEdgesSize = edgesSize;
     for (int i = 0; i < edgesSize; ++i) {
-      if (visited[edges[i][0]] != visited[edges[i][1]]) {
-        visited[edges[i][0]] = true;
-        visited[edges[i][1]] = true;
-
+      if ((visited[edges[i][0] / 8] >> edges[i][0] % 8 & 1) != (visited[edges[i][1] / 8] >> edges[i][1] % 8 & 1)) {
+        visited[edges[i][0] / 8] |= 1 << edges[i][0] % 8;
+        visited[edges[i][1] / 8] |= 1 << edges[i][1] % 8;
         edges[i] = edges[--edgesSize];
         --i;
       }
     }
-    if (visited[destination]) return true;
+    if ((visited[destination / 8] >> destination % 8 & 1) == 1) return true;
   }
 
   return false;
