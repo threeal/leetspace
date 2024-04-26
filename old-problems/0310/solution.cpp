49644828bcfefb2a18d1195f701ab40cc00f4aa1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-26 19:33:43 +0700

    test: add test for problem 310. Minimum Height Trees

---
+++ old-problems/0310/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> findMinHeightTrees(int n, std::vector<std::vector<int>>& edges) {
+    (void)n;
+    (void)edges;
+    return {};
+  }
+};
