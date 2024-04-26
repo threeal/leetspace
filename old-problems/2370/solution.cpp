886ee30055a514b1fb1a2f6ec88fe1c2679d91d6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-26 11:58:26 +0700

    feat(problem-2370/cpp): solve by storing counting possibilities in array

--- old-problems/2370/solution.cpp
+++ old-problems/2370/solution.cpp
@@ -1,9 +1,22 @@
+#include <algorithm>
+#include <cstdlib>
 #include <string>
+#include <vector>
 
 class Solution {
  public:
   int longestIdealString(std::string s, int k) {
-    (void)k;
-    return s.size();
+    std::vector<int> counts(26, 0);
+    for (const auto c : s) {
+      const int i{c - 'a'};
+      const int count{counts[i] + 1};
+
+      const int n{std::min(25, i + k)};
+      for (int j{std::max(0, i - k)}; j <= n; ++j) {
+        if (count > counts[j]) counts[j] = count;
+      }
+    }
+
+    return *std::max_element(counts.begin(), counts.end());
   }
 };
