28d3c8339daab490475f108fcb2e2f8b8dae86db
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-26 20:20:21 +0700

    test(problem-1971): add support to test in C language

---
+++ old-problems/1971/c/solution.c
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+
+bool validPath(int n, int** edges, int edgesSize, int* edgesColSize, int source, int destination) {
+  (void)n;
+  (void)edges;
+  (void)edgesSize;
+  (void)edgesColSize;
+  (void)source;
+  (void)destination;
+  return false;
+}
