1d6f12723dc7b53bbcfcb4f2b4d3bda1a8aefea6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-26 19:49:09 +0700

    perf(problem-1971/cpp): check valid path by iterating edges

--- old-problems/1971/solution.cpp
+++ old-problems/1971/solution.cpp
@@ -1,5 +1,3 @@
-#include <list>
-#include <queue>
 #include <vector>
 
 class Solution {
@@ -7,27 +5,20 @@ class Solution {
   bool validPath(int n, std::vector<std::vector<int>>& edges, int source, int destination) {
     if (source == destination) return true;
 
-    std::vector<std::list<int>> connections(n);
-    for (const auto& edge : edges) {
-      connections[edge[0]].push_back(edge[1]);
-      connections[edge[1]].push_back(edge[0]);
-    }
-
     std::vector<bool> visited(n, false);
     visited[source] = true;
 
-    std::queue<int> queue;
-    queue.push(source);
-
-    while (!queue.empty()) {
-      for (const auto next : connections[queue.front()]) {
-        if (next == destination) return true;
-        if (!visited[next]) {
-          visited[next] = true;
-          queue.push(next);
+    bool valid{true};
+    while (valid) {
+      valid = false;
+      for (const auto& edge : edges) {
+        if (visited[edge[0]] != visited[edge[1]]) {
+          visited[edge[0]] = true;
+          visited[edge[1]] = true;
+          valid = true;
         }
       }
-      queue.pop();
+      if (visited[destination]) return true;
     }
 
     return false;
