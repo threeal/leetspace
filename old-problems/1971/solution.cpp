3b989489416b62e276c5764426ea92cab5e3dec0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-26 19:29:10 +0700

    test: add test for problem 1971. Find if Path Exists in Graph

---
+++ old-problems/1971/solution.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+class Solution {
+ public:
+  bool validPath(int n, std::vector<std::vector<int>>& edges, int source, int destination) {
+    (void)n;
+    (void)edges;
+    (void)source;
+    (void)destination;
+    return false;
+  }
+};
