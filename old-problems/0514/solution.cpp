a1f958e6511642adc16ad056c8946c8b27144471
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-27 13:41:39 +0700

    feat(problem-514/cpp): solve by using dynamic programming

--- old-problems/0514/solution.cpp
+++ old-problems/0514/solution.cpp
@@ -1,44 +1,45 @@
 #include <algorithm>
+#include <limits>
+#include <list>
 #include <string>
 #include <vector>
 
 class Solution {
  public:
   int findRotateSteps(std::string ring, std::string key) {
-    for (auto &c : ring) c -= 'a';
-    for (auto &c : key) c -= 'a';
+    for (auto& c : ring) c -= 'a';
+    for (auto& c : key) c -= 'a';
 
-    std::vector<std::vector<int>> distances(ring.size(), std::vector(26, 101));
-    std::vector<std::vector<int>> nexts{std::vector(ring.size(), std::vector<int>(26))};
-
-    for (int i{0}; i < static_cast<int>(ring.size()); ++i) {
-      for (int j{0}; j < i; ++j) {
-        const int distance{std::min(i - j, static_cast<int>(ring.size()) + j - i)};
-        if (distance < distances[i][ring[j]]) {
-          nexts[i][ring[j]] = j;
-          distances[i][ring[j]] = distance;
-        }
-      }
-
-      nexts[i][ring[i]] = i;
-      distances[i][ring[i]] = 0;
-
-      for (int j{i + 1}; j < static_cast<int>(ring.size()); ++j) {
-        const int distance{std::min(j - i, static_cast<int>(ring.size()) + i - j)};
-        if (distance < distances[i][ring[j]]) {
-          nexts[i][ring[j]] = j;
-          distances[i][ring[j]] = distance;
-        }
-      }
+    std::vector<std::list<std::size_t>> positions(26);
+    for (std::size_t i{0}; i < ring.size(); ++i) {
+      positions[ring[i]].push_back(i);
     }
 
-    int steps{0};
-    int index{0};
-    for (const auto c : key) {
-      steps += distances[index][c] + 1;
-      index = nexts[index][c];
+    std::vector<std::vector<int>> cache(ring.size(), std::vector(key.size(), -1));
+    return findRotateSteps(ring, key, positions, cache, 0, 0);
+  }
+
+  int findRotateSteps(
+      const std::string& ring,
+      const std::string& key,
+      const std::vector<std::list<std::size_t>>& positions,
+      std::vector<std::vector<int>>& cache,
+      std::size_t ringPos,
+      std::size_t keyPos) {
+    if (keyPos == key.size()) return 0;
+
+    if (cache[ringPos][keyPos] < 0) {
+      int minStep{std::numeric_limits<int>::max()};
+      for (const auto pos : positions[key[keyPos]]) {
+        const int distance = pos > ringPos
+            ? std::min(pos - ringPos, ring.size() + ringPos - pos)
+            : std::min(ringPos - pos, ring.size() + pos - ringPos);
+        int step{1 + distance + findRotateSteps(ring, key, positions, cache, pos, keyPos + 1)};
+        if (step < minStep) minStep = step;
+      }
+      cache[ringPos][keyPos] = minStep;
     }
 
-    return steps;
+    return cache[ringPos][keyPos];
   }
 };
