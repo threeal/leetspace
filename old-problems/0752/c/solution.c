29e8d28d5db5df95ed06fffa75d30cba8fe8d916
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-27 18:25:34 +0700

    perf(problem-752/c): store seen in bytes

--- old-problems/0752/c/solution.c
+++ old-problems/0752/c/solution.c
@@ -1,26 +1,24 @@
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 int openLock(char** deadends, int deadendsSize, char* targetStr) {
   if (targetStr[0] == '0' && targetStr[1] == '0' && targetStr[2] == '0' && targetStr[3] == '0') return 0;
 
-  bool seen[10000];
-  for (int i = 0; i < 10000; ++i) {
-    seen[i] = false;
-  }
-
+  uint8_t seen[1250];
+  memset(seen, 0, 1250 * sizeof(uint8_t));
   for (int i = 0; i < deadendsSize; ++i) {
-    seen[atoi(deadends[i])] = true;
+    int deadend = atoi(deadends[i]);
+    seen[deadend / 8] |= 1 << deadend % 8;
   }
 
-  if (seen[0]) return -1;
+  if ((seen[0] & 1) == 1) return -1;
 
   int16_t queue[1500];
   int queueBegin = 0;
   int queueEnd = 1;
   queue[queueBegin] = 0;
-  seen[0] = true;
+  seen[0] |= 1;
 
   int steps = 1;
   int16_t target = atoi(targetStr);
@@ -30,19 +28,19 @@ int openLock(char** deadends, int deadendsSize, char* targetStr) {
         int key = queue[queueBegin];
         key += (key / exp) % 10 == 9 ? -9 * exp : exp;
         if (key == target) return steps;
-        if (!seen[key]) {
+        if ((seen[key / 8] >> key % 8 & 1) == 0) {
           queue[queueEnd] = key;
           if (++queueEnd == 1500) queueEnd = 0;
-          seen[key] = true;
+          seen[key / 8] |= 1 << key % 8;
         }
 
         key = queue[queueBegin];
         key += (key / exp) % 10 == 0 ? 9 * exp : -exp;
         if (key == target) return steps;
-        if (!seen[key]) {
+        if ((seen[key / 8] >> key % 8 & 1) == 0) {
           queue[queueEnd] = key;
           if (++queueEnd == 1500) queueEnd = 0;
-          seen[key] = true;
+          seen[key / 8] |= 1 << key % 8;
         }
       }
       if (++queueBegin == 1500) queueBegin = 0;
