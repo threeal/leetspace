f9e9966e56b921977425327427deb3f95ab22539
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-28 21:35:50 +0700

    feat(problem-2370/c): solve by storing counting possibilities in array

--- old-problems/2370/c/solution.c
+++ old-problems/2370/c/solution.c
@@ -1,4 +1,22 @@
 int longestIdealString(char* s, int k) {
-  (void)s;
-  return k;
+  int counts[26] = {0};
+
+  while (*s != 0) {
+    *s -= 'a';
+    const int count = counts[*s] + 1;
+
+    char n = *s + k;
+    if (n > 25) n = 25;
+    for (char i = *s >= k ? *s - k : 0; i <= n; ++i) {
+      if (count > counts[i]) counts[i] = count;
+    }
+
+    ++s;
+  }
+
+  for (char i = 1; i < 26; ++i) {
+    if (counts[i] > counts[0]) counts[0] = counts[i];
+  }
+
+  return counts[0];
 }
