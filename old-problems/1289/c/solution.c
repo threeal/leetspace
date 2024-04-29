25595e762270d11da83116639f5d4f6fb75f2f16
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-29 16:50:33 +0700

    feat(problem-1289/c): solve by summing elements in grid

--- old-problems/1289/c/solution.c
+++ old-problems/1289/c/solution.c
@@ -1,6 +1,33 @@
+#include <limits.h>
+
 int minFallingPathSum(int** grid, int gridSize, int* gridColSize) {
-  (void)grid;
-  (void)gridSize;
-  (void)gridColSize;
-  return -0;
+  for (int i = 1; i < gridSize; ++i) {
+    int minVals[2] = {INT_MAX, INT_MAX};
+    for (int j = 0; j < gridColSize[i - 1]; ++j) {
+      if (grid[i - 1][j] < minVals[1]) {
+        if (grid[i - 1][j] <= minVals[0]) {
+          minVals[1] = minVals[0];
+          minVals[0] = grid[i - 1][j];
+        } else {
+          minVals[1] = grid[i - 1][j];
+        }
+      }
+    }
+
+    if (minVals[0] == minVals[1]) {
+      for (int j = 0; j < gridColSize[i]; ++j) {
+        grid[i][j] += minVals[0];
+      }
+    } else {
+      for (int j = 0; j < gridColSize[i]; ++j) {
+        grid[i][j] += grid[i - 1][j] == minVals[0] ? minVals[1] : minVals[0];
+      }
+    }
+  }
+
+  int minVal = INT_MAX;
+  for (int i = 0; i < gridColSize[gridSize - 1]; ++i) {
+    if (grid[gridSize - 1][i] < minVal) minVal = grid[gridSize - 1][i];
+  }
+  return minVal;
 }
