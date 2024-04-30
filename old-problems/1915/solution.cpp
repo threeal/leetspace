9cca8c8de84529781b580e43a5dc827eaf6358f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-30 13:29:07 +0700

    feat(problem-1915/cpp): solve by storing  prev counting in bytes

--- old-problems/1915/solution.cpp
+++ old-problems/1915/solution.cpp
@@ -1,9 +1,17 @@
 #include <string>
+#include <vector>
 
 class Solution {
  public:
   long long wonderfulSubstrings(std::string word) {
-    (void)word;
-    return 0;
+    long long count{0};
+    std::vector<int> frequencies(1024, 0);
+    frequencies[0] = 1;
+    int sum{0};
+    for (const auto c : word) {
+      sum ^= 1 << (c - 'a');
+      count += frequencies[sum]++ + frequencies[sum ^ 1] + frequencies[sum ^ 2] + frequencies[sum ^ 4] + frequencies[sum ^ 8] + frequencies[sum ^ 16] + frequencies[sum ^ 32] + frequencies[sum ^ 64] + frequencies[sum ^ 128] + frequencies[sum ^ 256] + frequencies[sum ^ 512];
+    }
+    return count;
   }
 };
