d4d2156e14b511163e8ccdcbf5eb635aed834906
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-01 17:02:53 +0700

    feat(problem-2000/cpp): solve by using `std::reverse`

--- old-problems/2000/solution.cpp
+++ old-problems/2000/solution.cpp
@@ -1,8 +1,15 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
-  std::string reversePrefix(std::string word, char /*ch*/) {
+  std::string reversePrefix(std::string word, char ch) {
+    for (auto it{word.begin()}; it != word.end(); ++it) {
+      if (*it == ch) {
+        std::reverse(word.begin(), it + 1);
+        break;
+      }
+    }
     return word;
   }
 };
