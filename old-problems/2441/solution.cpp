95b8961cd33746b058a62094e4c1142a6616aa97
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-02 10:57:08 +0700

    feat(problem-2441/cpp): solve by marking negative numbers

--- old-problems/2441/solution.cpp
+++ old-problems/2441/solution.cpp
@@ -2,7 +2,17 @@
 
 class Solution {
  public:
-  int findMaxK(std::vector<int>& /*nums*/) {
-    return 0;
+  int findMaxK(std::vector<int>& nums) {
+    std::vector<bool> exists(1001, false);
+    for (const auto num : nums) {
+      if (num < 0) exists[-num] = true;
+    }
+
+    int largestNum{-1};
+    for (const auto num : nums) {
+      if (num > 0 && exists[num] && largestNum < num) largestNum = num;
+    }
+
+    return largestNum;
   }
 };
