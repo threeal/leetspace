d466b6534b80d9be258e33037da014c2222a236d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-03 09:44:47 +0700

    test: add test for problem 165. Compare Version Numbers

---
+++ old-problems/0165/solution.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+class Solution {
+ public:
+  int compareVersion(std::string version1, std::string version2) {
+    (void)version1;
+    (void)version2;
+    return 0;
+  }
+};
