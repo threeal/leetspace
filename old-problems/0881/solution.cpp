4326aa1fcfa6a41d9fa63e485b618c04e2c4f386
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-04 16:08:09 +0700

    feat(problem-881/cpp): solve by sort and two pointers

--- old-problems/0881/solution.cpp
+++ old-problems/0881/solution.cpp
@@ -1,10 +1,28 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int numRescueBoats(std::vector<int>& people, int limit) {
-    (void)people;
-    (void)limit;
-    return 0;
+    int boats{0};
+
+    std::sort(people.begin(), people.end());
+
+    std::size_t low{0};
+    std::size_t high{people.size() - 1};
+
+    while (low < high) {
+      ++boats;
+      if (people[low] + people[high] <= limit) {
+        ++low;
+        --high;
+      } else {
+        --high;
+      }
+    }
+
+    if (low == high) ++boats;
+
+    return boats;
   }
 };
