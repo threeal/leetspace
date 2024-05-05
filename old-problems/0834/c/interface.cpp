c1d6790029c3d8c5fb469dd10fcbe61fc2318ed7
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-05 23:33:45 +0700

    test(problem-834): add suppport to solve in C language

---
+++ old-problems/0834/c/interface.cpp
@@ -0,0 +1,23 @@
+#include <cstring>
+#include <vector>
+
+extern "C" {
+int* sumOfDistancesInTree(int n, int** edges, int edgesSize, int* edgesColSize, int* returnSize);
+}
+
+std::vector<int> solution_c(int n, std::vector<std::vector<int>> edges) {
+  std::vector<int*> edgesDatas(edges.size());
+  std::vector<int> edgesSizes(edges.size());
+  for (std::size_t i{0}; i < edges.size(); ++i) {
+    edgesDatas[i] = edges[i].data();
+    edgesSizes[i] = edges[i].size();
+  }
+
+  int returnSize{};
+  const int* returnData{sumOfDistancesInTree(n, edgesDatas.data(), edgesDatas.size(), edgesSizes.data(), &returnSize)};
+
+  std::vector<int> output(returnSize);
+  std::memcpy(output.data(), returnData, returnSize * sizeof(int));
+
+  return output;
+}
---
+++ old-problems/0834/c/solution.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+int* sumOfDistancesInTree(int n, int** edges, int edgesSize, int* edgesColSize, int* returnSize) {
+  (void)n;
+  (void)edges;
+  (void)edgesSize;
+  (void)edgesColSize;
+  *returnSize = 0;
+  return NULL;
+}
