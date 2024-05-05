60c32b870e63bbe3b81166dc122c8f3e256a4b94
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-05 11:24:52 +0700

    feat(problem-237/cpp): solve by setting given node with next node

--- old-problems/0237/solution.cpp
+++ old-problems/0237/solution.cpp
@@ -1,6 +1,8 @@
+#include <cstring>
+
 class Solution {
  public:
-  void deleteNode(ListNode* /*node*/) {
-    return;
+  void deleteNode(ListNode* node) {
+    std::memcpy(node, node->next, sizeof(ListNode));
   }
 };
