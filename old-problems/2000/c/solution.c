cb400c7a0e12940e20ce724d059353f51bcb7682
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-06 15:38:58 +0700

    feat(problem-2000/c): solve by manually reversing given string

--- old-problems/2000/c/solution.c
+++ old-problems/2000/c/solution.c
@@ -1,4 +1,15 @@
 char* reversePrefix(char* word, char ch) {
-  (void)ch;
+  for (char* right = word; *right != 0; ++right) {
+    if (*right == ch) {
+      char temp;
+      for (char* left = word; left < right; ++left) {
+        temp = *left;
+        *left = *right;
+        *right = temp;
+        --right;
+      }
+      break;
+    }
+  }
   return word;
 }
