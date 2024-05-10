aeb91b392aac89a18f86338cc3800cf83179e7d1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-10 22:20:09 +0700

    feat(problem-2441/c): solve by marking negative numbers

--- old-problems/2441/c/solution.c
+++ old-problems/2441/c/solution.c
@@ -1,3 +1,15 @@
+#include <stdint.h>
+
 int findMaxK(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  uint8_t exists[126] = {0};
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] < 0) exists[-nums[i] / 8] |= 1 << -nums[i] % 8;
+  }
+
+  int largestNum = -1;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] > 0 && (exists[nums[i] / 8] >> nums[i] % 8 & 1) && largestNum < nums[i]) largestNum = nums[i];
+  }
+
+  return largestNum;
 }
