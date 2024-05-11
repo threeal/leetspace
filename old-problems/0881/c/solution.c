8edd9e27edb649b6fafcf80eba160edbd26ee8ce
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-11 19:44:35 +0700

    feat(problem-881/c): solve by using sort and two pointers

--- old-problems/0881/c/solution.c
+++ old-problems/0881/c/solution.c
@@ -1,6 +1,21 @@
+#include <stdlib.h>
+
+int comp(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int numRescueBoats(int* people, int peopleSize, int limit) {
-  (void)people;
-  (void)peopleSize;
-  (void)limit;
-  return 0;
+  int boats = 0;
+
+  qsort(people, peopleSize, sizeof(int), comp);
+
+  int* peopleEnd = people + peopleSize - 1;
+  while (people < peopleEnd) {
+    ++boats;
+    if (*people + *peopleEnd <= limit) ++people;
+    --peopleEnd;
+  }
+
+  if (people == peopleEnd) ++boats;
+  return boats;
 }
