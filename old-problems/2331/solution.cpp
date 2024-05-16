2f174bf4a4992dc1f869d982745b2e19a95f6c8a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-16 10:31:56 +0700

    feat(problem-2331/cpp): solve by calling function recursively

--- old-problems/2331/solution.cpp
+++ old-problems/2331/solution.cpp
@@ -1,6 +1,22 @@
 class Solution {
  public:
-  bool evaluateTree(TreeNode* /*root*/) {
-    return false;
+  bool evaluateTree(TreeNode* root) {
+    switch (root->val) {
+      case 0:
+        return false;
+
+      case 1:
+        return true;
+
+      case 2:
+        if (evaluateTree(root->left)) return true;
+        return evaluateTree(root->right);
+
+      case 3:
+        if (!evaluateTree(root->left)) return false;
+        return evaluateTree(root->right);
+    }
+
+    return 0;
   }
 };
