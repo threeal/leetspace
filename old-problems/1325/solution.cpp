f23ae2160632cab69dc6d297e3f618beca591564
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-17 10:31:26 +0700

    feat(problem-1325/cpp): solve by using depth-first search

--- old-problems/1325/solution.cpp
+++ old-problems/1325/solution.cpp
@@ -1,6 +1,16 @@
 class Solution {
  public:
-  TreeNode* removeLeafNodes(TreeNode* root, int /*target*/) {
+  TreeNode* removeLeafNodes(TreeNode* root, int target) {
+    dfs(root, target);
     return root;
+  }
+
+  void dfs(TreeNode*& root, int target) {
+    if (root->left != nullptr) dfs(root->left, target);
+    if (root->right != nullptr) dfs(root->right, target);
+
+    if (root->left == nullptr && root->right == nullptr && root->val == target) {
+      root = nullptr;
+    }
   }
 };
