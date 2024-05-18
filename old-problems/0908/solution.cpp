0f5739e201b93a9c6d0ed04719b54a2c75244b36
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-18 18:29:12 +0700

    feat(problem-908/cpp): solve by comparing min and max values

--- old-problems/0908/solution.cpp
+++ old-problems/0908/solution.cpp
@@ -1,8 +1,16 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
-  int smallestRangeI(std::vector<int>& /*nums*/, int /*k*/) {
-    return 0;
+  int smallestRangeI(std::vector<int>& nums, int k) {
+    int min{10001};
+    int max{-1};
+    for (const auto num : nums) {
+      if (num > max) max = num;
+      if (num < min) min = num;
+    }
+
+    return std::max(max - min - k - k, 0);
   }
 };
