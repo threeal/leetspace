8de4d744f26357383c995ad02fa8b892e8bce9b6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-18 18:11:24 +0700

    feat(problem-979/cpp): solve using DFS and setting parent's coins

--- old-problems/0979/solution.cpp
+++ old-problems/0979/solution.cpp
@@ -1,6 +1,27 @@
+#include <cstdlib>
+
 class Solution {
  public:
-  int distributeCoins(TreeNode* /*root*/) {
-    return 0;
+  int distributeCoins(TreeNode* root) {
+    int movement{0};
+
+    if (root->left != nullptr) movement += distributeCoins(root, root->left);
+    if (root->right != nullptr) movement += distributeCoins(root, root->right);
+
+    return movement;
+  }
+
+  int distributeCoins(TreeNode* parent, TreeNode* node) {
+    int movement{0};
+
+    if (node->left != nullptr) movement += distributeCoins(node, node->left);
+    if (node->right != nullptr) movement += distributeCoins(node, node->right);
+
+    if (node->val != 1) {
+      parent->val += node->val - 1;
+      movement += std::abs(node->val - 1);
+    }
+
+    return movement;
   }
 };
