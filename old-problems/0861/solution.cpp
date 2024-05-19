6186eb0ba6ea6ab5599ffd6469ce7437317435b6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-19 19:42:53 +0700

    perf(problem-861/cpp): count element instead

--- old-problems/0861/solution.cpp
+++ old-problems/0861/solution.cpp
@@ -1,37 +1,21 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int matrixScore(std::vector<std::vector<int>>& grid) {
-    for (auto& row : grid) {
-      if (row[0] == 1) continue;
-      for (auto& val : row) val = val == 0 ? 1 : 0;
-    }
-
     const int m = grid.size();
     const int n = grid.front().size();
-    for (int j{0}; j < n; ++j) {
-      int ones{0};
+
+    int score{(1 << (n - 1)) * m};
+    for (int j{1}; j < n; ++j) {
+      int count{0};
       for (int i{0}; i < m; ++i) {
-        if (grid[i][j] == 1) ++ones;
+        if (grid[i][j] == grid[i][0]) ++count;
       }
-
-      if (ones < m - ones) {
-        for (int i{0}; i < m; ++i) {
-          grid[i][j] = grid[i][j] == 0 ? 1 : 0;
-        }
-      }
+      score += (1 << (n - j - 1)) * std::max(count, m - count);
     }
 
-    int totalScore{0};
-    for (auto& row : grid) {
-      int score{0};
-      for (int j{0}; j < n; ++j) {
-        if (row[j] == 1) score |= 1 << (n - j - 1);
-      }
-      totalScore += score;
-    }
-
-    return totalScore;
+    return score;
   }
 };
