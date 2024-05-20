525b8f5cb30ba6bf1a7ff76e468d6e559dfc822c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-20 12:54:09 +0700

    feat(problem-861/cpp): solve by counting elements

--- old-problems/0861/c/solution.c
+++ old-problems/0861/c/solution.c
@@ -1,6 +1,14 @@
-int matrixScore(int** grid, int gridSize, int* gridColSize) {
-  (void)grid;
-  (void)gridSize;
-  (void)gridColSize;
-  return 0;
+int matrixScore(int** grid, int gridSize, int* gridColSizes) {
+  const int gridColSize = gridColSizes[0];
+
+  int score = (1 << (gridColSize - 1)) * gridSize;
+  for (int j = 1; j < gridColSize; ++j) {
+    int count = 0;
+    for (int i = 0; i < gridSize; ++i) {
+      if (grid[i][j] == grid[i][0]) ++count;
+    }
+    score += (1 << (gridColSize - j - 1)) * (count > gridSize - count ? count : gridSize - count);
+  }
+
+  return score;
 }
