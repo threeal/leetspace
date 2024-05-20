c5e49d1c9e185218ce79ae7796c2756e10c097f9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-20 13:33:12 +0700

    feat(problem-3075/c): solve by sorting and iterating

--- old-problems/3075/c/solution.c
+++ old-problems/3075/c/solution.c
@@ -1,6 +1,17 @@
+#include <stdlib.h>
+
+int comp(const void* a, const void* b) {
+  return *(int*)b - *(int*)a;
+}
+
 long long maximumHappinessSum(int* happiness, int happinessSize, int k) {
-  (void)happiness;
-  (void)happinessSize;
-  (void)k;
-  return 0;
+  qsort(happiness, happinessSize, sizeof(int), comp);
+
+  long long sum = 0;
+  for (int i = 0; i < k; ++i) {
+    if (happiness[i] <= i) break;
+    sum += happiness[i] - i;
+  }
+
+  return sum;
 }
