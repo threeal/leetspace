0f18710a493f849834c60f0e19bb61c2c1f5e1d3
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-20 09:24:52 +0700

    feat(problem-1863/cpp): solve by iterating and storing prev calculation

--- old-problems/1863/solution.cpp
+++ old-problems/1863/solution.cpp
@@ -1,8 +1,40 @@
 #include <vector>
 
+int pown(int x, int n) {
+  int result{1};
+  while (n) {
+    if (n & 1) result *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return result;
+}
+
 class Solution {
  public:
-  int subsetXORSum(std::vector<int>& /*nums*/) {
-    return 0;
+  int subsetXORSum(std::vector<int>& nums) {
+    if (nums.empty()) return 0;
+    int sum{0};
+
+    std::vector<int> caches{};
+    caches.reserve(pown(2, nums.size() - 1) - 1);
+
+    for (int i = nums.size() - 1; i > 0; --i) {
+      for (int j = caches.size() - 1; j >= 0; --j) {
+        const auto val{caches[j] ^ nums[i]};
+        sum += val;
+        caches.push_back(val);
+      }
+
+      sum += nums[i];
+      caches.push_back(nums[i]);
+    }
+
+    for (const auto cache : caches) {
+      sum += cache ^ nums[0];
+    }
+    sum += nums[0];
+
+    return sum;
   }
 };
