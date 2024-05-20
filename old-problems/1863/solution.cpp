522d6a8d5c1b4e260dc1afd01b475674ade3e7d2
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-20 09:34:01 +0700

    perf(problem-1863/cpp): solve by using bit operation instead

--- old-problems/1863/solution.cpp
+++ old-problems/1863/solution.cpp
@@ -1,40 +1,12 @@
 #include <vector>
 
-int pown(int x, int n) {
-  int result{1};
-  while (n) {
-    if (n & 1) result *= x;
-    x *= x;
-    n >>= 1;
-  }
-  return result;
-}
-
 class Solution {
  public:
   int subsetXORSum(std::vector<int>& nums) {
-    if (nums.empty()) return 0;
-    int sum{0};
-
-    std::vector<int> caches{};
-    caches.reserve(pown(2, nums.size() - 1) - 1);
-
-    for (int i = nums.size() - 1; i > 0; --i) {
-      for (int j = caches.size() - 1; j >= 0; --j) {
-        const auto val{caches[j] ^ nums[i]};
-        sum += val;
-        caches.push_back(val);
-      }
-
-      sum += nums[i];
-      caches.push_back(nums[i]);
+    int result{0};
+    for (const auto num : nums) {
+      result |= num;
     }
-
-    for (const auto cache : caches) {
-      sum += cache ^ nums[0];
-    }
-    sum += nums[0];
-
-    return sum;
+    return result << (nums.size() - 1);
   }
 };
