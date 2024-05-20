3b98985486d3a642f4f5cf02747049c5aa7ee23b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-20 13:14:24 +0700

    feat(problem-2373/c): solve by iterating grid

--- old-problems/2373/c/solution.c
+++ old-problems/2373/c/solution.c
@@ -1,10 +1,27 @@
 #include <stdlib.h>
 
 int** largestLocal(int** grid, int gridSize, int* gridColSize, int* returnSize, int** returnColumnSizes) {
-  (void)grid;
-  (void)gridSize;
   (void)gridColSize;
-  (void)returnColumnSizes;
-  *returnSize = 0;
-  return NULL;
+
+  *returnSize = gridSize - 2;
+  *returnColumnSizes = malloc(*returnSize * sizeof(int));
+  int** outputGrid = malloc(*returnSize * sizeof(int*));
+  for (int i = gridSize - 3; i >= 0; --i) {
+    (*returnColumnSizes)[i] = gridSize - 2;
+    outputGrid[i] = malloc((*returnColumnSizes)[i] * sizeof(int));
+  }
+
+  for (int i = gridSize - 3; i >= 0; --i) {
+    for (int j = gridSize - 3; j >= 0; --j) {
+      int largest = 0;
+      for (int ii = i + 2; ii >= i; --ii) {
+        for (int jj = j + 2; jj >= j; --jj) {
+          if (grid[ii][jj] > largest) largest = grid[ii][jj];
+        }
+      }
+      outputGrid[i][j] = largest;
+    }
+  }
+
+  return outputGrid;
 }
