6b0c771996fc9cde0037168b6a581e4112505068
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-22 12:20:39 +0700

    perf(problem-131/cpp): push to the same vector

--- old-problems/0131/solution.cpp
+++ old-problems/0131/solution.cpp
@@ -4,30 +4,26 @@
 class Solution {
  public:
   std::vector<std::vector<std::string>> partition(std::string s) {
-    return partition(s.data(), s.size());
+    std::vector<std::vector<std::string>> output{};
+    partition(output, s.data(), s.size());
+    return output;
   }
 
-  std::vector<std::vector<std::string>> partition(const std::string& s, int n) {
-    std::vector<std::vector<std::string>> output;
-
+  void partition(std::vector<std::vector<std::string>>& output, const std::string& s, int n) {
     for (int i{n - 1}; i >= 0; --i) {
       if (isPalindrome(s.data(), i, n - 1)) {
         if (i == 0) {
           output.push_back({s.substr(i, n - i)});
         } else {
-          auto parts{partition(s, i)};
-          if (!parts.empty()) {
-            const auto substr{s.substr(i, n - i)};
-            for (auto& part : parts) {
-              part.push_back(substr);
-            }
-            output.insert(output.end(), parts.begin(), parts.end());
+          std::size_t j{output.size()};
+          partition(output, s, i);
+          while (j < output.size()) {
+            output[j].push_back({s.substr(i, n - i)});
+            ++j;
           }
         }
       }
     }
-
-    return output;
   }
 
   bool isPalindrome(const char* s, std::size_t left, std::size_t right) {
