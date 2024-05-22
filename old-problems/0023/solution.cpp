fef80f151cfb090f98acc072d365055263b1452a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-22 13:57:43 +0700

    feat(problem-23/cpp): solve by using priority queue

--- old-problems/0023/solution.cpp
+++ old-problems/0023/solution.cpp
@@ -1,8 +1,40 @@
+#include <queue>
+#include <tuple>
 #include <vector>
 
 class Solution {
  public:
-  ListNode* mergeKLists(std::vector<ListNode*>& /*lists*/) {
-    return nullptr;
+  ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+    std::priority_queue<
+        std::tuple<int, ListNode*>,
+        std::vector<std::tuple<int, ListNode*>>,
+        std::greater<std::tuple<int, ListNode*>>>
+        queue{};
+
+    for (const auto list : lists) {
+      if (list == nullptr) continue;
+      queue.push({list->val, list});
+    }
+
+    if (queue.empty()) return nullptr;
+
+    auto head{std::get<1>(queue.top())};
+    queue.pop();
+
+    if (head->next != nullptr)
+      queue.push({head->next->val, head->next});
+
+    auto node{head};
+    while (!queue.empty()) {
+      node->next = std::get<1>(queue.top());
+      queue.pop();
+
+      if (node->next->next != nullptr)
+        queue.push({node->next->next->val, node->next->next});
+
+      node = node->next;
+    }
+
+    return head;
   }
 };
