2f48b2d11476057489a178d05ee60f2794348c89
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-24 11:15:45 +0700

    test: add test for problem 1255. Maximum Score Words Formed by Letters

---
+++ old-problems/1255/solution.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  int maxScoreWords(std::vector<std::string>& /*words*/, std::vector<char>& /*letters*/, std::vector<int>& /*score*/) {
+    return 0;
+  }
+};
