4d132f99ad81af805cb0d1df051433ca0c8eb2f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-24 21:16:24 +0700

    feat(problem-1184/cpp): solve by iterating list of distances

--- old-problems/1184/solution.cpp
+++ old-problems/1184/solution.cpp
@@ -1,8 +1,19 @@
+#include <utility>
 #include <vector>
 
 class Solution {
  public:
-  int distanceBetweenBusStops(std::vector<int>& /*distance*/, int /*start*/, int /*destination*/) {
-    return 0;
+  int distanceBetweenBusStops(std::vector<int>& distance, int start, int destination) {
+    if (start == destination) return 0;
+    if (destination < start) std::swap(start, destination);
+
+    int forwardDistance{0};
+    for (int i{start}; i < destination; ++i) forwardDistance += distance[i];
+
+    int backwardDistance{0};
+    for (int i = distance.size() - 1; i >= destination; --i) backwardDistance += distance[i];
+    for (int i{start - 1}; i >= 0; --i) backwardDistance += distance[i];
+
+    return forwardDistance < backwardDistance ? forwardDistance : backwardDistance;
   }
 };
