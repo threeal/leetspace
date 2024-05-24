4175803bd3868a48b6cdeeb6a5a4bef98868be1e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-24 20:25:56 +0700

    feat(problem-25/cpp): solve by storing reference

--- old-problems/0025/solution.cpp
+++ old-problems/0025/solution.cpp
@@ -1,6 +1,28 @@
+#include <utility>
+
 class Solution {
  public:
-  ListNode* reverseKGroup(ListNode* head, int /*k*/) {
+  ListNode* reverseKGroup(ListNode* head, int k) {
+    ListNode** node{&head};
+    while (*node != nullptr) {
+      auto end{(*node)->next};
+      for (int i{1}; i < k; ++i) {
+        if (end == nullptr) return head;
+        end = end->next;
+      }
+
+      auto it{(*node)->next};
+      (*node)->next = end;
+      for (int i{1}; i < k; ++i) {
+        std::swap(it->next, *node);
+        std::swap(it, *node);
+      }
+
+      for (int i{0}; i < k; ++i) {
+        node = &((*node)->next);
+      }
+    }
+
     return head;
   }
 };
