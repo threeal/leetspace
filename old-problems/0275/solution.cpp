859f88d7804183abd788d6cd42fb359ea34a53f3
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-24 20:44:40 +0700

    feat(problem-275/cpp): solve by using forward iteration

--- old-problems/0275/solution.cpp
+++ old-problems/0275/solution.cpp
@@ -2,7 +2,11 @@
 
 class Solution {
  public:
-  int hIndex(std::vector<int>& /*citations*/) {
-    return 0;
+  int hIndex(std::vector<int>& citations) {
+    int count = citations.size();
+    for (std::size_t i{0}; i < citations.size(); ++i, --count) {
+      if (citations[i] >= count) return count;
+    }
+    return count;
   }
 };
