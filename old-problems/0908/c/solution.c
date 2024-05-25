ede112a9134671343986e77b016954a91f1d5b86
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-25 11:15:00 +0700

    feat(problem-908/c): solve by comparing min and max values

--- old-problems/0908/c/solution.c
+++ old-problems/0908/c/solution.c
@@ -1,6 +1,10 @@
 int smallestRangeI(int* nums, int numsSize, int k) {
-  (void)nums;
-  (void)numsSize;
-  (void)k;
-  return 0;
+  int min = 10001;
+  int max = -1;
+  for (int i = numsSize - 1; i >= 0; --i) {
+    if (nums[i] > max) max = nums[i];
+    if (nums[i] < min) min = nums[i];
+  }
+
+  return max - min - k - k > 0 ? max - min - k - k : 0;
 }
