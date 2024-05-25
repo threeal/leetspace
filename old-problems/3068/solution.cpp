f7499e1dd716cfa35cb49116172db8e3c7f21a62
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-25 11:21:33 +0700

    test: add test for problem 3068. Find the Maximum Sum of Node Values

---
+++ old-problems/3068/solution.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+class Solution {
+ public:
+  long long maximumValueSum(std::vector<int>& /*nums*/, int /*k*/, std::vector<std::vector<int>>& /*edges*/) {
+    return 0;
+  }
+};
