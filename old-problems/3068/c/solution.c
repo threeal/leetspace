01e959d25e844b3041cfe869968c40c2ee62e409
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-27 11:36:36 +0700

    test(problem-3068): add support to test in C language

---
+++ old-problems/3068/c/solution.c
@@ -0,0 +1,9 @@
+long long maximumValueSum(int* nums, int numsSize, int k, int** edges, int edgesSize, int* edgesColSize) {
+  (void)nums;
+  (void)numsSize;
+  (void)k;
+  (void)edges;
+  (void)edgesSize;
+  (void)edgesColSize;
+  return 0;
+}
