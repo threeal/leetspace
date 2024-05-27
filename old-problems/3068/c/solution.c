d5e369dc5566069be64f83f291c7540e8ffcecab
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-27 11:37:01 +0700

    feat(problem-3068/c): solve by iterating node values

--- old-problems/3068/c/solution.c
+++ old-problems/3068/c/solution.c
@@ -1,9 +1,29 @@
+#include <limits.h>
+
 long long maximumValueSum(int* nums, int numsSize, int k, int** edges, int edgesSize, int* edgesColSize) {
-  (void)nums;
-  (void)numsSize;
-  (void)k;
   (void)edges;
   (void)edgesSize;
   (void)edgesColSize;
-  return 0;
+
+  long long total = 0;
+  int gainCount = 0;
+  int minGain = INT_MAX;
+  int minLoss = INT_MIN;
+  for (int i = 0; i < numsSize; ++i) {
+    total += nums[i];
+    const int gain = (nums[i] ^ k) - nums[i];
+    if (gain > 0) {
+      total += gain;
+      ++gainCount;
+      if (gain < minGain) minGain = gain;
+    } else {
+      if (gain > minLoss) minLoss = gain;
+    }
+  }
+  if (gainCount % 2 == 1) {
+    total -= minLoss != INT_MIN
+        ? (minGain < -minLoss ? minGain : -minLoss)
+        : minGain;
+  }
+  return total;
 }
