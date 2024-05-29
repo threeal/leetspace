bb9fe0309bb8ac83c2f1fd0ecd8a765b1bd4e03b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-29 12:22:36 +0700

    feat(problem-1404/cpp): solve by using iteration

--- old-problems/1404/solution.cpp
+++ old-problems/1404/solution.cpp
@@ -3,6 +3,23 @@
 class Solution {
  public:
   int numSteps(std::string s) {
-    return s.size();
+    int steps{0};
+    int i = s.size() - 1;
+
+    while (i > 0 && s[i] == '0') {
+      --i;
+      ++steps;
+    }
+
+    while (i > 0) {
+      steps += 2;
+      --i;
+      while (i >= 0 && s[i] == '1') {
+        --i;
+        ++steps;
+      }
+    }
+
+    return steps;
   }
 };
