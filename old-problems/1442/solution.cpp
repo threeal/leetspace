fe4762c1479dde79f433ac4833438d894d266b75
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-30 14:54:12 +0700

    feat(problem-1442/cpp): solve by using for loop

--- old-problems/1442/solution.cpp
+++ old-problems/1442/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int countTriplets(std::vector<int>& arr) {
-    return arr.size();
+    int count{0};
+    for (int k = arr.size() - 1; k > 0; --k) {
+      int sum{arr[k]};
+      for (int i{k - 1}; i >= 0; --i) {
+        sum ^= arr[i];
+        if (sum == 0) count += k - i;
+      }
+    }
+    return count;
   }
 };
