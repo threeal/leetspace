8d19d858184b08c3e25c15c74ff494b857c6bee0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-31 13:16:32 +0700

    test(problem-1184): add support to test in C language

---
+++ old-problems/1184/c/solution.c
@@ -0,0 +1,7 @@
+int distanceBetweenBusStops(int* distance, int distanceSize, int start, int destination) {
+  (void)distance;
+  (void)distanceSize;
+  (void)start;
+  (void)destination;
+  return 0;
+}
