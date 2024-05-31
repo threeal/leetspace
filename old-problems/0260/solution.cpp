de6a61429fe84ae77333136269e7de0fb431ab8c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-31 12:36:23 +0700

    feat(problem-260/cpp): solve by using XOR operation

--- old-problems/0260/solution.cpp
+++ old-problems/0260/solution.cpp
@@ -1,8 +1,26 @@
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> singleNumber(std::vector<int>& nums) {
-    return {nums[0], nums[1]};
+    const int sum{std::accumulate(nums.begin(), nums.end(), 0, std::bit_xor())};
+
+    int mask{1};
+    while ((sum & mask) == 0) {
+      mask = mask << 1;
+    }
+
+    int a{0};
+    int b{0};
+    for (const int num : nums) {
+      if ((num & mask) == 0) {
+        b ^= num;
+      } else {
+        a ^= num;
+      }
+    }
+
+    return {a, b};
   }
 };
