1fae5c1459dcf0ea257c0eecd96ed2b071e4c93a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-31 12:56:38 +0700

    test(problem-1255): add support to test in C language

---
+++ old-problems/1255/c/solution.c
@@ -0,0 +1,9 @@
+int maxScoreWords(char** words, int wordsSize, char* letters, int lettersSize, int* score, int scoreSize) {
+  (void)words;
+  (void)wordsSize;
+  (void)letters;
+  (void)lettersSize;
+  (void)score;
+  (void)scoreSize;
+  return 0;
+}
