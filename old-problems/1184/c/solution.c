618ab8e33d0c1119b045e450ba5ccf9766f1d04e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-31 13:20:45 +0700

    feat(problem-1184/c): solve by iterating list of distances

--- old-problems/1184/c/solution.c
+++ old-problems/1184/c/solution.c
@@ -1,7 +1,17 @@
 int distanceBetweenBusStops(int* distance, int distanceSize, int start, int destination) {
-  (void)distance;
-  (void)distanceSize;
-  (void)start;
-  (void)destination;
-  return 0;
+  if (start == destination) return 0;
+  if (destination < start) {
+    const int temp = destination;
+    destination = start;
+    start = temp;
+  }
+
+  int forwardDistance = 0;
+  for (int i = start; i < destination; ++i) forwardDistance += distance[i];
+
+  int backwardDistance = 0;
+  for (int i = destination; i < distanceSize; ++i) backwardDistance += distance[i];
+  for (int i = start - 1; i >= 0; --i) backwardDistance += distance[i];
+
+  return forwardDistance < backwardDistance ? forwardDistance : backwardDistance;
 }
