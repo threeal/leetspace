cd3ce239d7a25f0ce47fc8120f2c296817a747c1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-01 20:18:30 +0700

    feat(problem-3110/cpp): solve by using iteration

--- old-problems/3110/solution.cpp
+++ old-problems/3110/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   int scoreOfString(std::string s) {
-    return s.size();
+    int score{0};
+    for (std::size_t i{s.size() - 1}; i > 0; --i) {
+      score += std::abs(s[i] - s[i - 1]);
+    }
+    return score;
   }
 };
