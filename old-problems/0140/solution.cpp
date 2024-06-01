ca5331dc3160f9f895932fd958c50069d5f10488
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-01 20:29:21 +0700

    test: add test for problem 140. Word Break II

---
+++ old-problems/0140/solution.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::string> wordBreak(std::string /*s*/, std::vector<std::string>& /*wordDict*/) {
+    return {};
+  }
+};
