0d68cb904ae5d51eb64371a76fa5e661ace859ef
Rejwan Azam Mondal <rejwan.azam.mondal321@gmail.com>
2024-06-02 16:11:39 +0530

    Create interface.cpp
    Signed-off-by: Rejwan Azam Mondal <rejwan.azam.mondal321@gmail.com>

---
+++ old-problems/0078/c/interface.cpp
@@ -0,0 +1,8 @@
+#include<vector>
+
+extern "C"{
+int** subsets (int* nums,int numSize, int* returnSize, int** returnColumnSizes);
+}
+
+std::vector<std::vector<int>> solution_c(std::vector<int> nums, int numSize){
+}
