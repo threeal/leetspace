1be35a4f930b32b13fd421ec53943d941a2d8c19
Rejwan Azam Mondal <rejwan.azam.mondal321@gmail.com>
2024-06-03 02:32:32 +0530

    Update interface.cpp
    Signed-off-by: Rejwan Azam Mondal <rejwan.azam.mondal321@gmail.com>

--- old-problems/0078/c/interface.cpp
+++ old-problems/0078/c/interface.cpp
@@ -1,3 +1,4 @@
+#include<cstring>
 #include<vector>
 
 extern "C"{
@@ -5,4 +6,18 @@ int** subsets (int* nums,int numSize, int* returnSize, int** returnColumnSizes);
 }
 
 std::vector<std::vector<int>> solution_c(std::vector<int> nums, int numSize){
+  std::vector<int*> numsDatas(nums.size());
+  std::vector<int> numsSizes(nums.size());
+  for(std::size_t i{0}; i<grid.size(); ++i){
+    numsDatas[i] = nums[i].data();
+    numsSizes[i] = nums[i].size();
+  }
+  
+  int returnSize{};
+  const int* returnData{subsets(numsDatas.data() ,numsDatas.size(), &returnSize, numsSize.data())};
+  
+  std::vector<int> output(returnSize);
+  std::memcpy(output.data(), returnData, returnSize * sizeof(int));
+
+  return output;
 }
