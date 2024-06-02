9ae87841a0b1caea04446d51a11c56fa2b1d8121
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-02 14:45:12 +0700

    feat(problem-303/cpp): solve by precalculating sum

--- old-problems/0303/solution.cpp
+++ old-problems/0303/solution.cpp
@@ -1,10 +1,17 @@
 #include <vector>
 
 class NumArray {
+ private:
+  std::vector<int> sums;
+
  public:
-  NumArray(std::vector<int>& /*nums*/) {}
+  NumArray(std::vector<int>& nums) : sums{nums} {
+    for (std::size_t i{1}; i < sums.size(); ++i) {
+      sums[i] += sums[i - 1];
+    }
+  }
 
   int sumRange(int left, int right) {
-    return left + right;
+    return left > 0 ? sums[right] - sums[left - 1] : sums[right];
   }
 };
