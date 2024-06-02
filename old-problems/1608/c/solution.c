898df45adf55b0e52ada831a12090aa3f901914f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-02 22:00:13 +0700

    feat(problem-1608/c): solve by using sorting and iteration

--- old-problems/1608/c/solution.c
+++ old-problems/1608/c/solution.c
@@ -1,4 +1,18 @@
+#include <stdlib.h>
+
+int comp(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int specialArray(int* nums, int numsSize) {
-  (void)nums;
-  return numsSize;
+  qsort(nums, numsSize, sizeof(int), comp);
+
+  if (nums[0] >= numsSize) return numsSize;
+
+  for (int i = 1; i < numsSize; ++i) {
+    const int x = numsSize - i;
+    if (nums[i] >= x && nums[i - 1] < x) return x;
+  }
+
+  return -1;
 }
