80b248fda6f91473ddf0d629a3f48a88bf15770c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-03 12:19:00 +0700

    feat(problem-2486/cpp): solve by using iteration

--- old-problems/2486/solution.cpp
+++ old-problems/2486/solution.cpp
@@ -2,7 +2,11 @@
 
 class Solution {
  public:
-  int appendCharacters(std::string /*s*/, std::string /*t*/) {
-    return 0;
+  int appendCharacters(std::string s, std::string t) {
+    std::size_t ti{0};
+    for (std::size_t si{0}; si < s.size(); ++si) {
+      if (s[si] == t[ti]) ++ti;
+    }
+    return t.size() - ti;
   }
 };
