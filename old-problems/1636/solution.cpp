d0f824aad3db4d3a06035e24f279c976d516d152
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-03 14:01:54 +0700

    feat(problem-1636/cpp): solve by sorting by frequencies

--- old-problems/1636/solution.cpp
+++ old-problems/1636/solution.cpp
@@ -1,8 +1,21 @@
+#include <cstdint>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> frequencySort(std::vector<int>& nums) {
+    std::unordered_map<int, std::int8_t> frequencies{};
+    for (const auto num : nums) {
+      ++frequencies[num];
+    }
+
+    std::sort(nums.begin(), nums.end(), [&](const int a, const int b) {
+      const auto freqa = frequencies[a];
+      const auto freqb = frequencies[b];
+      return freqa == freqb ? a > b : freqa < freqb;
+    });
+
     return nums;
   }
 };
