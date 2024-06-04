068fc26b524843e53429a9622591a3d4155cfd85
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-04 12:48:55 +0700

    feat(problem-409/cpp): solve by counting duplicate characters

--- old-problems/0409/solution.cpp
+++ old-problems/0409/solution.cpp
@@ -1,8 +1,22 @@
 #include <string>
+#include <vector>
 
 class Solution {
  public:
   int longestPalindrome(std::string s) {
-    return s.size();
+    std::vector<bool> exists('z' + 1, false);
+
+    int length{0};
+    for (const auto c : s) {
+      if (exists[c]) {
+        exists[c] = false;
+        length += 2;
+      } else {
+        exists[c] = true;
+      }
+    }
+
+    if (length < static_cast<int>(s.size())) ++length;
+    return length;
   }
 };
