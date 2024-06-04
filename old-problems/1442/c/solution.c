b4881a6fd90f1ebbb157bb2a02e58bfe7fdbd833
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-04 19:36:24 +0700

    feat(problem-1442/c): solve by using for loop

--- old-problems/1442/c/solution.c
+++ old-problems/1442/c/solution.c
@@ -1,5 +1,11 @@
 int countTriplets(int* arr, int arrSize) {
-  (void)arr;
-  (void)arrSize;
-  return 0;
+  int count = 0;
+  for (int k = arrSize - 1; k > 0; --k) {
+    int sum = arr[k];
+    for (int i = k - 1; i >= 0; --i) {
+      sum ^= arr[i];
+      if (sum == 0) count += k - i;
+    }
+  }
+  return count;
 }
