e5dda8bad403ae28ee1c3e619fdaecf7ecef81e4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-05 13:09:58 +0700

    feat(problem-1404/c): solve by using iteration

--- old-problems/1404/c/solution.c
+++ old-problems/1404/c/solution.c
@@ -1,4 +1,22 @@
+#include <string.h>
+
 int numSteps(char* s) {
-  (void)s;
-  return 0;
+  int steps = 0;
+  int i = strlen(s) - 1;
+
+  while (i > 0 && s[i] == '0') {
+    --i;
+    ++steps;
+  }
+
+  while (i > 0) {
+    steps += 2;
+    --i;
+    while (i >= 0 && s[i] == '1') {
+      --i;
+      ++steps;
+    }
+  }
+
+  return steps;
 }
