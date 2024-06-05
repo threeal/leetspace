2ee2ed20833c50b0dc5ce25c073177bbd3060c82
Rejwan Azam Mondal <rejwan.azam.mondal321@gmail.com>
2024-06-05 11:05:43 +0530

    Update interface.cpp
    Signed-off-by: Rejwan Azam Mondal <rejwan.azam.mondal321@gmail.com>

--- old-problems/0078/c/interface.cpp
+++ old-problems/0078/c/interface.cpp
@@ -1,23 +1,19 @@
 #include<cstring>
 #include<vector>
+#include<cstdlib>
 
 extern "C"{
 int** subsets (int* nums,int numSize, int* returnSize, int** returnColumnSizes);
 }
 
 std::vector<std::vector<int>> solution_c(std::vector<int> nums, int numSize){
-  std::vector<int*> numsDatas(nums.size());
-  std::vector<int> numsSizes(nums.size());
-  for(std::size_t i{0}; i<grid.size(); ++i){
-    numsDatas[i] = nums[i].data();
-    numsSizes[i] = nums[i].size();
-  }
+
+  int returnSize;
+  int* returnColumnSizes;
+  const auto returnData = subsets(nums.data() ,nums.size(), &returnSize, &returnColumnSizes);
   
-  int returnSize{};
-  const int* returnData{subsets(numsDatas.data() ,numsDatas.size(), &returnSize, numsSize.data())};
-  
-  std::vector<int> output(returnSize);
-  std::memcpy(output.data(), returnData, returnSize * sizeof(int));
+   std::vector<std::vector<int>> output(returnSize);
+   std::memcpy(output.data(), returnData, returnSize * sizeof(int));
 
   return output;
 }
