720267bb653ee61cc6c6c23d23be60f3d51cd6a0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-05 20:26:39 +0700

    feat(problem-151/cpp): solve by reversing each word and whole string

--- old-problems/0151/solution.cpp
+++ old-problems/0151/solution.cpp
@@ -1,8 +1,36 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   std::string reverseWords(std::string s) {
+    auto right = s.begin();
+    auto it = right;
+
+    while (it != s.end() && *it == ' ') ++it;
+
+    while (it != s.end()) {
+      const auto left = right;
+      while (it != s.end() && *it != ' ') {
+        *right = *it;
+        ++right;
+        ++it;
+      }
+
+      std::reverse(left, right);
+
+      while (it != s.end() && *it == ' ') ++it;
+
+      if (it != s.end()) {
+        *right = ' ';
+        ++right;
+      }
+    }
+
+    s.resize(std::distance(s.begin(), right));
+
+    std::reverse(s.begin(), s.end());
+
     return s;
   }
 };
