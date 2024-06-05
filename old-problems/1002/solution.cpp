b8ab870a8d83fa02a6fdbcb57678f497b1d70a2f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-05 12:15:24 +0700

    feat(problem-1002/cpp): solve by counting character frequencies

--- old-problems/1002/solution.cpp
+++ old-problems/1002/solution.cpp
@@ -1,9 +1,37 @@
+#include <algorithm>
+#include <cstdint>
+#include <limits>
+#include <numeric>
 #include <string>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::string> commonChars(std::vector<std::string>& words) {
-    return words;
+    std::vector<std::int8_t> allCounts(26, std::numeric_limits<std::int8_t>::max());
+    std::vector<std::int8_t> counts(26, 0);
+
+    for (const auto& word : words) {
+      std::fill(counts.begin(), counts.end(), 0);
+      for (const auto c : word) {
+        ++counts[c - 'a'];
+      }
+
+      for (std::int8_t i{25}; i >= 0; --i) {
+        if (allCounts[i] > counts[i]) allCounts[i] = counts[i];
+      }
+    }
+
+    std::vector<std::string> chars(std::accumulate(allCounts.begin(), allCounts.end(), 0), "a");
+    auto it = chars.rbegin();
+    for (std::int8_t i{25}; i >= 0; --i) {
+      while (allCounts[i] > 0) {
+        --allCounts[i];
+        (*it)[0] += i;
+        ++it;
+      }
+    }
+
+    return chars;
   }
 };
