f32a984fb933d359712021f81f73352768336033
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-06 15:02:57 +0700

    feat(problem-1170/cpp): solve by sorting frequencies and binary search

--- old-problems/1170/solution.cpp
+++ old-problems/1170/solution.cpp
@@ -1,9 +1,40 @@
+#include <algorithm>
 #include <string>
+#include <string_view>
 #include <vector>
 
 class Solution {
  public:
-  std::vector<int> numSmallerByFrequency(std::vector<std::string>& queries, std::vector<std::string>& /*words*/) {
-    return std::vector<int>(queries.size(), 0);
+  std::vector<int> numSmallerByFrequency(std::vector<std::string>& queries, std::vector<std::string>& words) {
+    std::vector<int> frequencies(words.size());
+    for (int i = words.size() - 1; i >= 0; --i) {
+      frequencies[i] = calculateFrequency(words[i]);
+    }
+    std::sort(frequencies.begin(), frequencies.end());
+
+    std::vector<int> output(queries.size());
+    for (int i = queries.size() - 1; i >= 0; --i) {
+      const int frequency{calculateFrequency(queries[i])};
+      const auto it = std::upper_bound(frequencies.begin(), frequencies.end(), frequency);
+      output[i] = std::distance(it, frequencies.end());
+    }
+
+    return output;
+  }
+
+  int calculateFrequency(std::string_view v) {
+    char c = v[0];
+    int frequency = 1;
+    for (int i = v.size() - 1; i > 0; --i) {
+      if (v[i] <= c) {
+        if (v[i] == c) {
+          ++frequency;
+        } else {
+          c = v[i];
+          frequency = 1;
+        }
+      }
+    }
+    return frequency;
   }
 };
