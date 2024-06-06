3e497be25ef57f0f41ed89379dcebee6095f4361
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-06 14:17:53 +0700

    feat(problem-3110/c): solve by using iteration

--- old-problems/3110/c/solution.c
+++ old-problems/3110/c/solution.c
@@ -1,4 +1,10 @@
+#include <stdlib.h>
+
 int scoreOfString(char* s) {
-  (void)s;
-  return 0;
+  int score = 0;
+  while (s[1] != 0) {
+    score += abs(s[0] - s[1]);
+    ++s;
+  }
+  return score;
 }
