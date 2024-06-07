3fa712fd0e81e5b7980dcf85255ba81a4433959e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-07 10:56:23 +0700

    test: add test for problem 648. Replace Words

---
+++ old-problems/0648/solution.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::string replaceWords(std::vector<std::string>& /*dictionary*/, std::string sentence) {
+    return sentence;
+  }
+};
