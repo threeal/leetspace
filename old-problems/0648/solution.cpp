0d7c238b02f5eb16f35bac76cda0873d3b279caf
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-07 11:34:00 +0700

    feat(problem-648/cpp): solve by comparing dictionary for each word

--- old-problems/0648/solution.cpp
+++ old-problems/0648/solution.cpp
@@ -1,9 +1,45 @@
+#include <algorithm>
 #include <string>
 #include <vector>
 
 class Solution {
  public:
-  std::string replaceWords(std::vector<std::string>& /*dictionary*/, std::string sentence) {
-    return sentence;
+  std::string replaceWords(std::vector<std::string>& dictionary, std::string sentence) {
+    std::sort(dictionary.begin(), dictionary.end());
+
+    std::string output{""};
+    output.reserve(sentence.size());
+
+    std::string_view view(sentence);
+    std::size_t left{0};
+    std::size_t right{view.find(' ', left)};
+
+    while (right != std::string::npos) {
+      std::string_view subview{view.substr(left, right - left)};
+      const auto it = std::find_if(dictionary.begin(), dictionary.end(), [&](auto str) {
+        return subview.starts_with(str);
+      });
+      if (it == dictionary.end()) {
+        output += subview;
+      } else {
+        output += *it;
+      }
+      output += " ";
+
+      left = right + 1;
+      right = view.find(' ', left);
+    }
+
+    std::string_view subview{view.substr(left)};
+    const auto it = std::find_if(dictionary.begin(), dictionary.end(), [&](auto str) {
+      return subview.starts_with(str);
+    });
+    if (it == dictionary.end()) {
+      output += subview;
+    } else {
+      output += *it;
+    }
+
+    return output;
   }
 };
