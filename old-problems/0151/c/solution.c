3c119b98f4e9caf8484c9ec1ae3ca8685bf1e524
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-07 13:20:18 +0700

    feat(problem-151/c): solve by reversing each word and whole string

--- old-problems/0151/c/solution.c
+++ old-problems/0151/c/solution.c
@@ -1,3 +1,38 @@
+void strrev(char* left, char* right) {
+  while (left < right) {
+    char c = *left;
+    *left = *right;
+    *right = c;
+    ++left;
+    --right;
+  }
+}
+
 char* reverseWords(char* s) {
+  char* right = s;
+  char* it = right;
+
+  while (*it == ' ') ++it;
+
+  while (*it != 0) {
+    char* left = right;
+    while (*it != 0 && *it != ' ') {
+      *right = *it;
+      ++right;
+      ++it;
+    }
+
+    strrev(left, right - 1);
+
+    while (*it == ' ') ++it;
+
+    if (*it != 0) {
+      *right = ' ';
+      ++right;
+    }
+  }
+
+  *right = 0;
+  strrev(s, right - 1);
   return s;
 }
