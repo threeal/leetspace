2260504420758fbca85ff06e51df724d380e1a3d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-07 20:54:16 +0700

    feat(problem-1636/c): solve by sorting by frequencies

--- old-problems/1636/c/solution.c
+++ old-problems/1636/c/solution.c
@@ -1,4 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+
+int frequencies[201];
+int* frequenciesCenter = frequencies + 100;
+
+int comp(const void* a, const void* b) {
+  const int freqa = frequenciesCenter[*(int*)a];
+  const int freqb = frequenciesCenter[*(int*)b];
+  return freqa == freqb ? *(int*)b - *(int*)a : freqa - freqb;
+}
+
 int* frequencySort(int* nums, int numsSize, int* returnSize) {
+  memset(frequencies, 0, 201 * sizeof(int));
+
+  for (int i = numsSize - 1; i >= 0; --i) {
+    ++frequenciesCenter[nums[i]];
+  }
+
+  qsort(nums, numsSize, sizeof(int), comp);
+
   *returnSize = numsSize;
   return nums;
 }
