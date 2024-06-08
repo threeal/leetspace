d1afe64a984013218055f1c2621ee337ea6faaa6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-08 19:25:46 +0700

    feat(problem-2124/cpp): solve by iterating backward

--- old-problems/2124/solution.cpp
+++ old-problems/2124/solution.cpp
@@ -3,6 +3,9 @@
 class Solution {
  public:
   bool checkString(std::string s) {
-    return s.empty();
+    int i = s.size() - 1;
+    while (i >= 0 && s[i] == 'b') --i;
+    while (i >= 0 && s[i] == 'a') --i;
+    return i < 0;
   }
 };
