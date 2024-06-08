a8e41818280679f88b494016e9178d1ec1b6bc74
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-08 10:45:42 +0700

    feat(problem-523/cpp): solve by iterating sum of subarrays

--- old-problems/0523/solution.cpp
+++ old-problems/0523/solution.cpp
@@ -2,7 +2,20 @@
 
 class Solution {
  public:
-  bool checkSubarraySum(std::vector<int>& /*nums*/, int /*k*/) {
+  bool checkSubarraySum(std::vector<int>& nums, int k) {
+    int sum{nums[nums.size() - 1]};
+    for (int i = nums.size() - 2; i >= 0; --i) {
+      if (nums[i] == 0 && nums[i] == nums[i + 1]) return true;
+
+      sum += nums[i];
+      if (sum % k == 0) return true;
+
+      int tempSum{sum};
+      for (int j = nums.size() - 1; j > i + 1 && tempSum > k; --j) {
+        tempSum -= nums[j];
+        if (tempSum % k == 0) return true;
+      }
+    }
     return false;
   }
 };
