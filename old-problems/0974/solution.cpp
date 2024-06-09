c15666b71eda8aee635709aa64804fea141cec05
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-09 18:15:24 +0700

    feat(problem-974/cpp): solve by storing modulo

--- old-problems/0974/solution.cpp
+++ old-problems/0974/solution.cpp
@@ -2,7 +2,22 @@
 
 class Solution {
  public:
-  int subarraysDivByK(std::vector<int>& nums, int /*k*/) {
-    return nums.size();
+  int subarraysDivByK(std::vector<int>& nums, int k) {
+    int count{0};
+    std::vector<int> counts(k, 0);
+    counts[0] = 1;
+
+    int sum{0};
+    for (const auto num : nums) {
+      sum += num;
+
+      int mod{sum % k};
+      if (mod < 0) mod += k;
+
+      count += counts[mod];
+      ++counts[mod];
+    }
+
+    return count;
   }
 };
