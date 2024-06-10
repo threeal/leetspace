14f3f3ecdf1e2a6664d7e04f585a765cf6c4632e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-10 13:24:46 +0700

    docs(proble-1770/cpp): add a comment that describe the solution

--- old-problems/1770/solution.cpp
+++ old-problems/1770/solution.cpp
@@ -1,17 +1,39 @@
+// In this solution, we calculate backward, starting from the last
+// multiplication and working towards the first.
+//
+// Since we're calculating backward, we only need a one-dimensional array to
+// store the previous multiplication results, which reduces memory usage.
+//
+// In each multiplication calculation, we determine the left and right indices
+// of the subarray we're going to consider and update the current calculation
+// with the maximum value of either the left or right value multiplied by the
+// current multiplier, combined with the corresponding multiplication from the
+// previous calculation.
+
 #include <cstdlib>
 #include <vector>
 
 class Solution {
  public:
   int maximumScore(std::vector<int>& nums, std::vector<int>& multipliers) {
+    // We only need a one-dimensional array to store the memoization.
+    // The extra element at the end (+ 1) is for handling the multiplication on
+    // the first calculation.
     std::vector<int> memo(multipliers.size() + 1, 0);
+
+    // Do calculation from the last multiplication to the first.
     for (int i = multipliers.size() - 1; i >= 0; --i) {
-      for (int left = 0, right = nums.size() - i - 1; left <= i; ++left, ++right) {
-        memo[left] = std::max(
-            nums[left] * multipliers[i] + memo[left + 1],
-            nums[right] * multipliers[i] + memo[left]);
+      // Determine the left and right indices of the subarray.
+      for (int l = 0, r = nums.size() - i - 1; l <= i; ++l, ++r) {
+        // Update the memoization with the maximum value considering both left
+        // and right options.
+        memo[l] = std::max(
+            nums[l] * multipliers[i] + memo[l + 1],
+            nums[r] * multipliers[i] + memo[l]);
       }
     }
+
+    // Return the result of the last calculation.
     return memo[0];
   }
 };
