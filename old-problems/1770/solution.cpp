2d9a5291ec64beeb704a5619056e559d613b7f83
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-10 13:05:41 +0700

    feat(problem-1770/cpp): solve by using for loop with memoization

--- old-problems/1770/solution.cpp
+++ old-problems/1770/solution.cpp
@@ -1,8 +1,17 @@
+#include <cstdlib>
 #include <vector>
 
 class Solution {
  public:
-  int maximumScore(std::vector<int>& /*nums*/, std::vector<int>& /*multipliers*/) {
-    return 0;
+  int maximumScore(std::vector<int>& nums, std::vector<int>& multipliers) {
+    std::vector<int> memo(multipliers.size() + 1, 0);
+    for (int i = multipliers.size() - 1; i >= 0; --i) {
+      for (int left = 0, right = nums.size() - i - 1; left <= i; ++left, ++right) {
+        memo[left] = std::max(
+            nums[left] * multipliers[i] + memo[left + 1],
+            nums[right] * multipliers[i] + memo[left]);
+      }
+    }
+    return memo[0];
   }
 };
