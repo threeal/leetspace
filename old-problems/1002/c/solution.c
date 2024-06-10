8c8900c73d8178db0cf80c3a7f7e89ef01d398c4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-10 20:15:27 +0700

    feat(problem-1002/c): solve by counting character frequencies

--- old-problems/1002/c/solution.c
+++ old-problems/1002/c/solution.c
@@ -1,4 +1,41 @@
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
 char** commonChars(char** words, int wordsSize, int* returnSize) {
-  *returnSize = wordsSize;
-  return words;
+  int8_t allCounts[26];
+  memset(allCounts, CHAR_MAX, 26 * sizeof(int8_t));
+
+  int8_t counts[26];
+
+  while (--wordsSize >= 0) {
+    memset(counts, 0, 26 * sizeof(int8_t));
+    for (char* c = words[wordsSize]; *c != 0; ++c) {
+      ++counts[*c - 'a'];
+    }
+
+    for (int8_t i = 25; i >= 0; --i) {
+      if (allCounts[i] > counts[i]) allCounts[i] = counts[i];
+    }
+  }
+
+  *returnSize = 0;
+  for (int8_t i = 25; i >= 0; --i) *returnSize += allCounts[i];
+
+  char** chars = malloc((*returnSize) * sizeof(char*));
+  for (int i = (*returnSize) - 1; i >= 0; --i) {
+    chars[i] = malloc(2 * sizeof(char));
+  }
+
+  char** it = chars + (*returnSize) - 1;
+  for (int8_t i = 25; i >= 0; --i) {
+    while (--allCounts[i] >= 0) {
+      (*it)[0] = 'a' + i;
+      (*it)[1] = 0;
+      --it;
+    }
+  }
+
+  return chars;
 }
