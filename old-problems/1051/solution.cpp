021cad9b3fdd9ada598dc3262d48dfbb02e600e7
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-10 12:02:00 +0700

    feat(problem-1051/cpp): solve by comparing with sorted array

--- old-problems/1051/solution.cpp
+++ old-problems/1051/solution.cpp
@@ -1,8 +1,17 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int heightChecker(std::vector<int>& heights) {
-    return heights.size();
+    std::vector<int> sortedHeights{heights};
+    std::sort(sortedHeights.begin(), sortedHeights.end());
+
+    int diff{0};
+    for (int i = heights.size() - 1; i >= 0; --i) {
+      if (heights[i] != sortedHeights[i]) ++diff;
+    }
+
+    return diff;
   }
 };
