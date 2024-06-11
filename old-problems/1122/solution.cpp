1d8486f7d217b00a618bbf3c62a365f079767840
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-11 10:47:54 +0700

    feat(problem-1122/cpp): solve by using custom sorting

--- old-problems/1122/solution.cpp
+++ old-problems/1122/solution.cpp
@@ -1,8 +1,33 @@
+#include <algorithm>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
-  std::vector<int> relativeSortArray(std::vector<int>& arr1, std::vector<int>& /*arr2*/) {
+  std::vector<int> relativeSortArray(std::vector<int>& arr1, std::vector<int>& arr2) {
+    std::unordered_map<int, int> order{};
+    for (int i = arr2.size() - 1; i >= 0; --i) {
+      order.emplace(arr2[i], i);
+    }
+
+    std::sort(arr1.begin(), arr1.end(), [&order](const int a, const int b) {
+      const auto ai = order.find(a);
+      const auto bi = order.find(b);
+      if (ai == order.end()) {
+        if (bi == order.end()) {
+          return a < b;
+        } else {
+          return false;
+        }
+      } else {
+        if (bi == order.end()) {
+          return true;
+        } else {
+          return ai->second < bi->second;
+        }
+      }
+    });
+
     return arr1;
   }
 };
