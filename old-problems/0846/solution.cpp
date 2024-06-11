797a4fe1c90564ef86e09f8ce27de90fffb8d967
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-11 17:06:26 +0700

    feat(problem-846/cpp): solve by using sorting and binary search

--- old-problems/0846/solution.cpp
+++ old-problems/0846/solution.cpp
@@ -1,8 +1,30 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   bool isNStraightHand(std::vector<int>& hand, int groupSize) {
-    return hand.size() % groupSize == 0;
+    if (hand.size() % groupSize != 0) return false;
+
+    std::sort(hand.begin(), hand.end());
+
+    for (std::size_t i{0}; i < hand.size(); i += groupSize) {
+      auto begin = hand.begin() + i;
+      int val{*begin + 1};
+      *begin = -1;
+
+      auto it = begin + 1;
+      for (int i{groupSize - 1}; i > 0; --i) {
+        it = std::lower_bound(it, hand.end(), val);
+        if (it == hand.end() || *it != val) return false;
+        *it = -1;
+        ++it;
+        ++val;
+      }
+
+      std::sort(begin, it);
+    }
+
+    return true;
   }
 };
