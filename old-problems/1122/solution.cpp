1025ba8017093653363d5db2d8b23becbf7e3a83
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-11 10:53:22 +0700

    perf(problem-1122/cpp): replace hash map with large array

--- old-problems/1122/solution.cpp
+++ old-problems/1122/solution.cpp
@@ -1,29 +1,27 @@
 #include <algorithm>
-#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> relativeSortArray(std::vector<int>& arr1, std::vector<int>& arr2) {
-    std::unordered_map<int, int> order{};
+    std::vector<int> order(1001, -1);
     for (int i = arr2.size() - 1; i >= 0; --i) {
-      order.emplace(arr2[i], i);
+      order[arr2[i]] = i;
     }
 
-    std::sort(arr1.begin(), arr1.end(), [&order](const int a, const int b) {
-      const auto ai = order.find(a);
-      const auto bi = order.find(b);
-      if (ai == order.end()) {
-        if (bi == order.end()) {
+    const auto orderData = order.data();
+    std::sort(arr1.begin(), arr1.end(), [orderData](const int a, const int b) {
+      if (orderData[a] < 0) {
+        if (orderData[b] < 0) {
           return a < b;
         } else {
           return false;
         }
       } else {
-        if (bi == order.end()) {
+        if (orderData[b] < 0) {
           return true;
         } else {
-          return ai->second < bi->second;
+          return orderData[a] < orderData[b];
         }
       }
     });
