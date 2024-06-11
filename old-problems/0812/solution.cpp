8414fdf759ed06feb276f07552a887c456022d19
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-11 21:26:14 +0700

    feat(problem-812/cpp): solve by using shoelace formula in each points

--- old-problems/0812/solution.cpp
+++ old-problems/0812/solution.cpp
@@ -2,7 +2,20 @@
 
 class Solution {
  public:
-  double largestTriangleArea(std::vector<std::vector<int>>& /*points*/) {
-    return 0.0;
+  double largestTriangleArea(std::vector<std::vector<int>>& points) {
+    int largestArea{0};
+    for (int i = points.size() - 1; i >= 2; --i) {
+      for (int j = i - 1; j >= 1; --j) {
+        for (int k = j - 1; k >= 0; --k) {
+          int area{
+              points[i][0] * (points[j][1] - points[k][1]) +
+              points[j][0] * (points[k][1] - points[i][1]) +
+              points[k][0] * (points[i][1] - points[j][1])};
+          if (area < 0) area = -area;
+          if (area > largestArea) largestArea = area;
+        }
+      }
+    }
+    return largestArea * 0.5;
   }
 };
