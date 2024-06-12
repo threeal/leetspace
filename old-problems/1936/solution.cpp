5b4c86fe04a4db50522f11c35872211e2b12108c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-12 15:56:41 +0700

    feat(problem-1936/cpp): solve by comparing adjacent elements

--- old-problems/1936/solution.cpp
+++ old-problems/1936/solution.cpp
@@ -2,7 +2,11 @@
 
 class Solution {
  public:
-  int addRungs(std::vector<int>& /*rungs*/, int /*dist*/) {
-    return 0;
+  int addRungs(std::vector<int>& rungs, int dist) {
+    int extraRungs{(rungs[0] - 1) / dist};
+    for (std::size_t i{rungs.size() - 1}; i > 0; --i) {
+      extraRungs += (rungs[i] - rungs[i - 1] - 1) / dist;
+    }
+    return extraRungs;
   }
 };
