147f1249d7b85493ac1827a4e85c0b4c2b8ae63c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-13 11:38:30 +0700

    feat(problem-2037/cpp): solve by comparing elements after sorting

--- old-problems/2037/solution.cpp
+++ old-problems/2037/solution.cpp
@@ -1,8 +1,18 @@
+#include <algorithm>
+#include <cstdlib>
 #include <vector>
 
 class Solution {
  public:
-  int minMovesToSeat(std::vector<int>& /*seats*/, std::vector<int>& /*students*/) {
-    return 0;
+  int minMovesToSeat(std::vector<int>& seats, std::vector<int>& students) {
+    std::sort(seats.begin(), seats.end());
+    std::sort(students.begin(), students.end());
+
+    int moves{0};
+    for (int i = seats.size() - 1; i >= 0; --i) {
+      moves += std::abs(seats[i] - students[i]);
+    }
+
+    return moves;
   }
 };
