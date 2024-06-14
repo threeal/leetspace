2be0627389c92214872bce367b9de62804d32c60
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-14 13:35:28 +0700

    perf(problem-945/cpp): shift all numbers instead of storing availables

--- old-problems/0945/solution.cpp
+++ old-problems/0945/solution.cpp
@@ -1,25 +1,19 @@
 #include <algorithm>
-#include <limits>
-#include <stack>
-#include <tuple>
 #include <vector>
 
 class Solution {
  public:
   int minIncrementForUnique(std::vector<int>& nums) {
-    std::sort(nums.begin(), nums.end());
-
-    std::stack<std::tuple<int, int>> availables{};
-    availables.push({nums.back() + 1, std::numeric_limits<int>::max()});
+    std::ranges::sort(nums);
 
     int moves{0};
-    for (int i = nums.size() - 2; i >= 0; --i) {
-      if (nums[i] == nums[i + 1]) {
-        auto& [min, max] = availables.top();
-        moves += min - nums[i];
-        if (++min > max) availables.pop();
-      } else if (nums[i] + 1 < nums[i + 1]) {
-        availables.push({nums[i] + 1, nums[i + 1] - 1});
+    int minAvailable{0};
+    for (const auto num : nums) {
+      if (minAvailable > num) {
+        moves += minAvailable - num;
+        ++minAvailable;
+      } else {
+        minAvailable = num + 1;
       }
     }
 
