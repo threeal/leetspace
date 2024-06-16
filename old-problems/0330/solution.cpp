8690a1447087bc5d9b4b0e74402f9bdb0d5a9dde
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-16 20:56:02 +0700

    feat(problem-330/cpp): solve by using greedy approach

--- old-problems/0330/solution.cpp
+++ old-problems/0330/solution.cpp
@@ -2,7 +2,21 @@
 
 class Solution {
  public:
-  int minPatches(std::vector<int>& /*nums*/, int /*n*/) {
-    return 0;
+  int minPatches(std::vector<int>& nums, int n) {
+    unsigned int unavailable{1};
+    int patches{0};
+
+    auto it = nums.begin();
+    while (unavailable <= static_cast<unsigned int>(n)) {
+      if (it != nums.end() && static_cast<unsigned int>(*it) <= unavailable) {
+        unavailable += *it;
+        ++it;
+      } else {
+        unavailable += unavailable;
+        ++patches;
+      }
+    }
+
+    return patches;
   }
 };
