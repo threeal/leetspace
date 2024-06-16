409e3e92a5c2a0e66e7c192fbaf4ea3c106a6038
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-16 22:31:37 +0700

    feat(problem-203/c): solve by using while loop

--- old-problems/0263/c/solution.c
+++ old-problems/0263/c/solution.c
@@ -1,5 +1,9 @@
 #include <stdbool.h>
 
 bool isUgly(int n) {
-  return n == 0;
+  if (n <= 0) return false;
+  while (n % 2 == 0) n /= 2;
+  while (n % 3 == 0) n /= 3;
+  while (n % 5 == 0) n /= 5;
+  return n == 1;
 }
