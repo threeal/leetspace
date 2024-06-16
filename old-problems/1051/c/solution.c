c217bf4e3fab6a655160f8eed40f73d46d53bc38
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-16 22:24:51 +0700

    feat(problem-1051/c): solve by comparing with sorted array

--- old-problems/1051/c/solution.c
+++ old-problems/1051/c/solution.c
@@ -1,5 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+
+int comp(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int heightChecker(int* heights, int heightsSize) {
-  (void)heights;
-  (void)heightsSize;
-  return 0;
+  int* sortedHeights = malloc(heightsSize * sizeof(int));
+  memcpy(sortedHeights, heights, heightsSize * sizeof(int));
+
+  qsort(sortedHeights, heightsSize, sizeof(int), comp);
+
+  int diff = 0;
+  for (int i = heightsSize - 1; i >= 0; --i) {
+    if (heights[i] != sortedHeights[i]) ++diff;
+  }
+
+  free(sortedHeights);
+
+  return diff;
 }
