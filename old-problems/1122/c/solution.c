c28f73c1c722e98db267e4b0bf1fca4e0b53bf63
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-16 23:01:33 +0700

    feat(problem-1122/c): solve by using custom sorting

--- old-problems/1122/c/solution.c
+++ old-problems/1122/c/solution.c
@@ -1,9 +1,33 @@
 #include <stdlib.h>
+#include <string.h>
+
+int order[1001];
+
+int comp(const void* a, const void* b) {
+  if (order[*(int*)a] < 0) {
+    if (order[*(int*)b] < 0) {
+      return *(int*)a - *(int*)b;
+    } else {
+      return 1;
+    }
+  } else {
+    if (order[*(int*)b] < 0) {
+      return -1;
+    } else {
+      return order[*(int*)a] - order[*(int*)b];
+    }
+  }
+}
 
 int* relativeSortArray(
     int* arr1, int arr1Size, int* arr2, int arr2Size, int* returnSize) {
-  (void)arr2;
-  (void)arr2Size;
+  memset(order, -1, 1001 * sizeof(int));
+  for (int i = arr2Size - 1; i >= 0; --i) {
+    order[arr2[i]] = i;
+  }
+
+  qsort(arr1, arr1Size, sizeof(int), comp);
+
   *returnSize = arr1Size;
   return arr1;
 }
