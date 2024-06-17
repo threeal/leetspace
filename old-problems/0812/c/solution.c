9c373abf38dd091930203ff5704600613c647a01
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-17 21:07:50 +0700

    feat(problem-812/c): solve by using shoelace formula in each points

--- old-problems/0812/c/solution.c
+++ old-problems/0812/c/solution.c
@@ -1,6 +1,17 @@
 double largestTriangleArea(int** points, int pointsSize, int* pointsColSize) {
-  (void)points;
-  (void)pointsSize;
   (void)pointsColSize;
-  return 0.0;
+  int largestArea = 0;
+  for (int i = pointsSize - 1; i >= 2; --i) {
+    for (int j = i - 1; j >= 1; --j) {
+      for (int k = j - 1; k >= 0; --k) {
+        int area =
+            points[i][0] * (points[j][1] - points[k][1]) +
+            points[j][0] * (points[k][1] - points[i][1]) +
+            points[k][0] * (points[i][1] - points[j][1]);
+        if (area < 0) area = -area;
+        if (area > largestArea) largestArea = area;
+      }
+    }
+  }
+  return largestArea * 0.5;
 }
