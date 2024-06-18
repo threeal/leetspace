4637e2c70d46635a293b93904ba369fa0a52d5d6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-18 12:13:23 +0700

    feat(problem-3007/cpp): solve by using binary search

--- old-problems/3007/solution.cpp
+++ old-problems/3007/solution.cpp
@@ -1,6 +1,27 @@
+#include <algorithm>
+
 class Solution {
  public:
-  long long findMaximumNumber(long long k, int /*x*/) {
-    return k;
+  long long findMaximumNumber(long long k, int x) {
+    long long low{0};
+    long long high{10000000000000000};
+
+    while (low < high) {
+      long long mid = low + (high - low + 1) / 2;
+
+      long long price{0};
+      for (long long i = 1 << (x - 1); i <= mid; i <<= x) {
+        price += (mid + 1) / (i + i) * i +
+            std::max(0LL, (mid + 1) % (i + i) - i);
+      }
+
+      if (price <= k) {
+        low = mid;
+      } else {
+        high = mid - 1;
+      }
+    }
+
+    return low;
   }
 };
