78ac0369d8476ecb40788be70497cb883f58fa7e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-18 21:22:39 +0700

    feat(problem-2037/c): solve by comparing elements after sorting

--- old-problems/2037/c/solution.c
+++ old-problems/2037/c/solution.c
@@ -1,8 +1,18 @@
+#include <stdlib.h>
+
+int comp(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int minMovesToSeat(
     int* seats, int seatsSize, int* students, int studentsSize) {
-  (void)seats;
-  (void)seatsSize;
-  (void)students;
-  (void)studentsSize;
-  return 0;
+  qsort(seats, seatsSize, sizeof(int), comp);
+  qsort(students, studentsSize, sizeof(int), comp);
+
+  int moves = 0;
+  for (int i = seatsSize - 1; i >= 0; --i) {
+    moves += abs(seats[i] - students[i]);
+  }
+
+  return moves;
 }
