a79fac210790bf89f713ee075ba18012bb440b06
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-20 14:09:44 +0700

    feat(problem-1552/cpp): solve by using binary search

--- old-problems/1552/solution.cpp
+++ old-problems/1552/solution.cpp
@@ -1,8 +1,31 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
-  int maxDistance(std::vector<int>& /*position*/, int /*m*/) {
-    return 0;
+  int maxDistance(std::vector<int>& position, int m) {
+    std::sort(position.begin(), position.end());
+
+    int low{1};
+    int high{(position.back() - position.front()) / (m - 1)};
+    while (low < high) {
+      const int mid{(low + high + 1) / 2};
+
+      int balls{1};
+      auto it = position.begin();
+      while (balls <= m) {
+        it = std::lower_bound(it, position.end(), *it + mid);
+        if (it == position.end()) break;
+        ++balls;
+      }
+
+      if (balls < m) {
+        high = mid - 1;
+      } else {
+        low = mid;
+      }
+    }
+
+    return high;
   }
 };
