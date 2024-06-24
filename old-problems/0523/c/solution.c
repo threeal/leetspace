df3d8a010853ba589c7cb6126f7419f8abc8a9bc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-24 10:31:21 +0700

    feat(problem-523/c): solve by iterating sum of subarrays

--- old-problems/0523/c/solution.c
+++ old-problems/0523/c/solution.c
@@ -1,8 +1,19 @@
 #include <stdbool.h>
 
 bool checkSubarraySum(int* nums, int numsSize, int k) {
-  (void)nums;
-  (void)numsSize;
-  (void)k;
+  int sum = nums[numsSize - 1];
+  for (int i = numsSize - 2; i >= 0; --i) {
+    if (nums[i] == 0 && nums[i] == nums[i + 1]) return true;
+
+    sum += nums[i];
+    if (sum % k == 0) return true;
+
+    int tempSum = sum;
+    for (int j = numsSize - 1; j > i + 1 && tempSum > k; --j) {
+      tempSum -= nums[j];
+      if (tempSum % k == 0) return true;
+    }
+  }
+
   return false;
 }
