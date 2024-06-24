7c04ded1a2fb06bb312facd3945f4a44eb7f9013
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-24 10:22:34 +0700

    feat(problem-995/cpp): solve by tracking flip positions

--- old-problems/0995/solution.cpp
+++ old-problems/0995/solution.cpp
@@ -3,6 +3,32 @@
 class Solution {
  public:
   int minKBitFlips(std::vector<int>& nums, int k) {
-    return nums.size() - k;
+    std::vector<bool> mustFlips(nums.size(), false);
+
+    int target{1};
+    int flip{0};
+    for (int i = nums.size() - 1; i >= k; --i) {
+      if (mustFlips[i]) target = target == 0 ? 1 : 0;
+      if (nums[i] != target) {
+        mustFlips[i - k] = true;
+        target = target == 0 ? 1 : 0;
+        ++flip;
+      }
+    }
+
+    if (mustFlips[k - 1]) target = target == 0 ? 1 : 0;
+    if (nums[k - 1] == target) {
+      for (int i{k - 2}; i >= 0; --i) {
+        if (mustFlips[i]) target = target == 0 ? 1 : 0;
+        if (nums[i] != target) return -1;
+      }
+      return flip;
+    } else {
+      for (int i{k - 2}; i >= 0; --i) {
+        if (mustFlips[i]) target = target == 0 ? 1 : 0;
+        if (nums[i] == target) return -1;
+      }
+      return flip + 1;
+    }
   }
 };
