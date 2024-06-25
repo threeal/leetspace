b785c48b9d3bc4ae7a1e5a4ea3e13c730ec48531
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-25 10:32:57 +0700

    feat(problem-1038/cpp): solve by using depth-first search approach

--- old-problems/1038/solution.cpp
+++ old-problems/1038/solution.cpp
@@ -1,6 +1,12 @@
 class Solution {
  public:
   TreeNode* bstToGst(TreeNode* root) {
+    sumOf(root, 0);
     return root;
+  }
+
+  int sumOf(TreeNode* node, int sum) {
+    node->val += node->right != nullptr ? sumOf(node->right, sum) : sum;
+    return node->left != nullptr ? sumOf(node->left, node->val) : node->val;
   }
 };
