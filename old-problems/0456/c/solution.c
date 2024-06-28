12029c0e9838273d6b1d338d4b53db9226e7bd70
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-28 16:42:16 +0700

    perf(problem-456/c): avoid extra memory for stack

--- old-problems/0456/c/solution.c
+++ old-problems/0456/c/solution.c
@@ -3,25 +3,19 @@
 #include <stdlib.h>
 
 bool find132pattern(int* nums, int numsSize) {
-  int* js = malloc((numsSize + 1) * sizeof(int));
-  int* j = js;
-  *j = INT_MAX;
+  int j = numsSize;
   int k = INT_MIN;
   for (int i = numsSize - 1; i >= 0; --i) {
-    if (nums[i] < k) {
-      free(js);
-      return true;
+    if (nums[i] < k) return true;
+
+    while (j < numsSize && nums[i] > nums[j]) {
+      k = nums[j];
+      ++j;
     }
 
-    while (nums[i] > *j) {
-      k = *j;
-      --j;
-    }
-
-    ++j;
-    *j = nums[i];
+    --j;
+    nums[j] = nums[i];
   }
 
-  free(js);
   return false;
 }
