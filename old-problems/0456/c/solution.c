ad5588b22b70c232618b92d915ae006b94bd3e36
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-28 14:10:11 +0700

    feat(problem-456/c): solve by using backward iteration and stack

--- old-problems/0456/c/solution.c
+++ old-problems/0456/c/solution.c
@@ -1,7 +1,27 @@
+#include <limits.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 bool find132pattern(int* nums, int numsSize) {
-  (void)nums;
-  (void)numsSize;
+  int* js = malloc((numsSize + 1) * sizeof(int));
+  int* j = js;
+  *j = INT_MAX;
+  int k = INT_MIN;
+  for (int i = numsSize - 1; i >= 0; --i) {
+    if (nums[i] < k) {
+      free(js);
+      return true;
+    }
+
+    while (nums[i] > *j) {
+      k = *j;
+      --j;
+    }
+
+    ++j;
+    *j = nums[i];
+  }
+
+  free(js);
   return false;
 }
