0c0cabcf0a90aa84598a0be6d92e26c9ca7df043
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-28 13:43:15 +0700

    feat(problem-2285/cpp): solve by assigning values to the most connections

--- old-problems/2285/solution.cpp
+++ old-problems/2285/solution.cpp
@@ -1,8 +1,24 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
-  long long maximumImportance(int /*n*/, std::vector<std::vector<int>>& /*roads*/) {
-    return 0;
+  long long maximumImportance(int n, std::vector<std::vector<int>>& roads) {
+    std::vector<int> counts(n, 0);
+    for (const auto& road : roads) {
+      ++counts[road[0]];
+      ++counts[road[1]];
+    }
+
+    std::sort(counts.begin(), counts.end());
+
+    long long importance{0};
+    long long id{1};
+    for (const auto count : counts) {
+      importance += id * count;
+      ++id;
+    }
+
+    return importance;
   }
 };
