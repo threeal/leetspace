3357d747bfb133cad15060a77ff2b2fd5619ee63
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-28 13:04:03 +0700

    feat(problem-456/cpp): solve by using backward iteration and stack

--- old-problems/0456/solution.cpp
+++ old-problems/0456/solution.cpp
@@ -1,8 +1,22 @@
+#include <limits>
+#include <stack>
 #include <vector>
 
 class Solution {
  public:
-  bool find132pattern(std::vector<int>& /*nums*/) {
+  bool find132pattern(std::vector<int>& nums) {
+    std::stack<int> js;
+    js.push(std::numeric_limits<int>::max());
+    int k = std::numeric_limits<int>::min();
+    for (int i = nums.size() - 1; i >= 0; --i) {
+      if (nums[i] < k) return true;
+      while (nums[i] > js.top()) {
+        k = js.top();
+        js.pop();
+      }
+      js.push(nums[i]);
+    }
+
     return false;
   }
 };
