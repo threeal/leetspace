06eacd226ef5aaae6081d7b85c90851a4c3bd527
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-30 12:25:47 +0700

    feat: solve problem 633. Sum of Square Numbers (#1100)
    * test: add test for problem 633. Sum of Square Numbers
    
    * feat(problem-633/cpp): solve by using two pointers

---
+++ old-problems/0633/solution.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+
+class Solution {
+ public:
+  bool judgeSquareSum(int c) {
+    unsigned int a{0};
+    unsigned int b = std::sqrt(c);
+
+    unsigned int a2{0};
+    unsigned int b2{b * b};
+
+    while (a <= b) {
+      if (c - a2 <= b2) {
+        if (c - a2 == b2) return true;
+        --b;
+        b2 = b * b;
+      } else {
+        ++a;
+        a2 = a * a;
+      }
+    }
+
+    return false;
+  }
+};
