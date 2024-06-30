ac9cd01fc6fe9167af8cbf5c39490d47137af80a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-30 19:08:45 +0700

    feat(problem-502/c): solve by sorting projects

--- old-problems/0502/c/solution.c
+++ old-problems/0502/c/solution.c
@@ -1,11 +1,58 @@
+#include <stdlib.h>
+
+struct project_t {
+  int profit;
+  int capital;
+};
+
+int compProject(const void* a, const void* b) {
+  return ((struct project_t*)a)->capital != ((struct project_t*)b)->capital
+      ? ((struct project_t*)a)->capital - ((struct project_t*)b)->capital
+      : ((struct project_t*)a)->profit - ((struct project_t*)b)->profit;
+}
+
+int compInt(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int findMaximizedCapital(
     int k, int w,
     int* profits, int profitsSize,
     int* capital, int capitalSize) {
-  (void)k;
-  (void)profits;
-  (void)profitsSize;
-  (void)capital;
   (void)capitalSize;
+
+  struct project_t* projects = malloc(profitsSize * sizeof(struct project_t));
+  for (int i = profitsSize - 1; i >= 0; --i) {
+    projects[i].profit = profits[i];
+    projects[i].capital = capital[i];
+  }
+
+  qsort(projects, profitsSize, sizeof(struct project_t), compProject);
+  struct project_t* projectsIt = projects;
+  struct project_t* projectsEnd = projects + profitsSize;
+
+  int* maxProfits = malloc(profitsSize * sizeof(int));
+  int* maxProfitsIt = maxProfits;
+
+  while (--k >= 0) {
+    if (projectsIt != projectsEnd && projectsIt->capital <= w) {
+      do {
+        *maxProfitsIt = projectsIt->profit;
+        ++maxProfitsIt;
+        ++projectsIt;
+      } while (projectsIt != projectsEnd && projectsIt->capital <= w);
+
+      qsort(maxProfits, maxProfitsIt - maxProfits, sizeof(int), compInt);
+    }
+
+    if (maxProfitsIt == maxProfits) return w;
+
+    --maxProfitsIt;
+    w += *maxProfitsIt;
+  }
+
+  free(maxProfits);
+  free(projects);
+
   return w;
 }
