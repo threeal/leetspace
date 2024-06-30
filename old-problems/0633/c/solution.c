d804acdd8322cf59217959191d55b202b8e20d44
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-30 12:33:50 +0700

    feat(problem-633/c): solve by using two pointers

--- old-problems/0633/c/solution.c
+++ old-problems/0633/c/solution.c
@@ -1,5 +1,23 @@
+#include <math.h>
 #include <stdbool.h>
 
 bool judgeSquareSum(int c) {
-  return c == 0;
+  unsigned int a = 0;
+  unsigned int b = sqrt(c);
+
+  unsigned int a2 = 0;
+  unsigned int b2 = b * b;
+
+  while (a <= b) {
+    if (c - a2 <= b2) {
+      if (c - a2 == b2) return true;
+      --b;
+      b2 = b * b;
+    } else {
+      ++a;
+      a2 = a * a;
+    }
+  }
+
+  return false;
 }
