4fbb68f406a6f3b29379ac623a3dda1a94eb63dc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-01 11:43:21 +0700

    feat(problem-3007/c): solve by using binary search

--- old-problems/3007/c/solution.c
+++ old-problems/3007/c/solution.c
@@ -1,3 +1,23 @@
 long long findMaximumNumber(long long k, int x) {
-  return k + x;
+  long long low = 0;
+  long long high = 10000000000000000;
+
+  while (low < high) {
+    long long mid = low + (high - low + 1) / 2;
+
+    long long price = 0;
+    for (long long i = 1 << (x - 1); i <= mid; i <<= x) {
+      price += (mid + 1) / (i + i) * i;
+      long long val = (mid + 1) % (i + i) - i;
+      if (val > 0LL) price += val;
+    }
+
+    if (price <= k) {
+      low = mid;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  return low;
 }
