9aeab7ba4fac98fb4bca1d3e328796e9bcb44120
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-01 20:20:34 +0700

    test: add test for problem 2018. Check if Word Can Be Placed In Crossword

---
+++ old-problems/2018/solution.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  bool placeWordInCrossword(
+      std::vector<std::vector<char>>& /*board*/, std::string /*word*/) {
+    return false;
+  }
+};
