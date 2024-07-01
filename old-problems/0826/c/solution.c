2f76173b4d3b171b254a46da721bc42c8d331644
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-01 14:12:07 +0700

    test(problem-826): add support to test in C

---
+++ old-problems/0826/c/solution.c
@@ -0,0 +1,12 @@
+int maxProfitAssignment(
+    int* difficulty, int difficultySize,
+    int* profit, int profitSize,
+    int* worker, int workerSize) {
+  (void)difficulty;
+  (void)difficultySize;
+  (void)profit;
+  (void)profitSize;
+  (void)worker;
+  (void)workerSize;
+  return 0;
+}
