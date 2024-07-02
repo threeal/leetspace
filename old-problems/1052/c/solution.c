efe20c991ddd21b68a4b5bae2e19c488cfa50fcf
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-02 20:11:15 +0700

    feat(problem-1052/c): solve by using a sliding window

--- old-problems/1052/c/solution.c
+++ old-problems/1052/c/solution.c
@@ -1,10 +1,27 @@
 int maxSatisfied(
     int* customers, int customersSize,
     int* grumpy, int grumpySize, int minutes) {
-  (void)customers;
   (void)customersSize;
-  (void)grumpy;
-  (void)grumpySize;
-  (void)minutes;
-  return 0;
+
+  int right = grumpySize - 1;
+  int left = grumpySize - minutes;
+
+  int satisfied = 0;
+  for (int i = right; i >= left; --i) {
+    satisfied += customers[i];
+  }
+
+  for (int i = left - 1; i >= 0; --i) {
+    if (!grumpy[i]) satisfied += customers[i];
+  }
+
+  int maxSatisfied = satisfied;
+  while (--left >= 0) {
+    if (grumpy[left]) satisfied += customers[left];
+    if (grumpy[right]) satisfied -= customers[right];
+    if (satisfied > maxSatisfied) maxSatisfied = satisfied;
+    --right;
+  }
+
+  return maxSatisfied;
 }
