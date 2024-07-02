2dbcd943cbeef59e4d38a7e789074f9737b38abb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-02 19:55:37 +0700

    feat(problem-1052/cpp): solve by using a sliding window

--- old-problems/1052/solution.cpp
+++ old-problems/1052/solution.cpp
@@ -2,7 +2,27 @@
 
 class Solution {
  public:
-  int maxSatisfied(std::vector<int>& /*customers*/, std::vector<int>& /*grumpy*/, int /*minutes*/) {
-    return 0;
+  int maxSatisfied(std::vector<int>& customers, std::vector<int>& grumpy, int minutes) {
+    int right = grumpy.size() - 1;
+    int left = grumpy.size() - minutes;
+
+    int satisfied{0};
+    for (int i{right}; i >= left; --i) {
+      satisfied += customers[i];
+    }
+
+    for (int i{left - 1}; i >= 0; --i) {
+      if (!grumpy[i]) satisfied += customers[i];
+    }
+
+    int maxSatisfied{satisfied};
+    while (--left >= 0) {
+      if (grumpy[left]) satisfied += customers[left];
+      if (grumpy[right]) satisfied -= customers[right];
+      if (satisfied > maxSatisfied) maxSatisfied = satisfied;
+      --right;
+    }
+
+    return maxSatisfied;
   }
 };
