2a1d0170d8dcf58b6449cd7114cf02f296bc977a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-03 17:46:15 +0700

    feat(problem-875/c): solve by using binary search

--- old-problems/0875/c/solution.c
+++ old-problems/0875/c/solution.c
@@ -1,3 +1,27 @@
 int minEatingSpeed(int* piles, int pilesSize, int h) {
-  return piles[pilesSize - 1] / h;
+  int left = 1;
+
+  int right = piles[0];
+  for (int i = pilesSize - 1; i > 0; --i) {
+    if (piles[i] > right) right = piles[i];
+  }
+
+  while (left < right) {
+    const int mid = (left + right) / 2;
+
+    int time = 0;
+    for (int i = pilesSize - 1; i >= 0; --i) {
+      time += piles[i] / mid;
+      if (piles[i] % mid > 0) ++time;
+      if (time > h) break;
+    }
+
+    if (time > h) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
 }
