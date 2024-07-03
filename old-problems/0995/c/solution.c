8aee656215e06b259c88fb5de7abbf1b1d562358
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-03 17:12:54 +0700

    feat(problem-995/c): solve by tracking flip positions

--- old-problems/0995/c/solution.c
+++ old-problems/0995/c/solution.c
@@ -1,4 +1,43 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
 int minKBitFlips(int* nums, int numsSize, int k) {
-  (void)nums;
-  return numsSize / k;
+  int mustFlipsSize = numsSize / 8 + (numsSize % 8 == 0 ? 0 : 1);
+  uint8_t* mustFlips = malloc(mustFlipsSize);
+  memset(mustFlips, 0, mustFlipsSize);
+
+  int target = 1;
+  int flip = 0;
+  for (int i = numsSize - 1; i >= k; --i) {
+    if (mustFlips[i / 8] >> i % 8 & 1) target = target == 0 ? 1 : 0;
+    if (nums[i] != target) {
+      mustFlips[(i - k) / 8] |= 1 << (i - k) % 8;
+      target = target == 0 ? 1 : 0;
+      ++flip;
+    }
+  }
+
+  if (mustFlips[(k - 1) / 8] >> (k - 1) % 8 & 1) target = target == 0 ? 1 : 0;
+  if (nums[k - 1] == target) {
+    for (int i = k - 2; i >= 0; --i) {
+      if (mustFlips[i / 8] >> i % 8 & 1) target = target == 0 ? 1 : 0;
+      if (nums[i] != target) {
+        free(mustFlips);
+        return -1;
+      }
+    }
+    free(mustFlips);
+    return flip;
+  } else {
+    for (int i = k - 2; i >= 0; --i) {
+      if (mustFlips[i / 8] >> i % 8 & 1) target = target == 0 ? 1 : 0;
+      if (nums[i] == target) {
+        free(mustFlips);
+        return -1;
+      }
+    }
+    free(mustFlips);
+    return flip + 1;
+  }
 }
