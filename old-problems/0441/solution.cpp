ec8966c80489c00bc4cfb09f5a9b7d246a086346
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-04 13:23:57 +0700

    feat(problem-441/cpp): solve by using binary search

--- old-problems/0441/solution.cpp
+++ old-problems/0441/solution.cpp
@@ -1,6 +1,19 @@
+#include <cstdint>
+
 class Solution {
  public:
   int arrangeCoins(int n) {
-    return n;
+    std::uint_fast32_t low{1};
+    std::uint_fast32_t high{92681};
+
+    while (low < high) {
+      const std::uint_fast32_t mid{(low + high + 1) / 2};
+      if ((mid + mid * mid) / 2 > static_cast<std::uint_fast32_t>(n)) {
+        high = mid - 1;
+      } else {
+        low = mid;
+      }
+    }
+    return low;
   }
 };
