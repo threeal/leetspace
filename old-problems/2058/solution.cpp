73a39f5e957814e7738699540442f9b5d8acaf21
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-05 15:58:41 +0700

    feat(problem-2058/cpp): solve by iterating nodes

--- old-problems/2058/solution.cpp
+++ old-problems/2058/solution.cpp
@@ -2,7 +2,41 @@
 
 class Solution {
  public:
-  std::vector<int> nodesBetweenCriticalPoints(ListNode* /*head*/) {
+  std::vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+    // Find the first critical point.
+    while (head->next->next != nullptr) {
+      if ((head->val > head->next->val && head->next->val < head->next->next->val) ||
+          (head->val < head->next->val && head->next->val > head->next->next->val)) {
+        // Find the second critical point.
+        int index{1};
+        head = head->next;
+        while (head->next->next != nullptr) {
+          if ((head->val > head->next->val && head->next->val < head->next->next->val) ||
+              (head->val < head->next->val && head->next->val > head->next->next->val)) {
+            int min{index};
+            int max{index};
+            // Find the subsequent critical points.
+            index = 1;
+            head = head->next;
+            while (head->next->next != nullptr) {
+              if ((head->val > head->next->val && head->next->val < head->next->next->val) ||
+                  (head->val < head->next->val && head->next->val > head->next->next->val)) {
+                max += index;
+                if (index < min) min = index;
+                index = 0;
+              }
+              head = head->next;
+              ++index;
+            }
+            return {min, max};
+          }
+          head = head->next;
+          ++index;
+        }
+        return {-1, -1};
+      }
+      head = head->next;
+    }
     return {-1, -1};
   }
 };
