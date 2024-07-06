c2da1269c4e5a3ada83eeca29ae0d8b7f9440f2f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-06 11:20:29 +0700

    feat(problem-1329/c): solve by sorting in a temp array

--- old-problems/1329/c/solution.c
+++ old-problems/1329/c/solution.c
@@ -1,7 +1,46 @@
+#include <stdlib.h>
+
+int comp(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int** diagonalSort(
     int** mat, int matSize, int* matColSize,
     int* returnSize, int** returnColumnSizes) {
   *returnSize = matSize;
   *returnColumnSizes = matColSize;
+
+  const int h = matSize;
+  if (h == 1) return mat;
+
+  const int w = matColSize[0];
+  if (w == 1) return mat;
+
+  int* tempArr = malloc((h > w ? h : w) * sizeof(int));
+
+  for (int x = w - 1; x >= 0; --x) {
+    int n = w - x < h ? w - x : h;
+    int i = n;
+    while (--i >= 0) tempArr[i] = mat[i][x + i];
+
+    qsort(tempArr, n, sizeof(int), comp);
+
+    i = n;
+    while (--i >= 0) mat[i][x + i] = tempArr[i];
+  }
+
+  for (int y = h - 1; y > 0; --y) {
+    int n = h - y < w ? h - y : w;
+    int i = n;
+    while (--i >= 0) tempArr[i] = mat[y + i][i];
+
+    qsort(tempArr, n, sizeof(int), comp);
+
+    i = n;
+    while (--i >= 0) mat[y + i][i] = tempArr[i];
+  }
+
+  free(tempArr);
+
   return mat;
 }
