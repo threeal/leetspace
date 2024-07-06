bd6166001f3b4ea293a1d1c532f94a7ed5098ed4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-06 11:16:27 +0700

    test(problem-1329): add support to solve in C

---
+++ old-problems/1329/c/solution.c
@@ -0,0 +1,7 @@
+int** diagonalSort(
+    int** mat, int matSize, int* matColSize,
+    int* returnSize, int** returnColumnSizes) {
+  *returnSize = matSize;
+  *returnColumnSizes = matColSize;
+  return mat;
+}
