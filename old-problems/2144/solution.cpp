cb8130cd482b0910c1492f65b9a6dc5e718e9f15
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-07 13:15:23 +0700

    feat(problem-2144/cpp): solve by sorting candies and pick two largest

--- old-problems/2144/solution.cpp
+++ old-problems/2144/solution.cpp
@@ -1,8 +1,19 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
-  int minimumCost([[maybe_unused]] std::vector<int>& cost) {
-    return 0;
+  int minimumCost(std::vector<int>& cost) {
+    std::sort(cost.begin(), cost.end());
+
+    int total{0};
+    int i = cost.size() - 2;
+    while (i >= 0) {
+      total += cost[i] + cost[i + 1];
+      i -= 3;
+    }
+    if (i == -1) total += cost[0];
+
+    return total;
   }
 };
