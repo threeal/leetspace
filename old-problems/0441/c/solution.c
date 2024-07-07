7a40faf36eec62544fc53e350f943af19a0d78c2
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-07 22:15:36 +0700

    feat(problem-441/c): solve by using binary search

--- old-problems/0441/c/solution.c
+++ old-problems/0441/c/solution.c
@@ -1,3 +1,16 @@
+#include <stdint.h>
+
 int arrangeCoins(int n) {
-  return n;
+  uint_fast32_t low = 1;
+  uint_fast32_t high = 92681;
+
+  while (low < high) {
+    const uint_fast32_t mid = (low + high + 1) / 2;
+    if ((mid + mid * mid) / 2 > (uint_fast32_t)n) {
+      high = mid - 1;
+    } else {
+      low = mid;
+    }
+  }
+  return low;
 }
