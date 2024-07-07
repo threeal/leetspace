6f6d6b3c35dd77baf23ca6fc8150e01b3178f23e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-07 12:16:43 +0700

    feat(problem-1518/cpp): solve by repeatedly dividing water bottles

--- old-problems/1518/solution.cpp
+++ old-problems/1518/solution.cpp
@@ -1,6 +1,11 @@
 class Solution {
  public:
   int numWaterBottles(int numBottles, int numExchange) {
-    return numBottles / numExchange;
+    int drinkBottles{numBottles};
+    while (numBottles >= numExchange) {
+      drinkBottles += numBottles / numExchange;
+      numBottles = numBottles / numExchange + numBottles % numExchange;
+    }
+    return drinkBottles;
   }
 };
