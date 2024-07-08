b37398f5a966ae3a11f6f0ef7da70aebf228bd9d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-08 10:24:05 +0700

    feat(problem-1823/cpp): solve by using queue

--- old-problems/1823/solution.cpp
+++ old-problems/1823/solution.cpp
@@ -1,6 +1,19 @@
+#include <queue>
+
 class Solution {
  public:
   int findTheWinner(int n, int k) {
-    return n / k;
+    std::queue<int> friends{};
+    for (int i{1}; i <= n; ++i) friends.push(i);
+
+    while (friends.size() > 1) {
+      for (int i{k}; i > 1; --i) {
+        friends.push(friends.front());
+        friends.pop();
+      }
+      friends.pop();
+    }
+
+    return friends.front();
   }
 };
