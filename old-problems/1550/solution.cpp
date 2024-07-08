26ec76eab5b4116516b17595f96d0ce9110ffcc9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-08 20:21:57 +0700

    feat(problem-1550/cpp): solve by using a while-loop

--- old-problems/1550/solution.cpp
+++ old-problems/1550/solution.cpp
@@ -3,6 +3,22 @@
 class Solution {
  public:
   bool threeConsecutiveOdds(std::vector<int>& arr) {
-    return arr.empty();
+    int i = arr.size() - 3;
+    while (i >= 0) {
+      if (arr[i] % 2 != 0) {
+        if (arr[i + 1] % 2 != 0) {
+          if (arr[i + 2] % 2 != 0) {
+            return true;
+          } else {
+            --i;
+          }
+        } else {
+          i -= 2;
+        }
+      } else {
+        i -= 3;
+      }
+    }
+    return false;
   }
 };
