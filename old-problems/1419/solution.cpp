7393d4843b93b20aac6ba1415bbe7c50283aa903
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-08 21:40:26 +0700

    feat(problem-1419/cpp): solve by iterating and checking prev chars

--- old-problems/1419/solution.cpp
+++ old-problems/1419/solution.cpp
@@ -3,6 +3,36 @@
 class Solution {
  public:
   int minNumberOfFrogs(std::string croakOfFrogs) {
-    return croakOfFrogs.size() / 5;
+    int min{0}, c{0}, r{0}, o{0}, a{0};
+
+    for (const auto ch : croakOfFrogs) {
+      switch (ch) {
+        case 'c':
+          if (++c > min) min = c;
+          break;
+
+        case 'r':
+          if (++r > c) return -1;
+          break;
+
+        case 'o':
+          if (++o > r) return -1;
+          break;
+
+        case 'a':
+          if (++a > o) return -1;
+          break;
+
+        case 'k':
+          if (o == 0) return -1;
+          --c;
+          --r;
+          --o;
+          --a;
+      }
+    }
+
+    if (c > 0 || a > 0) return -1;
+    return min;
   }
 };
