50a916066a1f7ad759c8e4f54df9ea0fc45038b9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-09 13:39:46 +0700

    feat(problem-204): solve by using Sieve of Eratosthenes

--- old-problems/0204/solution.cpp
+++ old-problems/0204/solution.cpp
@@ -1,6 +1,19 @@
+#include <vector>
+
 class Solution {
  public:
   int countPrimes(int n) {
-    return n;
+    if (n < 2) return 0;
+    int count{0};
+
+    std::vector<bool> nums(n, false);
+    for (int i{2}; i < n; ++i) {
+      if (!nums[i]) {
+        ++count;
+        for (int j{i + i}; j < n; j += i) nums[j] = true;
+      }
+    }
+
+    return count;
   }
 };
