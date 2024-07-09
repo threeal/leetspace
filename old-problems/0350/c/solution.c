f00cf7d7ab8965f4a4b383135b369e34f03a007e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-09 13:11:57 +0700

    feat(problem-350/c): solve by using sorting and iteration

--- old-problems/0350/c/solution.c
+++ old-problems/0350/c/solution.c
@@ -1,7 +1,30 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int* intersect(
     int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
-  (void)nums2;
-  (void)nums2Size;
-  *returnSize = nums1Size;
-  return nums1;
+  qsort(nums1, nums1Size, sizeof(int), compare);
+  qsort(nums2, nums2Size, sizeof(int), compare);
+
+  int* nums = nums1Size > nums2Size ? nums1 : nums2;
+
+  int i = 0, i1 = 0, i2 = 0;
+  while (1) {
+    if (nums1[i1] == nums2[i2]) {
+      nums[i] = nums1[i1];
+      ++i;
+      if (++i1 == nums1Size) break;
+      if (++i2 == nums2Size) break;
+    } else if (nums1[i1] < nums2[i2]) {
+      if (++i1 == nums1Size) break;
+    } else {
+      if (++i2 == nums2Size) break;
+    }
+  }
+
+  *returnSize = i;
+  return nums;
 }
