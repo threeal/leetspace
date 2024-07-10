b7e9ac4076a667ae122d124da386dc9f7a9aed11
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-10 12:31:25 +0700

    feat(problem-1749/cpp): solve by using Kadane's algorithm

--- old-problems/1749/solution.cpp
+++ old-problems/1749/solution.cpp
@@ -3,6 +3,23 @@
 class Solution {
  public:
   int maxAbsoluteSum(std::vector<int>& nums) {
-    return nums.size();
+    int negativeSum{0}, positiveSum{0}, minNegative{0}, maxPositive{0};
+    for (const auto num : nums) {
+      negativeSum += num;
+      if (negativeSum > 0) {
+        negativeSum = 0;
+      } else if (negativeSum < minNegative) {
+        minNegative = negativeSum;
+      }
+
+      positiveSum += num;
+      if (positiveSum < 0) {
+        positiveSum = 0;
+      } else if (positiveSum > maxPositive) {
+        maxPositive = positiveSum;
+      }
+    }
+
+    return -minNegative > maxPositive ? -minNegative : maxPositive;
   }
 };
