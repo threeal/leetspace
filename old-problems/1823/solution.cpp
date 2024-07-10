d62114fe8337409ae7e6f8fe2811dee9893cc5d9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-10 11:20:17 +0700

    feat(problem-1823/cpp): solve by using a recursive function

--- old-problems/1823/solution.cpp
+++ old-problems/1823/solution.cpp
@@ -1,24 +1,11 @@
-#include <vector>
-
 class Solution {
  public:
   int findTheWinner(int n, int k) {
-    std::vector<int> friends(n);
-    for (int i{n}; i > 0; --i) friends[i - 1] = i;
+    return recurse(n, k) + 1;
+  }
 
-    int begin{0};
-    int end{0};
-
-    int left{n};
-    while (--left > 0) {
-      for (int i{k}; i > 1; --i) {
-        friends[end] = friends[begin];
-        if (++begin >= n) begin = 0;
-        if (++end >= n) end = 0;
-      }
-      if (++begin >= n) begin = 0;
-    }
-
-    return friends[begin];
+  int recurse(int n, int k) {
+    if (n == 1) return 0;
+    return (recurse(n - 1, k) + k) % n;
   }
 };
