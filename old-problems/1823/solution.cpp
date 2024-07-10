e40c659f634d6f5c90e7a03ff6bb517d12de8cbe
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-10 11:28:27 +0700

    perf(problem-1823/cpp): replace recursive function call with for-loop

--- old-problems/1823/solution.cpp
+++ old-problems/1823/solution.cpp
@@ -1,11 +1,10 @@
 class Solution {
  public:
   int findTheWinner(int n, int k) {
-    return recurse(n, k) + 1;
-  }
-
-  int recurse(int n, int k) {
-    if (n == 1) return 0;
-    return (recurse(n - 1, k) + k) % n;
+    int res{0};
+    for (int i{2}; i <= n; ++i) {
+      res = (res + k) % i;
+    }
+    return res + 1;
   }
 };
