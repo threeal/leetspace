0ca9d3b99ad2b0e71ee4b86534de1d7494bc98f4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-10 11:06:59 +0700

    feat(problem-1598/cpp): solve by increment/decrement level

--- old-problems/1598/solution.cpp
+++ old-problems/1598/solution.cpp
@@ -4,6 +4,14 @@
 class Solution {
  public:
   int minOperations(std::vector<std::string>& logs) {
-    return logs.size();
+    int level{0};
+    for (const auto& log : logs) {
+      if (log[0] == '.') {
+        if (log[1] == '.' && level > 0) --level;
+      } else {
+        ++level;
+      }
+    }
+    return level;
   }
 };
