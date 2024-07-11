3b0e2d1fa4183c4b7b7f872f48e109a3671ba107
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-11 11:42:23 +0700

    feat(problem-860): solve by simulating the problem

--- old-problems/0860/solution.cpp
+++ old-problems/0860/solution.cpp
@@ -3,6 +3,31 @@
 class Solution {
  public:
   bool lemonadeChange(std::vector<int>& bills) {
-    return bills.empty();
+    int fives{0}, tens{0};
+    for (const auto bill : bills) {
+      switch (bill) {
+        case 5:
+          ++fives;
+          break;
+
+        case 10:
+          if (fives <= 0) return false;
+          --fives;
+          ++tens;
+          break;
+
+        case 20:
+          if (tens <= 0) {
+            if (fives <= 2) return false;
+            fives -= 3;
+          } else {
+            if (fives <= 0) return false;
+            --fives;
+            --tens;
+          }
+      }
+    }
+
+    return true;
   }
 };
