7cc3b2ec846a0bb2aa26e07b3a15ac47d6146566
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-11 20:53:31 +0700

    feat(problem-125/cpp): solve by using two pointers

--- old-problems/0125/solution.cpp
+++ old-problems/0125/solution.cpp
@@ -3,6 +3,36 @@
 class Solution {
  public:
   bool isPalindrome(std::string s) {
-    return s.empty();
+    std::size_t left{0};
+    std::size_t right{s.size() - 1};
+
+    while (left < right) {
+      while (left < right) {
+        if (s[left] >= '0' && s[left] <= '9') break;
+        if (s[left] >= 'A' && s[left] <= 'Z') break;
+        if (s[left] >= 'a' && s[left] <= 'z') {
+          s[left] += 'A' - 'a';
+          break;
+        }
+        ++left;
+      }
+
+      while (left < right) {
+        if (s[right] >= '0' && s[right] <= '9') break;
+        if (s[right] >= 'A' && s[right] <= 'Z') break;
+        if (s[right] >= 'a' && s[right] <= 'z') {
+          s[right] += 'A' - 'a';
+          break;
+        }
+        --right;
+      }
+
+      if (s[left] != s[right]) return false;
+
+      ++left;
+      --right;
+    }
+
+    return true;
   }
 };
