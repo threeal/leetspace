9d34cf3daaa2a81ecb5a5f7cab9db555e45af0c8
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-11 11:22:24 +0700

    feat(problem-2181/cpp): solve by iterating list nodes

--- old-problems/2181/solution.cpp
+++ old-problems/2181/solution.cpp
@@ -1,6 +1,22 @@
 class Solution {
  public:
   ListNode* mergeNodes(ListNode* head) {
-    return head;
+    auto begin = head;
+    auto it = begin->next;
+
+    int sum{0};
+    while (it != nullptr) {
+      if (it->val == 0) {
+        it->val = sum;
+        begin->next = it;
+        begin = it;
+        sum = 0;
+      } else {
+        sum += it->val;
+      }
+      it = it->next;
+    }
+
+    return head->next;
   }
 };
