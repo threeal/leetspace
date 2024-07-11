de221ae8e1bfa2c91af0556fa0b92015a1d95888
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-11 21:19:55 +0700

    feat(problem-55/cpp): solve by iterating numbers

--- old-problems/0055/solution.cpp
+++ old-problems/0055/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   bool canJump(std::vector<int>& nums) {
-    return nums.empty();
+    if (nums.size() <= 1) return true;
+    nums.back() = 1;
+
+    int maxJump{0};
+    for (const auto num : nums) {
+      --maxJump;
+      if (num == 0) {
+        if (maxJump <= 0) return false;
+      } else {
+        if (num > maxJump) maxJump = num;
+      }
+    }
+
+    return true;
   }
 };
