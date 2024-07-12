98a305ff8e1946cb319501dc6f46e46cfe350898
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-12 18:11:59 +0700

    feat(problem-1175/cpp): solve by counting permutations

--- old-problems/1175/solution.cpp
+++ old-problems/1175/solution.cpp
@@ -1,6 +1,25 @@
+#include <vector>
+
 class Solution {
  public:
   int numPrimeArrangements(int n) {
-    return n;
+    const std::vector<int> primes{
+        0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+        67, 71, 73, 79, 83, 89, 97};
+
+    for (int i = primes.size() - 1; i >= 0; --i) {
+      if (n >= primes[i]) {
+        long long count{1};
+        for (int j = i; j > 0; --j) {
+          count = (count * j) % 1000000007;
+        }
+        for (int j = n - i; j > 0; --j) {
+          count = (count * j) % 1000000007;
+        }
+        return count;
+      }
+    }
+
+    return 0;
   }
 };
