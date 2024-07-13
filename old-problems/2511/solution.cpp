247a7c3df9122af343e96122368e319e80c7b649
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-13 14:49:30 +0700

    feat(problem-2511/cpp): solve by iterating array

--- old-problems/2511/solution.cpp
+++ old-problems/2511/solution.cpp
@@ -2,7 +2,52 @@
 
 class Solution {
  public:
-  int captureForts(std::vector<int>& /*forts*/) {
+  int captureForts(std::vector<int>& forts) {
+    for (std::size_t i{forts.size() - 1}; i > 0; --i) {
+      if (forts[i] == 0) continue;
+
+      int maxCapture{0};
+      int possibleCapture{0};
+      bool prevEmpty{forts[i] == -1};
+
+      while (--i > 0) {
+        switch (forts[i]) {
+          case -1:
+            if (!prevEmpty) {
+              prevEmpty = true;
+              if (possibleCapture > maxCapture) maxCapture = possibleCapture;
+            }
+            possibleCapture = 0;
+            break;
+
+          case 0:
+            ++possibleCapture;
+            break;
+
+          case 1:
+            if (prevEmpty) {
+              prevEmpty = false;
+              if (possibleCapture > maxCapture) maxCapture = possibleCapture;
+            }
+            possibleCapture = 0;
+        }
+      }
+
+      switch (forts[i]) {
+        case -1:
+          if (!prevEmpty && possibleCapture > maxCapture) {
+            maxCapture = possibleCapture;
+          }
+          break;
+
+        case 1:
+          if (prevEmpty && possibleCapture > maxCapture) {
+            maxCapture = possibleCapture;
+          }
+      }
+
+      return maxCapture;
+    }
     return 0;
   }
 };
