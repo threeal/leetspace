462e107f9fa5f5a206d2d304de3c533563ee9001
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-13 22:31:29 +0700

    feat(problem-2211/cpp): solve by iterating directions

--- old-problems/2211/solution.cpp
+++ old-problems/2211/solution.cpp
@@ -2,7 +2,30 @@
 
 class Solution {
  public:
-  int countCollisions([[maybe_unused]] std::string directions) {
-    return 0;
+  int countCollisions(std::string directions) {
+    char* direction = directions.data();
+    while (*direction == 'L') ++direction;
+
+    int collisions{0};
+    int rights{0};
+    while (*direction != 0) {
+      switch (*direction) {
+        case 'L':
+          collisions += rights + 1;
+          rights = 0;
+          break;
+
+        case 'R':
+          ++rights;
+          break;
+
+        case 'S':
+          collisions += rights;
+          rights = 0;
+      }
+      ++direction;
+    }
+
+    return collisions;
   }
 };
