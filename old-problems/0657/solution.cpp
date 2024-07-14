af99cdb6116d58943d7135d6dc6801718bdfc7ea
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-14 20:09:41 +0700

    feat(problem-657/cpp): solve by tracking robot position

--- old-problems/0657/solution.cpp
+++ old-problems/0657/solution.cpp
@@ -3,6 +3,25 @@
 class Solution {
  public:
   bool judgeCircle(std::string moves) {
-    return moves.empty();
+    int horizontal{0}, vertical{0};
+    for (const auto move : moves) {
+      switch (move) {
+        case 'L':
+          --horizontal;
+          break;
+
+        case 'R':
+          ++horizontal;
+          break;
+
+        case 'U':
+          --vertical;
+          break;
+
+        case 'D':
+          ++vertical;
+      }
+    }
+    return horizontal == 0 && vertical == 0;
   }
 };
