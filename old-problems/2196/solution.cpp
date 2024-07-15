5aa6dfdaa4b7ef579d196fd8cc6b0430354a2a3c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-15 07:55:15 +0700

    feat(problem-2196/cpp): solve by storing nodes in map

--- old-problems/2196/solution.cpp
+++ old-problems/2196/solution.cpp
@@ -1,8 +1,41 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
-  TreeNode* createBinaryTree(std::vector<std::vector<int>>& /*descriptions*/) {
+  TreeNode* createBinaryTree(std::vector<std::vector<int>>& descriptions) {
+    std::unordered_map<int, std::pair<TreeNode*, bool>> nodes{};
+
+    for (const auto& description : descriptions) {
+      auto parent = nodes.find(description[0]);
+      if (parent == nodes.end()) {
+        parent = nodes.emplace(
+                          description[0],
+                          std::make_pair(new TreeNode(description[0]), true))
+                     .first;
+      }
+
+      auto child = nodes.find(description[1]);
+      if (child == nodes.end()) {
+        child = nodes.emplace(
+                         description[1],
+                         std::make_pair(new TreeNode(description[1]), false))
+                    .first;
+      } else {
+        child->second.second = false;
+      }
+
+      if (description[2]) {
+        parent->second.first->left = child->second.first;
+      } else {
+        parent->second.first->right = child->second.first;
+      }
+    }
+
+    for (const auto& [key, node] : nodes) {
+      if (node.second) return node.first;
+    }
+
     return nullptr;
   }
 };
