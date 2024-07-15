4b0d804404622c3de0c6522bf34e97bf1635f8dc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-15 14:24:19 +0700

    feat(problem-1701/c): solve by iterating customers

--- old-problems/1701/c/solution.c
+++ old-problems/1701/c/solution.c
@@ -1,7 +1,19 @@
 double averageWaitingTime(
     int** customers, int customersSize, int* customersColSize) {
-  (void)customers;
-  (void)customersSize;
   (void)customersColSize;
-  return 0.0;
+
+  double totalWaitTime = 0.0;
+
+  int currentTime = 0;
+  for (int i = 0; i < customersSize; ++i) {
+    if (currentTime > customers[i][0]) {
+      totalWaitTime += currentTime - customers[i][0];
+    } else {
+      currentTime = customers[i][0];
+    }
+    currentTime += customers[i][1];
+    totalWaitTime += customers[i][1];
+  }
+
+  return totalWaitTime / customersSize;
 }
