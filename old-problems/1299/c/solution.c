66b4b33e16ef9e504528e2e8176248a9d8aeb0dd
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-16 18:42:39 +0700

    feat(problem-1299/c): solve by using a iteration

--- old-problems/1299/c/solution.c
+++ old-problems/1299/c/solution.c
@@ -1,4 +1,17 @@
 int* replaceElements(int* arr, int arrSize, int* returnSize) {
+  int max = arr[arrSize - 1];
+  arr[arrSize - 1] = -1;
+
+  for (int i = arrSize - 2; i >= 0; --i) {
+    if (arr[i] > max) {
+      int temp = arr[i];
+      arr[i] = max;
+      max = temp;
+    } else {
+      arr[i] = max;
+    }
+  }
+
   *returnSize = arrSize;
   return arr;
 }
