954f95071e7d0c350b51238e4ed137ed9800d2f3
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-16 18:44:16 +0700

    feat: solve problem 1090. Largest Values From Labels (#1254)
    * test: add test for problem 1090. Largest Values From Labels
    
    * feat(problem-1090): solve by sorting values for each labels

---
+++ old-problems/1090/solution.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+
+class Solution {
+ public:
+  int largestValsFromLabels(
+      std::vector<int>& values, std::vector<int>& labels,
+      int numWanted, int useLimit) {
+    std::vector<int> indexes(values.size());
+    for (int i = values.size() - 1; i >= 0; --i) {
+      indexes[i] = i;
+    }
+
+    auto valuesData = values.data();
+    auto labelsData = labels.data();
+    std::sort(
+        indexes.begin(), indexes.end(),
+        [valuesData, labelsData](int a, int b) {
+          return labelsData[a] == labelsData[b]
+              ? valuesData[a] < valuesData[b]
+              : labelsData[a] < labelsData[b];
+        });
+
+    auto label = labelsData[indexes.back()];
+    auto count = useLimit - 1;
+    for (int i = indexes.size() - 2; i >= 0; --i) {
+      if (labels[indexes[i]] == label) {
+        if (count > 0) {
+          --count;
+        } else {
+          valuesData[indexes[i]] = -1;
+        }
+      } else {
+        label = labelsData[indexes[i]];
+        count = useLimit - 1;
+      }
+    }
+
+    std::sort(values.begin(), values.end());
+
+    int sum{0};
+    for (int i = values.size() - 1; numWanted > 0; --i, --numWanted) {
+      if (valuesData[i] < 0) break;
+      sum += valuesData[i];
+    }
+
+    return sum;
+  }
+};
