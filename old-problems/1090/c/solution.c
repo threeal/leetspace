304f22dd049c6f685fcc497214ba27c75d268cd3
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-16 18:50:50 +0700

    test(problem-1090): add support to test in C

---
+++ old-problems/1090/c/solution.c
@@ -0,0 +1,11 @@
+int largestValsFromLabels(
+    int* values, int valuesSize, int* labels, int labelsSize,
+    int numWanted, int useLimit) {
+  (void)values;
+  (void)valuesSize;
+  (void)labels;
+  (void)labelsSize;
+  (void)numWanted;
+  (void)useLimit;
+  return 0;
+}
