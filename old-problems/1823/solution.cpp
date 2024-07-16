50c3a130a08db3b9e93e839cc31b1f9cc0210b03
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-16 22:08:52 +0700

    feat: solve problem 1823. Find the Winner of the Circular Game in linear time (#1258)
    * feat(problem-1823/cpp): solve by using a recursive function
    
    * perf(problem-1823/cpp): replace recursive function call with for-loop

--- old-problems/1823/solution.cpp
+++ old-problems/1823/solution.cpp
@@ -1,24 +1,10 @@
-#include <vector>
-
 class Solution {
  public:
   int findTheWinner(int n, int k) {
-    std::vector<int> friends(n);
-    for (int i{n}; i > 0; --i) friends[i - 1] = i;
-
-    int begin{0};
-    int end{0};
-
-    int left{n};
-    while (--left > 0) {
-      for (int i{k}; i > 1; --i) {
-        friends[end] = friends[begin];
-        if (++begin >= n) begin = 0;
-        if (++end >= n) end = 0;
-      }
-      if (++begin >= n) begin = 0;
+    int res{0};
+    for (int i{2}; i <= n; ++i) {
+      res = (res + k) % i;
     }
-
-    return friends[begin];
+    return res + 1;
   }
 };
