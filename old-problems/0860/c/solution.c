9b4ca9b0b650f6ac43b1cebdafe554bfac121d58
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-17 11:42:47 +0700

    feat(problem-860/c): solve by simulating the problem

--- old-problems/0860/c/solution.c
+++ old-problems/0860/c/solution.c
@@ -1,7 +1,30 @@
 #include <stdbool.h>
 
 bool lemonadeChange(int* bills, int billsSize) {
-  (void)bills;
-  (void)billsSize;
-  return false;
+  int fives = 0, tens = 0;
+  for (int i = 0; i < billsSize; ++i) {
+    switch (bills[i]) {
+      case 5:
+        ++fives;
+        break;
+
+      case 10:
+        if (fives <= 0) return false;
+        --fives;
+        ++tens;
+        break;
+
+      case 20:
+        if (tens <= 0) {
+          if (fives <= 2) return false;
+          fives -= 3;
+        } else {
+          if (fives <= 0) return false;
+          --fives;
+          --tens;
+        }
+    }
+  }
+
+  return true;
 }
