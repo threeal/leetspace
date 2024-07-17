86a1efcac7af3758172c0c76587db2d91a60a985
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-17 12:06:18 +0700

    feat(problem-796/cpp): solve by comparing string starting from any index

--- old-problems/0796/solution.cpp
+++ old-problems/0796/solution.cpp
@@ -3,6 +3,20 @@
 class Solution {
  public:
   bool rotateString(std::string s, std::string goal) {
-    return s == goal;
+    for (int i = s.size() - 1; i >= 0; --i) {
+      if (!compare(s.data() + i, goal.data(), s.size() - i)) continue;
+      if (!compare(s.data(), goal.data() + s.size() - i, i)) continue;
+      return true;
+    }
+
+    return false;
+  }
+
+  bool compare(char* a, char* b, int bSize) {
+    for (int i = bSize - 1; i >= 0; --i) {
+      if (a[i] != b[i]) return false;
+    }
+
+    return true;
   }
 };
