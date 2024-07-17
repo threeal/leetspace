26f5d058645afa55bd1244888f61c62bcfaeaa0e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-17 22:55:31 +0700

    feat: solve problem 1175. Prime Arrangements in C (#1280)
    * test(problem-1175): add support to test in C
    
    * feat(problem-1175/c): solve by counting permutations

---
+++ old-problems/1175/c/solution.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+int numPrimeArrangements(int n) {
+  int8_t primes[26] = {
+      0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+      67, 71, 73, 79, 83, 89, 97};
+
+  for (int8_t i = 25; i > 0; --i) {
+    if (n >= primes[i]) {
+      long long count = 1;
+      for (int8_t j = i; j > 0; --j) {
+        count = (count * j) % 1000000007;
+      }
+      for (int8_t j = n - i; j > 0; --j) {
+        count = (count * j) % 1000000007;
+      }
+      return count;
+    }
+  }
+
+  return 1;
+}
