269d9d0befa8c4d860685f31fefb72bad7a46ff3
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-18 19:54:08 +0700

    feat(problem-1071/cpp): solve by using a greedy approach

--- old-problems/1071/solution.cpp
+++ old-problems/1071/solution.cpp
@@ -1,8 +1,33 @@
 #include <string>
+#include <string_view>
 
 class Solution {
  public:
   std::string gcdOfStrings(std::string str1, std::string str2) {
-    return str1 + str2;
+    for (std::size_t n = std::min(str1.size(), str2.size()); n > 0; --n) {
+      if (str1.size() % n != 0) continue;
+      if (str2.size() % n != 0) continue;
+      if (checkCommonDivisor(str1, str2, n)) {
+        return str1.substr(0, n);
+      }
+    }
+    return "";
+  }
+
+  bool checkCommonDivisor(
+      std::string_view str1, std::string_view str2, std::size_t n) {
+    for (int i = n - 1; i >= 0; --i) {
+      if (str1[i] != str2[i]) return false;
+
+      for (std::size_t j = i + n; j < str1.size(); j += n) {
+        if (str1[j] != str1[i]) return false;
+      }
+
+      for (std::size_t j = i + n; j < str2.size(); j += n) {
+        if (str2[j] != str2[i]) return false;
+      }
+    }
+
+    return true;
   }
 };
