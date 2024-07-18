903dd4405de84660cd5492d049837c3f782118a3
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-18 20:43:15 +0700

    feat(problem-680): solve by checking palindrome recursively

--- old-problems/0680/solution.cpp
+++ old-problems/0680/solution.cpp
@@ -3,6 +3,23 @@
 class Solution {
  public:
   bool validPalindrome(std::string s) {
-    return s.empty();
+    return validPalindromeWithDelete(s.data(), s.data() + s.size() - 1);
+  }
+
+  bool validPalindromeWithDelete(char* left, char* right) {
+    if (left >= right) return true;
+    if (validPalindrome(left + 1, right)) return true;
+    if (validPalindrome(left, right - 1)) return true;
+    if (*left != *right) return false;
+    return validPalindromeWithDelete(left + 1, right - 1);
+  }
+
+  bool validPalindrome(char* left, char* right) {
+    while (left < right) {
+      if (*left != *right) return false;
+      ++left;
+      --right;
+    }
+    return true;
   }
 };
