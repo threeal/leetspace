011e8ad0047e5e71a949ae83d4082b41a6904bf3
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-18 19:30:19 +0700

    feat(problem-1190/c): solve by using stack and reverse

--- old-problems/1190/c/solution.c
+++ old-problems/1190/c/solution.c
@@ -1,3 +1,50 @@
+#include <stdlib.h>
+
 char* reverseParentheses(char* s) {
+  // Reverse substrings based on the parentheses.
+  int openersMaxSize = 32;
+  int openersSize = 0;
+  char** openers = malloc(openersMaxSize * sizeof(char*));
+
+  for (char* c = s; *c != 0; ++c) {
+    switch (*c) {
+      case '(':
+        openers[openersSize] = c;
+        if (++openersSize >= openersMaxSize) {
+          openersMaxSize *= 8;
+          openers = realloc(openers, openersMaxSize * sizeof(char*));
+        }
+        break;
+
+      case ')':
+        --openersSize;
+        char* left = openers[openersSize] + 1;
+        char* right = c - 1;
+        while (left < right) {
+          char tmp = *left;
+          *left = *right;
+          *right = tmp;
+          ++left;
+          --right;
+        }
+    }
+  }
+  free(openers);
+
+  // Remove any parentheses from the string.
+  char* out = s;
+  for (char* c = s; *c != 0; ++c) {
+    switch (*c) {
+      case ')':
+      case '(':
+        break;
+
+      default:
+        *out = *c;
+        ++out;
+    }
+  }
+  *out = 0;
+
   return s;
 }
