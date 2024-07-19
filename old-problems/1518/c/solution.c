a58c60c480e1c183eed8a13b4c5d2b40a7a6edc6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-19 19:28:15 +0700

    feat: solve problem 1518. Water Bottles in C (#1290)
    * test(problem-1518): add support to solve in C
    
    * feat(problem-1518/c): solve by repeatedly dividing water bottles

---
+++ old-problems/1518/c/solution.c
@@ -0,0 +1,8 @@
+int numWaterBottles(int numBottles, int numExchange) {
+  int drinkBottles = numBottles;
+  while (numBottles >= numExchange) {
+    drinkBottles += numBottles / numExchange;
+    numBottles = numBottles / numExchange + numBottles % numExchange;
+  }
+  return drinkBottles;
+}
