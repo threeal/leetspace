c995da4b410ec5d193d408d82f8d9c825bc9c51b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-19 20:11:55 +0700

    test: add test for problem 2751. Robot Collisions

---
+++ old-problems/2751/solution.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> survivedRobotsHealths(
+      std::vector<int>& /*positions*/,
+      std::vector<int>& healths,
+      std::string /*directions*/) {
+    return healths;
+  }
+};
