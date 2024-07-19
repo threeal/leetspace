990e27828e9381e228b159b6f9f4f3f564a825a6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-19 20:28:57 +0700

    feat(problem-485/cpp): solve by using iteration

--- old-problems/0485/solution.cpp
+++ old-problems/0485/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   int findMaxConsecutiveOnes(std::vector<int>& nums) {
-    return nums.size();
+    int ones{0};
+    int maxOnes{0};
+    for (const auto num : nums) {
+      if (num == 1) {
+        ++ones;
+      } else {
+        if (maxOnes < ones) maxOnes = ones;
+        ones = 0;
+      }
+    }
+    return ones > maxOnes ? ones : maxOnes;
   }
 };
