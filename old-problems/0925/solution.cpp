4eb5e903149abdd9b8104182ab212eabfba3b370
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-19 20:08:59 +0700

    feat(problem-925/cpp): solve by iterating given strings

--- old-problems/0925/solution.cpp
+++ old-problems/0925/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   bool isLongPressedName(std::string name, std::string typed) {
-    return name == typed;
+    char prevT = 0;
+    auto t = typed.data();
+
+    for (const auto c : name) {
+      while (c != *t && prevT == *t) ++t;
+      if (c != *t) return false;
+      prevT = *t;
+      ++t;
+    }
+    while (prevT == *t) ++t;
+
+    return *t == 0;
   }
 };
