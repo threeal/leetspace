490705a644b736bc4c8ea899dadf96a4a76cc95f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-20 00:19:54 +0700

    feat(problem-2135/cpp): solve by comparing char masks existence

--- old-problems/2135/solution.cpp
+++ old-problems/2135/solution.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <string>
 #include <vector>
 
@@ -6,6 +7,30 @@ class Solution {
   int wordCount(
       std::vector<std::string>& startWords,
       std::vector<std::string>& targetWords) {
-    return startWords.size() * targetWords.size();
+    std::vector<bool> startMaskExist(1 << 26, false);
+    for (const auto& word : startWords) {
+      std::uint32_t mask{0};
+      for (const auto c : word) {
+        mask |= 1 << (c - 'a');
+      }
+      startMaskExist[mask] = true;
+    }
+
+    int count{0};
+    for (const auto& word : targetWords) {
+      std::uint32_t mask{0};
+      for (const auto c : word) {
+        mask |= 1 << (c - 'a');
+      }
+
+      for (std::uint32_t i = 1 << 25; i > 0; i = i >> 1) {
+        if (mask & i && startMaskExist[mask ^ i]) {
+          ++count;
+          break;
+        }
+      }
+    }
+
+    return count;
   }
 };
