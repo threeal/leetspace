b4e5fae58d45de2527de435c7b4c496a9edbacbb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-20 22:36:32 +0700

    feat(problem-657/c): solve by tracking robot position

--- old-problems/0657/c/solution.c
+++ old-problems/0657/c/solution.c
@@ -1,5 +1,25 @@
 #include <stdbool.h>
 
 bool judgeCircle(char* moves) {
-  return moves == 0;
+  int horizontal = 0, vertical = 0;
+  while (*moves != 0) {
+    switch (*moves) {
+      case 'L':
+        --horizontal;
+        break;
+
+      case 'R':
+        ++horizontal;
+        break;
+
+      case 'U':
+        --vertical;
+        break;
+
+      case 'D':
+        ++vertical;
+    }
+    ++moves;
+  }
+  return horizontal == 0 && vertical == 0;
 }
