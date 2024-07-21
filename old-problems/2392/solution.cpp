d3721831545e13e246f406420d504eed6042947c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-21 12:31:36 +0700

    test: add test for problem 2392. Build a Matrix With Conditions

---
+++ old-problems/2392/solution.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> buildMatrix(
+      int /*k*/,
+      std::vector<std::vector<int>>& /*rowConditions*/,
+      std::vector<std::vector<int>>& /*colConditions*/) {
+    return {};
+  }
+};
