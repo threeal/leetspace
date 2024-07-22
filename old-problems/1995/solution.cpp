92fb5421a3b4e5f9415a1c0b225fdb9a2703cd04
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-22 10:42:24 +0700

    feat(problem-1995/cpp): solve by using brute force

--- old-problems/1995/solution.cpp
+++ old-problems/1995/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   int countQuadruplets(std::vector<int>& nums) {
-    return nums.size() - 3;
+    int count{0};
+    for (int d = nums.size() - 1; d >= 3; --d) {
+      for (int c{d - 1}; c >= 2; --c) {
+        for (int b{c - 1}; b >= 1; --b) {
+          for (int a{b - 1}; a >= 0; --a) {
+            if (nums[a] + nums[b] + nums[c] == nums[d]) ++count;
+          }
+        }
+      }
+    }
+    return count;
   }
 };
