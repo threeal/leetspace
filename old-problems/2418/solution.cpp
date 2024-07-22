9cc45681c3b456b9e5d52a310714f7c0317340f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-22 08:17:47 +0700

    test: add test for problem 2418. Sort the People

---
+++ old-problems/2418/solution.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::string> sortPeople(
+      std::vector<std::string>& names, std::vector<int>& /*heights*/) {
+    return names;
+  }
+};
