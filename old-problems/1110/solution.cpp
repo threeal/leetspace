f83c6d06e5efa3908cd827423fbf14b37d72184f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-22 14:39:11 +0700

    perf(problem-1110): return boolean to indicate if node is deleted

--- old-problems/1110/solution.cpp
+++ old-problems/1110/solution.cpp
@@ -11,36 +11,40 @@ class Solution {
     }
 
     std::vector<TreeNode*> trees{};
-    collectTrees(trees, &root, deleteList);
-    if (root != nullptr) trees.push_back(root);
+    const bool deleted = collectTrees(trees, root, deleteList);
+    if (!deleted) trees.push_back(root);
 
     return trees;
   }
 
-  void collectTrees(
+  bool collectTrees(
       std::vector<TreeNode*>& trees,
-      TreeNode** node,
+      TreeNode* node,
       std::unordered_set<int>& deleteList) {
-    if (deleteList.contains((*node)->val)) {
-      if ((*node)->right != nullptr) {
-        collectTrees(trees, &((*node)->right), deleteList);
-        if (((*node)->right) != nullptr) trees.push_back(((*node)->right));
+    if (deleteList.contains(node->val)) {
+      if (node->right != nullptr) {
+        const bool deleted = collectTrees(trees, node->right, deleteList);
+        if (!deleted) trees.push_back(node->right);
       }
 
-      if ((*node)->left != nullptr) {
-        collectTrees(trees, &((*node)->left), deleteList);
-        if (((*node)->left) != nullptr) trees.push_back(((*node)->left));
+      if (node->left != nullptr) {
+        const bool deleted = collectTrees(trees, node->left, deleteList);
+        if (!deleted) trees.push_back(node->left);
       }
 
-      *node = nullptr;
+      return true;
     } else {
-      if ((*node)->right != nullptr) {
-        collectTrees(trees, &((*node)->right), deleteList);
+      if (node->right != nullptr) {
+        const bool deleted = collectTrees(trees, node->right, deleteList);
+        if (deleted) node->right = nullptr;
       }
 
-      if ((*node)->left != nullptr) {
-        collectTrees(trees, &((*node)->left), deleteList);
+      if (node->left != nullptr) {
+        const bool deleted = collectTrees(trees, node->left, deleteList);
+        if (deleted) node->left = nullptr;
       }
+
+      return false;
     }
   }
 };
