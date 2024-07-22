2ba36e71a1a590319d478262f25610e24b2528f6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-22 14:00:00 +0700

    feat(problem-1668/cpp): solve by using brute force

--- old-problems/1668/solution.cpp
+++ old-problems/1668/solution.cpp
@@ -3,6 +3,21 @@
 class Solution {
  public:
   int maxRepeating(std::string sequence, std::string word) {
-    return sequence.size() / word.size();
+    int maxRepeating{0};
+    for (int i = word.size() - 1; i >= 0; --i) {
+      int repeating{0};
+      for (int j = sequence.size() - word.size() - i; j >= 0; j -= word.size()) {
+        for (int k = word.size() - 1; k >= 0; --k) {
+          if (sequence[j + k] != word[k]) {
+            repeating = -1;
+            break;
+          }
+        }
+        if (++repeating > 0) {
+          if (maxRepeating < repeating) maxRepeating = repeating;
+        }
+      }
+    }
+    return maxRepeating;
   }
 };
