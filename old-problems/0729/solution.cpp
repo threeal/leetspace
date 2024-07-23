15157410ec25f308ff7ce63dfe0eaca44f017371
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-23 14:36:10 +0700

    DONT MERGE

--- old-problems/0729/solution.cpp
+++ old-problems/0729/solution.cpp
@@ -1,8 +1,30 @@
+#include <algorithm>
+#include <vector>
+
 class MyCalendar {
+ private:
+  struct Schedule {
+    int start;
+    int end;
+  };
+
+  std::vector<Schedule> schedules;
+
  public:
-  MyCalendar() {}
+  MyCalendar() : schedules{} {}
 
   bool book(int start, int end) {
+    const auto it = std::lower_bound(
+      schedules.begin(), schedules.end(), start,
+      [](const Schedule& schedule, int val) {
+        return schedule.start < val;
+      });
+
+    if (it == schedules.end()) {
+      schedules.insert(it, {.start=start, .end=end});
+      return true;
+    }
+
     return start == end;
   }
 };
