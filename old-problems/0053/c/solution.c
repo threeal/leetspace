8b369e5255274a5faabd2c815563cf1bd05e7f1b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-23 16:03:49 +0700

    feat(problem-53/c): solve by using Kadane's algorithm

--- old-problems/0053/c/solution.c
+++ old-problems/0053/c/solution.c
@@ -1,3 +1,11 @@
+#include <limits.h>
+
 int maxSubArray(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int sum = 0, maxSum = INT_MIN;
+  for (int i = numsSize - 1; i >= 0; --i) {
+    sum += nums[i];
+    if (sum > maxSum) maxSum = sum;
+    if (sum < 0) sum = 0;
+  }
+  return maxSum;
 }
