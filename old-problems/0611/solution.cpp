afae8786a85651916013c0f4596ad978d90424cb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-23 20:35:34 +0700

    feat(problem-611/cpp): solve by iterating after sorting the number

--- old-problems/0611/solution.cpp
+++ old-problems/0611/solution.cpp
@@ -1,8 +1,20 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int triangleNumber(std::vector<int>& nums) {
-    return nums.size();
+    std::sort(nums.begin(), nums.end());
+
+    int count{0};
+    for (int c = nums.size() - 1; c >= 2; --c) {
+      for (int b{c - 1}; b >= 1; --b) {
+        int a{0};
+        while (a < b && nums[a] + nums[b] <= nums[c]) ++a;
+        count += b - a;
+      }
+    }
+
+    return count;
   }
 };
