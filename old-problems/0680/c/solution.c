f3ee1d27b3ca6ca7db42dec8a8249a809370666f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-23 20:09:58 +0700

    feat(problem-680/c): solve by checking palindrome recursively

--- old-problems/0680/c/solution.c
+++ old-problems/0680/c/solution.c
@@ -1,5 +1,26 @@
 #include <stdbool.h>
+#include <string.h>
+
+bool checkValidPalindromeWithDelete(char* left, char* right);
+bool checkValidPalindrome(char* left, char* right);
 
 bool validPalindrome(char* s) {
-  return *s == 0;
+  return checkValidPalindromeWithDelete(s, s + strlen(s) - 1);
+}
+
+bool checkValidPalindromeWithDelete(char* left, char* right) {
+  if (left >= right) return true;
+  if (checkValidPalindrome(left + 1, right)) return true;
+  if (checkValidPalindrome(left, right - 1)) return true;
+  if (*left != *right) return false;
+  return checkValidPalindromeWithDelete(left + 1, right - 1);
+}
+
+bool checkValidPalindrome(char* left, char* right) {
+  while (left < right) {
+    if (*left != *right) return false;
+    ++left;
+    --right;
+  }
+  return true;
 }
