f7da2b375a152e9dfac095580fa9ae290c679e9b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-24 22:24:37 +0700

    feat(problem-1109/cpp): solve by calculate seats increment

--- old-problems/1109/solution.cpp
+++ old-problems/1109/solution.cpp
@@ -3,7 +3,19 @@
 class Solution {
  public:
   std::vector<int> corpFlightBookings(
-      std::vector<std::vector<int>>& /*bookings*/, int n) {
-    return std::vector(n, 0);
+      std::vector<std::vector<int>>& bookings, int n) {
+    std::vector<int> seats(n + 1, 0);
+
+    for (const auto& booking : bookings) {
+      seats[booking[0] - 1] += booking[2];
+      seats[booking[1]] -= booking[2];
+    }
+
+    for (int i{1}; i < n; ++i) {
+      seats[i] += seats[i - 1];
+    }
+
+    seats.pop_back();
+    return seats;
   }
 };
