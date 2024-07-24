7279ac79c56892ad2ab358e93494855993226c2e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-24 22:29:58 +0700

    feat(problem-1605/cpp): solve by putting element for each row sum and col sum

--- old-problems/1605/solution.cpp
+++ old-problems/1605/solution.cpp
@@ -4,6 +4,27 @@ class Solution {
  public:
   std::vector<std::vector<int>> restoreMatrix(
       std::vector<int>& rowSum, std::vector<int>& colSum) {
-    return {rowSum, colSum};
+    std::vector<std::vector<int>> mat(
+        rowSum.size(), std::vector<int>(colSum.size(), 0));
+
+    int row = rowSum.size() - 1;
+    int col = colSum.size() - 1;
+    while (row >= 0 && col >= 0) {
+      if (rowSum[row] == colSum[col]) {
+        mat[row][col] = rowSum[row];
+        --row;
+        --col;
+      } else if (rowSum[row] < colSum[col]) {
+        mat[row][col] = rowSum[row];
+        colSum[col] -= rowSum[row];
+        --row;
+      } else {
+        mat[row][col] = colSum[col];
+        rowSum[row] -= colSum[col];
+        --col;
+      }
+    }
+
+    return mat;
   }
 };
