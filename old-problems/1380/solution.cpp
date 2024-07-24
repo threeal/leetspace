002b069c3597c75090d170f5855a3251acde811a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-24 20:46:13 +0700

    feat(problem-1380/cpp): solve by iterating through the matrix

--- old-problems/1380/solution.cpp
+++ old-problems/1380/solution.cpp
@@ -3,6 +3,22 @@
 class Solution {
  public:
   std::vector<int> luckyNumbers(std::vector<std::vector<int>>& matrix) {
-    return matrix.front();
+    std::vector<int> numbers{};
+    for (const auto& row : matrix) {
+      std::size_t min{0};
+      for (std::size_t i{row.size() - 1}; i > 0; --i) {
+        if (row[i] < row[min]) min = i;
+      }
+
+      for (const auto& otherRow : matrix) {
+        if (otherRow[min] > row[min]) {
+          min = row.size();
+          break;
+        }
+      }
+
+      if (min < row.size()) numbers.push_back(row[min]);
+    }
+    return numbers;
   }
 };
