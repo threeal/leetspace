051a8b271e4553d486fcf0580ea124123a1b60ec
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-25 12:25:53 +0700

    feat(problem-1605/c): solve by putting element for each row sum and col sum

--- old-problems/1605/c/solution.c
+++ old-problems/1605/c/solution.c
@@ -4,15 +4,30 @@ int** restoreMatrix(
     int* rowSum, int rowSumSize,
     int* colSum, int colSumSize,
     int* returnSize, int** returnColumnSizes) {
-  (void)rowSum;
-  (void)colSum;
-
   *returnSize = rowSumSize;
   int** returnData = malloc(rowSumSize * sizeof(int*));
   *returnColumnSizes = malloc(rowSumSize * sizeof(int));
-  for (int i = colSumSize - 1; i >= 0; --i) {
-    returnData[i] = malloc(colSumSize * sizeof(int));
+  for (int i = rowSumSize - 1; i >= 0; --i) {
+    returnData[i] = calloc(colSumSize, sizeof(int));
     (*returnColumnSizes)[i] = colSumSize;
+  }
+
+  int row = rowSumSize - 1;
+  int col = colSumSize - 1;
+  while (row >= 0 && col >= 0) {
+    if (rowSum[row] == colSum[col]) {
+      returnData[row][col] = rowSum[row];
+      --row;
+      --col;
+    } else if (rowSum[row] < colSum[col]) {
+      returnData[row][col] = rowSum[row];
+      colSum[col] -= rowSum[row];
+      --row;
+    } else {
+      returnData[row][col] = colSum[col];
+      rowSum[row] -= colSum[col];
+      --col;
+    }
   }
 
   return returnData;
