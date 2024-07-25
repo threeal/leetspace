707e894fc12c3cf8711ff1eed3dc81f11b64f70d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-25 19:30:24 +0700

    test: add test for problem 2545. Sort the Students by Their Kth Score

---
+++ old-problems/2545/solution.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> sortTheStudents(
+      std::vector<std::vector<int>>& score, int /*k*/) {
+    return score;
+  }
+};
