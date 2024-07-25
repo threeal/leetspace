abaf3a82a02a671a66169d20d847841371e9db9a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-25 16:53:51 +0700

    feat(problem-137/cpp): solve by using magic

--- old-problems/0137/solution.cpp
+++ old-problems/0137/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   int singleNumber(std::vector<int>& nums) {
-    return nums.front();
+    int onces{0};
+    int twices{0};
+    for (const auto num : nums) {
+      onces ^= num & ~twices;
+      twices ^= num & ~onces;
+    }
+    return onces;
   }
 };
