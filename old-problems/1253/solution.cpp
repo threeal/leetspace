247ed6910a5ab55c2523865c3a883601bbb5dcd9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-25 12:29:26 +0700

    test: add test for problem 1253. Reconstruct a 2-Row Binary Matrix

---
+++ old-problems/1253/solution.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> reconstructMatrix(
+      int /*upper*/, int /*lower*/, std::vector<int>& /*colsum*/) {
+    return {};
+  }
+};
