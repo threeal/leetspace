4bd42787691129021519da901e0abbdf555a935c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-25 13:18:06 +0700

    feat(problem-3137/cpp): solve by counting substring frequencies

--- old-problems/3137/solution.cpp
+++ old-problems/3137/solution.cpp
@@ -1,8 +1,19 @@
 #include <string>
+#include <unordered_map>
 
 class Solution {
  public:
   int minimumOperationsToMakeKPeriodic(std::string word, int k) {
-    return word.size() / k;
+    std::unordered_map<std::string, int> frequencies{};
+    for (int i = word.size() - k; i >= 0; i -= k) {
+      ++frequencies[word.substr(i, k)];
+    }
+
+    int highestFrequency{0};
+    for (const auto& [word, frequency] : frequencies) {
+      if (frequency > highestFrequency) highestFrequency = frequency;
+    }
+
+    return word.size() / k - highestFrequency;
   }
 };
