c421726c616ac92032ecedeed44f274680f632b5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-26 10:22:57 +0700

    test: add test for problem 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance

---
+++ old-problems/1334/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  int findTheCity(
+      int n, std::vector<std::vector<int>>& /*edges*/,
+      int /*distanceThreshold*/) {
+    return n;
+  }
+};
