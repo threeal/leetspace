cf9e6b631c2dee34e2be1475ded19eaf00a97f1d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-26 15:37:25 +0700

    feat(problem-611/c): solve by using sorting and two pointers

--- old-problems/0611/c/solution.c
+++ old-problems/0611/c/solution.c
@@ -1,4 +1,25 @@
-int triangleNumber(int* nums, int numsSize) {
-  (void)nums;
-  return numsSize;
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+  return *(int *)a - *(int *)b;
+}
+
+int triangleNumber(int *nums, int numsSize) {
+  qsort(nums, numsSize, sizeof(int), compare);
+
+  int count = 0;
+  for (int c = numsSize - 1; c >= 2; --c) {
+    int a = 0;
+    int b = c - 1;
+    while (a < b) {
+      if (nums[a] + nums[b] > nums[c]) {
+        count += b - a;
+        --b;
+      } else {
+        ++a;
+      }
+    }
+  }
+
+  return count;
 }
