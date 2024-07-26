e47c85a889c9242b90debf93782ff7ca4c340c77
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-26 15:18:10 +0700

    test(problem-2497): add support to test in C

---
+++ old-problems/2497/c/solution.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+int maxStarSum(
+    int* vals, int valsSize,
+    int** edges, int edgesSize, int* edgesColSize, int k) {
+  (void)vals;
+  (void)edges;
+  (void)edgesSize;
+  (void)edgesColSize;
+  (void)k;
+  return valsSize;
+}
