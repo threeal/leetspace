3d944c7c0699d3888c51260caeaca9e69725523f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-26 15:59:22 +0700

    feat(problem-541/cpp): solve by iterating in intervals

--- old-problems/0541/solution.cpp
+++ old-problems/0541/solution.cpp
@@ -1,8 +1,12 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
-  std::string reverseStr(std::string s, int /*k*/) {
+  std::string reverseStr(std::string s, int k) {
+    for (std::size_t i{0}; i < s.size(); i += k + k) {
+      std::reverse(s.begin() + i, s.begin() + std::min(s.size(), i + k));
+    }
     return s;
   }
 };
