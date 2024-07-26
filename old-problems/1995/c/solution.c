c544b34153be125a8a162751cebae5d4982d8058
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-26 10:39:38 +0700

    feat(problem-1995/c): solve by using brute force

--- old-problems/1995/c/solution.c
+++ old-problems/1995/c/solution.c
@@ -1,4 +1,13 @@
 int countQuadruplets(int* nums, int numsSize) {
-  (void)nums;
-  return numsSize;
+  int count = 0;
+  for (int d = numsSize - 1; d >= 3; --d) {
+    for (int c = d - 1; c >= 2; --c) {
+      for (int b = c - 1; b >= 1; --b) {
+        for (int a = b - 1; a >= 0; --a) {
+          if (nums[a] + nums[b] + nums[c] == nums[d]) ++count;
+        }
+      }
+    }
+  }
+  return count;
 }
