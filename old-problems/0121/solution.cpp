10eb3259ac83db12b648de6f705217d244899a7d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-26 15:30:05 +0700

    feat(problem-121/cpp): solve by tracking lowest price

--- old-problems/0121/solution.cpp
+++ old-problems/0121/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int maxProfit(std::vector<int>& prices) {
-    return prices.front();
+    int highestProfit{0};
+    int lowestPrice{10001};
+    for (const auto price : prices) {
+      if (price < lowestPrice) {
+        lowestPrice = price;
+      } else if (price - lowestPrice > highestProfit) {
+        highestProfit = price - lowestPrice;
+      }
+    }
+    return highestProfit;
   }
 };
