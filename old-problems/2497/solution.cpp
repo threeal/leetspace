2e55bf21bb93c9a904bdf5ec8dc8f98350dd21c1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-26 10:32:47 +0700

    test: add test for problem 2497. Maximum Star Sum of a Graph

---
+++ old-problems/2497/solution.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+class Solution {
+ public:
+  int maxStarSum(
+      std::vector<int>& vals,
+      std::vector<std::vector<int>>& /*edges*/,
+      int /*k*/) {
+    return vals[0];
+  }
+};
