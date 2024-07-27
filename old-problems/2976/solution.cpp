7730ba73b759227f1c67da94a696b8701e67bc0f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-27 20:08:31 +0700

    test: add test for problem 2976. Minimum Cost to Convert String I

---
+++ old-problems/2976/solution.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  long long minimumCost(
+      std::string /*source*/,
+      std::string /*target*/,
+      std::vector<char>& /*original*/,
+      std::vector<char>& /*changed*/,
+      std::vector<int>& /*cost*/) {
+    return 0;
+  }
+};
