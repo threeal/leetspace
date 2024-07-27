edd56f7baaeb8d6ce5f1287d5582408f46b0dc13
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-27 20:01:09 +0700

    feat(problem-1380/c): solve by iterating through the matrix

--- old-problems/1380/c/solution.c
+++ old-problems/1380/c/solution.c
@@ -1,5 +1,26 @@
+#include <stdlib.h>
+
 int* luckyNumbers(
     int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
-  *returnSize = matrixColSize[matrixSize - 1];
-  return matrix[matrixSize - 1];
+  *returnSize = 0;
+  int* numbers = malloc(matrixSize * matrixColSize[0] * sizeof(int));
+  for (int i = matrixSize - 1; i >= 0; --i) {
+    int min = 0;
+    for (int j = matrixColSize[i] - 1; j > 0; --j) {
+      if (matrix[i][j] < matrix[i][min]) min = j;
+    }
+
+    for (int ii = matrixSize - 1; ii >= 0; --ii) {
+      if (matrix[ii][min] > matrix[i][min]) {
+        min = -1;
+        break;
+      }
+    }
+
+    if (min >= 0) {
+      numbers[*returnSize] = matrix[i][min];
+      ++(*returnSize);
+    }
+  }
+  return numbers;
 }
