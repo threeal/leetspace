69fdb89c853a34beb9d2755adc05b224be82ab89
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-28 20:46:59 +0700

    feat(problem-2545/c): solve by using a custom comparator for sorting

--- old-problems/2545/c/solution.c
+++ old-problems/2545/c/solution.c
@@ -1,7 +1,18 @@
+#include <stdlib.h>
+
+int gk;
+
+int compare(const void* a, const void* b) {
+  return (*(int**)b)[gk] - (*(int**)a)[gk];
+}
+
 int** sortTheStudents(
     int** score, int scoreSize, int* scoreColSize, int k,
     int* returnSize, int** returnColumnSizes) {
-  (void)k;
+  gk = k;
+
+  qsort(score, scoreSize, sizeof(int*), compare);
+
   *returnSize = scoreSize;
   *returnColumnSizes = scoreColSize;
   return score;
