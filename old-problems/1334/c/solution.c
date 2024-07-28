356d9a7e8d0b70a47fc6fb59b1b9678531cea42a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-28 21:11:34 +0700

    test(problem-1334): add support to test in C

---
+++ old-problems/1334/c/solution.c
@@ -0,0 +1,9 @@
+int findTheCity(
+    int n, int** edges, int edgesSize, int* edgesColSize,
+    int distanceThreshold) {
+  (void)edges;
+  (void)edgesSize;
+  (void)edgesColSize;
+  (void)distanceThreshold;
+  return n;
+}
