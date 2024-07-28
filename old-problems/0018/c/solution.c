057e73955811ec14678207498369e1ed2d1d082d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-28 21:39:36 +0700

    test(problem-18): add support to test in C

---
+++ old-problems/0018/c/solution.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+int** fourSum(
+    int* nums, int numsSize, int target,
+    int* returnSize, int** returnColumnSizes) {
+  (void)nums;
+  (void)numsSize;
+  (void)target;
+  *returnSize = 0;
+  *returnColumnSizes = NULL;
+  return NULL;
+}
