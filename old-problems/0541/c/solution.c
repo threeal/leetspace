08907af243ac5249c75728fb825b26e5495fce27
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-29 11:30:03 +0700

    feat(problem-541/c): solve by iterating in intervals

--- old-problems/0541/c/solution.c
+++ old-problems/0541/c/solution.c
@@ -1,4 +1,18 @@
+#include <string.h>
+
 char* reverseStr(char* s, int k) {
-  (void)k;
+  const int n = strlen(s);
+  for (int i = 0; i < n; i += k + k) {
+    int left = i;
+    int right = i + k - 1;
+    if (right >= n) right = n - 1;
+    while (left < right) {
+      const char temp = s[left];
+      s[left] = s[right];
+      s[right] = temp;
+      ++left;
+      --right;
+    }
+  }
   return s;
 }
