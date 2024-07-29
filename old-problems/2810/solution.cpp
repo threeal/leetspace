284c4bd28cd5a3478701365ef5c19bf579e2d6fd
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-29 11:42:19 +0700

    feat(problem-2810/cpp): solve by reversing string whenever it encounters `i`

--- old-problems/2810/solution.cpp
+++ old-problems/2810/solution.cpp
@@ -1,8 +1,18 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   std::string finalString(std::string s) {
-    return s;
+    std::size_t out{0};
+    for (std::size_t i = 0; i < s.size(); ++i) {
+      if (s[i] == 'i') {
+        std::reverse(s.begin(), s.begin() + out);
+      } else {
+        s[out] = s[i];
+        ++out;
+      }
+    }
+    return s.substr(0, out);
   }
 };
