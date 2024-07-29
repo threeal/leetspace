17e3e2b68361941513a52e862695ed1056e43a14
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-29 17:42:18 +0700

    feat(problem-1046/cpp): solve by using priority queue

--- old-problems/1046/solution.cpp
+++ old-problems/1046/solution.cpp
@@ -1,8 +1,17 @@
+#include <queue>
 #include <vector>
 
 class Solution {
  public:
   int lastStoneWeight(std::vector<int>& stones) {
-    return stones.front();
+    std::priority_queue<int> queue(stones.begin(), stones.end());
+    while (queue.size() > 1) {
+      int stone{queue.top()};
+      queue.pop();
+      stone -= queue.top();
+      queue.pop();
+      if (stone > 0) queue.push(stone);
+    }
+    return queue.empty() ? 0 : queue.top();
   }
 };
