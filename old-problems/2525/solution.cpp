e6b79441d520b3304748865bf409463342ffc93a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-30 20:13:32 +0700

    feat(problem-2525/cpp): solve by categorizing inputs

--- old-problems/2525/solution.cpp
+++ old-problems/2525/solution.cpp
@@ -2,8 +2,11 @@
 
 class Solution {
  public:
-  std::string categorizeBox(
-      int /*length*/, int /*width*/, int /*height*/, int /*mass*/) {
-    return "Neither";
+  std::string categorizeBox(int length, int width, int height, int mass) {
+    if (length >= 10000 || width >= 10000 || height >= 10000 ||
+        static_cast<long long>(length) * width * height >= 1000000000) {
+      return mass >= 100 ? "Both" : "Bulky";
+    }
+    return mass >= 100 ? "Heavy" : "Neither";
   }
 };
