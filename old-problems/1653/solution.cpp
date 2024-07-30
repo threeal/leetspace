78b36709aa773ab57f5da24b9d020b3e4eb1588f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-30 10:14:58 +0700

    feat(problem-1653/cpp): solve by countng left and right characters

--- old-problems/1653/solution.cpp
+++ old-problems/1653/solution.cpp
@@ -3,6 +3,13 @@
 class Solution {
  public:
   int minimumDeletions(std::string s) {
-    return s.size();
+    int deletions{0};
+    int aCount{0};
+    for (int i = s.size() - 2; i >= 0; --i) {
+      if (s[i] == 'b') ++deletions;
+      if (s[i + 1] == 'a') ++aCount;
+      if (aCount < deletions) deletions = aCount;
+    }
+    return deletions;
   }
 };
