4befc5c2f66fd6d844ae4bff5f7f35adc6a3cfc8
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-30 12:05:28 +0700

    feat(problem-454/cpp): solve by precalculate first pairs count

--- problems/0454/solution.cpp
+++ problems/0454/solution.cpp
@@ -1,3 +1,4 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
@@ -5,6 +6,21 @@ class Solution {
   int fourSumCount(
       std::vector<int>& nums1, std::vector<int>& nums2,
       std::vector<int>& nums3, std::vector<int>& nums4) {
-    return nums1.size() + nums2.size() + nums3.size() + nums4.size();
+    std::unordered_map<int, int> firstPairsCount{};
+    for (const auto i : nums1) {
+      for (const auto j : nums2) {
+        ++firstPairsCount[i + j];
+      }
+    }
+
+    int count{0};
+    for (const auto k : nums3) {
+      for (const auto l : nums4) {
+        const auto it = firstPairsCount.find(-k - l);
+        if (it != firstPairsCount.end()) count += it->second;
+      }
+    }
+
+    return count;
   }
 };
