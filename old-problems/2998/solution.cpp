8dc12ceb9d6b6d0ea1d25f5cd877b158eca96592
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-30 11:34:36 +0700

    feat(problem-2998/cpp): solve by checking all possibilities recursively

--- old-problems/2998/solution.cpp
+++ old-problems/2998/solution.cpp
@@ -1,6 +1,15 @@
+#include <algorithm>
+
 class Solution {
  public:
   int minimumOperationsToMakeEqual(int x, int y) {
-    return x + y;
+    if (x <= y) return y - x;
+    const int eleven = x % 11 <= 5
+        ? x % 11 + minimumOperationsToMakeEqual(x / 11, y)
+        : 11 - x % 11 + minimumOperationsToMakeEqual(x / 11 + 1, y);
+    const int five = x % 5 <= 2
+        ? x % 5 + minimumOperationsToMakeEqual(x / 5, y)
+        : 5 - x % 5 + minimumOperationsToMakeEqual(x / 5 + 1, y);
+    return std::min(x - y, 1 + std::min(eleven, five));
   }
 };
