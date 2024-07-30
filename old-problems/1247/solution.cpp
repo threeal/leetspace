ac214e85356977229954d8155a001cc597ca2d52
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-30 11:12:14 +0700

    feat(problem-1247/cpp): solve by counting xs and ys

--- old-problems/1247/solution.cpp
+++ old-problems/1247/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   int minimumSwap(std::string s1, std::string s2) {
-    return s1.size() < s2.size() ? s1.size() : s2.size();
+    int x1{0}, y1{0};
+    for (int i = s1.size() - 1; i >= 0; --i) {
+      if (s1[i] != s2[i]) {
+        if (s1[i] == 'x') {
+          ++x1;
+        } else {
+          ++y1;
+        }
+      }
+    }
+
+    return x1 % 2 == 0
+        ? (y1 % 2 == 0 ? (x1 + y1) / 2 : -1)
+        : (y1 % 2 == 0 ? -1 : (x1 + y1) / 2 + 1);
   }
 };
