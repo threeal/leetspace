8f50c6bdcf62b071979d24449c55b442effd3e14
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-31 20:16:51 +0700

    feat(problem-1209/c): solve by modifying string in-place

--- old-problems/1209/c/solution.c
+++ old-problems/1209/c/solution.c
@@ -1,4 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+
 char* removeDuplicates(char* s, int k) {
-  (void)k;
+  int n = strlen(s);
+  int* freqs = malloc(n * sizeof(int));
+
+  int out = 0;
+  for (int i = 0; i < n; ++i) {
+    while (
+        out > 0 && i < n &&
+        s[out - 1] == s[i] && freqs[out - 1] + 1 >= k) {
+      --out;
+      ++i;
+    }
+    if (i < n) {
+      if (out > 0 && s[out - 1] == s[i]) {
+        ++freqs[out - 1];
+      } else {
+        s[out] = s[i];
+        freqs[out] = 1;
+        ++out;
+      }
+    }
+  }
+
+  int pos = 0;
+  for (int i = out - 1; i >= 0; --i) {
+    pos += freqs[i];
+  }
+
+  s[pos] = 0;
+  for (int i = out - 1; i >= 0; --i) {
+    pos -= freqs[i];
+    memset(s + pos, s[i], freqs[i] * sizeof(char));
+  }
+
+  free(freqs);
+
   return s;
 }
