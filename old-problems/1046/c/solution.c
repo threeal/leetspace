e7c92475578124e5bd1cefe7834c75139750ea2d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-31 19:58:30 +0700

    feat(problem-1046/c): solve by finding two heaviest stones

--- old-problems/1046/c/solution.c
+++ old-problems/1046/c/solution.c
@@ -1,3 +1,41 @@
 int lastStoneWeight(int* stones, int stonesSize) {
-  return stones[stonesSize - 1];
+  while (stonesSize > 1) {
+    int largest = stones[0] > stones[1] ? 0 : 1;
+    int nextLargest = stones[0] > stones[1] ? 1 : 0;
+
+    for (int i = stonesSize - 1; i > 1; --i) {
+      if (stones[i] > stones[largest]) {
+        nextLargest = largest;
+        largest = i;
+      } else if (stones[i] > stones[nextLargest]) {
+        nextLargest = i;
+      }
+    }
+
+    if (stones[largest] == stones[nextLargest]) {
+      if (largest < stonesSize - 2) {
+        if (nextLargest < stonesSize - 2) {
+          stones[largest] = stones[stonesSize - 2];
+          stones[nextLargest] = stones[stonesSize - 1];
+        } else {
+          stones[largest] = nextLargest == stonesSize - 2
+              ? stones[stonesSize - 1]
+              : stones[stonesSize - 2];
+        }
+      } else {
+        if (nextLargest < stonesSize - 2) {
+          stones[nextLargest] = largest == stonesSize - 2
+              ? stones[stonesSize - 1]
+              : stones[stonesSize - 2];
+        }
+      }
+      stonesSize -= 2;
+    } else {
+      stones[largest] -= stones[nextLargest];
+      stones[nextLargest] = stones[stonesSize - 1];
+      --stonesSize;
+    }
+  }
+
+  return stonesSize == 0 ? 0 : stones[0];
 }
