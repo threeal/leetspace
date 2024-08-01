077d20839c2c8466ce6e3f459a773420c234d1d0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-01 23:54:50 +0700

    feat(problem-1247/c): solve by counting xs and ys

--- old-problems/1247/c/solution.c
+++ old-problems/1247/c/solution.c
@@ -1,5 +1,18 @@
 int minimumSwap(char* s1, char* s2) {
-  (void)s1;
-  (void)s2;
-  return 0;
+  int x1 = 0, y1 = 0;
+  while (*s1 != 0) {
+    if (*s1 != *s2) {
+      if (*s1 == 'x') {
+        ++x1;
+      } else {
+        ++y1;
+      }
+    }
+    ++s1;
+    ++s2;
+  }
+
+  return x1 % 2 == 0
+      ? (y1 % 2 == 0 ? (x1 + y1) / 2 : -1)
+      : (y1 % 2 == 0 ? -1 : (x1 + y1) / 2 + 1);
 }
