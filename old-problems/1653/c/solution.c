6d6a7a4a8e340e1febb2c1ff5c16d680db26d719
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-01 20:32:56 +0700

    feat(problem-1653/c): solve by counting left and right characters

--- old-problems/1653/c/solution.c
+++ old-problems/1653/c/solution.c
@@ -1,4 +1,12 @@
+#include <string.h>
+
 int minimumDeletions(char* s) {
-  (void)s;
-  return 0;
+  int deletions = 0;
+  int aCount = 0;
+  for (int i = strlen(s) - 2; i >= 0; --i) {
+    if (s[i] == 'b') ++deletions;
+    if (s[i + 1] == 'a') ++aCount;
+    if (aCount < deletions) deletions = aCount;
+  }
+  return deletions;
 }
