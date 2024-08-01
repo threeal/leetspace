1047f02b2baef5c81d04fafaa63e177aecbcdc18
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-01 11:31:21 +0700

    feat(problem-976/c): solve by sorting and finding three largest length

--- old-problems/0976/c/solution.c
+++ old-problems/0976/c/solution.c
@@ -1,3 +1,15 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int largestPerimeter(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  qsort(nums, numsSize, sizeof(int), compare);
+  for (int i = numsSize - 3; i >= 0; --i) {
+    if (nums[i] + nums[i + 1] > nums[i + 2]) {
+      return nums[i] + nums[i + 1] + nums[i + 2];
+    }
+  }
+  return 0;
 }
