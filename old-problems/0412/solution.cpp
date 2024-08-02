8977b9615cfb07d43833811707154886ca70e355
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-02 13:23:23 +0700

    feat(problem-412): solve by using iteration

--- old-problems/0412/solution.cpp
+++ old-problems/0412/solution.cpp
@@ -4,6 +4,23 @@
 class Solution {
  public:
   std::vector<std::string> fizzBuzz(int n) {
-    return std::vector<std::string>(n);
+    std::vector<std::string> output{};
+    output.reserve(n);
+
+    for (int i = 1; i <= n; ++i) {
+      if (i % 3 == 0) {
+        if (i % 5 == 0) {
+          output.push_back("FizzBuzz");
+        } else {
+          output.push_back("Fizz");
+        }
+      } else if (i % 5 == 0) {
+        output.push_back("Buzz");
+      } else {
+        output.push_back(std::to_string(i));
+      }
+    }
+
+    return output;
   }
 };
