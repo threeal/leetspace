355460899a96f39dddc3954a38a7221a0c17638d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-03 12:06:26 +0700

    feat(problem-1460/cpp): solve by sorting and comparing arrays

--- old-problems/1460/solution.cpp
+++ old-problems/1460/solution.cpp
@@ -1,8 +1,11 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   bool canBeEqual(std::vector<int>& target, std::vector<int>& arr) {
+    std::sort(target.begin(), target.end());
+    std::sort(arr.begin(), arr.end());
     return target == arr;
   }
 };
