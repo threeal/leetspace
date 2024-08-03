35a2476661ccd571264b249821988339866c947b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-03 21:47:23 +0700

    feat(problem-1105/c): solve by checking all possibilities to split books

--- old-problems/1105/c/solution.c
+++ old-problems/1105/c/solution.c
@@ -1,8 +1,26 @@
+#include <stdlib.h>
+
 int minHeightShelves(
     int** books, int booksSize, int* booksColSize, int shelfWidth) {
-  (void)books;
-  (void)booksSize;
   (void)booksColSize;
-  (void)shelfWidth;
-  return 0;
+
+  int* minHeights = malloc((booksSize + 1) * sizeof(int));
+  minHeights[booksSize] = 0;
+
+  for (int i = booksSize - 1; i >= 0; --i) {
+    int width = books[i][0];
+    int height = books[i][1];
+
+    minHeights[i] = height + minHeights[i + 1];
+    for (int j = i + 1; j < booksSize; ++j) {
+      width += books[j][0];
+      if (width > shelfWidth) break;
+      if (books[j][1] > height) height = books[j][1];
+      if (height + minHeights[j + 1] < minHeights[i]) {
+        minHeights[i] = height + minHeights[j + 1];
+      }
+    }
+  }
+
+  return minHeights[0];
 }
