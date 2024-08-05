db537b9e3160e842a6d473f0aa7ba29c33e27a5b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-05 19:46:30 +0700

    test: add test for problem 2467. Most Profitable Path in a Tree

---
+++ old-problems/2467/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  int mostProfitablePath(
+      std::vector<std::vector<int>>& /*edges*/, int /*bob*/,
+      std::vector<int>& /*amount*/) {
+    return 0;
+  }
+};
