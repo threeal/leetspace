93fb19ad1aff865acf4ce07d8761ca2925f57144
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-06 09:46:00 +0700

    feat(problem-3016/cpp): solve by counting frequencies

--- old-problems/3016/solution.cpp
+++ old-problems/3016/solution.cpp
@@ -1,8 +1,19 @@
+#include <algorithm>
 #include <string>
+#include <vector>
 
 class Solution {
  public:
   int minimumPushes(std::string word) {
-    return word.size();
+    std::vector freqs(26, 0);
+    for (const auto c : word) ++freqs[c - 'a'];
+
+    std::sort(freqs.begin(), freqs.end(), std::greater<int>());
+
+    int pushes{0};
+    for (int i = 0; i < 26 && freqs[i] > 0; ++i) {
+      pushes += freqs[i] * (i / 8 + 1);
+    }
+    return pushes;
   }
 };
