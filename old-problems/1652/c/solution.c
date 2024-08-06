64f95e8a099aa33e649bec74402d3c12721be759
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-06 20:42:05 +0700

    feat(problem-1652/cpp): solve by iterating code with precalculated sum

--- old-problems/1652/c/solution.c
+++ old-problems/1652/c/solution.c
@@ -1,5 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+
 int* decrypt(int* code, int codeSize, int k, int* returnSize) {
-  (void)k;
+  int* output = malloc(codeSize * sizeof(int));
   *returnSize = codeSize;
-  return code;
+
+  if (k > 0) {
+    int sum = 0;
+    for (int i = 0; i < k; ++i) sum += code[i];
+
+    for (int l = 0, r = k; r < codeSize; ++l, ++r) {
+      sum -= code[l];
+      sum += code[r];
+      output[l] = sum;
+    }
+
+    for (int l = codeSize - k, r = 0; l < codeSize; ++l, ++r) {
+      sum -= code[l];
+      sum += code[r];
+      output[l] = sum;
+    }
+  } else if (k < 0) {
+    int sum = 0;
+    for (int i = codeSize + k; i < codeSize; ++i) sum += code[i];
+
+    for (int l = codeSize + k - 1, r = codeSize - 1; l >= 0; --l, --r) {
+      sum += code[l];
+      sum -= code[r];
+      output[r] = sum;
+    }
+
+    for (int l = codeSize - 1, r = -k - 1; r >= 0; --l, --r) {
+      sum += code[l];
+      sum -= code[r];
+      output[r] = sum;
+    }
+  } else {
+    memset(output, 0, codeSize * sizeof(int));
+  }
+
+  return output;
 }
