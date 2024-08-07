a03b84d8c7df9c49ad0984104ad23ea17f956b47
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-07 23:10:09 +0700

    feat(problem-2490/cpp): solve by iterating string

--- old-problems/2490/solution.cpp
+++ old-problems/2490/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   bool isCircularSentence(std::string sentence) {
-    return sentence.empty();
+    if (sentence.front() != sentence.back()) return false;
+    for (int i = sentence.size() - 2; i > 0; --i) {
+      if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1]) {
+        return false;
+      }
+    }
+    return true;
   }
 };
