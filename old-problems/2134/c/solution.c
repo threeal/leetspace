3526ae8d8970a577bbec94f2cbbe6ba69dc1ee79
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-08 10:38:11 +0700

    feat(problem-2134/c): solve by using sliding window approach

--- old-problems/2134/c/solution.c
+++ old-problems/2134/c/solution.c
@@ -1,4 +1,34 @@
 int minSwaps(int* nums, int numsSize) {
-  (void)nums;
-  return numsSize;
+  int ones = 0;
+  for (int i = numsSize - 1; i >= 0; --i) {
+    if (nums[i] == 1) ++ones;
+  }
+  if (ones == 0) return 0;
+
+  int zeros = 0;
+  for (int i = numsSize - ones + 1; i < numsSize; ++i) {
+    if (nums[i] == 0) ++zeros;
+  }
+
+  int minZeros = ones;
+  int left = numsSize - ones;
+  int right = numsSize - 1;
+  while (left >= 0) {
+    if (nums[left] == 0) ++zeros;
+    if (zeros < minZeros) minZeros = zeros;
+    if (nums[right] == 0) --zeros;
+    --left;
+    --right;
+  }
+
+  left += numsSize;
+  while (right >= 0) {
+    if (nums[left] == 0) ++zeros;
+    if (zeros < minZeros) minZeros = zeros;
+    if (nums[right] == 0) --zeros;
+    --left;
+    --right;
+  }
+
+  return minZeros;
 }
