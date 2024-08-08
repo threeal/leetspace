a02ecffe9e8aca14c0cc1a08a63b798fe6a1f31b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-08 09:41:14 +0700

    test: add test for problem 885. Spiral Matrix III

---
+++ old-problems/0885/solution.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> spiralMatrixIII(
+      int /*rows*/, int /*cols*/, int /*rStart*/, int /*cStart*/) {
+    return {};
+  }
+};
