5787c07c98d1d566d245f02fb80cf07ad72c4181
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-10 08:49:06 +0700

    feat(problem-1195/cpp): solve by using condition variable

--- old-problems/1195/solution.cpp
+++ old-problems/1195/solution.cpp
@@ -1,22 +1,59 @@
+#include <condition_variable>
 #include <functional>
+#include <mutex>
 
 class FizzBuzz {
+ private:
+  std::mutex mtx;
+  std::condition_variable cv;
+
+  int i;
+  int n;
+
  public:
-  FizzBuzz(int /*n*/) {}
+  FizzBuzz(int n) : mtx{}, cv{}, i{1}, n(n) {}
 
   void fizz(std::function<void()> printFizz) {
-    printFizz();
+    std::unique_lock<std::mutex> lock(mtx);
+    while (i <= n) {
+      while (i <= n && (i % 3 != 0 || i % 5 == 0)) cv.wait(lock);
+      if (i > n) continue;
+      printFizz();
+      ++i;
+      cv.notify_all();
+    }
   }
 
   void buzz(std::function<void()> printBuzz) {
-    printBuzz();
+    std::unique_lock<std::mutex> lock(mtx);
+    while (i <= n) {
+      while (i <= n && (i % 3 == 0 || i % 5 != 0)) cv.wait(lock);
+      if (i > n) continue;
+      printBuzz();
+      ++i;
+      cv.notify_all();
+    }
   }
 
   void fizzbuzz(std::function<void()> printFizzBuzz) {
-    printFizzBuzz();
+    std::unique_lock<std::mutex> lock(mtx);
+    while (i <= n) {
+      while (i <= n && (i % 3 != 0 || i % 5 != 0)) cv.wait(lock);
+      if (i > n) continue;
+      printFizzBuzz();
+      ++i;
+      cv.notify_all();
+    }
   }
 
   void number(std::function<void(int)> printNumber) {
-    printNumber(1);
+    std::unique_lock<std::mutex> lock(mtx);
+    while (i <= n) {
+      while (i <= n && (i % 3 == 0 || i % 5 == 0)) cv.wait(lock);
+      if (i > n) continue;
+      printNumber(i);
+      ++i;
+      cv.notify_all();
+    }
   }
 };
