1f44b207490e823f71682fb0ca12306317f46713
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-10 09:51:56 +0700

    feat(problem-1508/c): solve by using brute force approach

--- old-problems/1508/c/solution.c
+++ old-problems/1508/c/solution.c
@@ -1,8 +1,31 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int rangeSum(int* nums, int numsSize, int n, int left, int right) {
-  (void)nums;
-  (void)numsSize;
-  (void)n;
-  (void)left;
-  (void)right;
-  return 0;
+  int sumsSize = (n * (n + 1)) / 2;
+  int* sums = malloc(sumsSize * sizeof(int));
+  int* it = sums;
+
+  for (int i = 0; i < numsSize; ++i) {
+    for (int j = 0; j < i; ++j) {
+      nums[j] += nums[i];
+      *it = nums[j];
+      ++it;
+    }
+
+    *it = nums[i];
+    ++it;
+  }
+
+  qsort(sums, sumsSize, sizeof(int), compare);
+
+  int sum = 0;
+  for (int i = left - 1; i < right; ++i) {
+    sum = (sum + sums[i]) % 1000000007;
+  }
+
+  return sum;
 }
