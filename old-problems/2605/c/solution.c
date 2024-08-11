362bd42a6dc903e8753ae86b0da775b683606ba5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-11 22:13:03 +0700

    feat(problem-2605/c): solve by using bit manipulation

--- old-problems/2605/c/solution.c
+++ old-problems/2605/c/solution.c
@@ -1,7 +1,9 @@
 int minNumber(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-  (void)nums1;
-  (void)nums1Size;
-  (void)nums2;
-  (void)nums2Size;
-  return 0;
+  int first = 0, second = 0;
+  for (int i = nums1Size - 1; i >= 0; --i) first |= 1 << nums1[i];
+  for (int i = nums2Size - 1; i >= 0; --i) second |= 1 << nums2[i];
+  if ((first & second) > 0) return __builtin_ctz(first & second);
+  first = __builtin_ctz(first);
+  second = __builtin_ctz(second);
+  return first < second ? first * 10 + second : second * 10 + first;
 }
