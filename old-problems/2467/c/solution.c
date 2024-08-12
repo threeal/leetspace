041ebf49599bc3210a657e3142d8987318140ea8
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-12 21:57:07 +0700

    test(problem-2467): add support to test in C

---
+++ old-problems/2467/c/solution.c
@@ -0,0 +1,11 @@
+int mostProfitablePath(
+    int** edges, int edgesSize, int* edgesColSize,
+    int bob, int* amount, int amountSize) {
+  (void)edges;
+  (void)edgesSize;
+  (void)edgesColSize;
+  (void)bob;
+  (void)amount;
+  (void)amountSize;
+  return 0;
+}
