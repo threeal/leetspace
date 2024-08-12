f839ede99d253a20147db3ebb3f83f6abdd9b0f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-12 08:24:56 +0700

    feat(problem-703/cpp): solve by using priority queue

--- old-problems/0703/test.cpp
+++ old-problems/0703/test.cpp
@@ -14,4 +14,14 @@ TEST_CASE("703. Kth Largest Element in a Stream") {
     REQUIRE(kthLargest.add(9) == 8);
     REQUIRE(kthLargest.add(4) == 8);
   }
+
+  SECTION("Test Case 1") {
+    std::vector<int> nums{};
+    KthLargest kthLargest(1, nums);
+    REQUIRE(kthLargest.add(-3) == -3);
+    REQUIRE(kthLargest.add(-2) == -2);
+    REQUIRE(kthLargest.add(-2) == -2);
+    REQUIRE(kthLargest.add(0) == 0);
+    REQUIRE(kthLargest.add(4) == 4);
+  }
 }
