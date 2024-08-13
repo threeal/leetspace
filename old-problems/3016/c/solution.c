30a4cfd46d44e2e2466c92d07ae754e88fb8d91d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-13 12:33:23 +0700

    feat(problem-3016/c): solve by counting frequencies

--- old-problems/3016/c/solution.c
+++ old-problems/3016/c/solution.c
@@ -1,4 +1,21 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)b - *(int*)a;
+}
+
 int minimumPushes(char* word) {
-  (void)word;
-  return 0;
+  int freqs[26] = {0};
+  while (*word != 0) {
+    ++freqs[*word - 'a'];
+    ++word;
+  }
+
+  qsort(freqs, 26, sizeof(int), compare);
+
+  int pushes = 0;
+  for (int i = 0; i < 26 && freqs[i] > 0; ++i) {
+    pushes += freqs[i] * (i / 8 + 1);
+  }
+  return pushes;
 }
