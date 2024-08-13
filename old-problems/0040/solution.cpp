8c8bb1046cf337dbc1c0b35530ac507f46a06c35
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-13 10:51:38 +0700

    test: add test for problem 40. Combination Sum II

---
+++ old-problems/0040/solution.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> combinationSum2(
+      std::vector<int>& candidates, int /*target*/) {
+    return {candidates};
+  }
+};
