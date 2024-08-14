531d90ee81e0be51bff23b1f4dab0016d9b1cdfa
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-14 14:54:32 +0700

    feat(problem-719/cpp): solve by using sort and binary search

--- old-problems/0719/solution.cpp
+++ old-problems/0719/solution.cpp
@@ -1,8 +1,28 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
-  int smallestDistancePair(std::vector<int>& /*nums*/, int /*k*/) {
-    return 0;
+  int smallestDistancePair(std::vector<int>& nums, int k) {
+    std::sort(nums.begin(), nums.end());
+
+    int low{0};
+    int high{nums.back() - nums.front()};
+    while (low < high) {
+      int mid{low + (high - low) / 2};
+      int count{0};
+      for (std::size_t l{0}, r{1}; r < nums.size(); ++r) {
+        while (l < r && nums[r] - nums[l] > mid) ++l;
+        count += r - l;
+      }
+
+      if (count < k) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+
+    return low;
   }
 };
