cb6c705395f60cfb7c92d05f248535bf7b26b301
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-14 15:07:33 +0700

    feat(problem-2490/c): solve by iterating string

--- old-problems/2490/c/solution.c
+++ old-problems/2490/c/solution.c
@@ -1,5 +1,12 @@
 #include <stdbool.h>
 
 bool isCircularSentence(char* sentence) {
-  return *sentence == 0;
+  int i = 0;
+  while (sentence[i] != 0) {
+    if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1]) {
+      return false;
+    }
+    ++i;
+  }
+  return sentence[0] == sentence[i - 1];
 }
