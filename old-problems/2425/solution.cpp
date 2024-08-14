34745d072508a0a05f5f8f1c3bc139cf3c678092
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-14 15:38:59 +0700

    feat(problem-2425/cpp): solve by XOR all numbers in arrays

--- old-problems/2425/solution.cpp
+++ old-problems/2425/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   int xorAllNums(std::vector<int>& nums1, std::vector<int>& nums2) {
-    return nums1.size() + nums2.size();
+    int sum{0};
+    if (nums2.size() % 2 != 0) {
+      for (const auto num : nums1) {
+        sum ^= num;
+      }
+    }
+
+    if (nums1.size() % 2 != 0) {
+      for (const auto num : nums2) {
+        sum ^= num;
+      }
+    }
+
+    return sum;
   }
 };
