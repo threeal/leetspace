054b57f9ef98bb1f2b68d82b1c60f184e8909ecf
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-15 23:46:41 +0700

    feat(problem-885/c): solve by iterating each directions

--- old-problems/0885/c/solution.c
+++ old-problems/0885/c/solution.c
@@ -1,11 +1,72 @@
+#include <stdlib.h>
+
 int** spiralMatrixIII(
-    int rows, int cols, int rStart, int cStart,
+    int rows, int cols, int y, int x,
     int* returnSize, int** returnColumnSizes) {
-  (void)rows;
-  (void)cols;
-  (void)rStart;
-  (void)cStart;
-  *returnSize = 0;
-  **returnColumnSizes = 0;
-  return 0;
+  *returnSize = rows * cols;
+  *returnColumnSizes = malloc(*returnSize * sizeof(int));
+  int** output = malloc(*returnSize * sizeof(int*));
+  for (int i = *returnSize - 1; i >= 0; --i) {
+    (*returnColumnSizes)[i] = 2;
+    output[i] = malloc(2 * sizeof(int));
+  }
+
+  output[0][0] = y;
+  output[0][1] = x;
+  int i = 1;
+
+  int maxX = x, maxY = y, minX = x, minY = y;
+  while (i < *returnSize) {
+    maxX += 1;
+    if (y >= 0) {
+      for (++x; x <= maxX && x < cols; ++x, ++i) {
+        output[i][0] = y;
+        output[i][1] = x;
+      }
+    } else {
+      y = -1;
+    }
+    x = maxX;
+
+    if (i >= *returnSize) break;
+
+    maxY += 1;
+    if (x < cols) {
+      for (++y; y <= maxY && y < rows; ++y, ++i) {
+        output[i][0] = y;
+        output[i][1] = x;
+      }
+    } else {
+      x = cols;
+    }
+    y = maxY;
+
+    if (i >= *returnSize) break;
+
+    minX -= 1;
+    if (y < rows) {
+      for (--x; x >= minX && x >= 0; --x, ++i) {
+        output[i][0] = y;
+        output[i][1] = x;
+      }
+    } else {
+      y = rows;
+    }
+    x = minX;
+
+    if (i >= *returnSize) break;
+
+    minY -= 1;
+    if (x >= 0) {
+      for (--y; y >= minY && y >= 0; --y, ++i) {
+        output[i][0] = y;
+        output[i][1] = x;
+      }
+    } else {
+      x = -1;
+    }
+    y = minY;
+  }
+
+  return output;
 }
