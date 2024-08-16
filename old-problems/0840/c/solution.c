51510be3d522ce29ab4baadce5ee878e99b0c303
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-16 12:05:45 +0700

    feat(problem-840/c): solve by checking all permutations

--- old-problems/0840/c/solution.c
+++ old-problems/0840/c/solution.c
@@ -1,6 +1,74 @@
 int numMagicSquaresInside(int** grid, int gridSize, int* gridColSize) {
-  (void)grid;
-  (void)gridSize;
-  (void)gridColSize;
-  return 0;
+  int num = 0;
+  for (int i = gridSize - 2; i > 0; --i) {
+    for (int j = gridColSize[i] - 2; j > 0; --j) {
+      if (grid[i][j] != 5) continue;
+      switch (grid[i - 1][j - 1]) {
+        case 2:
+          switch (grid[i - 1][j]) {
+            case 9:
+              if (grid[i - 1][j + 1] == 4 && grid[i][j - 1] == 7 &&
+                  grid[i][j + 1] == 3 && grid[i + 1][j - 1] == 6 &&
+                  grid[i + 1][j] == 1 && grid[i + 1][j + 1] == 8) ++num;
+              break;
+
+            case 7:
+              if (grid[i - 1][j + 1] == 6 && grid[i][j - 1] == 9 &&
+                  grid[i][j + 1] == 1 && grid[i + 1][j - 1] == 4 &&
+                  grid[i + 1][j] == 3 && grid[i + 1][j + 1] == 8) ++num;
+              break;
+          }
+          break;
+
+        case 4:
+          switch (grid[i - 1][j]) {
+            case 3:
+              if (grid[i - 1][j + 1] == 8 && grid[i][j - 1] == 9 &&
+                  grid[i][j + 1] == 1 && grid[i + 1][j - 1] == 2 &&
+                  grid[i + 1][j] == 7 && grid[i + 1][j + 1] == 6) ++num;
+              break;
+
+            case 9:
+              if (grid[i - 1][j + 1] == 2 && grid[i][j - 1] == 3 &&
+                  grid[i][j + 1] == 7 && grid[i + 1][j - 1] == 8 &&
+                  grid[i + 1][j] == 1 && grid[i + 1][j + 1] == 6) ++num;
+              break;
+          }
+          break;
+
+        case 6:
+          switch (grid[i - 1][j]) {
+            case 7:
+              if (grid[i - 1][j + 1] == 2 && grid[i][j - 1] == 1 &&
+                  grid[i][j + 1] == 9 && grid[i + 1][j - 1] == 8 &&
+                  grid[i + 1][j] == 3 && grid[i + 1][j + 1] == 4) ++num;
+              break;
+
+            case 1:
+              if (grid[i - 1][j + 1] == 8 && grid[i][j - 1] == 7 &&
+                  grid[i][j + 1] == 3 && grid[i + 1][j - 1] == 2 &&
+                  grid[i + 1][j] == 9 && grid[i + 1][j + 1] == 4) ++num;
+              break;
+          }
+          break;
+
+        case 8:
+          switch (grid[i - 1][j]) {
+            case 1:
+              if (grid[i - 1][j + 1] == 6 && grid[i][j - 1] == 3 &&
+                  grid[i][j + 1] == 7 && grid[i + 1][j - 1] == 4 &&
+                  grid[i + 1][j] == 9 && grid[i + 1][j + 1] == 2) ++num;
+              break;
+
+            case 3:
+              if (grid[i - 1][j + 1] == 4 && grid[i][j - 1] == 1 &&
+                  grid[i][j + 1] == 9 && grid[i + 1][j - 1] == 6 &&
+                  grid[i + 1][j] == 7 && grid[i + 1][j + 1] == 2) ++num;
+              break;
+          }
+          break;
+      }
+    }
+  }
+  return num;
 }
