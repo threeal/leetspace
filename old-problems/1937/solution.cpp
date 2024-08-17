829cf9f075d782c8902a1c03b366b9e4a5620954
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-17 15:03:19 +0700

    feat(problem-1937/cpp): solve by tracking the best index for each row

--- old-problems/1937/solution.cpp
+++ old-problems/1937/solution.cpp
@@ -1,8 +1,28 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   long long maxPoints(std::vector<std::vector<int>>& points) {
-    return points.size();
+    const int n = points.front().size();
+    std::vector<long long> current(n), prev(n, 0);
+
+    for (const auto& row : points) {
+      long long max{0};
+      for (int j{0}; j < n; ++j) {
+        max = std::max(max - 1, prev[j]);
+        current[j] = max;
+      }
+
+      max = 0;
+      for (int j{n - 1}; j >= 0; --j) {
+        max = std::max(max - 1, prev[j]);
+        current[j] = row[j] + std::max(max, current[j]);
+      }
+
+      std::swap(current, prev);
+    }
+
+    return *std::max_element(prev.begin(), prev.end());
   }
 };
