9bebea8a78e5a18a6577219b13b076d4698cdf01
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-17 22:17:19 +0700

    feat: solve problem 2544. Alternating Digit Sum (#1646)
    * test: add test for problem 2544. Alternating Digit Sum
    
    * feat(problem-2544/cpp): solve by using a while loop

---
+++ old-problems/2544/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  int alternateDigitSum(int n) {
+    int sum{0};
+    while (n > 0) {
+      sum -= n % 10;
+      n /= 10;
+      if (n == 0) return -sum;
+
+      sum += n % 10;
+      n /= 10;
+    }
+    return sum;
+  }
+};
