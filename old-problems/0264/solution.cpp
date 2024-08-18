514c6ae4c1eda82724585b0bb35fe871741a85c4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-18 09:28:42 +0700

    feat(problem-264/cpp): solve by storing numbers in a priority queue

--- old-problems/0264/solution.cpp
+++ old-problems/0264/solution.cpp
@@ -1,6 +1,19 @@
+#include <queue>
+
 class Solution {
  public:
   int nthUglyNumber(int n) {
-    return n;
+    std::priority_queue<long long, std::vector<long long>, std::greater<long long>> nums{};
+    nums.push(1);
+
+    while (--n > 0) {
+      const auto num = nums.top();
+      nums.push(num * 2);
+      nums.push(num * 3);
+      nums.push(num * 5);
+      while (nums.top() == num) nums.pop();
+    }
+
+    return nums.top();
   }
 };
