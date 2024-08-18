03fd8500269d9b20b648590f97f4f242de1ecc42
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-18 09:57:17 +0700

    feat(problem-264/cpp): solve by incrementing multiplicated numbers

--- old-problems/0264/solution.cpp
+++ old-problems/0264/solution.cpp
@@ -1,19 +1,26 @@
-#include <queue>
+#include <vector>
 
 class Solution {
  public:
   int nthUglyNumber(int n) {
-    std::priority_queue<long long, std::vector<long long>, std::greater<long long>> nums{};
-    nums.push(1);
+    std::vector<int> nums(n);
+    nums[0] = 1;
 
-    while (--n > 0) {
-      const auto num = nums.top();
-      nums.push(num * 2);
-      nums.push(num * 3);
-      nums.push(num * 5);
-      while (nums.top() == num) nums.pop();
+    int i2 = 0, i3 = 0, i5 = 0;
+    for (int i{1}; i < n; ++i) {
+      const int num2 = nums[i2] * 2;
+      const int num3 = nums[i3] * 3;
+      const int num5 = nums[i5] * 5;
+
+      const int min = std::min(num2, std::min(num3, num5));
+
+      if (num2 == min) ++i2;
+      if (num3 == min) ++i3;
+      if (num5 == min) ++i5;
+
+      nums[i] = min;
     }
 
-    return nums.top();
+    return nums.back();
   }
 };
