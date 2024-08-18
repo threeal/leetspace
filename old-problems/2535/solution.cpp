1b0e9dc71ea7c513086099ab4330ed49ded8c537
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-18 11:17:30 +0700

    feat(problem-2535/cpp): solve by sum the given numbers

--- old-problems/2535/solution.cpp
+++ old-problems/2535/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   int differenceOfSum(std::vector<int>& nums) {
-    return nums.size();
+    int elementSum{0};
+    int digitSum{0};
+
+    for (auto num : nums) {
+      elementSum += num;
+      while (num > 0) {
+        digitSum += num % 10;
+        num /= 10;
+      }
+    }
+
+    return elementSum >= digitSum
+        ? elementSum - digitSum
+        : digitSum - elementSum;
   }
 };
