59a92573216d2905649bcbc9f430da156b05f790
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-19 00:08:56 +0700

    feat(problem-719/c): solve by using sort and binary search

--- old-problems/0719/c/solution.c
+++ old-problems/0719/c/solution.c
@@ -1,6 +1,28 @@
-int smallestDistancePair(int* nums, int numsSize, int k) {
-  (void)nums;
-  (void)numsSize;
-  (void)k;
-  return 0;
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+  return *(int *)a - *(int *)b;
+}
+
+int smallestDistancePair(int *nums, int numsSize, int k) {
+  qsort(nums, numsSize, sizeof(int), compare);
+
+  int low = 0;
+  int high = nums[numsSize - 1] - nums[0];
+  while (low < high) {
+    const int mid = low + (high - low) / 2;
+    int count = 0;
+    for (int l = 0, r = 1; r < numsSize; ++r) {
+      while (l < r && nums[r] - nums[l] > mid) ++l;
+      count += r - l;
+    }
+
+    if (count < k) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+  }
+
+  return low;
 }
