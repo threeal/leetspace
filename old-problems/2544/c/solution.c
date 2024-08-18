04ab60f15793decbbb95d07164b6ad7babd31149
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-18 11:13:00 +0700

    feat(problem-2544/c): solve by using a while loop

--- old-problems/2544/c/solution.c
+++ old-problems/2544/c/solution.c
@@ -1,3 +1,12 @@
 int alternateDigitSum(int n) {
-  return n;
+  int sum = 0;
+  while (n > 0) {
+    sum -= n % 10;
+    n /= 10;
+    if (n == 0) return -sum;
+
+    sum += n % 10;
+    n /= 10;
+  }
+  return sum;
 }
