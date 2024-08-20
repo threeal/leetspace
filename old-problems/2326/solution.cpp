f7041ff4b0d386bb7d615108b3a2d9a8674fca64
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-20 12:09:20 +0700

    feat(problem-2326/cpp): solve by filling matrix spirally

--- old-problems/2326/solution.cpp
+++ old-problems/2326/solution.cpp
@@ -2,7 +2,48 @@
 
 class Solution {
  public:
-  std::vector<std::vector<int>> spiralMatrix(int m, int n, ListNode* /*head*/) {
-    return std::vector(m, std::vector(n, -1));
+  std::vector<std::vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+    std::vector<std::vector<int>> output(m, std::vector(n, -1));
+
+    int y{0}, x{-1};
+    for (int i{n}; i > 0; --i) {
+      if (head == nullptr) return output;
+      output[y][++x] = head->val;
+      head = head->next;
+    }
+
+    --m, --n;
+    while (m > 0 || n > 0) {
+      for (int i{m}; i > 0; --i) {
+        if (head == nullptr) return output;
+        output[++y][x] = head->val;
+        head = head->next;
+      }
+
+      for (int i{n}; i > 0; --i) {
+        if (head == nullptr) return output;
+        output[y][--x] = head->val;
+        head = head->next;
+      }
+
+      --m, --n;
+      if (m <= 0 && n <= 0) break;
+
+      for (int i{m}; i > 0; --i) {
+        if (head == nullptr) return output;
+        output[--y][x] = head->val;
+        head = head->next;
+      }
+
+      for (int i{n}; i > 0; --i) {
+        if (head == nullptr) return output;
+        output[y][++x] = head->val;
+        head = head->next;
+      }
+
+      --m, --n;
+    }
+
+    return output;
   }
 };
