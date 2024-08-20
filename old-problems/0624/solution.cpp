0f78b089dfe12e5c2de5c24c03d0b5c1a214ad82
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-20 20:22:44 +0700

    feat(problem-624/cpp): solve by comparing distance while tracking previous min and max

--- old-problems/0624/solution.cpp
+++ old-problems/0624/solution.cpp
@@ -1,13 +1,27 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maxDistance(std::vector<std::vector<int>>& arrays) {
-    int min{10000}, max{-10000};
+    int distance{0};
+
+    int prevMin{10001};
+    int prevMax{-10001};
+
     for (const auto& array : arrays) {
-      if (array.front() < min) min = array.front();
-      if (array.back() > max) max = array.back();
+      if (prevMax - array.front() > distance) {
+        distance = prevMax - array.front();
+      }
+
+      if (array.back() - prevMin > distance) {
+        distance = array.back() - prevMin;
+      }
+
+      if (array.front() < prevMin) prevMin = array.front();
+      if (array.back() > prevMax) prevMax = array.back();
     }
-    return max - min;
+
+    return distance;
   }
 };
