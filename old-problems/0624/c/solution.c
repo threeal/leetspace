7540d78e5e70e87fa42e4513a68e990a2b4ec899
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-20 20:55:48 +0700

    feat(problem-624/c): solve by comparing distance while tracking previous min and max

--- old-problems/0624/c/solution.c
+++ old-problems/0624/c/solution.c
@@ -1,3 +1,19 @@
 int maxDistance(int** arrays, int arraysSize, int* arraysColSize) {
-  return arrays[arraysSize - 1][arraysColSize[arraysSize - 1] - 1];
+  int distance = 0;
+
+  int prevMin = 10001;
+  int prevMax = -10001;
+
+  for (int i = arraysSize - 1; i >= 0; --i) {
+    const int min = arrays[i][0];
+    const int max = arrays[i][arraysColSize[i] - 1];
+
+    if (prevMax - min > distance) distance = prevMax - min;
+    if (max - prevMin > distance) distance = max - prevMin;
+
+    if (min < prevMin) prevMin = min;
+    if (max > prevMax) prevMax = max;
+  }
+
+  return distance;
 }
