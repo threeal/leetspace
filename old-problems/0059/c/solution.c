fe51a9bd713470b07be9338fdfa943c80d4fa208
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-20 11:28:05 +0700

    feat(problem-59/c): solve by filling matrix spirally

--- old-problems/0059/c/solution.c
+++ old-problems/0059/c/solution.c
@@ -8,5 +8,18 @@ int** generateMatrix(int n, int* returnSize, int** returnColumnSizes) {
     (*returnColumnSizes)[i] = n;
     output[i] = malloc((*returnColumnSizes)[i] * sizeof(int));
   }
+
+  int y = 0, x = -1, i = 0;
+  for (int j = n; j > 0; --j) output[y][++x] = ++i;
+
+  while (--n > 0) {
+    for (int j = n; j > 0; --j) output[++y][x] = ++i;
+    for (int j = n; j > 0; --j) output[y][--x] = ++i;
+
+    if (--n <= 0) break;
+    for (int j = n; j > 0; --j) output[--y][x] = ++i;
+    for (int j = n; j > 0; --j) output[y][++x] = ++i;
+  }
+
   return output;
 }
