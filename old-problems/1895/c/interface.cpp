4ddbdba480bef6ab84b89fdc294d5e59e868c702
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-23 16:11:22 +0700

    feat: solve problem 1895. Largest Magic Square in C (#1742)
    * test(problem-1895): add support to test in C
    
    * feat(problem-1895/c): solve by using brute force

---
+++ old-problems/1895/c/interface.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+extern "C" {
+int largestMagicSquare(int** grid, int gridSize, int* gridColSize);
+}
+
+int solution_c(std::vector<std::vector<int>> grid) {
+  std::vector<int*> gridDatas(grid.size());
+  std::vector<int> gridSizes(grid.size());
+  for (int i = grid.size() - 1; i >= 0; --i) {
+    gridDatas[i] = grid[i].data();
+    gridSizes[i] = grid[i].size();
+  }
+
+  return largestMagicSquare(
+      gridDatas.data(), gridDatas.size(), gridSizes.data());
+}
