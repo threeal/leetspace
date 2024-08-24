6527648ebb059282ec38de2f820a0a54d7c8c25c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-24 22:19:35 +0700

    feat(problem-2427/cpp): solve by checking all possible factors

--- old-problems/2427/solution.cpp
+++ old-problems/2427/solution.cpp
@@ -1,8 +1,15 @@
+#include <utility>
 #include <vector>
 
 class Solution {
  public:
   int commonFactors(int a, int b) {
-    return a + b;
+    if (b < a) std::swap(a, b);
+
+    int factors{1};
+    for (int i{2}; i <= a; ++i) {
+      if (a % i == 0 && b % i == 0) ++factors;
+    }
+    return factors;
   }
 };
