19f3018cf39bcf88fe30811ac5d48afa2bbf4c61
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-24 22:56:24 +0700

    feat(problem-2351/cpp): solve by storing existing chars in binary map

--- old-problems/2351/solution.cpp
+++ old-problems/2351/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   char repeatedCharacter(std::string s) {
-    return s.front();
+    std::uint32_t exists{0};
+    for (const auto c : s) {
+      if ((exists & 1 << (c - 'a')) != 0) return c;
+      exists |= 1 << (c - 'a');
+    }
+    return 0;
   }
 };
