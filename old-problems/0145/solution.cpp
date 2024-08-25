8456b8277b4ac5479d98c56d35bf03c8cd4fad07
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-25 09:56:14 +0700

    feat(problem-145/cpp): solve by using recursive function

--- old-problems/0145/solution.cpp
+++ old-problems/0145/solution.cpp
@@ -2,7 +2,15 @@
 
 class Solution {
  public:
-  std::vector<int> postorderTraversal(TreeNode* /*root*/) {
-    return {};
+  std::vector<int> postorderTraversal(TreeNode* root) {
+    std::vector<int> output{};
+    if (root != nullptr) traverse(output, root);
+    return output;
+  }
+
+  void traverse(std::vector<int>& output, TreeNode* node) {
+    if (node->left != nullptr) traverse(output, node->left);
+    if (node->right != nullptr) traverse(output, node->right);
+    output.push_back(node->val);
   }
 };
