f7a5659186d49abdbd5f7e5e1809a42aa702d182
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-25 22:13:44 +0700

    perf(problem-1140/cpp): replace `std::tuple` with `std::int64_t`

--- old-problems/1140/solution.cpp
+++ old-problems/1140/solution.cpp
@@ -1,37 +1,35 @@
 #include <algorithm>
-#include <tuple>
 #include <vector>
 
 class Solution {
  public:
   int stoneGameII(std::vector<int>& piles) {
-    std::vector<std::vector<std::tuple<int, int>>> caches(piles.size() + 1);
-    const auto& [alice, bob] = pickPiles(caches, piles, 0, 1);
-    return alice;
+    std::vector<std::vector<std::int64_t>> caches(
+        piles.size() + 1, std::vector<std::int64_t>(piles.size() + 1, -1));
+    return pickPiles(caches, piles, 0, 1)[0];
   }
 
-  std::tuple<int, int> pickPiles(
-      std::vector<std::vector<std::tuple<int, int>>>& caches,
+  std::int32_t* pickPiles(
+      std::vector<std::vector<std::int64_t>>& caches,
       const std::vector<int>& piles, int i, int m) {
-    if (static_cast<int>(caches[i].size()) < m) caches[i].resize(m, {-1, -1});
-    auto& [bestCurrent, bestOther] = caches[i][m - 1];
-    if (bestCurrent >= 0) return {bestCurrent, bestOther};
+    std::int32_t* best =
+        reinterpret_cast<std::int32_t*>(caches[i].data() + m - 1);
 
-    bestCurrent = 0;
-    bestOther = 0;
+    if (best[0] >= 0) return best;
+    *reinterpret_cast<std::int64_t*>(best) = 0;
 
     int stones{0};
     for (int j{0}; j < m * 2 && i + j < static_cast<int>(piles.size()); ++j) {
       stones += piles[i + j];
-      const auto& [other, current] =
+      const auto current =
           pickPiles(caches, piles, i + j + 1, std::max(m, j + 1));
 
-      if (stones + current > bestCurrent) {
-        bestCurrent = stones + current;
-        bestOther = other;
+      if (stones + current[1] > best[0]) {
+        best[0] = stones + current[1];
+        best[1] = current[0];
       }
     }
 
-    return {bestCurrent, bestOther};
+    return best;
   }
 };
