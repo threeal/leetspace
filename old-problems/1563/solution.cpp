ec999401a221ee519e185386bb6f394f11f82977
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-26 17:05:30 +0700

    perf(problem-1563/cpp): replace for-loop with a recursive function

--- old-problems/1563/solution.cpp
+++ old-problems/1563/solution.cpp
@@ -10,27 +10,32 @@ class Solution {
       sums[i + 1] = sums[i] + stoneValue[i];
     }
 
-    std::vector<std::vector<int>> bests(stoneValue.size() + 1);
-    bests[1].resize(stoneValue.size(), 0);
+    std::vector<std::vector<int>> caches(
+        stoneValue.size(), std::vector<int>(stoneValue.size() + 1, -1));
+    caches.front().resize(stoneValue.size(), 0);
 
-    for (std::size_t n{2}; n <= stoneValue.size(); ++n) {
-      bests[n].resize(stoneValue.size() - n + 1, 0);
-      for (std::size_t i{0}; i + n <= stoneValue.size(); ++i) {
-        for (std::size_t j{i + 1}; j < i + n; ++j) {
-          const int left{sums[j] - sums[i]};
-          const int right{sums[i + n] - sums[j]};
+    return find(caches.data(), sums.data(), stoneValue.size(), 0);
+  }
 
-          if (left <= right) {
-            bests[n][i] = std::max(bests[n][i], left + bests[j - i][i]);
-          }
+ private:
+  static int find(std::vector<int>* caches, int* sums, int n, int i) {
+    auto& best = caches[n - 1][i];
+    if (best < 0) {
+      best = 0;
+      for (int j = i + 1; j < i + n; ++j) {
+        const int left{sums[j] - sums[i]};
+        const int right{sums[i + n] - sums[j]};
 
-          if (right <= left) {
-            bests[n][i] = std::max(bests[n][i], right + bests[i + n - j][j]);
-          }
+        if (left <= right) {
+          best = std::max(best, left + find(caches, sums, j - i, i));
+        }
+
+        if (right <= left) {
+          best = std::max(best, right + find(caches, sums, i + n - j, j));
         }
       }
     }
 
-    return bests.back().back();
+    return best;
   }
 };
