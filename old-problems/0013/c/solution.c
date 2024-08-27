9a72a7331724f83adda5959f60dca5904c611cbb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-27 13:10:14 +0700

    feat: solve [roblem 13. Roman to Integer in C (#1780)
    * test(problem-13): add support to test in C language
    
    * feat(problem-13/c): solve by parsing roman digits

---
+++ old-problems/0013/c/solution.c
@@ -0,0 +1,68 @@
+#include <string.h>
+
+int romanToInt(char* s) {
+  int num = 0;
+  for (int i = strlen(s) - 1; i >= 0; --i) {
+    switch (s[i]) {
+      case 'I':
+        num += 1;
+        break;
+
+      case 'V':
+        if (i > 0 && s[i - 1] == 'I') {
+          num += 4;
+          --i;
+        } else {
+          num += 5;
+        }
+        break;
+
+      case 'X':
+        if (i > 0 && s[i - 1] == 'I') {
+          num += 9;
+          --i;
+        } else {
+          num += 10;
+        }
+        break;
+
+      case 'L':
+        if (i > 0 && s[i - 1] == 'X') {
+          num += 40;
+          --i;
+        } else {
+          num += 50;
+        }
+        break;
+
+      case 'C':
+        if (i > 0 && s[i - 1] == 'X') {
+          num += 90;
+          --i;
+        } else {
+          num += 100;
+        }
+        break;
+
+      case 'D':
+        if (i > 0 && s[i - 1] == 'C') {
+          num += 400;
+          --i;
+        } else {
+          num += 500;
+        }
+        break;
+
+      case 'M':
+        if (i > 0 && s[i - 1] == 'C') {
+          num += 900;
+          --i;
+        } else {
+          num += 1000;
+        }
+        break;
+    }
+  }
+
+  return num;
+}
