af5a21c77eebae9188c2f6a8c99dbdd4285c440f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-27 13:27:49 +0700

    feat(problem-664/c): solve by removing duplicates and dynamic programming

--- old-problems/0664/c/solution.c
+++ old-problems/0664/c/solution.c
@@ -1,3 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+
+int strangePrinterRange(int** caches, const char* s, int l, int r);
+
 int strangePrinter(char* s) {
-  return *s;
+  int n = 0, i = 0;
+  while (s[i] != 0) {
+    s[n] = s[i];
+    ++i;
+    while (s[i] != 0 && s[i] == s[n]) ++i;
+    ++n;
+  }
+
+  int** caches = malloc(n * sizeof(int*));
+  for (int i = 0; i < n; ++i) {
+    caches[i] = malloc((n - i) * sizeof(int));
+    memset(caches[i], -1, (n - i) * sizeof(int));
+  }
+
+  const int res = strangePrinterRange(caches, s, 0, n - 1);
+
+  for (int i = 0; i < n; ++i) free(caches[i]);
+  free(caches);
+
+  return res;
+}
+
+int strangePrinterRange(int** caches, const char* s, int l, int r) {
+  if (r - l < 0) return 0;
+
+  int* cache = caches[l] + r - l;
+  if (*cache < 0) {
+    *cache = 1 + strangePrinterRange(caches, s, l + 1, r);
+    for (int i = l + 1; i <= r; ++i) {
+      if (s[i] == s[l]) {
+        const int res = strangePrinterRange(caches, s, l, i - 1) +
+            strangePrinterRange(caches, s, i + 1, r);
+        if (res < *cache) *cache = res;
+      }
+    }
+  }
+
+  return *cache;
 }
