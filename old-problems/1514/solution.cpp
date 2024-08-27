18a1f5143d9b1e6d8344044c4e59e41d7a61ab26
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-27 10:18:21 +0700

    test: add test for problem 1514. Path with Maximum Probability

---
+++ old-problems/1514/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  double maxProbability(
+      int /*n*/, std::vector<std::vector<int>>& /*edges*/,
+      std::vector<double>& /*succProb*/, int /*start*/, int /*end*/) {
+    return 0.0;
+  }
+};
