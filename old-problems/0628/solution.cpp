651283a970369c32498637c3884e12cec1226b43
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-27 13:16:14 +0700

    feat(problem-628/cpp): solve by using partial sort

--- old-problems/0628/solution.cpp
+++ old-problems/0628/solution.cpp
@@ -1,8 +1,15 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maximumProduct(std::vector<int>& nums) {
-    return nums.size();
+    std::partial_sort(nums.begin(), nums.begin() + 2, nums.end());
+    std::partial_sort(
+        nums.rbegin(), nums.rbegin() + 3, nums.rend(), std::greater<int>());
+
+    return std::max(
+        nums[0] * nums[1] * nums[nums.size() - 1],
+        nums[nums.size() - 1] * nums[nums.size() - 2] * nums[nums.size() - 3]);
   }
 };
