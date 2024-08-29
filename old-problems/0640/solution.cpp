1e53649767097be3aab040e6b4308b3bcf99045d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-29 23:12:03 +0700

    feat(problem-640/cpp): solve by parsing coefficient and constant from equation

--- old-problems/0640/solution.cpp
+++ old-problems/0640/solution.cpp
@@ -1,8 +1,78 @@
+#include <sstream>
 #include <string>
 
 class Solution {
  public:
   std::string solveEquation(std::string equation) {
-    return equation;
+    int coefficient{0};
+    int constant{0};
+
+    auto c = equation.data();
+    while (*c != '=') {
+      bool negative{false};
+      if (*c == '+') ++c;
+      if (*c == '-') {
+        negative = true;
+        ++c;
+      }
+
+      if (*c == 'x') {
+        coefficient += negative ? -1 : 1;
+        ++c;
+        continue;
+      }
+
+      int num{0};
+      while (*c >= '0' && *c <= '9') {
+        num = num * 10 + *c - '0';
+        ++c;
+      }
+      if (negative) num = -num;
+
+      if (*c == 'x') {
+        coefficient += num;
+        ++c;
+      } else {
+        constant -= num;
+      }
+    }
+
+    ++c;
+    while (*c != 0) {
+      bool negative{false};
+      if (*c == '+') ++c;
+      if (*c == '-') {
+        negative = true;
+        ++c;
+      }
+
+      if (*c == 'x') {
+        coefficient -= negative ? -1 : 1;
+        ++c;
+        continue;
+      }
+
+      int num{0};
+      while (*c >= '0' && *c <= '9') {
+        num = num * 10 + *c - '0';
+        ++c;
+      }
+      if (negative) num = -num;
+
+      if (*c == 'x') {
+        coefficient -= num;
+        ++c;
+      } else {
+        constant += num;
+      }
+    }
+
+    if (coefficient == 0) {
+      return constant == 0 ? "Infinite solutions" : "No solution";
+    }
+
+    std::stringstream ss{};
+    ss << "x=" << constant / coefficient;
+    return ss.str();
   }
 };
