2e3e327a397dbbe9f75aa572a7e66e5af586d47c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-29 21:06:30 +0700

    feat(problem-1894/cpp): solve by using binary search

--- old-problems/1894/solution.cpp
+++ old-problems/1894/solution.cpp
@@ -1,8 +1,18 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
-  int chalkReplacer(std::vector<int>& /*chalk*/, int /*k*/) {
-    return 0;
+  int chalkReplacer(std::vector<int>& chalk, int k) {
+    if (k < chalk[0]) return 0;
+    for (std::size_t i{1}; i < chalk.size(); ++i) {
+      if (k - chalk[i - 1] < chalk[i]) return i;
+      chalk[i] += chalk[i - 1];
+    }
+
+    const auto it = std::upper_bound(
+        chalk.begin(), chalk.end(), k % chalk.back());
+
+    return std::distance(chalk.begin(), it);
   }
 };
