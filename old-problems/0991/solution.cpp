715ae4079b2f57f0708990c5147b8752996c0e86
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-31 11:38:04 +0700

    feat: solve problem 991. Broken Calculator (#1768)
    * test: add test for problem 991. Broken Calculator
    
    * test(problem-991): add test case 5
    
    * feat(problem-991/cpp): solve by repeatedly divides target by 2

---
+++ old-problems/0991/solution.cpp
@@ -0,0 +1,17 @@
+#include <queue>
+
+class Solution {
+ public:
+  int brokenCalc(int startValue, int target) {
+    int operations{0};
+    while (target > startValue) {
+      ++operations;
+      if (target % 2 == 0) {
+        target /= 2;
+      } else {
+        target += 1;
+      }
+    }
+    return operations + startValue - target;
+  }
+};
