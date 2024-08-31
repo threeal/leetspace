ba12ef1cc8b36d0dd3f69361f442ccd42d735971
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-31 11:43:05 +0700

    feat(problem-991/c): solve by repeatedly divides target by 2

--- old-problems/0991/c/solution.c
+++ old-problems/0991/c/solution.c
@@ -1,3 +1,12 @@
 int brokenCalc(int startValue, int target) {
-  return target - startValue;
+  int operations = 0;
+  while (target > startValue) {
+    ++operations;
+    if (target % 2 == 0) {
+      target /= 2;
+    } else {
+      target += 1;
+    }
+  }
+  return operations + startValue - target;
 }
