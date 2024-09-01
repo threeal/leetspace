16c6b9538001d7a9c8d63f3fd967414bb0d13d31
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-01 12:00:33 +0700

    test: add test for problem 2022. Convert 1D Array Into 2D Array

---
+++ old-problems/2022/solution.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> construct2DArray(
+      std::vector<int>& /*original*/, int /*m*/, int /*n*/) {
+    return {};
+  }
+};
