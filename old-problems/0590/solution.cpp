0413f9c5632ef83c1c7655e6a68ef13dea412fe6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-01 17:57:35 +0700

    feat(problem-590/cpp): solve by using a recursive function

--- old-problems/0590/solution.cpp
+++ old-problems/0590/solution.cpp
@@ -2,7 +2,15 @@
 
 class Solution {
  public:
-  std::vector<int> postorder(Node* /*root*/) {
-    return {};
+  std::vector<int> postorder(Node* root) {
+    std::vector<int> output{};
+    if (root != nullptr) traverse(output, root);
+    return output;
+  }
+
+ private:
+  static void traverse(std::vector<int>& output, Node* node) {
+    for (const auto child : node->children) traverse(output, child);
+    output.push_back(node->val);
   }
 };
