deb299ad40e8c5c13c793a2551c43552f20c2f9d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-01 12:05:56 +0700

    feat(problem-2022/cpp): solve by filling 2D array from given 1D array

--- old-problems/2022/solution.cpp
+++ old-problems/2022/solution.cpp
@@ -1,9 +1,15 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::vector<int>> construct2DArray(
-      std::vector<int>& /*original*/, int /*m*/, int /*n*/) {
-    return {};
+      std::vector<int>& original, int m, int n) {
+    if (original.size() != static_cast<std::size_t>(m * n)) return {};
+    std::vector<std::vector<int>> output(m, std::vector<int>(n));
+    for (int i{m - 1}; i >= 0; --i) {
+      std::copy_n(original.begin() + i * n, n, output[i].begin());
+    }
+    return output;
   }
 };
