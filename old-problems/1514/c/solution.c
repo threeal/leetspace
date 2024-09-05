76a69a8ea535d1e8948ad356cf609a5abb0acdb9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-05 20:26:44 +0700

    feat(problem-1514/c): solve by updating max probs while it is possible to update

--- old-problems/1514/c/solution.c
+++ old-problems/1514/c/solution.c
@@ -1,13 +1,32 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
 double maxProbability(
     int n, int** edges, int edgesSize, int* edgesColSize,
-    double* succProb, int succProbSize, int start_node, int end_node) {
-  (void)n;
-  (void)edges;
-  (void)edgesSize;
+    double* succProb, int succProbSize, int start, int end) {
   (void)edgesColSize;
-  (void)succProb;
   (void)succProbSize;
-  (void)start_node;
-  (void)end_node;
-  return 0.0;
+
+  double* maxProb = malloc(n * sizeof(double));
+  memset(maxProb, 0, n * sizeof(double));
+  maxProb[start] = 1.0;
+
+  bool updated = true;
+  while (updated) {
+    updated = false;
+    for (int i = edgesSize - 1; i >= 0; --i) {
+      if (maxProb[edges[i][0]] * succProb[i] > maxProb[edges[i][1]]) {
+        maxProb[edges[i][1]] = maxProb[edges[i][0]] * succProb[i];
+        updated = true;
+      }
+
+      if (maxProb[edges[i][1]] * succProb[i] > maxProb[edges[i][0]]) {
+        maxProb[edges[i][0]] = maxProb[edges[i][1]] * succProb[i];
+        updated = true;
+      }
+    }
+  }
+
+  return maxProb[end];
 }
