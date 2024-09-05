7b8740ecbfaf0a89a3fbaf68b807fed0876278bc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-05 20:12:04 +0700

    feat(problem-1893/cpp): solve by using a bitmap

--- old-problems/1893/solution.cpp
+++ old-problems/1893/solution.cpp
@@ -1,9 +1,14 @@
+#include <cstdint>
 #include <vector>
 
 class Solution {
  public:
-  bool isCovered(
-      std::vector<std::vector<int>>& /*ranges*/, int /*left*/, int /*right*/) {
-    return false;
+  bool isCovered(std::vector<std::vector<int>>& ranges, int left, int right) {
+    std::uint64_t actual{0};
+    for (const auto& range : ranges) {
+      actual |= ((1ul << (range[1] + 1)) - 1) & ~((1ul << range[0]) - 1);
+    }
+    std::uint64_t expected = ((1ul << (right + 1)) - 1) & ~((1ul << left) - 1);
+    return (actual & expected) == expected;
   }
 };
