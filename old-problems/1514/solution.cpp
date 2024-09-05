39309e528ceb81774aa0522780a1a77703827e46
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-05 20:13:10 +0700

    feat(problem-1514/cpp): solve problem by updating max probs while it is possible to update (#1816)

--- old-problems/1514/solution.cpp
+++ old-problems/1514/solution.cpp
@@ -1,6 +1,3 @@
-#include <list>
-#include <queue>
-#include <utility>
 #include <vector>
 
 class Solution {
@@ -8,27 +5,21 @@ class Solution {
   double maxProbability(
       int n, std::vector<std::vector<int>>& edges,
       std::vector<double>& succProb, int start, int end) {
-    std::vector<std::list<std::pair<double, int>>> neighbors(n);
-    for (int i = edges.size() - 1; i >= 0; --i) {
-      neighbors[edges[i][0]].push_back({succProb[i], edges[i][1]});
-      neighbors[edges[i][1]].push_back({succProb[i], edges[i][0]});
-    }
-
     std::vector<double> maxProb(n, 0.0);
     maxProb[start] = 1.0;
 
-    std::queue<int> queue{};
-    queue.push(start);
+    bool updated{true};
+    while (updated) {
+      updated = false;
+      for (int i = edges.size() - 1; i >= 0; --i) {
+        if (maxProb[edges[i][0]] * succProb[i] > maxProb[edges[i][1]]) {
+          maxProb[edges[i][1]] = maxProb[edges[i][0]] * succProb[i];
+          updated = true;
+        }
 
-    while (!queue.empty()) {
-      const auto node = queue.front();
-      queue.pop();
-
-      for (const auto& neighbor : neighbors[node]) {
-        const auto newProb = maxProb[node] * neighbor.first;
-        if (newProb > maxProb[neighbor.second]) {
-          maxProb[neighbor.second] = newProb;
-          queue.push(neighbor.second);
+        if (maxProb[edges[i][1]] * succProb[i] > maxProb[edges[i][0]]) {
+          maxProb[edges[i][0]] = maxProb[edges[i][1]] * succProb[i];
+          updated = true;
         }
       }
     }
