10a5b9e876dbddfcc29342ec9b1117e23eddea39
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-07 22:38:40 +0700

    feat(problem-2749/cpp): solve by checking bit counts in each operations

--- old-problems/2749/c/solution.c
+++ old-problems/2749/c/solution.c
@@ -1,3 +1,11 @@
+#include <stdint.h>
+
 int makeTheIntegerZero(int num1, int num2) {
-  return num1 + num2;
+  int64_t target = num1;
+  target -= num2;
+  for (int n = 1; target >= n; ++n) {
+    if (__builtin_popcountll(target) <= n) return n;
+    target -= num2;
+  }
+  return -1;
 }
