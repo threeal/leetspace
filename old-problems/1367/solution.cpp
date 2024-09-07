7b3e16680098c9182082531c77388cbd7a06f634
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-07 12:28:35 +0700

    feat(problem-1367): solve by using a recursive function

--- old-problems/1367/solution.cpp
+++ old-problems/1367/solution.cpp
@@ -1,6 +1,20 @@
 class Solution {
  public:
   bool isSubPath(ListNode* head, TreeNode* root) {
-    return head->val == root->val;
+    if (head->val == root->val && checkSubPath(head, root)) return true;
+    if (root->left != nullptr && isSubPath(head, root->left)) return true;
+    if (root->right != nullptr && isSubPath(head, root->right)) return true;
+    return false;
+  }
+
+  bool checkSubPath(ListNode* head, TreeNode* root) {
+    if (head->next == nullptr) return true;
+    if (root->left != nullptr && head->next->val == root->left->val) {
+      if (checkSubPath(head->next, root->left)) return true;
+    }
+    if (root->right != nullptr && head->next->val == root->right->val) {
+      if (checkSubPath(head->next, root->right)) return true;
+    }
+    return false;
   }
 };
