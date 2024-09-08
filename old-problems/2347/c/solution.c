32dc44f9c2bcee7d629db09d539d3df9c15cb13c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-08 22:09:14 +0700

    feat(problem-2347/c): solve by using for loop to check pairs

--- old-problems/2347/c/solution.c
+++ old-problems/2347/c/solution.c
@@ -1,7 +1,21 @@
 char* bestHand(int* ranks, int ranksSize, char* suits, int suitsSize) {
-  (void)ranks;
-  (void)ranksSize;
-  (void)suits;
   (void)suitsSize;
-  return "High Card";
+  for (int i = 4; i > 0; --i) {
+    if (suits[i] != suits[0]) {
+      int ones = 0, twos = 0;
+      for (int j = 0; j < ranksSize; ++j) {
+        if ((ones & (1 << ranks[j])) > 0) {
+          if ((twos & (1 << ranks[j])) > 0) {
+            return "Three of a Kind";
+          } else {
+            twos |= 1 << ranks[j];
+          }
+        } else {
+          ones |= 1 << ranks[j];
+        }
+      }
+      return twos > 0 ? "Pair" : "High Card";
+    }
+  }
+  return "Flush";
 }
