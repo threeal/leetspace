f91dc65b7d086f6be97b50404263400a03d3e413
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-08 13:04:47 +0700

    feat(problem-2022): solve by filling 2D array from given 1D array

--- old-problems/2022/c/solution.c
+++ old-problems/2022/c/solution.c
@@ -3,11 +3,21 @@
 int** construct2DArray(
     int* original, int originalSize, int m, int n,
     int* returnSize, int** returnColumnSizes) {
-  (void)original;
-  (void)originalSize;
-  (void)m;
-  (void)n;
-  *returnSize = 0;
-  *returnColumnSizes = NULL;
-  return NULL;
+  if (originalSize != m * n) {
+    *returnSize = 0;
+    *returnColumnSizes = NULL;
+    return NULL;
+  }
+
+  int** output = malloc(m * sizeof(int*));
+
+  *returnSize = m;
+  *returnColumnSizes = malloc(m * sizeof(int));
+
+  for (int i = m - 1; i >= 0; --i) {
+    output[i] = original + i * n;
+    (*returnColumnSizes)[i] = n;
+  }
+
+  return output;
 }
