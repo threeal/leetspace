6385967786afe4bed4aeba9aa399a73a2cb000be
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-08 13:32:46 +0700

    feat(problem-2347/cpp): solve by using for loop to check pairs

--- old-problems/2347/solution.cpp
+++ old-problems/2347/solution.cpp
@@ -3,8 +3,22 @@
 
 class Solution {
  public:
-  std::string bestHand(
-      std::vector<int>& /*ranks*/, std::vector<char>& /*suits*/) {
-    return "High Card";
+  std::string bestHand(std::vector<int>& ranks, std::vector<char>& suits) {
+    for (int i{4}; i > 0; --i) {
+      if (suits[i] != suits[0]) {
+        int maxPair{0};
+        for (int i{4}; i >= 0; --i) {
+          int pair{0};
+          for (int j{4}; j >= 0; --j) {
+            if (ranks[i] == ranks[j]) ++pair;
+          }
+          if (pair > maxPair) maxPair = pair;
+        }
+        return maxPair < 3
+            ? (maxPair < 2 ? "High Card" : "Pair")
+            : "Three of a Kind";
+      }
+    }
+    return "Flush";
   }
 };
