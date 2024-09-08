891eedf35e0bad539296bb28fc637663de4f0d4a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-08 13:32:39 +0700

    test: add test for problem 2347. Best Poker Hand

---
+++ old-problems/2347/solution.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::string bestHand(
+      std::vector<int>& /*ranks*/, std::vector<char>& /*suits*/) {
+    return "High Card";
+  }
+};
