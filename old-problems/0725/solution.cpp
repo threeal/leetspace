79d6606a9e2e55f8cec4c1347104798e66130bc4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-08 12:46:16 +0700

    feat(problem-725/cpp): solve by counting list size before distributing

--- old-problems/0725/solution.cpp
+++ old-problems/0725/solution.cpp
@@ -2,7 +2,28 @@
 
 class Solution {
  public:
-  std::vector<ListNode*> splitListToParts(ListNode* head, int /*k*/) {
-    return {head};
+  std::vector<ListNode*> splitListToParts(ListNode* head, int k) {
+    int n{0};
+    auto node = head;
+    while (node != nullptr) {
+      ++n;
+      node = node->next;
+    }
+
+    std::vector<ListNode*> output(k);
+    for (int i{0}; i < k; ++i) {
+      output[i] = head;
+      for (int j{n / k + (i < n % k ? 1 : 0)}; j > 1; --j) {
+        head = head->next;
+      }
+
+      if (head != nullptr) {
+        node = head;
+        head = head->next;
+        node->next = nullptr;
+      }
+    }
+
+    return output;
   }
 };
