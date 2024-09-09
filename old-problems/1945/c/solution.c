b51ccc13b0073ffc18b8f466d2ce4a8ac39fe8ad
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-09 08:53:54 +0700

    feat(problem-1945/c): solve by iterating digits

--- old-problems/1945/c/solution.c
+++ old-problems/1945/c/solution.c
@@ -1,5 +1,24 @@
 int getLucky(char* s, int k) {
-  (void)s;
-  (void)k;
-  return 0;
+  int num = 0;
+  while (*s != 0) {
+    if (*s > 115) {
+      num += *s - 114;
+    } else if (*s > 105) {
+      num += *s - 105;
+    } else {
+      num += *s - 96;
+    }
+    ++s;
+  }
+
+  while (--k > 0) {
+    int temp = 0;
+    while (num > 0) {
+      temp += num % 10;
+      num /= 10;
+    }
+    num = temp;
+  }
+
+  return num;
 }
