70a12d4496db115e0ea2b1adbe1a9c1ab68e2fcb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-09 09:23:21 +0700

    feat(problem-202/cpp): solve by using while loop and hash set

--- old-problems/0202/solution.cpp
+++ old-problems/0202/solution.cpp
@@ -1,6 +1,20 @@
+#include <unordered_set>
+#include <utility>
+
 class Solution {
  public:
   bool isHappy(int n) {
-    return n == 1;
+    std::unordered_set<int> processed{};
+    while (n != 1) {
+      if (processed.contains(n)) return false;
+      processed.insert(n);
+      int res{0};
+      while (n > 0) {
+        res += (n % 10) * (n % 10);
+        n /= 10;
+      }
+      n = res;
+    }
+    return true;
   }
 };
