4ec87934d44e034265773edca6f4c2f2586bebf5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-09 09:42:01 +0700

    feat(problem-1905/c): solve by parsing time

--- old-problems/1904/c/solution.c
+++ old-problems/1904/c/solution.c
@@ -1,5 +1,13 @@
 int numberOfRounds(char* loginTime, char* logoutTime) {
-  (void)loginTime;
-  (void)logoutTime;
-  return 0;
+  int login = ((loginTime[0] - '0') * 10 + loginTime[1] - '0') * 60 +
+      (loginTime[3] - '0') * 10 + loginTime[4] - '0';
+
+  int logout = ((logoutTime[0] - '0') * 10 + logoutTime[1] - '0') * 60 +
+      (logoutTime[3] - '0') * 10 + logoutTime[4] - '0';
+
+  if (logout < login) logout += 24 * 60;
+  if (login % 15 > 0) login += 15;
+
+  const int rounds = logout / 15 - login / 15;
+  return rounds > 0 ? rounds : 0;
 }
