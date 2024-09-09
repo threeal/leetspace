89bcc8a4bc279be331288b779f738cf80df94bb3
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-09 08:30:01 +0700

    feat(problem-0877/cpp): solve by checking all possibilities

--- old-problems/0877/solution.cpp
+++ old-problems/0877/solution.cpp
@@ -1,8 +1,17 @@
+#include <utility>
 #include <vector>
 
 class Solution {
  public:
   bool stoneGame(std::vector<int>& piles) {
-    return piles.size() % 2 == 0;
+    std::vector<int> prevDiffs = piles, diffs;
+    for (std::size_t i = 1; i < diffs.size(); ++i) {
+      for (int j = diffs.size() - i - 1; j >= 0; --j) {
+        diffs[j] = std::max(
+            piles[j] - prevDiffs[j + i], piles[j + i] - prevDiffs[j]);
+      }
+      std::swap(prevDiffs, diffs);
+    }
+    return prevDiffs[0] > 0;
   }
 };
