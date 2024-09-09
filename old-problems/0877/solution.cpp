c9cbdcb3582773e6061b867aee82efa6859ecfc6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-09 08:31:15 +0700

    feat(problem-0877/cpp): solve by return true

--- old-problems/0877/solution.cpp
+++ old-problems/0877/solution.cpp
@@ -1,17 +1,8 @@
-#include <utility>
 #include <vector>
 
 class Solution {
  public:
-  bool stoneGame(std::vector<int>& piles) {
-    std::vector<int> prevDiffs = piles, diffs;
-    for (std::size_t i = 1; i < diffs.size(); ++i) {
-      for (int j = diffs.size() - i - 1; j >= 0; --j) {
-        diffs[j] = std::max(
-            piles[j] - prevDiffs[j + i], piles[j + i] - prevDiffs[j]);
-      }
-      std::swap(prevDiffs, diffs);
-    }
-    return prevDiffs[0] > 0;
+  bool stoneGame([[maybe_unused]] std::vector<int>& piles) {
+    return true;
   }
 };
