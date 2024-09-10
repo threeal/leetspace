16bb99947761bcac4ce3aad99394d37120a40a81
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-10 20:55:44 +0700

    feat(problem--1888): solve by using sliding window

--- old-problems/1888/solution.cpp
+++ old-problems/1888/solution.cpp
@@ -1,8 +1,42 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   int minFlips(std::string s) {
-    return s.size();
+    int zeroDiff{0}, oneDiff{0};
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      if (s[i] == '0') {
+        ++oneDiff;
+      } else {
+        ++zeroDiff;
+      }
+
+      if (++i >= s.size()) break;
+
+      if (s[i] == '1') {
+        ++oneDiff;
+      } else {
+        ++zeroDiff;
+      }
+    }
+
+    int minDiff = std::min(zeroDiff, oneDiff);
+    if (s.size() % 2 != 0) {
+      for (std::size_t i{s.size() - 1}; i > 0; --i) {
+        std::swap(zeroDiff, oneDiff);
+        if (s[i] == '1') {
+          ++zeroDiff;
+          --oneDiff;
+          if (oneDiff < minDiff) minDiff = oneDiff;
+        } else {
+          --zeroDiff;
+          ++oneDiff;
+          if (zeroDiff < minDiff) minDiff = zeroDiff;
+        }
+      }
+    }
+
+    return minDiff;
   }
 };
