ac2be81c866fac198f1a43bc5cae3c6cb3799769
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-10 20:54:16 +0700

    feat(problem-2028/cpp): solve by determining missing rolls from mean

--- old-problems/2028/solution.cpp
+++ old-problems/2028/solution.cpp
@@ -1,9 +1,19 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
-  std::vector<int> missingRolls(std::vector<int>& rolls, int /*mean*/, int n) {
-    rolls.resize(n);
-    return rolls;
+  std::vector<int> missingRolls(std::vector<int>& rolls, int mean, int n) {
+    const int observedTotal = std::accumulate(rolls.begin(), rolls.end(), 0);
+    const int missingTotal = mean * (rolls.size() + n) - observedTotal;
+
+    if (missingTotal < n || missingTotal > n * 6) return {};
+
+    std::vector<int> out(n);
+    std::fill(out.begin(), out.begin() + missingTotal % n, missingTotal / n + 1);
+    std::fill(out.begin() + missingTotal % n, out.end(), missingTotal / n);
+
+    return out;
   }
 };
