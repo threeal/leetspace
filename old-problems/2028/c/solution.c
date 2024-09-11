c5a337c1e6d3a79fd6e1048e1f53091fe470b714
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-11 12:07:56 +0700

    feat(problem-2028/c): solve by determining missing rolls from mean

--- old-problems/2028/c/solution.c
+++ old-problems/2028/c/solution.c
@@ -1,8 +1,22 @@
+#include <stdlib.h>
+
 int* missingRolls(int* rolls, int rollsSize, int mean, int n, int* returnSize) {
-  (void)rolls;
-  (void)rollsSize;
-  (void)mean;
-  (void)n;
-  *returnSize = 0;
-  return 0;
+  int observedTotal = 0;
+  for (int i = rollsSize - 1; i >= 0; --i) {
+    observedTotal += rolls[i];
+  }
+
+  const int missingTotal = mean * (rollsSize + n) - observedTotal;
+
+  if (missingTotal < n || missingTotal > n * 6) {
+    *returnSize = 0;
+    return NULL;
+  }
+
+  *returnSize = n;
+  int* out = malloc(n * sizeof(int));
+  for (int i = missingTotal % n - 1; i >= 0; --i) out[i] = missingTotal / n + 1;
+  for (int i = missingTotal % n; i < n; ++i) out[i] = missingTotal / n;
+
+  return out;
 }
