9502e393734c42c0a12e30b71b52bee30676f80c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-12 12:21:40 +0700

    feat(problem-1888/c): solve by using sliding window

--- old-problems/1888/c/solution.c
+++ old-problems/1888/c/solution.c
@@ -1,4 +1,40 @@
 int minFlips(char* s) {
-  (void)s;
-  return 0;
+  int i = 0, zeroDiff = 0, oneDiff = 0;
+  while (s[i] != 0) {
+    if (s[i] == '0') {
+      ++oneDiff;
+    } else {
+      ++zeroDiff;
+    }
+
+    if (s[++i] == 0) break;
+
+    if (s[i] == '1') {
+      ++oneDiff;
+    } else {
+      ++zeroDiff;
+    }
+
+    ++i;
+  }
+
+  int minDiff = zeroDiff < oneDiff ? zeroDiff : oneDiff;
+  if (i % 2 != 0) {
+    for (--i; i > 0; --i) {
+      const int temp = zeroDiff;
+      zeroDiff = oneDiff;
+      oneDiff = temp;
+      if (s[i] == '1') {
+        ++zeroDiff;
+        --oneDiff;
+        if (oneDiff < minDiff) minDiff = oneDiff;
+      } else {
+        --zeroDiff;
+        ++oneDiff;
+        if (zeroDiff < minDiff) minDiff = zeroDiff;
+      }
+    }
+  }
+
+  return minDiff;
 }
