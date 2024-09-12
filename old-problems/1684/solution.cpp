a894c446526e915631bf821a737447108154a2dc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-12 11:58:20 +0700

    feat(problem-1684/cpp): solve by using an array map

--- old-problems/1684/solution.cpp
+++ old-problems/1684/solution.cpp
@@ -5,6 +5,22 @@ class Solution {
  public:
   int countConsistentStrings(
       std::string allowed, std::vector<std::string>& words) {
-    return words.size() - allowed.size();
+    std::vector<bool> exists(26, false);
+    for (const auto c : allowed) {
+      exists[c - 'a'] = true;
+    }
+
+    int count{0};
+    for (const auto& word : words) {
+      ++count;
+      for (const auto c : word) {
+        if (!exists[c - 'a']) {
+          --count;
+          break;
+        }
+      }
+    }
+
+    return count;
   }
 };
