65526b9428062e1e1c37676da537ecb9df896a2a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-13 14:16:43 +0700

    feat(problem-1690/cpp): solve by checking all possibilities

--- old-problems/1690/solution.cpp
+++ old-problems/1690/solution.cpp
@@ -3,6 +3,21 @@
 class Solution {
  public:
   int stoneGameVII(std::vector<int>& stones) {
-    return stones.front();
+    for (std::size_t i = 1; i < stones.size(); ++i) {
+      stones[i] += stones[i - 1];
+    }
+
+    std::vector<int> diffs = stones;
+    std::vector<int> prevDiffs(stones.size(), 0);
+    for (std::size_t i = 1; i < diffs.size(); ++i) {
+      for (int j = diffs.size() - i - 1; j >= 0; --j) {
+        diffs[j] = std::max(
+            stones[j + i] - stones[j] - prevDiffs[j + 1],
+            stones[j + i - 1] - (j > 0 ? stones[j - 1] : 0) - prevDiffs[j]);
+      }
+      std::swap(prevDiffs, diffs);
+    }
+
+    return prevDiffs[0];
   }
 };
