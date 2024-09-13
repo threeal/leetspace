1fed7d4405cd343dbd7259715996ce923558fd7d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-13 09:44:38 +0700

    feat(problem-1310/cpp): solve by using a prefix XOR array

--- old-problems/1310/solution.cpp
+++ old-problems/1310/solution.cpp
@@ -4,6 +4,17 @@ class Solution {
  public:
   std::vector<int> xorQueries(
       std::vector<int>& arr, std::vector<std::vector<int>>& queries) {
-    return std::vector(queries.size(), arr.front());
+    for (std::size_t i{1}; i < arr.size(); ++i) arr[i] ^= arr[i - 1];
+
+    std::vector<int> outputs{};
+    outputs.reserve(queries.size());
+    for (const auto& query : queries) {
+      if (query[0] > 0) {
+        outputs.push_back(arr[query[1]] ^ arr[query[0] - 1]);
+      } else {
+        outputs.push_back(arr[query[1]]);
+      }
+    }
+    return outputs;
   }
 };
