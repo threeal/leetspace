0711efffabe53d5576e38102ac9ee47e3f7b35d8
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-14 10:10:31 +0700

    feat(problem-2419/cpp): solve by finding longest subarray of max elements

--- old-problems/2419/solution.cpp
+++ old-problems/2419/solution.cpp
@@ -3,6 +3,23 @@
 class Solution {
  public:
   int longestSubarray(std::vector<int>& nums) {
-    return nums.size();
+    int maxConsecutive{0};
+    int maxNum{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      if (nums[i] < maxNum) continue;
+      if (nums[i] > maxNum) {
+        maxNum = nums[i];
+        maxConsecutive = 1;
+        for (++i; i < nums.size() && nums[i] == maxNum; ++i) ++maxConsecutive;
+        --i;
+      } else {
+        maxNum = nums[i];
+        int consecutive{1};
+        for (++i; i < nums.size() && nums[i] == maxNum; ++i) ++consecutive;
+        if (consecutive > maxConsecutive) maxConsecutive = consecutive;
+        --i;
+      }
+    }
+    return maxConsecutive;
   }
 };
