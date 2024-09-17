9cdb49325dd3629d7953521460d81e60937e62e4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-17 16:40:11 +0700

    feat(problem-2348/c): solve by iterating consecutive zeros
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2348/c/solution.c
+++ old-problems/2348/c/solution.c
@@ -1,3 +1,12 @@
 long long zeroFilledSubarray(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  long long count = 0, zeros = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] == 0) {
+      ++zeros;
+    } else {
+      count += ((zeros + 1) * zeros) / 2;
+      zeros = 0;
+    }
+  }
+  return count + ((zeros + 1) * zeros) / 2;
 }
