f842a54d5fd1f42f394e637b446dffbb34cd83f2
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-17 17:32:14 +0700

    feat(problem-413/cpp): solve by counting arithmetic sequences
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0413/solution.cpp
+++ old-problems/0413/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int numberOfArithmeticSlices(std::vector<int>& nums) {
-    return nums.size();
+    int count{0}, length{1};
+    for (std::size_t i{2}; i < nums.size(); ++i) {
+      if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+        count += length;
+        ++length;
+      } else {
+        length = 1;
+      }
+    }
+    return count;
   }
 };
