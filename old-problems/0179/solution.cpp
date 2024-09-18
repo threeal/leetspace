4108e8e3b84ed2a0f6b0cc91f5a0b1a8b0546b7f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-18 14:26:34 +0700

    feat(problem-179/cpp): solve by sorting numbers lexically
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0179/solution.cpp
+++ old-problems/0179/solution.cpp
@@ -1,9 +1,23 @@
+#include <algorithm>
 #include <string>
 #include <vector>
 
 class Solution {
  public:
-  std::string largestNumber(std::vector<int>& /*nums*/) {
-    return "";
+  std::string largestNumber(std::vector<int>& nums) {
+    std::vector<std::string> strs(nums.size());
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      strs[i] = std::to_string(nums[i]);
+    }
+
+    std::sort(
+        strs.begin(), strs.end(),
+        [](const std::string& a, const std::string& b) {
+          return a + b > b + a;
+        });
+
+    std::string output{};
+    for (const auto& str : strs) output += str;
+    return output.front() == '0' ? "0" : output;
   }
 };
