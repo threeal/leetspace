156932efd01dcc7e36bddf67aa67bcef7ea40028
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-19 14:40:32 +0700

    feat(problem-546/cpp): solve by using a dynamic programming approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0546/solution.cpp
+++ old-problems/0546/solution.cpp
@@ -3,6 +3,35 @@
 class Solution {
  public:
   int removeBoxes(std::vector<int>& boxes) {
-    return boxes.size();
+    std::vector<std::vector<std::vector<int>>> cache(
+        boxes.size(),
+        std::vector<std::vector<int>>(
+            boxes.size(), std::vector<int>(boxes.size(), -1)));
+
+    return removeBoxesRange(cache.data(), boxes.data(), 0, boxes.size() - 1, 0);
+  }
+
+ private:
+  static int removeBoxesRange(
+      std::vector<std::vector<int>>* cache,
+      int* boxes, int left, int right, int prev) {
+    if (left > right) return 0;
+    if (cache[left][right][prev] >= 0) return cache[left][right][prev];
+
+    int i{left + 1};
+    while (i <= right && boxes[i] == boxes[left]) ++i;
+    const int count = prev + i - left;
+
+    int bestScore = count * count + removeBoxesRange(cache, boxes, i, right, 0);
+    for (int j{i + 1}; j <= right; ++j) {
+      if (boxes[j] == boxes[left]) {
+        const int score = removeBoxesRange(cache, boxes, i, j - 1, 0) +
+            removeBoxesRange(cache, boxes, j, right, count);
+        if (score > bestScore) bestScore = score;
+      }
+    }
+
+    cache[left][right][prev] = bestScore;
+    return bestScore;
   }
 };
