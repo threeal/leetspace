b097cfe42a617a955cabfc58d14b1edb55f8c2fe
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-19 14:40:40 +0700

    perf(problem-546/cpp): use a 1d array for the cache
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0546/solution.cpp
+++ old-problems/0546/solution.cpp
@@ -3,20 +3,18 @@
 class Solution {
  public:
   int removeBoxes(std::vector<int>& boxes) {
-    std::vector<std::vector<std::vector<int>>> cache(
-        boxes.size(),
-        std::vector<std::vector<int>>(
-            boxes.size(), std::vector<int>(boxes.size(), -1)));
-
-    return removeBoxesRange(cache.data(), boxes.data(), 0, boxes.size() - 1, 0);
+    std::vector<int> cache(boxes.size() * boxes.size() * boxes.size(), -1);
+    return removeBoxesRange(cache.data(), boxes, 0, boxes.size() - 1, 0);
   }
 
  private:
   static int removeBoxesRange(
-      std::vector<std::vector<int>>* cache,
-      int* boxes, int left, int right, int prev) {
+      int* cache, const std::vector<int>& boxes, int left, int right, int prev) {
     if (left > right) return 0;
-    if (cache[left][right][prev] >= 0) return cache[left][right][prev];
+
+    const int cacheId =
+      left * boxes.size() * boxes.size() + right * boxes.size() + prev;
+    if (cache[cacheId] >= 0) return cache[cacheId];
 
     int i{left + 1};
     while (i <= right && boxes[i] == boxes[left]) ++i;
@@ -28,10 +26,14 @@ class Solution {
         const int score = removeBoxesRange(cache, boxes, i, j - 1, 0) +
             removeBoxesRange(cache, boxes, j, right, count);
         if (score > bestScore) bestScore = score;
+
+        ++j;
+        while (j <= right && boxes[j] == boxes[left]) ++j;
+        --j;
       }
     }
 
-    cache[left][right][prev] = bestScore;
+    cache[cacheId] = bestScore;
     return bestScore;
   }
 };
