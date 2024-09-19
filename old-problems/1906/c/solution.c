f586062c9bb37c9807f9705a67b85fa46716c571
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-19 20:10:35 +0700

    feat(problem-1906/c): solve by using a prefix sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1906/c/solution.c
+++ old-problems/1906/c/solution.c
@@ -1,15 +1,46 @@
+#include <limits.h>
 #include <stdlib.h>
+#include <string.h>
 
 int* minDifference(
     int* nums, int numsSize,
     int** queries, int queriesSize, int* queriesColSize,
     int* returnSize) {
-  (void)nums;
-  (void)numsSize;
-  (void)queries;
   (void)queriesColSize;
 
-  int* returnData = malloc(queriesSize * sizeof(int));
+  int* freqs = malloc((numsSize + 1) * 100 * sizeof(int));
+  memset(freqs, 0, 100 * sizeof(int));
+
+  for (int i = 0; i < numsSize; ++i) {
+    memcpy(freqs + (i + 1) * 100, freqs + i * 100, 100 * sizeof(int));
+    ++freqs[(i + 1) * 100 + nums[i] - 1];
+  }
+
+  int* diffs = malloc(queriesSize * sizeof(int));
   *returnSize = queriesSize;
-  return returnData;
+  for (int i = 0; i < queriesSize; ++i) {
+    int diff = INT_MAX;
+
+    int prev = 0, j = 0;
+    while (j < 100) {
+      if (freqs[(queries[i][1] + 1) * 100 + j] - freqs[queries[i][0] * 100 + j] > 0) {
+        prev = j;
+        break;
+      }
+      ++j;
+    }
+
+    for (int j = prev + 1; j < 100; ++j) {
+      if (freqs[(queries[i][1] + 1) * 100 + j] - freqs[queries[i][0] * 100 + j] > 0) {
+        if (j - prev < diff) diff = j - prev;
+        prev = j;
+      }
+    }
+
+    diffs[i] = diff == INT_MAX ? -1 : diff;
+  }
+
+  free(freqs);
+
+  return diffs;
 }
