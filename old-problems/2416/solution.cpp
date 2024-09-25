bafafe57443dd23e11f7176be692d489b5196dfa
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-25 12:47:41 +0700

    test: add test for problem 2416. Sum of Prefix Scores of Strings
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2416/solution.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> sumPrefixScores(std::vector<std::string>& words) {
+    return std::vector(words.size(), 0);
+  }
+};
