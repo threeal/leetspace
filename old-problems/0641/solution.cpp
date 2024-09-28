5d4e3e3fcce4307637b27f9bd4d59e1311cf20b2
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-28 15:30:00 +0700

    feat(problem-641/cpp): solve by storing deque in fixed-size array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0641/solution.cpp
+++ old-problems/0641/solution.cpp
@@ -1,36 +1,69 @@
+#include <vector>
+
 class MyCircularDeque {
+ private:
+  std::vector<int> data;
+  std::size_t left;
+  std::size_t size;
+
  public:
-  MyCircularDeque(int /*k*/) {}
+  MyCircularDeque(int k) : data(k), left{0}, size{0} {}
 
   bool insertFront(int value) {
-    return value == 0;
+    if (size == data.size()) return false;
+
+    if (left == 0) {
+      left = data.size() - 1;
+    } else {
+      --left;
+    }
+
+    data[left] = value;
+    ++size;
+
+    return true;
   }
 
   bool insertLast(int value) {
-    return value == 0;
+    if (size == data.size()) return false;
+
+    data[(left + size) % data.size()] = value;
+    ++size;
+
+    return true;
   }
 
   bool deleteFront() {
-    return false;
+    if (size == 0) return false;
+    if (left == data.size() - 1) {
+      left = 0;
+    } else {
+      ++left;
+    }
+    --size;
+    return true;
   }
 
   bool deleteLast() {
-    return false;
+    if (size == 0) return false;
+    --size;
+    return true;
   }
 
   int getFront() {
-    return 0;
+    return size == 0 ? -1 : data[left];
   }
 
   int getRear() {
-    return 0;
+    if (size == 0) return -1;
+    return size == 0 ? -1 : data[(left + size - 1) % data.size()];
   }
 
   bool isEmpty() {
-    return false;
+    return size == 0;
   }
 
   bool isFull() {
-    return false;
+    return size == data.size();
   }
 };
