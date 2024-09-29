0c65fcd43e4e99427d31ee031d8e6b688b971bce
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-29 22:08:43 +0700

    feat(problem-1690/c): solve by checking all possibilities
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1690/c/solution.c
+++ old-problems/1690/c/solution.c
@@ -1,3 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+
 int stoneGameVII(int* stones, int stonesSize) {
-  return stones[stonesSize - 1];
+  for (int i = 1; i < stonesSize; ++i) {
+    stones[i] += stones[i - 1];
+  }
+
+  int* diffs = malloc(stonesSize * sizeof(int));
+  memset(diffs, 0, stonesSize * sizeof(int));
+
+  for (int i = 1; i < stonesSize; ++i) {
+    for (int j = 0; j < stonesSize - i; ++j) {
+      const int a = stones[j + i] - stones[j] - diffs[j + 1];
+      const int b = stones[j + i - 1] - (j > 0 ? stones[j - 1] : 0) - diffs[j];
+      diffs[j] = a > b ? a : b;
+    }
+  }
+
+  const int res = diffs[0];
+  free(diffs);
+  return res;
 }
