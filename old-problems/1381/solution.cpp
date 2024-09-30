0385932c0ab1df48326652b99f5def9ac2a5b2bf
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-30 09:19:42 +0700

    feat(problem-1381/cpp): solve by storing stack in fixed-size array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1381/solution.cpp
+++ old-problems/1381/solution.cpp
@@ -1,12 +1,30 @@
+#include <vector>
+
 class CustomStack {
+ private:
+  std::vector<int> data;
+  std::size_t size;
+
  public:
-  CustomStack(int /*maxSize*/) {}
+  CustomStack(int maxSize) : data(maxSize), size{0} {}
 
-  void push(int /*x*/) {}
-
-  int pop() {
-    return -1;
+  void push(int x) {
+    if (size == data.size()) return;
+    data[size] = x;
+    if (size > 0) data[size - 1] -= x;
+    ++size;
   }
 
-  void increment(int /*k*/, int /*val*/) {}
+  int pop() {
+    if (size == 0) return -1;
+    --size;
+    if (size > 0) data[size - 1] += data[size];
+    return data[size];
+  }
+
+  void increment(int k, int val) {
+    if (size == 0) return;
+    if (k > static_cast<int>(size)) k = size;
+    data[k - 1] += val;
+  }
 };
