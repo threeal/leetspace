a310151e5d92ef04c8fedd2544618a8e1d1f8045
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-01 13:07:04 +0700

    feat(problem-1497/cpp): solve by counting modulo frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1497/solution.cpp
+++ old-problems/1497/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   bool canArrange(std::vector<int>& arr, int k) {
-    return arr.front() == k;
+    std::vector<int> mod(k, 0);
+    for (const auto num : arr) ++mod[((num % k) + k) % k];
+
+    if (mod[0] % 2 != 0) return false;
+    if (k % 2 == 0 && mod[k / 2] % 2 != 0) return false;
+
+    for (int i = 1; i < k - i; ++i) {
+      if (mod[i] != mod[k - i]) return false;
+    }
+
+    return true;
   }
 };
