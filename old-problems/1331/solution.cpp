4a49a0c04470bdc73efd7450dcc8f46812f5154b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-02 12:55:54 +0700

    feat(problem-1331/cpp): solve by sorting index
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1331/solution.cpp
+++ old-problems/1331/solution.cpp
@@ -1,8 +1,27 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> arrayRankTransform(std::vector<int>& arr) {
+    std::vector<int> idxs(arr.size());
+    std::iota(idxs.begin(), idxs.end(), 0);
+
+    std::sort(idxs.begin(), idxs.end(), [&arr](int a, int b) {
+      return arr[a] < arr[b];
+    });
+
+    int prev{-1};
+    int rank{0};
+    for (const auto idx : idxs) {
+      if (arr[idx] != prev) {
+        prev = arr[idx];
+        ++rank;
+      }
+      arr[idx] = rank;
+    }
+
     return arr;
   }
 };
