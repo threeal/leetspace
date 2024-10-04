9477eaa08b39dcadc1c0804caf72cb8fe6b78da5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-04 22:30:54 +0700

    feat(problem-1371/c): solve by using XOR mask
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1371/c/solution.c
+++ old-problems/1371/c/solution.c
@@ -1,3 +1,38 @@
 int findTheLongestSubstring(char* s) {
-  return *s;
+  int maskPos[32] = {
+      -1, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+      -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2};
+
+  int longest = 0;
+  unsigned char mask = 0;
+  for (int i = 0; s[i] != 0; ++i) {
+    switch (s[i]) {
+      case 'a':
+        mask ^= 1;
+        break;
+
+      case 'e':
+        mask ^= 2;
+        break;
+
+      case 'i':
+        mask ^= 4;
+        break;
+
+      case 'o':
+        mask ^= 8;
+        break;
+
+      case 'u':
+        mask ^= 16;
+        break;
+    }
+
+    if (maskPos[mask] < -1) {
+      maskPos[mask] = i;
+    } else if (i - maskPos[mask] > longest) {
+      longest = i - maskPos[mask];
+    }
+  }
+  return longest;
 }
