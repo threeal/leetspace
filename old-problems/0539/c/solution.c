cc333d53ba11a79b78a0a596076e9f9c066d25e7
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-05 15:11:42 +0700

    feat(problem-539/c): solve by converting to integer and sort
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0539/c/solution.c
+++ old-problems/0539/c/solution.c
@@ -1,3 +1,25 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int findMinDifference(char** timePoints, int timePointsSize) {
-  return *(timePoints[timePointsSize - 1]);
+  int* minutes = malloc(timePointsSize * sizeof(int));
+  for (int i = timePointsSize - 1; i >= 0; --i) {
+    minutes[i] = (timePoints[i][0] - '0') * 600 +
+        (timePoints[i][1] - '0') * 60 +
+        (timePoints[i][3] - '0') * 10 +
+        (timePoints[i][4] - '0');
+  }
+
+  qsort(minutes, timePointsSize, sizeof(int), compare);
+
+  int min = 24 * 60 + minutes[0] - minutes[timePointsSize - 1];
+  for (int i = timePointsSize - 1; i > 0; --i) {
+    if (minutes[i] - minutes[i - 1] < min) min = minutes[i] - minutes[i - 1];
+  }
+
+  free(minutes);
+  return min;
 }
