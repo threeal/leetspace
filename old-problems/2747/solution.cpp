989fa313f4f5762778ede353feca4d049ea3f426
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-06 13:04:12 +0700

    feat(problem-2747/cpp): solve by using sorting and sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2747/solution.cpp
+++ old-problems/2747/solution.cpp
@@ -1,10 +1,50 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> countServers(
-      int n, std::vector<std::vector<int>>& /*logs*/, int /*x*/,
+      int n, std::vector<std::vector<int>>& logs, int x,
       std::vector<int>& queries) {
-    return std::vector<int>(queries.size(), n);
+    std::sort(logs.begin(), logs.end(), [](const auto& a, const auto& b) {
+      return a[1] < b[1];
+    });
+
+    std::vector<int> queryIdxs(queries.size());
+    std::iota(queryIdxs.begin(), queryIdxs.end(), 0);
+    std::sort(
+        queryIdxs.begin(), queryIdxs.end(),
+        [&queries](const auto a, const auto b) {
+          return queries[a] < queries[b];
+        });
+
+    int currentMissing{n};
+    std::vector<int> currentRequests(n, 0);
+    auto startLog = logs.begin();
+    auto endLog = logs.begin();
+
+    std::vector<int> missings(queries.size());
+    for (std::size_t i{0}; i < queryIdxs.size(); ++i) {
+      const int end{queries[queryIdxs[i]]};
+      while (startLog != logs.end() && (*startLog)[1] <= end) {
+        const int id{(*startLog)[0] - 1};
+        if (currentRequests[id] == 0) --currentMissing;
+        ++currentRequests[id];
+        ++startLog;
+      }
+
+      const int start{queries[queryIdxs[i]] - x};
+      while (endLog != logs.end() && (*endLog)[1] < start) {
+        const int id{(*endLog)[0] - 1};
+        if (currentRequests[id] == 1) ++currentMissing;
+        --currentRequests[id];
+        ++endLog;
+      }
+
+      missings[queryIdxs[i]] = currentMissing;
+    }
+
+    return missings;
   }
 };
