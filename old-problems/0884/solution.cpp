f60e2dff45d22f5ec4ab41bf7a1d5e1a4158d4e3
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-06 13:05:33 +0700

    feat: solve problem 884. Uncommon Words from Two Sentences (#1927)
    * test: add test for problem 884. Uncommon Words from Two Sentences
    
    * feat(problem-884/cpp): solve by counting frequencies in hash map
    
    * test(problem-884): sort output
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0884/solution.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::string> uncommonFromSentences(
+      std::string s1, std::string s2) {
+    std::unordered_map<std::string, int> freqs{};
+
+    std::size_t start{0};
+    for (std::size_t end{1}; end < s1.size(); ++end) {
+      if (s1[end] == ' ') {
+        ++freqs[s1.substr(start, end - start)];
+        start = end + 1;
+        ++end;
+      }
+    }
+    ++freqs[s1.substr(start, s1.size() - start)];
+
+    start = 0;
+    for (std::size_t end{1}; end < s2.size(); ++end) {
+      if (s2[end] == ' ') {
+        ++freqs[s2.substr(start, end - start)];
+        start = end + 1;
+        ++end;
+      }
+    }
+    ++freqs[s2.substr(start, s2.size() - start)];
+
+    std::vector<std::string> output{};
+    for (const auto& [s, freq] : freqs) {
+      if (freq == 1) output.push_back(s);
+    }
+    return output;
+  }
+};
