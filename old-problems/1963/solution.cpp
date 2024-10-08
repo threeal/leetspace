89340c3e803c33b654061581e4739f3aedc13cd1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-08 13:01:31 +0700

    feat(problem-1963/cpp): solve by using two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1963/solution.cpp
+++ old-problems/1963/solution.cpp
@@ -3,6 +3,39 @@
 class Solution {
  public:
   int minSwaps(std::string s) {
-    return s.size();
+    int swap{0}, lb{0}, rb{0};
+    std::size_t l{0}, r{s.size() - 1};
+    while (l < r) {
+      while (l < r) {
+        if (s[l] == '[') {
+          ++lb;
+        } else {
+          --lb;
+          if (lb < 0) {
+            lb += 2;
+            ++swap;
+            break;
+          }
+        }
+        ++l;
+      }
+
+      while (l < r) {
+        if (s[r] == ']') {
+          ++rb;
+        } else {
+          --rb;
+          if (rb < 0) {
+            rb -= 2;
+            break;
+          }
+        }
+        --r;
+      }
+
+      ++l, --r;
+    }
+
+    return swap;
   }
 };
