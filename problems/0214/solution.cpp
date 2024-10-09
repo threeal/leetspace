c6bee90d86f5735ed4d88642c5af3d776a7aef91
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-09 21:34:04 +0700

    feat(problem-214/cpp): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- problems/0214/solution.cpp
+++ problems/0214/solution.cpp
@@ -1,8 +1,16 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   std::string shortestPalindrome(std::string s) {
-    return s;
+    std::size_t i{0};
+    for (int j = s.size() - 1; j >= 0; --j) {
+      if (s[i] == s[j]) ++i;
+    }
+    if (i == s.size()) return s;
+    auto rev = s.substr(i);
+    std::reverse(rev.begin(), rev.end());
+    return rev + shortestPalindrome(s.substr(0, i)) + s.substr(i);
   }
 };
