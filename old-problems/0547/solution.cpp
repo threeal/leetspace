8b5112b19e0d0c06c97c2dd187c51d2303724a3b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-10 19:53:48 +0700

    feat(problem-547/cpp): solve by merging graphs
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0547/solution.cpp
+++ old-problems/0547/solution.cpp
@@ -1,8 +1,34 @@
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   int findCircleNum(std::vector<std::vector<int>>& isConnected) {
-    return isConnected.size();
+    int distinctCount = isConnected.size();
+
+    std::vector<int> roots(isConnected.size());
+    std::iota(roots.begin(), roots.end(), 0);
+
+    for (std::size_t i{0}; i < isConnected.size(); ++i) {
+      for (std::size_t j{i + 1}; j < isConnected.size(); ++j) {
+        if (isConnected[i][j] == 0) continue;
+
+        int rootI = findRoot(roots.data(), i);
+        int rootJ = findRoot(roots.data(), j);
+        if (rootI != rootJ) {
+          if (rootI > rootJ) std::swap(rootI, rootJ);
+          roots[rootI] = rootJ;
+          --distinctCount;
+        }
+      }
+    }
+
+    return distinctCount;
+  }
+
+ private:
+  static int findRoot(int* roots, int i) {
+    if (roots[i] != i) roots[i] = findRoot(roots, roots[i]);
+    return roots[i];
   }
 };
