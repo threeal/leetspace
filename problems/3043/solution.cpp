93509b42869a49895138367c94a490b0ec44f463
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-10 19:52:37 +0700

    feat(problem-3043/cpp): solve by using a hash set
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- problems/3043/solution.cpp
+++ problems/3043/solution.cpp
@@ -1,8 +1,33 @@
+#include <unordered_set>
 #include <vector>
 
 class Solution {
  public:
   int longestCommonPrefix(std::vector<int>& arr1, std::vector<int>& arr2) {
-    return arr1.size() * arr2.size();
+    std::unordered_set<int> prefixes{};
+    for (auto num : arr1) {
+      while (num > 0) {
+        prefixes.insert(num);
+        num /= 10;
+      }
+    }
+
+    int maxLength{0};
+    for (auto num : arr2) {
+      while (num > 0) {
+        if (prefixes.contains(num)) {
+          int length{0};
+          while (num > 0) {
+            ++length;
+            num /= 10;
+          }
+          if (length > maxLength) maxLength = length;
+          break;
+        }
+        num /= 10;
+      }
+    }
+
+    return maxLength;
   }
 };
