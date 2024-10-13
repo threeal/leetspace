d4b00f2ac885cecef5e248610f268c1a950a7425
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-13 21:43:10 +0700

    feat(problem-1497/c): solve by counting modulo frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1497/c/solution.c
+++ old-problems/1497/c/solution.c
@@ -1,5 +1,21 @@
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 bool canArrange(int* arr, int arrSize, int k) {
-  return arr[arrSize] == k;
+  int* mod = malloc(k * sizeof(int));
+  memset(mod, 0, k * sizeof(int));
+
+  for (int i = 0; i < arrSize; ++i) {
+    ++mod[((arr[i] % k) + k) % k];
+  }
+
+  if (mod[0] % 2 != 0) return false;
+  if (k % 2 == 0 && mod[k / 2] % 2 != 0) return false;
+
+  for (int i = 1; i < k - i; ++i) {
+    if (mod[i] != mod[k - i]) return false;
+  }
+
+  return true;
 }
