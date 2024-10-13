1b40b5872d353c64c56e6bf0937f336b8ed2b4e4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-13 21:34:00 +0700

    feat(problem-2216/cpp): solve by iterating array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2216/solution.cpp
+++ old-problems/2216/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int minDeletion(std::vector<int>& nums) {
-    return nums.size();
+    int deletion{};
+    for (std::size_t i{1}; i < nums.size(); i += 2) {
+      while (i < nums.size() && nums[i - 1] == nums[i]) {
+        ++deletion;
+        ++i;
+      }
+    }
+    if ((nums.size() - deletion) % 2 != 0) ++deletion;
+    return deletion;
   }
 };
