4a001e8113e9a455de00000ee3537635eda14bc5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-14 09:25:36 +0700

    feat(problem-2530/cpp): solve by using a priority queue
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2530/solution.cpp
+++ old-problems/2530/solution.cpp
@@ -1,8 +1,19 @@
+#include <queue>
 #include <vector>
 
 class Solution {
  public:
   long long maxKelements(std::vector<int>& nums, int k) {
-    return nums.front() * k;
+    long long sum{0};
+    std::priority_queue<int> maxs(nums.begin(), nums.end());
+    while (k > 0) {
+      const int max{maxs.top()};
+      maxs.pop();
+
+      sum += max;
+      maxs.push(max % 3 == 0 ? max / 3 : max / 3 + 1);
+      --k;
+    }
+    return sum;
   }
 };
