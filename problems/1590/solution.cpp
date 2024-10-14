57e297f3f32547f867a71cfc4eb19f5680391f66
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-14 21:19:57 +0700

    feat(problem-1590/cpp): solve by using for loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- problems/1590/solution.cpp
+++ problems/1590/solution.cpp
@@ -1,8 +1,22 @@
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   int minSubarray(std::vector<int>& nums, int p) {
-    return nums.size() - p;
+    const auto mod = std::accumulate(nums.begin(), nums.end(), 0ll) % p;
+    if (mod == 0) return 0;
+
+    for (std::size_t n{1}; n < nums.size(); ++n) {
+      auto sum = std::accumulate(nums.begin(), nums.begin() + n, 0ll);
+      if (sum % p == mod) return n;
+
+      for (std::size_t i{n}; i < nums.size(); ++i) {
+        sum = sum + nums[i] - nums[i - n];
+        if (sum % p == mod) return n;
+      }
+    }
+
+    return -1;
   }
 };
