a1dba1250c966da2e878d2cad9b951dafa78e280
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-14 19:50:20 +0700

    feat(problem-2216/c): solve by iterating array

--- old-problems/2216/c/solution.c
+++ old-problems/2216/c/solution.c
@@ -1,3 +1,11 @@
 int minDeletion(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int deletion = 0;
+  for (int i = 1; i < numsSize; i += 2) {
+    while (i < numsSize && nums[i - 1] == nums[i]) {
+      ++deletion;
+      ++i;
+    }
+  }
+  if ((numsSize - deletion) % 2 != 0) ++deletion;
+  return deletion;
 }
