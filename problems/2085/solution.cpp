3c4a24865ba860403615437cd056aca073244ee6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-15 21:03:44 +0700

    feat(problem-2085/cpp): solve by using a hash map
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- problems/2085/solution.cpp
+++ problems/2085/solution.cpp
@@ -1,10 +1,28 @@
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int countWords(
       std::vector<std::string>& words1, std::vector<std::string>& words2) {
-    return words1.size() + words2.size();
+    std::unordered_map<std::string, int> frequencies{};
+    for (const auto& word : words1) ++frequencies[word];
+
+    int count{0};
+    for (const auto& word : words2) {
+      auto it = frequencies.find(word);
+      if (it != frequencies.end()) {
+        if (it->second == 1) {
+          ++count;
+          --(it->second);
+        } else if (it->second == 0) {
+          --count;
+          --(it->second);
+        }
+      }
+    }
+
+    return count;
   }
 };
