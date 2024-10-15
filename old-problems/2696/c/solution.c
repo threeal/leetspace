c922de1cb98acbe871916a15169d667b6ecda25e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-15 21:10:10 +0700

    feat(problem-2696/c): solve by using stack and iteration
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2696/c/solution.c
+++ old-problems/2696/c/solution.c
@@ -1,3 +1,11 @@
 int minLength(char* s) {
-  return *s;
+  int i = -1;
+  for (char* c = s; *c != 0; ++c) {
+    if (i >= 0 && ((s[i] == 'A' && *c == 'B') || (s[i] == 'C' && *c == 'D'))) {
+      --i;
+    } else {
+      s[++i] = *c;
+    }
+  }
+  return i + 1;
 }
