57e7b780f2e06ce2897cc61d67dd14449d6e2021
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-15 12:34:10 +0700

    feat(problem-567/c): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0567/c/solution.c
+++ old-problems/0567/c/solution.c
@@ -1,5 +1,25 @@
 #include <stdbool.h>
+#include <string.h>
 
 bool checkInclusion(char* s1, char* s2) {
-  return s1 == s2;
+  int freqs1[26] = {0};
+  int s1Len = 0;
+  while (s1[s1Len] != 0) {
+    ++freqs1[s1[s1Len] - 'a'];
+    ++s1Len;
+  }
+
+  int freqs2[26] = {0};
+  int s2Len = strlen(s2);
+  for (int l = 0, r = 0; r < s2Len; ++r) {
+    const char c = s2[r] - 'a';
+    ++freqs2[c];
+    while (l <= r && freqs2[c] > freqs1[c]) {
+      --freqs2[s2[l] - 'a'];
+      ++l;
+    }
+    if (r - l + 1 == s1Len) return true;
+  }
+
+  return false;
 }
