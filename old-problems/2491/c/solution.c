ff973426d9b23a10c29d248dbaf76b0ef606ef64
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-15 11:22:18 +0700

    feat(problem-2491/c): solve by sorting and then distribute skills
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2491/c/solution.c
+++ old-problems/2491/c/solution.c
@@ -1,3 +1,18 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 long long dividePlayers(int* skill, int skillSize) {
-  return skill[skillSize - 1];
+  qsort(skill, skillSize, sizeof(int), compare);
+  const int teamSkill = skill[0] + skill[skillSize - 1];
+
+  long long chemistry = 0;
+  for (int l = 0, r = skillSize - 1; l < r; ++l, --r) {
+    if (skill[l] + skill[r] != teamSkill) return -1;
+    chemistry += skill[l] * skill[r];
+  }
+
+  return chemistry;
 }
