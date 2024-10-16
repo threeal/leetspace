ed396419ec1d177bed97b730470503ed91b0590f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-16 08:42:14 +0700

    feat(problem-1963/c): solve by using two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1963/c/solution.c
+++ old-problems/1963/c/solution.c
@@ -1,3 +1,40 @@
+#include <string.h>
+
 int minSwaps(char* s) {
-  return *s;
+  int sLen = strlen(s);
+
+  int swap = 0, lb = 0, rb = 0;
+  int l = 0, r = sLen - 1;
+  while (l < r) {
+    while (l < r) {
+      if (s[l] == '[') {
+        ++lb;
+      } else {
+        --lb;
+        if (lb < 0) {
+          lb += 2;
+          ++swap;
+          break;
+        }
+      }
+      ++l;
+    }
+
+    while (l < r) {
+      if (s[r] == ']') {
+        ++rb;
+      } else {
+        --rb;
+        if (rb < 0) {
+          rb -= 2;
+          break;
+        }
+      }
+      --r;
+    }
+
+    ++l, --r;
+  }
+
+  return swap;
 }
