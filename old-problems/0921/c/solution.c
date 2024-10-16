763d8db5fd4e07febbc7429816722db43374e19a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-16 19:04:27 +0700

    feat(problem-921/c): solve by tracking level
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0921/c/solution.c
+++ old-problems/0921/c/solution.c
@@ -1,3 +1,17 @@
 int minAddToMakeValid(char* s) {
-  return *s;
+  int added = 0;
+  int level = 0;
+  while (*s != 0) {
+    if (*s == '(') {
+      ++level;
+    } else {
+      if (level == 0) {
+        ++added;
+      } else {
+        --level;
+      }
+    }
+    ++s;
+  }
+  return level + added;
 }
