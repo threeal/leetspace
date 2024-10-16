4be3e026bfd3c87bf469f83e29ae15d4b68af41d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-16 18:48:24 +0700

    feat(problem-440/c): solve by checking the count of number with specific prefix
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0440/c/solution.c
+++ old-problems/0440/c/solution.c
@@ -1,3 +1,24 @@
 int findKthNumber(int n, int k) {
-  return n * k;
+  int num = 1;
+
+  --k;
+  while (k > 0) {
+    int count = 0;
+    long long curr = num, next = num + 1;
+    while (curr <= n && count <= k) {
+      count += (next < n + 1 ? next : n + 1) - curr;
+      curr *= 10;
+      next *= 10;
+    }
+
+    if (count <= k) {
+      ++num;
+      k -= count;
+    } else {
+      num *= 10;
+      --k;
+    }
+  }
+
+  return num;
 }
