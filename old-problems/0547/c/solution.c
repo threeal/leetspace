7428b6bf2a13e74ccc91c9be30ef50c48c08df39
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-21 16:28:29 +0700

    feat(problem-547/c): solve by merging graphs
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0547/c/solution.c
+++ old-problems/0547/c/solution.c
@@ -1,7 +1,40 @@
+#include <stdlib.h>
+
+int findRoot(int* roots, int i);
+
 int findCircleNum(
     int** isConnected, int isConnectedSize, int* isConnectedColSize) {
-  (void)isConnected;
-  (void)isConnectedSize;
   (void)isConnectedColSize;
-  return 0;
+
+  int distinctCount = isConnectedSize;
+
+  int* roots = malloc(isConnectedSize * sizeof(int));
+  for (int i = 0; i < isConnectedSize; ++i) roots[i] = i;
+
+  for (int i = 0; i < isConnectedSize; ++i) {
+    for (int j = i + 1; j < isConnectedSize; ++j) {
+      if (isConnected[i][j] == 0) continue;
+
+      int rootI = findRoot(roots, i);
+      int rootJ = findRoot(roots, j);
+      if (rootI != rootJ) {
+        if (rootI > rootJ) {
+          const int temp = rootI;
+          rootI = rootJ;
+          rootJ = temp;
+        }
+        roots[rootI] = rootJ;
+        --distinctCount;
+      }
+    }
+  }
+
+  free(roots);
+
+  return distinctCount;
+}
+
+int findRoot(int* roots, int i) {
+  if (roots[i] != i) roots[i] = findRoot(roots, roots[i]);
+  return roots[i];
 }
