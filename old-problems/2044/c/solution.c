f844b2646b6d85e990c62e3903a4b84e1252b8e5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-23 20:03:43 +0700

    feat(problem-2044/c): solve by accumulating bit or and enumerate
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2044/c/solution.c
+++ old-problems/2044/c/solution.c
@@ -1,3 +1,14 @@
+static int countSubsets(int* nums, int numsSize, int i, int max, int prev);
+
 int countMaxOrSubsets(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int max = 0;
+  for (int i = 0; i < numsSize; ++i) max |= nums[i];
+  return countSubsets(nums, numsSize, 0, max, 0);
+}
+
+static int countSubsets(int* nums, int numsSize, int i, int max, int prev) {
+  if (i == numsSize) return 0;
+  return ((prev | nums[i]) == max ? 1 : 0) +
+      countSubsets(nums, numsSize, i + 1, max, prev | nums[i]) +
+      countSubsets(nums, numsSize, i + 1, max, prev);
 }
