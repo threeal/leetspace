3cd00b531f8ced0a73aefeb14e794bcc9360440f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-24 08:45:16 +0700

    feat(problem-951/cpp): solve by recursively comparing nodes
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0951/solution.cpp
+++ old-problems/0951/solution.cpp
@@ -1,6 +1,9 @@
 class Solution {
  public:
-  bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-    return root1 == root2;
+  bool flipEquiv(TreeNode* a, TreeNode* b) {
+    if (a == nullptr || b == nullptr) return a == b;
+    if (a->val != b->val) return false;
+    return (flipEquiv(a->left, b->left) && flipEquiv(a->right, b->right)) ||
+        (flipEquiv(a->left, b->right) && flipEquiv(a->right, b->left));
   }
 };
