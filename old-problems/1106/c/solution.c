1ab2ab990971f66ffaf919209e9ce49fd7dc0a93
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-27 22:16:02 +0700

    feat(problem-1106/c): solve by recursively parse string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1106/c/solution.c
+++ old-problems/1106/c/solution.c
@@ -1,5 +1,47 @@
 #include <stdbool.h>
 
+static bool parse(char** c);
+
 bool parseBoolExpr(char* expression) {
-  return expression == 0;
+  char* c = expression;
+  return parse(&c);
+}
+
+static bool parse(char** c) {
+  if (**c == 't') {
+    ++(*c);
+    return true;
+  }
+
+  if (**c == 'f') {
+    ++(*c);
+    return false;
+  }
+
+  if (**c == '!') {
+    *c += 2;
+    const bool b = !parse(c);
+    ++(*c);
+    return b;
+  }
+
+  if (**c == '&') {
+    bool b = true;
+    ++(*c);
+    while (**c != ')') {
+      ++(*c);
+      b &= parse(c);
+    }
+    ++(*c);
+    return b;
+  }
+
+  bool b = false;
+  ++(*c);
+  while (**c != ')') {
+    ++(*c);
+    b |= parse(c);
+  }
+  ++(*c);
+  return b;
 }
