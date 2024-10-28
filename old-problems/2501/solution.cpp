2a60227b05efa46074f68a833437ca0c25f8ebbd
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-28 09:51:47 +0700

    feat(problem-2501/cpp): solve by using sort and hash map
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2501/solution.cpp
+++ old-problems/2501/solution.cpp
@@ -1,8 +1,27 @@
+#include <algorithm>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int longestSquareStreak(std::vector<int>& nums) {
-    return nums.size();
+    std::sort(nums.begin(), nums.end(), std::greater<int>());
+
+    int maxSquare{-1};
+    std::unordered_map<int, int> squares{};
+    for (const auto num : nums) {
+      if (num <= 316) {
+        const auto it = squares.find(num * num);
+        if (it != squares.end()) {
+          squares.emplace(num, 1 + it->second);
+          if (1 + it->second > maxSquare) maxSquare = 1 + it->second;
+          continue;
+        }
+      }
+
+      squares.emplace(num, 1);
+    }
+
+    return maxSquare;
   }
 };
