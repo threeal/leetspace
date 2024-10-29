ef9710c0a02ffb9fa8de8137cad0ab704edfeaa6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-29 12:22:59 +0700

    feat(problem-2684/cpp): solve by using a dynamic programming approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2684/solution.cpp
+++ old-problems/2684/solution.cpp
@@ -1,8 +1,47 @@
+#include <algorithm>
+#include <utility>
 #include <vector>
 
 class Solution {
  public:
   int maxMoves(std::vector<std::vector<int>>& grid) {
-    return grid.size();
+    std::vector<int> moves(grid.size(), 0);
+    std::vector<int> nextMoves(grid.size());
+
+    for (int i = grid.back().size() - 2; i >= 0; --i) {
+      std::swap(moves, nextMoves);
+
+      int j = moves.size() - 1;
+      moves[j] = 0;
+      if (grid[j][i] < grid[j - 1][i + 1] && moves[j] < 1 + nextMoves[j - 1]) {
+        moves[j] = 1 + nextMoves[j - 1];
+      }
+      if (grid[j][i] < grid[j][i + 1] && moves[j] < 1 + nextMoves[j]) {
+        moves[j] = 1 + nextMoves[j];
+      }
+
+      for (--j; j > 0; --j) {
+        moves[j] = 0;
+        if (grid[j][i] < grid[j - 1][i + 1] && moves[j] < 1 + nextMoves[j - 1]) {
+          moves[j] = 1 + nextMoves[j - 1];
+        }
+        if (grid[j][i] < grid[j][i + 1] && moves[j] < 1 + nextMoves[j]) {
+          moves[j] = 1 + nextMoves[j];
+        }
+        if (grid[j][i] < grid[j + 1][i + 1] && moves[j] < 1 + nextMoves[j + 1]) {
+          moves[j] = 1 + nextMoves[j + 1];
+        }
+      }
+
+      moves[0] = 0;
+      if (grid[0][i] < grid[0][i + 1] && moves[0] < 1 + nextMoves[0]) {
+        moves[0] = 1 + nextMoves[0];
+      }
+      if (grid[0][i] < grid[1][i + 1] && moves[0] < 1 + nextMoves[1]) {
+        moves[0] = 1 + nextMoves[1];
+      }
+    }
+
+    return *std::max_element(moves.begin(), moves.end());
   }
 };
