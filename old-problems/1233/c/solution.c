a6ef14e98895601daac2aaca352a54f301290cbd
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-30 20:28:57 +0700

    feat(problem-1233/c): solve by sorting the folder first
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1233/c/solution.c
+++ old-problems/1233/c/solution.c
@@ -1,4 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+
+int compare(const void* a, const void* b) {
+  return strcmp(*(char**)a, *(char**)b);
+}
+
 char** removeSubfolders(char** folder, int folderSize, int* returnSize) {
-  *returnSize = folderSize;
-  return folder;
+  qsort(folder, folderSize, sizeof(char*), compare);
+
+  char** output = malloc(sizeof(char*) * folderSize);
+
+  int n = -1;
+  char* prev = output[++n] = folder[0];
+  int prevSize = strlen(prev);
+
+  for (int i = 1; i < folderSize; ++i) {
+    int folderSize = strlen(folder[i]);
+    if (prevSize < folderSize && folder[i][prevSize] == '/') {
+      char temp = folder[i][prevSize];
+      folder[i][prevSize] = 0;
+      if (strcmp(folder[i], prev) == 0) {
+        folder[i][prevSize] = temp;
+        continue;
+      }
+      folder[i][prevSize] = temp;
+    }
+
+    prev = output[++n] = folder[i];
+    prevSize = folderSize;
+  }
+
+  *returnSize = n + 1;
+  return output;
 }
