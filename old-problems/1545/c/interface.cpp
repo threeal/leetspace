8584cb51b758789c7fe9ef7798f773a471b63bab
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-30 20:58:59 +0700

    feat: solve problem 1545. Find Kth Bit in Nth Binary String in C (#2105)
    * test: add test for problem 1545. Find Kth Bit in Nth Binary String
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(problem-1545/c): solve by calling function recursively
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1545/c/interface.cpp
@@ -0,0 +1,7 @@
+extern "C" {
+char findKthBit(int n, int k);
+}
+
+char solution_c(int n, int k) {
+  return findKthBit(n, k);
+}
---
+++ old-problems/1545/c/solution.c
@@ -0,0 +1,8 @@
+char findKthBit(int n, int k) {
+  if (n == 1) return '0';
+  const int mid = 1 << (n - 1);
+  if (k == mid) return '1';
+  return k < mid
+      ? findKthBit(n - 1, k)
+      : (findKthBit(n - 1, mid * 2 - k) == '0' ? '1' : '0');
+}
