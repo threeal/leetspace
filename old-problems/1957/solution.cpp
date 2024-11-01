e300040ad7305f82f8e2f2e06240130cca31366b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-01 09:35:25 +0700

    feat(problem-1957/cpp): solve by iterating given string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1957/solution.cpp
+++ old-problems/1957/solution.cpp
@@ -3,6 +3,22 @@
 class Solution {
  public:
   std::string makeFancyString(std::string s) {
-    return s;
+    std::string out{};
+    out.reserve(s.size());
+
+    char prev{0};
+    bool twice{false};
+    for (const auto c : s) {
+      if (c != prev) {
+        prev = c;
+        twice = false;
+      } else {
+        if (twice) continue;
+        twice = true;
+      }
+      out += c;
+    }
+
+    return out;
   }
 };
