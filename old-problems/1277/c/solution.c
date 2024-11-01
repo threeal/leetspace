0e3132b487472baae712698a39e1cb311d1b6db9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-01 19:08:26 +0700

    feat(problem-1277/c): solve by using a dynamic programming approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1277/c/solution.c
+++ old-problems/1277/c/solution.c
@@ -1,5 +1,18 @@
 int countSquares(int** matrix, int matrixSize, int* matrixColSize) {
-  (void)matrix;
-  (void)matrixColSize;
-  return matrixSize;
+  int count = 0;
+  for (int j = 0; j < matrixColSize[0]; ++j) count += matrix[0][j];
+
+  for (int i = 1; i < matrixSize; ++i) {
+    count += matrix[i][0];
+    for (int j = 1; j < matrixColSize[i]; ++j) {
+      if (matrix[i][j] == 0) continue;
+      int val = matrix[i - 1][j - 1];
+      if (matrix[i][j - 1] < val) val = matrix[i][j - 1];
+      if (matrix[i - 1][j] < val) val = matrix[i - 1][j];
+      matrix[i][j] += val;
+      count += matrix[i][j];
+    }
+  }
+
+  return count;
 }
