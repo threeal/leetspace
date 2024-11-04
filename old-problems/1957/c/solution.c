fadcb16fc81efafc84ad758aaff21f0a765e31e0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-04 19:26:41 +0700

    feat(problem-1957/c): solve by iterating given string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1957/c/solution.c
+++ old-problems/1957/c/solution.c
@@ -1,3 +1,22 @@
+#include <stdbool.h>
+
 char* makeFancyString(char* s) {
+  int out = 0;
+  char prev = 0;
+  bool twice = false;
+
+  for (char* c = s; *c != 0; ++c) {
+    if (*c != prev) {
+      prev = *c;
+      twice = false;
+    } else {
+      if (twice) continue;
+      twice = true;
+    }
+
+    s[out++] = *c;
+  }
+
+  s[out++] = 0;
   return s;
 }
