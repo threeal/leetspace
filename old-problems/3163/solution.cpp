dbe88e26081414ca236544ee7d0eb3aaaf7abcbf
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-04 10:50:12 +0700

    feat(problem-3163/cpp): solve by counting consecutive characters
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3163/solution.cpp
+++ old-problems/3163/solution.cpp
@@ -3,6 +3,28 @@
 class Solution {
  public:
   std::string compressedString(std::string word) {
-    return word;
+    std::string out{};
+
+    std::size_t i{0};
+    while (i < word.size()) {
+      const char c = word[i];
+
+      std::size_t n{1};
+      while (i + n < word.size() && word[i + n] == c) ++n;
+      i += n;
+
+      while (n > 0) {
+        if (n > 9) {
+          out += '9';
+          n -= 9;
+        } else {
+          out += '0' + n;
+          n = 0;
+        }
+        out += c;
+      }
+    }
+
+    return out;
   }
 };
