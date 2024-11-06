6c779d8be5f6fc7aa6682d2974e46f1963a199fc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-06 10:07:26 +0700

    feat(problem-3011/cpp): solve by iterating given array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3011/solution.cpp
+++ old-problems/3011/solution.cpp
@@ -1,8 +1,23 @@
+#include <bit>
 #include <vector>
 
 class Solution {
  public:
   bool canSortArray(std::vector<int>& nums) {
-    return nums.empty();
+    int prevMax{0}, max{0}, bitCount{-1};
+    for (const auto num : nums) {
+      const auto count = std::popcount(static_cast<unsigned int>(num));
+      if (count == bitCount) {
+        if (num < prevMax) return false;
+        if (num > max) max = num;
+      } else {
+        if (num < max) return false;
+        bitCount = count;
+        prevMax = max;
+        max = num;
+      }
+    }
+
+    return true;
   }
 };
