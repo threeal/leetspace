8563021594ed3b741e4458d96e19b423a4da4cef
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-07 17:11:55 +0700

    feat(problem-2406/c): solve by sorting starts and ends
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2406/c/solution.c
+++ old-problems/2406/c/solution.c
@@ -1,3 +1,35 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int minGroups(int** intervals, int intervalsSize, int* intervalsColSize) {
-  return intervals[intervalsSize - 1][intervalsColSize[intervalsSize - 1]];
+  (void)intervalsColSize;
+
+  int* starts = malloc(intervalsSize * sizeof(int));
+  int* ends = malloc(intervalsSize * sizeof(int));
+
+  for (int i = 0; i < intervalsSize; ++i) {
+    starts[i] = intervals[i][0];
+    ends[i] = intervals[i][1];
+  }
+
+  qsort(starts, intervalsSize, sizeof(int), compare);
+  qsort(ends, intervalsSize, sizeof(int), compare);
+
+  int group = 0;
+  int* end = ends;
+  for (int i = 0; i < intervalsSize; ++i) {
+    if (starts[i] > *end) {
+      ++end;
+    } else {
+      ++group;
+    }
+  }
+
+  free(starts);
+  free(ends);
+
+  return group;
 }
