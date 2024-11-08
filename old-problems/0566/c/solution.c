62746dd6c0402d159138316fccb6a5e1173c5ecf
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-08 21:06:59 +0700

    test(problem-566): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0566/c/solution.c
@@ -0,0 +1,12 @@
+int** matrixReshape(
+    int** mat, int matSize, int* matColSize, int r, int c,
+    int* returnSize, int** returnColumnSizes) {
+  (void)mat;
+  (void)matSize;
+  (void)matColSize;
+  (void)r;
+  (void)c;
+  *returnSize = 0;
+  *returnColumnSizes = 0;
+  return 0;
+}
