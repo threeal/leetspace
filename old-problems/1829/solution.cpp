98477115be14f97653af7e4adecda963c896a171
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-08 12:28:27 +0700

    feat(problem-1829/cpp): solve by filling answer backward
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1829/solution.cpp
+++ old-problems/1829/solution.cpp
@@ -2,7 +2,16 @@
 
 class Solution {
  public:
-  std::vector<int> getMaximumXor(std::vector<int>& nums, int /*maximumBit*/) {
-    return nums;
+  std::vector<int> getMaximumXor(std::vector<int>& nums, int maximumBit) {
+    std::vector<int> output(nums.size());
+
+    int max = (1 << maximumBit) - 1;
+    int ans{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      ans ^= nums[i];
+      output[output.size() - i - 1] = max ^ ans;
+    }
+
+    return output;
   }
 };
