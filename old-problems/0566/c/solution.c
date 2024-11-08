d459262af923ed2952412f2d3abbda0f8c62041e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-08 21:13:05 +0700

    feat(problem-566/c): solve by filling new shape with iterators
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0566/c/solution.c
+++ old-problems/0566/c/solution.c
@@ -1,12 +1,32 @@
+#include <stdlib.h>
+
 int** matrixReshape(
     int** mat, int matSize, int* matColSize, int r, int c,
     int* returnSize, int** returnColumnSizes) {
-  (void)mat;
-  (void)matSize;
-  (void)matColSize;
-  (void)r;
-  (void)c;
-  *returnSize = 0;
-  *returnColumnSizes = 0;
-  return 0;
+  const int matR = matSize;
+  const int matC = matColSize[0];
+  if (matR * matC != r * c) {
+    *returnSize = matSize;
+    *returnColumnSizes = matColSize;
+    return mat;
+  }
+
+  int* data = malloc(r * c * sizeof(int));
+
+  int** out = malloc(r * sizeof(int**));
+  *returnSize = r;
+  *returnColumnSizes = malloc(r * sizeof(int));
+  for (int i = 0; i < r; ++i) {
+    out[i] = data + i * c;
+    (*returnColumnSizes)[i] = c;
+  }
+
+  int matY = 0, matX = 0, y = 0, x = 0;
+  while (y < r) {
+    out[y][x] = mat[matY][matX];
+    if (++x >= c) ++y, x = 0;
+    if (++matX >= matC) ++matY, matX = 0;
+  }
+
+  return out;
 }
