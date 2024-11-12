d26e64176af9e04244ebabdaa225a92ef7f2e872
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-12 10:41:19 +0700

    feat(problem-386/c): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0386/c/solution.c
+++ old-problems/0386/c/solution.c
@@ -1,5 +1,27 @@
+#include <stdlib.h>
+
+static void pushLexicalOrder(int** it, int num, int n);
+
 int* lexicalOrder(int n, int* returnSize) {
-  (void)n;
-  *returnSize = 0;
-  return 0;
+  int* out = malloc(n * sizeof(int));
+  *returnSize = n;
+
+  int* it = out;
+  const int end = n < 9 ? n : 9;
+  for (int i = 1; i <= end; ++i) {
+    pushLexicalOrder(&it, i, n);
+  }
+
+  return out;
+}
+
+static void pushLexicalOrder(int** it, int num, int n) {
+  **it = num;
+  ++(*it);
+  if (num * 10 <= n) {
+    const int end = n < num * 10 + 9 ? n : num * 10 + 9;
+    for (int i = num * 10; i <= end; ++i) {
+      pushLexicalOrder(it, i, n);
+    }
+  }
 }
