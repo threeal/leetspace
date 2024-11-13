4f97b8cceed485987582c6a8d9c239d0b406208e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-13 10:56:43 +0700

    feat(problem-2563/cpp): solve by using sorting and binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2563/solution.cpp
+++ old-problems/2563/solution.cpp
@@ -1,9 +1,22 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
-  long long countFairPairs(
-      std::vector<int>& nums, int /*lower*/, int /*upper*/) {
-    return nums.size();
+  long long countFairPairs(std::vector<int>& nums, int lower, int upper) {
+    std::sort(nums.begin(), nums.end());
+
+    long long count{0};
+    for (auto it = nums.begin(); it != nums.end(); ++it) {
+      const auto left = std::lower_bound(
+          std::next(it), nums.end(), lower - *it);
+
+      if (left != nums.end()) {
+        const auto right = std::upper_bound(left, nums.end(), upper - *it);
+        count += std::distance(left, right);
+      }
+    }
+
+    return count;
   }
 };
