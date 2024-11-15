880d40e528c46b3bcb3f43e87e618fbfeada0562
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-15 14:25:13 +0700

    feat(problem-1829/cpp): solve by filling answer backward
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1829/c/solution.c
+++ old-problems/1829/c/solution.c
@@ -1,5 +1,15 @@
+#include <stdlib.h>
+
 int* getMaximumXor(int* nums, int numsSize, int maximumBit, int* returnSize) {
-  (void)maximumBit;
+  int* output = malloc(numsSize * sizeof(int));
   *returnSize = numsSize;
-  return nums;
+
+  int max = (1 << maximumBit) - 1;
+  int ans = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    ans ^= nums[i];
+    output[numsSize - i - 1] = max ^ ans;
+  }
+
+  return output;
 }
