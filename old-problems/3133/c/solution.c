56d9b34ec1d269072ff6c38ca77ffc58e59eb70a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-16 22:25:35 +0700

    feat(problem-3133/c): solve by using bit manipulation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3133/c/solution.c
+++ old-problems/3133/c/solution.c
@@ -1,3 +1,13 @@
 long long minEnd(int n, int x) {
-  return n * x;
+  long long res = 0, mask = 1, nn = n - 1, xx = x;
+  while (mask <= nn || mask <= xx) {
+    if ((xx & mask) != 0) {
+      nn = nn << 1;
+      res |= mask;
+    } else if ((nn & mask) != 0) {
+      res |= mask;
+    }
+    mask = mask << 1;
+  }
+  return res;
 }
