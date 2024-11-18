1b78906b3cc6464105d380fea28a5ab548405957
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-18 22:04:25 +0700

    feat(problem-3097/c): solve by counting bits and using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3097/c/solution.c
+++ old-problems/3097/c/solution.c
@@ -1,3 +1,32 @@
 int minimumSubarrayLength(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] * k;
+  int l = 0, r = 0, n = numsSize + 1;
+
+  int ans = nums[0];
+  int bitCounts[33] = {0};
+  for (int i = 0; (1 << i) <= nums[0]; ++i) {
+    if ((1 << i & nums[0]) == 0) continue;
+    ++bitCounts[i];
+  }
+
+  while (1) {
+    if (ans >= k) {
+      if (r - l + 1 < n) n = r - l + 1;
+      if (l < r) {
+        for (int i = 0; (1 << i) <= nums[l]; ++i) {
+          if ((1 << i & nums[l]) == 0) continue;
+          if (--bitCounts[i] == 0) ans = ans & ~(1 << i);
+        }
+        ++l;
+        continue;
+      }
+    }
+
+    if (++r >= numsSize) break;
+    for (int i = 0; (1 << i) <= nums[r]; ++i) {
+      if ((1 << i & nums[r]) == 0) continue;
+      if (++bitCounts[i] == 1) ans = ans | (1 << i);
+    }
+  }
+
+  return n <= numsSize ? n : -1;
 }
