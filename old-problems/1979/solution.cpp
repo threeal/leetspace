032fb1ecab1cb48291d69d54c01d8865dfee4204
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-19 21:45:42 +0700

    feat(problem-1979/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1979/solution.cpp
+++ old-problems/1979/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   int findGCD(std::vector<int>& nums) {
-    return nums.front();
+    int smallest = nums[0];
+    int largest = nums[0];
+    for (std::size_t i = nums.size() - 1; i > 0; --i) {
+      if (smallest > nums[i]) smallest = nums[i];
+      if (largest < nums[i]) largest = nums[i];
+    }
+
+    for (int i = smallest; i > 0; --i) {
+      if (smallest % i == 0 && largest % i == 0) return i;
+    }
+
+    return 1;
   }
 };
