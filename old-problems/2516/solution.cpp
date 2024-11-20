0c511f17517b78ab9f3c49e32844506f16194bbc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-20 11:04:36 +0700

    feat(problem-2516/cpp): solve by using two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2516/solution.cpp
+++ old-problems/2516/solution.cpp
@@ -3,6 +3,74 @@
 class Solution {
  public:
   int takeCharacters(std::string s, int k) {
-    return s.size() * k;
+    std::size_t r{s.size()};
+    int a{0}, b{0}, c{0};
+    while ((a < k || b < k || c < k) && r > 0) {
+      --r;
+      switch (s[r]) {
+        case 'a':
+          ++a;
+          break;
+        case 'b':
+          ++b;
+          break;
+        case 'c':
+          ++c;
+          break;
+      }
+    }
+
+    if (a < k || b < k || c < k) return -1;
+
+    int minTime = s.size() - r;
+    for (std::size_t l{0}; l < s.size(); ++l) {
+      switch (s[l]) {
+        case 'a':
+          ++a;
+          break;
+        case 'b':
+          ++b;
+          break;
+        case 'c':
+          ++c;
+          break;
+      }
+
+    repeat:
+      if (r < s.size()) {
+        switch (s[r]) {
+          case 'a':
+            if (a > k) {
+              --a;
+              ++r;
+              goto repeat;
+            }
+            break;
+
+          case 'b':
+            if (b > k) {
+              --b;
+              ++r;
+              goto repeat;
+            }
+            break;
+
+          case 'c':
+            if (c > k) {
+              --c;
+              ++r;
+              goto repeat;
+            }
+            break;
+        }
+      }
+
+      if (l >= r) break;
+
+      const int time = 1 + l + s.size() - r;
+      if (time < minTime) minTime = time;
+    }
+
+    return minTime;
   }
 };
