5de64352849dfdff0783b413742ed6c7ad39b420
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-23 14:39:44 +0700

    feat(problem-1460/c): solve by sorting and comparing arrays
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1460/c/solution.c
+++ old-problems/1460/c/solution.c
@@ -1,5 +1,15 @@
 #include <stdbool.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
 
 bool canBeEqual(int* target, int targetSize, int* arr, int arrSize) {
-  return target[targetSize - 1] == arr[arrSize - 1];
+  qsort(target, targetSize, sizeof(int), compare);
+  qsort(arr, arrSize, sizeof(int), compare);
+  for (int i = 0; i < arrSize; ++i) {
+    if (target[i] != arr[i]) return false;
+  }
+  return true;
 }
