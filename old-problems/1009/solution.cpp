7acb665ae6a801fedeb9facc14182f50c3b14d62
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-24 22:22:37 +0700

    feat(problem-1009/cpp): solve by using `std::bit_ceil`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1009/solution.cpp
+++ old-problems/1009/solution.cpp
@@ -1,6 +1,9 @@
+#include <bit>
+
 class Solution {
  public:
   int bitwiseComplement(int n) {
-    return n;
+    if (n == 0) return 1;
+    return ~n & (std::bit_ceil<unsigned int>(n) - 1);
   }
 };
