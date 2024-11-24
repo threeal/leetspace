8895c1592ee15c3106dedd9c6ae5821dce4c0c60
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-24 13:15:30 +0700

    feat(problem-1975/cpp): solve by sum absolute values
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1975/solution.cpp
+++ old-problems/1975/solution.cpp
@@ -3,6 +3,24 @@
 class Solution {
  public:
   long long maxMatrixSum(std::vector<std::vector<int>>& matrix) {
-    return matrix.front().front();
+    long long sum{0};
+    int negCount{0}, minVal{100000};
+    bool hasZero{false};
+    for (const auto& row : matrix) {
+      for (const auto val : row) {
+        if (val == 0) {
+          hasZero = true;
+        } else if (val < 0) {
+          ++negCount;
+          sum += -val;
+          if (-val < minVal) minVal = -val;
+        } else {
+          sum += val;
+          if (val < minVal) minVal = val;
+        }
+      }
+    }
+    if (!hasZero && negCount % 2 == 1) sum -= 2 * minVal;
+    return sum;
   }
 };
