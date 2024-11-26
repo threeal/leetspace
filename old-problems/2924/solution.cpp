69b9afb0107cc21f333836a6718618e16fae3d20
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-26 13:44:50 +0700

    feat(problem-2924/cpp): solve by checking node not mentioned in defeated edge
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2924/solution.cpp
+++ old-problems/2924/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   int findChampion(int n, std::vector<std::vector<int>>& edges) {
-    return n * edges.size();
+    std::vector<bool> isChild(n, false);
+    for (const auto& edge : edges) {
+      isChild[edge[1]] = true;
+    }
+
+    int parent{-1};
+    for (int i{0}; i < n; ++i) {
+      if (!isChild[i]) {
+        if (parent >= 0) return -1;
+        parent = i;
+      }
+    }
+
+    return parent;
   }
 };
