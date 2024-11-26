90e007178fbf001c8a24ef7ee21b67b03324779f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-26 23:03:08 +0700

    test(problem-2257): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2257/c/solution.c
@@ -0,0 +1,13 @@
+int countUnguarded(
+    int m, int n, int** guards, int guardsSize, int* guardsColSize,
+    int** walls, int wallsSize, int* wallsColSize) {
+  (void)m;
+  (void)n;
+  (void)guards;
+  (void)guardsSize;
+  (void)guardsColSize;
+  (void)walls;
+  (void)wallsSize;
+  (void)wallsColSize;
+  return 0;
+}
