7fda1cfc6c06f48ea370187b30054f842a3dcb27
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-01 14:05:23 +0700

    feat(problem-1346/cpp): solve by keeping track of checked numbers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1346/solution.cpp
+++ old-problems/1346/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   bool checkIfExist(std::vector<int>& arr) {
-    return arr.empty();
+    std::vector<bool> exists(2001, false);
+    for (const auto num : arr) {
+      if (num >= -500 && num <= 500 && exists[1000 + num * 2]) {
+        return true;
+      } else if (num % 2 == 0 && exists[1000 + num / 2]) {
+        return true;
+      }
+      exists[1000 + num] = true;
+    }
+    return false;
   }
 };
