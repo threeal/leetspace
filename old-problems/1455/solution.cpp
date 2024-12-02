8abcc033b3d4b61e414bc55fc124bf8d26fd0853
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-02 16:02:36 +0700

    feat(problem-1455/cpp): solve by iterating through the chars in string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1455/solution.cpp
+++ old-problems/1455/solution.cpp
@@ -3,6 +3,25 @@
 class Solution {
  public:
   int isPrefixOfWord(std::string sentence, std::string searchWord) {
-    return sentence.size() + searchWord.size();
+    int i{1};
+    char* ca{sentence.data()};
+
+    while (true) {
+      char* cb{searchWord.data()};
+      while (*cb != 0 && *ca == *cb) {
+        ++ca;
+        ++cb;
+      }
+
+      if (*cb == 0) return i;
+      while (*ca >= 'a') ++ca;
+
+      if (*ca == 0) break;
+
+      ++i;
+      ++ca;
+    }
+
+    return -1;
   }
 };
