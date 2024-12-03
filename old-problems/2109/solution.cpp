8023b60d3763b5d576df611eae59ae978dce1b0c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-03 12:34:46 +0700

    feat(problem-2109/cpp): solve by resizing string and shifting characters
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2109/solution.cpp
+++ old-problems/2109/solution.cpp
@@ -4,7 +4,23 @@
 class Solution {
  public:
   std::string addSpaces(std::string s, std::vector<int>& spaces) {
+    int l = s.size() - 1;
+    int i = spaces.size() - 1;
+
     s.resize(s.size() + spaces.size(), ' ');
+    int r = s.size() - 1;
+
+    while (l >= 0) {
+      if (l == spaces[i] - 1) {
+        if (--i < 0) break;
+      } else {
+        s[r] = s[l];
+        s[l] = ' ';
+        --l;
+      }
+      --r;
+    }
+
     return s;
   }
 };
