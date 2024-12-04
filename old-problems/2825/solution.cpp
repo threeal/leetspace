46168b0ed950dbb45690bcc049f7a31227cd36bb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-04 12:08:25 +0700

    feat(problem-2825/cpp): solve by comparing characters in both string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2825/solution.cpp
+++ old-problems/2825/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   bool canMakeSubsequence(std::string str1, std::string str2) {
-    return str1 == str2;
+    char* cc = str2.data();
+    for (const auto c : str1) {
+      if (c == *cc || (c + 1 - 'a') % 26 == *cc - 'a') {
+        if (*(++cc) == 0) return true;
+      }
+    }
+    return false;
   }
 };
