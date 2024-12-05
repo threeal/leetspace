0105c0e546028082f7182d995b622d42668231ea
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-05 14:58:57 +0700

    feat(problem-2337/cpp): solve by iterating given string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2337/solution.cpp
+++ old-problems/2337/solution.cpp
@@ -3,6 +3,26 @@
 class Solution {
  public:
   bool canChange(std::string start, std::string target) {
-    return start == target;
+    int l{0}, r{0};
+    for (std::size_t i{0}; i < start.size(); ++i) {
+      if (start[i] == 'R') {
+        if (l > 0) return false;
+        ++r;
+      }
+      if (target[i] == 'L') {
+        if (r > 0) return false;
+        ++l;
+      }
+      if (start[i] == 'L') {
+        if (l == 0) return false;
+        --l;
+      }
+      if (target[i] == 'R') {
+        if (r == 0) return false;
+        --r;
+      }
+    }
+
+    return l == 0 && r == 0;
   }
 };
