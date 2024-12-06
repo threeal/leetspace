a3f8bfdd7065f1c26054dc8098e2a121125fd487
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-06 13:33:49 +0700

    feat(problem-2554/cpp): solve by sorting banned nums and distribute smallest nums
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2554/solution.cpp
+++ old-problems/2554/solution.cpp
@@ -1,8 +1,23 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maxCount(std::vector<int>& banned, int n, int maxSum) {
-    return banned.size() + n + maxSum;
+    std::sort(banned.begin(), banned.end());
+    auto it = banned.begin();
+
+    int count{0};
+    for (int i = 1; i <= n && i <= maxSum; ++i) {
+      if (it != banned.end() && *it == i) {
+        ++it;
+        continue;
+      }
+
+      maxSum -= i;
+      ++count;
+    }
+
+    return count;
   }
 };
