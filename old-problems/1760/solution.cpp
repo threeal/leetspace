b06c65e20e40efe104edd50970895682436aca8c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-07 11:20:07 +0700

    feat(problem-1760/cpp): solve by using binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1760/solution.cpp
+++ old-problems/1760/solution.cpp
@@ -1,8 +1,26 @@
+#include <algorithm>
+#include <queue>
 #include <vector>
 
 class Solution {
  public:
   int minimumSize(std::vector<int>& nums, int maxOperations) {
-    return nums.front() / maxOperations;
+    int min{1};
+    int max = *std::max_element(nums.begin(), nums.end());
+    while (min < max) {
+      int mid = min + (max - min) / 2;
+
+      int operations{0};
+      for (const auto num : nums) {
+        operations += (num - 1) / mid;
+      }
+
+      if (operations > maxOperations) {
+        min = mid + 1;
+      } else {
+        max = mid;
+      }
+    }
+    return min;
   }
 };
