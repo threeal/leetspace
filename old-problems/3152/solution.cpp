a72d3472e20fb812bf29fd92436403fddd1f712a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-09 11:45:40 +0700

    feat(problem-3152/cpp): solve by grouping numbers based on parity
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3152/solution.cpp
+++ old-problems/3152/solution.cpp
@@ -4,6 +4,21 @@ class Solution {
  public:
   std::vector<bool> isArraySpecial(
       std::vector<int>& nums, std::vector<std::vector<int>>& queries) {
-    return std::vector(queries.size(), nums.empty());
+    int group{0};
+    int mod{nums[0] % 2};
+    nums[0] = group;
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] % 2 == mod) ++group;
+      mod = nums[i] % 2;
+      nums[i] = group;
+    }
+
+    std::vector<bool> outputs{};
+    outputs.reserve(queries.size());
+    for (const auto& query : queries) {
+      outputs.push_back(nums[query[0]] == nums[query[1]]);
+    }
+
+    return outputs;
   }
 };
