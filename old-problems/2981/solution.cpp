020e31b8c95d6d4453e529859caef46b116e12dc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-10 11:16:24 +0700

    perf(problem-2981/cpp): replace priority queue with tracking 3 largest substring
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2981/solution.cpp
+++ old-problems/2981/solution.cpp
@@ -1,43 +1,52 @@
-#include <queue>
 #include <string>
 #include <unordered_map>
+
+struct Specials {
+  int a;
+  int b;
+  int c;
+};
 
 class Solution {
  public:
   int maximumLength(std::string s) {
-    std::unordered_map<char, std::priority_queue<int>> charSpecials{};
+    std::unordered_map<char, Specials> specials{};
 
-    std::size_t prev{0};
-    for (std::size_t i{1}; i < s.size(); ++i) {
+    int prev{0};
+    for (int i{1}; i <= static_cast<int>(s.size()); ++i) {
       if (s[i] != s[i - 1]) {
-        charSpecials[s[i - 1]].push(i - prev);
+        auto it = specials.find(s[i - 1]);
+        if (it == specials.end()) {
+          specials.emplace(s[i - 1], Specials{.a = i - prev, .b = 0, .c = 0});
+        } else {
+          if (i - prev > it->second.a) {
+            it->second.c = it->second.b;
+            it->second.b = it->second.a;
+            it->second.a = i - prev;
+          } else if (i - prev > it->second.b) {
+            it->second.c = it->second.b;
+            it->second.b = i - prev;
+          } else if (i - prev > it->second.c) {
+            it->second.c = i - prev;
+          }
+        }
         prev = i;
       }
     }
-    charSpecials[s.back()].push(s.size() - prev);
 
     int max{0};
-    for (auto& [c, specials] : charSpecials) {
-      const auto a = specials.top();
-      specials.pop();
+    for (const auto& [c, s] : specials) {
+      if (s.a - 2 > max) max = s.a - 2;
 
-      if (a - 2 > max) max = a - 2;
-
-      if (!specials.empty()) {
-        const auto b = specials.top();
-        specials.pop();
-
-        if (a == b) {
-          if (a - 1 > max) max = a - 1;
+      if (s.b > 0) {
+        if (s.a == s.b) {
+          if (s.a - 1 > max) max = s.a - 1;
         } else {
-          if (b > max) max = b;
+          if (s.b > max) max = s.b;
         }
 
-        if (!specials.empty()) {
-          const auto c = specials.top();
-          specials.pop();
-
-          if (c > max) max = c;
+        if (s.c > 0) {
+          if (s.c > max) max = s.c;
         }
       }
     }
