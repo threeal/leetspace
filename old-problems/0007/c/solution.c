6b0a9d7f08c1e06ca23b69614cb4eecb9a9835ab
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-10 22:30:10 +0700

    feat(problem-7/c): solve by using while loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0007/c/solution.c
+++ old-problems/0007/c/solution.c
@@ -1,3 +1,25 @@
+#include <stdbool.h>
+
 int reverse(int x) {
-  return x;
+  if (x == -2147483648) return 0;
+
+  int out = 0;
+
+  bool isNeg = false;
+  if (x < 0) {
+    isNeg = true;
+    x = -x;
+  }
+
+  while (x > 0) {
+    if (out >= 214748364) {
+      if (out > 214748364) return 0;
+      if (x % 10 > 7) return 0;
+    }
+
+    out = out * 10 + x % 10;
+    x /= 10;
+  }
+
+  return isNeg ? -out : out;
 }
