1406b44337bf199cac87fbea50ea5ae235b9ca79
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-13 22:39:02 +0700

    test: add test for problem 2070. Most Beautiful Item for Each Query
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2070/solution.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> maximumBeauty(
+      std::vector<std::vector<int>>& /*items*/, std::vector<int>& queries) {
+    return queries;
+  }
+};
