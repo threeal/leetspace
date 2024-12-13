6c014ff9e5345265d872cd1e6cb836b5fb86c72e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-13 18:31:11 +0700

    feat(problem-2593/cpp): solve by using priority queue with custom comparator
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2593/solution.cpp
+++ old-problems/2593/solution.cpp
@@ -1,8 +1,32 @@
+#include <queue>
 #include <vector>
 
 class Solution {
  public:
   long long findScore(std::vector<int>& nums) {
-    return nums.size();
+    const auto compare = [&](std::size_t a, std::size_t b) {
+      return nums[a] == nums[b] ? a > b : nums[a] > nums[b];
+    };
+
+    std::priority_queue<
+        std::size_t, std::vector<std::size_t>, decltype(compare)>
+        pq(compare);
+
+    for (std::size_t i{0}; i < nums.size(); ++i) pq.push(i);
+
+    std::vector<bool> marked(nums.size(), false);
+
+    long long score{0};
+    while (!pq.empty()) {
+      if (!marked[pq.top()]) {
+        score += nums[pq.top()];
+        marked[pq.top()] = true;
+        if (pq.top() > 0) marked[pq.top() - 1] = true;
+        if (pq.top() + 1 < marked.size()) marked[pq.top() + 1] = true;
+      }
+      pq.pop();
+    }
+
+    return score;
   }
 };
