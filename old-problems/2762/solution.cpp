bfc07391d11eb28825f2046e595e4274e468a540
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-14 11:54:54 +0700

    perf(problem-2762/cpp): use double ended queue to maintain min and max
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2762/solution.cpp
+++ old-problems/2762/solution.cpp
@@ -1,29 +1,26 @@
-#include <cstdlib>
-#include <set>
+#include <deque>
 #include <vector>
 
 class Solution {
  public:
   long long continuousSubarrays(std::vector<int>& nums) {
     long long count{0};
+    std::deque<int> mins{}, maxs{};
+    for (std::size_t l{0}, r{0}; r < nums.size(); ++r) {
+      while (!mins.empty() && nums[r] < mins.back()) mins.pop_back();
+      mins.push_back(nums[r]);
 
-    std::multiset<int> vals{};
-    vals.insert(nums[0]);
+      while (!maxs.empty() && nums[r] > maxs.back()) maxs.pop_back();
+      maxs.push_back(nums[r]);
 
-    std::size_t l{0}, r{0};
-    while (r < nums.size()) {
-      if (std::abs(*vals.begin() - *vals.rbegin()) <= 2) {
-        count += r + 1 - l;
-        if (++r < nums.size()) {
-          vals.insert(nums[r]);
-        }
-      } else {
-        const auto it = vals.find(nums[l]);
-        vals.erase(it);
+      while (maxs.front() - mins.front() > 2) {
+        if (!mins.empty() && nums[l] == mins.front()) mins.pop_front();
+        if (!maxs.empty() && nums[l] == maxs.front()) maxs.pop_front();
         ++l;
       }
-    }
 
+      count += r + 1 - l;
+    }
     return count;
   }
 };
