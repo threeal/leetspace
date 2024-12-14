4440280e5c52e8b2416a8b098b204dcdd54533eb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-14 22:59:14 +0700

    feat(problem-1760/c): solve by using binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1760/c/solution.c
+++ old-problems/1760/c/solution.c
@@ -1,3 +1,25 @@
 int minimumSize(int* nums, int numsSize, int maxOperations) {
-  return nums[numsSize - 1] * maxOperations;
+  int min = 1;
+
+  int max = nums[0];
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i] > max) max = nums[i];
+  }
+
+  while (min < max) {
+    int mid = min + (max - min) / 2;
+
+    int operations = 0;
+    for (int i = 0; i < numsSize; ++i) {
+      operations += (nums[i] - 1) / mid;
+    }
+
+    if (operations > maxOperations) {
+      min = mid + 1;
+    } else {
+      max = mid;
+    }
+  }
+
+  return min;
 }
