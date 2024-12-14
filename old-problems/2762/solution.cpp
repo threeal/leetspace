a135854b8ca1841b5924b9325e79648a6db0d62b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-14 11:41:05 +0700

    feat(problem-2762/cpp): solve by using sliding window and multiset
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2762/solution.cpp
+++ old-problems/2762/solution.cpp
@@ -1,8 +1,29 @@
+#include <cstdlib>
+#include <set>
 #include <vector>
 
 class Solution {
  public:
   long long continuousSubarrays(std::vector<int>& nums) {
-    return nums.size();
+    long long count{0};
+
+    std::multiset<int> vals{};
+    vals.insert(nums[0]);
+
+    std::size_t l{0}, r{0};
+    while (r < nums.size()) {
+      if (std::abs(*vals.begin() - *vals.rbegin()) <= 2) {
+        count += r + 1 - l;
+        if (++r < nums.size()) {
+          vals.insert(nums[r]);
+        }
+      } else {
+        const auto it = vals.find(nums[l]);
+        vals.erase(it);
+        ++l;
+      }
+    }
+
+    return count;
   }
 };
