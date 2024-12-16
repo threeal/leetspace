ddf6a3c9d56a74e8b411b9ea64bce742813df4e9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-16 23:34:07 +0700

    feat(problem-1455/c): solve by iterating through chars in string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1455/c/solution.c
+++ old-problems/1455/c/solution.c
@@ -1,3 +1,22 @@
 int isPrefixOfWord(char* sentence, char* searchWord) {
-  return *sentence + *searchWord;
+  int i = 1;
+  char* ca = sentence;
+
+  while (1) {
+    char* cb = searchWord;
+    while (*cb != 0 && *ca == *cb) {
+      ++ca;
+      ++cb;
+    }
+
+    if (*cb == 0) return i;
+    while (*ca >= 'a') ++ca;
+
+    if (*ca == 0) break;
+
+    ++i;
+    ++ca;
+  }
+
+  return -1;
 }
