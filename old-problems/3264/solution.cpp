3c4b75e0e18dcfe0cfb91358a7a2867c024644ea
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-16 23:17:55 +0700

    feat(problem-3264/cpp): solve by using partial sort
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3264/solution.cpp
+++ old-problems/3264/solution.cpp
@@ -1,9 +1,28 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> getFinalState(
-      std::vector<int>& nums, int /*k*/, int /*multiplier*/) {
+      std::vector<int>& nums, int k, int multiplier) {
+    std::vector<std::size_t> idxs(nums.size());
+    std::iota(idxs.begin(), idxs.end(), 0);
+
+    auto end = idxs.end();
+    while (k > 0) {
+      const auto range = idxs.begin() + std::min<std::size_t>(k, idxs.size());
+      std::partial_sort(
+          idxs.begin(), range, end,
+          [&nums](const std::size_t a, const std::size_t b) -> bool {
+            return nums[a] == nums[b] ? a < b : nums[a] < nums[b];
+          });
+
+      nums[idxs.front()] *= multiplier;
+      end = range;
+      --k;
+    }
+
     return nums;
   }
 };
