0e0c68e9f53df39d616ed1396cfde720779b68df
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-17 20:13:31 +0700

    feat(problem-2825/c): solve by comparing characters in both string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2825/c/solution.c
+++ old-problems/2825/c/solution.c
@@ -1,5 +1,10 @@
 #include <stdbool.h>
 
 bool canMakeSubsequence(char* str1, char* str2) {
-  return *str1 == *str2;
+  for (char *c1 = str1, *c2 = str2; *c1 != 0; ++c1) {
+    if (*c1 == *c2 || (*c1 + 1 - 'a') % 26 == *c2 - 'a') {
+      if (*(++c2) == 0) return true;
+    }
+  }
+  return false;
 }
