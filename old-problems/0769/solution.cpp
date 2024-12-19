40bc5e4070fb2904bbba2a8efabf36d693bcbb42
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-19 15:35:04 +0700

    feat(problem-769/cpp): solve by keeping track of max number
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0769/solution.cpp
+++ old-problems/0769/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   int maxChunksToSorted(std::vector<int>& arr) {
-    return arr.size();
+    int chunks{0}, max{0};
+    for (int i{0}; i < static_cast<int>(arr.size()); ++i) {
+      if (arr[i] > max) max = arr[i];
+      if (max == i) ++chunks;
+    }
+    return chunks;
   }
 };
