a8d196e6b590f1353c324c7a73adc76e0c5b3112
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-20 21:24:37 +0700

    feat(problem-2415/cpp): solve by reversing values of nodes at the same odd level
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2415/solution.cpp
+++ old-problems/2415/solution.cpp
@@ -1,6 +1,38 @@
+#include <list>
+#include <utility>
+
 class Solution {
  public:
   TreeNode* reverseOddLevels(TreeNode* root) {
+    std::list<TreeNode*> nodes{};
+    nodes.push_back(root);
+
+    bool isOdd{false};
+    while (!nodes.empty()) {
+      if (isOdd) {
+        auto left = nodes.begin();
+        auto right = nodes.rbegin();
+        for (std::size_t n{nodes.size() / 2}; n > 0; --n) {
+          std::swap((*left)->val, (*right)->val);
+          ++left;
+          ++right;
+        }
+      }
+      isOdd = !isOdd;
+
+      for (std::size_t n{nodes.size()}; n > 0; --n) {
+        if (nodes.front()->left != nullptr) {
+          nodes.push_back(nodes.front()->left);
+        }
+
+        if (nodes.front()->right != nullptr) {
+          nodes.push_back(nodes.front()->right);
+        }
+
+        nodes.pop_front();
+      }
+    }
+
     return root;
   }
 };
