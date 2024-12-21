d7073d6c3b73d51e2aa262163eb1ffd52f4cd1ab
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-21 10:13:49 +0700

    feat(problem-2872/cpp): solve by using recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2872/solution.cpp
+++ old-problems/2872/solution.cpp
@@ -1,3 +1,5 @@
+#include <list>
+#include <tuple>
 #include <vector>
 
 class Solution {
@@ -5,6 +7,33 @@ class Solution {
   int maxKDivisibleComponents(
       int n, std::vector<std::vector<int>>& edges,
       std::vector<int>& values, int k) {
-    return n * edges.size() * values.size() * k;
+    std::vector<std::list<int>> leaves(n);
+    for (const auto& edge : edges) {
+      leaves[edge[0]].push_back(edge[1]);
+      leaves[edge[1]].push_back(edge[0]);
+    }
+
+    const auto [count, sum] = splitOrMerge(leaves, values, k, -1, 0);
+    return count;
+  }
+
+ private:
+  static std::tuple<int, int> splitOrMerge(
+      const std::vector<std::list<int>>& leaves,
+      const std::vector<int>& values, int k, int parent, int node) {
+    int count{0}, sum{values[node] % k};
+
+    for (const auto leaf : leaves[node]) {
+      if (leaf == parent) continue;
+      const auto [leafCount, leafSum] = splitOrMerge(leaves, values, k, node, leaf);
+      count += leafCount;
+      sum = (sum + leafSum) % k;
+    }
+
+    if (sum % k == 0) {
+      return {count + 1, 0};
+    } else {
+      return {count, sum};
+    }
   }
 };
