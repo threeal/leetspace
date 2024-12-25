1e4506a9315ac042cf708dbb30b2c602377188ef
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-25 10:33:37 +0700

    feat(problem-515/cpp): solve by using BFS approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0515/solution.cpp
+++ old-problems/0515/solution.cpp
@@ -1,8 +1,29 @@
+#include <limits>
+#include <queue>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> largestValues(TreeNode* root) {
-    return {root->val};
+    std::vector<int> values{};
+    if (root == nullptr) return values;
+
+    std::queue<TreeNode*> nodes{};
+    nodes.push(root);
+
+    while (!nodes.empty()) {
+      int maxValue{std::numeric_limits<int>::min()};
+      for (std::size_t n{nodes.size()}; n > 0; --n) {
+        const auto node = nodes.front();
+        nodes.pop();
+
+        if (node->val > maxValue) maxValue = node->val;
+        if (node->left != nullptr) nodes.push(node->left);
+        if (node->right != nullptr) nodes.push(node->right);
+      }
+      values.push_back(maxValue);
+    }
+
+    return values;
   }
 };
