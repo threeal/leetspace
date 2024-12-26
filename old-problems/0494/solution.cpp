949d208a10e0a2b205e8b67ac9d8e3aa01f9bf99
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-26 16:18:37 +0700

    feat(problem-494/cpp): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0494/solution.cpp
+++ old-problems/0494/solution.cpp
@@ -3,6 +3,13 @@
 class Solution {
  public:
   int findTargetSumWays(std::vector<int>& nums, int target) {
-    return nums.size() * target;
+    return countWays(nums.data(), nums.size(), target);
+  }
+
+ private:
+  static int countWays(int* nums, int numsSize, int target) {
+    if (numsSize == 0) return target == 0 ? 1 : 0;
+    return countWays(nums + 1, numsSize - 1, target + nums[0]) +
+        countWays(nums + 1, numsSize - 1, target - nums[0]);
   }
 };
