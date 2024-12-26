3c2418009d5847f3c023975339e1b8dc4614167d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-26 19:25:23 +0700

    feat(problem-2779/c): solve by using sorting and two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2779/c/solution.c
+++ old-problems/2779/c/solution.c
@@ -1,3 +1,16 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int maximumBeauty(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] * k;
+  qsort(nums, numsSize, sizeof(int), compare);
+
+  int max = 1;
+  for (int i = 0, j = 1; j < numsSize; ++i) {
+    while (j < numsSize && nums[j] <= nums[i] + k * 2) ++j;
+    if (j - i > max) max = j - i;
+  }
+  return max;
 }
