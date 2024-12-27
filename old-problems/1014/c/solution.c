468e8686f5a135cfd1b396174c100f63cc4a04d1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-27 10:36:46 +0700

    feat(problem-1014/c): solve by tracking max value while iterating
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1014/c/solution.c
+++ old-problems/1014/c/solution.c
@@ -1,3 +1,9 @@
 int maxScoreSightseeingPair(int* values, int valuesSize) {
-  return values[valuesSize - 1];
+  int maxScore = 0, maxVal = values[0];
+  for (int i = 1; i < valuesSize; ++i) {
+    --maxVal;
+    if (maxVal + values[i] > maxScore) maxScore = maxVal + values[i];
+    if (values[i] >= maxVal) maxVal = values[i];
+  }
+  return maxScore;
 }
