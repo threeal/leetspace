0e1577f2ea182358ec8a3834e1a8381f2aa86304
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-27 10:05:32 +0700

    feat(problem-1014/cpp): solve by tracking max value while iterating
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1014/solution.cpp
+++ old-problems/1014/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   int maxScoreSightseeingPair(std::vector<int>& values) {
-    return values.size();
+    int maxScore{0}, i{0};
+    for (int j{1}; j < static_cast<int>(values.size()); ++j) {
+      const auto score = values[i] + values[j] + i - j;
+      if (score > maxScore) maxScore = score;
+      if (values[j] >= values[i] + i - j) i = j;
+    }
+    return maxScore;
   }
 };
