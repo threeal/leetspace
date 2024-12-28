091cb8792fb513f67738be06b7245b129a61519d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-28 17:43:53 +0700

    feat(problem-2539/c): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2593/c/solution.c
+++ old-problems/2593/c/solution.c
@@ -1,3 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+
+int* gNums;
+
+int compare(const void* a, const void* b) {
+  const int aa = *(int*)a;
+  const int bb = *(int*)b;
+  return gNums[aa] == gNums[bb] ? aa - bb : gNums[aa] - gNums[bb];
+}
+
 long long findScore(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  gNums = nums;
+
+  char* marked = malloc(numsSize * sizeof(char));
+  memset(marked, 0, numsSize * sizeof(char));
+
+  int* idxs = malloc(numsSize * sizeof(int));
+  for (int i = 0; i < numsSize; ++i) idxs[i] = i;
+
+  qsort(idxs, numsSize, sizeof(int), compare);
+
+  long long score = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (marked[idxs[i]]) continue;
+    score += nums[idxs[i]];
+    marked[idxs[i]] = 1;
+    if (idxs[i] > 0) marked[idxs[i] - 1] = 1;
+    if (idxs[i] < numsSize - 1) marked[idxs[i] + 1] = 1;
+  }
+
+  return score;
 }
