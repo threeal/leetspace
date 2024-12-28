4460966a806caf19e98c04a2ae78290cbded5755
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-28 22:28:25 +0700

    feat(problem-1475/c): solve by using double for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1475/c/solution.c
+++ old-problems/1475/c/solution.c
@@ -1,4 +1,12 @@
 int* finalPrices(int* prices, int pricesSize, int* returnSize) {
+  for (int i = 0; i < pricesSize; ++i) {
+    for (int j = i + 1; j < pricesSize; ++j) {
+      if (prices[j] <= prices[i]) {
+        prices[i] -= prices[j];
+        break;
+      }
+    }
+  }
   *returnSize = pricesSize;
   return prices;
 }
