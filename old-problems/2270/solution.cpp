c4adb6c68c2c6603f5a5bbe4bbe22691ed55ce33
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-03 10:48:46 +0700

    feat(problem-2270/cpp): solve using iteration while calculating sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2270/solution.cpp
+++ old-problems/2270/solution.cpp
@@ -1,8 +1,20 @@
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   int waysToSplitArray(std::vector<int>& nums) {
-    return nums.size();
+    long long right{std::accumulate(nums.begin(), nums.end(), 0ll)};
+    nums.pop_back();
+
+    long long left{0};
+    int count{0};
+    for (const auto num : nums) {
+      left += num;
+      right -= num;
+      if (left >= right) ++count;
+    }
+
+    return count;
   }
 };
