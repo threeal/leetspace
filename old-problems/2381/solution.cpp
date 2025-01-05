dac03295e9281f2cda190995477691ad7192251e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-05 09:36:00 +0700

    test: add test for problem 2381. Shifting Letters II
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2381/solution.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::string shiftingLetters(
+      std::string s, std::vector<std::vector<int>>& /*shifts*/) {
+    return s;
+  }
+};
