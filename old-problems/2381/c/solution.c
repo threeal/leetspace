2351fcd3cf9c844fa9701feb39cf01006727351f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-05 10:53:13 +0700

    feat(problem-2381/c): solve by building prefix sum of letter shift
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2381/c/solution.c
+++ old-problems/2381/c/solution.c
@@ -1,7 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+
 char* shiftingLetters(
     char* s, int** shifts, int shiftsSize, int* shiftsColSize) {
-  (void)shifts;
-  (void)shiftsSize;
   (void)shiftsColSize;
+
+  const int sLen = strlen(s);
+
+  int* prefixes = malloc((sLen + 1) * sizeof(int));
+  memset(prefixes, 0, (sLen + 1) * sizeof(int));
+
+  for (int i = 0; i < shiftsSize; ++i) {
+    if (shifts[i][2] == 0) {
+      --prefixes[shifts[i][0]];
+      ++prefixes[shifts[i][1] + 1];
+    } else {
+      ++prefixes[shifts[i][0]];
+      --prefixes[shifts[i][1] + 1];
+    }
+  }
+
+  int shift = 0;
+  for (int i = 0; i < sLen; ++i) {
+    shift = (shift + prefixes[i]) % 26;
+    s[i] = 'a' + ((s[i] - 'a' + shift) % 26 + 26) % 26;
+  }
+
   return s;
 }
