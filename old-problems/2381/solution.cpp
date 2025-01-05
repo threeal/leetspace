fe0c7825a295b388fea5047890e61e2bb61c1054
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-05 09:45:24 +0700

    feat(problem-2381/cpp): solve by building prefix sum of letter shift
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2381/solution.cpp
+++ old-problems/2381/solution.cpp
@@ -4,7 +4,24 @@
 class Solution {
  public:
   std::string shiftingLetters(
-      std::string s, std::vector<std::vector<int>>& /*shifts*/) {
+      std::string s, std::vector<std::vector<int>>& shifts) {
+    std::vector<int> prefixes(s.size() + 1, 0);
+    for (const auto& shift : shifts) {
+      if (shift[2] == 0) {
+        --prefixes[shift[0]];
+        ++prefixes[shift[1] + 1];
+      } else {
+        ++prefixes[shift[0]];
+        --prefixes[shift[1] + 1];
+      }
+    }
+
+    int shift = 0;
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      shift = (shift + prefixes[i]) % 26;
+      s[i] = 'a' + ((s[i] - 'a' + shift) % 26 + 26) % 26;
+    }
+
     return s;
   }
 };
