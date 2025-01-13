39b1fc6d56f14c202efbfc5fa7148547b0c55394
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-13 14:19:23 +0700

    feat(problem-3223/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3223/solution.cpp
+++ old-problems/3223/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   int minimumLength(std::string s) {
-    return s.size();
+    int freqs[26] = {0};
+    for (const auto c : s) {
+      ++freqs[c - 'a'];
+    }
+
+    int len{0};
+    for (int i{0}; i < 26; ++i) {
+      if (freqs[i] == 0) continue;
+      len += freqs[i] % 2 == 0 ? 2 : 1;
+    }
+
+    return len;
   }
 };
