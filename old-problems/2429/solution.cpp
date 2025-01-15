0646c16b549416579c2c66cac7290c194fa0b25e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-15 13:18:36 +0700

    feat(problem-2429/cpp): solve by using popcount and bit manipulation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2429/solution.cpp
+++ old-problems/2429/solution.cpp
@@ -1,6 +1,23 @@
+#include <bit>
+
 class Solution {
  public:
   int minimizeXor(int num1, int num2) {
-    return num1 + num2;
+    int n1{std::popcount<unsigned int>(num1)};
+    int n2{std::popcount<unsigned int>(num2)};
+
+    if (n1 < n2) {
+      for (int b{1}, i{n1}; i < n2; ++i) {
+        while ((num1 & b) != 0) b = b << 1;
+        num1 |= b;
+      }
+    } else if (n1 > n2) {
+      for (int b{1}, i{n2}; i < n1; ++i) {
+        while ((num1 & b) == 0) b = b << 1;
+        num1 ^= b;
+      }
+    }
+
+    return num1;
   }
 };
