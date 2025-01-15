6f42424e78062722bb1f1ab1f5f33679c7215398
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-15 13:22:36 +0700

    feat(problem-495/cpp): solve by using for loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0495/solution.cpp
+++ old-problems/0495/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   int findPoisonedDuration(std::vector<int>& timeSeries, int duration) {
-    return timeSeries.size() * duration;
+    int totalDuration{duration};
+    for (std::size_t i{1}; i < timeSeries.size(); ++i) {
+      totalDuration += std::min(duration, timeSeries[i] - timeSeries[i - 1]);
+    }
+    return totalDuration;
   }
 };
