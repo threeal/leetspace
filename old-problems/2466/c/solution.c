f7f574edc78612a33bce6ad5409c566a9838bbd5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-17 22:49:10 +0700

    feat(problem-2466/c): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2466/c/solution.c
+++ old-problems/2466/c/solution.c
@@ -1,3 +1,13 @@
 int countGoodStrings(int low, int high, int zero, int one) {
-  return low + high + zero + one;
+  int counts[100001];
+  counts[0] = 1;
+
+  for (int i = 1; i <= high; ++i) {
+    int count = 1;
+    if (i >= zero) count += counts[i - zero];
+    if (i >= one) count += counts[i - one];
+    counts[i] = count % 1000000007;
+  }
+
+  return (1000000007 + counts[high] - counts[low - 1]) % 1000000007;
 }
