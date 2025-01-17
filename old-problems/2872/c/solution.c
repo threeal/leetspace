d3db7d7337eb39c9d9a55b31d0b77aa14d56ede8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-17 20:39:54 +0700

    test(problem-2872): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2872/c/solution.c
@@ -0,0 +1,11 @@
+int maxKDivisibleComponents(
+    int n, int** edges, int edgesSize, int* edgesColSize,
+    int* values, int valuesSize, int k) {
+  (void)edges;
+  (void)edgesSize;
+  (void)edgesColSize;
+  (void)values;
+  (void)valuesSize;
+  (void)k;
+  return n;
+}
