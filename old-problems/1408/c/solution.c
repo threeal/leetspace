97e361babcc105089c76c357b148135d2e963937
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-19 22:02:58 +0700

    feat(problem-1408/c): solve by using brute-force approach with sorting it first
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1408/c/solution.c
+++ old-problems/1408/c/solution.c
@@ -1,4 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+
+int compare(const void* a, const void* b) {
+  return strlen(*(char**)a) - strlen(*(char**)b);
+}
+
 char** stringMatching(char** words, int wordsSize, int* returnSize) {
-  *returnSize = wordsSize;
-  return words;
+  qsort(words, wordsSize, sizeof(char*), compare);
+
+  char** output = malloc(wordsSize * sizeof(char*));
+  int outputSize = 0;
+
+  for (int i = 0; i < wordsSize; ++i) {
+    for (int j = i + 1; j < wordsSize; ++j) {
+      if (strstr(words[j], words[i]) != NULL) {
+        output[outputSize++] = words[i];
+        break;
+      }
+    }
+  }
+
+  *returnSize = outputSize;
+  return output;
 }
