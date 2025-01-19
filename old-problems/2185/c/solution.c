595ce315ee25d6e56b564dff04b3982b995f68f2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-19 22:32:57 +0700

    feat(problem-2185/c): solve by using brute force approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2185/c/solution.c
+++ old-problems/2185/c/solution.c
@@ -1,5 +1,10 @@
+#include <string.h>
+
 int prefixCount(char** words, int wordsSize, char* pref) {
-  (void)words;
-  (void)pref;
-  return wordsSize;
+  int prefLen = strlen(pref);
+  int count = 0;
+  for (int i = 0; i < wordsSize; ++i) {
+    if (strncmp(words[i], pref, prefLen) == 0) ++count;
+  }
+  return count;
 }
