ce724749dbafdc21dd533ba58f8740cf22023e72
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-19 21:26:52 +0700

    feat(problem-2185/cpp): solve by using brute force approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2185/solution.cpp
+++ old-problems/2185/solution.cpp
@@ -4,6 +4,10 @@
 class Solution {
  public:
   int prefixCount(std::vector<std::string>& words, std::string pref) {
-    return words.size() + pref.size();
+    int count{0};
+    for (const auto& word : words) {
+      if (word.compare(0, pref.size(), pref) == 0) ++count;
+    }
+    return count;
   }
 };
