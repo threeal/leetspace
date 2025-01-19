c6235aef3304e7cbda01873615d2ec3c8c7de263
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-19 21:23:50 +0700

    feat(problem-1408/cpp): solve by using brute-force approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1408/solution.cpp
+++ old-problems/1408/solution.cpp
@@ -4,6 +4,15 @@
 class Solution {
  public:
   std::vector<std::string> stringMatching(std::vector<std::string>& words) {
-    return words;
+    std::vector<std::string> output{};
+    for (const auto& word : words) {
+      for (const auto& other_word : words) {
+        if (word != other_word && other_word.find(word) != std::string::npos) {
+          output.push_back(word);
+          break;
+        }
+      }
+    }
+    return output;
   }
 };
