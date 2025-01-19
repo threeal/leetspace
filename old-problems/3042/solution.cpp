a67fe47bd450c45fdfc4e8aba52adc724c17dddc
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-19 21:25:24 +0700

    feat(problem-3042/cpp): solve by using brute force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3042/solution.cpp
+++ old-problems/3042/solution.cpp
@@ -4,6 +4,16 @@
 class Solution {
  public:
   int countPrefixSuffixPairs(std::vector<std::string>& words) {
-    return words.size();
+    int count{0};
+    for (std::size_t i{0}; i < words.size(); ++i) {
+      const auto n = words[i].size();
+      for (std::size_t j{i + 1}; j < words.size(); ++j) {
+        if (n > words[j].size()) continue;
+        if (words[j].compare(0, n, words[i]) != 0) continue;
+        if (words[j].compare(words[j].size() - n, n, words[i]) != 0) continue;
+        ++count;
+      }
+    }
+    return count;
   }
 };
