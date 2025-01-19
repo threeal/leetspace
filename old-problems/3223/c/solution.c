2648818a026b717168e7faf2c126549e8a7eae01
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-19 21:37:28 +0700

    feat(problem-3223/c): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3223/c/solution.c
+++ old-problems/3223/c/solution.c
@@ -1,3 +1,15 @@
 int minimumLength(char* s) {
-  return *s;
+  int freqs[26] = {0};
+  while (*s != 0) {
+    ++freqs[*s - 'a'];
+    ++s;
+  }
+
+  int len = 0;
+  for (int i = 0; i < 26; ++i) {
+    if (freqs[i] == 0) continue;
+    len += freqs[i] % 2 == 0 ? 2 : 1;
+  }
+
+  return len;
 }
