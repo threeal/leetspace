fbf13460504b9fe7a327d134735e0b3f17a64e40
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-19 21:29:42 +0700

    perf(problem-1408/cpp): sort words based on the length first
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1408/solution.cpp
+++ old-problems/1408/solution.cpp
@@ -1,18 +1,24 @@
+#include <algorithm>
 #include <string>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::string> stringMatching(std::vector<std::string>& words) {
+    std::sort(words.begin(), words.end(), [](const auto& a, const auto& b) {
+      return a.size() < b.size();
+    });
+
     std::vector<std::string> output{};
-    for (const auto& word : words) {
-      for (const auto& other_word : words) {
-        if (word != other_word && other_word.find(word) != std::string::npos) {
-          output.push_back(word);
+    for (auto it = words.begin(); it != words.end(); ++it) {
+      for (auto otherIt = std::next(it); otherIt != words.end(); ++otherIt) {
+        if (otherIt->find(*it) != std::string::npos) {
+          output.push_back(*it);
           break;
         }
       }
     }
+
     return output;
   }
 };
