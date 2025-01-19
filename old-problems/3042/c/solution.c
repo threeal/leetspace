92e160706b61247807fdf5d4d8fe99967203b693
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-19 22:15:12 +0700

    feat(problem-3042/c): solve by using brute force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3042/c/solution.c
+++ old-problems/3042/c/solution.c
@@ -1,4 +1,16 @@
+#include <string.h>
+
 int countPrefixSuffixPairs(char** words, int wordsSize) {
-  (void)words;
-  return wordsSize;
+  int count = 0;
+  for (int i = 0; i < wordsSize; ++i) {
+    const int ni = strlen(words[i]);
+    for (int j = i + 1; j < wordsSize; ++j) {
+      const int nj = strlen(words[j]);
+      if (ni > nj) continue;
+      if (strncmp(words[j], words[i], ni) != 0) continue;
+      if (strncmp(words[j] + nj - ni, words[i], ni) != 0) continue;
+      ++count;
+    }
+  }
+  return count;
 }
