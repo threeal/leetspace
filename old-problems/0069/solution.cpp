755d6f8e5eefc38f053eb9f49cbd7f3d5bca8759
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-20 13:54:46 +0700

    feat(problem-69/cpp): solve by using binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0069/solution.cpp
+++ old-problems/0069/solution.cpp
@@ -1,6 +1,16 @@
 class Solution {
  public:
   int mySqrt(int x) {
-    return x;
+    if (x == 0) return 0;
+    int low{0}, high{x};
+    while (low < high) {
+      const int mid{high - ((high - low) / 2)};
+      if (x / mid < mid) {
+        high = mid - 1;
+      } else {
+        low = mid;
+      }
+    }
+    return low;
   }
 };
