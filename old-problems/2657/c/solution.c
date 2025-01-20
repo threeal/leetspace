1cdbf3d28319b898ef210f54b630766ce127e90f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-20 19:37:04 +0700

    feat(problem-2657/c): solve by using bitmap
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2657/c/solution.c
+++ old-problems/2657/c/solution.c
@@ -1,5 +1,12 @@
 int* findThePrefixCommonArray(
     int* A, int ASize, int* B, int BSize, int* returnSize) {
-  *returnSize = ASize < BSize ? ASize : BSize;
-  return ASize < BSize ? A : B;
+  (void)BSize;
+  unsigned long long a = 0, b = 0;
+  for (int i = 0; i < ASize; ++i) {
+    a |= 1l << A[i];
+    b |= 1l << B[i];
+    A[i] = __builtin_popcountll(a & b);
+  }
+  *returnSize = ASize;
+  return A;
 }
