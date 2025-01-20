76fa79b200591fecd42e765ab4befda1cb788cab
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-20 20:07:26 +0700

    feat(problem-3/cpp): solve by using two pointers (#2369)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- problems/0003/solution.cpp
+++ problems/0003/solution.cpp
@@ -1,23 +1,18 @@
 #include <string>
-#include <unordered_set>
 
 class Solution {
  public:
   int lengthOfLongestSubstring(std::string s) {
-    return longest(s.c_str(), s.size());
-  }
-
-  int longest(const char* str, int n) {
-    std::unordered_set<char> cs;
-    int i = 0;
-    while (i < n) {
-      if (cs.contains(str[i])) break;
-      cs.insert(str[i]);
-      ++i;
+    std::size_t longest{0};
+    bool exists[256]{false};
+    for (std::size_t l{0}, r{0}; r < s.size(); ++r) {
+      while (exists[s[r]]) {
+        exists[s[l]] = false;
+        ++l;
+      }
+      exists[s[r]] = true;
+      if (r - l + 1 > longest) longest = r - l + 1;
     }
-    if (n <= 1) return i;
-    auto j = longest(str + 1, n - 1);
-
-    return std::max(i, j);
+    return longest;
   }
 };
