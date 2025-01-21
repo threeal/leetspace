67cf978803ef5b48c6a83f1453767e99b54d350e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-21 15:45:41 +0700

    test: add test for problem 2429. Minimize XOR
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2429/c/interface.cpp
@@ -0,0 +1,7 @@
+extern "C" {
+int minimizeXor(int num1, int num2);
+}
+
+int solution_c(int num1, int num2) {
+  return minimizeXor(num1, num2);
+}
---
+++ old-problems/2429/c/solution.c
@@ -0,0 +1,3 @@
+int minimizeXor(int num1, int num2) {
+  return num1 ^ num2;
+}
