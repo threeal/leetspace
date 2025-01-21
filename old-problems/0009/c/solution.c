76be73742ae1bb261651608e40b53de410aa3824
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-21 22:18:17 +0700

    feat(problem-9/c): solve by using division and modulo of power 10
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0009/c/solution.c
+++ old-problems/0009/c/solution.c
@@ -1,5 +1,17 @@
 #include <stdbool.h>
 
 bool isPalindrome(int x) {
-  return x == 0;
+  if (x < 0) return false;
+
+  int high = 1000000000;
+  while (high > x) high /= 10;
+
+  int low = 1;
+  while (low < high) {
+    if (x / low % 10 != x / high % 10) return false;
+    high /= 10;
+    low *= 10;
+  }
+
+  return true;
 }
