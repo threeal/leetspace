f3aaab81dc18c0e9ae99c093140401886b9c661b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-21 21:56:37 +0700

    feat(problem-2559/c): solve by using prefix sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2559/c/solution.c
+++ old-problems/2559/c/solution.c
@@ -1,11 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+
 int* vowelStrings(
     char** words, int wordsSize,
     int** queries, int queriesSize, int* queriesColSize, int* returnSize) {
-  (void)words;
-  (void)wordsSize;
-  (void)queries;
-  (void)queriesSize;
   (void)queriesColSize;
-  *returnSize = 0;
-  return 0;
+
+  int* prefixCounts = malloc((wordsSize + 1) * sizeof(int));
+  prefixCounts[0] = 0;
+
+  int count = 0;
+  for (int i = 0; i < wordsSize; ++i) {
+    switch (words[i][0]) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+        switch (words[i][strlen(words[i]) - 1]) {
+          case 'a':
+          case 'e':
+          case 'i':
+          case 'o':
+          case 'u':
+            ++count;
+        }
+    }
+    prefixCounts[i + 1] = count;
+  }
+
+  int* output = malloc(queriesSize * sizeof(int));
+  for (int i = 0; i < queriesSize; ++i) {
+    output[i] = prefixCounts[queries[i][1] + 1] - prefixCounts[queries[i][0]];
+  }
+
+  *returnSize = queriesSize;
+  return output;
 }
