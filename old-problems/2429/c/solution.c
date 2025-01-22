aa496ffe7aac2d63c3e3b6ca33cd06b8dacb5ac1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-22 19:05:08 +0700

    feat(problem-2429/c): solve by using popcount and bit manipulation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2429/c/solution.c
+++ old-problems/2429/c/solution.c
@@ -1,3 +1,18 @@
 int minimizeXor(int num1, int num2) {
-  return num1 ^ num2;
+  int n1 = __builtin_popcount(num1);
+  int n2 = __builtin_popcount(num2);
+
+  if (n1 < n2) {
+    for (int b = 1, i = n1; i < n2; ++i) {
+      while ((num1 & b) != 0) b = b << 1;
+      num1 |= b;
+    }
+  } else if (n1 > n2) {
+    for (int b = 1, i = n2; i < n1; ++i) {
+      while ((num1 & b) == 0) b = b << 1;
+      num1 ^= b;
+    }
+  }
+
+  return num1;
 }
