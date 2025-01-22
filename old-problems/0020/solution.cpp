6ddbcbb6ab473cfeab10516ed2702d89ee75b633
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-22 22:13:23 +0700

    feat(problem-20/cpp): solve by keeping open brackets in stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0020/solution.cpp
+++ old-problems/0020/solution.cpp
@@ -3,6 +3,32 @@
 class Solution {
  public:
   bool isValid(std::string s) {
-    return s.empty();
+    std::size_t i{0};
+    for (const auto c : s) {
+      switch (c) {
+        case '(':
+        case '[':
+        case '{':
+          s[i] = c;
+          ++i;
+          break;
+
+        case ')':
+          if (i == 0 || s[i - 1] != '(') return false;
+          --i;
+          break;
+
+        case ']':
+          if (i == 0 || s[i - 1] != '[') return false;
+          --i;
+          break;
+
+        case '}':
+          if (i == 0 || s[i - 1] != '{') return false;
+          --i;
+          break;
+      }
+    }
+    return i == 0;
   }
 };
