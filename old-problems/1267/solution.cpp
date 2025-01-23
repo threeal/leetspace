5243d70bde3e414ad9398ebbbd14500e448e8586
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-23 13:21:27 +0700

    feat(problem-1267/cpp): solve by counting servers in each row and column
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1267/solution.cpp
+++ old-problems/1267/solution.cpp
@@ -3,6 +3,29 @@
 class Solution {
  public:
   int countServers(std::vector<std::vector<int>>& grid) {
-    return grid.size();
+    std::vector<int> rowCounts(grid.size(), 0);
+    std::vector<int> colCounts(grid.front().size(), 0);
+
+    for (std::size_t i{0}; i < grid.size(); ++i) {
+      for (std::size_t j{0}; j < grid[i].size(); ++j) {
+        if (grid[i][j] == 1) {
+          ++rowCounts[i];
+          ++colCounts[j];
+        }
+      }
+    }
+
+    int count{0};
+    for (std::size_t i{0}; i < grid.size(); ++i) {
+      if (rowCounts[i] > 1) {
+        count += rowCounts[i];
+      } else if (rowCounts[i] == 1) {
+        for (std::size_t j{0}; j < grid[i].size(); ++j) {
+          if (grid[i][j] == 1 && colCounts[j] > 1) ++count;
+        }
+      }
+    }
+
+    return count;
   }
 };
