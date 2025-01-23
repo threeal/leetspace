3d5515c7dfca1530c8ffcc28329fbcf7664b1ceb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-23 10:49:23 +0700

    feat(problem-4/c): solve by iterating both arrays
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0004/c/solution.c
+++ old-problems/0004/c/solution.c
@@ -1,4 +1,57 @@
 double findMedianSortedArrays(
     int* nums1, int nums1Size, int* nums2, int nums2Size) {
-  return nums1Size > nums2Size ? nums1[nums1Size - 1] : nums2[nums2Size - 1];
+  int target = (nums1Size + nums2Size) / 2 + 1;
+  if ((nums1Size + nums2Size) % 2 == 0) {
+    int i1 = 0, i2 = 0, median1, median2;
+    while (target > 0) {
+      if (i1 == nums1Size) {
+        median2 = (target == 1) ? median1 : nums2[i2 + target - 2];
+        median1 = nums2[i2 + target - 1];
+        break;
+      }
+
+      if (i2 == nums2Size) {
+        median2 = (target == 1) ? median1 : nums1[i1 + target - 2];
+        median1 = nums1[i1 + target - 1];
+        break;
+      }
+
+      median2 = median1;
+      if (nums1[i1] <= nums2[i2]) {
+        median1 = nums1[i1];
+        ++i1;
+      } else {
+        median1 = nums2[i2];
+        ++i2;
+      }
+
+      --target;
+    }
+    return (median1 + median2) / 2.0;
+  } else {
+    int i1 = 0, i2 = 0, median;
+    while (target > 0) {
+      if (i1 == nums1Size) {
+        median = nums2[i2 + target - 1];
+        break;
+      }
+
+      if (i2 == nums2Size) {
+        median = nums1[i1 + target - 1];
+        break;
+      }
+
+      if (nums1[i1] <= nums2[i2]) {
+        median = nums1[i1];
+        ++i1;
+      } else {
+        median = nums2[i2];
+        ++i2;
+      }
+
+      --target;
+    }
+
+    return median;
+  }
 }
