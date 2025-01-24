a146b84e193696de08e407ae8cc5ebc697ac192b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-24 21:45:17 +0700

    feat(problem-2661/cpp): solve by finding row or column with minimum max value
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2661/solution.cpp
+++ old-problems/2661/solution.cpp
@@ -4,6 +4,33 @@ class Solution {
  public:
   int firstCompleteIndex(
       std::vector<int>& arr, std::vector<std::vector<int>>& mat) {
-    return arr.size() ? mat.size() : arr.size();
+    std::vector<std::size_t> arrPos(arr.size() + 1);
+    for (std::size_t i{0}; i < arr.size(); ++i) {
+      arrPos[arr[i]] = i;
+    }
+
+    std::size_t minPos{arr.size() - 1};
+
+    for (std::size_t i{0}; i < mat.size(); ++i) {
+      std::size_t maxPos{0};
+      for (const int val : mat[i]) {
+        if (arrPos[val] > maxPos) maxPos = arrPos[val];
+        if (maxPos >= minPos) break;
+      }
+
+      if (maxPos < minPos) minPos = maxPos;
+    }
+
+    for (std::size_t j{0}; j < mat[0].size(); ++j) {
+      std::size_t maxPos{0};
+      for (std::size_t i{0}; i < mat.size(); ++i) {
+        if (arrPos[mat[i][j]] > maxPos) maxPos = arrPos[mat[i][j]];
+        if (maxPos >= minPos) break;
+      }
+
+      if (maxPos < minPos) minPos = maxPos;
+    }
+
+    return minPos;
   }
 };
