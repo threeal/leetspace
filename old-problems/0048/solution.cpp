de4545ed770252f45d2be05b3e9c2e984f33c04b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-24 21:03:59 +0700

    feat(problem-48/cpp): solve by iterating matrix circularly to swap elements
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0048/solution.cpp
+++ old-problems/0048/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   void rotate(std::vector<std::vector<int>>& matrix) {
-    (void)matrix;
+    const std::size_t n{matrix.size()};
+    for (std::size_t i{0}; i < n / 2; ++i) {
+      for (std::size_t j{i}; j < n - i - 1; ++j) {
+        const int temp{matrix[j][i]};
+        matrix[j][i] = matrix[n - 1 - i][j];
+        matrix[n - 1 - i][j] = matrix[n - 1 - j][n - 1 - i];
+        matrix[n - 1 - j][n - 1 - i] = matrix[i][n - 1 - j];
+        matrix[i][n - 1 - j] = temp;
+      }
+    }
   }
 };
