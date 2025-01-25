e3a8ddc2ada6c2a57406729ea30933df83a281ea
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-25 20:40:28 +0700

    feat(problem-2661/c): solve by finding row or column with minimum max value
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2661/c/solution.c
+++ old-problems/2661/c/solution.c
@@ -1,4 +1,33 @@
+#include <stdlib.h>
+
 int firstCompleteIndex(
     int* arr, int arrSize, int** mat, int matSize, int* matColSize) {
-  return arr[arrSize - 1] + mat[matSize - 1][matColSize[matSize - 1]];
+  int* arrPos = malloc((arrSize + 1) * sizeof(int));
+  for (int i = 0; i < arrSize; ++i) {
+    arrPos[arr[i]] = i;
+  }
+
+  int minPos = arrSize - 1;
+
+  for (int i = 0; i < matSize; ++i) {
+    int maxPos = 0;
+    for (int j = 0; j < matColSize[i]; ++j) {
+      if (arrPos[mat[i][j]] > maxPos) maxPos = arrPos[mat[i][j]];
+      if (maxPos >= minPos) break;
+    }
+
+    if (maxPos < minPos) minPos = maxPos;
+  }
+
+  for (int j = 0; j < matColSize[0]; ++j) {
+    int maxPos = 0;
+    for (int i = 0; i < matSize; ++i) {
+      if (arrPos[mat[i][j]] > maxPos) maxPos = arrPos[mat[i][j]];
+      if (maxPos >= minPos) break;
+    }
+
+    if (maxPos < minPos) minPos = maxPos;
+  }
+
+  return minPos;
 }
