67a0f6de85698d1817d043ea9c03609903ba609d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-25 21:27:25 +0700

    feat(problem-28/c): solve by using for loop and `strncmp`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0028/c/solution.c
+++ old-problems/0028/c/solution.c
@@ -1,5 +1,12 @@
+#include <string.h>
+
 int strStr(char* haystack, char* needle) {
-  (void)haystack;
-  (void)needle;
+  const int haystackLen = strlen(haystack);
+  const int needleLen = strlen(needle);
+  if (haystackLen >= needleLen) {
+    for (int i = 0; i <= haystackLen - needleLen; ++i) {
+      if (strncmp(haystack + i, needle, needleLen) == 0) return i;
+    }
+  }
   return -1;
 }
