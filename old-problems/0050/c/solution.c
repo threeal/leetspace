293d293731ce5981fd03d758790ef570f5b47e6a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-25 22:29:16 +0700

    feat(problem-50/c): solve by repeatedly multiply based on bit mask
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0050/c/solution.c
+++ old-problems/0050/c/solution.c
@@ -1,3 +1,18 @@
 double myPow(double x, int n) {
-  return x * n;
+  unsigned int un;
+  if (n < 0) {
+    x = 1.0 / x;
+    un = n == -2147483648 ? 2147483648u : -n;
+  } else {
+    un = n;
+  }
+
+  double ans = 1.0;
+  while (un != 0) {
+    if ((un & 1) != 0) ans *= x;
+    x *= x;
+    un >>= 1;
+  }
+
+  return ans;
 }
