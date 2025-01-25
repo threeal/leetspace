d1f4f3023e805a75a0e000fa354930c98f122f86
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-25 19:20:35 +0700

    test: add test for problem 2948. Make Lexicographically Smallest Array by Swapping Elements
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2948/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> lexicographicallySmallestArray(
+      std::vector<int>& nums, int limit) {
+    (void)limit;
+    return nums;
+  }
+};
