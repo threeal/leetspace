a6d9a04ba44c200c09caa36bbca97626fbb8add8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-26 22:10:13 +0700

    test(problem-2017/c): solve by using sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2017/c/solution.c
+++ old-problems/2017/c/solution.c
@@ -1,3 +1,21 @@
 long long gridGame(int** grid, int gridSize, int* gridColSize) {
-  return grid[gridSize - 1][gridColSize[gridSize - 1]];
+  (void)gridSize;
+
+  long long top = 0;
+  for (int i = 1; i < gridColSize[0]; ++i) {
+    top += grid[0][i];
+  }
+
+  long long min = top, bottom = 0;
+  for (int i = 1; i < gridColSize[0]; ++i) {
+    top -= grid[0][i];
+    bottom += grid[1][i - 1];
+    if (top > bottom) {
+      if (top < min) min = top;
+    } else {
+      if (bottom < min) min = bottom;
+    }
+  }
+
+  return min;
 }
