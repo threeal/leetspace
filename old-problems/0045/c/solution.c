7d25e56624dec9ccb39598fc39038994e8b12eed
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-27 19:15:10 +0700

    feat(problem-45/c): solve by determining nearest and farthest element for each jump
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0045/c/solution.c
+++ old-problems/0045/c/solution.c
@@ -1,3 +1,13 @@
 int jump(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int near = 0, far = 0, jumpCount = 0;
+  while (far < numsSize - 1) {
+    int farthest = 0;
+    for (int i = near; i <= far; ++i) {
+      farthest = farthest > i + nums[i] ? farthest : i + nums[i];
+    }
+    near = far + 1;
+    far = farthest;
+    ++jumpCount;
+  }
+  return jumpCount;
 }
