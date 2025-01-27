a3c288e2c44d60bfb03d66273affd6aed19be725
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-27 12:57:32 +0700

    feat(problem-1462/cpp): solve by using array map and DFS
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1462/solution.cpp
+++ old-problems/1462/solution.cpp
@@ -5,8 +5,35 @@ class Solution {
   std::vector<bool> checkIfPrerequisite(
       int numCourses, std::vector<std::vector<int>>& prerequisites,
       std::vector<std::vector<int>>& queries) {
-    (void)numCourses;
-    (void)prerequisites;
-    return std::vector<bool>(queries.size(), false);
+    std::vector<bool> isPrerequisite(numCourses * numCourses, false);
+
+    for (const auto& prerequisite : prerequisites) {
+      isPrerequisite[prerequisite[0] * numCourses + prerequisite[1]] = true;
+    }
+
+    std::vector<bool> isChecked(numCourses, false), output(queries.size());
+    for (std::size_t i{0}; i < queries.size(); ++i) {
+      if (!isChecked[queries[i][0]]) {
+        check(queries[i][0], numCourses, isPrerequisite, isChecked);
+      }
+      output[i] = isPrerequisite[queries[i][0] * numCourses + queries[i][1]];
+    }
+
+    return output;
+  }
+
+ private:
+  static void check(
+      int i, int numCourses, std::vector<bool>& isPrerequisite,
+      std::vector<bool>& isChecked) {
+    isChecked[i] = true;
+    for (int j{0}; j < numCourses; ++j) {
+      if (!isPrerequisite[i * numCourses + j]) continue;
+      if (!isChecked[j]) check(j, numCourses, isPrerequisite, isChecked);
+      for (int k{0}; k < numCourses; ++k) {
+        if (!isPrerequisite[j * numCourses + k]) continue;
+        isPrerequisite[i * numCourses + k] = true;
+      }
+    }
   }
 };
