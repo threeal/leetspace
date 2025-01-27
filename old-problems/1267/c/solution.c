a850787d8f3b663d1fc1e5c60e3b94ed5059f40a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-27 21:02:09 +0700

    feat(problem-1267/c): solve by counting servers in each row and column
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1267/c/solution.c
+++ old-problems/1267/c/solution.c
@@ -1,3 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+
 int countServers(int** grid, int gridSize, int* gridColSize) {
-  return grid[gridSize - 1][gridColSize[gridSize - 1] - 1];
+  int* rowCounts = malloc(gridSize * sizeof(int));
+  memset(rowCounts, 0, gridSize * sizeof(int));
+
+  int* colCounts = malloc(gridColSize[0] * sizeof(int));
+  memset(colCounts, 0, gridColSize[0] * sizeof(int));
+
+  for (int i = 0; i < gridSize; ++i) {
+    for (int j = 0; j < gridColSize[i]; ++j) {
+      if (grid[i][j] == 1) {
+        ++rowCounts[i];
+        ++colCounts[j];
+      }
+    }
+  }
+
+  int count = 0;
+  for (int i = 0; i < gridSize; ++i) {
+    if (rowCounts[i] > 1) {
+      count += rowCounts[i];
+    } else if (rowCounts[i] == 1) {
+      for (int j = 0; j < gridColSize[i]; ++j) {
+        if (grid[i][j] == 1 && colCounts[j] > 1) ++count;
+      }
+    }
+  }
+
+  return count;
 }
