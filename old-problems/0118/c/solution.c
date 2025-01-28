4131a209e2aa782d45a2e9f28ba0261e837f7305
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-28 12:02:05 +0700

    feat(problem-118/c): solve by filling each row based on prev row
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0118/c/solution.c
+++ old-problems/0118/c/solution.c
@@ -1,6 +1,23 @@
+#include <stdlib.h>
+
 int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
-  (void)numRows;
-  (void)returnColumnSizes;
-  *returnSize = 0;
-  return 0;
+  *returnSize = numRows;
+  *returnColumnSizes = malloc(numRows * sizeof(int));
+  int** rows = malloc(numRows * sizeof(int*));
+
+  (*returnColumnSizes)[0] = 1;
+  rows[0] = malloc(sizeof(int));
+  rows[0][0] = 1;
+
+  for (int i = 1; i < numRows; ++i) {
+    (*returnColumnSizes)[i] = i + 1;
+    rows[i] = malloc((i + 1) * sizeof(int));
+    rows[i][0] = rows[i][i] = 1;
+
+    for (int j = 1; j < i; ++j) {
+      rows[i][j] = rows[i - 1][j - 1] + rows[i - 1][j];
+    }
+  }
+
+  return rows;
 }
