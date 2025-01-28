b02d876200872dd119cedca8273aae2831e8fd28
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-28 12:20:05 +0700

    feat(problem-119/cpp): solve by using for loop (#2458)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- problems/0119/solution.cpp
+++ problems/0119/solution.cpp
@@ -1,28 +1,13 @@
-#include <functional>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> getRow(int rowIndex) {
-    auto cache = std::vector(rowIndex + 1, std::vector<int>());
-    for (int i = 0; i <= rowIndex; ++i) {
-      cache[i].resize(i + 1);
+    std::vector<int> row(rowIndex + 1);
+    for (int i{0}; i <= rowIndex; ++i) {
+      row[i] = 1;
+      for (int j{i - 1}; j > 0; --j) row[j] += row[j - 1];
     }
-
-    const std::function<int(int, int)> fn = [&](int row, int index) {
-      if (index < 0 || index > row) return 0;
-      if (index == 0 || index == row) return 1;
-      if (cache[row][index] <= 0) {
-        cache[row][index] = fn(row - 1, index - 1) + fn(row - 1, index);
-      }
-      return cache[row][index];
-    };
-
-    std::vector<int> arr(rowIndex + 1);
-    for (int i = 0; i <= rowIndex; ++i) {
-      arr[i] = fn(rowIndex, i);
-    }
-
-    return arr;
+    return row;
   }
 };
