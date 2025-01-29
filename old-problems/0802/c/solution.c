70582b6981f60eb89055d52c7de8b58a57167336
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-29 21:45:28 +0700

    feat(problem-802/c): solve by repeatedly marking safe nodes
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0802/c/solution.c
+++ old-problems/0802/c/solution.c
@@ -1,8 +1,45 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
 int* eventualSafeNodes(
     int** graph, int graphSize, int* graphColSize, int* returnSize) {
-  (void)graph;
-  (void)graphSize;
-  (void)graphColSize;
-  *returnSize = 0;
-  return 0;
+  bool* isSafeNodes = malloc(graphSize * sizeof(bool));
+  memset(isSafeNodes, 0, graphSize * sizeof(bool));
+  for (int i = 0; i < graphSize; ++i) {
+    if (graphColSize[i] == 0) isSafeNodes[i] = true;
+  }
+
+  bool haveNewSafeNode = true;
+  while (haveNewSafeNode) {
+    haveNewSafeNode = false;
+    for (int i = 0; i < graphSize; ++i) {
+      if (!isSafeNodes[i]) {
+        bool allIsSafeNode = true;
+        for (int j = 0; j < graphColSize[i]; ++j) {
+          if (!isSafeNodes[graph[i][j]]) {
+            allIsSafeNode = false;
+            break;
+          }
+        }
+        if (allIsSafeNode) {
+          isSafeNodes[i] = true;
+          haveNewSafeNode = true;
+        }
+      }
+    }
+  }
+
+  int* safeNodes = malloc(graphSize * sizeof(int));
+  int safeNodesSize = 0;
+
+  for (int i = 0; i < graphSize; ++i) {
+    if (isSafeNodes[i]) {
+      safeNodes[safeNodesSize] = i;
+      ++safeNodesSize;
+    }
+  }
+
+  *returnSize = safeNodesSize;
+  return safeNodes;
 }
