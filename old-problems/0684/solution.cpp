b8158d217c631f61762f40234557271f0764ba5b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-29 19:38:21 +0700

    feat(problem-684/cpp): solve by using union find
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0684/solution.cpp
+++ old-problems/0684/solution.cpp
@@ -1,9 +1,33 @@
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> findRedundantConnection(
       std::vector<std::vector<int>>& edges) {
+    std::vector<int> roots(edges.size() + 1);
+    std::iota(roots.begin(), roots.end(), 0);
+
+    std::vector<int> rootSizes(edges.size() + 1, 1);
+    for (const auto& edge : edges) {
+      const auto a = findRoot(roots, edge[0]);
+      const auto b = findRoot(roots, edge[1]);
+      if (a == b) return edge;
+      if (rootSizes[a] <= rootSizes[b]) {
+        roots[b] = a;
+        rootSizes[a] += rootSizes[b];
+      } else {
+        roots[a] = b;
+        rootSizes[b] += rootSizes[a];
+      }
+    }
+
     return edges.front();
+  }
+
+ private:
+  static int findRoot(std::vector<int>& roots, int node) {
+    if (roots[node] == node) return node;
+    return roots[node] = findRoot(roots, roots[node]);
   }
 };
