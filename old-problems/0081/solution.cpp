bf5cd00e58ebe4ffbc9dadbd4c2a93188aef122c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-29 20:45:21 +0700

    feat(problem-81/cpp): solve by using binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0081/solution.cpp
+++ old-problems/0081/solution.cpp
@@ -3,6 +3,47 @@
 class Solution {
  public:
   bool search(std::vector<int>& nums, int target) {
-    return nums.front() == target;
+    if (nums.front() == target || nums.back() == target) return true;
+
+    std::size_t front{0}, back{nums.size() - 1};
+    while (nums[front] == nums.front() && nums[back] == nums.back() &&
+           front < back) {
+      ++front;
+      --back;
+    }
+
+    if (front > back) return false;
+
+    if (target <= nums[back]) {
+      if (target == nums[back]) return true;
+
+      std::size_t low{front}, high{back};
+      while (low < high) {
+        const std::size_t mid{low + (high - low) / 2};
+        if (nums[mid] > nums[back] || nums[mid] < target) {
+          low = mid + 1;
+        } else {
+          high = mid;
+        }
+      }
+
+      return nums[low] == target;
+    } else if (target >= nums[front]) {
+      if (target == nums[front]) return true;
+
+      std::size_t low{front}, high{back};
+      while (low < high) {
+        const std::size_t mid{high - (high - low) / 2};
+        if (nums[mid] < nums[front] || nums[mid] > target) {
+          high = mid - 1;
+        } else {
+          low = mid;
+        }
+      }
+
+      return nums[low] == target;
+    }
+
+    return false;
   }
 };
