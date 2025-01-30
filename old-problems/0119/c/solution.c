f1c6d4d55f43c1a0f27a09961ee600765276744f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-30 22:53:14 +0700

    feat(problem-119/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0119/c/solution.c
+++ old-problems/0119/c/solution.c
@@ -1,5 +1,11 @@
+#include <stdlib.h>
+
 int* getRow(int rowIndex, int* returnSize) {
-  (void)rowIndex;
-  *returnSize = 0;
-  return 0;
+  *returnSize = rowIndex + 1;
+  int* row = malloc((rowIndex + 1) * sizeof(int));
+  for (int i = 0; i <= rowIndex; ++i) {
+    row[i] = 1;
+    for (int j = i - 1; j > 0; --j) row[j] += row[j - 1];
+  }
+  return row;
 }
