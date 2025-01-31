bed52f6fa4ccb79dc05344633301eafe3d361163
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-31 15:59:48 +0700

    perf(problem-827/cpp): replace boolean array with branching functions
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0827/solution.cpp
+++ old-problems/0827/solution.cpp
@@ -22,31 +22,7 @@ class Solution {
     for (std::size_t y{0}; y < grid.size(); ++y) {
       for (std::size_t x{0}; x < grid[y].size(); ++x) {
         if (grid[y][x] == 0) {
-          std::vector<bool> visited(islandSizes.size(), false);
-          int size{1};
-
-          if (y > 0 && grid[y - 1][x] > 1 && !visited[grid[y - 1][x] - 2]) {
-            visited[grid[y - 1][x] - 2] = true;
-            size += islandSizes[grid[y - 1][x] - 2];
-          }
-
-          if (x > 0 && grid[y][x - 1] > 1 && !visited[grid[y][x - 1] - 2]) {
-            visited[grid[y][x - 1] - 2] = true;
-            size += islandSizes[grid[y][x - 1] - 2];
-          }
-
-          if (y < grid.size() - 1 && grid[y + 1][x] > 1 &&
-              !visited[grid[y + 1][x] - 2]) {
-            visited[grid[y + 1][x] - 2] = true;
-            size += islandSizes[grid[y + 1][x] - 2];
-          }
-
-          if (x < grid[y].size() - 1 && grid[y][x + 1] > 1 &&
-              !visited[grid[y][x + 1] - 2]) {
-            visited[grid[y][x + 1] - 2] = true;
-            size += islandSizes[grid[y][x + 1] - 2];
-          }
-
+          const int size = 1 + checkTopIsland(grid, islandSizes, y, x);
           if (size > maxIslandSize) maxIslandSize = size;
         }
       }
@@ -79,5 +55,59 @@ class Solution {
     }
 
     return size;
+  }
+
+  static int checkTopIsland(
+      std::vector<std::vector<int>>& grid,
+      std::vector<int>& islandSizes,
+      std::size_t y, std::size_t x) {
+    if (y > 0 && grid[y - 1][x] > 1) {
+      const int topSize = islandSizes[grid[y - 1][x] - 2];
+      islandSizes[grid[y - 1][x] - 2] = 0;
+      const int size = checkLeftIsland(grid, islandSizes, y, x);
+      islandSizes[grid[y - 1][x] - 2] = topSize;
+      return topSize + size;
+    } else {
+      return checkLeftIsland(grid, islandSizes, y, x);
+    }
+  }
+
+  static int checkLeftIsland(
+      std::vector<std::vector<int>>& grid,
+      std::vector<int>& islandSizes,
+      std::size_t y, std::size_t x) {
+    if (x > 0 && grid[y][x - 1] > 1) {
+      const int leftSize = islandSizes[grid[y][x - 1] - 2];
+      islandSizes[grid[y][x - 1] - 2] = 0;
+      const int size = checkBottomIsland(grid, islandSizes, y, x);
+      islandSizes[grid[y][x - 1] - 2] = leftSize;
+      return leftSize + size;
+    } else {
+      return checkBottomIsland(grid, islandSizes, y, x);
+    }
+  }
+
+  static int checkBottomIsland(
+      std::vector<std::vector<int>>& grid,
+      std::vector<int>& islandSizes,
+      std::size_t y, std::size_t x) {
+    if (y < grid.size() - 1 && grid[y + 1][x] > 1) {
+      const int topSize = islandSizes[grid[y + 1][x] - 2];
+      islandSizes[grid[y + 1][x] - 2] = 0;
+      const int size = checkRightIsland(grid, islandSizes, y, x);
+      islandSizes[grid[y + 1][x] - 2] = topSize;
+      return topSize + size;
+    } else {
+      return checkRightIsland(grid, islandSizes, y, x);
+    }
+  }
+
+  static int checkRightIsland(
+      std::vector<std::vector<int>>& grid,
+      std::vector<int>& islandSizes,
+      std::size_t y, std::size_t x) {
+    return (x < grid[y].size() - 1 && grid[y][x + 1] > 1)
+        ? islandSizes[grid[y][x + 1] - 2]
+        : 0;
   }
 };
