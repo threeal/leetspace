0a3003b3840647df019511bdfe3d494ea3021345
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-31 21:14:12 +0700

    feat(problem-64/c): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0064/c/solution.c
+++ old-problems/0064/c/solution.c
@@ -1,3 +1,17 @@
 int minPathSum(int** grid, int gridSize, int* gridColSize) {
+  for (int j = 1; j < gridColSize[0]; ++j) {
+    grid[0][j] += grid[0][j - 1];
+  }
+
+  for (int i = 1; i < gridSize; ++i) {
+    grid[i][0] += grid[i - 1][0];
+
+    for (int j = 1; j < gridColSize[i]; ++j) {
+      grid[i][j] += grid[i - 1][j] <= grid[i][j - 1]
+          ? grid[i - 1][j]
+          : grid[i][j - 1];
+    }
+  }
+
   return grid[gridSize - 1][gridColSize[gridSize - 1] - 1];
 }
