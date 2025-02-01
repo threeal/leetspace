d190b437388669ff57500e7b479453b0e0e84901
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-01 10:25:16 +0700

    feat(problem-3151/cpp): solve by iterating through the array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3151/solution.cpp
+++ old-problems/3151/solution.cpp
@@ -3,6 +3,9 @@
 class Solution {
  public:
   bool isArraySpecial(std::vector<int>& nums) {
-    return nums.empty();
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] % 2 == nums[i - 1] % 2) return false;
+    }
+    return true;
   }
 };
