da102bcb74f178fd45b09be9e6a82bff0f9c84ee
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-02 14:49:18 +0700

    feat(problem-1752/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1752/solution.cpp
+++ old-problems/1752/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   bool check(std::vector<int>& nums) {
-    return nums.empty();
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] < nums[i - 1]) {
+        if (nums[i] > nums[0]) return false;
+        for (++i; i < nums.size(); ++i) {
+          if (nums[i] > nums[0] || nums[i] < nums[i - 1]) return false;
+        }
+      }
+    }
+    return true;
   }
 };
