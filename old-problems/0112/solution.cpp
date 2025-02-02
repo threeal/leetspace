3fa44b41774dac84e05b143cbb0f39716e8e59d8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-02 20:52:07 +0700

    feat(problem-112/cpp): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0112/solution.cpp
+++ old-problems/0112/solution.cpp
@@ -1,6 +1,16 @@
 class Solution {
  public:
   bool hasPathSum(TreeNode* root, int targetSum) {
-    return root != nullptr && root->val == targetSum;
+    if (root == nullptr) return false;
+    if (root->left != nullptr) {
+      return root->right != nullptr
+          ? hasPathSum(root->left, targetSum - root->val) ||
+              hasPathSum(root->right, targetSum - root->val)
+          : hasPathSum(root->left, targetSum - root->val);
+    } else {
+      return root->right != nullptr
+          ? hasPathSum(root->right, targetSum - root->val)
+          : root->val == targetSum;
+    }
   }
 };
