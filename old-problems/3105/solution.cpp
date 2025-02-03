b4f3d43c6183dae88513d02d51a3ccb52bce39d9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-03 11:22:16 +0700

    feat(problem-3105/cpp): solve by iterating array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3105/solution.cpp
+++ old-problems/3105/solution.cpp
@@ -3,6 +3,20 @@
 class Solution {
  public:
   int longestMonotonicSubarray(std::vector<int>& nums) {
-    return nums.size();
+    std::size_t longest{1};
+    for (std::size_t l1{0}, l2{0}, r{1}; r < nums.size(); ++r) {
+      if (nums[r] <= nums[r - 1]) {
+        l1 = r;
+      } else if (r - l1 + 1 > longest) {
+        longest = r - l1 + 1;
+      }
+
+      if (nums[r] >= nums[r - 1]) {
+        l2 = r;
+      } else if (r - l2 + 1 > longest) {
+        longest = r - l2 + 1;
+      }
+    }
+    return longest;
   }
 };
