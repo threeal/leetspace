c7770f180886ea5953070757d1fa82692694e372
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-03 14:20:12 +0700

    feat(problem-55/c): solve by iterating numbers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0055/c/solution.c
+++ old-problems/0055/c/solution.c
@@ -1,5 +1,18 @@
 #include <stdbool.h>
 
 bool canJump(int* nums, int numsSize) {
-  return nums[numsSize - 1] == 0;
+  if (numsSize <= 1) return true;
+  nums[numsSize - 1] = 1;
+
+  int maxJump = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    --maxJump;
+    if (nums[i] == 0) {
+      if (maxJump <= 0) return false;
+    } else {
+      if (nums[i] > maxJump) maxJump = nums[i];
+    }
+  }
+
+  return true;
 }
