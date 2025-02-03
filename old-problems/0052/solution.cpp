61844a0511f11e6c31a26e51d9344a761a3b281e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-03 11:52:49 +0700

    feat(problem-52/cpp): solve by using backtrack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0052/solution.cpp
+++ old-problems/0052/solution.cpp
@@ -1,6 +1,39 @@
+#include <vector>
+
 class Solution {
  public:
   int totalNQueens(int n) {
-    return n;
+    std::vector<std::vector<bool>> board(n, std::vector(n, false));
+    return backtrack(n, board, 0);
+  }
+
+ private:
+  static int backtrack(int n, std::vector<std::vector<bool>>& board, int i) {
+    int count{0};
+    for (int j{0}; j < n; ++j) {
+      bool ok{true};
+      for (int k{i - 1}, l{j - 1}, r{j + 1}; k >= 0; --k, --l, ++r) {
+        if (board[k][j]) {
+          ok = false;
+          break;
+        }
+
+        if (l >= 0 && board[k][l]) {
+          ok = false;
+          break;
+        }
+
+        if (r < n && board[k][r]) {
+          ok = false;
+          break;
+        }
+      }
+      if (!ok) continue;
+
+      board[i][j] = true;
+      count += i == n - 1 ? 1 : backtrack(n, board, i + 1);
+      board[i][j] = false;
+    }
+    return count;
   }
 };
