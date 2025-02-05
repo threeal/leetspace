e71476894200c0e4a57b3ba384a1f0ca7735bf73
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-05 12:30:25 +0700

    feat(problem-1790/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1790/solution.cpp
+++ old-problems/1790/solution.cpp
@@ -3,6 +3,18 @@
 class Solution {
  public:
   bool areAlmostEqual(std::string s1, std::string s2) {
-    return s1 == s2;
+    for (std::size_t i{0}; i < s1.size(); ++i) {
+      if (s1[i] == s2[i]) continue;
+      for (std::size_t j{i + 1}; j < s1.size(); ++j) {
+        if (s1[j] == s2[j]) continue;
+        if (s1[i] != s2[j] || s1[j] != s2[i]) return false;
+        for (++j; j < s1.size(); ++j) {
+          if (s1[j] != s2[j]) return false;
+        }
+        return true;
+      }
+      return false;
+    }
+    return true;
   }
 };
