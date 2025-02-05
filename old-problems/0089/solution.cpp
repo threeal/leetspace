82bfa143440a39031f48f6d5eb51e6b7354d4a90
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-05 13:30:16 +0700

    feat(problem-89): solve by repeating the pattern
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0089/solution.cpp
+++ old-problems/0089/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   std::vector<int> grayCode(int n) {
-    return std::vector<int>(1 << n);
+    std::vector<int> output(1 << n);
+    output[0] = 0;
+    output[1] = 1;
+
+    for (int i{1}; i < n; ++i) {
+      for (int l{(1 << i) - 1}, r{1 << i}; l >= 0; --l, ++r) {
+        output[r] = (1 << i) | output[l];
+      }
+    }
+
+    return output;
   }
 };
