7f7f66d3cd88428e12612668ecb69e03e567d643
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-05 12:16:50 +0700

    test(problem-56): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0056/c/solution.c
@@ -0,0 +1,7 @@
+int** merge(
+    int** intervals, int intervalsSize, int* intervalsColSize,
+    int* returnSize, int** returnColumnSizes) {
+  *returnSize = intervalsSize;
+  *returnColumnSizes = intervalsColSize;
+  return intervals;
+}
