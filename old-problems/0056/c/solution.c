22f246fb3d41d756450744169ebcedda1bdfd6e1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-05 12:17:02 +0700

    feat(problem-56/c): solve by sorting intervals before merging
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0056/c/solution.c
+++ old-problems/0056/c/solution.c
@@ -1,7 +1,27 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return **(int**)a - **(int**)b;
+}
+
 int** merge(
     int** intervals, int intervalsSize, int* intervalsColSize,
     int* returnSize, int** returnColumnSizes) {
-  *returnSize = intervalsSize;
+  qsort(intervals, intervalsSize, sizeof(int*), compare);
+
+  int i = 0;
+  for (int j = 1; j < intervalsSize; ++j) {
+    if (intervals[i][1] >= intervals[j][0]) {
+      intervals[i][1] = intervals[i][1] >= intervals[j][1]
+          ? intervals[i][1]
+          : intervals[j][1];
+    } else {
+      ++i;
+      intervals[i] = intervals[j];
+    }
+  }
+
+  *returnSize = i + 1;
   *returnColumnSizes = intervalsColSize;
   return intervals;
 }
