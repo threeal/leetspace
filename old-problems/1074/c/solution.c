9ea5adab139dbaead32c5f1ef94833f31c1a0552
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-05 21:47:34 +0700

    feat(problem-1074/c): solve by using bottom-up DP approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1074/c/solution.c
+++ old-problems/1074/c/solution.c
@@ -1,4 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+
 int numSubmatrixSumTarget(
     int** matrix, int matrixSize, int* matrixColSize, int target) {
-  return matrix[matrixSize - 1][matrixColSize[matrixSize - 1] - 1] * target;
+  int count = 0;
+  for (int i = 0; i < matrixSize; ++i) {
+    for (int j = 0; j < matrixColSize[i]; ++j) {
+      if (matrix[i][j] == target) ++count;
+    }
+  }
+
+  int** hSum = malloc(matrixSize * sizeof(int*));
+  int** vSum = malloc(matrixSize * sizeof(int*));
+  for (int i = 0; i < matrixSize; ++i) {
+    hSum[i] = malloc(matrixColSize[i] * sizeof(int));
+    memcpy(hSum[i], matrix[i], matrixColSize[i] * sizeof(int));
+
+    vSum[i] = malloc(matrixColSize[i] * sizeof(int));
+    memcpy(vSum[i], matrix[i], matrixColSize[i] * sizeof(int));
+  }
+
+  for (int i = 0; i < matrixSize; ++i) {
+    for (int j = 0; j < matrixColSize[i]; ++j) {
+      for (int ii = i; ii < matrixSize; ++ii) {
+        for (int jj = matrixColSize[ii] - 1; jj > j; --jj) {
+          vSum[ii][jj] = hSum[ii][jj] + vSum[ii][jj - 1];
+          if (vSum[ii][jj] == target) ++count;
+        }
+      }
+    }
+
+    for (int ii = matrixSize - 1; ii > i; --ii) {
+      for (int jj = 0; jj < matrixColSize[ii]; ++jj) {
+        hSum[ii][jj] = matrix[ii][jj] + hSum[ii - 1][jj];
+        if (hSum[ii][jj] == target) ++count;
+      }
+      memcpy(vSum[ii], hSum[ii], matrixColSize[ii] * sizeof(int));
+    }
+  }
+
+  for (int i = 0; i < matrixSize; ++i) {
+    free(hSum[i]);
+    free(vSum[i]);
+  }
+  free(hSum);
+  free(vSum);
+
+  return count;
 }
