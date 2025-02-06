e206616172563d5f68195f5929bd55afa0db3d82
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-06 14:57:26 +0700

    feat(problem-65): solve by using branching
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0065/solution.cpp
+++ old-problems/0065/solution.cpp
@@ -3,6 +3,69 @@
 class Solution {
  public:
   bool isNumber(std::string s) {
-    return s.empty();
+    std::size_t i{0};
+    if (s[i] == '+' || s[i] == '-') ++i;
+    if (i < s.size()) {
+      if (s[i] == '.') {
+        ++i;
+        if (s[i] >= '0' && s[i] <= '9') {
+          ++i;
+          while (i < s.size() && s[i] >= '0' && s[i] <= '9') ++i;
+          if (i >= s.size()) return true;
+
+          if (s[i] == 'e' || s[i] == 'E') {
+            ++i;
+            if (s[i] == '+' || s[i] == '-') ++i;
+            if (i < s.size() && s[i] >= '0' && s[i] <= '9') {
+              ++i;
+              while (i < s.size() && s[i] >= '0' && s[i] <= '9') ++i;
+              if (i >= s.size()) return true;
+            }
+          }
+        }
+      } else if (s[i] >= '0' && s[i] <= '9') {
+        ++i;
+        while (i < s.size() && s[i] >= '0' && s[i] <= '9') ++i;
+        if (i >= s.size()) return true;
+
+        if (s[i] == '.') {
+          ++i;
+          if (i >= s.size()) return true;
+
+          if (s[i] >= '0' && s[i] <= '9') {
+            ++i;
+            while (i < s.size() && s[i] >= '0' && s[i] <= '9') ++i;
+            if (i >= s.size()) return true;
+
+            if (s[i] == 'e' || s[i] == 'E') {
+              ++i;
+              if (s[i] == '+' || s[i] == '-') ++i;
+              if (i < s.size() && s[i] >= '0' && s[i] <= '9') {
+                ++i;
+                while (i < s.size() && s[i] >= '0' && s[i] <= '9') ++i;
+                if (i >= s.size()) return true;
+              }
+            }
+          } else if (s[i] == 'e' || s[i] == 'E') {
+            ++i;
+            if (s[i] == '+' || s[i] == '-') ++i;
+            if (i < s.size() && s[i] >= '0' && s[i] <= '9') {
+              ++i;
+              while (i < s.size() && s[i] >= '0' && s[i] <= '9') ++i;
+              if (i >= s.size()) return true;
+            }
+          }
+        } else if (s[i] == 'e' || s[i] == 'E') {
+          ++i;
+          if (s[i] == '+' || s[i] == '-') ++i;
+          if (i < s.size() && s[i] >= '0' && s[i] <= '9') {
+            ++i;
+            while (i < s.size() && s[i] >= '0' && s[i] <= '9') ++i;
+            if (i >= s.size()) return true;
+          }
+        }
+      }
+    }
+    return false;
   }
 };
