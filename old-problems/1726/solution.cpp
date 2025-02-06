cedf2eb29fcb2d429520e8cd98d3d07c1ba83184
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-06 11:46:54 +0700

    feat(problem-1726/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1726/solution.cpp
+++ old-problems/1726/solution.cpp
@@ -1,8 +1,21 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int tupleSameProduct(std::vector<int>& nums) {
-    return nums.size();
+    std::unordered_map<int, int> frequencies{};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      for (std::size_t j{i + 1}; j < nums.size(); ++j) {
+        ++frequencies[nums[i] * nums[j]];
+      }
+    }
+
+    int count{0};
+    for (const auto& [product, freq] : frequencies) {
+      count += freq * (freq - 1) * 4;
+    }
+
+    return count;
   }
 };
