0e5f3f6bd71457bffc0d09b93167b59e6fce51fd
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-06 11:58:54 +0700

    feat(problem-83/cpp): solve by iterating list node
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0083/solution.cpp
+++ old-problems/0083/solution.cpp
@@ -1,6 +1,13 @@
 class Solution {
  public:
   ListNode* deleteDuplicates(ListNode* head) {
+    ListNode* node = head;
+    while (node != nullptr) {
+      while (node->next != nullptr && node->val == node->next->val) {
+        node->next = node->next->next;
+      }
+      node = node->next;
+    }
     return head;
   }
 };
