e59ddbd6a82721214993d03cfdcdcdfa1ab2182c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-06 19:46:59 +0700

    test: add test for problem 787. Cheapest Flights Within K Stops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0787/solution.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+class Solution {
+ public:
+  int findCheapestPrice(
+      int n, std::vector<std::vector<int>>& flights, int src, int dst, int k) {
+    (void)n;
+    (void)flights;
+    (void)src;
+    (void)dst;
+    (void)k;
+    return -1;
+  }
+};
