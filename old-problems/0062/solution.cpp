da5bd04d90ff062c369644dca7d6a77109a86183
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-06 12:20:32 +0700

    feat(problem-62/cpp): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0062/solution.cpp
+++ old-problems/0062/solution.cpp
@@ -1,6 +1,14 @@
+#include <vector>
+
 class Solution {
  public:
   int uniquePaths(int m, int n) {
-    return m * n;
+    std::vector<int> arr(m, 1);
+    for (int i{1}; i < n; ++i) {
+      for (int j{1}; j < m; ++j) {
+        arr[j] += arr[j - 1];
+      }
+    }
+    return arr.back();
   }
 };
