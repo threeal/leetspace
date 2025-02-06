bfdca6a7da9db6bfcdc1e715803437e69af5205b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-06 11:56:39 +0700

    feat(problem-61/cpp): solve by finding list size
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0061/solution.cpp
+++ old-problems/0061/solution.cpp
@@ -1,7 +1,26 @@
 class Solution {
  public:
   ListNode* rotateRight(ListNode* head, int k) {
-    (void)k;
-    return head;
+    if (head == nullptr) return head;
+
+    int n{1};
+    ListNode* tail{head};
+    while (tail->next != nullptr) {
+      tail = tail->next;
+      ++n;
+    }
+
+    k %= n;
+    if (k == 0) return head;
+
+    ListNode* node{head};
+    for (int i{n - k - 1}; i > 0; --i) {
+      node = node->next;
+    }
+
+    ListNode* temp{node->next};
+    node->next = nullptr;
+    tail->next = head;
+    return temp;
   }
 };
