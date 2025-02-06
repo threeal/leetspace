dfe887356037176d71d96d67443d098dd778c932
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-06 20:34:52 +0700

    feat(problem-3151/c): solve by iterating through the array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3151/c/solution.c
+++ old-problems/3151/c/solution.c
@@ -1,5 +1,8 @@
 #include <stdbool.h>
 
 bool isArraySpecial(int* nums, int numsSize) {
-  return nums[numsSize - 1] == 0;
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i] % 2 == nums[i - 1] % 2) return false;
+  }
+  return true;
 }
