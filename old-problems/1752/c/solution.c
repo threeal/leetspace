ae5a58eda518be69c9d223aa688d88248ddbd0f2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-07 14:14:53 +0700

    feat(problem-1752/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1752/c/solution.c
+++ old-problems/1752/c/solution.c
@@ -1,5 +1,13 @@
 #include <stdbool.h>
 
 bool check(int* nums, int numsSize) {
-  return nums[numsSize - 1] == 0;
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i] < nums[i - 1]) {
+      if (nums[i] > nums[0]) return false;
+      for (++i; i < numsSize; ++i) {
+        if (nums[i] > nums[0] || nums[i] < nums[i - 1]) return false;
+      }
+    }
+  }
+  return true;
 }
