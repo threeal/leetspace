ad3a76d98d107b5ed2402591fe4627748fd85064
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-07 19:47:13 +0700

    feat(problem-73/cpp): solve by marking rows and cols with zero
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0073/solution.cpp
+++ old-problems/0073/solution.cpp
@@ -1,8 +1,44 @@
+#include <cstdlib>
+#include <cstring>
 #include <vector>
 
 class Solution {
  public:
   void setZeroes(std::vector<std::vector<int>>& matrix) {
-    (void)matrix;
+    for (std::size_t i{0}; i < matrix.size(); ++i) {
+      for (std::size_t j{0}; j < matrix[i].size(); ++j) {
+        if (matrix[i][j] != 0) continue;
+
+        for (std::size_t k{i}; k < matrix.size(); ++k) {
+          for (std::size_t l{0}; l < matrix[k].size(); ++l) {
+            if (matrix[k][l] == 0) {
+              matrix[i][l] = 0;
+              matrix[k][j] = 0;
+            }
+          }
+        }
+
+        for (std::size_t k{i + 1}; k < matrix.size(); ++k) {
+          if (matrix[k][j] == 0) {
+            std::memset(matrix[k].data(), 0, matrix[k].size() * sizeof(int));
+          }
+        }
+
+        for (std::size_t l{0}; l < matrix[i].size(); ++l) {
+          if (matrix[i][l] == 0) {
+            for (std::size_t k{0}; k < matrix.size(); ++k) {
+              matrix[k][l] = 0;
+            }
+          }
+        }
+
+        std::memset(matrix[i].data(), 0, matrix[i].size() * sizeof(int));
+        for (std::size_t k{0}; k < matrix.size(); ++k) {
+          matrix[k][j] = 0;
+        }
+
+        return;
+      }
+    }
   }
 };
