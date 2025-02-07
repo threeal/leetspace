78df6de3ff04d81ae2ae2ef3330620836d74d270
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-07 19:09:11 +0700

    feat(problem-74/c): solvve by using binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0074/c/solution.c
+++ old-problems/0074/c/solution.c
@@ -2,5 +2,27 @@
 
 bool searchMatrix(
     int** matrix, int matrixSize, int* matrixColSize, int target) {
-  return matrix[matrixSize - 1][matrixColSize[matrixSize - 1] - 1] == target;
+  int vLow = 0, vHigh = matrixSize - 1;
+  while (vLow < vHigh) {
+    int mid = vLow + (vHigh - vLow) / 2;
+    if (matrix[mid][matrixColSize[mid] - 1] < target) {
+      vLow = mid + 1;
+    } else {
+      vHigh = mid;
+    }
+  }
+
+  if (matrix[vLow][matrixColSize[vLow] - 1] < target) return false;
+
+  int hLow = 0, hHigh = matrixColSize[vLow] - 1;
+  while (hLow < hHigh) {
+    int mid = hLow + (hHigh - hLow) / 2;
+    if (matrix[vLow][mid] < target) {
+      hLow = mid + 1;
+    } else {
+      hHigh = mid;
+    }
+  }
+
+  return matrix[vLow][hLow] == target;
 }
