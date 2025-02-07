e62461c986fe33352574d08fd8411d2a88f644c7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-07 19:05:24 +0700

    test: add test for problem 74. Search a 2D Matrix
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0074/c/solution.c
@@ -0,0 +1,6 @@
+#include <stdbool.h>
+
+bool searchMatrix(
+    int** matrix, int matrixSize, int* matrixColSize, int target) {
+  return matrix[matrixSize - 1][matrixColSize[matrixSize - 1] - 1] == target;
+}
