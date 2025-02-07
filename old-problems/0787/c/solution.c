a0960edf09c20901af39a6c203d02282bca30e42
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-07 11:48:48 +0700

    test(problem-787): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0787/c/solution.c
@@ -0,0 +1,12 @@
+int findCheapestPrice(
+    int n, int** flights, int flightsSize, int* flightsColSize,
+    int src, int dst, int k) {
+  (void)n;
+  (void)flights;
+  (void)flightsSize;
+  (void)flightsColSize;
+  (void)src;
+  (void)dst;
+  (void)k;
+  return 0;
+}
