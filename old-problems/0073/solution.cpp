4a632baade200caf93b299ccd23d8aa1c73da030
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-07 19:46:58 +0700

    test: add test for problem 73. Set Matrix Zeroes
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0073/solution.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+class Solution {
+ public:
+  void setZeroes(std::vector<std::vector<int>>& matrix) {
+    (void)matrix;
+  }
+};
---
+++ old-problems/0073/test.cpp
@@ -0,0 +1,24 @@
+// clang-format off
+#include "solution.cpp"
+// clang-format on
+
+#include <catch2/catch_test_macros.hpp>
+#include <vector>
+
+TEST_CASE("73. Set Matrix Zeroes") {
+  SECTION("Example 1") {
+    std::vector<std::vector<int>> matrix{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    Solution{}.setZeroes(matrix);
+    std::vector<std::vector<int>> expected{{1, 0, 1}, {0, 0, 0}, {1, 0, 1}};
+    REQUIRE(matrix == expected);
+  }
+
+  SECTION("Example 2") {
+    std::vector<std::vector<int>> matrix{
+        {0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+    Solution{}.setZeroes(matrix);
+    std::vector<std::vector<int>> expected{
+        {0, 0, 0, 0}, {0, 4, 5, 0}, {0, 3, 1, 0}};
+    REQUIRE(matrix == expected);
+  }
+}
