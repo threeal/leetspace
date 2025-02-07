4821fb640997795ab7bc653ac431f285a6152899
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-07 14:12:26 +0700

    feat(problem-65/c): solve by using branching
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0065/c/solution.c
+++ old-problems/0065/c/solution.c
@@ -1,5 +1,67 @@
 #include <stdbool.h>
 
 bool isNumber(char* s) {
-  return *s != 0;
+  if (*s == '+' || *s == '-') ++s;
+  if (*s != 0) {
+    if (*s == '.') {
+      ++s;
+      if (*s >= '0' && *s <= '9') {
+        ++s;
+        while (*s != 0 && *s >= '0' && *s <= '9') ++s;
+        if (*s == 0) return true;
+
+        if (*s == 'e' || *s == 'E') {
+          ++s;
+          if (*s == '+' || *s == '-') ++s;
+          if (*s != 0 && *s >= '0' && *s <= '9') {
+            ++s;
+            while (*s != 0 && *s >= '0' && *s <= '9') ++s;
+            if (*s == 0) return true;
+          }
+        }
+      }
+    } else if (*s >= '0' && *s <= '9') {
+      ++s;
+      while (*s != 0 && *s >= '0' && *s <= '9') ++s;
+      if (*s == 0) return true;
+
+      if (*s == '.') {
+        ++s;
+        if (*s == 0) return true;
+
+        if (*s >= '0' && *s <= '9') {
+          ++s;
+          while (*s != 0 && *s >= '0' && *s <= '9') ++s;
+          if (*s == 0) return true;
+
+          if (*s == 'e' || *s == 'E') {
+            ++s;
+            if (*s == '+' || *s == '-') ++s;
+            if (*s != 0 && *s >= '0' && *s <= '9') {
+              ++s;
+              while (*s != 0 && *s >= '0' && *s <= '9') ++s;
+              if (*s == 0) return true;
+            }
+          }
+        } else if (*s == 'e' || *s == 'E') {
+          ++s;
+          if (*s == '+' || *s == '-') ++s;
+          if (*s != 0 && *s >= '0' && *s <= '9') {
+            ++s;
+            while (*s != 0 && *s >= '0' && *s <= '9') ++s;
+            if (*s == 0) return true;
+          }
+        }
+      } else if (*s == 'e' || *s == 'E') {
+        ++s;
+        if (*s == '+' || *s == '-') ++s;
+        if (*s != 0 && *s >= '0' && *s <= '9') {
+          ++s;
+          while (*s != 0 && *s >= '0' && *s <= '9') ++s;
+          if (*s == 0) return true;
+        }
+      }
+    }
+  }
+  return false;
 }
