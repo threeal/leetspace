29545f4cc9b0bd826d72ef2084e29847cb235de4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-07 19:53:15 +0700

    feat(problem-3105/c): solve by iterating array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3105/c/solution.c
+++ old-problems/3105/c/solution.c
@@ -1,3 +1,17 @@
 int longestMonotonicSubarray(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int longest = 1;
+  for (int l1 = 0, l2 = 0, r = 1; r < numsSize; ++r) {
+    if (nums[r] <= nums[r - 1]) {
+      l1 = r;
+    } else if (r - l1 + 1 > longest) {
+      longest = r - l1 + 1;
+    }
+
+    if (nums[r] >= nums[r - 1]) {
+      l2 = r;
+    } else if (r - l2 + 1 > longest) {
+      longest = r - l2 + 1;
+    }
+  }
+  return longest;
 }
