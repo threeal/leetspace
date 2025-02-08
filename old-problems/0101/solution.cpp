1a6b2efca346b88033e96f1d4115e31c340d3d68
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-08 11:48:19 +0700

    feat(problem-101/cpp): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0101/solution.cpp
+++ old-problems/0101/solution.cpp
@@ -1,6 +1,16 @@
 class Solution {
  public:
   bool isSymmetric(TreeNode* root) {
-    return root != nullptr;
+    return isEqual(root->left, root->right);
+  }
+
+ private:
+  static bool isEqual(TreeNode* left, TreeNode* right) {
+    if (left != nullptr) {
+      if (right == nullptr || left->val != right->val) return false;
+      return isEqual(left->left, right->right) &&
+          isEqual(left->right, right->left);
+    }
+    return right == nullptr;
   }
 };
