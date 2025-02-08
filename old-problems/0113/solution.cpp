da1c10b24f7b4ed50ae3d4de3144898b8744da1a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-08 12:04:39 +0700

    feat(problem-113/cpp): solve by using DFS
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0113/solution.cpp
+++ old-problems/0113/solution.cpp
@@ -3,8 +3,32 @@
 class Solution {
  public:
   std::vector<std::vector<int>> pathSum(TreeNode* root, int targetSum) {
-    (void)root;
-    (void)targetSum;
-    return {};
+    std::vector<std::vector<int>> output{};
+    if (root != nullptr) {
+      std::vector<int> path{};
+      explore(output, path, 0, root, targetSum);
+    }
+    return output;
+  }
+
+ private:
+  static void explore(
+      std::vector<std::vector<int>>& output, std::vector<int>& path, int sum,
+      TreeNode* root, int targetSum) {
+    path.push_back(root->val);
+    sum += root->val;
+    if (root->left != nullptr) {
+      explore(output, path, sum, root->left, targetSum);
+      if (root->right != nullptr) {
+        explore(output, path, sum, root->right, targetSum);
+      }
+    } else {
+      if (root->right != nullptr) {
+        explore(output, path, sum, root->right, targetSum);
+      } else {
+        if (sum == targetSum) output.push_back(path);
+      }
+    }
+    path.pop_back();
   }
 };
