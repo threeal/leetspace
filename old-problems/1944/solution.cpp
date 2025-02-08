ed3188e0397c826d59c7e9eed423e70513e78690
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-08 20:38:41 +0700

    feat(problem-1944/cpp): solve by keeping heights in stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1944/solution.cpp
+++ old-problems/1944/solution.cpp
@@ -1,8 +1,20 @@
+#include <stack>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> canSeePersonsCount(std::vector<int>& heights) {
+    std::stack<int> stack{};
+    for (std::size_t i{heights.size()}; i > 0; --i) {
+      int count{0};
+      while (!stack.empty() && stack.top() <= heights[i - 1]) {
+        ++count;
+        stack.pop();
+      }
+      if (!stack.empty()) ++count;
+      stack.push(heights[i - 1]);
+      heights[i - 1] = count;
+    }
     return heights;
   }
 };
