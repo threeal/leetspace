4821145c9fd84108ea45059f4272c6bda813ffa3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-08 12:21:15 +0700

    feat(problem-122/cpp): solve by iterating array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0122/solution.cpp
+++ old-problems/0122/solution.cpp
@@ -1,8 +1,13 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maxProfit(std::vector<int>& prices) {
-    return prices.size();
+    int totalProfit{0};
+    for (std::size_t i{1}; i < prices.size(); ++i) {
+      totalProfit += std::max(0, prices[i] - prices[i - 1]);
+    }
+    return totalProfit;
   }
 };
