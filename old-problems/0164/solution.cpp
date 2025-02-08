e14a9bf24a70de1f7ef2df9856dfe9e2be7ea744
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-08 12:38:55 +0700

    feat(problem-164/cpp): solve by sorting array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0164/solution.cpp
+++ old-problems/0164/solution.cpp
@@ -1,8 +1,14 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maximumGap(std::vector<int>& nums) {
-    return nums.size();
+    std::sort(nums.begin(), nums.end());
+    int maxGap{0};
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      maxGap = std::max(maxGap, nums[i] - nums[i - 1]);
+    }
+    return maxGap;
   }
 };
