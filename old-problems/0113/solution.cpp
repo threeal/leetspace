146a3a16012da23ef3621e62ebf5a5ae81e30938
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-08 11:58:20 +0700

    test: add test for problem 113. Path Sum II
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0113/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> pathSum(TreeNode* root, int targetSum) {
+    (void)root;
+    (void)targetSum;
+    return {};
+  }
+};
