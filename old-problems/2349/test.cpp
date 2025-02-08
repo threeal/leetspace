98705912d78f1309316b5daf0d89ea544c7e55ee
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-08 11:09:17 +0700

    test(problem-2349): add test case 10
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2349/test.cpp
+++ old-problems/2349/test.cpp
@@ -16,4 +16,28 @@ TEST_CASE("2349. Design a Number Container System") {
     nc.change(1, 20);
     REQUIRE(nc.find(10) == 2);
   }
+
+  SECTION("Test Case 10") {
+    NumberContainers nc;
+    nc.change(25, 50);
+    nc.change(56, 31);
+    REQUIRE(nc.find(50) == 25);
+    REQUIRE(nc.find(50) == 25);
+    REQUIRE(nc.find(43) == -1);
+    nc.change(30, 50);
+    REQUIRE(nc.find(31) == 56);
+    REQUIRE(nc.find(43) == -1);
+    nc.change(25, 20);
+    REQUIRE(nc.find(50) == 30);
+    nc.change(56, 43);
+    nc.change(68, 31);
+    nc.change(56, 31);
+    REQUIRE(nc.find(20) == 25);
+    REQUIRE(nc.find(43) == -1);
+    nc.change(25, 43);
+    REQUIRE(nc.find(43) == 25);
+    nc.change(56, 31);
+    nc.change(54, 43);
+    nc.change(63, 43);
+  }
 }
