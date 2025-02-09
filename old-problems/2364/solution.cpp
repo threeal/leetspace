ce400ac504816e17724a2cb3a661f22a88a65de0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-09 09:53:17 +0700

    feat(problem-2364/cpp): solve by counting frequencies in hash map
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2364/solution.cpp
+++ old-problems/2364/solution.cpp
@@ -1,8 +1,21 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   long long countBadPairs(std::vector<int>& nums) {
-    return nums.size();
+    std::unordered_map<int, long long> freqs{};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      ++freqs[nums[i] - i];
+    }
+
+    long long total = nums.size();
+    total = total * (total - 1) / 2;
+
+    for (const auto& [i, freq] : freqs) {
+      total -= freq * (freq - 1) / 2;
+    }
+
+    return total;
   }
 };
