e76e5234e65a8323795a5900823ebc0285213907
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-09 11:06:44 +0700

    feat(problem-1790/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1790/c/solution.c
+++ old-problems/1790/c/solution.c
@@ -1,5 +1,17 @@
 #include <stdbool.h>
 
 bool areAlmostEqual(char* s1, char* s2) {
-  return s1 == s2;
+  for (int i = 0; s1[i] != 0; ++i) {
+    if (s1[i] == s2[i]) continue;
+    for (int j = i + 1; s1[j] != 0; ++j) {
+      if (s1[j] == s2[j]) continue;
+      if (s1[i] != s2[j] || s1[j] != s2[i]) return false;
+      for (++j; s1[j] != 0; ++j) {
+        if (s1[j] != s2[j]) return false;
+      }
+      return true;
+    }
+    return false;
+  }
+  return true;
 }
