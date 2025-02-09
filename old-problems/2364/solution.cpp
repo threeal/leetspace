1b498b996cf59c0aa9794c9fb8d668e01a23eab3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-09 09:57:32 +0700

    feat(problem-2364/cpp): count frequencies in the array instead
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2364/solution.cpp
+++ old-problems/2364/solution.cpp
@@ -1,21 +1,25 @@
-#include <unordered_map>
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   long long countBadPairs(std::vector<int>& nums) {
-    std::unordered_map<int, long long> freqs{};
-    for (std::size_t i{0}; i < nums.size(); ++i) {
-      ++freqs[nums[i] - i];
-    }
+    for (std::size_t i{0}; i < nums.size(); ++i) nums[i] -= i;
+    std::sort(nums.begin(), nums.end());
 
     long long total = nums.size();
     total = total * (total - 1) / 2;
 
-    for (const auto& [i, freq] : freqs) {
-      total -= freq * (freq - 1) / 2;
+    long long freq{1};
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] == nums[i - 1]) {
+        ++freq;
+      } else {
+        total -= freq * (freq - 1) / 2;
+        freq = 1;
+      }
     }
 
-    return total;
+    return total - freq * (freq - 1) / 2;
   }
 };
