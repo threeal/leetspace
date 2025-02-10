a1ed1a43d77044233bde9acca2ee58c9a524ba30
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-10 09:58:49 +0700

    feat(problem-168/cpp): solve by repeatedly divide the given number
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0168/solution.cpp
+++ old-problems/0168/solution.cpp
@@ -1,8 +1,16 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   std::string convertToTitle(int columnNumber) {
-    return std::string(columnNumber, '.');
+    std::string str{};
+    while (columnNumber > 0) {
+      --columnNumber;
+      str += 'A' + columnNumber % 26;
+      columnNumber /= 26;
+    }
+    std::reverse(str.begin(), str.end());
+    return str;
   }
 };
