38c00a7417a06fe3e01023c171261f379a98aca2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-10 17:03:07 +0700

    feat(problem-134/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0134/solution.cpp
+++ old-problems/0134/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost) {
-    return gas.size() + cost.size();
+    int totalGas{0};
+    for (std::size_t l{0}, n{0}; l < gas.size(); ++l, --n) {
+      while (totalGas >= 0 && n < gas.size()) {
+        totalGas += gas[(l + n) % gas.size()] - cost[(l + n) % gas.size()];
+        ++n;
+      }
+      if (totalGas >= 0) return l;
+      totalGas -= gas[l] - cost[l];
+    }
+    return -1;
   }
 };
