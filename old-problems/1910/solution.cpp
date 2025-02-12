9c8b36d20b5f1dd4fe374fa2df93111405d39abd
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-12 12:23:03 +0700

    feat(problem-1910/cpp): solve by storing previous chars in stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1910/solution.cpp
+++ old-problems/1910/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   std::string removeOccurrences(std::string s, std::string part) {
-    return s + part;
+    if (s.size() < part.size()) return s;
+
+    std::size_t n{part.size() - 1};
+    for (std::size_t i{n}; i < s.size(); ++i) {
+      s[n] = s[i];
+      ++n;
+      if (n >= part.size() &&
+          s.compare(n - part.size(), part.size(), part) == 0) {
+        n -= part.size();
+      }
+    }
+
+    s.resize(n);
+    return s;
   }
 };
