4e33371121f1d5a03b6b6e9985bedb934d890658
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-14 22:12:59 +0700

    feat(problem-164/c): solve by sorting array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0164/c/solution.c
+++ old-problems/0164/c/solution.c
@@ -1,3 +1,14 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int maximumGap(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  qsort(nums, numsSize, sizeof(int), compare);
+  int maxGap = 0;
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i] - nums[i - 1] > maxGap) maxGap = nums[i] - nums[i - 1];
+  }
+  return maxGap;
 }
