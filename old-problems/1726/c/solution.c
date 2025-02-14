e14f5a9d0740898d701a27cb36e9bcd471e6cab0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-14 21:44:17 +0700

    feat(problem-1726/c): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1726/c/solution.c
+++ old-problems/1726/c/solution.c
@@ -1,3 +1,30 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int tupleSameProduct(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int productsSize = 0;
+  int* products = malloc(numsSize * (numsSize - 1) / 2 * sizeof(int));
+
+  for (int i = 0; i < numsSize; ++i) {
+    for (int j = i + 1; j < numsSize; ++j) {
+      products[productsSize++] = nums[i] * nums[j];
+    }
+  }
+
+  qsort(products, productsSize, sizeof(int), compare);
+
+  int count = 0;
+  for (int i = 1; i < productsSize; ++i) {
+    int n = 1;
+    while (i < productsSize && products[i] == products[i - 1]) {
+      ++i;
+      ++n;
+    }
+    count += n * (n - 1) * 4;
+  }
+
+  return count;
 }
