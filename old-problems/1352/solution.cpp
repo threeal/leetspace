99d0caa696a299932750fc9d9a945bfdb360a28a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-14 22:16:01 +0700

    feat(problem-1352/cpp): solve by using prefix product
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1352/solution.cpp
+++ old-problems/1352/solution.cpp
@@ -1,10 +1,31 @@
-class ProductOfNumbers {
- public:
-  ProductOfNumbers() {}
+#include <vector>
 
-  void add(int /*num*/) {}
+class ProductOfNumbers {
+ private:
+  std::vector<int> products;
+  std::size_t n;
+
+ public:
+  ProductOfNumbers() : products{}, n{0} {}
+
+  void add(int num) {
+    if (num == 0) {
+      n = 0;
+    } else {
+      if (n > 0) num *= products[n - 1];
+      if (n >= products.size()) {
+        products.push_back(num);
+      } else {
+        products[n] = num;
+      }
+      ++n;
+    }
+  }
 
   int getProduct(int k) {
-    return k;
+    if (static_cast<std::size_t>(k) > n) return 0;
+    return static_cast<std::size_t>(k) < n
+        ? products[n - 1] / products[n - k - 1]
+        : products[n - 1];
   }
 };
