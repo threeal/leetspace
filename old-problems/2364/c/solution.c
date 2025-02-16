11a95ea4adeb8f2328d7b9b34e5df08d1cb01f8a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-16 22:33:54 +0700

    feat(problem-2364/c): solve by counting frequencies in array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2364/c/solution.c
+++ old-problems/2364/c/solution.c
@@ -1,3 +1,25 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 long long countBadPairs(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  for (int i = 0; i < numsSize; ++i) nums[i] -= i;
+  qsort(nums, numsSize, sizeof(int), compare);
+
+  long long total = numsSize;
+  total = total * (total - 1) / 2;
+
+  long long freq = 1;
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i] == nums[i - 1]) {
+      ++freq;
+    } else {
+      total -= freq * (freq - 1) / 2;
+      freq = 1;
+    }
+  }
+
+  return total - freq * (freq - 1) / 2;
 }
