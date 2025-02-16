85ce4bac38109d6da1b669826dcdf10ed2477f73
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-16 22:44:01 +0700

    feat(problem-168/c): solve by repeatedly divide the given number
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0168/c/solution.c
+++ old-problems/0168/c/solution.c
@@ -1,4 +1,11 @@
+char str[8] = {0};
+
 char* convertToTitle(int columnNumber) {
-  (void)columnNumber;
-  return "";
+  int strPos = 7;
+  while (columnNumber > 0) {
+    --columnNumber;
+    str[--strPos] = 'A' + columnNumber % 26;
+    columnNumber /= 26;
+  }
+  return str + strPos;
 }
