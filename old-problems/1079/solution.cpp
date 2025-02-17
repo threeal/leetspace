b430a0cad29e2d29b079b944f8d32df38bc5b415
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-17 13:16:58 +0700

    feat(problem-1079/cpp): solve by using backtracking
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1079/solution.cpp
+++ old-problems/1079/solution.cpp
@@ -1,8 +1,41 @@
 #include <string>
+#include <vector>
 
 class Solution {
  public:
   int numTilePossibilities(std::string tiles) {
-    return tiles.size();
+    std::vector<int> freqs(26, 0);
+    for (const auto c : tiles) ++freqs[c - 'A'];
+
+    std::size_t freqsSize{0};
+    for (const auto freq : freqs) {
+      if (freq > 0) freqs[freqsSize++] = freq;
+    }
+    freqs.resize(freqsSize);
+
+    int count{0};
+    for (std::size_t n{1}; n <= tiles.size(); ++n) {
+      count += countPossibilities(n, freqs);
+    }
+
+    return count;
+  }
+
+ private:
+  int countPossibilities(std::size_t n, std::vector<int>& freqs) {
+    int count{0};
+    if (n == 1) {
+      for (const auto freq : freqs) {
+        if (freq > 0) ++count;
+      }
+    } else {
+      for (auto& freq : freqs) {
+        if (freq == 0) continue;
+        --freq;
+        count += countPossibilities(n - 1, freqs);
+        ++freq;
+      }
+    }
+    return count;
   }
 };
