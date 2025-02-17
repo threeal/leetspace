801339f8f8d7251873c9db7740fe9888e546ca45
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-17 21:28:12 +0700

    feat(problem-1910/c): solve by storing previous chars in stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1910/c/solution.c
+++ old-problems/1910/c/solution.c
@@ -1,4 +1,19 @@
+#include <string.h>
+
 char* removeOccurrences(char* s, char* part) {
-  (void)part;
+  const int sSize = strlen(s);
+  const int partSize = strlen(part);
+  if (sSize < partSize) return s;
+
+  int n = partSize - 1;
+  for (int i = n; i < sSize; ++i) {
+    s[n] = s[i];
+    ++n;
+    if (n >= partSize && strncmp(s + n - partSize, part, partSize) == 0) {
+      n -= partSize;
+    }
+  }
+
+  s[n] = 0;
   return s;
 }
