0e14388de2ac8b7699d0db1ff97bac15ffbf0f26
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-18 21:09:35 +0700

    feat(problem-2342/cpp): solve by tracking the maximum number of each sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2342/solution.cpp
+++ old-problems/2342/solution.cpp
@@ -1,36 +1,18 @@
-#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maximumSum(std::vector<int>& nums) {
-    std::vector<int> sums(nums.size());
-    std::vector<std::size_t> indexes(nums.size());
-    for (std::size_t i{0}; i < nums.size(); ++i) {
-      int sum{0}, num{nums[i]};
-      while (num > 0) {
-        sum += num % 10;
-        num /= 10;
-      }
-      sums[i] = sum;
-      indexes[i] = i;
-    }
-
-    std::sort(
-      indexes.begin(), indexes.end(),
-      [&sums, &nums](std::size_t a, std::size_t b) {
-        return sums[a] == sums[b] ? nums[a] < nums[b] : sums[a] < sums[b];
-      });
-
     int max{-1};
-    for (std::size_t i{1}; i < indexes.size(); ++i) {
-      if (sums[indexes[i]] == sums[indexes[i - 1]]) {
-        if (nums[indexes[i]] + nums[indexes[i - 1]] > max) {
-          max = nums[indexes[i]] + nums[indexes[i - 1]];
-        }
+    int maxOfSum[82] = {0};
+    for (auto num : nums) {
+      int sum{0};
+      for (int n{num}; n > 0; n /= 10) sum += n % 10;
+      if (maxOfSum[sum] > 0 && maxOfSum[sum] + num > max) {
+        max = maxOfSum[sum] + num;
       }
+      if (num > maxOfSum[sum]) maxOfSum[sum] = num;
     }
-
     return max;
   }
 };
