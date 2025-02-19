a5d2e58e400fc79a53266ea76ea18ae4e813442e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-19 13:37:15 +0700

    feat(problem-98/cpp): solve by using recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0098/solution.cpp
+++ old-problems/0098/solution.cpp
@@ -1,6 +1,23 @@
+#include <limits>
+
 class Solution {
  public:
   bool isValidBST(TreeNode* root) {
-    return root != nullptr;
+    return isInRange(
+        root, std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+  }
+
+ private:
+  static bool isInRange(TreeNode* root, int min, int max) {
+    if (root->val < min || root->val > max) return false;
+    if (root->left != nullptr) {
+      if (root->val == std::numeric_limits<int>::min()) return false;
+      if (!isInRange(root->left, min, root->val - 1)) return false;
+    }
+    if (root->right != nullptr) {
+      if (root->val == std::numeric_limits<int>::max()) return false;
+      if (!isInRange(root->right, root->val + 1, max)) return false;
+    }
+    return true;
   }
 };
