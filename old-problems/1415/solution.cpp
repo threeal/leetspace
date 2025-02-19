e81b51fd3f0084e7bc6a87ec16a9a6fe51003497
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-19 09:39:35 +0700

    feat(problem-1415/cpp): solve by using backtracking
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1415/solution.cpp
+++ old-problems/1415/solution.cpp
@@ -3,6 +3,21 @@
 class Solution {
  public:
   std::string getHappyString(int n, int k) {
-    return std::string(n, 'a' + k % 3);
+    std::string s(n, 'a');
+    findHappyString(s, 0, k);
+    return k == 0 ? s : "";
+  }
+
+  void findHappyString(std::string& s, std::size_t i, int& k) {
+    for (char c{'a'}; c <= 'c'; ++c) {
+      if (i > 0 && s[i - 1] == c) continue;
+      s[i] = c;
+      if (i + 1 < s.size()) {
+        findHappyString(s, i + 1, k);
+      } else {
+        --k;
+      }
+      if (k == 0) return;
+    }
   }
 };
