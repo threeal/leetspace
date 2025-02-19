14e486c8934e58849d98963bd3a8dc1f3d74364c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-19 15:34:50 +0700

    feat(problem-123/cpp): solve by iterating array forward and backward
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0123/solution.cpp
+++ old-problems/0123/solution.cpp
@@ -3,6 +3,29 @@
 class Solution {
  public:
   int maxProfit(std::vector<int>& prices) {
-    return prices.size();
+    std::vector<int> profits(prices.size());
+
+    int prevPrice{prices.front()}, maxProfit{0};
+    for (std::size_t i{0}; i < prices.size(); ++i) {
+      if (prices[i] >= prevPrice) {
+        maxProfit = std::max(maxProfit, prices[i] - prevPrice);
+      } else {
+        prevPrice = prices[i];
+      }
+      profits[i] = maxProfit;
+    }
+
+    prevPrice = prices.back();
+    maxProfit = profits.back();
+
+    for (std::size_t i{prices.size() - 1}; i > 0; --i) {
+      if (prices[i] <= prevPrice) {
+        maxProfit = std::max(maxProfit, profits[i - 1] + prevPrice - prices[i]);
+      } else {
+        prevPrice = prices[i];
+      }
+    }
+
+    return maxProfit;
   }
 };
