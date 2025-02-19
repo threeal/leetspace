63c020480a5590ec7a141f0a6c4b4085b1823cb3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-19 14:54:56 +0700

    feat(problem-2342/c): solve by tracking the maximum number of each sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2342/c/solution.c
+++ old-problems/2342/c/solution.c
@@ -1,3 +1,13 @@
 int maximumSum(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int max = -1;
+  int maxOfSum[82] = {0};
+  for (int i = 0; i < numsSize; ++i) {
+    int sum = 0;
+    for (int n = nums[i]; n > 0; n /= 10) sum += n % 10;
+    if (maxOfSum[sum] > 0 && maxOfSum[sum] + nums[i] > max) {
+      max = maxOfSum[sum] + nums[i];
+    }
+    if (nums[i] > maxOfSum[sum]) maxOfSum[sum] = nums[i];
+  }
+  return max;
 }
