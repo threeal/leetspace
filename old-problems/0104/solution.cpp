28edc24e72853102b23b8c15a85121b1fe2fb993
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-19 13:53:37 +0700

    feat(problem-104/cpp): solve by using recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0104/solution.cpp
+++ old-problems/0104/solution.cpp
@@ -1,6 +1,9 @@
+#include <algorithm>
+
 class Solution {
  public:
   int maxDepth(TreeNode* root) {
-    return root->val;
+    if (root == nullptr) return 0;
+    return 1 + std::max(maxDepth(root->left), maxDepth(root->right));
   }
 };
