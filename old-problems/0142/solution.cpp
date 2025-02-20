96c2e05c5a1332db22289c0d3dcb4bbe32f09654
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-20 13:52:22 +0700

    feat(problem-142/cpp): solve by using slow and fast pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0142/solution.cpp
+++ old-problems/0142/solution.cpp
@@ -1,6 +1,19 @@
 class Solution {
  public:
   ListNode* detectCycle(ListNode* head) {
-    return head;
+    ListNode* slow{head};
+    ListNode* fast{head};
+    while (fast != nullptr && fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+        while (head != slow) {
+          head = head->next;
+          slow = slow->next;
+        }
+        return head;
+      }
+    }
+    return nullptr;
   }
 };
