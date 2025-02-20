4dfeb75e42a1116075bef9619c61dab2a4c952c2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-20 22:06:16 +0700

    feat(problem-1718/c): solve by using backtrack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1718/c/solution.c
+++ old-problems/1718/c/solution.c
@@ -1,5 +1,57 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+static bool construct(
+    int n, bool* exists, int* output, const int outputSize, int pos);
+
 int* constructDistancedSequence(int n, int* returnSize) {
-  (void)n;
-  *returnSize = 0;
-  return 0;
+  bool* exists = malloc((n + 1) * sizeof(bool));
+  memset(exists, false, (n + 1) * sizeof(bool));
+
+  const int outputSize = n * 2 - 1;
+  int* output = malloc(outputSize * sizeof(int));
+  memset(output, 0, outputSize * sizeof(int));
+
+  construct(n, exists, output, outputSize, 0);
+
+  free(exists);
+
+  *returnSize = outputSize;
+  return output;
+}
+
+static bool construct(
+    int n, bool* exists, int* output, const int outputSize, int pos) {
+  while (pos < outputSize && output[pos] != 0) ++pos;
+
+  if (pos == outputSize) return true;
+
+  for (int i = n; i >= 2; --i) {
+    if (exists[i]) continue;
+    if (pos + i >= outputSize) return false;
+    if (output[pos + i] != 0) continue;
+
+    exists[i] = true;
+    output[pos] = i;
+    output[pos + i] = i;
+
+    if (construct(n, exists, output, outputSize, pos + 1)) return true;
+
+    exists[i] = false;
+    output[pos] = 0;
+    output[pos + i] = 0;
+  }
+
+  if (!exists[1]) {
+    exists[1] = true;
+    output[pos] = 1;
+
+    if (construct(n, exists, output, outputSize, pos + 1)) return true;
+
+    exists[1] = false;
+    output[pos] = 0;
+  }
+
+  return false;
 }
