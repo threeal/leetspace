da470106d16edbaacf66ed271034293003fb6abd
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-20 13:19:38 +0700

    feat(problem-153/cpp): solve by using binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0153/solution.cpp
+++ old-problems/0153/solution.cpp
@@ -3,6 +3,18 @@
 class Solution {
  public:
   int findMin(std::vector<int>& nums) {
-    return nums[0];
+    if (nums.front() < nums.back()) return nums.front();
+
+    std::size_t low{0}, high{nums.size() - 1};
+    while (low < high) {
+      const std::size_t mid{low + (high - low) / 2};
+      if (nums[mid] < nums.back()) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return nums[low];
   }
 };
