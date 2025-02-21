1121b150228c7d6fbb4dcc8e9db765be4703a07b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-21 20:31:24 +0700

    feat(problem-2375/c): solve by storing recovered nodes in a hash set
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1261/solution.cpp
+++ old-problems/1261/solution.cpp
@@ -1,10 +1,31 @@
+#include <unordered_set>
+
 class FindElements {
+ private:
+  std::unordered_set<int> exists;
+
  public:
   FindElements(TreeNode* root) {
-    (void)root;
+    root->val = 0;
+    traverse(root);
   }
 
   bool find(int target) {
-    return target == 0;
+    return exists.contains(target);
+  }
+
+ private:
+  void traverse(TreeNode* node) {
+    exists.insert(node->val);
+
+    if (node->left != nullptr) {
+      node->left->val = node->val * 2 + 1;
+      traverse(node->left);
+    }
+
+    if (node->right != nullptr) {
+      node->right->val = node->val * 2 + 2;
+      traverse(node->right);
+    }
   }
 };
