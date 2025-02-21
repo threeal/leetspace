030e59ef833d4a84e22aee3f3cc5d4df8c258adb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-21 22:01:15 +0700

    feat(problem-1415/c): solve by using backtracking
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1415/c/solution.c
+++ old-problems/1415/c/solution.c
@@ -1,5 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+
+void findHappyString(char* s, int n, int i, int* k);
+
 char* getHappyString(int n, int k) {
-  (void)n;
-  (void)k;
-  return "";
+  char* s = malloc((n + 1) * sizeof(char));
+  memset(s, 'a', n * sizeof(char));
+  s[n] = '\0';
+
+  findHappyString(s, n, 0, &k);
+  return k == 0 ? s : "";
+}
+
+void findHappyString(char* s, int n, int i, int* k) {
+  for (char c = 'a'; c <= 'c'; ++c) {
+    if (i > 0 && s[i - 1] == c) continue;
+    s[i] = c;
+    if (i + 1 < n) {
+      findHappyString(s, n, i + 1, k);
+    } else {
+      --(*k);
+    }
+    if (*k == 0) return;
+  }
 }
