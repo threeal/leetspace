ec09fe7a86b82d46b2a70eaeaeb49e77601e83e1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-23 22:01:00 +0700

    feat(problem-128/c): solve by sorting the given array first
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0128/c/solution.c
+++ old-problems/0128/c/solution.c
@@ -1,3 +1,21 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int longestConsecutive(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  if (numsSize == 0) return 0;
+  qsort(nums, numsSize, sizeof(int), compare);
+  int consecutive = 1, maxConsecutive = 1;
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i] == nums[i - 1]) continue;
+    if (nums[i - 1] + 1 == nums[i]) {
+      ++consecutive;
+    } else {
+      if (consecutive > maxConsecutive) maxConsecutive = consecutive;
+      consecutive = 1;
+    }
+  }
+  return consecutive > maxConsecutive ? consecutive : maxConsecutive;
 }
