4de7bbe5aca2ec2319d3937c3ead6ccf44f4faaa
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-24 13:11:12 +0700

    feat(problem-462/cpp): solve by sorting the given array first
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0462/solution.cpp
+++ old-problems/0462/solution.cpp
@@ -1,8 +1,22 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int minMoves2(std::vector<int>& nums) {
-    return nums.front();
+    std::sort(nums.begin(), nums.end());
+
+    int moves{0};
+    const std::size_t mid{nums.size() / 2};
+
+    for (std::size_t i{0}; i < mid; ++i) {
+      moves += nums[mid] - nums[i];
+    }
+
+    for (std::size_t i{mid + 1}; i < nums.size(); ++i) {
+      moves += nums[i] - nums[mid];
+    }
+
+    return moves;
   }
 };
