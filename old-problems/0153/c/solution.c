fde38e1011db8a507202e6808661b0dffa70efce
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-24 22:17:42 +0700

    feat(problem-153/c): solve by using binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0153/c/solution.c
+++ old-problems/0153/c/solution.c
@@ -1,3 +1,15 @@
 int findMin(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  if (nums[0] < nums[numsSize - 1]) return nums[0];
+
+  int low = 0, high = numsSize - 1;
+  while (low < high) {
+    const int mid = low + (high - low) / 2;
+    if (nums[mid] < nums[numsSize - 1]) {
+      high = mid;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return nums[low];
 }
