b20cbcd6358097a027a589eaa1f6a90745dc8bf9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-24 22:48:34 +0700

    feat(problem-3046/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3046/solution.cpp
+++ old-problems/3046/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   bool isPossibleToSplit(std::vector<int>& nums) {
-    return nums.empty();
+    char freqs[101] = {0};
+    for (const auto num : nums) {
+      if (++freqs[num] == 3) return false;
+    }
+    return true;
   }
 };
