bbb305f5e4fc0d56c5d3ae81db038c682f3aae3b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-25 12:04:51 +0700

    feat(problem-1524/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1524/solution.cpp
+++ old-problems/1524/solution.cpp
@@ -1,8 +1,20 @@
+#include <utility>
 #include <vector>
 
 class Solution {
  public:
   int numOfSubarrays(std::vector<int>& arr) {
-    return arr.size();
+    int count{0}, even{0}, first{0}, second{0};
+    for (const auto num : arr) {
+      if (num % 2 == 0) {
+        ++even;
+      } else {
+        first += even + 1;
+        std::swap(first, second);
+        even = 0;
+      }
+      count = (count + second) % 1000000007;
+    }
+    return count;
   }
 };
