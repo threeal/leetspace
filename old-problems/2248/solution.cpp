a16e0a30a69951a6ccc3bffa7e78e10213d69356
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-25 14:50:39 +0700

    feat(problem-2248/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2248/solution.cpp
+++ old-problems/2248/solution.cpp
@@ -1,8 +1,20 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> intersection(std::vector<std::vector<int>>& nums) {
-    return nums.front();
+    std::size_t freqs[10001] = {0};
+    for (std::size_t i{nums.size() - 1}; i > 0; --i) {
+      for (const auto num : nums[i]) ++freqs[num];
+    }
+
+    std::vector<int> output{};
+    for (const auto num : nums[0]) {
+      if (++freqs[num] == nums.size()) output.push_back(num);
+    }
+
+    std::sort(output.begin(), output.end());
+    return output;
   }
 };
