3fd7b2b0307046f32c95241911ada8e5302888ce
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-26 13:20:23 +0700

    feat(problem-1798/cpp): solve by sorting pile of coins first
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1798/solution.cpp
+++ old-problems/1798/solution.cpp
@@ -1,8 +1,17 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int getMaximumConsecutive(std::vector<int>& coins) {
-    return coins.size();
+    std::sort(coins.begin(), coins.end());
+
+    int max{1};
+    for (const auto coin : coins) {
+      if (coin > max) break;
+      max += coin;
+    }
+
+    return max;
   }
 };
