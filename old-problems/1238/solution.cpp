13e564a70be645ed0d2b3dc8f459168d1ed51039
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-26 15:17:57 +0700

    feat(problem-1238/cpp): solve by using XOR operation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1238/solution.cpp
+++ old-problems/1238/solution.cpp
@@ -1,22 +1,12 @@
-#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> circularPermutation(int n, int start) {
     std::vector<int> output(1 << n);
-    output[0] = 0;
-    output[1] = 1;
-
-    for (int i{1}; i < n; ++i) {
-      for (int l{(1 << i) - 1}, r{1 << i}; l >= 0; --l, ++r) {
-        output[r] = (1 << i) | output[l];
-      }
+    for (std::size_t i{0}; i < output.size(); ++i) {
+      output[i] = i ^ (i >> 1) ^ start;
     }
-
-    const auto it = std::find(output.begin(), output.end(), start);
-    std::rotate(output.begin(), it, output.end());
-
     return output;
   }
 };
