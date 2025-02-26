fccfb9db0418a3961ca96ddc8113a756d05275aa
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-26 21:01:12 +0700

    feat(problem-1524/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1524/c/solution.c
+++ old-problems/1524/c/solution.c
@@ -1,3 +1,16 @@
 int numOfSubarrays(int* arr, int arrSize) {
-  return arr[arrSize - 1];
+  int count = 0, even = 0, first = 0, second = 0;
+  for (int i = 0; i < arrSize; ++i) {
+    if (arr[i] % 2 == 0) {
+      ++even;
+    } else {
+      first += even + 1;
+      first = first ^ second;
+      second = first ^ second;
+      first = first ^ second;
+      even = 0;
+    }
+    count = (count + second) % 1000000007;
+  }
+  return count;
 }
