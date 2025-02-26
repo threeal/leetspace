555872cb5ce95766da29d9d198a46fab9dbb2e24
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-26 15:13:06 +0700

    feat(problem-1627/cpp): solve by joining graph components
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1627/solution.cpp
+++ old-problems/1627/solution.cpp
@@ -1,11 +1,41 @@
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   std::vector<bool> areConnected(
       int n, int threshold, std::vector<std::vector<int>>& queries) {
-    (void)n;
-    (void)threshold;
-    return std::vector(queries.size(), false);
+    std::vector<int> roots(n + 1);
+    std::iota(roots.begin(), roots.end(), 0);
+
+    std::vector<int> rootSizes(n + 1, 1);
+    for (int a{threshold + 1}; a <= n; ++a) {
+      int rootA{findRoot(roots, a)};
+      for (int b{a + a}; b <= n; b += a) {
+        const int rootB{findRoot(roots, b)};
+        if (rootSizes[rootA] < rootSizes[rootB]) rootA = rootB;
+      }
+
+      for (int b{a}; b <= n; b += a) {
+        const int rootB{findRoot(roots, b)};
+        if (rootB != rootA) {
+          roots[rootB] = rootA;
+          rootSizes[rootA] += rootSizes[rootB];
+        }
+      }
+    }
+
+    std::vector<bool> outputs(queries.size());
+    for (std::size_t i{0}; i < queries.size(); ++i) {
+      outputs[i] =
+          findRoot(roots, queries[i][0]) == findRoot(roots, queries[i][1]);
+    }
+
+    return outputs;
+  }
+
+ private:
+  static int findRoot(std::vector<int>& roots, int n) {
+    return roots[n] == n ? n : roots[n] = findRoot(roots, roots[n]);
   }
 };
