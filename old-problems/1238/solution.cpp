f6c0af97fab1e540689df07a0a4671e769ce5fd6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-26 15:11:31 +0700

    feat(problem-1238/cpp): solve by repeateing the pattern
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1238/solution.cpp
+++ old-problems/1238/solution.cpp
@@ -1,8 +1,22 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> circularPermutation(int n, int start) {
-    return std::vector<int>(1 << n, start);
+    std::vector<int> output(1 << n);
+    output[0] = 0;
+    output[1] = 1;
+
+    for (int i{1}; i < n; ++i) {
+      for (int l{(1 << i) - 1}, r{1 << i}; l >= 0; --l, ++r) {
+        output[r] = (1 << i) | output[l];
+      }
+    }
+
+    const auto it = std::find(output.begin(), output.end(), start);
+    std::rotate(output.begin(), it, output.end());
+
+    return output;
   }
 };
