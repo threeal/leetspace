d1ddc31594cf9049db8f8ff794131770394ccada
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-26 10:11:42 +0700

    feat(problem-3443/cpp): solve by counting the directions
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3443/solution.cpp
+++ old-problems/3443/solution.cpp
@@ -2,7 +2,35 @@
 
 class Solution {
  public:
-  int maxDistance(std::string s, int k) {
-    return s.size() + k;
+  int maxDistance(std::string str, int k) {
+    int maxDist{0}, n{0}, s{0}, e{0}, w{0};
+    for (const auto c : str) {
+      switch (c) {
+        case 'N':
+          ++n;
+          break;
+
+        case 'S':
+          ++s;
+          break;
+
+        case 'E':
+          ++e;
+          break;
+
+        case 'W':
+          ++w;
+          break;
+      }
+
+      const int minh = n < s ? n : s;
+      const int maxh = n >= s ? n : s;
+      const int minw = e < w ? e : w;
+      const int maxw = e >= w ? e : w;
+      const int fix = minh + minw <= k ? minh + minw : k;
+      const int dist = maxh + maxw - minh - minw + 2 * fix;
+      if (dist > maxDist) maxDist = dist;
+    }
+    return maxDist;
   }
 };
