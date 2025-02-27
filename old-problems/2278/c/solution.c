06b88821ad0b0ee10a01caf79874ec7c7f932706
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-27 19:32:56 +0700

    feat(problem-2278/c): solve by counting occurrences of given letter
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2278/c/solution.c
+++ old-problems/2278/c/solution.c
@@ -1,3 +1,8 @@
 int percentageLetter(char* s, char letter) {
-  return *s / letter;
+  int count = 0, sSize = 0;
+  while (s[sSize] != '\0') {
+    if (s[sSize] == letter) ++count;
+    ++sSize;
+  }
+  return (count * 100) / sSize;
 }
