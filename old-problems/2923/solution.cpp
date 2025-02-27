318dde4aa4e702daaef5d85eb26de27bd5b68a35
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-27 20:14:50 +0700

    feat(problem-2293): solve by finding the first column where all values are `0`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2923/solution.cpp
+++ old-problems/2923/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int findChampion(std::vector<std::vector<int>>& grid) {
-    return grid.size() - 1;
+    for (std::size_t j{0}; j < grid[0].size(); ++j) {
+      std::size_t i{0};
+      while (i < grid.size()) {
+        if (grid[i][j] == 1) break;
+        ++i;
+      }
+      if (i == grid.size()) return j;
+    }
+    return 0;
   }
 };
