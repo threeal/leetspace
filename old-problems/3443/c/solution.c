cb75aaac7d65707f3951ed659df0c212b97674f2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-28 21:38:39 +0700

    feat(problem-3443/c): solve by counting the direction
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3443/c/solution.c
+++ old-problems/3443/c/solution.c
@@ -1,3 +1,27 @@
-int maxDistance(char* s, int k) {
-  return *s + k;
+int maxDistance(char* str, int k) {
+  int maxDist = 0, n = 0, s = 0, e = 0, w = 0;
+  for (char* c = str; *c != '\0'; ++c) {
+    switch (*c) {
+      case 'N':
+        ++n;
+        break;
+      case 'S':
+        ++s;
+        break;
+      case 'E':
+        ++e;
+        break;
+      case 'W':
+        ++w;
+        break;
+    }
+    const int minh = n < s ? n : s;
+    const int maxh = n >= s ? n : s;
+    const int minw = e < w ? e : w;
+    const int maxw = e >= w ? e : w;
+    const int fix = minh + minw <= k ? minh + minw : k;
+    const int dist = maxh + maxw - minh - minw + 2 * fix;
+    if (dist > maxDist) maxDist = dist;
+  }
+  return maxDist;
 }
