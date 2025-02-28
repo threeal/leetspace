54a74cc0260b0f7e8c0202188be01344012caf4f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-02-28 11:33:12 +0700

    feat(problem-396/cpp): solve by calculating result from previous result
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0396/solution.cpp
+++ old-problems/0396/solution.cpp
@@ -3,6 +3,18 @@
 class Solution {
  public:
   int maxRotateFunction(std::vector<int>& nums) {
-    return nums.size();
+    int value{0}, sum{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      value += nums[i] * i;
+      sum += nums[i];
+    }
+
+    int maxValue{value};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      value += nums[i] * nums.size() - sum;
+      if (value > maxValue) maxValue = value;
+    }
+
+    return maxValue;
   }
 };
