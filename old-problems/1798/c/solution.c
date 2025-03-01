3d3be45183ed2a26c3217fdfc127c724d8731688
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-01 21:03:05 +0700

    feat(problem-1798/c): solve by sorting pile of coins first
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1798/c/solution.c
+++ old-problems/1798/c/solution.c
@@ -1,3 +1,17 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int getMaximumConsecutive(int* coins, int coinsSize) {
-  return coins[coinsSize - 1];
+  qsort(coins, coinsSize, sizeof(int), compare);
+
+  int max = 1;
+  for (int i = 0; i < coinsSize; ++i) {
+    if (coins[i] > max) break;
+    max += coins[i];
+  }
+
+  return max;
 }
