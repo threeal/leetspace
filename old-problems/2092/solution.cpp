f13ca575b3ea541fea4aeb621d06f40b82e3f147
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-01 21:42:51 +0700

    test: add test for problem 2092. Find All People With Secret
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2092/solution.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> findAllPeople(
+      int n, std::vector<std::vector<int>>& meetings, int firstPerson) {
+    (void)n;
+    (void)meetings;
+    (void)firstPerson;
+    return {};
+  }
+};
