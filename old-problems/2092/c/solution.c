13f88f0c0c09122ad9ff1a7b3a5da125a83781bb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-02 20:55:48 +0700

    test(problem-2092): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2092/c/solution.c
@@ -0,0 +1,11 @@
+int* findAllPeople(
+    int n, int** meetings, int meetingsSize, int* meetingsColSize,
+    int firstPerson, int* returnSize) {
+  (void)n;
+  (void)meetings;
+  (void)meetingsSize;
+  (void)meetingsColSize;
+  (void)firstPerson;
+  *returnSize = 0;
+  return 0;
+}
