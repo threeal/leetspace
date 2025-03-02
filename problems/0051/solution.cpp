0e293e87b720126fcd5a3fe67d872cef2db035d7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-02 15:32:06 +0700

    test: add test for problem 51. N-Queens
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ problems/0051/solution.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<std::string>> solveNQueens(int n) {
+    return std::vector<std::vector<std::string>>(n);
+  }
+};
