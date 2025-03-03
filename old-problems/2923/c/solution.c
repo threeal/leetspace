f2aad35ce4860af69b3eaf0c4c81d5374e49bb08
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-03 20:04:23 +0700

    feat(problem-2923/c): solve by finding the first column where all values are `0`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2923/c/solution.c
+++ old-problems/2923/c/solution.c
@@ -1,3 +1,11 @@
 int findChampion(int** grid, int gridSize, int* gridColSize) {
-  return grid[gridSize - 1][gridColSize[gridSize - 1] - 1];
+  for (int j = 0; j < gridColSize[0]; ++j) {
+    int i = 0;
+    while (i < gridSize) {
+      if (grid[i][j] == 1) break;
+      ++i;
+    }
+    if (i == gridSize) return j;
+  }
+  return 0;
 }
