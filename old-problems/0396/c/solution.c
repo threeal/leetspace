6302f4d91e8c83ea7aadc09152975a8f7c0659b9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-04 20:17:27 +0700

    feat(problem-396/c): solve by calculating result from previous result
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0396/c/solution.c
+++ old-problems/0396/c/solution.c
@@ -1,3 +1,15 @@
 int maxRotateFunction(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int value = 0, sum = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    value += nums[i] * i;
+    sum += nums[i];
+  }
+
+  int maxValue = value;
+  for (int i = 0; i < numsSize; ++i) {
+    value += nums[i] * numsSize - sum;
+    if (value > maxValue) maxValue = value;
+  }
+
+  return maxValue;
 }
