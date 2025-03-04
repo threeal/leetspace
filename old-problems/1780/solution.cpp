ea4b600f1f35612fdf566dea84b14a4d5a5d1021
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-04 20:01:57 +0700

    feat(problem-1780/cpp): solve by repeatedly substract number with power of 3
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1780/solution.cpp
+++ old-problems/1780/solution.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
+
 class Solution {
  public:
   bool checkPowersOfThree(int n) {
+    for (int exp{4782969}; exp > 0; exp /= 3) {
+      if (exp <= n) n -= exp;
+    }
     return n == 0;
   }
 };
