a0539e5078b1b6d73ca4a5798a751c7161ae5ae3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-05 22:33:11 +0700

    feat(problem-2579/c): solve by repeatedly choose elements to push from the arrays
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2570/c/solution.c
+++ old-problems/2570/c/solution.c
@@ -1,8 +1,46 @@
+#include <stdlib.h>
+
 int** mergeArrays(
     int** nums1, int nums1Size, int* nums1ColSize,
     int** nums2, int nums2Size, int* nums2ColSize,
     int* returnSize, int** returnColumnSizes) {
-  *returnSize = nums1Size > nums2Size ? nums1Size : nums2Size;
-  *returnColumnSizes = nums1Size > nums2Size ? nums1ColSize : nums2ColSize;
-  return nums1Size > nums2Size ? nums1 : nums2;
+  int** output = malloc((nums1Size + nums2Size) * sizeof(int*));
+  int* outputColSize = malloc((nums1Size + nums2Size) * sizeof(int));
+  int outputSize = 0;
+
+  int i1 = 0, i2 = 0;
+  while (i1 < nums1Size && i2 < nums2Size) {
+    if (nums1[i1][0] == nums2[i2][0]) {
+      nums1[i1][1] += nums2[i2][1];
+      output[outputSize] = nums1[i1];
+      outputColSize[outputSize] = nums1ColSize[i1];
+      ++outputSize, ++i1, ++i2;
+    } else {
+      if (nums1[i1][0] < nums2[i2][0]) {
+        output[outputSize] = nums1[i1];
+        outputColSize[outputSize] = nums1ColSize[i1];
+        ++outputSize, ++i1;
+      } else {
+        output[outputSize] = nums2[i2];
+        outputColSize[outputSize] = nums2ColSize[i2];
+        ++outputSize, ++i2;
+      }
+    }
+  }
+
+  while (i1 < nums1Size) {
+    output[outputSize] = nums1[i1];
+    outputColSize[outputSize] = nums1ColSize[i1];
+    ++outputSize, ++i1;
+  }
+
+  while (i2 < nums2Size) {
+    output[outputSize] = nums2[i2];
+    outputColSize[outputSize] = nums2ColSize[i2];
+    ++outputSize, ++i2;
+  }
+
+  *returnSize = outputSize;
+  *returnColumnSizes = outputColSize;
+  return output;
 }
