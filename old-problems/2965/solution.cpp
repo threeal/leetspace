bd56478a35d0c70d4b331aa0d2451eeb77aba4e3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-06 16:37:43 +0700

    feat(problem-2965/cpp): solve by using flag array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2965/solution.cpp
+++ old-problems/2965/solution.cpp
@@ -4,6 +4,27 @@ class Solution {
  public:
   std::vector<int> findMissingAndRepeatedValues(
       std::vector<std::vector<int>>& grid) {
-    return grid.front();
+    int repeated{};
+    std::vector<bool> exists(grid.size() * grid.size() + 1, false);
+
+    for (const auto& row : grid) {
+      for (const auto val : row) {
+        if (exists[val]) {
+          repeated = val;
+        } else {
+          exists[val] = true;
+        }
+      }
+    }
+
+    int missing{};
+    for (std::size_t val{1}; val < exists.size(); ++val) {
+      if (!exists[val]) {
+        missing = val;
+        break;
+      }
+    }
+
+    return {repeated, missing};
   }
 };
