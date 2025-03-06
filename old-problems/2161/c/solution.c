8f1de773014299f3bf12d4dfd2878111a64af633
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-06 22:12:43 +0700

    feat(problem-2161/c): solve by pushing elements
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2161/c/solution.c
+++ old-problems/2161/c/solution.c
@@ -1,5 +1,24 @@
+#include <stdlib.h>
+
 int* pivotArray(int* nums, int numsSize, int pivot, int* returnSize) {
-  (void)pivot;
-  *returnSize = numsSize;
+  int* temps = malloc(numsSize * sizeof(int));
+  int tempsSize = 0, outputSize = 0, pivotCount = 0;
+
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] == pivot) {
+      ++pivotCount;
+    } else if (nums[i] < pivot) {
+      nums[outputSize++] = nums[i];
+    } else {
+      temps[tempsSize++] = nums[i];
+    }
+  }
+
+  while (--pivotCount >= 0) nums[outputSize++] = pivot;
+  for (int i = 0; i < tempsSize; ++i) nums[outputSize++] = temps[i];
+
+  free(temps);
+
+  *returnSize = outputSize;
   return nums;
 }
