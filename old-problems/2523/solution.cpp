b67e8b1fee2d93d9965dd9b10353ad2bee262515
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-07 12:57:19 +0700

    feat(problem-2523/cpp): solve by using Sieve of Eratosthenes
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2523/solution.cpp
+++ old-problems/2523/solution.cpp
@@ -1,8 +1,27 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> closestPrimes(int left, int right) {
-    return {left, right};
+    std::vector<bool> flags(right + 1, false);
+
+    for (int i{2}; i < left; ++i) {
+      if (!flags[i]) {
+        for (int j{i + i}; j <= right; j += i) flags[j] = true;
+      }
+    }
+
+    int a{-right}, b{right}, prev{-right};
+    for (int i{std::max(left, 2)}; i <= right; ++i) {
+      if (!flags[i]) {
+        if (i - prev < b - a) a = prev, b = i;
+        for (int j{i + i}; j <= right; j += i) flags[j] = true;
+        prev = i;
+      }
+    }
+
+    if (a < 0) return {-1, -1};
+    return {a, b};
   }
 };
