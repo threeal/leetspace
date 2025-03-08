a282ef54f14814a7ff60ef5a684b200e5f6d2f88
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-08 16:52:02 +0700

    feat(problem-415/cpp): solve by using school math algorithm
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0415/solution.cpp
+++ old-problems/0415/solution.cpp
@@ -1,8 +1,34 @@
 #include <string>
+#include <utility>
 
 class Solution {
  public:
   std::string addStrings(std::string num1, std::string num2) {
-    return num1 + num2;
+    if (num1.size() < num2.size()) std::swap(num1, num2);
+
+    char carry{0};
+    auto it1 = num1.rbegin();
+    for (auto it2 = num2.rbegin(); it2 != num2.rend(); ++it1, ++it2) {
+      *it1 += carry + *it2 - '0';
+      if (*it1 > '9') {
+        *it1 -= 10;
+        carry = 1;
+      } else {
+        carry = 0;
+      }
+    }
+
+    while (carry >= 0 && it1 != num1.rend()) {
+      *it1 += carry;
+      if (*it1 > '9') {
+        *it1 -= 10;
+        carry = 1;
+      } else {
+        carry = 0;
+      }
+      ++it1;
+    }
+
+    return carry == 0 ? num1 : "1" + num1;
   }
 };
