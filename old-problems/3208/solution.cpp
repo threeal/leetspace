fef7fc6f4daf8d5fc9ea543e857d5b0a2d77a326
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-09 16:51:27 +0700

    DONT MERGE

--- old-problems/3208/solution.cpp
+++ old-problems/3208/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   int numberOfAlternatingGroups(std::vector<int>& colors, int k) {
-    return colors.size() - k;
+    int count{0}, left{0};
+    for (int i{0}; i < colors.size() + k - 1; ++i) {
+      while (left > 0 &&
+      if (right - left + 1 >= k) ++count;
+    }
+    return count;
   }
 };
