675fe20b782d17ad0f1c15d104face67c309ab16
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-11 17:19:32 +0700

    feat(problem-2965/c): solve by using flag array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2965/c/solution.c
+++ old-problems/2965/c/solution.c
@@ -1,5 +1,37 @@
+#include <stdbool.h>
+#include <string.h>
+
 int* findMissingAndRepeatedValues(
     int** grid, int gridSize, int* gridColSize, int* returnSize) {
-  *returnSize = gridColSize[gridSize - 1];
-  return grid[gridSize - 1];
+  const int n = gridSize * gridSize;
+
+  static bool exists[50 * 50 + 1];
+  memset(exists, false, (n + 1) * sizeof(bool));
+
+  int repeated = 0;
+  for (int i = 0; i < gridSize; ++i) {
+    for (int j = 0; j < gridColSize[i]; ++j) {
+      if (exists[grid[i][j]]) {
+        repeated = grid[i][j];
+      } else {
+        exists[grid[i][j]] = true;
+      }
+    }
+  }
+
+  int missing = 0;
+  for (int val = 1; val <= n; ++val) {
+    if (!exists[val]) {
+      missing = val;
+      break;
+    }
+  }
+
+  static int output[2];
+
+  output[0] = repeated;
+  output[1] = missing;
+
+  *returnSize = 2;
+  return output;
 }
