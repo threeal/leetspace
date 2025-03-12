e2f69d2894ec85052aa48c0c6a0f8ab98ba56629
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-12 11:21:31 +0700

    feat(problem-2529): solve by using binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2529/solution.cpp
+++ old-problems/2529/solution.cpp
@@ -1,8 +1,15 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maximumCount(std::vector<int>& nums) {
-    return nums.size();
+    const auto positive = std::upper_bound(nums.begin(), nums.end(), 0);
+    const auto negative = std::upper_bound(
+        nums.rbegin(), nums.rend(), 0, std::greater<int>());
+
+    return std::max(
+        std::distance(positive, nums.end()),
+        std::distance(negative, nums.rend()));
   }
 };
