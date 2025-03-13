b431869cee98d922697e1110211dca12eb59ecd0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-13 22:50:47 +0700

    feat(problem-2379/c): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2379/c/solution.c
+++ old-problems/2379/c/solution.c
@@ -1,3 +1,19 @@
+#include <string.h>
+
 int minimumRecolors(char* blocks, int k) {
-  return blocks[0] + k;
+  const int blocksLen = strlen(blocks);
+
+  int freq = 0;
+  for (int i = 0; i < k; ++i) {
+    if (blocks[i] == 'W') ++freq;
+  }
+
+  int minFreq = freq;
+  for (int i = k; i < blocksLen; ++i) {
+    if (blocks[i] == 'W') ++freq;
+    if (blocks[i - k] == 'W') --freq;
+    if (freq < minFreq) minFreq = freq;
+  }
+
+  return minFreq;
 }
