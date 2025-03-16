0f5dec9134fbc2f47f88bc2b72984260e6248401
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-16 15:22:36 +0700

    feat(problem-2560/cpp): solve by using a binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2560/solution.cpp
+++ old-problems/2560/solution.cpp
@@ -1,9 +1,27 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
-  int minCapability(
-    [[maybe_unused]] std::vector<int>& nums, [[maybe_unused]] int k) {
-    return 0;
+  int minCapability(std::vector<int>& nums, int k) {
+    int low{0}, high{*std::max_element(nums.begin(), nums.end())};
+    while (low < high) {
+      const int mid{low + (high - low) / 2};
+
+      int count{0};
+      for (std::size_t i{0}; i < nums.size(); ++i) {
+        if (nums[i] <= mid) {
+          if (++count == k) break;
+          ++i;
+        }
+      }
+
+      if (count != k) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+    return low;
   }
 };
