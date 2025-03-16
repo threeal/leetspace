72338332f6a6c32d0b9d05d65b9336e67251f47d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-16 15:14:13 +0700

    feat(problem-2594): solve by using binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2594/solution.cpp
+++ old-problems/2594/solution.cpp
@@ -1,8 +1,33 @@
+#include <cmath>
 #include <vector>
 
 class Solution {
  public:
   long long repairCars(std::vector<int>& ranks, int cars) {
-    return ranks.size() * cars;
+    long long low{100}, high{1};
+    for (const auto rank : ranks) {
+      if (rank < low) low = rank;
+      if (rank > high) high = rank;
+    }
+
+    high *= cars;
+    high *= cars;
+
+    while (low < high) {
+      const long long mid{low + (high - low) / 2};
+
+      long long count{0};
+      for (const auto rank : ranks) {
+        count += static_cast<long long>(std::sqrt(mid / rank));
+      }
+
+      if (count < cars) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+
+    return low;
   }
 };
