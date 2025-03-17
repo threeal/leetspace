48e1dd20ea96b14c16fc3ac299263ba54e4ade77
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-17 09:57:40 +0700

    feat(problem-2206/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2206/solution.cpp
+++ old-problems/2206/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   bool divideArray(std::vector<int>& nums) {
-    return nums.empty();
+    int freqs[501]{0};
+
+    int odds{0};
+    for (const auto num : nums) {
+      if (++freqs[num] % 2 == 0) {
+        --odds;
+      } else {
+        ++odds;
+      }
+    }
+
+    return odds == 0;
   }
 };
