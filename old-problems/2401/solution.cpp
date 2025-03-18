a0d96ac9ac74247bf54ea87a85e888559aac70dd
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-18 10:45:24 +0700

    feat(problem-2401/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2401/solution.cpp
+++ old-problems/2401/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   int longestNiceSubarray(std::vector<int>& nums) {
-    return nums.size();
+    int sum{nums[0]};
+    std::size_t longest{1};
+    for (std::size_t l{0}, r{1}; r < nums.size(); ++r) {
+      while ((sum & nums[r]) != 0) {
+        sum &= ~nums[l];
+        ++l;
+      }
+      if (r - l + 1 > longest) longest = r - l + 1;
+      sum |= nums[r];
+    }
+    return longest;
   }
 };
