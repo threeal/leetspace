08efb30ead2920ee6b8c542cd1dd824fc2aad471
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-19 14:46:09 +0700

    feat(problem-3191/cpp): solve by greedily perform the operation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3191/solution.cpp
+++ old-problems/3191/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   int minOperations(std::vector<int>& nums) {
-    return nums.size();
+    int operations{0};
+    for (std::size_t i{2}; i < nums.size(); ++i) {
+      if (nums[i - 2] == 0) {
+        ++operations;
+        nums[i - 1] ^= 1;
+        nums[i] ^= 1;
+      }
+    }
+
+    return nums[nums.size() - 1] == 1 && nums[nums.size() - 2] == 1
+        ? operations
+        : -1;
   }
 };
