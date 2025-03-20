6b1f8f2fcca112fb2b05c88a1ddfde1515873dbb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-20 15:56:26 +0700

    feat(problem-2206/c): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2206/c/solution.c
+++ old-problems/2206/c/solution.c
@@ -1,5 +1,16 @@
 #include <stdbool.h>
 
 bool divideArray(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int freqs[501] = {0};
+
+  int odds = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (++freqs[nums[i]] % 2 == 0) {
+      --odds;
+    } else {
+      ++odds;
+    }
+  }
+
+  return odds == 0;
 }
