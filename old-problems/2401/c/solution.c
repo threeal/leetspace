399a6199aa3149b2e5244ac6a780bd097e81c4a4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-21 13:21:59 +0700

    feat(problem-2401/c): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2401/c/solution.c
+++ old-problems/2401/c/solution.c
@@ -1,3 +1,12 @@
 int longestNiceSubarray(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int longest = 1, sum = nums[0];
+  for (int l = 0, r = 1; r < numsSize; ++r) {
+    while ((sum & nums[r]) != 0) {
+      sum &= ~nums[l];
+      ++l;
+    }
+    if (r - l + 1 > longest) longest = r - l + 1;
+    sum |= nums[r];
+  }
+  return longest;
 }
