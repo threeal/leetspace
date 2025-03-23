9ae20834a9b563a4cb23b0cc4e82c4c5241e2947
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-23 15:48:33 +0700

    feat(problem-2001/cpp): solve by sorting rectangles divided by their GCD
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2001/solution.cpp
+++ old-problems/2001/solution.cpp
@@ -1,9 +1,29 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   long long interchangeableRectangles(
       std::vector<std::vector<int>>& rectangles) {
-    return rectangles.size();
+    for (auto& rectangle : rectangles) {
+      const int gcd{std::gcd(rectangle[0], rectangle[1])};
+      rectangle[0] /= gcd;
+      rectangle[1] /= gcd;
+    }
+
+    std::sort(rectangles.begin(), rectangles.end());
+
+    long long totalCount{0}, count{0};
+    for (std::size_t i{1}; i < rectangles.size(); ++i) {
+      if (rectangles[i][0] == rectangles[i - 1][0] &&
+          rectangles[i][1] == rectangles[i - 1][1]) {
+        totalCount += ++count;
+      } else {
+        count = 0;
+      }
+    }
+
+    return totalCount;
   }
 };
