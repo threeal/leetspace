3a15f896a3abe94e2ea3afd7a7f6787678cfff2a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-28 20:12:10 +0700

    test: add test for problem 2503. Maximum Number of Points From Grid Queries
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2503/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> maxPoints(
+      std::vector<std::vector<int>>& grid, std::vector<int>& queries) {
+    (void)grid;
+    return queries;
+  }
+};
