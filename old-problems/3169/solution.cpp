64ec266c9968f5e8b9ed91971827363774c15592
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-29 17:06:20 +0700

    feat(problem-3169/cpp): solve by sorting meeting first
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3169/solution.cpp
+++ old-problems/3169/solution.cpp
@@ -1,8 +1,19 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int countDays(int days, std::vector<std::vector<int>>& meetings) {
-    return days - meetings.size();
+    std::sort(meetings.begin(), meetings.end());
+
+    int totalDay{0}, day{1};
+    for (const auto& meeting : meetings) {
+      if (meeting[0] > day) totalDay += meeting[0] - day;
+      if (meeting[1] >= day) day = meeting[1] + 1;
+    }
+
+    if (days >= day) totalDay += days - day + 1;
+
+    return totalDay;
   }
 };
