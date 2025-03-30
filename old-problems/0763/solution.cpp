df8744304e31da3909f93fbe21233db7b4b88e83
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-30 23:17:57 +0700

    feat(problem-763/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0763/solution.cpp
+++ old-problems/0763/solution.cpp
@@ -4,7 +4,27 @@
 class Solution {
  public:
   std::vector<int> partitionLabels(std::string s) {
-    (void)s;
-    return {};
+    int allFreqs[26]{0};
+    for (const auto c : s) ++allFreqs[c - 'a'];
+
+    int freqs[26]{0};
+    int missing{0};
+
+    std::vector<int> output{};
+    for (std::size_t l{0}, r{0}; r < s.size(); ++r) {
+      ++freqs[s[r] - 'a'];
+      if (freqs[s[r] - 'a'] == allFreqs[s[r] - 'a']) {
+        if (freqs[s[r] - 'a'] != 1) --missing;
+      } else {
+        if (freqs[s[r] - 'a'] == 1) ++missing;
+      }
+
+      if (missing == 0) {
+        output.push_back(r - l + 1);
+        l = r + 1;
+      }
+    }
+
+    return output;
   }
 };
