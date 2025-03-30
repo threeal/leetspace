af8e2e00b28cf6f64debe18957be61e23f1c442c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-30 23:05:49 +0700

    feat(problem-3169/c): solve by sorting meeting first
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3169/c/solution.c
+++ old-problems/3169/c/solution.c
@@ -1,4 +1,24 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  int* aa = *(int**)a;
+  int* bb = *(int**)b;
+  return aa[0] == bb[0] ? aa[1] - bb[1] : aa[0] - bb[0];
+}
+
 int countDays(
     int days, int** meetings, int meetingsSize, int* meetingsColSize) {
-  return days + meetings[meetingsSize - 1][meetingsColSize[meetingsSize - 1] - 1];
+  (void)meetingsColSize;
+
+  qsort(meetings, meetingsSize, sizeof(int*), compare);
+
+  int totalDay = 0, day = 1;
+  for (int i = 0; i < meetingsSize; ++i) {
+    if (meetings[i][0] > day) totalDay += meetings[i][0] - day;
+    if (meetings[i][1] >= day) day = meetings[i][1] + 1;
+  }
+
+  if (days >= day) totalDay += days - day + 1;
+
+  return totalDay;
 }
