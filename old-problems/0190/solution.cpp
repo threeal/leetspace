d42ae0a9f4ffcefc38886895031b04d181e50c8b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-31 15:02:14 +0700

    feat(problem-190/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0190/solution.cpp
+++ old-problems/0190/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   std::uint32_t reverseBits(std::uint32_t n) {
-    return n;
+    std::uint32_t out{0};
+    for (int i{0}; i < 32; ++i) {
+      if ((n & (1 << i)) != 0) out |= 1 << (31 - i);
+    }
+    return out;
   }
 };
