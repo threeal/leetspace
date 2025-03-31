60a1159bf56e98343a47fa8e2807f8709e75e7b2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-31 14:45:06 +0700

    feat(problem-2551/cpp): solve by counting the weights of adjacent marbles
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2551/solution.cpp
+++ old-problems/2551/solution.cpp
@@ -1,8 +1,18 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   long long putMarbles(std::vector<int>& weights, int k) {
-    return weights.front() + k;
+    for (std::size_t i{1}; i < weights.size(); ++i) {
+      weights[i - 1] += weights[i];
+    }
+
+    weights.pop_back();
+    std::sort(weights.begin(), weights.end());
+
+    return std::accumulate(weights.end() - k + 1, weights.end(), 0ll) -
+        std::accumulate(weights.begin(), weights.begin() + k - 1, 0ll);
   }
 };
