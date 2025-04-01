6c16932a1f043b8344d6a1398069405d82f3d534
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-01 15:00:22 +0700

    feat(problem-2140/cpp): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2140/solution.cpp
+++ old-problems/2140/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   long long mostPoints(std::vector<std::vector<int>>& questions) {
-    return questions.front().front();
+    std::vector<long long> points(questions.size());
+    points.back() = questions.back()[0];
+
+    for (std::size_t i{questions.size() - 1}; i > 0; --i) {
+      points[i - 1] = questions[i - 1][0];
+      if (i + questions[i - 1][1] < questions.size()) {
+        points[i - 1] += points[i + questions[i - 1][1]];
+      }
+      if (points[i] > points[i - 1]) points[i - 1] = points[i];
+    }
+
+    return points.front();
   }
 };
