d47e6d3886e30935ec8d890e1458b3998d8ab14d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-02 20:48:04 +0700

    feat(problem-2873/cpp): solve by keep track of max diff and val
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2873/solution.cpp
+++ old-problems/2873/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   long long maximumTripletValue(std::vector<int>& nums) {
-    return nums.front();
+    long long maxTriplet{0}, maxDiff{0}, maxVal{0};
+    for (const auto num : nums) {
+      if (maxDiff * num > maxTriplet) maxTriplet = maxDiff * num;
+      if (maxVal - num > maxDiff) maxDiff = maxVal - num;
+      if (num > maxVal) maxVal = num;
+    }
+    return maxTriplet;
   }
 };
