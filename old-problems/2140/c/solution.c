7aeaeabca7168d648f8c2686ee14fcad5dcd580d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-03 16:20:27 +0700

    feat(problem-2140/c): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2140/c/solution.c
+++ old-problems/2140/c/solution.c
@@ -1,5 +1,17 @@
 long long mostPoints(
     int** questions, int questionsSize, int* questionsColSize) {
   (void)questionsColSize;
-  return questions[questionsSize - 1][1];
+
+  static long long points[100001];
+  points[questionsSize - 1] = questions[questionsSize - 1][0];
+
+  for (int i = questionsSize - 1; i > 0; --i) {
+    points[i - 1] = questions[i - 1][0];
+    if (i + questions[i - 1][1] < questionsSize) {
+      points[i - 1] += points[i + questions[i - 1][1]];
+    }
+    if (points[i] > points[i - 1]) points[i - 1] = points[i];
+  }
+
+  return points[0];
 }
