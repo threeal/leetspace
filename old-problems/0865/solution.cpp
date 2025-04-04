7e199f56c60eb808303b23fc111b900950d3382f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-04 22:29:06 +0700

    feat(problem-865/cpp): solve by using recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0865/solution.cpp
+++ old-problems/0865/solution.cpp
@@ -1,6 +1,34 @@
+#include <tuple>
+
 class Solution {
  public:
   TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-    return root;
+    return std::get<1>(explore(root));
+  }
+
+ private:
+  static std::tuple<int, TreeNode*> explore(TreeNode* node) {
+    if (node->left != nullptr) {
+      const auto [ldepth, lnode] = explore(node->left);
+      if (node->right != nullptr) {
+        const auto& [rdepth, rnode] = explore(node->right);
+        if (ldepth == rdepth) {
+          return {ldepth + 1, node};
+        } else if (ldepth > rdepth) {
+          return {ldepth + 1, lnode};
+        } else {
+          return {rdepth + 1, rnode};
+        }
+      } else {
+        return {ldepth + 1, lnode};
+      }
+    } else {
+      if (node->right != nullptr) {
+        const auto [rdepth, rnode] = explore(node->right);
+        return {rdepth + 1, rnode};
+      } else {
+        return {0, node};
+      }
+    }
   }
 };
