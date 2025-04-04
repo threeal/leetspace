fa9c447cdd8f683a23941bd46461fe5c2563c07b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-04 23:04:28 +0700

    feat(problem-367/cpp): solve by using binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0367/solution.cpp
+++ old-problems/0367/solution.cpp
@@ -1,6 +1,15 @@
 class Solution {
  public:
   bool isPerfectSquare(int num) {
-    return num == 0;
+    int low{1}, high{num};
+    while (low < high) {
+      const int mid{low + (high - low) / 2};
+      if (num / mid > mid) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+    return num / low == low && num % low == 0;
   }
 };
