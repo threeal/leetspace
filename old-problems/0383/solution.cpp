fdf1c4131441a91a3ac1ac37f72e996856af5751
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-05 19:28:21 +0700

    feat(problem-383/cpp): solve by comparing frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0383/solution.cpp
+++ old-problems/0383/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   bool canConstruct(std::string ransomNote, std::string magazine) {
-    return ransomNote == magazine;
+    int freqs[26]{0};
+    for (const auto c : ransomNote) ++freqs[c - 'a'];
+    for (const auto c : magazine) --freqs[c - 'a'];
+
+    for (int i{0}; i < 26; ++i) {
+      if (freqs[i] > 0) return false;
+    }
+
+    return true;
   }
 };
