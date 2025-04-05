ea26e24822e69c0420f3b884fcb59d019993c084
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-05 15:46:11 +0700

    feat(problem-367/c): solve by using binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0367/c/solution.c
+++ old-problems/0367/c/solution.c
@@ -1,5 +1,14 @@
 #include <stdbool.h>
 
 bool isPerfectSquare(int num) {
-  return num == 0;
+  int low = 1, high = num;
+  while (low < high) {
+    const int mid = low + (high - low) / 2;
+    if (num / mid > mid) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+  }
+  return num / low == low && num % low == 0;
 }
