1d9bd67d0e856d8365fd583f0b34677e53f2e4c9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-05 15:31:42 +0700

    feat(problem-338/cpp): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0338/solution.cpp
+++ old-problems/0338/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   std::vector<int> countBits(int n) {
-    return std::vector(n, 0);
+    std::vector<int> counts(n + 1);
+    counts[0] = 0;
+    for (int i{1}, e{1}; i <= n; ++i) {
+      if (i == e * 2) e = i;
+      counts[i] = counts[i - e] + 1;
+    }
+    return counts;
   }
 };
