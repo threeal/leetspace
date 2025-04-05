a8bd232a8a49bd6e00be2ec0c9bd832eb020bb29
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-05 15:12:39 +0700

    feat(problem-290/cpp): solve by mapping pattern to word in an array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0290/solution.cpp
+++ old-problems/0290/solution.cpp
@@ -1,8 +1,31 @@
 #include <string>
+#include <string_view>
+#include <unordered_set>
 
 class Solution {
  public:
   bool wordPattern(std::string pattern, std::string s) {
-    return pattern == s;
+    std::string_view sentences(s);
+    std::string_view words[26]{};
+    std::unordered_set<std::string_view> exists{};
+
+    std::size_t i{0}, r{0};
+    while (i < pattern.size() && r < sentences.size()) {
+      const std::size_t l{r};
+      while (r < sentences.size() && sentences[r] != ' ') ++r;
+
+      const auto word = sentences.substr(l, r - l);
+      if (words[pattern[i] - 'a'].empty()) {
+        if (exists.contains(word)) return false;
+        words[pattern[i] - 'a'] = word;
+        exists.insert(word);
+      } else if (word != words[pattern[i] - 'a']) {
+        return false;
+      }
+
+      ++i, ++r;
+    }
+
+    return i == pattern.size() && r == sentences.size() + 1;
   }
 };
