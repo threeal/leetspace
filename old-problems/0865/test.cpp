6ce4303593571e0e07b5504928ab54d3dfc84ef9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-06 09:53:05 +0700

    feat: solve problem 865. Smallest Subtree with all the Deepest Nodes (#2943)
    * test: add test for problem 865. Smallest Subtree with all the Deepest Nodes
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(problem-865/cpp): solve by using recursive function
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0865/test.cpp
@@ -0,0 +1,88 @@
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+// clang-format off
+#include "solution.cpp"
+// clang-format on
+
+#include <catch2/catch_test_macros.hpp>
+#include <optional>
+#include <queue>
+#include <vector>
+
+const auto null = std::nullopt;
+
+TreeNode* to_tree(const std::vector<std::optional<int>>& list) {
+  if (list.empty()) return nullptr;
+
+  auto root = new TreeNode(*list[0]);
+
+  std::queue<TreeNode**> children{};
+  children.push(&(root->left));
+  children.push(&(root->right));
+
+  for (std::size_t i{1}; i < list.size(); ++i) {
+    if (list[i]) {
+      auto node = new TreeNode(*list[i]);
+      *children.front() = node;
+      children.push(&(node->left));
+      children.push(&(node->right));
+    }
+    children.pop();
+  }
+
+  return root;
+}
+
+std::vector<std::optional<int>> from_tree(TreeNode* root) {
+  if (root == nullptr) return {};
+  std::vector<std::optional<int>> list{};
+
+  std::queue<TreeNode*> nodes{};
+  nodes.push(root);
+
+  while (!nodes.empty()) {
+    if (nodes.front() == nullptr) {
+      list.push_back(null);
+    } else {
+      list.push_back(nodes.front()->val);
+      nodes.push(nodes.front()->left);
+      nodes.push(nodes.front()->right);
+    }
+    nodes.pop();
+  }
+
+  while (!list.back()) list.pop_back();
+  return list;
+}
+
+TEST_CASE("865. Smallest Subtree with all the Deepest Nodes") {
+  SECTION("Example 1") {
+    auto root = to_tree({3, 5, 1, 6, 2, 0, 8, null, null, 7, 4});
+    auto actual = from_tree(Solution{}.subtreeWithAllDeepest(root));
+    std::vector<std::optional<int>> expected{2, 7, 4};
+    REQUIRE(actual == expected);
+  }
+
+  SECTION("Example 2") {
+    auto root = to_tree({1});
+    auto actual = from_tree(Solution{}.subtreeWithAllDeepest(root));
+    std::vector<std::optional<int>> expected{1};
+    REQUIRE(actual == expected);
+  }
+
+  SECTION("Example 3") {
+    auto root = to_tree({0, 1, 3, null, 2});
+    auto actual = from_tree(Solution{}.subtreeWithAllDeepest(root));
+    std::vector<std::optional<int>> expected{2};
+    REQUIRE(actual == expected);
+  }
+}
