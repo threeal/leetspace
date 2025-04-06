9ba939cc5f9cc8e4ebe31db8e70e8c3d76a16d23
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-06 15:51:31 +0700

    feat(problem-405/cpp): solve by getting each 4-bits
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0405/solution.cpp
+++ old-problems/0405/solution.cpp
@@ -3,7 +3,17 @@
 class Solution {
  public:
   std::string toHex(int num) {
-    (void)num;
-    return "";
+    if (num == 0) return "0";
+
+    int n{28};
+    while (num >> n == 0) n -= 4;
+
+    std::string hex(1 + n / 4, '0');
+    for (int i{0}; i <= n; i += 4) {
+      const int val{(num >> i) & 15};
+      hex[(n - i) / 4] = val >= 10 ? 'a' + val - 10 : '0' + val;
+    }
+
+    return hex;
   }
 };
