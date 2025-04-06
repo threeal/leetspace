bd876a7df2d06d5b0765c92811646429fd85e41f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-06 21:54:17 +0700

    feat(problem-2780/cpp): solve by using Boyer-Moore algorithm
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2780/solution.cpp
+++ old-problems/2780/solution.cpp
@@ -3,6 +3,35 @@
 class Solution {
  public:
   int minimumIndex(std::vector<int>& nums) {
-    return nums.size();
+    int majority{}, count{0};
+    for (const auto num : nums) {
+      if (count == 0) {
+        majority = num;
+        ++count;
+      } else if (num == majority) {
+        ++count;
+      } else {
+        --count;
+      }
+    }
+
+    std::size_t majorityCount{0};
+    for (const auto num : nums) {
+      if (num == majority) ++majorityCount;
+    }
+
+    std::size_t leftCount{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      if (nums[i] == majority) {
+        ++leftCount;
+      }
+
+      if (2 * leftCount > i + 1 &&
+          2 * (majorityCount - leftCount) > nums.size() - i - 1) {
+        return i;
+      }
+    }
+
+    return -1;
   }
 };
