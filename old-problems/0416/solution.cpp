bb9d2c207a468da87e91424ebd3a7b52907b9a22
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-07 09:24:24 +0700

    feat(problem-416/cpp): solve by using a dynamic programming approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0416/solution.cpp
+++ old-problems/0416/solution.cpp
@@ -1,8 +1,27 @@
+#include <numeric>
+#include <utility>
 #include <vector>
 
 class Solution {
  public:
   bool canPartition(std::vector<int>& nums) {
-    return nums.empty();
+    int sum{std::accumulate(nums.begin(), nums.end(), 0)};
+    if (sum % 2 == 1) return false;
+    sum /= 2;
+
+    std::vector<bool> prev(sum + 1);
+    prev[0] = true;
+
+    std::vector<bool> curr(sum + 1);
+    curr[0] = true;
+
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      std::swap(prev, curr);
+      for (int k{1}; k <= sum; ++k) {
+        curr[k] = prev[k] || (nums[i] <= k ? prev[k - nums[i]] : false);
+      }
+    }
+
+    return curr[sum];
   }
 };
