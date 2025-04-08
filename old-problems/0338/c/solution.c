302c07522a5e788818a9f21540608c5e95b57fe5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-08 23:06:59 +0700

    feat(problem-338/c): solve by using DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0338/c/solution.c
+++ old-problems/0338/c/solution.c
@@ -1,5 +1,12 @@
+#include <stdlib.h>
+
 int* countBits(int n, int* returnSize) {
-  (void)n;
-  *returnSize = 0;
-  return 0;
+  int* counts = malloc((n + 1) * sizeof(int));
+  counts[0] = 0;
+  for (int i = 1, e = 1; i <= n; ++i) {
+    if (i == e * 2) e = i;
+    counts[i] = counts[i - e] + 1;
+  }
+  *returnSize = n + 1;
+  return counts;
 }
