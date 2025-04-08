310da217b2b19819de7a531477c8cb31ab190184
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-08 09:51:37 +0700

    feat(problem-3396/cpp): solve by tracking duplicates in an array map
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3396/solution.cpp
+++ old-problems/3396/solution.cpp
@@ -3,6 +3,25 @@
 class Solution {
  public:
   int minimumOperations(std::vector<int>& nums) {
-    return nums.size();
+    std::vector<bool> exists(101, false);
+
+    std::size_t l{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      while (i >= l + 3 && exists[nums[i]]) {
+        exists[nums[l++]] = false;
+        exists[nums[l++]] = false;
+        exists[nums[l++]] = false;
+      }
+
+      if (exists[nums[i]]) {
+        for (std::size_t j{l}; j < i; ++j) exists[nums[j]] = false;
+        i = l + 2;
+        l += 3;
+      } else {
+        exists[nums[i]] = true;
+      }
+    }
+
+    return l / 3;
   }
 };
