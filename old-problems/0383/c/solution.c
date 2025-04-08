c8d33ef0e2d619607cc9a9f8d92ea8bc580abf6d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-08 22:48:06 +0700

    feat(problem-383/c): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0383/c/solution.c
+++ old-problems/0383/c/solution.c
@@ -1,5 +1,13 @@
 #include <stdbool.h>
 
 bool canConstruct(char* ransomNote, char* magazine) {
-  return *ransomNote == *magazine;
+  int freqs[26] = {0};
+  for (char* c = ransomNote; *c != 0; ++c) ++freqs[*c - 'a'];
+  for (char* c = magazine; *c != 0; ++c) --freqs[*c - 'a'];
+
+  for (int i = 0; i < 26; ++i) {
+    if (freqs[i] > 0) return false;
+  }
+
+  return true;
 }
