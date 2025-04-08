2ce129ba570ca143ae48d25d9a7ff378caa62700
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-08 11:27:04 +0700

    feat(problem-2780/c): solve by using Boyer-Moore algorithm
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2780/c/solution.c
+++ old-problems/2780/c/solution.c
@@ -1,3 +1,32 @@
 int minimumIndex(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int majority, count = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (count == 0) {
+      majority = nums[i];
+      ++count;
+    } else if (nums[i] == majority) {
+      ++count;
+    } else {
+      --count;
+    }
+  }
+
+  int majorityCount = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] == majority) ++majorityCount;
+  }
+
+  int leftCount = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] == majority) {
+      ++leftCount;
+    }
+
+    if (2 * leftCount > i + 1 &&
+        2 * (majorityCount - leftCount) > numsSize - i - 1) {
+      return i;
+    }
+  }
+
+  return -1;
 }
