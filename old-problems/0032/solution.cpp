6e5f121061f67509d5ff047b20eca6dd0b3e0706
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-08 15:17:56 +0700

    feat(problem-32/cpp): solve by using stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0032/solution.cpp
+++ old-problems/0032/solution.cpp
@@ -1,8 +1,26 @@
+#include <stack>
 #include <string>
 
 class Solution {
  public:
   int longestValidParentheses(std::string s) {
-    return s.size();
+    std::stack<std::size_t> stack{};
+    stack.push(0);
+
+    std::size_t longest{0};
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      if (s[i] == '(') {
+        stack.push(i + 1);
+      } else {
+        stack.pop();
+        if (stack.empty()) {
+          stack.push(i + 1);
+        } else if (i + 1 - stack.top() > longest) {
+          longest = i + 1 - stack.top();
+        }
+      }
+    }
+
+    return longest;
   }
 };
