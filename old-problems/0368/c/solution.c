1cd84b2b16c14358e6c2b0f6040d07535590a9eb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-08 11:08:16 +0700

    feat(problem-368/c): solve by chaining previous common divisors
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0368/c/solution.c
+++ old-problems/0368/c/solution.c
@@ -1,4 +1,42 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int* largestDivisibleSubset(int* nums, int numsSize, int* returnSize) {
-  *returnSize = numsSize;
-  return nums;
+  qsort(nums, numsSize, sizeof(int), compare);
+
+  int* prevOf = malloc(numsSize * sizeof(int));
+  int* countOf = malloc(numsSize * sizeof(int));
+  for (int i = 0; i < numsSize; ++i) {
+    prevOf[i] = -1;
+    countOf[i] = 1;
+  }
+
+  int candidate = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    for (int j = 0; j < i; ++j) {
+      if (nums[i] % nums[j] == 0 && countOf[j] + 1 > countOf[i]) {
+        prevOf[i] = j;
+        countOf[i] = countOf[j] + 1;
+      }
+    }
+
+    if (countOf[i] > countOf[candidate]) {
+      candidate = i;
+    }
+  }
+
+  int* output = malloc(countOf[candidate] * sizeof(int));
+  int outputSize = 0;
+  for (int i = candidate; i >= 0; i = prevOf[i]) {
+    output[outputSize++] = nums[i];
+  }
+
+  free(countOf);
+  free(prevOf);
+
+  *returnSize = outputSize;
+  return output;
 }
