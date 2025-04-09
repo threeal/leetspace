b45f68a3f31195a1279c99c9e1b542169d1371a4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-09 19:56:12 +0700

    feat(problem-3396/cpp): solve by keeping track of each number index
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3396/solution.cpp
+++ old-problems/3396/solution.cpp
@@ -3,25 +3,14 @@
 class Solution {
  public:
   int minimumOperations(std::vector<int>& nums) {
-    std::vector<bool> exists(101, false);
+    std::vector<std::size_t> indices(101, 0);
 
-    std::size_t l{0};
-    for (std::size_t i{0}; i < nums.size(); ++i) {
-      while (i >= l + 3 && exists[nums[i]]) {
-        exists[nums[l++]] = false;
-        exists[nums[l++]] = false;
-        exists[nums[l++]] = false;
-      }
-
-      if (exists[nums[i]]) {
-        for (std::size_t j{l}; j < i; ++j) exists[nums[j]] = false;
-        i = l + 2;
-        l += 3;
-      } else {
-        exists[nums[i]] = true;
-      }
+    std::size_t i{0};
+    for (std::size_t j{0}; j < nums.size(); ++j) {
+      if (indices[nums[j]] > i) i = indices[nums[j]];
+      indices[nums[j]] = j + 1;
     }
 
-    return l / 3;
+    return i % 3 == 0 ? i / 3 : i / 3 + 1;
   }
 };
