e0d2bb1d44baff71c494663afe5cf3b53448d010
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-09 10:29:55 +0700

    feat(problem-914/cpp): solve by using map and GCD
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0914/solution.cpp
+++ old-problems/0914/solution.cpp
@@ -1,8 +1,20 @@
+#include <map>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   bool hasGroupsSizeX(std::vector<int>& deck) {
-    return deck.empty();
+    std::map<int, int> freqs{};
+    for (const auto card : deck) ++freqs[card];
+
+    int prevFreq{freqs.begin()->second};
+    for (const auto [card, freq] : freqs) {
+      const int gcd{std::gcd(prevFreq, freq)};
+      if (gcd == 1) return false;
+      if (gcd < prevFreq) prevFreq = gcd;
+    }
+
+    return true;
   }
 };
