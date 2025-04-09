8bccd2906e64fd0b55976bd0dcabb83099875a7e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-09 10:07:33 +0700

    feat(problem-42/cpp): solve by using left and right pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0042/solution.cpp
+++ old-problems/0042/solution.cpp
@@ -3,6 +3,23 @@
 class Solution {
  public:
   int trap(std::vector<int>& height) {
-    return height.size();
+    int volume{0};
+    std::size_t l{0}, r{height.size() - 1};
+    while (l + 1 < r) {
+      if (height[l] <= height[r]) {
+        if (height[l] > height[l + 1]) {
+          volume += height[l] - height[l + 1];
+          height[l + 1] = height[l];
+        }
+        ++l;
+      } else {
+        if (height[r] > height[r - 1]) {
+          volume += height[r] - height[r - 1];
+          height[r - 1] = height[r];
+        }
+        --r;
+      }
+    }
+    return volume;
   }
 };
