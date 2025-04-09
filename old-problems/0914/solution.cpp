96ae36ebc4b94eb9c917ae3350f30152dbbe6a15
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-09 10:42:45 +0700

    feat(problem-914/cpp): solve by using sort and gcd
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0914/solution.cpp
+++ old-problems/0914/solution.cpp
@@ -1,18 +1,25 @@
-#include <map>
+#include <algorithm>
 #include <numeric>
 #include <vector>
 
 class Solution {
  public:
   bool hasGroupsSizeX(std::vector<int>& deck) {
-    std::map<int, int> freqs{};
-    for (const auto card : deck) ++freqs[card];
+    std::sort(deck.begin(), deck.end());
 
-    int prevFreq{freqs.begin()->second};
-    for (const auto [card, freq] : freqs) {
-      const int gcd{std::gcd(prevFreq, freq)};
-      if (gcd == 1) return false;
-      if (gcd < prevFreq) prevFreq = gcd;
+    std::size_t n{1};
+    while (n < deck.size() && deck[n - 1] == deck[n]) ++n;
+    if (n == 1) return false;
+
+    std::size_t r{n};
+    while (r < deck.size()) {
+      const std::size_t l{r};
+      ++r;
+
+      while (r < deck.size() && deck[r - 1] == deck[r]) ++r;
+
+      n = std::gcd(n, r - l);
+      if (n == 1) return false;
     }
 
     return true;
