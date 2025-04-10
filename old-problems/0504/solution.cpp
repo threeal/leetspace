3bffc47f501322ec002ec66cc7893edaa0fd847b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-10 20:54:32 +0700

    feat(problem-504/cpp): solve by repeatedly divide by 7
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0504/solution.cpp
+++ old-problems/0504/solution.cpp
@@ -1,9 +1,22 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   std::string convertToBase7(int num) {
-    (void)num;
-    return "";
+    if (num == 0) return "0";
+
+    const bool neg{num < 0};
+    if (neg) num = -num;
+
+    std::string out{};
+    while (num > 0) {
+      out += '0' + num % 7;
+      num /= 7;
+    }
+
+    if (neg) out += "-";
+    std::reverse(out.begin(), out.end());
+    return out;
   }
 };
