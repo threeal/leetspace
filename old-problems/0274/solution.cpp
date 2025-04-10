f47f84934ad602b28dbe49fc4a5bbf27d4a69b3c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-10 20:31:40 +0700

    feat(problem-274/cpp): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0274/solution.cpp
+++ old-problems/0274/solution.cpp
@@ -1,8 +1,13 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int hIndex(std::vector<int>& citations) {
-    return citations.size();
+    std::sort(citations.begin(), citations.end());
+    for (int n = citations.size(); n > 0; --n) {
+      if (citations[citations.size() - n] >= n) return n;
+    }
+    return 0;
   }
 };
