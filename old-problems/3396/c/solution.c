479082caa43c0f3c096f80802a17cb57c321d169
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-11 16:58:24 +0700

    feat(problem-3396/c): solve by keeping track of each number index
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3396/c/solution.c
+++ old-problems/3396/c/solution.c
@@ -1,3 +1,11 @@
 int minimumOperations(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int indices[101] = {0};
+
+  int i = 0;
+  for (int j = 0; j < numsSize; ++j) {
+    if (indices[nums[j]] > i) i = indices[nums[j]];
+    indices[nums[j]] = j + 1;
+  }
+
+  return i % 3 == 0 ? i / 3 : i / 3 + 1;
 }
