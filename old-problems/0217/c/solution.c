43112aa1b1d36f6e6f2435faedd36aa2561d393f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-11 22:45:54 +0700

    feat: solve problem 217. Contains Duplicate in C (#2999)
    * test(problem-217): add support to test in C
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(problem-217/c): solve by using sorting
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0217/c/solution.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
+bool containsDuplicate(int* nums, int numsSize) {
+  qsort(nums, numsSize, sizeof(int), compare);
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i] == nums[i - 1]) return true;
+  }
+  return false;
+}
