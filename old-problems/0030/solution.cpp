523b742c165346345ada49d9dce0daef9cdbfc0e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-11 10:39:41 +0700

    test: add test for problem 30. Substring with Concatenation of All Words
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0030/solution.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> findSubstring(
+      std::string s, std::vector<std::string>& words) {
+    (void)s;
+    (void)words;
+    return {};
+  }
+};
