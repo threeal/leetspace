c0bd72f51bb088c23aae75c6ec233860182dc070
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-11 22:41:12 +0700

    feat(problem-3375/c): solve by counting distinct numbers greater than k
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3375/c/solution.c
+++ old-problems/3375/c/solution.c
@@ -1,3 +1,15 @@
+#include <stdbool.h>
+
 int minOperations(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] + k;
+  bool exists[101] = {false};
+  int count = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] < k) return -1;
+    if (!exists[nums[i]]) {
+      exists[nums[i]] = true;
+      ++count;
+    }
+  }
+  if (exists[k]) --count;
+  return count;
 }
