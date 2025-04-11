b6381b4f8166f888411945e69e42a55cf92f0946
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-11 22:45:17 +0700

    feat(problem-405/c): solve by getting each 4-bits
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0405/c/solution.c
+++ old-problems/0405/c/solution.c
@@ -1,4 +1,18 @@
+#include <stdlib.h>
+
 char* toHex(int num) {
-  (void)num;
-  return "";
+  if (num == 0) return "0";
+
+  int n = 28;
+  while (num >> n == 0) n -= 4;
+
+  int hexLen = 2 + n / 4;
+  char* hex = malloc(hexLen * sizeof(char));
+  for (int i = 0; i <= n; i += 4) {
+    const int val = (num >> i) & 15;
+    hex[(n - i) / 4] = val >= 10 ? 'a' + val - 10 : '0' + val;
+  }
+
+  hex[hexLen - 1] = 0;
+  return hex;
 }
