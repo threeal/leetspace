b5fd72255c8fe7f6edeba141113971e1914ba4e2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-12 23:02:47 +0700

    feat(problem-914/c): solve by using sort and gcd
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0914/c/solution.c
+++ old-problems/0914/c/solution.c
@@ -1,5 +1,37 @@
 #include <stdbool.h>
+#include <stdlib.h>
+
+static int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
+static int findGcd(int a, int b) {
+  int tmp;
+  while (b) {
+    tmp = a % b;
+    a = b;
+    b = tmp;
+  }
+  return a < 0 ? -a : a;
+}
 
 bool hasGroupsSizeX(int* deck, int deckSize) {
-  return deck[deckSize - 1] == 0;
+  qsort(deck, deckSize, sizeof(int), compare);
+
+  int n = 1;
+  while (n < deckSize && deck[n - 1] == deck[n]) ++n;
+  if (n == 1) return false;
+
+  int r = n;
+  while (r < deckSize) {
+    const int l = r;
+    ++r;
+
+    while (r < deckSize && deck[r - 1] == deck[r]) ++r;
+
+    n = findGcd(n, r - l);
+    if (n == 1) return false;
+  }
+
+  return true;
 }
