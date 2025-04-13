a1ab44e2435c9b19f9b4789d563700c350c049bd
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-13 22:56:30 +0700

    feat(problem-504/c): solve by repeatedly divide by 7
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0504/c/solution.c
+++ old-problems/0504/c/solution.c
@@ -1,4 +1,19 @@
+#include <stdlib.h>
+
 char* convertToBase7(int num) {
-  (void)num;
-  return "";
+  if (num == 0) return "0";
+
+  const int neg = num < 0;
+  if (neg) num = -num;
+
+  char* out = malloc(11 * sizeof(char));
+  *(out += 10) = 0;
+
+  while (num > 0) {
+    *(--out) = '0' + num % 7;
+    num /= 7;
+  }
+
+  if (neg) *(--out) = '-';
+  return out;
 }
