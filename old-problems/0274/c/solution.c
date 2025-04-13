1da897d99438578075c7195401c4742c778544e5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-13 22:37:00 +0700

    feat(problem-274/c): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0274/c/solution.c
+++ old-problems/0274/c/solution.c
@@ -1,3 +1,13 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int hIndex(int* citations, int citationsSize) {
-  return citations[citationsSize - 1];
+  qsort(citations, citationsSize, sizeof(int), compare);
+  for (int n = citationsSize; n > 0; --n) {
+    if (citations[citationsSize - n] >= n) return n;
+  }
+  return 0;
 }
