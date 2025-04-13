296b771ab39af9c0aa37ae739ca9d34c219ce25b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-13 20:05:08 +0700

    feat(problem-1922/cpp): solve by following the pattern of `5 * 4 * 5 * 4 * ...`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1922/solution.cpp
+++ old-problems/1922/solution.cpp
@@ -1,6 +1,17 @@
 class Solution {
  public:
   int countGoodNumbers(long long n) {
-    return n;
+    return (pow(5, n / 2 + n % 2) * pow(4, n / 2)) % 1000000007;
+  }
+
+ private:
+  static long long pow(long long x, long long n) {
+    long long res{1};
+    while (n != 0) {
+      if (n % 2) res = (res * x) % 1000000007;
+      x = (x * x) % 1000000007;
+      n /= 2;
+    }
+    return res;
   }
 };
