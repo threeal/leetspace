202e490303d3d38ebf1a3954b662d1e7cd8273c9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-14 14:12:40 +0700

    test(problem-373): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0373/c/solution.c
@@ -0,0 +1,13 @@
+int** kSmallestPairs(
+    int* nums1, int nums1Size, int* nums2, int nums2Size, int k,
+    int* returnSize, int** returnColumnSizes) {
+  (void)nums1;
+  (void)nums1Size;
+  (void)nums2;
+  (void)nums2Size;
+  (void)k;
+
+  *returnSize = 0;
+  *returnColumnSizes = 0;
+  return 0;
+}
