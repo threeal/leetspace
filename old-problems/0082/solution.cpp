31805b60fda82e9349156de86544c284bd4380be
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-14 23:31:54 +0700

    feat(problem-82/cpp): solve by repeatedly delete duplicates
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0082/solution.cpp
+++ old-problems/0082/solution.cpp
@@ -1,6 +1,17 @@
 class Solution {
  public:
   ListNode* deleteDuplicates(ListNode* head) {
+    ListNode** node{&head};
+    while (*node != nullptr) {
+      if ((*node)->next != nullptr && (*node)->val == (*node)->next->val) {
+        const int val{(*node)->val};
+        while (*node != nullptr && (*node)->val == val) {
+          *node = (*node)->next;
+        }
+      } else {
+        node = &((*node)->next);
+      }
+    }
     return head;
   }
 };
