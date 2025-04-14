8823a1e49b16b145c49027963579ad5eb70b2e98
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-14 22:28:17 +0700

    feat(problem-60/cpp): solve by repeatedly substract `k` with factorial of `n`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0060/solution.cpp
+++ old-problems/0060/solution.cpp
@@ -1,8 +1,30 @@
+#include <cstdint>
 #include <string>
 
 class Solution {
  public:
   std::string getPermutation(int n, int k) {
-    return std::string(n, '0' + k % 10);
+    std::string permutation(n, '0');
+
+    int factorial{1};
+    for (int i{2}; i <= n; ++i) factorial *= i;
+
+    std::uint16_t exists{0};
+    for (int i{0}; i < n; ++i) {
+      factorial /= n - i;
+      for (int j{1}; j <= 9; ++j) {
+        if ((exists & (1 << j)) == 0) {
+          if (k <= factorial) {
+            exists |= 1 << j;
+            permutation[i] += j;
+            break;
+          } else {
+            k -= factorial;
+          }
+        }
+      }
+    }
+
+    return permutation;
   }
 };
