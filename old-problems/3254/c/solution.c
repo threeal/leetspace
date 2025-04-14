77830d362b08020271eaeb45969387bb78d912a8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-14 13:55:46 +0700

    feat(problem-3254/cp): solve by tracking consecutive elements
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3254/c/solution.c
+++ old-problems/3254/c/solution.c
@@ -1,5 +1,30 @@
+#include <stdlib.h>
+
 int* resultsArray(int* nums, int numsSize, int k, int* returnSize) {
-  (void)k;
-  *returnSize = numsSize;
-  return nums;
+  if (--k == 0) {
+    *returnSize = numsSize;
+    return nums;
+  }
+
+  int count = 0;
+  for (int i = 1; i < k; ++i) {
+    if (nums[i - 1] + 1 == nums[i]) {
+      ++count;
+    } else {
+      count = 0;
+    }
+  }
+
+  int* output = malloc((numsSize - k) * sizeof(int));
+  for (int i = k; i < numsSize; ++i) {
+    if (nums[i - 1] + 1 == nums[i]) {
+      output[i - k] = ++count >= k ? nums[i] : -1;
+    } else {
+      count = 0;
+      output[i - k] = -1;
+    }
+  }
+
+  *returnSize = numsSize - k;
+  return output;
 }
