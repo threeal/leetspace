d7d2b3809b3bbb46ca89206aac0153e41e11ea7c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-15 20:34:55 +0700

    feat(problem-2843/c): solve by checking numbers in range
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2843/c/solution.c
+++ old-problems/2843/c/solution.c
@@ -1,3 +1,15 @@
 int countSymmetricIntegers(int low, int high) {
-  return high - low;
+  int count = 0;
+  for (int num = low; num <= high; ++num) {
+    if (num <= 10) {
+      num = 10;
+    } else if (num <= 99) {
+      if (num / 10 == num % 10) ++count;
+    } else if (num <= 1000) {
+      num = 1000;
+    } else if (num <= 9999) {
+      if (num / 1000 + num / 100 % 10 == num / 10 % 10 + num % 10) ++count;
+    }
+  }
+  return count;
 }
