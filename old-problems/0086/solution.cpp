585fbe8322f6b1bd3041a236bcf55b1ec03bc672
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-15 20:15:29 +0700

    feat(problem-86/cpp): solve by using two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0086/solution.cpp
+++ old-problems/0086/solution.cpp
@@ -1,7 +1,20 @@
 class Solution {
  public:
   ListNode* partition(ListNode* head, int x) {
-    (void)x;
+    ListNode* headX{nullptr};
+    ListNode** tail{&head};
+    ListNode** tailX{&headX};
+    for (ListNode* node(head); node != nullptr; node = node->next) {
+      if (node->val < x) {
+        *tail = node;
+        tail = &(node->next);
+      } else {
+        *tailX = node;
+        tailX = &(node->next);
+      }
+    }
+    *tail = headX;
+    *tailX = nullptr;
     return head;
   }
 };
