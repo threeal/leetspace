7d7b0bad0a5dbde8a5c21aa31f4b819076d19415
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-16 11:44:51 +0700

    feat(problem-2537/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- problems/2537/solution.cpp
+++ problems/2537/solution.cpp
@@ -1,8 +1,18 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   long long countGood(std::vector<int>& nums, int k) {
-    return nums.size() * k;
+    long long count{0};
+    std::unordered_map<int, int> freqs{};
+    for (std::size_t l{0}, r{0}; r < nums.size(); ++r) {
+      k -= freqs[nums[r]]++;
+      while (k <= 0) {
+        count += nums.size() - r;
+        k += --freqs[nums[l++]];
+      }
+    }
+    return count;
   }
 };
