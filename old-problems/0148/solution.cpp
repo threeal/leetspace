1bb080ed38482a93688df72dd6ac1e056b67299a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-16 15:33:39 +0700

    feat(problem-148/cpp): solve by using a merge sort
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0148/solution.cpp
+++ old-problems/0148/solution.cpp
@@ -1,6 +1,35 @@
 class Solution {
  public:
   ListNode* sortList(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) return head;
+
+    ListNode* slow{head};
+    ListNode* fast{head->next};
+    while (fast != nullptr && fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+
+    ListNode* left{head};
+    ListNode* right{slow->next};
+    slow->next = nullptr;
+
+    left = sortList(left);
+    right = sortList(right);
+
+    ListNode** tail{&head};
+    while (left != nullptr && right != nullptr) {
+      if (left->val <= right->val) {
+        *tail = left;
+        left = left->next;
+      } else {
+        *tail = right;
+        right = right->next;
+      }
+      tail = &((*tail)->next);
+    }
+    *tail = left != nullptr ? left : right;
+
     return head;
   }
 };
