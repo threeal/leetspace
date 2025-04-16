0e3fb2f22bc09327c8f2ddfbf0d03f657e66db81
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-16 15:05:16 +0700

    feat(problem-3516/cpp): solve by comparing diff
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3516/solution.cpp
+++ old-problems/3516/solution.cpp
@@ -1,6 +1,8 @@
 class Solution {
  public:
   int findClosest(int x, int y, int z) {
-    return x + y + z;
+    const int xx{z >= x ? z - x : x - z};
+    const int yy{z >= y ? z - y : y - z};
+    return xx == yy ? 0 : (xx < yy ? 1 : 2);
   }
 };
