452ae6db3a013b2f651985c72521fec8fae483d3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-17 14:36:51 +0700

    feat(problem-1513/cpp): solve by counting series of subsequent `1`s
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1513/solution.cpp
+++ old-problems/1513/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int numSub(std::string s) {
-    return s.size();
+    long long num{0}, ones{0};
+    for (const char c : s) {
+      if (c == '0') {
+        num = (num + (ones * (ones + 1)) / 2) % 1000000007;
+        ones = 0;
+      } else {
+        ++ones;
+      }
+    }
+    return (num + (ones * (ones + 1)) / 2) % 1000000007;
   }
 };
