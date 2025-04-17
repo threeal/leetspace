5f6559e47c07a1a502ca713e7b4d7319489eb61e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-17 14:13:00 +0700

    test(problem-911): add test case 9
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0911/test.cpp
+++ old-problems/0911/test.cpp
@@ -17,4 +17,20 @@ TEST_CASE("911. Online Election") {
     REQUIRE(topVotedCandidate.q(24) == 0);
     REQUIRE(topVotedCandidate.q(8) == 1);
   }
+
+  SECTION("Test Case 9") {
+    std::vector<int> persons{0, 0, 0, 0, 1};
+    std::vector<int> times{0, 6, 39, 52, 75};
+    TopVotedCandidate topVotedCandidate(persons, times);
+    REQUIRE(topVotedCandidate.q(45) == 0);
+    REQUIRE(topVotedCandidate.q(49) == 0);
+    REQUIRE(topVotedCandidate.q(59) == 0);
+    REQUIRE(topVotedCandidate.q(68) == 0);
+    REQUIRE(topVotedCandidate.q(42) == 0);
+    REQUIRE(topVotedCandidate.q(37) == 0);
+    REQUIRE(topVotedCandidate.q(99) == 0);
+    REQUIRE(topVotedCandidate.q(26) == 0);
+    REQUIRE(topVotedCandidate.q(78) == 0);
+    REQUIRE(topVotedCandidate.q(43) == 0);
+  }
 }
