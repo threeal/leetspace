5d00ad6f932282570ed2dc99d6ec0bb36d9cfa47
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-17 17:05:31 +0700

    feat(problem-1513/c): solve by counting series of subsequent `1`s
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1513/c/solution.c
+++ old-problems/1513/c/solution.c
@@ -1,3 +1,12 @@
 int numSub(char* s) {
-  return *s;
+  long long num = 0, ones = 0;
+  for (char* c = s; *c != 0; ++c) {
+    if (*c == '0') {
+      num = (num + (ones * (ones + 1)) / 2) % 1000000007;
+      ones = 0;
+    } else {
+      ++ones;
+    }
+  }
+  return (num + (ones * (ones + 1)) / 2) % 1000000007;
 }
