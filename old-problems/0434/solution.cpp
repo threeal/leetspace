2330abfe81d04a266532d2319a83d269ae58d689
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-19 22:50:16 +0700

    feat(problem-434/cpp): solve by using while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0434/solution.cpp
+++ old-problems/0434/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int countSegments(std::string s) {
-    return s.size();
+    int count{0};
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      while (i < s.size() && s[i] == ' ') ++i;
+      if (i < s.size()) {
+        ++count;
+        while (i < s.size() && s[i] != ' ') ++i;
+      }
+    }
+    return count;
   }
 };
