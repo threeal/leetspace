f89fddd08278768cf07f44212f2549d1115fe792
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-19 20:24:39 +0700

    feat(problem-2179/c): solve by using Fenwick tree
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2179/c/solution.c
+++ old-problems/2179/c/solution.c
@@ -1,7 +1,44 @@
+#include <stdlib.h>
+
+static void updateTree(int* tree, int treeSize, int i, int delta);
+static int queryTree(int* tree, int i);
+
 long long goodTriplets(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-  (void)nums1;
-  (void)nums1Size;
-  (void)nums2;
-  (void)nums2Size;
-  return 0;
+  int* indices2 = malloc(nums2Size * sizeof(int));
+  for (int i2 = 0; i2 < nums2Size; ++i2) indices2[nums2[i2]] = i2;
+
+  const int leftCountsSize = nums1Size + 1;
+  int* leftCounts = malloc(leftCountsSize * sizeof(int));
+  for (int i = 0; i < leftCountsSize; ++i) leftCounts[i] = 0;
+
+  long long counts = 0;
+  for (int i1 = 0; i1 < nums1Size; ++i1) {
+    const int i2 = indices2[nums1[i1]];
+
+    const long long leftCount = queryTree(leftCounts, i2);
+    updateTree(leftCounts, leftCountsSize, i2, 1);
+
+    const long long rightCount = nums2Size - 1 - i2 - (i1 - leftCount);
+    counts += leftCount * rightCount;
+  }
+
+  return counts;
+}
+
+static void updateTree(int* tree, int treeSize, int i, int delta) {
+  ++i;
+  while (i < treeSize) {
+    tree[i] += delta;
+    i += i & -i;
+  }
+}
+
+static int queryTree(int* tree, int i) {
+  ++i;
+  int sum = 0;
+  while (i > 0) {
+    sum += tree[i];
+    i -= i & -i;
+  }
+  return sum;
 }
