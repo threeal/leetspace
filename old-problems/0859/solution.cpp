20fae8c095cbbbe2500f8008cc2bbeae9221deb6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-19 23:38:03 +0700

    feat(problem-859/cpp): solve by checking mismatched chars
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0859/solution.cpp
+++ old-problems/0859/solution.cpp
@@ -3,6 +3,30 @@
 class Solution {
  public:
   bool buddyStrings(std::string s, std::string goal) {
-    return s == goal;
+    if (s.size() != goal.size()) return false;
+
+    int freqs[26]{0};
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      if (s[i] == goal[i]) {
+        ++freqs[s[i] - 'a'];
+        continue;
+      }
+
+      for (std::size_t j{i + 1}; j < s.size(); ++j) {
+        if (s[j] == goal[j]) continue;
+        if (s[i] != goal[j] || s[j] != goal[i]) return false;
+        for (std::size_t k{j + 1}; k < s.size(); ++k) {
+          if (s[k] != goal[k]) return false;
+        }
+        return true;
+      }
+      return false;
+    }
+
+    for (int i{0}; i < 26; ++i) {
+      if (freqs[i] >= 2) return true;
+    }
+
+    return false;
   }
 };
