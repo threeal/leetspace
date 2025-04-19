6b7fc5146160831aecfbfd99417006bdf490ac4b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-19 20:20:48 +0700

    feat(problem-84/c): solve by using monotonic stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0084/c/solution.c
+++ old-problems/0084/c/solution.c
@@ -1,3 +1,33 @@
 int largestRectangleArea(int* heights, int heightsSize) {
-  return heights[heightsSize - 1];
+  int indices[100000];
+  int indicesTop = -1;
+
+  int lefts[100000];
+  for (int i = 0; i < heightsSize; ++i) {
+    while (indicesTop >= 0 && heights[indices[indicesTop]] >= heights[i]) {
+      --indicesTop;
+    }
+
+    lefts[i] = indicesTop < 0 ? 0 : indices[indicesTop] + 1;
+    indices[++indicesTop] = i;
+  }
+
+  indicesTop = -1;
+  int rights[100000];
+  for (int i = heightsSize - 1; i >= 0; --i) {
+    while (indicesTop >= 0 && heights[indices[indicesTop]] >= heights[i]) {
+      --indicesTop;
+    }
+
+    rights[i] = indicesTop < 0 ? heightsSize - 1 : indices[indicesTop] - 1;
+    indices[++indicesTop] = i;
+  }
+
+  int largestArea = 0;
+  for (int i = 0; i < heightsSize; ++i) {
+    const int area = heights[i] * (rights[i] - lefts[i] + 1);
+    if (area > largestArea) largestArea = area;
+  }
+
+  return largestArea;
 }
