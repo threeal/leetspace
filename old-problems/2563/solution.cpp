dd44443ff4a1c98d516d38e294b88f6bcb58a8f9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-19 23:27:32 +0800

    feat(problem-2563/cpp): solve by using two pointers (#3119)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2563/solution.cpp
+++ old-problems/2563/solution.cpp
@@ -7,13 +7,23 @@ class Solution {
     std::sort(nums.begin(), nums.end());
 
     long long count{0};
-    for (auto it = nums.begin(); it != nums.end(); ++it) {
-      const auto left = std::lower_bound(
-          std::next(it), nums.end(), lower - *it);
+    std::size_t l{0}, r{nums.size() - 1};
+    while (l < r) {
+      if (nums[l] + nums[r] > upper) {
+        --r;
+      } else {
+        count += r - l;
+        ++l;
+      }
+    }
 
-      if (left != nums.end()) {
-        const auto right = std::upper_bound(left, nums.end(), upper - *it);
-        count += std::distance(left, right);
+    l = 0, r = nums.size() - 1;
+    while (l < r) {
+      if (nums[l] + nums[r] >= lower) {
+        --r;
+      } else {
+        count -= r - l;
+        ++l;
       }
     }
 
