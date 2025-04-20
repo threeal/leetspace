663353f50494445b63ce718e82f73ffd7d699598
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-20 20:24:44 +0700

    feat(problem-781/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0781/solution.cpp
+++ old-problems/0781/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int numRabbits(std::vector<int>& answers) {
-    return answers.size();
+    int freqs[1000]{0};
+    for (const int answer : answers) ++freqs[answer];
+
+    int num{0};
+    for (int i{0}; i < 1000; ++i) {
+      num += (i + 1) * ((freqs[i] + i) / (i + 1));
+    }
+
+    return num;
   }
 };
