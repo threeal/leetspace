be38d14361b72574fd32ea60f8db39744fb18ff1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-20 21:54:24 +0700

    feat(problem-434/c): solve by using while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0434/c/solution.c
+++ old-problems/0434/c/solution.c
@@ -1,3 +1,11 @@
 int countSegments(char* s) {
-  return *s;
+  int count = 0, i = 0;
+  while (s[i] != 0) {
+    while (s[i] == ' ') ++i;
+    if (s[i] != 0) {
+      ++count;
+      while (s[i] != 0 && s[i] != ' ') ++i;
+    }
+  }
+  return count;
 }
