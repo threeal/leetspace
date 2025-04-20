23184cdeffb4797d660510dcb2e6eb5da838fb6c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-20 21:37:55 +0700

    feat(problem-459/cpp): solve by greedily check all possible substring length
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0459/solution.cpp
+++ old-problems/0459/solution.cpp
@@ -3,6 +3,23 @@
 class Solution {
  public:
   bool repeatedSubstringPattern(std::string s) {
-    return s.empty();
+    for (std::size_t n{1}; n * 2 <= s.size(); ++n) {
+      if (s.size() % n != 0) continue;
+
+      bool valid{true};
+      for (std::size_t i{n}; i < s.size(); i += n) {
+        for (std::size_t j{0}; j < n; ++j) {
+          if (s[i + j] != s[j]) {
+            valid = false;
+            i = s.size();
+            j = n;
+          }
+        }
+      }
+
+      if (valid) return true;
+    }
+
+    return false;
   }
 };
