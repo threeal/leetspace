6290cbdb28c8926128a185250f87bc00162a8a61
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-20 22:02:53 +0700

    feat(problem-448/c): solve by marking the given array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0448/c/solution.c
+++ old-problems/0448/c/solution.c
@@ -1,4 +1,14 @@
 int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
-  *returnSize = numsSize;
+  for (int i = 0; i < numsSize; ++i) {
+    const int idx = nums[i] > 0 ? nums[i] - 1 : -nums[i] - 1;
+    if (nums[idx] > 0) nums[idx] = -nums[idx];
+  }
+
+  int outSize = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] > 0) nums[outSize++] = i + 1;
+  }
+
+  *returnSize = outSize;
   return nums;
 }
