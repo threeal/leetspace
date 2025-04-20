76572b86d2e58dce37e6e34b9748b44c038811ac
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-20 22:14:22 +0700

    feat(problem-509/cpp): solve by using a while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0509/solution.cpp
+++ old-problems/0509/solution.cpp
@@ -1,6 +1,14 @@
+#include <utility>
+
 class Solution {
  public:
   int fib(int n) {
-    return n;
+    if (n == 0) return 0;
+    int a{0}, b{1};
+    while (--n != 0) {
+      a += b;
+      std::swap(a, b);
+    }
+    return b;
   }
 };
