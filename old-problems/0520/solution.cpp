3ee2c3df25ded7845330f510c6165bb959e555a7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-21 22:31:52 +0700

    feat(problem-520/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0520/solution.cpp
+++ old-problems/0520/solution.cpp
@@ -3,6 +3,21 @@
 class Solution {
  public:
   bool detectCapitalUse(std::string word) {
-    return word.empty();
+    if (word[0] >= 'a') {
+      for (std::size_t i{1}; i < word.size(); ++i) {
+        if (word[i] < 'a') return false;
+      }
+    } else if (word.size() > 1) {
+      if (word[1] >= 'a') {
+        for (std::size_t i{2}; i < word.size(); ++i) {
+          if (word[i] < 'a') return false;
+        }
+      } else {
+        for (std::size_t i{2}; i < word.size(); ++i) {
+          if (word[i] >= 'a') return false;
+        }
+      }
+    }
+    return true;
   }
 };
