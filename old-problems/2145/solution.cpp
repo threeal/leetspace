b0939d7edadba9e227e1cb2c957852cbfeecec23
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-21 09:18:12 +0700

    feat(problem-2145/cpp): solve by keeping track of min and max values
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2145/solution.cpp
+++ old-problems/2145/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   int numberOfArrays(std::vector<int>& differences, int lower, int upper) {
-    return differences.size() + upper - lower;
+    long long val{0}, min{0}, max{0};
+    for (const int diff : differences) {
+      val += diff;
+      if (val > max) max = val;
+      if (val < min) min = val;
+    }
+    return upper - lower >= max - min ? 1 + (upper - lower) - (max - min) : 0;
   }
 };
