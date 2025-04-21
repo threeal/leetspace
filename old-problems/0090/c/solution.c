781149e440f99f0d817488bbff79fa99802df0c2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-21 19:57:49 +0700

    test(problem-90): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0090/c/solution.c
@@ -0,0 +1,8 @@
+int** subsetsWithDup(
+    int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+  (void)nums;
+  (void)numsSize;
+  *returnSize = 0;
+  *returnColumnSizes = 0;
+  return 0;
+}
