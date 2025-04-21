ba13a82b809bab4f93e7a27ce119ec1181043a43
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-21 12:47:40 +0700

    feat(problem-295/cpp): solve by using priority queues
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0295/solution.cpp
+++ old-problems/0295/solution.cpp
@@ -1,12 +1,33 @@
+#include <queue>
+#include <vector>
+
 class MedianFinder {
+ private:
+  std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+  std::priority_queue<int> maxHeap;
+
  public:
-  MedianFinder() {}
+  MedianFinder() : minHeap{}, maxHeap{} {}
 
   void addNum(int num) {
-    (void)num;
+    if (maxHeap.empty() || num <= maxHeap.top()) {
+      maxHeap.push(num);
+      if (maxHeap.size() > minHeap.size() + 1) {
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+      }
+    } else {
+      minHeap.push(num);
+      if (minHeap.size() > maxHeap.size()) {
+        maxHeap.push(minHeap.top());
+        minHeap.pop();
+      }
+    }
   }
 
   double findMedian() {
-    return 0;
+    return maxHeap.size() == minHeap.size()
+        ? (maxHeap.top() + minHeap.top()) / 2.0
+        : maxHeap.top();
   }
 };
