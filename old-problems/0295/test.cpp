f39e91292fc96e5cdf975267c357139def93d2f0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-21 12:47:23 +0700

    test(problem-295): add test case 9
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0295/test.cpp
+++ old-problems/0295/test.cpp
@@ -11,4 +11,18 @@ TEST_CASE("295. Find Median from Data Stream") {
     finder.addNum(3);
     REQUIRE(finder.findMedian() == 2.0);
   }
+
+  SECTION("Test Case 9") {
+    MedianFinder finder;
+    finder.addNum(-1);
+    REQUIRE(finder.findMedian() == -1.0);
+    finder.addNum(-2);
+    REQUIRE(finder.findMedian() == -1.5);
+    finder.addNum(-3);
+    REQUIRE(finder.findMedian() == -2.0);
+    finder.addNum(-4);
+    REQUIRE(finder.findMedian() == -2.5);
+    finder.addNum(-5);
+    REQUIRE(finder.findMedian() == -3.0);
+  }
 }
