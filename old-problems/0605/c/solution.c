43282ac32a8e125c2ee572b4e8451b71e52f162d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-22 16:52:42 +0700

    feat(problem-605/c): solve by counting length of subsequent zeros
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0605/c/solution.c
+++ old-problems/0605/c/solution.c
@@ -1,5 +1,30 @@
 #include <stdbool.h>
 
 bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
-  return flowerbed[flowerbedSize - 1] * n;
+  int i = 0;
+  while (i < flowerbedSize && flowerbed[i] == 0) ++i;
+  if (i == flowerbedSize) {
+    n -= (i + 1) / 2;
+    return n <= 0;
+  };
+
+  n -= i / 2;
+  if (n <= 0) return true;
+
+  int zeros = 0;
+  while (i < flowerbedSize) {
+    if (flowerbed[i] == 0) {
+      ++zeros;
+    } else {
+      if (zeros > 2) {
+        n -= (zeros - 1) / 2;
+        if (n <= 0) return true;
+      }
+      zeros = 0;
+    }
+    ++i;
+  }
+
+  n -= zeros / 2;
+  return n <= 0;
 }
