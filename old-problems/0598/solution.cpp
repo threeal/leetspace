a078f126627e712fd4f67ab3550bc5a5ff7c026b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-22 17:10:31 +0700

    feat(problem-598/cpp): solve by finding the minimum range
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0598/solution.cpp
+++ old-problems/0598/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   int maxCount(int m, int n, std::vector<std::vector<int>>& ops) {
-    return m * n - ops.size();
+    for (const auto& op : ops) {
+      if (op[0] < m) m = op[0];
+      if (op[1] < n) n = op[1];
+    }
+    return m * n;
   }
 };
