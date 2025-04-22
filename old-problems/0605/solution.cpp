d077eca06a98becc9f2ef8ea8af1126d84746e93
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-22 15:14:28 +0700

    feat(problem-605/cpp): solve by counting length of subsequent zeros
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0605/solution.cpp
+++ old-problems/0605/solution.cpp
@@ -3,6 +3,31 @@
 class Solution {
  public:
   bool canPlaceFlowers(std::vector<int>& flowerbed, int n) {
-    return flowerbed.empty() || n == 0;
+    std::size_t i{0};
+    while (i < flowerbed.size() && flowerbed[i] == 0) ++i;
+    if (i == flowerbed.size()) {
+      n -= (i + 1) / 2;
+      return n <= 0;
+    };
+
+    n -= i / 2;
+    if (n <= 0) return true;
+
+    int zeros{0};
+    while (i < flowerbed.size()) {
+      if (flowerbed[i] == 0) {
+        ++zeros;
+      } else {
+        if (zeros > 2) {
+          n -= (zeros - 1) / 2;
+          if (n <= 0) return true;
+        }
+        zeros = 0;
+      }
+      ++i;
+    }
+
+    n -= zeros / 2;
+    return n <= 0;
   }
 };
