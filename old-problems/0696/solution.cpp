76c8863d421070cdf9e6261682d569d55fd8e079
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-22 19:49:16 +0700

    feat(problem-696/cpp): solve by counting consecutive 0s and 1s
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0696/solution.cpp
+++ old-problems/0696/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   int countBinarySubstrings(std::string s) {
-    return s.size();
+    int total{0}, zeros{0}, ones{0};
+    std::size_t i{0};
+    while (i < s.size()) {
+      zeros = 0;
+      while (i < s.size() && s[i] == '0') ++i, ++zeros;
+      total += zeros <= ones ? zeros : ones;
+
+      ones = 0;
+      while (i < s.size() && s[i] == '1') ++i, ++ones;
+      total += zeros <= ones ? zeros : ones;
+    }
+    return total;
   }
 };
