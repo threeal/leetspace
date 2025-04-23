e1d449c99e8a5969ef343d903f963db2eb1cb3da
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-23 19:29:38 +0800

    feat: solve problem 357. Count Numbers with Unique Digits (#3190)
    * test: add test for problem 357. Count Numbers with Unique Digits
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(problem-357/cpp): solve by precompute the results
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0357/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+ public:
+  int countNumbersWithUniqueDigits(int n) {
+    static const int counts[]{
+        1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851};
+    return counts[n];
+  }
+};
