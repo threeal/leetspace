e58b094858a0c5bda3fa119d2a591d4f4d33adbc
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-23 12:34:09 +0700

    feat(problem-781/c): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0781/c/solution.c
+++ old-problems/0781/c/solution.c
@@ -1,3 +1,11 @@
 int numRabbits(int* answers, int answersSize) {
-  return answers[answersSize - 1];
+  int freqs[1000] = {0};
+  for (int i = 0; i < answersSize; ++i) ++freqs[answers[i]];
+
+  int num = 0;
+  for (int i = 0; i < 1000; ++i) {
+    num += (i + 1) * ((freqs[i] + i) / (i + 1));
+  }
+
+  return num;
 }
