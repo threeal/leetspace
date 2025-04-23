637524fb8cd63ed62041ad8c8aa02356bd34e08b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-23 14:33:47 +0700

    feat(problem-1399/c): solve by counting the same sum of digits
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1399/c/solution.c
+++ old-problems/1399/c/solution.c
@@ -1,3 +1,21 @@
 int countLargestGroup(int n) {
-  return n;
+  int counts[37] = {0};
+  while (n > 0) {
+    ++counts[(n / 10000) % 10 + (n / 1000) % 10 + (n / 100) % 10 + (n / 10) % 10 + n % 10];
+    --n;
+  }
+
+  int max = 0, count = 0;
+  for (int i = 0; i < 37; ++i) {
+    if (counts[i] >= max) {
+      if (counts[i] == max) {
+        ++count;
+      } else {
+        max = counts[i];
+        count = 1;
+      }
+    }
+  }
+
+  return count;
 }
