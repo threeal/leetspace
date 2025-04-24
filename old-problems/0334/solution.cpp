fb7d65c6b19e793edd9df02a9c14f5ce812a41c3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-24 09:26:20 +0700

    feat(problem-334/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0334/solution.cpp
+++ old-problems/0334/solution.cpp
@@ -1,8 +1,20 @@
+#include <limits>
 #include <vector>
 
 class Solution {
  public:
   bool increasingTriplet(std::vector<int>& nums) {
-    return nums.empty();
+    int a{std::numeric_limits<int>::max()};
+    int b{std::numeric_limits<int>::max()};
+    for (const int num : nums) {
+      if (num <= a) {
+        a = num;
+      } else if (num <= b) {
+        b = num;
+      } else {
+        return true;
+      }
+    }
+    return false;
   }
 };
