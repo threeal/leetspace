385d48809d54f9e1380f9557966fbc0ff216fe88
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-24 21:18:42 +0700

    feat(problem-598/c): solve by finding the minimum range
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0598/c/solution.c
+++ old-problems/0598/c/solution.c
@@ -1,3 +1,8 @@
 int maxCount(int m, int n, int** ops, int opsSize, int* opsColSize) {
-  return m * n * ops[opsSize - 1][opsColSize[opsSize - 1] - 1];
+  (void)opsColSize;
+  for (int i = 0; i < opsSize; ++i) {
+    if (ops[i][0] < m) m = ops[i][0];
+    if (ops[i][1] < n) n = ops[i][1];
+  }
+  return m * n;
 }
