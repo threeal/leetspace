500da0003ae83f78aac2eba0ab9bd34b8dc3d3f6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-24 11:49:33 +0700

    test: add test for problem 297. Serialize and Deserialize Binary Tree
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0297/solution.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+class Codec {
+ public:
+  std::string serialize(TreeNode* root) {
+    (void)root;
+    return "";
+  }
+
+  TreeNode* deserialize(std::string data) {
+    (void)data;
+    return nullptr;
+  }
+};
