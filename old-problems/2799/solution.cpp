be03fbe05268078851944f8516859b403ecd6898
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-24 09:44:41 +0700

    feat(problem-2799/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2799/solution.cpp
+++ old-problems/2799/solution.cpp
@@ -3,6 +3,28 @@
 class Solution {
  public:
   int countCompleteSubarrays(std::vector<int>& nums) {
-    return nums.size();
+    int totalDistinct{0}, freqs[2001]{0};
+    for (const int num : nums) {
+      if (++freqs[num] == 1) ++totalDistinct;
+    }
+
+    for (const int num : nums) --freqs[num];
+
+    int count{0}, distinct{0};
+    std::size_t l{0}, r{0};
+    while (true) {
+      while (r < nums.size() && distinct < totalDistinct) {
+        if (++freqs[nums[r]] == 1) ++distinct;
+        ++r;
+      }
+
+      if (distinct != totalDistinct) break;
+      count += nums.size() - r + 1;
+
+      if (--freqs[nums[l]] == 0) --distinct;
+      ++l;
+    }
+
+    return count;
   }
 };
