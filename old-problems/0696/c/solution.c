25887a74d019898977acc84154307150b82ae778
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-24 21:19:31 +0700

    feat(problem-696/c): solve by counting consecutive 0s and 1s
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0696/c/solution.c
+++ old-problems/0696/c/solution.c
@@ -1,3 +1,13 @@
 int countBinarySubstrings(char* s) {
-  return *s;
+  int total = 0, zeros = 0, ones = 0;
+  while (*s != 0) {
+    zeros = 0;
+    while (*s == '0') ++s, ++zeros;
+    total += zeros <= ones ? zeros : ones;
+
+    ones = 0;
+    while (*s == '1') ++s, ++ones;
+    total += zeros <= ones ? zeros : ones;
+  }
+  return total;
 }
