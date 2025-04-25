4df4b771ab783b09e4ab43ef4be2452f32ecc5dc
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-25 22:27:10 +0700

    feat(problem-3467/cpp): solve by counting even number in-place
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3467/solution.cpp
+++ old-problems/3467/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   std::vector<int> transformArray(std::vector<int>& nums) {
+    std::size_t l{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      if (nums[i] % 2 == 0) nums[l++] = 0;
+    }
+    while (l < nums.size()) nums[l++] = 1;
     return nums;
   }
 };
