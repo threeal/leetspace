ac41f16aa0615a4cdb32422b45dd48c521d27b00
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-25 20:25:17 +0700

    feat(problem-1108/cpp): solve by resizing and shifting string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1108/solution.cpp
+++ old-problems/1108/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   std::string defangIPaddr(std::string address) {
+    address.resize(address.size() + 6);
+    for (int l = address.size() - 7, r = address.size() - 1; l >= 0; --l, --r) {
+      if (address[l] == '.') {
+        address[r] = ']';
+        address[--r] = '.';
+        address[--r] = '[';
+      } else {
+        address[r] = address[l];
+      }
+    }
     return address;
   }
 };
