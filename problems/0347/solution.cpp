271642a182ac182573dd1a2a35c2bac3df5b9b73
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-25 16:17:59 +0700

    feat(problem-347/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- problems/0347/solution.cpp
+++ problems/0347/solution.cpp
@@ -1,9 +1,25 @@
+#include <queue>
+#include <tuple>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> topKFrequent(std::vector<int>& nums, int k) {
-    (void)k;
-    return nums;
+    std::unordered_map<int, int> freqs{};
+    for (const int num : nums) ++freqs[num];
+
+    std::priority_queue<std::tuple<int, int>> queue{};
+    for (const auto& [num, freq] : freqs) {
+      queue.push({freq, num});
+    }
+
+    std::vector<int> output(k);
+    for (int i{0}; i < k; ++i) {
+      output[i] = std::get<1>(queue.top());
+      queue.pop();
+    }
+
+    return output;
   }
 };
