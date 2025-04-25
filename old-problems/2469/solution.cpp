cdf7d2272220c7b9f5234612759c76f5ea7abe93
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-25 20:14:52 +0700

    feat(problem-2469): solve by using addition and multiplication
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2469/solution.cpp
+++ old-problems/2469/solution.cpp
@@ -3,6 +3,6 @@
 class Solution {
  public:
   std::vector<double> convertTemperature(double celsius) {
-    return {celsius, celsius};
+    return {celsius + 273.15, celsius * 1.80 + 32.00};
   }
 };
