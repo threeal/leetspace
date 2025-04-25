296a72a4075a520b49f9de68123af2da3987ff90
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-25 15:24:04 +0700

    feat(problem-219/cpp): solve by keeping track of previous number positions
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- problems/0219/solution.cpp
+++ problems/0219/solution.cpp
@@ -1,8 +1,19 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
-    return nums.empty() && k == 0;
+    std::unordered_map<int, std::size_t> exists{};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      auto it = exists.find(nums[i]);
+      if (it == exists.end()) {
+        exists.emplace(nums[i], i);
+      } else {
+        if (it->second + k >= i) return true;
+        it->second = i;
+      }
+    }
+    return false;
   }
 };
