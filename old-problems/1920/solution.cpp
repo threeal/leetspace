45a8c133d0dc1e310992ef30c0d75e597b737962
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-25 20:05:03 +0700

    feat(problem-1920/cpp): solve by setting `ans[i]` to `nums[nums[i]]`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1920/solution.cpp
+++ old-problems/1920/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   std::vector<int> buildArray(std::vector<int>& nums) {
-    return nums;
+    std::vector<int> ans(nums.size());
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      ans[i] = nums[nums[i]];
+    }
+    return ans;
   }
 };
