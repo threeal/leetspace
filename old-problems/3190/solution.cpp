5ea2884c0ee539b3e2e3c536285cd7e6f8b851cc
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-26 17:15:46 +0700

    feat(problem-3190/cpp): solve by counting the number that is not divisible by 3
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3190/solution.cpp
+++ old-problems/3190/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   int minimumOperations(std::vector<int>& nums) {
-    return nums.size();
+    int count{0};
+    for (const int num : nums) {
+      if (num % 3 != 0) ++count;
+    }
+    return count;
   }
 };
