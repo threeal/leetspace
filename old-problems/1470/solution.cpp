15a403d9c52a6d4698b2ba6ed88f812562cfe984
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-26 17:08:16 +0700

    feat(problem-1470/cpp): solve by filling array with element of given array in specified order
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1470/solution.cpp
+++ old-problems/1470/solution.cpp
@@ -3,7 +3,11 @@
 class Solution {
  public:
   std::vector<int> shuffle(std::vector<int>& nums, int n) {
-    (void)n;
-    return nums;
+    std::vector<int> output(nums.size());
+    for (int i{0}; i < n; ++i) {
+      output[i * 2] = nums[i];
+      output[i * 2 + 1] = nums[n + i];
+    }
+    return output;
   }
 };
