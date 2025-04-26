df4e9337a7038ea836648b292108734b808cc344
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-26 17:23:05 +0700

    feat(problem-1689/cpp): solve by returning the largest digit
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1689/solution.cpp
+++ old-problems/1689/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   int minPartitions(std::string n) {
-    return n.size();
+    char largest{'0'};
+    for (const char c : n) {
+      if (c > largest) largest = c;
+    }
+    return largest - '0';
   }
 };
