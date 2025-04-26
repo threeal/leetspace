6ee5680797534ed6ffe7b24801dc7730b21b5da9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-26 16:56:24 +0700

    feat(problem-771/cpp): solve by storing jewel in an array map
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0771/solution.cpp
+++ old-problems/0771/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int numJewelsInStones(std::string jewels, std::string stones) {
-    return jewels.size() + stones.size();
+    bool isJewel[58]{false};
+    for (const char c : jewels) isJewel[c - 'A'] = true;
+
+    int count{0};
+    for (const char c : stones) {
+      if (isJewel[c - 'A']) ++count;
+    }
+
+    return count;
   }
 };
