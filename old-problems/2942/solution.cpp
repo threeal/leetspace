474ed28fadcbd35e2c4e800598e55362f8bb7496
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-27 14:58:20 +0700

    feat(problem-2942/cpp): solve by iterating the given words
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2942/solution.cpp
+++ old-problems/2942/solution.cpp
@@ -5,8 +5,16 @@ class Solution {
  public:
   std::vector<int> findWordsContaining(
       std::vector<std::string>& words, char x) {
-    (void)words;
-    (void)x;
-    return {};
+    std::vector<int> output{};
+    output.reserve(words.size());
+    for (std::size_t i{0}; i < words.size(); ++i) {
+      for (const char c : words[i]) {
+        if (c == x) {
+          output.push_back(i);
+          break;
+        }
+      }
+    }
+    return output;
   }
 };
