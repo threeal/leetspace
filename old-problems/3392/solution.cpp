fd67c57e12360a6f61af8cb1e7b79971c6495e16
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-27 14:48:04 +0700

    feat(problem-3392/cpp): solve by iterating the given array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3392/solution.cpp
+++ old-problems/3392/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   int countSubarrays(std::vector<int>& nums) {
-    return nums.size();
+    int count{0};
+    for (std::size_t i{2}; i < nums.size(); ++i) {
+      if ((nums[i - 2] + nums[i]) * 2 == nums[i - 1]) ++count;
+    }
+    return count;
   }
 };
