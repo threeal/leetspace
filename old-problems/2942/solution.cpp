a7602f85c494e93b2addea4daf8a9a9e0b4f8f50
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-27 14:56:14 +0700

    test: add test for problem 2942. Find Words Containing Character
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2942/solution.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> findWordsContaining(
+      std::vector<std::string>& words, char x) {
+    (void)words;
+    (void)x;
+    return {};
+  }
+};
