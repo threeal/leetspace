b6683e14ee37a5b5a9b0d136b8947637024f9db7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-28 22:00:26 +0700

    feat(problem-1108/c): solve by shifting string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1108/c/solution.c
+++ old-problems/1108/c/solution.c
@@ -1,3 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+
 char* defangIPaddr(char* address) {
-  return address;
+  int addressLen = strlen(address);
+  char* output = malloc((addressLen + 7) * sizeof(char));
+
+  for (int i = 0, j = 0; i < addressLen; ++i, ++j) {
+    if (address[i] == '.') {
+      output[j] = '[';
+      output[++j] = '.';
+      output[++j] = ']';
+    } else {
+      output[j] = address[i];
+    }
+  }
+
+  output[addressLen + 6] = 0;
+  return output;
 }
