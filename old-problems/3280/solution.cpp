720f9aa0eac90f4097121d3236865281241db150
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-28 14:37:46 +0700

    feat(problem-3280/cpp): solve by parsing the given string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3280/solution.cpp
+++ old-problems/3280/solution.cpp
@@ -3,6 +3,58 @@
 class Solution {
  public:
   std::string convertDateToBinary(std::string date) {
-    return date;
+    std::string output(25, '0');
+    std::size_t i{0}, j{0};
+
+    int num{0};
+    while (date[i] != '-') {
+      num = num * 10 + date[i] - '0';
+      ++i;
+    }
+
+    int bit{1 << 12};
+    while ((num & bit) == 0) bit >>= 1;
+    while (bit != 0) {
+      if ((num & bit) != 0) ++output[j];
+      bit >>= 1;
+      ++j;
+    }
+
+    ++i;
+    output[j++] = '-';
+
+    num = 0;
+    while (date[i] != '-') {
+      num = num * 10 + date[i] - '0';
+      ++i;
+    }
+
+    bit = 1 << 5;
+    while ((num & bit) == 0) bit >>= 1;
+    while (bit != 0) {
+      if ((num & bit) != 0) ++output[j];
+      bit >>= 1;
+      ++j;
+    }
+
+    ++i;
+    output[j++] = '-';
+
+    num = 0;
+    while (date[i] != 0) {
+      num = num * 10 + date[i] - '0';
+      ++i;
+    }
+
+    bit = 1 << 6;
+    while ((num & bit) == 0) bit >>= 1;
+    while (bit != 0) {
+      if ((num & bit) != 0) ++output[j];
+      bit >>= 1;
+      ++j;
+    }
+
+    output.resize(j);
+    return output;
   }
 };
