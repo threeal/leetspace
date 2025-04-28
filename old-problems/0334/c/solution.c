9ba0bbfa60427759cfdddde7bd9484cccd10202d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-28 22:19:38 +0700

    feat(problem-334/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0334/c/solution.c
+++ old-problems/0334/c/solution.c
@@ -1,5 +1,17 @@
+#include <limits.h>
 #include <stdbool.h>
 
 bool increasingTriplet(int* nums, int numsSize) {
-  return nums[numsSize - 1] == 0;
+  int a = INT_MAX;
+  int b = INT_MAX;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] <= a) {
+      a = nums[i];
+    } else if (nums[i] <= b) {
+      b = nums[i];
+    } else {
+      return true;
+    }
+  }
+  return false;
 }
