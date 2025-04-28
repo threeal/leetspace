567fa0a901ac922f210dd9cb3982053adf7a6f11
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-28 14:58:13 +0700

    feat(problem-1431/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1431/solution.cpp
+++ old-problems/1431/solution.cpp
@@ -4,8 +4,16 @@ class Solution {
  public:
   std::vector<bool> kidsWithCandies(
       std::vector<int>& candies, int extraCandies) {
-    (void)candies;
-    (void)extraCandies;
-    return {};
+    int maxCandy{0};
+    for (const int candy : candies) {
+      if (candy > maxCandy) maxCandy = candy;
+    }
+
+    std::vector<bool> output(candies.size());
+    for (std::size_t i{0}; i < candies.size(); ++i) {
+      output[i] = candies[i] + extraCandies >= maxCandy;
+    }
+
+    return output;
   }
 };
