f6e011a1b3dd72a8f3e226b19ed2d53e9f328e54
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-28 10:37:21 +0700

    feat(problem-2302/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2302/solution.cpp
+++ old-problems/2302/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   long long countSubarrays(std::vector<int>& nums, long long k) {
-    return nums.size() * k;
+    long long count{0}, sum{0}, n = nums.size();
+    for (long long l{0}, r{0}; r < n; ++r) {
+      sum += nums[r];
+      while (sum * (r + 1 - l) >= k) sum -= nums[l++];
+      count += r + 1 - l;
+    }
+    return count;
   }
 };
