76080c8f25307d60bcee538073a72b1681819ecf
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-29 13:15:06 +0700

    feat(problem-2824/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2824/solution.cpp
+++ old-problems/2824/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   int countPairs(std::vector<int>& nums, int target) {
-    return nums.size() + target;
+    int count{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      for (std::size_t j{i + 1}; j < nums.size(); ++j) {
+        if (nums[i] + nums[j] < target) ++count;
+      }
+    }
+    return count;
   }
 };
