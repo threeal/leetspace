37693e4cf90f8ab8b955e0ed1fa7680523fd96b7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-29 22:51:41 +0700

    feat(problem-3211/cpp): solve by using a backtracking
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3211/solution.cpp
+++ old-problems/3211/solution.cpp
@@ -4,7 +4,30 @@
 class Solution {
  public:
   std::vector<std::string> validStrings(int n) {
-    (void)n;
-    return {};
+    std::vector<std::string> output{};
+
+    std::string str(n, '0');
+    generate(output, str, 1);
+
+    str[0] = '1';
+    generate(output, str, 1);
+
+    return output;
   }
+
+ private:
+  static void generate(
+      std::vector<std::string>& output, std::string& str, std::size_t i) {
+    if (i == str.size()) {
+      output.push_back(str);
+    } else {
+      if (str[i - 1] == '1') {
+        str[i] = '0';
+        generate(output, str, i + 1);
+      }
+
+      str[i] = '1';
+      generate(output, str, i + 1);
+    }
+  };
 };
