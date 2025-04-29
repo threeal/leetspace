294d76f748aa34c7f33870dbfdf0cece9d378f0c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-29 22:16:45 +0700

    feat(problem-3190/c): solve by counting the number that is not divisible by 3
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3190/c/solution.c
+++ old-problems/3190/c/solution.c
@@ -1,3 +1,7 @@
 int minimumOperations(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int count = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] % 3 != 0) ++count;
+  }
+  return count;
 }
