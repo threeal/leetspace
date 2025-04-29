e4fd60b0d56827fc0292218df7d636f5a4d65610
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-29 14:30:32 +0700

    feat(problem-1365/cpp): solve by sorting the given numbers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1365/solution.cpp
+++ old-problems/1365/solution.cpp
@@ -1,8 +1,30 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> smallerNumbersThanCurrent(std::vector<int>& nums) {
-    return nums;
+    std::vector<std::size_t> indices(nums.size());
+    std::iota(indices.begin(), indices.end(), 0);
+
+    std::sort(
+        indices.begin(), indices.end(),
+        [&](std::size_t a, std::size_t b) {
+          return nums[a] < nums[b];
+        });
+
+    std::vector<int> output(nums.size());
+    output[indices[0]] = 0;
+
+    for (std::size_t i{1}; i < indices.size(); ++i) {
+      if (nums[indices[i]] == nums[indices[i - 1]]) {
+        output[indices[i]] = output[indices[i - 1]];
+      } else {
+        output[indices[i]] = i;
+      }
+    }
+
+    return output;
   }
 };
