a53df7a556fee730c4b5e3e118a7303b2702254d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-30 16:40:22 +0700

    feat(problem-1720/cpp): solve by XOR-ing array elements
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1720/solution.cpp
+++ old-problems/1720/solution.cpp
@@ -3,7 +3,11 @@
 class Solution {
  public:
   std::vector<int> decode(std::vector<int>& encoded, int first) {
-    (void)first;
-    return encoded;
+    std::vector<int> output(encoded.size() + 1);
+    output[0] = first;
+    for (std::size_t i{0}; i < encoded.size(); ++i) {
+      output[i + 1] = output[i] ^ encoded[i];
+    }
+    return output;
   }
 };
