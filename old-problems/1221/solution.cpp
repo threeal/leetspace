0ea92792ff7374be98424d5cabf44da2ac803ab6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-30 21:15:01 +0700

    feat(problem-1221/cpp): solve by iterating the given string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1221/solution.cpp
+++ old-problems/1221/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int balancedStringSplit(std::string s) {
-    return s.size();
+    int count{0}, balance{0};
+    for (const char c : s) {
+      if (c == 'L') {
+        if (++balance == 0) ++count;
+      } else {
+        if (--balance == 0) ++count;
+      }
+    }
+    return count;
   }
 };
