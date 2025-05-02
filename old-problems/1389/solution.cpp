da58401269eb1dacf03b61ea3720a80e0f1a44c2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-02 22:46:53 +0700

    feat(problem-1389): solve by repeatedly inserting output array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1389/solution.cpp
+++ old-problems/1389/solution.cpp
@@ -4,7 +4,13 @@ class Solution {
  public:
   std::vector<int> createTargetArray(
       std::vector<int>& nums, std::vector<int>& index) {
-    (void)index;
-    return nums;
+    std::vector<int> output{};
+    output.reserve(nums.size());
+
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      output.insert(output.begin() + index[i], nums[i]);
+    }
+
+    return output;
   }
 };
