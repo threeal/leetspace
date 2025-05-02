cef99365ef0108c7c4b34caefdfd530aa5145eff
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-02 20:35:26 +0700

    feat(problem-1828/cpp): solve by comparing distance with radius
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1828/solution.cpp
+++ old-problems/1828/solution.cpp
@@ -5,8 +5,17 @@ class Solution {
   std::vector<int> countPoints(
       std::vector<std::vector<int>>& points,
       std::vector<std::vector<int>>& queries) {
-    (void)points;
-    (void)queries;
-    return {};
+    std::vector<int> output(queries.size());
+    for (std::size_t i{0}; i < queries.size(); ++i) {
+      int count{0};
+      const int r2{queries[i][2] * queries[i][2]};
+      for (const std::vector<int>& point : points) {
+        const int x{point[0] - queries[i][0]};
+        const int y{point[1] - queries[i][1]};
+        if (x * x + y * y <= r2) ++count;
+      }
+      output[i] = count;
+    }
+    return output;
   }
 };
