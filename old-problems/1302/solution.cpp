198950ac15b7ab9fb2b8253e731bd25ec1c0c66f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-02 22:16:11 +0700

    feat(problem-1302/cpp): solve by using breadth-first search approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1302/solution.cpp
+++ old-problems/1302/solution.cpp
@@ -1,6 +1,29 @@
+#include <queue>
+
 class Solution {
  public:
   int deepestLeavesSum(TreeNode* root) {
-    return root->val;
+    std::queue<TreeNode*> queue{};
+    queue.push(root);
+
+    int sum{0};
+    while (!queue.empty()) {
+      sum = 0;
+      for (std::size_t n{queue.size()}; n > 0; --n) {
+        sum += queue.front()->val;
+
+        if (queue.front()->left != nullptr) {
+          queue.push(queue.front()->left);
+        }
+
+        if (queue.front()->right != nullptr) {
+          queue.push(queue.front()->right);
+        }
+
+        queue.pop();
+      }
+    }
+
+    return sum;
   }
 };
