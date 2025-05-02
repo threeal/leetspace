eeeeb37474aa409c84498f1176630b25024339a6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-02 14:29:50 +0700

    feat(problem-2071/cpp): solve by using a binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2071/solution.cpp
+++ old-problems/2071/solution.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <deque>
 #include <vector>
 
 class Solution {
@@ -5,6 +7,43 @@ class Solution {
   int maxTaskAssign(
       std::vector<int>& tasks, std::vector<int>& workers,
       int pills, int strength) {
-    return tasks.size() + workers.size() + pills + strength;
+    std::sort(tasks.begin(), tasks.end());
+    std::sort(workers.begin(), workers.end());
+
+    std::size_t low{0}, high{std::min(tasks.size(), workers.size())};
+    while (low < high) {
+      std::size_t mid{high - (high - low) / 2};
+
+      bool isValid{true};
+
+      int freePills{pills};
+      std::deque<int> unassigned{};
+      for (std::size_t i{0}, j{workers.size()}; i < mid; ++i) {
+        const int task{tasks[mid - i - 1]};
+        if (!unassigned.empty() && unassigned.front() >= task) {
+          unassigned.pop_back();
+        } else if (j > 0 && workers[j - 1] >= task) {
+          --j;
+        } else {
+          while (j > 0 && workers[j - 1] + strength >= task) {
+            unassigned.push_back(workers[--j]);
+          }
+          if (unassigned.empty() || freePills == 0) {
+            isValid = false;
+            break;
+          }
+          unassigned.pop_back();
+          --freePills;
+        }
+      }
+
+      if (isValid) {
+        low = mid;
+      } else {
+        high = mid - 1;
+      }
+    }
+
+    return low;
   }
 };
