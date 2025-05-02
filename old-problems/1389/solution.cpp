47d5b9b778d1f14206484bc534e795f3b7ffed1a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-02 22:44:15 +0700

    test: add test for problem 1389. Create Target Array in the Given Order
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1389/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> createTargetArray(
+      std::vector<int>& nums, std::vector<int>& index) {
+    (void)index;
+    return nums;
+  }
+};
