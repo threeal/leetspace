733246e88a7f0a941df72a02d82fbf7e4b698a96
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-03 22:07:07 +0700

    test(problem-1431): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1431/c/solution.c
@@ -0,0 +1,10 @@
+#include <stdbool.h>
+
+bool* kidsWithCandies(
+    int* candies, int candiesSize, int extraCandies, int* returnSize) {
+  (void)candies;
+  (void)candiesSize;
+  (void)extraCandies;
+  *returnSize = 0;
+  return 0;
+}
