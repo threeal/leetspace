6ee9d01875200dde513d85b7e3c5f9e65f76099f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-03 18:06:43 +0700

    feat(problem-1007/cpp): solve by finding the consecutive number
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1007/solution.cpp
+++ old-problems/1007/solution.cpp
@@ -3,6 +3,30 @@
 class Solution {
  public:
   int minDominoRotations(std::vector<int>& tops, std::vector<int>& bottoms) {
-    return tops.size() - bottoms.size();
+    int rotations{minDominoRotationsForNumber(tops, bottoms, tops[0])};
+    if (rotations == -1 && tops[0] != bottoms[0]) {
+      rotations = minDominoRotationsForNumber(tops, bottoms, bottoms[0]);
+    }
+    return rotations;
+  }
+
+ private:
+  static int minDominoRotationsForNumber(
+      std::vector<int>& tops, std::vector<int>& bottoms, int num) {
+    int topFreqs{0}, bottomFreqs{0};
+    for (std::size_t i{0}; i < tops.size(); ++i) {
+      if (tops[i] == num) {
+        if (bottoms[i] != num) {
+          ++topFreqs;
+        }
+      } else {
+        if (bottoms[i] == num) {
+          ++bottomFreqs;
+        } else {
+          return -1;
+        }
+      }
+    }
+    return topFreqs <= bottomFreqs ? topFreqs : bottomFreqs;
   }
 };
