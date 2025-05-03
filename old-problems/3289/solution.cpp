f433f0c842cfa5716a84926f2d8acfb30417c6c7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-03 21:59:16 +0700

    feat(problem-3289/cpp): solve by marking seen numbers while iterating array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3289/solution.cpp
+++ old-problems/3289/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   std::vector<int> getSneakyNumbers(std::vector<int>& nums) {
-    return nums;
+    std::vector<int> output{};
+    output.reserve(2);
+
+    for (const auto num : nums) {
+      if (nums[num % 100] >= 100) {
+        output.push_back(num % 100);
+      } else {
+        nums[num % 100] += 100;
+      }
+    }
+
+    return output;
   }
 };
