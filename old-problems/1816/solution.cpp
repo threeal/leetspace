47e7175725e251314d1a6fb60f63f07b828b32db
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-03 22:16:55 +0700

    feat(problem-1816/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1816/solution.cpp
+++ old-problems/1816/solution.cpp
@@ -3,7 +3,12 @@
 class Solution {
  public:
   std::string truncateSentence(std::string s, int k) {
-    (void)k;
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      if (s[i] == ' ' && --k == 0) {
+        s.resize(i);
+        break;
+      }
+    }
     return s;
   }
 };
