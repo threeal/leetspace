2ab443de84c866d0839747ff92161a64f98d843e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-04 20:36:43 +0700

    test: add test for problem 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1379/solution.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+class Solution {
+ public:
+  TreeNode* getTargetCopy(
+      TreeNode* original, TreeNode* cloned, TreeNode* target) {
+    (void)original;
+    (void)target;
+    return cloned;
+  }
+};
