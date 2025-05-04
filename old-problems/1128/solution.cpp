345b2317d90e0f6340e2c527afa008c77fbdca14
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-04 16:06:50 +0700

    feat(problem-1128/cpp): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1128/solution.cpp
+++ old-problems/1128/solution.cpp
@@ -1,8 +1,28 @@
+#include <algorithm>
+#include <utility>
 #include <vector>
 
 class Solution {
  public:
   int numEquivDominoPairs(std::vector<std::vector<int>>& dominoes) {
-    return dominoes.size();
+    for (std::vector<int>& domino : dominoes) {
+      if (domino[0] > domino[1]) std::swap(domino[0], domino[1]);
+    }
+
+    std::sort(dominoes.begin(), dominoes.end());
+
+    int totalPairs{0}, pairs{0};
+    for (std::size_t i{1}; i < dominoes.size(); ++i) {
+      if (dominoes[i][0] == dominoes[i - 1][0] &&
+          dominoes[i][1] == dominoes[i - 1][1]) {
+        ++pairs;
+      } else {
+        if (pairs > 0) totalPairs += (pairs * (pairs + 1)) / 2;
+        pairs = 0;
+      }
+    }
+
+    if (pairs > 0) totalPairs += (pairs * (pairs + 1)) / 2;
+    return totalPairs;
   }
 };
