6087db9ffde81f60b348331e0daab7242ec86907
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-04 17:52:17 +0700

    feat(problem-654/cpp): solve by using a stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0654/solution.cpp
+++ old-problems/0654/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   TreeNode* constructMaximumBinaryTree(std::vector<int>& nums) {
-    return new TreeNode(nums.front());
+    std::vector<TreeNode*> nodes{};
+    for (const int num : nums) {
+      TreeNode* node{new TreeNode(num)};
+      while (!nodes.empty() && nodes.back()->val < num) {
+        node->left = nodes.back();
+        nodes.pop_back();
+      }
+      if (!nodes.empty()) nodes.back()->right = node;
+      nodes.push_back(node);
+    }
+    return nodes.front();
   }
 };
