92b3ec61435727e094152f4f8f2b025b9eb93737
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-04 20:36:52 +0700

    feat(problem-1379/cpp): solve by finding node recursively
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1379/solution.cpp
+++ old-problems/1379/solution.cpp
@@ -5,7 +5,23 @@ class Solution {
   TreeNode* getTargetCopy(
       TreeNode* original, TreeNode* cloned, TreeNode* target) {
     (void)original;
-    (void)target;
-    return cloned;
+    return findNode(cloned, target->val);
+  }
+
+ private:
+  static TreeNode* findNode(TreeNode* node, int val) {
+    if (node->val == val) return node;
+
+    if (node->left != nullptr) {
+      TreeNode* left{findNode(node->left, val)};
+      if (left != nullptr) return left;
+    }
+
+    if (node->right != nullptr) {
+      TreeNode* right{findNode(node->right, val)};
+      if (right != nullptr) return right;
+    }
+
+    return nullptr;
   }
 };
