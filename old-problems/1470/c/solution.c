8bdef2cc6859931254fa28fbc2c9ac7803a8bc31
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-05 19:10:03 +0700

    feat(problem-1470/c): solve by filling array with element of given array in specified order
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1470/c/solution.c
+++ old-problems/1470/c/solution.c
@@ -1,5 +1,11 @@
+#include <stdlib.h>
+
 int* shuffle(int* nums, int numsSize, int n, int* returnSize) {
-  (void)n;
+  int* output = malloc(numsSize * sizeof(int));
+  for (int i = 0; i < n; ++i) {
+    output[i * 2] = nums[i];
+    output[i * 2 + 1] = nums[n + i];
+  }
   *returnSize = numsSize;
-  return nums;
+  return output;
 }
