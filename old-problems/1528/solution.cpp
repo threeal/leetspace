27ed1c0111174f8b837178623b24ef80ead2c777
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-05 22:41:57 +0700

    feat(problem-1528/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1528/solution.cpp
+++ old-problems/1528/solution.cpp
@@ -4,7 +4,10 @@
 class Solution {
  public:
   std::string restoreString(std::string s, std::vector<int>& indices) {
-    (void)indices;
-    return s;
+    std::string out(s.size(), 0);
+    for (std::size_t i{0}; i < indices.size(); ++i) {
+      out[indices[i]] = s[i];
+    }
+    return out;
   }
 };
