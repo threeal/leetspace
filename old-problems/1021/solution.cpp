cd37488155526da73851612b71a26d57932c38f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-05 19:44:28 +0700

    feat(problem-1021/cpp): solve by keeping track of parentheses level
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1021/solution.cpp
+++ old-problems/1021/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   std::string removeOuterParentheses(std::string s) {
+    int level{0};
+    std::size_t n{0};
+    for (const char c : s) {
+      if (c == '(') {
+        if (++level != 1) s[n++] = c;
+      } else {
+        if (--level != 0) s[n++] = c;
+      }
+    }
+    s.resize(n);
     return s;
   }
 };
