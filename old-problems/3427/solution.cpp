e93b99fe5aac5baf1497fcdd5302270df6458778
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-05 23:09:29 +0700

    feat(problem-3427/cpp): solve by using a prefix sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3427/solution.cpp
+++ old-problems/3427/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int subarraySum(std::vector<int>& nums) {
-    return nums.size();
+    int sum{nums[0]};
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] < static_cast<int>(i)) {
+        sum -= nums[i - nums[i] - 1];
+      }
+      nums[i] += nums[i - 1];
+      sum += nums[i];
+    }
+    return sum;
   }
 };
