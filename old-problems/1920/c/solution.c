4d488eaa006b6e2ac61a0f6560194cad2bdbfa72
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-06 08:34:51 +0700

    feat(problem-1920/c): solve by setting `ans[i]` to `nums[nums[i]]`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1920/c/solution.c
+++ old-problems/1920/c/solution.c
@@ -1,4 +1,10 @@
+#include <stdlib.h>
+
 int* buildArray(int* nums, int numsSize, int* returnSize) {
+  int* ans = malloc(numsSize * sizeof(int));
+  for (int i = 0; i < numsSize; ++i) {
+    ans[i] = nums[nums[i]];
+  }
   *returnSize = numsSize;
-  return nums;
+  return ans;
 }
