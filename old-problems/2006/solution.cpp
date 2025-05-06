f28cad711f87184663ef7d1cfabfaf35eaeefba2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-06 10:05:16 +0700

    feat(problem-2006/cpp): solve by using sorting and two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2006/solution.cpp
+++ old-problems/2006/solution.cpp
@@ -1,8 +1,16 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int countKDifference(std::vector<int>& nums, int k) {
-    return nums.size() + k;
+    int count{0};
+    std::sort(nums.begin(), nums.end());
+    for (std::size_t l{0}, r{0}, i{1}; i < nums.size(); ++i) {
+      while (r < i && nums[r] + k <= nums[i]) ++r;
+      while (l < r && nums[l] + k < nums[i]) ++l;
+      count += r - l;
+    }
+    return count;
   }
 };
