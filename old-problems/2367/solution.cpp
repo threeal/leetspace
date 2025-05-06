9b65ccd1923893de370376d375d2a9253bd4b895
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-06 09:24:26 +0700

    feat(problem-2367/cpp): solve by using 3 iterators
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2367/solution.cpp
+++ old-problems/2367/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   int arithmeticTriplets(std::vector<int>& nums, int diff) {
-    return nums.size() + diff;
+    int count{0};
+    for (std::size_t i{0}, j{1}, k{2}; k < nums.size(); ++k) {
+      while (j < k && nums[j] + diff < nums[k]) ++j;
+      while (i < j && nums[i] + diff < nums[j]) ++i;
+      if (nums[i] + diff == nums[j] && nums[j] + diff == nums[k]) ++count;
+    }
+    return count;
   }
 };
