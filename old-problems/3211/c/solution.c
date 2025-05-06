f498787dd5670b04d9ff61a862bb6dc3eb83fd4e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-06 17:17:47 +0700

    feat(problem-3211/c): solve by using backtracking
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3211/c/solution.c
+++ old-problems/3211/c/solution.c
@@ -1,5 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+
+char** output = NULL;
+int outputSize;
+
+char str[19];
+
+static void generate(int n, int i);
+
 char** validStrings(int n, int* returnSize) {
-  (void)n;
-  *returnSize = 0;
-  return 0;
+  if (output == NULL) {
+    output = malloc(6765 * sizeof(char*));
+    for (int i = 0; i < 6765; ++i) output[i] = NULL;
+  }
+  outputSize = 0;
+  memset(str, '0', n * sizeof(char));
+  generate(n, 1);
+
+  str[0] = '1';
+  generate(n, 1);
+
+  *returnSize = outputSize;
+  return output;
+}
+
+static void generate(int n, int i) {
+  if (i == n) {
+    if (output[outputSize] == NULL) {
+      output[outputSize] = malloc(19 * sizeof(char));
+    }
+    memcpy(output[outputSize], str, n * sizeof(char));
+    output[outputSize][n] = 0;
+    ++outputSize;
+  } else {
+    if (str[i - 1] == '1') {
+      str[i] = '0';
+      generate(n, i + 1);
+    }
+
+    str[i] = '1';
+    generate(n, i + 1);
+  }
 }
