55fbb8001465cf139ddfd27e56bba67895a805d1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-07 22:23:58 +0700

    feat(problem-1295/c): solve by checking if number within the valid ranges
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1295/c/solution.c
+++ old-problems/1295/c/solution.c
@@ -1,3 +1,11 @@
 int findNumbers(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int count = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if ((nums[i] >= 10 && nums[i] < 100) ||
+        (nums[i] >= 1000 && nums[i] < 10000) ||
+        nums[i] == 100000) {
+      ++count;
+    }
+  }
+  return count;
 }
