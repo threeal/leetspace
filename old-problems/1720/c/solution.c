12eeb52d29002c6f34591a2dedd66fb581f7918c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-07 22:23:16 +0700

    feat(problem-1720/c): solve by XOR-ing array elements
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1720/c/solution.c
+++ old-problems/1720/c/solution.c
@@ -1,5 +1,11 @@
+#include <stdlib.h>
+
 int* decode(int* encoded, int encodedSize, int first, int* returnSize) {
-  (void)first;
-  *returnSize = encodedSize;
-  return encoded;
+  *returnSize = encodedSize + 1;
+  int* output = malloc(*returnSize * sizeof(int));
+  output[0] = first;
+  for (int i = 0; i < encodedSize; ++i) {
+    output[i + 1] = output[i] ^ encoded[i];
+  }
+  return output;
 }
