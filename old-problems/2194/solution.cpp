b72f89705843c0a1906d8559a9f1904032f683f0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-07 16:15:33 +0700

    feat(problem-2194/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2194/solution.cpp
+++ old-problems/2194/solution.cpp
@@ -4,6 +4,18 @@
 class Solution {
  public:
   std::vector<std::string> cellsInRange(std::string s) {
-    return {s};
+    std::vector<std::string> output(
+        (s[3] + 1 - s[0]) * (s[4] + 1 - s[1]), "A1");
+
+    std::size_t n{0};
+    for (char i{s[0]}; i <= s[3]; ++i) {
+      for (char j{s[1]}; j <= s[4]; ++j) {
+        output[n][0] = i;
+        output[n][1] = j;
+        ++n;
+      }
+    }
+
+    return output;
   }
 };
