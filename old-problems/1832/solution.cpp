b847a865e97d23a414f0aafc4dedc6d4259ca9ad
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-07 16:50:47 +0700

    feat(problem-1832/cpp): solve by using bitmap
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1832/solution.cpp
+++ old-problems/1832/solution.cpp
@@ -1,8 +1,13 @@
+#include <cstdint>
 #include <string>
 
 class Solution {
  public:
   bool checkIfPangram(std::string sentence) {
-    return sentence.empty();
+    std::uint32_t flag{0};
+    for (const char c : sentence) {
+      flag |= 1 << (c - 'a');
+    }
+    return flag == 0b11111111111111111111111111;
   }
 };
