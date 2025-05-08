bb43119f29e71c71264217f003757a8ef7862489
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-08 15:00:05 +0700

    feat(problem-1281/c): solve by checking if number within the valid ranges
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1281/c/solution.c
+++ old-problems/1281/c/solution.c
@@ -1,3 +1,9 @@
 int subtractProductAndSum(int n) {
-  return n;
+  int product = 1, sum = 0;
+  while (n > 0) {
+    product *= n % 10;
+    sum += n % 10;
+    n /= 10;
+  }
+  return product - sum;
 }
