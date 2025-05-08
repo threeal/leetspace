b82caab39805442c620aa6265092c1e843e4a002
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-08 14:20:43 +0700

    feat(problem-1588/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1588/solution.cpp
+++ old-problems/1588/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   int sumOddLengthSubarrays(std::vector<int>& arr) {
-    return arr.size();
+    int sum{0};
+    const int n{static_cast<int>(arr.size())};
+    for (int i{0}; i < n; ++i) {
+      sum += ((i + 1) * (n - i) + 1) / 2 * arr[i];
+    }
+    return sum;
   }
 };
