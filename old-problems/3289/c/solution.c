86c77235ed328b2ab4993b54fddec91757948459
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-08 20:18:25 +0700

    feat(problem-3289/c): solve by marking seen numbers while iterating array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3289/c/solution.c
+++ old-problems/3289/c/solution.c
@@ -1,4 +1,14 @@
+int output[2];
+
 int* getSneakyNumbers(int* nums, int numsSize, int* returnSize) {
-  *returnSize = numsSize;
-  return nums;
+  for (int i = 0, j = 0; j < numsSize; ++j) {
+    if (nums[nums[j] % 100] >= 100) {
+      output[i++] = nums[j] % 100;
+    } else {
+      nums[nums[j] % 100] += 100;
+    }
+  }
+
+  *returnSize = 2;
+  return output;
 }
