a00f590672e1d4d5d40414bf9ae7c5d7f2839cc3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-08 11:38:46 +0700

    feat(problem-1572/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1572/solution.cpp
+++ old-problems/1572/solution.cpp
@@ -3,6 +3,13 @@
 class Solution {
  public:
   int diagonalSum(std::vector<std::vector<int>>& mat) {
-    return mat.size();
+    int sum{0};
+    for (std::size_t i{0}; i < mat.size(); ++i) {
+      sum += mat[i][i];
+      if (mat.size() - i - 1 != i) {
+        sum += mat[i][mat.size() - i - 1];
+      }
+    }
+    return sum;
   }
 };
