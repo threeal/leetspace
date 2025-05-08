980e4e430cce292c12ebd33e2ab09b63c2957a95
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-08 20:23:16 +0700

    test(problem-1281): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1282/c/solution.c
@@ -0,0 +1,9 @@
+int** groupThePeople(
+    int* groupSizes, int groupSizesSize,
+    int* returnSize, int** returnColumnSizes) {
+  (void)groupSizes;
+  (void)groupSizesSize;
+  *returnSize = 0;
+  *returnColumnSizes = 0;
+  return 0;
+}
