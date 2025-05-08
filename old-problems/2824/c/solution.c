ae01712263121e26343b4af6eea33af041f7b7f9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-08 20:20:19 +0700

    feat(problem-2824/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2824/c/solution.c
+++ old-problems/2824/c/solution.c
@@ -1,3 +1,9 @@
 int countPairs(int* nums, int numsSize, int target) {
-  return nums[numsSize - 1] + target;
+  int count = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    for (int j = i + 1; j < numsSize; ++j) {
+      if (nums[i] + nums[j] < target) ++count;
+    }
+  }
+  return count;
 }
