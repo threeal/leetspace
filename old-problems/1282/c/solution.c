b1fe8dd27df73cfca2408a9d548cc876c5d1e22e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-08 20:23:34 +0700

    feat(problem-1282/c): solve by greedily pick people with the same group size
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1282/c/solution.c
+++ old-problems/1282/c/solution.c
@@ -1,9 +1,39 @@
+#include <stdlib.h>
+
+int* gGroupSizes;
+int compare(const void* a, const void* b) {
+  return gGroupSizes[*(int*)a] - gGroupSizes[*(int*)b];
+}
+
 int** groupThePeople(
     int* groupSizes, int groupSizesSize,
     int* returnSize, int** returnColumnSizes) {
-  (void)groupSizes;
-  (void)groupSizesSize;
-  *returnSize = 0;
-  *returnColumnSizes = 0;
-  return 0;
+  int* indices = malloc(groupSizesSize * sizeof(int));
+  for (int i = 0; i < groupSizesSize; ++i) indices[i] = i;
+
+  gGroupSizes = groupSizes;
+  qsort(indices, groupSizesSize, sizeof(int), compare);
+
+  int** output = malloc(groupSizesSize * sizeof(int*));
+  int* outputColSizes = malloc(groupSizesSize * sizeof(int));
+  int outputSize = 0;
+
+  int i = 0;
+  while (i < groupSizesSize) {
+    const int groupSize = groupSizes[indices[i]];
+    int* arr = malloc(groupSize * sizeof(int));
+    for (int j = 0; j < groupSize; ++j) {
+      arr[j] = indices[i + j];
+    }
+
+    output[outputSize] = arr;
+    outputColSizes[outputSize] = groupSize;
+    ++outputSize;
+
+    i += groupSize;
+  }
+
+  *returnSize = outputSize;
+  *returnColumnSizes = outputColSizes;
+  return output;
 }
