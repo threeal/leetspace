d932fb48e941fa162a0135c16b8ba49d1d8ad99e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-09 10:53:06 +0700

    feat(problem-804/cpp): solve by mapping characters and storing in hash set
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- problems/0804/solution.cpp
+++ problems/0804/solution.cpp
@@ -1,9 +1,22 @@
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 class Solution {
+ private:
+  static constexpr const char* morseMap[]{
+      ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+      "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+      "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
  public:
   int uniqueMorseRepresentations(std::vector<std::string>& words) {
-    return words.size();
+    std::unordered_set<std::string> morses{};
+    for (const std::string& word : words) {
+      std::string morse{};
+      for (const char c : word) morse += morseMap[c - 'a'];
+      morses.insert(morse);
+    }
+    return morses.size();
   }
 };
