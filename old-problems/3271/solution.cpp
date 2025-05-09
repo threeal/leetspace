39c38b026e3e83d143a20e1fb232a24a3cf805be
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-09 15:13:36 +0700

    feat(problem-3271/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3271/solution.cpp
+++ old-problems/3271/solution.cpp
@@ -3,7 +3,14 @@
 class Solution {
  public:
   std::string stringHash(std::string s, int k) {
-    (void)k;
+    for (std::size_t i{0}; i < s.size(); i += k) {
+      char hash{0};
+      for (int j{0}; j < k; ++j) {
+        hash = (hash + s[i + j] - 'a') % 26;
+      }
+      s[i / k] = 'a' + hash;
+    }
+    s.resize(s.size() / k);
     return s;
   }
 };
