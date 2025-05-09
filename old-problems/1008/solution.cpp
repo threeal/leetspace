1e7557be4e68fbbcf43770d16e8283c406623b8b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-09 11:24:25 +0700

    feat(problem-1008/cpp): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1008/solution.cpp
+++ old-problems/1008/solution.cpp
@@ -1,9 +1,28 @@
+#include <span>
 #include <vector>
 
 class Solution {
  public:
   TreeNode* bstFromPreorder(std::vector<int>& preorder) {
-    (void)preorder;
-    return NULL;
+    return bstFromPreorderRange(preorder);
+  }
+
+ private:
+  static TreeNode* bstFromPreorderRange(std::span<int> preorder) {
+    TreeNode* node{new TreeNode(preorder[0])};
+
+    std::size_t i{1};
+    while (i < preorder.size() && preorder[i] < preorder[0]) ++i;
+
+    if (i > 1) {
+      node->left = bstFromPreorderRange(preorder.subspan(1, i - 1));
+    }
+
+    if (i < preorder.size()) {
+      node->right =
+          bstFromPreorderRange(preorder.subspan(i, preorder.size() - i));
+    }
+
+    return node;
   }
 };
