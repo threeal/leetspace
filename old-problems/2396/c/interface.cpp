913c1d3c20a6131a335490f62840619484c02b58
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-09 10:28:55 +0700

    feat: solve problem 2396. Strictly Palindromic Number in C (#3361)
    * test(problem-2396): add support to test in C
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(problem-2396/c): solve by returning `false`
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2396/c/interface.cpp
@@ -0,0 +1,7 @@
+extern "C" {
+bool isStrictlyPalindromic(int n);
+}
+
+bool solution_c(int n) {
+  return isStrictlyPalindromic(n);
+}
---
+++ old-problems/2396/c/solution.c
@@ -0,0 +1,6 @@
+#include <stdbool.h>
+
+bool isStrictlyPalindromic(int n) {
+  (void)n;
+  return false;
+}
