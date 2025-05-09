d764f481f30eaf484d7d02f89a1612fa41f2cdf0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-09 15:45:55 +0700

    feat(problem-3498/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3498/solution.cpp
+++ old-problems/3498/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   int reverseDegree(std::string s) {
-    return s.size();
+    int degree{0}, pos{0};
+    for (const char c : s) {
+      degree += (123 - c) * (++pos);
+    }
+    return degree;
   }
 };
