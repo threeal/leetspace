47d9b8ae5a08a8b17c843f80d14a1dde15b682d5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-10 19:54:02 +0700

    feat(problem-1828/c): solve by comparing distance with radius
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1828/c/solution.c
+++ old-problems/1828/c/solution.c
@@ -1,12 +1,21 @@
+#include <stdlib.h>
+
 int* countPoints(
     int** points, int pointsSize, int* pointsColSize,
     int** queries, int queriesSize, int* queriesColSize, int* returnSize) {
-  (void)points;
-  (void)pointsSize;
   (void)pointsColSize;
-  (void)queries;
-  (void)queriesSize;
   (void)queriesColSize;
-  *returnSize = 0;
-  return 0;
+  int* output = malloc(queriesSize * sizeof(int));
+  for (int i = 0; i < queriesSize; ++i) {
+    int count = 0;
+    const int r2 = queries[i][2] * queries[i][2];
+    for (int j = 0; j < pointsSize; ++j) {
+      const int x = points[j][0] - queries[i][0];
+      const int y = points[j][1] - queries[i][1];
+      if (x * x + y * y <= r2) ++count;
+    }
+    output[i] = count;
+  }
+  *returnSize = queriesSize;
+  return output;
 }
