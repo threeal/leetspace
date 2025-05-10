60a940943cd33740b96f3960960629c97de617cc
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-10 19:59:56 +0700

    feat(problem-1389/c): solve by repeatedly inserting output array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1389/c/solution.c
+++ old-problems/1389/c/solution.c
@@ -1,7 +1,20 @@
+#include <stdlib.h>
+
 int* createTargetArray(
     int* nums, int numsSize, int* index, int indexSize, int* returnSize) {
-  (void)index;
   (void)indexSize;
-  *returnSize = numsSize;
-  return nums;
+
+  int* output = malloc(numsSize * sizeof(int));
+  int outputSize = 0;
+
+  for (int i = 0; i < numsSize; ++i) {
+    for (int j = outputSize; j > index[i]; --j) {
+      output[j] = output[j - 1];
+    }
+    output[index[i]] = nums[i];
+    ++outputSize;
+  }
+
+  *returnSize = outputSize;
+  return output;
 }
