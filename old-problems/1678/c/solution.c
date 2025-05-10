97b6c10af82918e46bd418e4f31aa5d5ac3481eb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-10 20:02:57 +0700

    feat(problem-16780/c): solve by parsing string in-place
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1678/c/solution.c
+++ old-problems/1678/c/solution.c
@@ -1,3 +1,20 @@
 char* interpret(char* command) {
+  int l = 0, r = 0;
+  while (command[r] != 0) {
+    if (command[r] == '(') {
+      if (command[r + 1] == ')') {
+        command[l++] = 'o';
+        r += 2;
+      } else {
+        command[l++] = 'a';
+        command[l++] = 'l';
+        r += 4;
+      }
+    } else {
+      command[l++] = 'G';
+      ++r;
+    }
+  }
+  command[l] = 0;
   return command;
 }
