b68641627fc2cee001bfab0b274994574600150c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-11 22:58:16 +0700

    feat(problem-3285/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3285/solution.cpp
+++ old-problems/3285/solution.cpp
@@ -3,7 +3,12 @@
 class Solution {
  public:
   std::vector<int> stableMountains(std::vector<int>& height, int threshold) {
-    (void)threshold;
+    height.pop_back();
+    std::size_t n{0};
+    for (std::size_t i{0}; i < height.size(); ++i) {
+      if (height[i] > threshold) height[n++] = i + 1;
+    }
+    height.resize(n);
     return height;
   }
 };
