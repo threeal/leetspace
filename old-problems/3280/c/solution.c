f10b8df1a33f222c69af13c27044045718da7503
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-11 21:24:40 +0700

    feat(problem-3280/c): solve by parsing the given string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3280/c/solution.c
+++ old-problems/3280/c/solution.c
@@ -1,3 +1,58 @@
+#include <stdlib.h>
+#include <string.h>
+
 char* convertDateToBinary(char* date) {
-  return date;
+  char* output = malloc(26 * sizeof(char));
+  memset(output, '0', 25 * sizeof(char));
+
+  int i = 0, j = 0, num = 0;
+  while (date[i] != '-') {
+    num = num * 10 + date[i] - '0';
+    ++i;
+  }
+
+  int bit = 1 << 12;
+  while ((num & bit) == 0) bit >>= 1;
+  while (bit != 0) {
+    if ((num & bit) != 0) ++output[j];
+    bit >>= 1;
+    ++j;
+  }
+
+  ++i;
+  output[j++] = '-';
+
+  num = 0;
+  while (date[i] != '-') {
+    num = num * 10 + date[i] - '0';
+    ++i;
+  }
+
+  bit = 1 << 5;
+  while ((num & bit) == 0) bit >>= 1;
+  while (bit != 0) {
+    if ((num & bit) != 0) ++output[j];
+    bit >>= 1;
+    ++j;
+  }
+
+  ++i;
+  output[j++] = '-';
+
+  num = 0;
+  while (date[i] != 0) {
+    num = num * 10 + date[i] - '0';
+    ++i;
+  }
+
+  bit = 1 << 6;
+  while ((num & bit) == 0) bit >>= 1;
+  while (bit != 0) {
+    if ((num & bit) != 0) ++output[j];
+    bit >>= 1;
+    ++j;
+  }
+
+  output[j] = 0;
+  return output;
 }
