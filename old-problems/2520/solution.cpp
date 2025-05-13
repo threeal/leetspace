03025a0f7612b7928bdcb9df174999be9837795b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-13 19:34:09 +0700

    feat: solve problem 2520. Count the Digits That Divide a Number (#3357)
    * test: add test for problem 2520. Count the Digits That Divide a Number
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(problem-2520/cpp): solve by using a for-loop
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2520/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+ public:
+  int countDigits(int num) {
+    int count{0};
+    for (int exp{1}; exp <= num; exp *= 10) {
+      if (num % (num / exp % 10) == 0) ++count;
+    }
+    return count;
+  }
+};
