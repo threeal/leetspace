565463abfe736c35ad8fbc200e58210150394064
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-13 23:03:06 +0700

    feat(problem-3162/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3162/c/solution.c
+++ old-problems/3162/c/solution.c
@@ -1,3 +1,10 @@
 int numberOfPairs(int* nums1, int nums1Size, int* nums2, int nums2Size, int k) {
-  return nums1[nums1Size - 1] + nums2[nums2Size - 1] + k;
+  int count = 0;
+  for (int i2 = 0; i2 < nums2Size; ++i2) {
+    const int div = nums2[i2] * k;
+    for (int i1 = 0; i1 < nums1Size; ++i1) {
+      if (nums1[i1] % div == 0) ++count;
+    }
+  }
+  return count;
 }
