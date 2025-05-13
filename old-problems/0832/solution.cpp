53beec8e21fe49e2655df165e7bc2dfbd324a6b6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-13 19:50:23 +0700

    feat(problem-832/cpp): solve by reversing and flipping
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0832/solution.cpp
+++ old-problems/0832/solution.cpp
@@ -4,6 +4,24 @@ class Solution {
  public:
   std::vector<std::vector<int>> flipAndInvertImage(
       std::vector<std::vector<int>>& image) {
+    for (std::vector<int>& row : image) {
+      std::size_t l{0}, r{row.size() - 1};
+      while (l < r) {
+        if (row[l] == 0) {
+          if (row[r] == 0) {
+            row[l] = 1;
+            row[r] = 1;
+          }
+        } else {
+          if (row[r] != 0) {
+            row[l] = 0;
+            row[r] = 0;
+          }
+        }
+        ++l, --r;
+      }
+      if (l == r) row[l] = row[l] == 0 ? 1 : 0;
+    }
     return image;
   }
 };
