80762451e89124d3822810e641fcd307a4726cd8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-13 20:23:10 +0700

    feat(problem-2859/c): solve by using `__builtin_popcount`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2859/c/solution.c
+++ old-problems/2859/c/solution.c
@@ -1,3 +1,7 @@
 int sumIndicesWithKSetBits(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] + k;
+  int sum = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (__builtin_popcount(i) == k) sum += nums[i];
+  }
+  return sum;
 }
