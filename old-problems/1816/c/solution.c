ed08070920bd73519a8501ca8bfc259df580b2af
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-13 20:07:38 +0700

    feat(problem-1816/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1816/c/solution.c
+++ old-problems/1816/c/solution.c
@@ -1,4 +1,9 @@
 char* truncateSentence(char* s, int k) {
-  (void)k;
+  for (int i = 0; s[i] != 0; ++i) {
+    if (s[i] == ' ' && --k == 0) {
+      s[i] = 0;
+      break;
+    }
+  }
   return s;
 }
