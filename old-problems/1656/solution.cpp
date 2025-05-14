5098a0052171d951b722b912410aaf2297378c19
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-14 21:40:04 +0700

    feat(problem-1656/cpp): solve by using a priority queue
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1656/solution.cpp
+++ old-problems/1656/solution.cpp
@@ -1,13 +1,32 @@
+#include <queue>
 #include <string>
+#include <tuple>
 #include <vector>
 
 class OrderedStream {
+ private:
+  using Stream = std::tuple<int, std::string>;
+  std::priority_queue<Stream, std::vector<Stream>, std::greater<Stream>> queue;
+  int i;
+
  public:
-  OrderedStream(int n) {
-    (void)n;
-  }
+  OrderedStream(int /*n*/) : i{1} {}
 
   std::vector<std::string> insert(int idKey, std::string value) {
-    return std::vector(idKey, value);
+    std::vector<std::string> output{};
+    if (i == idKey) {
+      output.push_back(value);
+      ++i;
+    } else {
+      queue.push({idKey, value});
+    }
+
+    while (!queue.empty() && std::get<0>(queue.top()) == i) {
+      output.push_back(std::get<1>(queue.top()));
+      queue.pop();
+      ++i;
+    }
+
+    return output;
   }
 };
