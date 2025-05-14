3fcb4ba0982815d9f9cb8d579f5dbf6ee75e930e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-14 21:56:36 +0700

    feat(problem-2325/c): solve by storing char map in array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2325/c/solution.c
+++ old-problems/2325/c/solution.c
@@ -1,3 +1,16 @@
 char* decodeMessage(char* key, char* message) {
-  return *key > *message ? key : message;
+  char map[26] = {0};
+
+  char val = 'a';
+  for (char* c = key; *c != 0; ++c) {
+    if (*c != ' ' && map[*c - 'a'] == 0) {
+      map[*c - 'a'] = val++;
+    }
+  }
+
+  for (char* c = message; *c != 0; ++c) {
+    if (*c != ' ') *c = map[*c - 'a'];
+  }
+
+  return message;
 }
