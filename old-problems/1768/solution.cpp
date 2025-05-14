5f8b478d502e827379203817a43f04aaee9e46e5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-14 21:24:30 +0700

    feat(problem-1768/cpp): solve by appending characters alternately
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1768/solution.cpp
+++ old-problems/1768/solution.cpp
@@ -1,8 +1,21 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   std::string mergeAlternately(std::string word1, std::string word2) {
-    return word1 + word2;
+    std::string output{};
+    output.reserve(word1.size() + word2.size());
+
+    std::size_t n{std::min(word1.size(), word2.size())};
+    for (std::size_t i{0}; i < n; ++i) {
+      output.push_back(word1[i]);
+      output.push_back(word2[i]);
+    }
+
+    for (std::size_t i{n}; i < word1.size(); ++i) output.push_back(word1[i]);
+    for (std::size_t i{n}; i < word2.size(); ++i) output.push_back(word2[i]);
+
+    return output;
   }
 };
