1d5f869f9eb1988d28e16e65896647ec2af56e1d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-14 14:17:03 +0700

    feat(problem-1021/c): solve by keeping track of parentheses level
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1021/c/solution.c
+++ old-problems/1021/c/solution.c
@@ -1,3 +1,12 @@
 char* removeOuterParentheses(char* s) {
+  int level = 0, n = 0;
+  for (char* c = s; *c != 0; ++c) {
+    if (*c == '(') {
+      if (++level != 1) s[n++] = '(';
+    } else {
+      if (--level != 0) s[n++] = ')';
+    }
+  }
+  s[n] = 0;
   return s;
 }
