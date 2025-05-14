7b42c8ff60d813ec2c2c47e8a3f6d42d8673d1f0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-14 14:08:51 +0700

    feat(problem-790/c): solve by using a bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0790/c/solution.c
+++ old-problems/0790/c/solution.c
@@ -1,3 +1,15 @@
 int numTilings(int n) {
-  return n;
+  if (n == 1) return 1;
+  if (n == 2) return 2;
+  if (n == 3) return 5;
+
+  int a = 5, b = 2, c = 1;
+  while (--n >= 3) {
+    const int num = ((a * 2) % 1000000007 + c) % 1000000007;
+    c = b;
+    b = a;
+    a = num;
+  }
+
+  return a;
 }
