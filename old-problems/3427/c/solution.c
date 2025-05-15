e116adabc7a918070c87fcaa0c02fcee5d9f34ee
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-15 21:01:20 +0700

    feat(problem-3427/c): solve by using a prefix sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3427/c/solution.c
+++ old-problems/3427/c/solution.c
@@ -1,3 +1,11 @@
 int subarraySum(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int sum = nums[0];
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i] < i) {
+      sum -= nums[i - nums[i] - 1];
+    }
+    nums[i] += nums[i - 1];
+    sum += nums[i];
+  }
+  return sum;
 }
