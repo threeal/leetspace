294ce386c30a75456eea6a157f8b5108c5226c1d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-15 13:27:57 +0700

    feat(problem-3131/cpp): solve by comparing the difference between minimums
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3131/solution.cpp
+++ old-problems/3131/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   int addedInteger(std::vector<int>& nums1, std::vector<int>& nums2) {
-    return nums2[0] - nums1[0];
+    int min1{nums1[0]}, min2{nums2[0]};
+    for (std::size_t i{0}; i < nums1.size(); ++i) {
+      if (nums1[i] < min1) min1 = nums1[i];
+      if (nums2[i] < min2) min2 = nums2[i];
+    }
+    return min2 - min1;
   }
 };
