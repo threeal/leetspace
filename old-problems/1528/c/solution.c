060f05e618ac78c98061dce5980f43580ebc22c2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-15 16:04:41 +0700

    feat(problem-1528/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1528/c/solution.c
+++ old-problems/1528/c/solution.c
@@ -1,5 +1,10 @@
+#include <stdlib.h>
+
 char* restoreString(char* s, int* indices, int indicesSize) {
-  (void)indices;
-  (void)indicesSize;
-  return s;
+  char* out = malloc((indicesSize + 1) * sizeof(char));
+  for (int i = 0; i < indicesSize; ++i) {
+    out[indices[i]] = s[i];
+  }
+  out[indicesSize] = 0;
+  return out;
 }
