e36f64d49c1552265a9751aa563e19ddddabfa26
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-15 12:32:30 +0700

    feat(problem-2900/cpp): solve by picking word alternately
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2900/solution.cpp
+++ old-problems/2900/solution.cpp
@@ -5,7 +5,15 @@ class Solution {
  public:
   std::vector<std::string> getLongestSubsequence(
       std::vector<std::string>& words, std::vector<int>& groups) {
-    words.resize(groups.size());
+    std::size_t n{1};
+    int group{groups[0]};
+    for (std::size_t i{1}; i < words.size(); ++i) {
+      if (groups[i] != group) {
+        group = groups[i];
+        words[n++] = words[i];
+      }
+    }
+    words.resize(n);
     return words;
   }
 };
