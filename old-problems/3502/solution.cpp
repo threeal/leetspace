db611d48af88d7d6772f252650cd39b610ab98e5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-15 14:05:26 +0700

    feat(problem-3502/cpp): solve by returning perfix minimum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3502/solution.cpp
+++ old-problems/3502/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   std::vector<int> minCosts(std::vector<int>& cost) {
+    int min{cost[0]};
+    for (std::size_t i{1}; i < cost.size(); ++i) {
+      if (min < cost[i]) {
+        cost[i] = min;
+      } else {
+        min = cost[i];
+      }
+    }
     return cost;
   }
 };
