8d0f781cfd7dff8e0c617ef740f74d48aed8404e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-15 13:36:46 +0700

    feat(problem-1967/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1967/solution.cpp
+++ old-problems/1967/solution.cpp
@@ -4,6 +4,10 @@
 class Solution {
  public:
   int numOfStrings(std::vector<std::string>& patterns, std::string word) {
-    return word.size() + patterns.size();
+    int count{0};
+    for (const std::string& pattern : patterns) {
+      if (word.find(pattern) != std::string::npos) ++count;
+    }
+    return count;
   }
 };
