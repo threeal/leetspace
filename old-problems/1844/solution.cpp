db326fc730f95432225d7a72a5ddacdac4ae6975
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-15 12:44:59 +0700

    feat(problem-1844/cpp): solve by modifying string in-place
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1844/solution.cpp
+++ old-problems/1844/solution.cpp
@@ -3,6 +3,9 @@
 class Solution {
  public:
   std::string replaceDigits(std::string s) {
+    for (std::size_t i{1}; i < s.size(); i += 2) {
+      s[i] += s[i - 1] - '0';
+    }
     return s;
   }
 };
