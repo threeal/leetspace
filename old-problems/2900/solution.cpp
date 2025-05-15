f010e5d106860ea510fb46d907cc65821e10f4c3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-15 12:29:54 +0700

    test: add test for problem 2900. Longest Unequal Adjacent Groups Subsequence I
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2900/solution.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::string> getLongestSubsequence(
+      std::vector<std::string>& words, std::vector<int>& groups) {
+    words.resize(groups.size());
+    return words;
+  }
+};
