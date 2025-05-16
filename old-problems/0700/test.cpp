ea75d09dcddd647826d8e0986c4d5824b6ec3206
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-16 12:18:11 +0700

    test: add test for problem 700. Search in a Binary Search Tree
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0700/test.cpp
@@ -0,0 +1,77 @@
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+// clang-format off
+#include "solution.cpp"
+// clang-format on
+
+#include <catch2/catch_test_macros.hpp>
+#include <optional>
+#include <queue>
+#include <vector>
+
+const auto null = std::nullopt;
+
+TreeNode* to_tree(const std::vector<std::optional<int>>& list) {
+  if (list.empty()) return nullptr;
+
+  auto root = new TreeNode(*list[0]);
+
+  std::queue<TreeNode**> children{};
+  children.push(&(root->left));
+  children.push(&(root->right));
+
+  for (std::size_t i{1}; i < list.size(); ++i) {
+    if (list[i]) {
+      auto node = new TreeNode(*list[i]);
+      *children.front() = node;
+      children.push(&(node->left));
+      children.push(&(node->right));
+    }
+    children.pop();
+  }
+
+  return root;
+}
+
+std::vector<std::optional<int>> from_tree(TreeNode* root) {
+  if (root == nullptr) return {};
+  std::vector<std::optional<int>> list{};
+
+  std::queue<TreeNode*> nodes{};
+  nodes.push(root);
+
+  while (!nodes.empty()) {
+    if (nodes.front() == nullptr) {
+      list.push_back(null);
+    } else {
+      list.push_back(nodes.front()->val);
+      nodes.push(nodes.front()->left);
+      nodes.push(nodes.front()->right);
+    }
+    nodes.pop();
+  }
+
+  while (!list.back()) list.pop_back();
+  return list;
+}
+
+TEST_CASE("700. Search in a Binary Search Tree") {
+  SECTION("Example 1") {
+    TreeNode* root{Solution{}.searchBST(to_tree({4, 2, 7, 1, 3}), 2)};
+    REQUIRE(from_tree(root) == std::vector<std::optional<int>>{2, 1, 3});
+  }
+
+  SECTION("Example 2") {
+    TreeNode* root{Solution{}.searchBST(to_tree({4, 2, 7, 1, 3}), 5)};
+    REQUIRE(from_tree(root) == std::vector<std::optional<int>>{});
+  }
+}
