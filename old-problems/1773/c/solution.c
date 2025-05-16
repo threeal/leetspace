fe510be31b28e1e2e15b513b7911540b184b166d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-16 15:40:51 +0700

    feat(problem-1773/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1773/c/solution.c
+++ old-problems/1773/c/solution.c
@@ -1,10 +1,13 @@
+#include <string.h>
+
 int countMatches(
     char*** items, int itemsSize, int* itemsColSize,
     char* ruleKey, char* ruleValue) {
-  (void)items;
-  (void)itemsSize;
   (void)itemsColSize;
-  (void)ruleKey;
-  (void)ruleValue;
-  return 0;
+  int count = 0;
+  int j = ruleKey[0] == 't' ? 0 : (ruleKey[0] == 'c' ? 1 : 2);
+  for (int i = 0; i < itemsSize; ++i) {
+    if (strcmp(items[i][j], ruleValue) == 0) ++count;
+  }
+  return count;
 }
