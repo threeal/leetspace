139ee1f39109e3bb1470e10183ed71e697c4bdfe
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-16 13:49:23 +0700

    test: add test for problem 2120. Execution of All Suffix Instructions Staying in a Grid
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2120/solution.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> executeInstructions(
+      int n, std::vector<int>& startPos, std::string s) {
+    (void)n;
+    (void)s;
+    return startPos;
+  }
+};
