c13d7306bdc0f57e9229ffba6bf4f11459a1d4e2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-16 17:40:38 +0700

    feat(problem-2367/c): solve by using 3 iterators
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2367/c/solution.c
+++ old-problems/2367/c/solution.c
@@ -1,3 +1,9 @@
 int arithmeticTriplets(int* nums, int numsSize, int diff) {
-  return nums[numsSize - 1] + diff;
+  int count = 0;
+  for (int i = 0, j = 1, k = 2; k < numsSize; ++k) {
+    while (j < k && nums[j] + diff < nums[k]) ++j;
+    while (i < j && nums[i] + diff < nums[j]) ++i;
+    if (nums[i] + diff == nums[j] && nums[j] + diff == nums[k]) ++count;
+  }
+  return count;
 }
