0e872cde35d409c06207823222b868dd92889286
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-16 14:11:07 +0700

    feat(problem-3248/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3248/solution.cpp
+++ old-problems/3248/solution.cpp
@@ -4,6 +4,26 @@
 class Solution {
  public:
   int finalPositionOfSnake(int n, std::vector<std::string>& commands) {
-    return n + commands.size();
+    int position{0};
+    for (const std::string& command : commands) {
+      switch (command[0]) {
+        case 'U':
+          position -= n;
+          break;
+
+        case 'R':
+          ++position;
+          break;
+
+        case 'D':
+          position += n;
+          break;
+
+        case 'L':
+          --position;
+          break;
+      }
+    }
+    return position;
   }
 };
