a6916d4ea885277119f685bddfb3ea9d57129950
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-16 14:29:50 +0700

    feat(problem-2744/cpp): solve by using brute force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2744/solution.cpp
+++ old-problems/2744/solution.cpp
@@ -4,6 +4,12 @@
 class Solution {
  public:
   int maximumNumberOfStringPairs(std::vector<std::string>& words) {
-    return words.size();
+    int count{0};
+    for (std::size_t j{1}; j < words.size(); ++j) {
+      for (std::size_t i{0}; i < j; ++i) {
+        if (words[i][0] == words[j][1] && words[i][1] == words[j][0]) ++count;
+      }
+    }
+    return count;
   }
 };
