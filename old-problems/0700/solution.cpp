40151bf1934594052aa46f0cc40d933c4b7711fa
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-16 12:21:48 +0700

    feat(problem-700/cpp): solve by using DFS
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0700/solution.cpp
+++ old-problems/0700/solution.cpp
@@ -1,7 +1,15 @@
 class Solution {
  public:
   TreeNode* searchBST(TreeNode* root, int val) {
-    root->val = val;
-    return root;
+    if (root->val == val) return root;
+    if (root->left != nullptr) {
+      TreeNode* node{searchBST(root->left, val)};
+      if (node != nullptr) return node;
+    }
+    if (root->right != nullptr) {
+      TreeNode* node{searchBST(root->right, val)};
+      if (node != nullptr) return node;
+    }
+    return nullptr;
   }
 };
