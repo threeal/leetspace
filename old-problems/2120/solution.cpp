314e4ea18fda84d109eb02a5d3c550aa0d785dfa
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-16 14:01:05 +0700

    feat(problem-2120/cpp): solve by using a brute force approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2120/solution.cpp
+++ old-problems/2120/solution.cpp
@@ -5,8 +5,24 @@ class Solution {
  public:
   std::vector<int> executeInstructions(
       int n, std::vector<int>& startPos, std::string s) {
-    (void)n;
-    (void)s;
-    return startPos;
+    std::vector<int> output(s.size());
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      int y{startPos[0]}, x{startPos[1]};
+      std::size_t j{i};
+      while (j < s.size()) {
+        if (s[j] == 'L') {
+          if (--x < 0) break;
+        } else if (s[j] == 'R') {
+          if (++x >= n) break;
+        } else if (s[j] == 'U') {
+          if (--y < 0) break;
+        } else {
+          if (++y >= n) break;
+        }
+        ++j;
+      }
+      output[i] = j - i;
+    }
+    return output;
   }
 };
