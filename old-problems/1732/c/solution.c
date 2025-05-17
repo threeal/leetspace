001a69472966c84445d12879612c1d1c729b2e2b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-17 22:47:23 +0700

    feat(problem-1732/c): solve by returning max prefix sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1732/c/solution.c
+++ old-problems/1732/c/solution.c
@@ -1,3 +1,8 @@
 int largestAltitude(int* gain, int gainSize) {
-  return gain[gainSize - 1];
+  int maxAltitude = 0, altitude = 0;
+  for (int i = 0; i < gainSize; ++i) {
+    altitude += gain[i];
+    if (altitude > maxAltitude) maxAltitude = altitude;
+  }
+  return maxAltitude;
 }
