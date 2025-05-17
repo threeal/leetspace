c34a5883b4b418b6d2f845c49e0eac73dcf9282a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-17 19:57:42 +0700

    feat(problem-3232/cpp): solve by comparing sum of single and double digits
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3232/solution.cpp
+++ old-problems/3232/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   bool canAliceWin(std::vector<int>& nums) {
-    return nums.empty();
+    int sum1{0}, sum2{0};
+    for (const int num : nums) {
+      if (num < 10) {
+        sum1 += num;
+      } else {
+        sum2 += num;
+      }
+    }
+    return sum1 != sum2;
   }
 };
