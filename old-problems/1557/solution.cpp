0cb51d1b71142257fca2cbcb97af480ee1ea71e7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-17 22:25:45 +0700

    feat(problem-1557/cpp): solve by returning nodes not pointed by other nodes
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1557/solution.cpp
+++ old-problems/1557/solution.cpp
@@ -4,7 +4,16 @@ class Solution {
  public:
   std::vector<int> findSmallestSetOfVertices(
       int n, std::vector<std::vector<int>>& edges) {
-    (void)n;
-    return edges.front();
+    std::vector<bool> exists(n);
+    for (const std::vector<int>& edge : edges) {
+      exists[edge[1]] = true;
+    }
+
+    std::vector<int> output{};
+    for (int i{0}; i < n; ++i) {
+      if (!exists[i]) output.push_back(i);
+    }
+
+    return output;
   }
 };
