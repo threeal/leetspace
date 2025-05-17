5e224a16e5bcec04975f84f74730fd4fa364ed1d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-17 22:32:17 +0700

    feat(problem-3194/cpp): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3194/solution.cpp
+++ old-problems/3194/solution.cpp
@@ -1,8 +1,15 @@
+#include <algorithm>
+#include <limits>
 #include <vector>
 
 class Solution {
  public:
   double minimumAverage(std::vector<int>& nums) {
-    return nums.size();
+    int minimum{std::numeric_limits<int>::max()};
+    std::sort(nums.begin(), nums.end());
+    for (std::size_t l{0}, r{nums.size() - 1}; l < r; ++l, --r) {
+      if (nums[r] + nums[l] < minimum) minimum = nums[r] + nums[l];
+    }
+    return static_cast<double>(minimum) / 2.0;
   }
 };
