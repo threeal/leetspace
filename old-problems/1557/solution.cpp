285e0b77100963e67915f13fe5c64b92428dc602
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-17 22:23:52 +0700

    test: add test for problem 1557. Minimum Number of Vertices to Reach All Nodes
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1557/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> findSmallestSetOfVertices(
+      int n, std::vector<std::vector<int>>& edges) {
+    (void)n;
+    return edges.front();
+  }
+};
