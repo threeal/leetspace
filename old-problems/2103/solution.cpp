ea0dc9f20cece675517c78e9a0e0b1710a9740d0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-17 22:16:12 +0700

    feat(problem-2103/cpp): solve by tracking rings using a binary map
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2103/solution.cpp
+++ old-problems/2103/solution.cpp
@@ -3,6 +3,28 @@
 class Solution {
  public:
   int countPoints(std::string rings) {
-    return rings.size();
+    char rods[10]{0};
+    for (std::size_t i{1}; i < rings.size(); i += 2) {
+      switch (rings[i - 1]) {
+        case 'B':
+          rods[rings[i] - '0'] |= 1;
+          break;
+
+        case 'G':
+          rods[rings[i] - '0'] |= 2;
+          break;
+
+        case 'R':
+          rods[rings[i] - '0'] |= 4;
+          break;
+      }
+    }
+
+    int count{0};
+    for (int i{0}; i < 10; ++i) {
+      if (rods[i] == 7) ++count;
+    }
+
+    return count;
   }
 };
