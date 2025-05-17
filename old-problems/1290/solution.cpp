f29a4539fad752f2ca80c8abea283b31eaf064c4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-17 21:13:07 +0700

    feat(problem-1290/cpp): solve by using a while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1290/solution.cpp
+++ old-problems/1290/solution.cpp
@@ -1,6 +1,12 @@
 class Solution {
  public:
   int getDecimalValue(ListNode* head) {
-    return head->val;
+    int num{0};
+    while (head != nullptr) {
+      num <<= 1;
+      if (head->val != 0) num |= 1;
+      head = head->next;
+    }
+    return num;
   }
 };
