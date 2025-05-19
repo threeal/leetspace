c532b2a17834188b3376b6fbea9449135e901c5e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-19 09:46:24 +0700

    feat(problem-3024/cpp): solve by using if conditions
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3024/solution.cpp
+++ old-problems/3024/solution.cpp
@@ -4,7 +4,11 @@
 class Solution {
  public:
   std::string triangleType(std::vector<int>& nums) {
-    (void)nums;
-    return "";
+    const int a{nums[0]}, b{nums[1]}, c{nums[2]};
+    if (a + b > c && a + c > b && b + c > a) {
+      if (a == b) return b == c ? "equilateral" : "isosceles";
+      return a == c || b == c ? "isosceles" : "scalene";
+    }
+    return "none";
   }
 };
