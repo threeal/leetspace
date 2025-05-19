4dcb8d98392d335ac2f9a3ba1414cc0c75f8c84e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-19 16:57:44 +0700

    feat(problem-3340/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3340/solution.cpp
+++ old-problems/3340/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   bool isBalanced(std::string num) {
-    return num.empty();
+    int even{0}, odd{0};
+    for (std::size_t i{0}; i < num.size(); ++i) {
+      even += num[i] - '0';
+      if (++i >= num.size()) break;
+      odd += num[i] - '0';
+    }
+    return even == odd;
   }
 };
