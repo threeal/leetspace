72fd9ea48e2ec3bbd20d146c6d11738ca1ec895b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-19 10:06:44 +0700

    feat(problem-2778/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2778/solution.cpp
+++ old-problems/2778/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   int sumOfSquares(std::vector<int>& nums) {
-    return nums.size();
+    int sum{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      if (nums.size() % (i + 1) == 0) sum += nums[i] * nums[i];
+    }
+    return sum;
   }
 };
