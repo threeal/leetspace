2b205eb41d455dd989fcab6f9a0550ca13621959
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-19 16:42:25 +0700

    test: add test for problem 2215. Find the Difference of Two Arrays
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2215/solution.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> findDifference(
+      std::vector<int>& nums1, std::vector<int>& nums2) {
+    return {nums1, nums2};
+  }
+};
