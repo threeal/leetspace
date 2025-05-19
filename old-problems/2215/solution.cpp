46b4bfcb87009095964e84a349f3f6434b89fc71
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-19 16:46:07 +0700

    feat(problem-2215/cpp): solve by storing existence in a bitmap array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2215/solution.cpp
+++ old-problems/2215/solution.cpp
@@ -1,9 +1,30 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::vector<int>> findDifference(
       std::vector<int>& nums1, std::vector<int>& nums2) {
-    return {nums1, nums2};
+    std::vector<std::vector<int>> output(2);
+
+    std::vector<bool> exists(2001, false);
+    for (const int num : nums2) exists[num + 1000] = true;
+    for (const int num : nums1) {
+      if (!exists[num + 1000]) {
+        exists[num + 1000] = true;
+        output[0].push_back(num);
+      }
+    }
+
+    std::fill(exists.begin(), exists.end(), false);
+    for (const int num : nums1) exists[num + 1000] = true;
+    for (const int num : nums2) {
+      if (!exists[num + 1000]) {
+        exists[num + 1000] = true;
+        output[1].push_back(num);
+      }
+    }
+
+    return output;
   }
 };
