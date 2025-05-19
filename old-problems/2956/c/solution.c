a830e2dc10194338c41c1acbeb2471113f2b273a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-19 20:51:20 +0700

    test(problem-2956): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2956/c/solution.c
@@ -0,0 +1,5 @@
+int* findIntersectionValues(
+    int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+  *returnSize = nums1Size >= nums2Size ? nums1Size : nums2Size;
+  return nums1Size >= nums2Size ? nums1 : nums2;
+}
