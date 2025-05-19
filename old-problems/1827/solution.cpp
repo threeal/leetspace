2ecfe56d7e07e1990229588e5209bb47f8fe66f6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-19 11:19:35 +0700

    feat(problem-1827/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1827/solution.cpp
+++ old-problems/1827/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int minOperations(std::vector<int>& nums) {
-    return nums.size();
+    int prev{nums[0]}, sum{0};
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] > prev) {
+        prev = nums[i];
+      } else {
+        ++prev;
+        sum += prev - nums[i];
+      }
+    }
+    return sum;
   }
 };
