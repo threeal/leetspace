6dc64d7facd845d81a2440006496616c6dda1c2b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-19 22:44:22 +0700

    feat(problem-2194/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2194/c/solution.c
+++ old-problems/2194/c/solution.c
@@ -1,5 +1,21 @@
+#include <stdlib.h>
+
 char** cellsInRange(char* s, int* returnSize) {
-  (void)s;
-  *returnSize = 0;
-  return 0;
+  const int outputSize = (s[3] + 1 - s[0]) * (s[4] + 1 - s[1]);
+  char** output = malloc(outputSize * sizeof(char*));
+  char* buffer = malloc(outputSize * 3 * sizeof(char));
+
+  int n = 0;
+  for (char i = s[0]; i <= s[3]; ++i) {
+    for (char j = s[1]; j <= s[4]; ++j) {
+      output[n] = buffer + n * 3;
+      output[n][0] = i;
+      output[n][1] = j;
+      output[n][2] = 0;
+      ++n;
+    }
+  }
+
+  *returnSize = outputSize;
+  return output;
 }
