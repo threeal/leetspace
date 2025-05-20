c74e23439d2659d369aab7a41db4b0dc376dcd71
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-20 22:49:37 +0700

    feat(problem-1588/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1588/c/solution.c
+++ old-problems/1588/c/solution.c
@@ -1,3 +1,7 @@
 int sumOddLengthSubarrays(int* arr, int arrSize) {
-  return arr[arrSize - 1];
+  int sum = 0;
+  for (int i = 0; i < arrSize; ++i) {
+    sum += ((i + 1) * (arrSize - i) + 1) / 2 * arr[i];
+  }
+  return sum;
 }
