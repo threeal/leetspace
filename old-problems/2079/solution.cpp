6ebde5d7245610731971f3035e46ca58a13c6124
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-20 20:26:17 +0700

    feat(problem-2079/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2079/solution.cpp
+++ old-problems/2079/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int wateringPlants(std::vector<int>& plants, int capacity) {
-    return plants.size() + capacity;
+    int steps{0}, water{capacity};
+    for (std::size_t i{0}; i < plants.size(); ++i) {
+      if (water < plants[i]) {
+        steps += i + i;
+        water = capacity - plants[i];
+      } else {
+        water -= plants[i];
+      }
+    }
+    return steps + plants.size();
   }
 };
