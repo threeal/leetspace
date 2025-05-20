03af7e78857a2113150dc3c478e650c0be47cfac
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-20 20:12:19 +0700

    feat(problem-1305/cpp): solve by using DFS and sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1305/solution.cpp
+++ old-problems/1305/solution.cpp
@@ -1,8 +1,20 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-    return {root1->val, root2->val};
+    std::vector<int> output{};
+    if (root1 != nullptr) pushNodes(output, root1);
+    if (root2 != nullptr) pushNodes(output, root2);
+    std::sort(output.begin(), output.end());
+    return output;
+  }
+
+ private:
+  static void pushNodes(std::vector<int>& output, TreeNode* node) {
+    output.push_back(node->val);
+    if (node->left != nullptr) pushNodes(output, node->left);
+    if (node->right != nullptr) pushNodes(output, node->right);
   }
 };
