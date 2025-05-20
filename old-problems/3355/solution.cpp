6644ccf2fe3414d74f66365b39ec440f56a665f3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-20 12:32:47 +0700

    feat(problem-3355/cpp): solve by using diff array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3355/solution.cpp
+++ old-problems/3355/solution.cpp
@@ -4,6 +4,22 @@ class Solution {
  public:
   bool isZeroArray(
       std::vector<int>& nums, std::vector<std::vector<int>>& queries) {
-    return nums.empty() && queries.empty();
+    const int n = nums.size();
+    for (int i{n - 1}; i > 0; --i) {
+      nums[i] -= nums[i - 1];
+    }
+
+    for (const std::vector<int>& query : queries) {
+      --nums[query[0]];
+      if (query[1] + 1 < n) ++nums[query[1] + 1];
+    }
+
+    int num{0};
+    for (int i{0}; i < n; ++i) {
+      num += nums[i];
+      if (num > 0) return false;
+    }
+
+    return true;
   }
 };
