1cd347a3f199f886b6e5aa73504f6566878519f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-20 18:29:15 +0700

    feat(problem-2442/cpp): solve by pushing reversed integers and using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2442/solution.cpp
+++ old-problems/2442/solution.cpp
@@ -1,8 +1,28 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int countDistinctIntegers(std::vector<int>& nums) {
-    return nums.size();
+    const std::size_t n{nums.size()};
+    nums.reserve(n * 2);
+
+    for (std::size_t i{0}; i < n; ++i) {
+      int num{nums[i]}, rnum{0};
+      while (num > 0) {
+        rnum = rnum * 10 + num % 10;
+        num /= 10;
+      }
+      nums.push_back(rnum);
+    }
+
+    std::sort(nums.begin(), nums.end());
+
+    int count{1};
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] != nums[i - 1]) ++count;
+    }
+
+    return count;
   }
 };
