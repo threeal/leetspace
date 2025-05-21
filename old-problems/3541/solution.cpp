c6bcfe3b5d24db2ab76f74bfdffb92005db2bab1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-21 13:10:26 +0700

    feat(problem-3541/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3541/solution.cpp
+++ old-problems/3541/solution.cpp
@@ -3,6 +3,25 @@
 class Solution {
  public:
   int maxFreqSum(std::string s) {
-    return s.size();
+    int freqs[26]{0};
+    for (const char c : s) ++freqs[c - 'a'];
+
+    int vowels{0}, consonants{0};
+    for (int i{0}; i < 26; ++i) {
+      switch (i) {
+        case 0:
+        case 4:
+        case 8:
+        case 14:
+        case 20:
+          if (freqs[i] > vowels) vowels = freqs[i];
+          break;
+
+        default:
+          if (freqs[i] > consonants) consonants = freqs[i];
+      }
+    }
+
+    return vowels + consonants;
   }
 };
