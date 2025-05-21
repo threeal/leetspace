df1ab82e4ccd41396dae13ed0db1074039ea4891
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-21 14:46:44 +0700

    feat(problem-2656/cpp): solve by finding max element
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2656/solution.cpp
+++ old-problems/2656/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   int maximizeSum(std::vector<int>& nums, int k) {
-    return nums.front() + k;
+    int max{nums[0]};
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] > max) max = nums[i];
+    }
+    return k * (2 * max + k - 1) / 2;
   }
 };
