51fbf912648663e8e54e51333387ab96a177b6c2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-22 11:56:35 +0700

    feat(problem-728/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0728/solution.cpp
+++ old-problems/0728/solution.cpp
@@ -3,6 +3,18 @@
 class Solution {
  public:
   std::vector<int> selfDividingNumbers(int left, int right) {
-    return {left, right};
+    std::vector<int> output{};
+    for (int num{left}; num <= right; ++num) {
+      bool valid{true};
+      for (int exp{1}; exp <= num; exp *= 10) {
+        const int digit{num / exp % 10};
+        if (digit == 0 || num % digit != 0) {
+          valid = false;
+          break;
+        }
+      }
+      if (valid) output.push_back(num);
+    }
+    return output;
   }
 };
