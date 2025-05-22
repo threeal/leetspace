08fbfd07a2efa61077c7721f0744e7476095078b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-22 17:35:29 +0700

    feat(problem-1812/cpp): solve by comparing parity
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1812/solution.cpp
+++ old-problems/1812/solution.cpp
@@ -3,6 +3,6 @@
 class Solution {
  public:
   bool squareIsWhite(std::string coordinates) {
-    return coordinates.empty();
+    return (coordinates[0] - 'a') % 2 != (coordinates[1] - '1') % 2;
   }
 };
