3fe8b499486a235cb11ca6c1e398cd4aaf072815
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-22 18:09:47 +0700

    feat(problem-1252/cpp): solve by tracking rows and cols to be incremented
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1252/solution.cpp
+++ old-problems/1252/solution.cpp
@@ -3,6 +3,20 @@
 class Solution {
  public:
   int oddCells(int m, int n, std::vector<std::vector<int>>& indices) {
-    return m + n + indices.front().front();
+    std::vector<int> rows(m, 0), cols(n, 0);
+    for (const std::vector<int>& index : indices) {
+      ++rows[index[0]];
+      ++cols[index[1]];
+    }
+
+    int count{0};
+    for (int i{0}; i < m; ++i) {
+      const int rowParity{rows[i] % 2};
+      for (int j{0}; j < n; ++j) {
+        if (rowParity != cols[j] % 2) ++count;
+      }
+    }
+
+    return count;
   }
 };
