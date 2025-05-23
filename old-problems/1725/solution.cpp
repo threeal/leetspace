f7ba4ab1011b851e6c74be8521e4ed2ce14eae6b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-23 15:09:51 +0700

    feat(problem-172/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1725/solution.cpp
+++ old-problems/1725/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   int countGoodRectangles(std::vector<std::vector<int>>& rectangles) {
-    return rectangles.size();
+    int count{0}, maxK{0};
+    for (const std::vector<int>& rectangle : rectangles) {
+      const int k{rectangle[0] <= rectangle[1] ? rectangle[0] : rectangle[1]};
+      if (k < maxK) continue;
+      if (k == maxK) {
+        ++count;
+      } else {
+        maxK = k;
+        count = 1;
+      }
+    }
+    return count;
   }
 };
