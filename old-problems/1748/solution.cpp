605d0190bed743dc30c682d42b928ec75708bf6c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-23 14:42:36 +0700

    feat(problem-1748/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1748/solution.cpp
+++ old-problems/1748/solution.cpp
@@ -3,6 +3,18 @@
 class Solution {
  public:
   int sumOfUnique(std::vector<int>& nums) {
-    return nums.size();
+    int sum{0}, freqs[101]{0};
+    for (const int num : nums) {
+      switch (++freqs[num]) {
+        case 1:
+          sum += num;
+          break;
+
+        case 2:
+          sum -= num;
+          break;
+      }
+    }
+    return sum;
   }
 };
