9f40f6b17a35a0fbd83b58f609bc2bb9f975b4c9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-27 10:10:42 +0700

    feat(problem-3300/cpp): solve by finding min digit sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3300/solution.cpp
+++ old-problems/3300/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int minElement(std::vector<int>& nums) {
-    return nums.size();
+    int minSum{nums.front()};
+    for (int num : nums) {
+      int sum{0};
+      while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+      }
+      if (sum < minSum) minSum = sum;
+    }
+    return minSum;
   }
 };
