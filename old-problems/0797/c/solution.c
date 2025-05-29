d90668e04f81ac6ccaaf4ea66f9e1b1aa81ace4d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-29 15:36:07 +0700

    test(problem-797): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0797/c/solution.c
@@ -0,0 +1,7 @@
+int** allPathsSourceTarget(
+    int** graph, int graphSize, int* graphColSize,
+    int* returnSize, int** returnColumnSizes) {
+  *returnSize = graphSize;
+  *returnColumnSizes = graphColSize;
+  return graph;
+}
