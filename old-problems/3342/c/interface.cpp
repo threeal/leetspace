c50664810e2dfbaa720db2e7dba18ea7063e3312
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-29 15:09:33 +0700

    test(problem-3342): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3342/c/interface.cpp
@@ -0,0 +1,17 @@
+#include <cstring>
+#include <vector>
+
+extern "C" {
+int minTimeToReach(int** moveTime, int moveTimeSize, int* moveTimeColSize);
+}
+
+int solution_c(std::vector<std::vector<int>> moveTime) {
+  std::vector<int*> moveTimeDatas(moveTime.size());
+  std::vector<int> moveTimeSizes(moveTime.size());
+  for (std::size_t i{0}; i < moveTime.size(); ++i) {
+    moveTimeDatas[i] = moveTime[i].data();
+    moveTimeSizes[i] = moveTime[i].size();
+  }
+
+  return minTimeToReach(moveTimeDatas.data(), moveTime.size(), moveTimeSizes.data());
+}
