b8293423ee5fc8ad9885f157a41e79b3259ab4e4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-30 12:31:34 +0700

    feat(problem-2315/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2315/solution.cpp
+++ old-problems/2315/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int countAsterisks(std::string s) {
-    return s.size();
+    bool ignore{false};
+    int count{0};
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      if (s[i] == '|') {
+        ignore = !ignore;
+      } else if (s[i] == '*' && !ignore) {
+        ++count;
+      }
+    }
+    return count;
   }
 };
