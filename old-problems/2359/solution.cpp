917534bdae4841f467bdeac4272047c086f2e4bd
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-30 12:19:49 +0700

    feat(problem-2359/cpp): solve by using DFS approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2359/solution.cpp
+++ old-problems/2359/solution.cpp
@@ -1,9 +1,37 @@
+#include <algorithm>
+#include <limits>
 #include <vector>
 
 class Solution {
  public:
   int closestMeetingNode(
       std::vector<int>& edges, int node1, int node2) {
-    return edges.front() + node1 + node2;
+    int dist{0};
+    std::vector<int> dist1(edges.size(), -1);
+    while (node1 != -1 && dist1[node1] == -1) {
+      dist1[node1] = dist++;
+      node1 = edges[node1];
+    }
+
+    dist = 0;
+    std::vector<int> dist2(edges.size(), -1);
+    while (node2 != -1 && dist2[node2] == -1) {
+      dist2[node2] = dist++;
+      node2 = edges[node2];
+    }
+
+    int maxDist{std::numeric_limits<int>::max()};
+    int res{-1};
+    for (std::size_t i{0}; i < dist1.size(); ++i) {
+      if (dist1[i] != -1 && dist2[i] != -1) {
+        const int dist{std::max(dist1[i], dist2[i])};
+        if (dist < maxDist) {
+          maxDist = dist;
+          res = i;
+        }
+      }
+    }
+
+    return res;
   }
 };
