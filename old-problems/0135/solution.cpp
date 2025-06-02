76b9475130ae6076b6db3d60f15bd6bce41376c2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-02 09:56:18 +0700

    feat(problem-135/cpp): solve by using double pass greedy
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0135/solution.cpp
+++ old-problems/0135/solution.cpp
@@ -3,6 +3,21 @@
 class Solution {
  public:
   int candy(std::vector<int>& ratings) {
-    return ratings.front();
+    std::vector<int> candies(ratings.size(), 1);
+    for (std::size_t i{1}; i < ratings.size(); ++i) {
+      if (ratings[i] > ratings[i - 1]) {
+        candies[i] = candies[i - 1] + 1;
+      }
+    }
+
+    int total{candies.back()};
+    for (std::size_t i{ratings.size() - 1}; i > 0; --i) {
+      if (ratings[i - 1] > ratings[i] && candies[i - 1] <= candies[i]) {
+        candies[i - 1] = candies[i] + 1;
+      }
+      total += candies[i - 1];
+    }
+
+    return total;
   }
 };
