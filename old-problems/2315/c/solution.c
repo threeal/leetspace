c82d3c0659925d22487baf9fb9c17dfd5a443d70
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-03 21:24:38 +0700

    feat(problem-2315/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2315/c/solution.c
+++ old-problems/2315/c/solution.c
@@ -1,3 +1,14 @@
+#include <stdbool.h>
+
 int countAsterisks(char* s) {
-  return *s;
+  bool ignore = false;
+  int count = 0;
+  for (int i = 0; s[i] != 0; ++i) {
+    if (s[i] == '|') {
+      ignore = !ignore;
+    } else if (s[i] == '*' && !ignore) {
+      ++count;
+    }
+  }
+  return count;
 }
