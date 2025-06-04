e30b1ebcdf548a3898374d522f5e76aea8839eb6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-04 10:55:40 +0700

    feat(problem-2710/cpp): solve by using a while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2710/solution.cpp
+++ old-problems/2710/solution.cpp
@@ -3,6 +3,9 @@
 class Solution {
  public:
   std::string removeTrailingZeros(std::string num) {
+    int n = num.size() - 1;
+    while (n >= 0 && num[n] == '0') --n;
+    num.resize(n + 1);
     return num;
   }
 };
