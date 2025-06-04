a613e85a7b18f822d4417a36c9e014f00388e98a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-04 09:42:22 +0700

    feat(problem-2221/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2221/solution.cpp
+++ old-problems/2221/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   int triangularSum(std::vector<int>& nums) {
+    for (std::size_t n{nums.size()}; n > 1; --n) {
+      for (std::size_t i{1}; i < n; ++i) {
+        nums[i - 1] = (nums[i - 1] + nums[i]) % 10;
+      }
+    }
     return nums.front();
   }
 };
