f45433b0b81032b615cad06e5e19402ae16b61bb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-04 10:42:38 +0700

    feat(problem-1941/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1941/solution.cpp
+++ old-problems/1941/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   bool areOccurrencesEqual(std::string s) {
-    return s.empty();
+    char freqs[26]{0};
+    for (const char c : s) ++freqs[c - 'a'];
+
+    for (int i{0}; i < 26; ++i) {
+      if (freqs[i] != 0) {
+        const int freq{freqs[i]};
+        for (int j{i + 1}; j < 26; ++j) {
+          if (freqs[j] != 0 && freqs[j] != freq) return false;
+        }
+        return true;
+      }
+    }
+
+    return true;
   }
 };
