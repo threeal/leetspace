f0b2e69e1ed326f844e58eab698fed13303802bb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-05 13:40:27 +0700

    feat(problem-1374/cpp): solve by returning string with the same character
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1374/solution.cpp
+++ old-problems/1374/solution.cpp
@@ -1,8 +1,11 @@
+#include <cstring>
 #include <string>
 
 class Solution {
  public:
   std::string generateTheString(int n) {
-    return std::string('a', n);
+    std::string s(n, 'a');
+    if (s.size() % 2 == 0) s[0] = 'b';
+    return s;
   }
 };
