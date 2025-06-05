ace3c55faba6a24b0998f669934fc90d7d2cb2fa
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-05 10:21:02 +0700

    feat(problem-3168/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3168/solution.cpp
+++ old-problems/3168/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int minimumChairs(std::string s) {
-    return s.size();
+    int maxChairs{0}, chairs{0};
+    for (const char c : s) {
+      if (c == 'E') {
+        if (++chairs > maxChairs) maxChairs = chairs;
+      } else {
+        --chairs;
+      }
+    }
+    return maxChairs;
   }
 };
