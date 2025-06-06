bc98f111144259ab9d8605fbcb49361459671364
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-06 23:16:03 +0700

    feat(problem-3131/c): solve by comparing the difference between minimums
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3131/c/solution.c
+++ old-problems/3131/c/solution.c
@@ -1,3 +1,9 @@
 int addedInteger(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-  return nums1[nums1Size - 1] + nums2[nums2Size - 1];
+  (void)nums2Size;
+  int min1 = nums1[0], min2 = nums2[0];
+  for (int i = 0; i < nums1Size; ++i) {
+    if (nums1[i] < min1) min1 = nums1[i];
+    if (nums2[i] < min2) min2 = nums2[i];
+  }
+  return min2 - min1;
 }
