bdd4793dd63b84304d0b057e09b94b061654f507
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-06 23:11:06 +0700

    test(problem-2900): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2900/c/interface.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+extern "C" {
+char** getLongestSubsequence(
+    char** words, int wordsSize, int* groups, int groupsSize, int* returnSize);
+}
+
+std::vector<std::string> solution_c(
+    std::vector<std::string> words, std::vector<int> groups) {
+  std::vector<char*> wordsDatas(words.size());
+  for (std::size_t i{0}; i < words.size(); ++i) {
+    wordsDatas[i] = words[i].data();
+  }
+
+  int returnSize{};
+  char** returnData{
+      getLongestSubsequence(
+          wordsDatas.data(), words.size(), groups.data(), groups.size(),
+          &returnSize)};
+
+  std::vector<std::string> output(returnSize);
+  for (int i{0}; i < returnSize; ++i) {
+    output[i] = returnData[i];
+  }
+
+  return output;
+}
---
+++ old-problems/2900/c/solution.c
@@ -0,0 +1,7 @@
+char** getLongestSubsequence(
+    char** words, int wordsSize, int* groups, int groupsSize, int* returnSize) {
+  (void)groups;
+  (void)groupsSize;
+  *returnSize = wordsSize;
+  return words;
+}
