fc5c91d1f02aaaac5aa362b27928d235bfa9998a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-10 10:08:23 +0700

    feat(problem-3442/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3442/solution.cpp
+++ old-problems/3442/solution.cpp
@@ -1,8 +1,26 @@
+#include <algorithm>
+#include <limits>
 #include <string>
 
 class Solution {
  public:
   int maxDifference(std::string s) {
-    return s.size();
+    int freqs[26]{0};
+    for (const char c : s) ++freqs[c - 'a'];
+
+    int even{std::numeric_limits<int>::max()};
+    int odd{std::numeric_limits<int>::min()};
+
+    for (int i{0}; i < 26; ++i) {
+      if (freqs[i] > 0) {
+        if (freqs[i] % 2 == 0) {
+          if (freqs[i] < even) even = freqs[i];
+        } else {
+          if (freqs[i] > odd) odd = freqs[i];
+        }
+      }
+    }
+
+    return odd - even;
   }
 };
