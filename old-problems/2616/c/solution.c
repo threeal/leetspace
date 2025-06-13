d7f1adcb816460bcad46e1d63584506a7f9deb60
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-13 10:06:00 +0700

    feat(problem-2616/c): solve by using a binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2616/c/solution.c
+++ old-problems/2616/c/solution.c
@@ -1,3 +1,26 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int minimizeMax(int* nums, int numsSize, int p) {
-  return nums[numsSize - 1] + p;
+  qsort(nums, numsSize, sizeof(int), compare);
+  int left = 0, right = nums[numsSize - 1] - nums[0];
+  while (left < right) {
+    const int mid = left + (right - left) / 2;
+    int pairs = 0;
+    for (int i = 1; i < numsSize; ++i) {
+      if (nums[i] - nums[i - 1] <= mid) {
+        ++pairs;
+        ++i;
+      }
+    }
+    if (pairs >= p) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
 }
