c43a645266cfe38941144fc0c2269bcb7465a115
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-13 09:26:59 +0700

    feat(problem-2616/cpp): solve by using a binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2616/solution.cpp
+++ old-problems/2616/solution.cpp
@@ -1,8 +1,26 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int minimizeMax(std::vector<int>& nums, int p) {
-    return nums.front() + p;
+    std::sort(nums.begin(), nums.end());
+    int left{0}, right{nums.back() - nums.front()};
+    while (left < right) {
+      const int mid{left + (right - left) / 2};
+      int pairs{0};
+      for (std::size_t i{1}; i < nums.size(); ++i) {
+        if (nums[i] - nums[i - 1] <= mid) {
+          ++pairs;
+          ++i;
+        }
+      }
+      if (pairs >= p) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return left;
   }
 };
