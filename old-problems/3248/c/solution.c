d4fae9cbf79c95ee5d8a3851d7f7c1dca5f0794c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-15 21:11:11 +0700

    feat(problem-3248/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3248/c/solution.c
+++ old-problems/3248/c/solution.c
@@ -1,3 +1,23 @@
 int finalPositionOfSnake(int n, char** commands, int commandsSize) {
-  return n + *commands[commandsSize - 1];
+  int position = 0;
+  for (int i = 0; i < commandsSize; ++i) {
+    switch (commands[i][0]) {
+      case 'U':
+        position -= n;
+        break;
+
+      case 'R':
+        ++position;
+        break;
+
+      case 'D':
+        position += n;
+        break;
+
+      case 'L':
+        --position;
+        break;
+    }
+  }
+  return position;
 }
