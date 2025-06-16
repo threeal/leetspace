7799ef92966175c0d127e978e19b4ed1d0da95f5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-16 08:25:30 +0700

    feat(problem-2016/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2016/solution.cpp
+++ old-problems/2016/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int maximumDifference(std::vector<int>& nums) {
-    return nums[0];
+    int diff{-1}, min{nums[0]};
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] == min) continue;
+      if (nums[i] < min) {
+        min = nums[i];
+      } else if (nums[i] - min > diff) {
+        diff = nums[i] - min;
+      }
+    }
+    return diff;
   }
 };
