a215732a0d149d8042839486ebe050fc5d3d3cdb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-16 09:01:09 +0700

    feat(problem-2744/c): solve by using brute force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2744/c/solution.c
+++ old-problems/2744/c/solution.c
@@ -1,3 +1,9 @@
 int maximumNumberOfStringPairs(char** words, int wordsSize) {
-  return *words[wordsSize - 1];
+  int count = 0;
+  for (int j = 1; j < wordsSize; ++j) {
+    for (int i = 0; i < j; ++i) {
+      if (words[i][0] == words[j][1] && words[i][1] == words[j][0]) ++count;
+    }
+  }
+  return count;
 }
