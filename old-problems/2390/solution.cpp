b5aa82c47205708f249f677c2f3ac024ca1ef9d6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-18 09:37:54 +0700

    feat(problem-2390): solve by using stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2390/solution.cpp
+++ old-problems/2390/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   std::string removeStars(std::string s) {
+    std::size_t n{0};
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      if (s[i] == '*') {
+        --n;
+      } else {
+        s[n++] = s[i];
+      }
+    }
+    s.resize(n);
     return s;
   }
 };
