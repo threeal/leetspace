dd3ba46fd07a9bdc99c3a094e54c8cc579a8a26e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-18 10:33:18 +0700

    feat(problem-3258/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3258/solution.cpp
+++ old-problems/3258/solution.cpp
@@ -3,6 +3,26 @@
 class Solution {
  public:
   int countKConstraintSubstrings(std::string s, int k) {
-    return s.size() + k;
+    std::size_t count{0};
+    int zeros{0}, ones{0};
+    for (std::size_t l{0}, r{0}; r < s.size(); ++r) {
+      if (s[r] == '0') {
+        ++zeros;
+      } else {
+        ++ones;
+      }
+
+      while (zeros > k && ones > k) {
+        if (s[l] == '0') {
+          --zeros;
+        } else {
+          --ones;
+        }
+        ++l;
+      }
+
+      count += r - l + 1;
+    }
+    return count;
   }
 };
