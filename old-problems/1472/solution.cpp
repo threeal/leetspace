eb9dd3882e4e56db1c19ca2a8de9c4b1a57f8ee4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-19 09:58:13 +0700

    feat(problem-1472/cpp): solve by using `std::vector` as a stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1472/solution.cpp
+++ old-problems/1472/solution.cpp
@@ -1,22 +1,35 @@
 #include <string>
+#include <vector>
 
 class BrowserHistory {
+ private:
+  std::vector<std::string> urls;
+  int current;
+
  public:
-  BrowserHistory(std::string homepage) {
-    (void)homepage;
-  }
+  BrowserHistory(std::string homepage) : urls{{homepage}}, current{0} {}
 
   void visit(std::string url) {
-    (void)url;
+    ++current;
+    urls.resize(current);
+    urls.push_back(url);
   }
 
   std::string back(int steps) {
-    (void)steps;
-    return "";
+    if (steps > current) {
+      current = 0;
+    } else {
+      current -= steps;
+    }
+    return urls[current];
   }
 
   std::string forward(int steps) {
-    (void)steps;
-    return "";
+    if (static_cast<std::size_t>(current + steps) >= urls.size()) {
+      current = urls.size() - 1;
+    } else {
+      current += steps;
+    }
+    return urls[current];
   }
 };
