d42444d0b911993a4f9f7587d12c435bd268c5c1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-19 09:16:10 +0700

    feat(problem-2294/cpp): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2294/solution.cpp
+++ old-problems/2294/solution.cpp
@@ -1,8 +1,17 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int partitionArray(std::vector<int>& nums, int k) {
-    return nums.front() + k;
+    std::sort(nums.begin(), nums.end());
+    int count{0}, max{-1};
+    for (const int num : nums) {
+      if (num > max) {
+        ++count;
+        max = num + k;
+      }
+    }
+    return count;
   }
 };
