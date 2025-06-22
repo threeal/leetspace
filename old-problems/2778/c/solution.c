3f320e48e8141e94bf87f724cfb213893253026a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-22 20:27:28 +0700

    feat(problem-2778/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2778/c/solution.c
+++ old-problems/2778/c/solution.c
@@ -1,3 +1,7 @@
 int sumOfSquares(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int sum = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (numsSize % (i + 1) == 0) sum += nums[i] * nums[i];
+  }
+  return sum;
 }
