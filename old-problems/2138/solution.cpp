4f71a269a655c9b0a6de6d6ccc73168bae4a42e8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-22 20:13:47 +0700

    feat(problem-2138/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2138/solution.cpp
+++ old-problems/2138/solution.cpp
@@ -4,6 +4,11 @@
 class Solution {
  public:
   std::vector<std::string> divideString(std::string s, int k, char fill) {
-    return {s, std::string(k, fill)};
+    std::vector<std::string> output((s.size() + k - 1) / k);
+    for (std::size_t i{0}; i < s.size(); i += k) {
+      output[i / k] = s.substr(i, k);
+    }
+    output.back().resize(k, fill);
+    return output;
   }
 };
