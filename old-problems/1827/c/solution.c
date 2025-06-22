52f8173262388897d1a68c8a20afdc6416cd36cc
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-22 20:33:05 +0700

    feat(problem-1827/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1827/c/solution.c
+++ old-problems/1827/c/solution.c
@@ -1,3 +1,12 @@
 int minOperations(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int prev = nums[0], sum = 0;
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i] > prev) {
+      prev = nums[i];
+    } else {
+      ++prev;
+      sum += prev - nums[i];
+    }
+  }
+  return sum;
 }
