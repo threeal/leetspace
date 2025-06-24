a10d72f072e179af46ddf2ce56c0ac3f0bd93511
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-24 08:31:10 +0700

    test: add test for problem 2200. Find All K-Distant Indices in an Array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2200/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> findKDistantIndices(std::vector<int>& nums, int key, int k) {
+    (void)key;
+    (void)k;
+    return nums;
+  }
+};
