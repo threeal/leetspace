0925e594cca017a4a0a0328b0db2480b4cbe31c7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-24 08:37:10 +0700

    feat(problem-2200/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2200/solution.cpp
+++ old-problems/2200/solution.cpp
@@ -3,8 +3,22 @@
 class Solution {
  public:
   std::vector<int> findKDistantIndices(std::vector<int>& nums, int key, int k) {
-    (void)key;
-    (void)k;
-    return nums;
+    const int n = nums.size();
+    std::vector<int> indices{};
+    for (int i{0}; i < n; ++i) {
+      if (nums[i] == key) {
+        int l{i > k ? i - k : 0};
+        if (!indices.empty() && indices.back() + 1 > l) {
+          l = indices.back() + 1;
+        }
+
+        const int r{i + k < n ? i + k : n - 1};
+        while (l <= r) {
+          indices.push_back(l);
+          ++l;
+        }
+      }
+    }
+    return indices;
   }
 };
