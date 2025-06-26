28a4d86a5add6e349f405142781dad98ea354e92
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-26 20:05:36 +0700

    feat(problem-2311/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2311/solution.cpp
+++ old-problems/2311/solution.cpp
@@ -1,8 +1,20 @@
+#include <bit>
 #include <string>
 
 class Solution {
  public:
   int longestSubsequence(std::string s, int k) {
-    return s.size() + k;
+    unsigned long long num{0};
+    int count{0}, maxCount{0};
+    for (const char c : s) {
+      ++count;
+      num = num << 1 | (c == '0' ? 0 : 1);
+      while (num > static_cast<unsigned long long>(k)) {
+        num &= ~(1 << (63 - std::countl_zero<unsigned long long>(num)));
+        --count;
+      }
+      if (count > maxCount) maxCount = count;
+    }
+    return maxCount;
   }
 };
