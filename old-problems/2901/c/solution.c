cf442769d0302c6ac4cdd9a345f34a8faf6f567b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-26 12:29:06 +0700

    test(problem-2901): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2901/c/solution.c
@@ -0,0 +1,7 @@
+char** getWordsInLongestSubsequence(
+    char** words, int wordsSize, int* groups, int groupsSize, int* returnSize) {
+  (void)groups;
+  (void)groupsSize;
+  *returnSize = wordsSize;
+  return words;
+}
