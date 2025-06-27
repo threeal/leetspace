e15b7eb1d401aa326479a9e9d560c0a0f3b663b6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-27 21:46:23 +0700

    feat(problem-3158/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3158/solution.cpp
+++ old-problems/3158/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int duplicateNumbersXOR(std::vector<int>& nums) {
-    return nums.front();
+    bool exists[51]{false};
+    int ans{0};
+    for (const auto num : nums) {
+      if (exists[num]) {
+        ans ^= num;
+      } else {
+        exists[num] = true;
+      }
+    }
+    return ans;
   }
 };
