145d023c87fc9d6f053e02b685b27388dd6005fe
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-27 21:36:59 +0700

    feat(problem-3340/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3340/c/solution.c
+++ old-problems/3340/c/solution.c
@@ -1,5 +1,11 @@
 #include <stdbool.h>
 
 bool isBalanced(char* num) {
-  return num == 0;
+  int even = 0, odd = 0;
+  for (int i = 0; num[i] != 0; ++i) {
+    even += num[i] - '0';
+    if (num[++i] == 0) break;
+    odd += num[i] - '0';
+  }
+  return even == odd;
 }
