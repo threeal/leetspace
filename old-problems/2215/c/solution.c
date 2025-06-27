1284890b28c82cb7866cb3745c16e5e5420204f2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-27 16:40:21 +0700

    test(problem-2215): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2215/c/solution.c
@@ -0,0 +1,11 @@
+int** findDifference(
+    int* nums1, int nums1Size, int* nums2, int nums2Size,
+    int* returnSize, int** returnColumnSizes) {
+  (void)nums1;
+  (void)nums1Size;
+  (void)nums2;
+  (void)nums2Size;
+  *returnSize = 0;
+  *returnColumnSizes = 0;
+  return 0;
+}
