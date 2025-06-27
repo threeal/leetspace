d70a452757f416919d4724910b5a42aad67d07f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-27 22:15:33 +0700

    feat(problem-1351/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1351/solution.cpp
+++ old-problems/1351/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   int countNegatives(std::vector<std::vector<int>>& grid) {
-    return grid.size();
+    const int n = grid[0].size() - 1;
+    int count{0}, j{n};
+    for (std::size_t i{0}; i < grid.size(); ++i) {
+      while (j >= 0 && grid[i][j] < 0) --j;
+      count += n - j;
+    }
+    return count;
   }
 };
