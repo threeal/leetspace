535a0567d46381a41a0078bf5cc12b0faf0c6792
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-27 22:02:45 +0700

    feat(problem-1974/cpp): solve by counting the difference between each character
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1974/solution.cpp
+++ old-problems/1974/solution.cpp
@@ -1,8 +1,15 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   int minTimeToType(std::string word) {
-    return word.size();
+    int steps{std::min(word[0] - 'a', 26 - word[0] + 'a')};
+    for (std::size_t i{1}; i < word.size(); ++i) {
+      steps += word[i] >= word[i - 1]
+          ? std::min(word[i] - word[i - 1], 26 - word[i] + word[i - 1])
+          : std::min(word[i - 1] - word[i], 26 - word[i - 1] + word[i]);
+    }
+    return steps + word.size();
   }
 };
