7e309e8e9625dd8e69a6181633da28d6aa2ad9e0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-03 21:22:40 +0700

    feat(problem-3158/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3158/c/solution.c
+++ old-problems/3158/c/solution.c
@@ -1,3 +1,14 @@
+#include <stdbool.h>
+
 int duplicateNumbersXOR(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  bool exists[51] = {false};
+  int ans = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (exists[nums[i]]) {
+      ans ^= nums[i];
+    } else {
+      exists[nums[i]] = true;
+    }
+  }
+  return ans;
 }
