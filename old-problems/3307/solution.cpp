db018deb4dc34c10913a753ed013aa65c6079307
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-04 09:02:52 +0700

    feat(problem-3307/cpp): solve by checking bit positions
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3307/solution.cpp
+++ old-problems/3307/solution.cpp
@@ -3,6 +3,13 @@
 class Solution {
  public:
   char kthCharacter(long long k, std::vector<int>& operations) {
-    return k + operations.size();
+    --k;
+    int increment{0};
+    for (int i{0}; (k >> i) != 0; ++i) {
+      if ((k >> i & 1) != 0) {
+        increment += operations[i];
+      }
+    }
+    return 'a' + (increment % 26);
   }
 };
