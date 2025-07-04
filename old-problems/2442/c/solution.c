e6e0b5be3fd5f23548fe22e5a038cc5e1c734349
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-04 21:37:00 +0700

    feat(problem-2442/c): solve by pushing reversed integers and using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2442/c/solution.c
+++ old-problems/2442/c/solution.c
@@ -1,3 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int countDistinctIntegers(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int* nums2 = malloc(numsSize * 2 * sizeof(int));
+  memcpy(nums2, nums, numsSize * sizeof(int));
+  int nums2Size = numsSize;
+
+  for (int i = 0; i < numsSize; ++i) {
+    int num = nums2[i], rnum = 0;
+    while (num > 0) {
+      rnum = rnum * 10 + num % 10;
+      num /= 10;
+    }
+    nums2[nums2Size++] = rnum;
+  }
+
+  qsort(nums2, nums2Size, sizeof(int), compare);
+
+  int count = 1;
+  for (int i = 1; i < nums2Size; ++i) {
+    if (nums2[i] != nums2[i - 1]) ++count;
+  }
+
+  return count;
 }
