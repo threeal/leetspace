1a3dbdfc7de8996e94c8ab2abc1f0ec5b6610cdc
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-05 14:47:09 +0700

    test(problem-3355): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3355/c/solution.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+
+bool isZeroArray(
+    int* nums, int numsSize,
+    int** queries, int queriesSize, int* queriesColSize) {
+  return nums[numsSize - 1] ==
+      queries[queriesSize - 1][queriesColSize[queriesSize - 1] - 1];
+}
