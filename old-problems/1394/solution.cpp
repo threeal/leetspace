33ab38af8e990b40599bd957e720f54a2ae7f642
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-05 14:49:57 +0700

    feat(problem-1394/cpp): solve by using sorting and for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1394/solution.cpp
+++ old-problems/1394/solution.cpp
@@ -1,8 +1,20 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int findLucky(std::vector<int>& arr) {
-    return arr.front();
+    const int n = arr.size();
+    std::sort(arr.begin(), arr.end(), std::greater<int>());
+
+    int l{0};
+    for (int r{1}; r < n; ++r) {
+      if (arr[r] != arr[r - 1]) {
+        if (r - l == arr[r - 1]) return arr[r - 1];
+        l = r;
+      }
+    }
+
+    return (n - l == arr.back()) ? arr.back() : -1;
   }
 };
