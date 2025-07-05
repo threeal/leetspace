0a2aa34f9519ae79ce184620f0f532061b703c86
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-05 14:48:24 +0700

    feat(problem-3355/c): solve by using diff array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3355/c/solution.c
+++ old-problems/3355/c/solution.c
@@ -3,6 +3,22 @@
 bool isZeroArray(
     int* nums, int numsSize,
     int** queries, int queriesSize, int* queriesColSize) {
-  return nums[numsSize - 1] ==
-      queries[queriesSize - 1][queriesColSize[queriesSize - 1] - 1];
+  (void)queriesColSize;
+
+  for (int i = numsSize - 1; i > 0; --i) {
+    nums[i] -= nums[i - 1];
+  }
+
+  for (int i = 0; i < queriesSize; ++i) {
+    --nums[queries[i][0]];
+    if (queries[i][1] + 1 < numsSize) ++nums[queries[i][1] + 1];
+  }
+
+  int num = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    num += nums[i];
+    if (num > 0) return false;
+  }
+
+  return true;
 }
