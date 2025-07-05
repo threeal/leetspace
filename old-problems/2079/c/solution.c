6a13669cc81e8ad841f45b0e8b5a0162c77c6c82
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-05 15:02:24 +0700

    feat(problem-2079/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2079/c/solution.c
+++ old-problems/2079/c/solution.c
@@ -1,3 +1,12 @@
 int wateringPlants(int* plants, int plantsSize, int capacity) {
-  return plants[plantsSize - 1] + capacity;
+  int steps = 0, water = capacity;
+  for (int i = 0; i < plantsSize; ++i) {
+    if (water < plants[i]) {
+      steps += i + i;
+      water = capacity - plants[i];
+    } else {
+      water -= plants[i];
+    }
+  }
+  return steps + plantsSize;
 }
