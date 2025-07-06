d6d2345edcdb5f4d82928051a70eed5b6c9d29eb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-06 13:10:30 +0700

    feat(problem-1865/cpp): solve by tracking frequencies of `nums2`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1865/solution.cpp
+++ old-problems/1865/solution.cpp
@@ -1,18 +1,30 @@
+#include <algorithm>
+#include <unordered_map>
 #include <vector>
 
 class FindSumPairs {
+  std::vector<int>&nums1{nums1}, nums2{nums2};
+  std::unordered_map<int, int> freqs{};
+
  public:
-  FindSumPairs(std::vector<int>& nums1, std::vector<int>& nums2) {
-    (void)nums1;
-    (void)nums2;
+  FindSumPairs(std::vector<int>& nums1, std::vector<int>& nums2) : nums1{nums1}, nums2{nums2} {
+    std::sort(this->nums1.begin(), this->nums1.end());
+    for (const int num : this->nums2) ++freqs[num];
   }
 
   void add(int index, int val) {
-    (void)index;
-    (void)val;
+    --freqs[nums2[index]];
+    nums2[index] += val;
+    ++freqs[nums2[index]];
   }
 
   int count(int tot) {
-    return tot;
+    int count{0};
+    for (const int num : nums1) {
+      if (num >= tot) break;
+      const auto it = freqs.find(tot - num);
+      if (it != freqs.end()) count += it->second;
+    }
+    return count;
   }
 };
