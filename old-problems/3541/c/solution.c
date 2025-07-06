70fd6cbf592124322b21dc4ae2e4b9d3230da6f0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-06 13:22:32 +0700

    feat(problem-3541/c): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3541/c/solution.c
+++ old-problems/3541/c/solution.c
@@ -1,3 +1,22 @@
 int maxFreqSum(char* s) {
-  return *s;
+  int freqs[26] = {0};
+  for (char* c = s; *c != 0; ++c) ++freqs[*c - 'a'];
+
+  int vowels = 0, consonants = 0;
+  for (int i = 0; i < 26; ++i) {
+    switch (i) {
+      case 0:
+      case 4:
+      case 8:
+      case 14:
+      case 20:
+        if (freqs[i] > vowels) vowels = freqs[i];
+        break;
+
+      default:
+        if (freqs[i] > consonants) consonants = freqs[i];
+    }
+  }
+
+  return vowels + consonants;
 }
