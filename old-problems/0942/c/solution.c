64906ef8305fbde4ff60084cdde437506f2c94fa
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-08 20:19:43 +0700

    feat(problem-942/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0942/c/solution.c
+++ old-problems/0942/c/solution.c
@@ -1,5 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+
 int* diStringMatch(char* s, int* returnSize) {
-  (void)s;
-  *returnSize = 0;
-  return 0;
+  const int sLen = strlen(s);
+  int* output = malloc((sLen + 1) * sizeof(int));
+  int a = 0, b = sLen;
+  for (int i = 0; i < sLen; ++i) {
+    output[i] = s[i] == 'I' ? a++ : b--;
+  }
+  output[sLen] = a;
+  *returnSize = sLen + 1;
+  return output;
 }
