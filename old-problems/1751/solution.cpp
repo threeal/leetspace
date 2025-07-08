2bd6fe3861f761f29608189b76dad73c93b21886
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-08 09:28:08 +0700

    feat(problem-1751/cpp): solve by using heap queue and bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1751/solution.cpp
+++ old-problems/1751/solution.cpp
@@ -1,8 +1,36 @@
+#include <algorithm>
+#include <queue>
 #include <vector>
 
 class Solution {
  public:
   int maxValue(std::vector<std::vector<int>>& events, int k) {
-    return events[0][0] + k;
+    std::sort(events.begin(), events.end());
+
+    std::vector<int> sums(events.size());
+    for (std::size_t i{0}; i < events.size(); ++i) {
+      sums[i] = events[i][2];
+    }
+
+    while (--k > 0) {
+      int maxPrevSum{0};
+      std::priority_queue<std::tuple<int, int>> prevEvents{};
+      for (std::size_t i{0}; i < events.size(); ++i) {
+        while (!prevEvents.empty()) {
+          const auto [negEndDay, sum] = prevEvents.top();
+          if (-negEndDay < events[i][0]) {
+            if (sum > maxPrevSum) maxPrevSum = sum;
+            prevEvents.pop();
+          } else {
+            break;
+          }
+        }
+
+        prevEvents.push({-events[i][1], sums[i]});
+        sums[i] = events[i][2] + maxPrevSum;
+      }
+    }
+
+    return *std::max_element(sums.begin(), sums.end());
   }
 };
