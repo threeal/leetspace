9c5ea81bd89ced3c1ca6c143634aa9bd585a9081
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-09 15:47:10 +0700

    feat(problem-3550/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3550/c/solution.c
+++ old-problems/3550/c/solution.c
@@ -1,3 +1,11 @@
 int smallestIndex(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  for (int i = 0; i < numsSize; ++i) {
+    int sum = 0, num = nums[i];
+    while (num > 0) {
+      sum += num % 10;
+      num /= 10;
+    }
+    if (sum == i) return i;
+  }
+  return -1;
 }
