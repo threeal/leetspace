6007a3caddcf22edc5e5c397abe829cf27b983e8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-09 15:06:00 +0700

    feat(problem-2656/c): solve by finding max element
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2656/c/solution.c
+++ old-problems/2656/c/solution.c
@@ -1,3 +1,7 @@
 int maximizeSum(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] + k;
+  int max = nums[0];
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i] > max) max = nums[i];
+  }
+  return k * (2 * max + k - 1) / 2;
 }
