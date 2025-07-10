023d2e36c5cd7f08bea726b6738b2c59f55f1a0e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-10 10:58:32 +0700

    feat(problem-728/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0728/c/solution.c
+++ old-problems/0728/c/solution.c
@@ -1,6 +1,23 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
 int* selfDividingNumbers(int left, int right, int* returnSize) {
-  (void)left;
-  (void)right;
-  *returnSize = 0;
-  return 0;
+  static int* output = NULL;
+  if (output == NULL) output = malloc(339 * sizeof(int));
+
+  int outputSize = 0;
+  for (int num = left; num <= right; ++num) {
+    bool valid = true;
+    for (int exp = 1; exp <= num; exp *= 10) {
+      const int digit = num / exp % 10;
+      if (digit == 0 || num % digit != 0) {
+        valid = false;
+        break;
+      }
+    }
+    if (valid) output[outputSize++] = num;
+  }
+
+  *returnSize = outputSize;
+  return output;
 }
