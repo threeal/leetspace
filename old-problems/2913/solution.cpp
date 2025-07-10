ffba4a298b214c85fb9e23b5ccb4c361c21d2f47
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-10 19:36:30 +0700

    feat(problem-2913/cpp): solve by storing existence in map array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2913/solution.cpp
+++ old-problems/2913/solution.cpp
@@ -3,6 +3,20 @@
 class Solution {
  public:
   int sumCounts(std::vector<int>& nums) {
-    return nums.front();
+    int sum = nums.size();
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      bool exists[101]{false};
+      exists[nums[i]] = true;
+
+      int distinct{1};
+      for (std::size_t j{i + 1}; j < nums.size(); ++j) {
+        if (!exists[nums[j]]) {
+          exists[nums[j]] = true;
+          ++distinct;
+        }
+        sum += distinct * distinct;
+      }
+    }
+    return sum;
   }
 };
