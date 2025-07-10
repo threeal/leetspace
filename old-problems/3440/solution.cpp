13e652b01e3dc0822007d7dce60225a80d6e65a1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-10 10:38:40 +0700

    feat(problem-3440/cpp): solve by checking gaps between events
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3440/solution.cpp
+++ old-problems/3440/solution.cpp
@@ -4,6 +4,52 @@ class Solution {
  public:
   int maxFreeTime(
       int eventTime, std::vector<int>& startTime, std::vector<int>& endTime) {
-    return eventTime + startTime[0] + endTime[0];
+    const std::size_t n{startTime.size()};
+
+    int maxFree{0}, prevFree{startTime[0]};
+    for (std::size_t i{1}; i < n; ++i) {
+      const int free{startTime[i] - endTime[i - 1]};
+      if (free + prevFree > maxFree) maxFree = free + prevFree;
+      prevFree = free;
+    }
+
+    const int free{eventTime - endTime[n - 1]};
+    if (free + prevFree > maxFree) maxFree = free + prevFree;
+
+    if (n >= 2) {
+      int maxEmpty{startTime[0]};
+      for (std::size_t i{2}; i < n; ++i) {
+        if (endTime[i - 1] - startTime[i - 1] <= maxEmpty) {
+          const int free{startTime[i] - endTime[i - 2]};
+          if (free > maxFree) maxFree = free;
+        }
+
+        const int empty{startTime[i - 1] - endTime[i - 2]};
+        if (empty > maxEmpty) maxEmpty = empty;
+      }
+
+      if (endTime[n - 1] - startTime[n - 1] <= maxEmpty) {
+        const int free{eventTime - endTime[n - 2]};
+        if (free > maxFree) maxFree = free;
+      }
+
+      maxEmpty = eventTime - endTime[n - 1];
+      for (std::size_t i{n - 1}; i >= 2; --i) {
+        if (endTime[i - 1] - startTime[i - 1] <= maxEmpty) {
+          const int free{startTime[i] - endTime[i - 2]};
+          if (free > maxFree) maxFree = free;
+        }
+
+        const int empty{startTime[i] - endTime[i - 1]};
+        if (empty > maxEmpty) maxEmpty = empty;
+      }
+
+      if (endTime[0] - startTime[0] <= maxEmpty) {
+        const int free{startTime[1]};
+        if (free > maxFree) maxFree = free;
+      }
+    }
+
+    return maxFree;
   }
 };
