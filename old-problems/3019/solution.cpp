0ca0daab4b5b48bb950752336712973d31e4b360
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-10 10:39:46 +0700

    feat(problem-3019/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3019/solution.cpp
+++ old-problems/3019/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   int countKeyChanges(std::string s) {
-    return s.size();
+    int changes{0};
+    if (s[0] > 'Z') s[0] += 'A' - 'a';
+    for (std::size_t i{1}; i < s.size(); ++i) {
+      if (s[i] > 'Z') s[i] += 'A' - 'a';
+      if (s[i] != s[i - 1]) ++changes;
+    }
+    return changes;
   }
 };
