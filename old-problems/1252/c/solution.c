ba28248ae1d67d1e9b27904bf17411609622ffe7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-11 11:21:39 +0700

    feat(problem-1252/c): solve by tracking rows and cols to be incremented
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1252/c/solution.c
+++ old-problems/1252/c/solution.c
@@ -1,4 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+
 int oddCells(
     int m, int n, int** indices, int indicesSize, int* indicesColSize) {
-  return m + n + indices[indicesSize - 1][indicesColSize[0] - 1];
+  (void)indicesColSize;
+
+  int* rows = malloc(m * sizeof(int));
+  memset(rows, 0, m * sizeof(int));
+
+  int* cols = malloc(n * sizeof(int));
+  memset(cols, 0, n * sizeof(int));
+
+  for (int i = 0; i < indicesSize; ++i) {
+    ++rows[indices[i][0]];
+    ++cols[indices[i][1]];
+  }
+
+  int count = 0;
+  for (int i = 0; i < m; ++i) {
+    const int rowParity = rows[i] % 2;
+    for (int j = 0; j < n; ++j) {
+      if (rowParity != cols[j] % 2) ++count;
+    }
+  }
+
+  return count;
 }
