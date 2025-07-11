9d21de4d7b04ac652b88329a9d9d7e6ebdd202a5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-11 11:28:55 +0700

    feat(problem-2913/c): solve by storing existence in map array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2913/c/solution.c
+++ old-problems/2913/c/solution.c
@@ -1,3 +1,19 @@
+#include <stdbool.h>
+
 int sumCounts(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int sum = numsSize;
+  for (int i = 0; i < numsSize; ++i) {
+    bool exists[101] = {false};
+    exists[nums[i]] = true;
+
+    int distinct = 1;
+    for (int j = i + 1; j < numsSize; ++j) {
+      if (!exists[nums[j]]) {
+        exists[nums[j]] = true;
+        ++distinct;
+      }
+      sum += distinct * distinct;
+    }
+  }
+  return sum;
 }
