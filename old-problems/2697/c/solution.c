b59b14858edbcdffafcee988a990d6127b9ea26a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-12 21:46:04 +0700

    feat(problem-2697/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2697/c/solution.c
+++ old-problems/2697/c/solution.c
@@ -1,3 +1,13 @@
+#include <string.h>
+
 char* makeSmallestPalindrome(char* s) {
+  const int sLen = strlen(s);
+  for (int l = 0, r = sLen - 1; l < r; ++l, --r) {
+    if (s[l] <= s[r]) {
+      s[r] = s[l];
+    } else {
+      s[l] = s[r];
+    }
+  }
   return s;
 }
