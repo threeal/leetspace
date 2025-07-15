df86d1bcebff3b894c7546dab5123b773fb7c0f3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-15 10:23:46 +0700

    feat(problem-3136/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3136/solution.cpp
+++ old-problems/3136/solution.cpp
@@ -3,6 +3,39 @@
 class Solution {
  public:
   bool isValid(std::string word) {
-    return word.empty();
+    if (word.size() < 3) return false;
+    bool hasVowel{false}, hasConsonant{false};
+    for (const char c : word) {
+      if (c >= 'A' && c <= 'Z') {
+        switch (c) {
+          case 'A':
+          case 'E':
+          case 'I':
+          case 'O':
+          case 'U':
+            hasVowel = true;
+            break;
+
+          default:
+            hasConsonant = true;
+        }
+      } else if (c >= 'a' && c <= 'z') {
+        switch (c) {
+          case 'a':
+          case 'e':
+          case 'i':
+          case 'o':
+          case 'u':
+            hasVowel = true;
+            break;
+
+          default:
+            hasConsonant = true;
+        }
+      } else if (c < '0' || c > '9') {
+        return false;
+      }
+    }
+    return hasVowel && hasConsonant;
   }
 };
