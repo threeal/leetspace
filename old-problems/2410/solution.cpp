f511cc96d4bdadb22d00ec695c9e0247de19869b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-15 10:07:28 +0700

    feat(problem-2410): solve by using sorting and greedy
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2410/solution.cpp
+++ old-problems/2410/solution.cpp
@@ -1,9 +1,19 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int matchPlayersAndTrainers(
       std::vector<int>& players, std::vector<int>& trainers) {
-    return players.front() + trainers.front();
+    std::sort(players.begin(), players.end());
+    std::sort(trainers.begin(), trainers.end());
+
+    std::size_t i{0}, j{0};
+    while (i < players.size() && j < trainers.size()) {
+      while (j < trainers.size() && players[i] > trainers[j]) ++j;
+      if (j < trainers.size()) ++i, ++j;
+    }
+
+    return i;
   }
 };
