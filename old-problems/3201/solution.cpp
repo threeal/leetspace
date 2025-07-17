d631b14fff58368369b0fc9dba80ed125aeb3793
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-17 11:54:39 +0700

    feat(problem-3201/cpp): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3201/solution.cpp
+++ old-problems/3201/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   int maximumLength(std::vector<int>& nums) {
-    return nums.size();
+    int dp[2][2]{{0, 0}, {0, 0}}, max{0};
+    for (int num : nums) {
+      num %= 2;
+
+      dp[num][0] = dp[0][num] + 1;
+      if (dp[num][0] > max) max = dp[num][0];
+
+      dp[num][1] = dp[1][num] + 1;
+      if (dp[num][1] > max) max = dp[num][1];
+    }
+    return max;
   }
 };
