3228f60ec37f6964af3b2c7d30ecc2f3e0629f9e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-19 10:33:01 +0700

    feat(problem-3324/cpp): solve by simulating the screen changes
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3324/solution.cpp
+++ old-problems/3324/solution.cpp
@@ -4,6 +4,18 @@
 class Solution {
  public:
   std::vector<std::string> stringSequence(std::string target) {
-    return {target};
+    std::vector<std::string> screens{};
+    std::string screen{};
+    while (screen.size() < target.size()) {
+      screen += 'a';
+      screens.push_back(screen);
+
+      const std::size_t i{screen.size() - 1};
+      while (screen[i] != target[i]) {
+        ++screen[i];
+        screens.push_back(screen);
+      }
+    }
+    return screens;
   }
 };
