92e6f2a4cc5bbd71d6005538d0927fd4bf08f7b8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-21 20:46:56 +0700

    feat(problem-1725/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1725/c/solution.c
+++ old-problems/1725/c/solution.c
@@ -1,5 +1,19 @@
 int countGoodRectangles(
     int** rectangles, int rectanglesSize, int* rectanglesColSize) {
-  const int j = rectanglesColSize[rectanglesSize - 1] - 1;
-  return rectangles[rectanglesSize - 1][j];
+  (void)rectanglesColSize;
+  int count = 0, maxK = 0;
+  for (int i = 0; i < rectanglesSize; ++i) {
+    const int k = rectangles[i][0] <= rectangles[i][1]
+        ? rectangles[i][0]
+        : rectangles[i][1];
+
+    if (k < maxK) continue;
+    if (k == maxK) {
+      ++count;
+    } else {
+      maxK = k;
+      count = 1;
+    }
+  }
+  return count;
 }
