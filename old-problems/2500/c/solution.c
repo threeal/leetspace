586d80fe9443686d06007c12c55d8470fb461b4c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-22 17:53:21 +0700

    feat(problem-2500/c): solve by sorting each row to find the maximum of each column
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2500/c/solution.c
+++ old-problems/2500/c/solution.c
@@ -1,3 +1,22 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int deleteGreatestValue(int** grid, int gridSize, int* gridColSize) {
-  return grid[gridSize - 1][gridColSize[gridSize - 1] - 1];
+  for (int i = 0; i < gridSize; ++i) {
+    qsort(grid[i], gridColSize[i], sizeof(int), compare);
+  }
+
+  int sum = 0;
+  for (int j = 0; j < gridColSize[0]; ++j) {
+    int max = grid[0][j];
+    for (int i = 1; i < gridSize; ++i) {
+      if (grid[i][j] > max) max = grid[i][j];
+    }
+    sum += max;
+  }
+
+  return sum;
 }
