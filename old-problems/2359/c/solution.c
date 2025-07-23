cb9ccdd767fa23c4bdf3c5c518afed60f98979ee
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-23 14:40:22 +0700

    feat(problem-2359/c): solve by using DFS approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2359/c/solution.c
+++ old-problems/2359/c/solution.c
@@ -1,3 +1,35 @@
+#include <limits.h>
+#include <stdlib.h>
+
 int closestMeetingNode(int* edges, int edgesSize, int node1, int node2) {
-  return edges[edgesSize - 1] + node1 + node2;
+  int dist = 0;
+  int* dist1 = malloc(edgesSize * sizeof(int));
+  for (int i = 0; i < edgesSize; ++i) dist1[i] = -1;
+
+  while (node1 != -1 && dist1[node1] == -1) {
+    dist1[node1] = dist++;
+    node1 = edges[node1];
+  }
+
+  dist = 0;
+  int* dist2 = malloc(edgesSize * sizeof(int));
+  for (int i = 0; i < edgesSize; ++i) dist2[i] = -1;
+
+  while (node2 != -1 && dist2[node2] == -1) {
+    dist2[node2] = dist++;
+    node2 = edges[node2];
+  }
+
+  int maxDist = INT_MAX, res = -1;
+  for (int i = 0; i < edgesSize; ++i) {
+    if (dist1[i] != -1 && dist2[i] != -1) {
+      const int dist = dist1[i] >= dist2[i] ? dist1[i] : dist2[i];
+      if (dist < maxDist) {
+        maxDist = dist;
+        res = i;
+      }
+    }
+  }
+
+  return res;
 }
