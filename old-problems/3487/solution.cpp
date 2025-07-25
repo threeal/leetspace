a062c3f7ff020ba3f714fb108140237e3102d126
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-25 18:11:31 +0700

    feat(problem-3487/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3487/solution.cpp
+++ old-problems/3487/solution.cpp
@@ -3,6 +3,20 @@
 class Solution {
  public:
   int maxSum(std::vector<int>& nums) {
-    return nums[0];
+    bool exists[101]{false}, positiveExists{false};
+    int maxNegative{-101}, sumPositives{0};
+    for (const int num : nums) {
+      if (num >= 0) {
+        positiveExists = true;
+        if (!exists[num]) {
+          exists[num] = true;
+          sumPositives += num;
+        }
+      } else {
+        if (num >= maxNegative) maxNegative = num;
+      }
+    }
+
+    return positiveExists ? sumPositives : maxNegative;
   }
 };
