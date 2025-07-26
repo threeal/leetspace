a9cf7bfddaea9044939da68687b6f3c688f15b16
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-26 20:51:15 +0700

    feat(problem-135/c): solve by using double pass greedy
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0135/c/solution.c
+++ old-problems/0135/c/solution.c
@@ -1,3 +1,22 @@
+#include <stdlib.h>
+
 int candy(int* ratings, int ratingsSize) {
-  return ratings[ratingsSize - 1];
+  int* candies = malloc(ratingsSize * sizeof(int));
+  for (int i = 0; i < ratingsSize; ++i) candies[i] = 1;
+
+  for (int i = 1; i < ratingsSize; ++i) {
+    if (ratings[i] > ratings[i - 1]) {
+      candies[i] = candies[i - 1] + 1;
+    }
+  }
+
+  int total = candies[ratingsSize - 1];
+  for (int i = ratingsSize - 1; i > 0; --i) {
+    if (ratings[i - 1] > ratings[i] && candies[i - 1] <= candies[i]) {
+      candies[i - 1] = candies[i] + 1;
+    }
+    total += candies[i - 1];
+  }
+
+  return total;
 }
