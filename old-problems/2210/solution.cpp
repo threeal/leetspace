52a1fe0ffb3a86f8bee29bac0355244ead722b01
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-27 12:43:03 +0700

    feat(problem-2210/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2210/solution.cpp
+++ old-problems/2210/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   int countHillValley(std::vector<int>& nums) {
-    return nums[0];
+    int count{0};
+    bool increasing{false}, decreasing{false};
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] > nums[i - 1]) {
+        if (decreasing) ++count;
+        increasing = true;
+        decreasing = false;
+      } else if (nums[i] < nums[i - 1]) {
+        if (increasing) ++count;
+        decreasing = true;
+        increasing = false;
+      }
+    }
+    return count;
   }
 };
