b549a0c9f3577c43894320d4a8580d32f2c2edec
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-27 20:18:41 +0700

    feat(problem-1641/c): solve by using a while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1641/c/solution.c
+++ old-problems/1641/c/solution.c
@@ -1,3 +1,10 @@
 int countVowelStrings(int n) {
-  return n;
+  int e = 1, i = 1, o = 1, u = 1;
+  while (e < n) {
+    u += 1 + e + i + o;
+    o += 1 + e + i;
+    i += 1 + e;
+    ++e;
+  }
+  return 1 + e + i + o + u;
 }
