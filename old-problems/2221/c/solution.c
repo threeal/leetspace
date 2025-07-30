f9e306df8c40d4b2d4d862301b6ed10972e6a333
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-30 20:03:21 +0700

    feat(problem-2221/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2221/c/solution.c
+++ old-problems/2221/c/solution.c
@@ -1,3 +1,8 @@
 int triangularSum(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  for (int n = numsSize; n > 1; --n) {
+    for (int i = 1; i < n; ++i) {
+      nums[i - 1] = (nums[i - 1] + nums[i]) % 10;
+    }
+  }
+  return nums[0];
 }
