9ba8bf22ab350d27f6e10a9706120b86430e3733
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-31 18:05:54 +0700

    feat(problem-3324/c): solve by simulating the screen changes
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3324/c/solution.c
+++ old-problems/3324/c/solution.c
@@ -1,5 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+
 char** stringSequence(char* target, int* returnSize) {
-  (void)target;
-  *returnSize = 0;
-  return 0;
+  int targetSize = 0, screensSize = 0;
+  for (char* c = target; *c != 0; ++c) {
+    screensSize += *c + 1 - 'a';
+    ++targetSize;
+  }
+
+  char** screens = malloc(screensSize * sizeof(char*));
+  screensSize = 0;
+
+  char* screen = malloc((targetSize + 1) * sizeof(char));
+  int screenSize = 0;
+
+  while (screenSize < targetSize) {
+    screen[screenSize] = 'a';
+    screen[++screenSize] = 0;
+
+    screens[screensSize] = malloc((screenSize + 1) * sizeof(char));
+    memcpy(screens[screensSize++], screen, (screenSize + 1) * sizeof(char));
+
+    const int i = screenSize - 1;
+    while (screen[i] != target[i]) {
+      ++screen[i];
+
+      screens[screensSize] = malloc((screenSize + 1) * sizeof(char));
+      memcpy(screens[screensSize++], screen, (screenSize + 1) * sizeof(char));
+    }
+  }
+
+  free(screen);
+
+  *returnSize = screensSize;
+  return screens;
 }
