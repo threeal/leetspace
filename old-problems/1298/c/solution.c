be7d2de92090bd57a63a718c21debd5fd73aa941
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-01 19:40:05 +0700

    test(problem-1298): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1298/c/solution.c
@@ -0,0 +1,20 @@
+int maxCandies(
+    int* status, int statusSize,
+    int* candies, int candiesSize,
+    int** keys, int keysSize, int* keysColSize,
+    int** containedBoxes, int containedBoxesSize, int* containedBoxesColSize,
+    int* initialBoxes, int initialBoxesSize) {
+  (void)status;
+  (void)statusSize;
+  (void)candies;
+  (void)candiesSize;
+  (void)keys;
+  (void)keysSize;
+  (void)keysColSize;
+  (void)containedBoxes;
+  (void)containedBoxesSize;
+  (void)containedBoxesColSize;
+  (void)initialBoxes;
+  (void)initialBoxesSize;
+  return 0;
+}
