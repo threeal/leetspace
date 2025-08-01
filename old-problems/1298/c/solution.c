a6e2570090940ce58d13854bc00c2c00fd299551
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-01 19:50:18 +0700

    feat(problem-1298/c): solve by using a queue
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1298/c/solution.c
+++ old-problems/1298/c/solution.c
@@ -1,20 +1,44 @@
+#include <stdlib.h>
+
 int maxCandies(
     int* status, int statusSize,
     int* candies, int candiesSize,
     int** keys, int keysSize, int* keysColSize,
     int** containedBoxes, int containedBoxesSize, int* containedBoxesColSize,
     int* initialBoxes, int initialBoxesSize) {
-  (void)status;
-  (void)statusSize;
-  (void)candies;
   (void)candiesSize;
-  (void)keys;
   (void)keysSize;
-  (void)keysColSize;
-  (void)containedBoxes;
   (void)containedBoxesSize;
-  (void)containedBoxesColSize;
-  (void)initialBoxes;
-  (void)initialBoxesSize;
-  return 0;
+
+  int* boxes = malloc(statusSize * sizeof(int));
+  int boxesSize = 0;
+
+  for (int i = 0; i < initialBoxesSize; ++i) {
+    if ((status[initialBoxes[i]] |= 2) == 3) {
+      status[initialBoxes[i]] |= 4;
+      boxes[boxesSize++] = initialBoxes[i];
+    }
+  }
+
+  int totalCandies = 0;
+  for (int i = 0; i < boxesSize; ++i) {
+    totalCandies += candies[boxes[i]];
+
+    for (int j = 0; j < keysColSize[boxes[i]]; ++j) {
+      if ((status[keys[boxes[i]][j]] |= 1) == 3) {
+        status[keys[boxes[i]][j]] |= 4;
+        boxes[boxesSize++] = keys[boxes[i]][j];
+      }
+    }
+
+    for (int j = 0; j < containedBoxesColSize[boxes[i]]; ++j) {
+      if ((status[containedBoxes[boxes[i]][j]] |= 2) == 3) {
+        status[containedBoxes[boxes[i]][j]] |= 4;
+        boxes[boxesSize++] = containedBoxes[boxes[i]][j];
+      }
+    }
+  }
+
+  free(boxes);
+  return totalCandies;
 }
