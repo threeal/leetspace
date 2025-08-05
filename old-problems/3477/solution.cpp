173fc60e5e94ff512e77e9b6c6e6bca8dc52a730
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-05 07:59:57 +0700

    feat(problem-3477/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3477/solution.cpp
+++ old-problems/3477/solution.cpp
@@ -4,6 +4,16 @@ class Solution {
  public:
   int numOfUnplacedFruits(
       std::vector<int>& fruits, std::vector<int>& baskets) {
-    return fruits.size() + baskets.size();
+    std::size_t num{fruits.size()};
+    for (const int fruit : fruits) {
+      for (int& basket : baskets) {
+        if (basket >= fruit) {
+          --num;
+          basket = 0;
+          break;
+        }
+      }
+    }
+    return num;
   }
 };
