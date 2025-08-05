e9ca60b3062850957f0320c3ffa48ab93e469408
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-05 21:19:00 +0700

    feat(problem-1941/c): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1941/c/solution.c
+++ old-problems/1941/c/solution.c
@@ -1,5 +1,19 @@
 #include <stdbool.h>
 
 bool areOccurrencesEqual(char* s) {
-  return *s == 0;
+  char freqs[26] = {0};
+
+  for (char* c = s; *c != 0; ++c) ++freqs[*c - 'a'];
+
+  for (int i = 0; i < 26; ++i) {
+    if (freqs[i] != 0) {
+      const int freq = freqs[i];
+      for (int j = i + 1; j < 26; ++j) {
+        if (freqs[j] != 0 && freqs[j] != freq) return false;
+      }
+      return true;
+    }
+  }
+
+  return true;
 }
