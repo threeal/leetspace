f1ed344ff05b0cd4759142c361bada207490ab5e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-17 22:11:33 +0700

    feat(problem-2405/c): solve by using bit-flags
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2405/c/solution.c
+++ old-problems/2405/c/solution.c
@@ -1,3 +1,16 @@
+#include <stdint.h>
+
 int partitionString(char* s) {
-  return *s;
+  int partitions = 1;
+  uint32_t flags = 0;
+  for (char* c = s; *c != 0; ++c) {
+    const uint32_t flag = 1u << (*c - 'a');
+    if ((flags & flag) == 0) {
+      flags |= flag;
+    } else {
+      flags = flag;
+      ++partitions;
+    }
+  }
+  return partitions;
 }
