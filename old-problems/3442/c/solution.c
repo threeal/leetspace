75deab04226ee6075b3bd70fbb84bd977767f11f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-19 10:57:53 +0700

    feat(problem-3442/c): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3442/c/solution.c
+++ old-problems/3442/c/solution.c
@@ -1,3 +1,21 @@
+#include <limits.h>
+
 int maxDifference(char* s) {
-  return *s;
+  int freqs[26] = {0};
+  for (char* c = s; *c != 0; ++c) ++freqs[*c - 'a'];
+
+  int even = INT_MAX;
+  int odd = INT_MIN;
+
+  for (int i = 0; i < 26; ++i) {
+    if (freqs[i] > 0) {
+      if (freqs[i] % 2 == 0) {
+        if (freqs[i] < even) even = freqs[i];
+      } else {
+        if (freqs[i] > odd) odd = freqs[i];
+      }
+    }
+  }
+
+  return odd - even;
 }
