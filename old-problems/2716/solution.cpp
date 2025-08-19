99f0155d29caed5e67bbfd8558e16602fd14cd04
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-19 15:26:36 +0700

    feat(problem-2716/cpp): solve by counting unique characters
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2716/solution.cpp
+++ old-problems/2716/solution.cpp
@@ -1,8 +1,17 @@
 #include <string>
+#include <vector>
 
 class Solution {
  public:
   int minimizedStringLength(std::string s) {
-    return s.size();
+    std::vector<bool> exists(26, false);
+    int unique{0};
+    for (const char c : s) {
+      if (!exists[c - 'a']) {
+        exists[c - 'a'] = true;
+        ++unique;
+      }
+    }
+    return unique;
   }
 };
