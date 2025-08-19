7c09089a2051af9916454c996b392b998e3435e7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-19 15:15:08 +0700

    feat(problem-3602/cpp): solve by using while-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3602/solution.cpp
+++ old-problems/3602/solution.cpp
@@ -1,8 +1,24 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   std::string concatHex36(int n) {
-    return std::string(n % 16, '0');
+    std::string str{};
+
+    int val{n * n * n};
+    while (val > 0) {
+      str += val % 36 < 10 ? ('0' + val % 36) : ('A' + val % 36 - 10);
+      val /= 36;
+    }
+
+    val = n * n;
+    while (val > 0) {
+      str += val % 16 < 10 ? ('0' + val % 16) : ('A' + val % 16 - 10);
+      val /= 16;
+    }
+
+    std::reverse(str.begin(), str.end());
+    return str;
   }
 };
