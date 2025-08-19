1c4b37e990bd4b91f1972ebeda5d9b381c2e8473
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-19 21:37:32 +0700

    feat(problem-3184/cpp): solve by using brute-force approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3184/solution.cpp
+++ old-problems/3184/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   int countCompleteDayPairs(std::vector<int>& hours) {
-    return hours.size();
+    int count{0};
+    for (std::size_t i{0}; i < hours.size(); ++i) {
+      for (std::size_t j{i + 1}; j < hours.size(); ++j) {
+        if ((hours[i] + hours[j]) % 24 == 0) ++count;
+      }
+    }
+    return count;
   }
 };
