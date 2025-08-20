5e927bdf04dee06b37cf2000e448aa2d762d1604
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-20 17:19:31 +0700

    feat(problem-2390/c): solve by using stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2390/c/solution.c
+++ old-problems/2390/c/solution.c
@@ -1,3 +1,12 @@
 char* removeStars(char* s) {
+  int n = 0;
+  for (int i = 0; s[i] != 0; ++i) {
+    if (s[i] == '*') {
+      --n;
+    } else {
+      s[n++] = s[i];
+    }
+  }
+  s[n] = 0;
   return s;
 }
