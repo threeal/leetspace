a8b95f7a03f529f6ba085a3f87457a87cbed77d1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-24 13:31:36 +0700

    feat(problem-1493/cpp): solve by using a while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1493/solution.cpp
+++ old-problems/1493/solution.cpp
@@ -3,6 +3,22 @@
 class Solution {
  public:
   int longestSubarray(std::vector<int>& nums) {
-    return nums.front();
+    std::size_t i{0};
+    while (i < nums.size() && nums[i] == 1) ++i;
+
+    if (i == nums.size()) return i - 1;
+
+    int count{0}, maxCount = i, prev = i;
+    while (++i < nums.size()) {
+      if (nums[i] == 0) {
+        if (prev + count > maxCount) maxCount = prev + count;
+        prev = count;
+        count = 0;
+      } else {
+        ++count;
+      }
+    }
+
+    return prev + count > maxCount ? prev + count : maxCount;
   }
 };
