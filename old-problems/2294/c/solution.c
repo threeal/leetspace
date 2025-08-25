aa119c4dc458d78b6668539ab5f6077ccd029efb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-08-25 21:30:29 +0700

    feat(problem-2294/c): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2294/c/solution.c
+++ old-problems/2294/c/solution.c
@@ -1,3 +1,17 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int partitionArray(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] * k;
+  qsort(nums, numsSize, sizeof(int), compare);
+  int count = 0, max = -1;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] > max) {
+      ++count;
+      max = nums[i] + k;
+    }
+  }
+  return count;
 }
