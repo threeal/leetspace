8eb2ec158f8311ae0b1198a9d3f10c7eda9270ca
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-03 08:03:48 +0700

    feat(problem-3027/cpp): solve by using for-loops after sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3027/solution.cpp
+++ old-problems/3027/solution.cpp
@@ -1,8 +1,29 @@
+#include <algorithm>
+#include <limits>
 #include <vector>
 
 class Solution {
  public:
   int numberOfPairs(std::vector<std::vector<int>>& points) {
-    return points.size();
+    std::sort(points.begin(), points.end(), [](const auto& a, const auto& b) {
+      return a[0] != b[0] ? a[0] < b[0] : a[1] > b[1];
+    });
+
+    int count{0};
+    for (std::size_t i{0}; i < points.size(); ++i) {
+      int xMin{points[i][0] - 1};
+      int yMin{std::numeric_limits<int>::min()};
+
+      for (std::size_t j{i + 1}; j < points.size(); ++j) {
+        if (points[j][0] > xMin && points[j][1] > yMin &&
+            points[j][1] <= points[i][1]) {
+          ++count;
+          xMin = points[j][0];
+          yMin = points[j][1];
+        }
+      }
+    }
+
+    return count;
   }
 };
