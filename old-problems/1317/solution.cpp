67438aa68b1e8ae352fe5ac4ac803cf387451554
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-08 13:02:13 +0700

    feat(problem-1317/cpp): solve by using for-loop and while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1317/solution.cpp
+++ old-problems/1317/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   std::vector<int> getNoZeroIntegers(int n) {
-    return {n, n};
+    for (int i{1}; i < n; ++i) {
+      int j{i};
+      while (j > 0 && j % 10 != 0) j /= 10;
+      if (j > 0) continue;
+
+      j = n - i;
+      while (j > 0 && j % 10 != 0) j /= 10;
+      if (j > 0) continue;
+
+      return {i, n - i};
+    }
+    return {0, n};
   }
 };
