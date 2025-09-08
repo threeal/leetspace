a52c4b27e4617e43af1dbd6b62f3078a9206a07a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-08 12:31:09 +0700

    feat(problem-1384/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1304/solution.cpp
+++ old-problems/1304/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   std::vector<int> sumZero(int n) {
-    return std::vector<int>(n);
+    std::vector<int> arr{};
+    arr.reserve(n);
+
+    for (int i{n / 2}; i > 0; --i) {
+      arr.push_back(i);
+      arr.push_back(-i);
+    }
+
+    if (n % 2 != 0) arr.push_back(0);
+
+    return arr;
   }
 };
