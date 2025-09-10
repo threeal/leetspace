faaec145e933839d5811a0ff9ef5ae5f48526199
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-10 10:53:52 +0700

    feat(problem-3202/c): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3202/c/solution.c
+++ old-problems/3202/c/solution.c
@@ -1,3 +1,12 @@
 int maximumLength(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] + k;
+  int max = 0;
+  int dp[1000][1000] = {{0}};
+  for (int i = 0; i < numsSize; ++i) {
+    const int num = nums[i] % k;
+    for (int prev = 0; prev < k; ++prev) {
+      dp[num][prev] = dp[prev][num] + 1;
+      if (dp[num][prev] > max) max = dp[num][prev];
+    }
+  }
+  return max;
 }
