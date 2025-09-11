af68b4d05acdb775f7e835f908eb9385ba629b4f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-11 10:51:08 +0700

    feat(problem-3405/c): solve by using a combinatorial mathematic
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3405/c/solution.c
+++ old-problems/3405/c/solution.c
@@ -1,3 +1,40 @@
+#include <stdlib.h>
+
+static long long* gFact = NULL;
+static long long* gInvFact = NULL;
+
+static long long qpow(long long x, int n) {
+  long long res = 1;
+  while (n != 0) {
+    if (n % 2 != 0) res = res * x % 1000000007;
+    x = x * x % 1000000007;
+    n /= 2;
+  }
+  return res;
+}
+
+static long long* calcFact(void) {
+  long long* fact = malloc(100000 * sizeof(long long));
+  fact[0] = 1;
+  for (long long i = 1; i < 100000; ++i) {
+    fact[i] = fact[i - 1] * i % 1000000007;
+  }
+  return fact;
+}
+
+static long long* calcInvFact(long long* fact) {
+  long long* invFact = malloc(100000 * sizeof(long long));
+  invFact[99999] = qpow(fact[99999], 1000000005);
+  for (long long i = 99999; i > 0; --i) {
+    invFact[i - 1] = invFact[i] * i % 1000000007;
+  }
+  return invFact;
+}
+
 int countGoodArrays(int n, int m, int k) {
-  return n + m + k;
+  if (gFact == NULL) gFact = calcFact();
+  if (gInvFact == NULL) gInvFact = calcInvFact(gFact);
+  const long long comb = gFact[n - 1] * gInvFact[k] % 1000000007 *
+      gInvFact[n - 1 - k] % 1000000007;
+  return m * comb % 1000000007 * qpow(m - 1, n - 1 - k) % 1000000007;
 }
