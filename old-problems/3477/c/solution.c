885dc36d806bad1110cec1b306dbadd33a050a62
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-12 14:24:18 +0700

    feat(problem-3477/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3477/c/solution.c
+++ old-problems/3477/c/solution.c
@@ -1,4 +1,14 @@
 int numOfUnplacedFruits(
     int* fruits, int fruitsSize, int* baskets, int basketsSize) {
-  return fruits[fruitsSize - 1] + baskets[basketsSize - 1];
+  int num = fruitsSize;
+  for (int i = 0; i < fruitsSize; ++i) {
+    for (int j = 0; j < basketsSize; ++j) {
+      if (baskets[j] >= fruits[i]) {
+        --num;
+        baskets[j] = 0;
+        break;
+      }
+    }
+  }
+  return num;
 }
