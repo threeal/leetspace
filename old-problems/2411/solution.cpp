017891bbc0fabc70bbc3db3cf42ed1781d105ed1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-12 14:11:14 +0700

    feat(problem-2411/cpp): solve by tracking position of each bit
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2411/solution.cpp
+++ old-problems/2411/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   std::vector<int> smallestSubarrays(std::vector<int>& nums) {
+    int bitPos[32]{0}, maxBits{0};
+    for (int i = nums.size() - 1; i >= 0; --i) {
+      while (nums[i] > (1 << maxBits)) ++maxBits;
+      int maxPos{i};
+      for (int j{0}; j <= maxBits; ++j) {
+        if ((nums[i] & (1 << j)) != 0) {
+          bitPos[j] = i;
+        } else if (bitPos[j] > maxPos) {
+          maxPos = bitPos[j];
+        }
+      }
+      nums[i] = maxPos + 1 - i;
+    }
     return nums;
   }
 };
