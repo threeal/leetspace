f382a625347a08088e36587c52d2200ba0feadc3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-14 22:20:01 +0700

    feat(problem-966/cpp): solve by using sets and maps
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0966/solution.cpp
+++ old-problems/0966/solution.cpp
@@ -1,10 +1,76 @@
 #include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::string> spellchecker(
       std::vector<std::string>& wordlist, std::vector<std::string>& queries) {
-    return wordlist.empty() ? wordlist : queries;
+    std::unordered_set<std::string_view> exactMatches{};
+    std::unordered_map<std::string, std::string_view> capitalMatches{};
+    std::unordered_map<std::string, std::string_view> vowelMatches{};
+
+    for (const std::string& word : wordlist) {
+      exactMatches.insert(word);
+
+      std::string query{word};
+      for (char& c : query) {
+        if (c < 'a') c += 'a' - 'A';
+      }
+
+      if (!capitalMatches.contains(query)) {
+        capitalMatches.emplace(query, word);
+      }
+
+      for (char& c : query) {
+        switch (c) {
+          case 'e':
+          case 'i':
+          case 'o':
+          case 'u':
+            c = 'a';
+        }
+      }
+
+      if (!vowelMatches.contains(query)) {
+        vowelMatches.emplace(query, word);
+      }
+    }
+
+    for (std::string& query : queries) {
+      if (exactMatches.contains(query)) continue;
+
+      for (char& c : query) {
+        if (c < 'a') c += 'a' - 'A';
+      }
+
+      const auto capitalMatch = capitalMatches.find(query);
+      if (capitalMatch != capitalMatches.end()) {
+        query = capitalMatch->second;
+        continue;
+      }
+
+      for (char& c : query) {
+        switch (c) {
+          case 'e':
+          case 'i':
+          case 'o':
+          case 'u':
+            c = 'a';
+        }
+      }
+
+      const auto vowelMatch = vowelMatches.find(query);
+      if (vowelMatch != vowelMatches.end()) {
+        query = vowelMatch->second;
+        continue;
+      }
+
+      query = "";
+    }
+
+    return queries;
   }
 };
