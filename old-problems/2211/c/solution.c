21a62cfe0546704aa110a49fd09961ea513664b8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-15 10:45:56 +0700

    feat(problem-2211/c): solve by iterating directions
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2211/c/solution.c
+++ old-problems/2211/c/solution.c
@@ -1,4 +1,26 @@
 int countCollisions(char* directions) {
-  (void)directions;
-  return 0;
+  char* direction = directions;
+  while (*direction == 'L') ++direction;
+
+  int collisions = 0;
+  int rights = 0;
+  while (*direction != 0) {
+    switch (*direction) {
+      case 'L':
+        collisions += rights + 1;
+        rights = 0;
+        break;
+
+      case 'R':
+        ++rights;
+        break;
+
+      case 'S':
+        collisions += rights;
+        rights = 0;
+    }
+    ++direction;
+  }
+
+  return collisions;
 }
