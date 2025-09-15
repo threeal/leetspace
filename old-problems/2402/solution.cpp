d1ba36db7a0bc9603450ed689518fea6bcc53084
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-15 09:55:19 +0700

    feat(problem-2402/cpp): solve by using double heap queue
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2402/solution.cpp
+++ old-problems/2402/solution.cpp
@@ -1,8 +1,46 @@
+#include <algorithm>
+#include <queue>
+#include <tuple>
 #include <vector>
 
 class Solution {
  public:
   int mostBooked(int n, std::vector<std::vector<int>>& meetings) {
-    return meetings.size() % n;
+    std::sort(meetings.begin(), meetings.end());
+
+    std::priority_queue<int> roomsAvailable{};
+    for (int i{0}; i < n; ++i) roomsAvailable.push(-i);
+
+    std::priority_queue<std::tuple<long long, int>> roomsUsed{};
+    std::vector<int> roomsCount(n, 0);
+
+    for (const auto& meeting : meetings) {
+      while (!roomsUsed.empty() && -std::get<0>(roomsUsed.top()) <= meeting[0]) {
+        roomsAvailable.push(std::get<1>(roomsUsed.top()));
+        roomsUsed.pop();
+      }
+
+      if (roomsAvailable.empty()) {
+        const auto [negEndTime, negid] = roomsUsed.top();
+        roomsUsed.pop();
+
+        ++roomsCount[-negid];
+        roomsUsed.push({negEndTime - meeting[1] + meeting[0], negid});
+      } else {
+        ++roomsCount[-roomsAvailable.top()];
+        roomsUsed.push({-meeting[1], roomsAvailable.top()});
+        roomsAvailable.pop();
+      }
+    }
+
+    int room{0}, roomCount{roomsCount[0]};
+    for (int i{1}; i < n; ++i) {
+      if (roomsCount[i] > roomCount) {
+        room = i;
+        roomCount = roomsCount[i];
+      }
+    }
+
+    return room;
   }
 };
