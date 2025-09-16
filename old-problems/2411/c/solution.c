be42a5c202fafb9440437c37ef35c4ed3dd2f115
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-16 22:14:56 +0700

    feat(problem-2411/c): solve by tracking position of each bit
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2411/c/solution.c
+++ old-problems/2411/c/solution.c
@@ -1,4 +1,17 @@
 int* smallestSubarrays(int* nums, int numsSize, int* returnSize) {
+  int bitPos[32] = {0}, maxBits = 0;
+  for (int i = numsSize - 1; i >= 0; --i) {
+    while (nums[i] > (1 << maxBits)) ++maxBits;
+    int maxPos = i;
+    for (int j = 0; j <= maxBits; ++j) {
+      if ((nums[i] & (1 << j)) != 0) {
+        bitPos[j] = i;
+      } else if (bitPos[j] > maxPos) {
+        maxPos = bitPos[j];
+      }
+    }
+    nums[i] = maxPos + 1 - i;
+  }
   *returnSize = numsSize;
   return nums;
 }
