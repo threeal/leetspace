3d2a6dfab5de801eb531bdd28aa97fe9edd0209d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-17 16:13:08 +0700

    feat(problem-2363/cpp): solve by sorting the given items and merge them
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2363/solution.cpp
+++ old-problems/2363/solution.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
@@ -5,6 +6,36 @@ class Solution {
   std::vector<std::vector<int>> mergeSimilarItems(
       std::vector<std::vector<int>>& items1,
       std::vector<std::vector<int>>& items2) {
-    return items1.size() <= items2.size() ? items1 : items2;
+    std::sort(items1.begin(), items1.end());
+    std::sort(items2.begin(), items2.end());
+
+    std::vector<std::vector<int>> output{};
+
+    std::size_t i1{0}, i2{0};
+    while (i1 < items1.size() && i2 < items2.size()) {
+      if (items1[i1][0] == items2[i2][0]) {
+        output.push_back({items1[i1][0], items1[i1][1] + items2[i2][1]});
+        ++i1;
+        ++i2;
+      } else if (items1[i1][0] < items2[i2][0]) {
+        output.push_back(items1[i1]);
+        ++i1;
+      } else {
+        output.push_back(items2[i2]);
+        ++i2;
+      }
+    }
+
+    while (i1 < items1.size()) {
+      output.push_back(items1[i1]);
+      ++i1;
+    }
+
+    while (i2 < items2.size()) {
+      output.push_back(items2[i2]);
+      ++i2;
+    }
+
+    return output;
   }
 };
