9ddbd5c7cd735ebff798874956c735ded0ca3834
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-18 16:25:29 +0700

    feat(problem-2182/c): solve by distributing largest char based on freqs
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2182/c/solution.c
+++ old-problems/2182/c/solution.c
@@ -1,4 +1,37 @@
 char* repeatLimitedString(char* s, int repeatLimit) {
-  (void)repeatLimit;
+  int freqs[26] = {0};
+  for (int i = 0; s[i] != 0; ++i) {
+    ++freqs[s[i] - 'a'];
+  }
+
+  int curr = 25;
+  while (curr >= 0 && freqs[curr] == 0) --curr;
+
+  int next = curr - 1;
+  while (next >= 0 && freqs[next] == 0) --next;
+
+  int i = 0;
+  while (curr >= 0) {
+    if (freqs[curr] > repeatLimit) {
+      for (int k = repeatLimit; k > 0; --k, ++i) s[i] = 'a' + curr;
+      freqs[curr] -= repeatLimit;
+
+      if (next < 0) break;
+      s[i] = 'a' + next;
+      ++i;
+
+      --freqs[next];
+      while (next >= 0 && freqs[next] == 0) --next;
+    } else {
+      for (int k = freqs[curr]; k > 0; --k, ++i) s[i] = 'a' + curr;
+      freqs[curr] = 0;
+
+      curr = next;
+      --next;
+      while (next >= 0 && freqs[next] == 0) --next;
+    }
+  }
+
+  s[i] = 0;
   return s;
 }
