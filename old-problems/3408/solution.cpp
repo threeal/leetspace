46536ab5a21d1dc76a345ced48dd07c55d4ca7ea
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-18 11:31:11 +0700

    feat(problem-3408/cpp): solve by using hash map and heap queue
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3408/solution.cpp
+++ old-problems/3408/solution.cpp
@@ -1,27 +1,47 @@
+#include <queue>
+#include <unordered_map>
 #include <vector>
 
 class TaskManager {
+ private:
+  std::unordered_map<int, std::tuple<int, int>> tasks{};
+  std::priority_queue<std::tuple<int, int>> taskPriorities;
+
  public:
   TaskManager(std::vector<std::vector<int>>& tasks) {
-    (void)tasks;
+    for (const auto& task : tasks) {
+      add(task[0], task[1], task[2]);
+    }
   }
 
   void add(int userId, int taskId, int priority) {
-    (void)userId;
-    (void)taskId;
-    (void)priority;
+    tasks.emplace(taskId, std::tuple<int, int>{userId, priority});
+    taskPriorities.push({priority, taskId});
   }
 
   void edit(int taskId, int newPriority) {
-    (void)taskId;
-    (void)newPriority;
+    std::get<1>(tasks[taskId]) = newPriority;
+    taskPriorities.push({newPriority, taskId});
   }
 
   void rmv(int taskId) {
-    (void)taskId;
+    tasks.erase(taskId);
   }
 
   int execTop() {
-    return 0;
+    while (!taskPriorities.empty()) {
+      const auto [priority, taskId] = taskPriorities.top();
+      taskPriorities.pop();
+
+      const auto it = tasks.find(taskId);
+      if (it != tasks.end()) {
+        const auto [userId, actualPriority] = it->second;
+        if (priority == actualPriority) {
+          rmv(taskId);
+          return userId;
+        }
+      }
+    }
+    return -1;
   }
 };
