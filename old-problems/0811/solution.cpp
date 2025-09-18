035bde7fd61b2da192007f64df811a663ebe0ea9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-18 12:18:49 +0700

    test: add test for problem 811. Subdomain Visit Count
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0811/solution.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::string> subdomainVisits(
+      std::vector<std::string>& cpdomains) {
+    return cpdomains;
+  }
+};
