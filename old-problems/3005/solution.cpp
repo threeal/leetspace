1ce6e6ea9bf954d03681000c86245b457bf678fe
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-22 09:36:00 +0700

    refactor(problem-3005): move to old problem
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3005/solution.cpp
@@ -0,0 +1,26 @@
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+ public:
+  int maxFrequencyElements(std::vector<int>& nums) {
+    int freqs[101]{0};
+    for (const auto num : nums) {
+      ++freqs[num];
+    }
+
+    int maxFreq{0}, sum{0};
+    for (int i{0}; i < 101; ++i) {
+      if (freqs[i] >= maxFreq) {
+        if (freqs[i] > maxFreq) {
+          maxFreq = freqs[i];
+          sum = maxFreq;
+        } else {
+          sum += maxFreq;
+        }
+      }
+    }
+
+    return sum;
+  }
+};
