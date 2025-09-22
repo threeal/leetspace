21a9f70adb08af01bb05bca414a20da7a5441881
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-22 11:08:35 +0700

    feat(problem-3602/c): solve by using while-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3602/c/solution.c
+++ old-problems/3602/c/solution.c
@@ -1,4 +1,27 @@
+char buff[12];
+
 char* concatHex36(int n) {
-  (void)n;
-  return 0;
+  int r = 0;
+
+  int val = n * n * n;
+  while (val > 0) {
+    buff[r++] = val % 36 < 10 ? ('0' + val % 36) : ('A' + val % 36 - 10);
+    val /= 36;
+  }
+
+  val = n * n;
+  while (val > 0) {
+    buff[r++] = val % 16 < 10 ? ('0' + val % 16) : ('A' + val % 16 - 10);
+    val /= 16;
+  }
+
+  buff[r--] = 0;
+
+  for (int l = 0; l < r; ++l, --r) {
+    const int temp = buff[l];
+    buff[l] = buff[r];
+    buff[r] = temp;
+  }
+
+  return buff;
 }
