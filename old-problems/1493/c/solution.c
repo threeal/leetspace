cc53ae08d56536909a48d0dcb1b9c47cbce9d689
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-23 14:51:28 +0700

    feat(problem-1493/c): solve by using a while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1493/c/solution.c
+++ old-problems/1493/c/solution.c
@@ -1,3 +1,19 @@
 int longestSubarray(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int i = 0;
+  while (i < numsSize && nums[i] == 1) ++i;
+
+  if (i == numsSize) return i - 1;
+
+  int count = 0, maxCount = i, prev = i;
+  while (++i < numsSize) {
+    if (nums[i] == 0) {
+      if (prev + count > maxCount) maxCount = prev + count;
+      prev = count;
+      count = 0;
+    } else {
+      ++count;
+    }
+  }
+
+  return prev + count > maxCount ? prev + count : maxCount;
 }
