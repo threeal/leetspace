efbef472d678b9d73dbcfb2e30f188cf7ab85123
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-23 20:51:13 +0700

    feat(problem-2032/cpp): solve by keeping track of existing numbers in map array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2032/solution.cpp
+++ old-problems/2032/solution.cpp
@@ -6,6 +6,37 @@ class Solution {
       std::vector<int>& nums1,
       std::vector<int>& nums2,
       std::vector<int>& nums3) {
-    return nums1.size() > nums2.size() ? nums2 : nums3;
+    std::vector<int> output{};
+    std::vector<bool> exists(101, false);
+    std::vector<bool> visited(101, false);
+
+    for (const int num : nums1) {
+      exists[num] = true;
+    }
+
+    for (const int num : nums2) {
+      if (!visited[num]) {
+        visited[num] = true;
+        if (exists[num]) {
+          exists[num] = false;
+          output.push_back(num);
+        } else {
+          exists[num] = true;
+        }
+      }
+    }
+
+    visited.assign(visited.size(), false);
+
+    for (const int num : nums3) {
+      if (!visited[num]) {
+        visited[num] = true;
+        if (exists[num]) {
+          output.push_back(num);
+        }
+      }
+    }
+
+    return output;
   }
 };
