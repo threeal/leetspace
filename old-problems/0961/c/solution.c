ac90265da43baed7a5ebb93957ff9133779c6309
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-23 14:30:20 +0700

    feat(problem-961/c): solve by finding element that appear twice
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0961/c/solution.c
+++ old-problems/0961/c/solution.c
@@ -1,3 +1,13 @@
+#include <stdint.h>
+
 int repeatedNTimes(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  uint8_t exists[10001 / 8] = {0};
+  for (int i = 0; i < numsSize; ++i) {
+    if ((exists[nums[i] / 8] & (1 << (nums[i] % 8))) != 0) {
+      return nums[i];
+    } else {
+      exists[nums[i] / 8] |= 1 << (nums[i] % 8);
+    }
+  }
+  return 0;
 }
