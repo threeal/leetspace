20b943d7bbaa6ecba71f9a1f0f915f9f3efad2af
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-23 14:45:09 +0700

    feat(problem-3195/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3195/solution.cpp
+++ old-problems/3195/solution.cpp
@@ -3,6 +3,28 @@
 class Solution {
  public:
   int minimumArea(std::vector<std::vector<int>>& grid) {
-    return grid.size();
+    std::size_t t{grid.size() - 1};
+    std::size_t l{grid[0].size() - 1};
+    std::size_t b{0};
+    std::size_t r{0};
+
+    for (std::size_t i{0}; i < grid.size(); ++i) {
+      bool has_one{false};
+      for (std::size_t j{0}; j < grid[i].size(); ++j) {
+        if (grid[i][j] == 1) {
+          has_one = true;
+
+          if (j < l) l = j;
+          if (j > r) r = j;
+        }
+      }
+
+      if (has_one) {
+        if (i < t) t = i;
+        if (i > b) b = i;
+      }
+    }
+
+    return (b - t + 1) * (r - l + 1);
   }
 };
