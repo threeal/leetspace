4e4daa8ac90a79cb1f376ac4c58eb1c67797f4a5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-25 15:49:44 +0700

    feat(problem-890/cpp): solve by keeping track of characters mapping
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0890/solution.cpp
+++ old-problems/0890/solution.cpp
@@ -5,7 +5,39 @@ class Solution {
  public:
   std::vector<std::string> findAndReplacePattern(
       std::vector<std::string>& words, std::string pattern) {
-    words.push_back(pattern);
+    std::vector<char> charIds(26, 0);
+    char charId{0};
+
+    for (std::size_t i{0}; i < pattern.size(); ++i) {
+      if (charIds[pattern[i] - 'a'] == 0) {
+        charIds[pattern[i] - 'a'] = ++charId;
+      }
+      pattern[i] = charIds[pattern[i] - 'a'];
+    }
+
+    std::size_t n{0};
+    for (std::size_t i{0}; i < words.size(); ++i) {
+      std::string word{words[i]};
+
+      charIds.assign(26, 0);
+      charId = 0;
+
+      bool valid{true};
+      for (std::size_t j{0}; j < pattern.size(); ++j) {
+        if (charIds[word[j] - 'a'] == 0) {
+          charIds[word[j] - 'a'] = ++charId;
+        }
+
+        if (charIds[word[j] - 'a'] != pattern[j]) {
+          valid = false;
+          break;
+        }
+      }
+
+      if (valid) words[n++] = words[i];
+    }
+
+    words.resize(n);
     return words;
   }
 };
