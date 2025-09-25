6a728f431e5dac98e403170542c6136b4fa4171e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-25 15:28:51 +0700

    feat(problem-3195/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3195/c/solution.c
+++ old-problems/3195/c/solution.c
@@ -1,3 +1,27 @@
+#include <stdbool.h>
+
 int minimumArea(int** grid, int gridSize, int* gridColSize) {
-  return grid[gridSize - 1][gridColSize[gridSize - 1] - 1];
+  int t = gridSize - 1;
+  int l = gridColSize[0] - 1;
+  int b = 0;
+  int r = 0;
+
+  for (int i = 0; i < gridSize; ++i) {
+    bool has_one = false;
+    for (int j = 0; j < gridColSize[i]; ++j) {
+      if (grid[i][j] == 1) {
+        has_one = true;
+
+        if (j < l) l = j;
+        if (j > r) r = j;
+      }
+    }
+
+    if (has_one) {
+      if (i < t) t = i;
+      if (i > b) b = i;
+    }
+  }
+
+  return (b - t + 1) * (r - l + 1);
 }
