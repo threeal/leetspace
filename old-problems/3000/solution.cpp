745c5d8498833e4771264d55d52185b15d984620
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-27 22:06:44 +0700

    feat(problem-3000/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3000/solution.cpp
+++ old-problems/3000/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   int areaOfMaxDiagonal(std::vector<std::vector<int>>& dimensions) {
-    return dimensions.front().front();
+    int maxDiagonal{0}, maxArea{0};
+    for (const auto& dim : dimensions) {
+      const int diagonal{dim[0] * dim[0] + dim[1] * dim[1]};
+      if (diagonal >= maxDiagonal) {
+        const int area{dim[0] * dim[1]};
+        if (diagonal > maxDiagonal) {
+          maxDiagonal = diagonal;
+          maxArea = area;
+        } else if (area > maxArea) {
+          maxArea = area;
+        }
+      }
+    }
+    return maxArea;
   }
 };
