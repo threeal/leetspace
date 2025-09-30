b3015f7b7ae37bbe726b55afc12088e43f04ff8d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-30 13:09:03 +0700

    feat(problem-3000/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3000/c/solution.c
+++ old-problems/3000/c/solution.c
@@ -1,4 +1,20 @@
 int areaOfMaxDiagonal(
     int** dimensions, int dimensionsSize, int* dimensionsColSize) {
-  return dimensions[dimensionsSize - 1][dimensionsColSize[dimensionsSize - 1] - 1];
+  (void)dimensionsColSize;
+  int maxDiagonal = 0, maxArea = 0;
+  for (int i = 0; i < dimensionsSize; ++i) {
+    const int diagonal =
+        dimensions[i][0] * dimensions[i][0] + dimensions[i][1] * dimensions[i][1];
+
+    if (diagonal >= maxDiagonal) {
+      const int area = dimensions[i][0] * dimensions[i][1];
+      if (diagonal > maxDiagonal) {
+        maxDiagonal = diagonal;
+        maxArea = area;
+      } else if (area > maxArea) {
+        maxArea = area;
+      }
+    }
+  }
+  return maxArea;
 }
