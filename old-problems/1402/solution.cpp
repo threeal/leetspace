b0a41f650610d1beefa7d820b65259f500c0fbdb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-30 15:51:52 +0700

    feat(problem-1402/cpp): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1402/solution.cpp
+++ old-problems/1402/solution.cpp
@@ -1,8 +1,15 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maxSatisfaction(std::vector<int>& satisfaction) {
-    return satisfaction.front();
+    std::sort(satisfaction.begin(), satisfaction.end(), std::greater<int>());
+    int maxSum{0}, sum{0}, diff{0};
+    for (const int num : satisfaction) {
+      if ((diff += num) < 0) break;
+      if ((sum += diff) > maxSum) maxSum = sum;
+    }
+    return maxSum;
   }
 };
