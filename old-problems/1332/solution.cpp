6ceb5a90a27497b5896516905c186c33ae17d210
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-01 16:46:25 +0700

    feat(problem-1332/cpp): solve by checking polindrom
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1332/solution.cpp
+++ old-problems/1332/solution.cpp
@@ -3,6 +3,9 @@
 class Solution {
  public:
   int removePalindromeSub(std::string s) {
-    return s.size();
+    for (std::size_t l{0}, r{s.size() - 1}; l < r; ++l, --r) {
+      if (s[l] != s[r]) return 2;
+    }
+    return 1;
   }
 };
