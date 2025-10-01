998e8f1750876a1a9cb4c1de680dd3959305b31a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-01 09:42:42 +0700

    feat(problem-3446/c): solve by storing diagonal in temporary array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3446/c/solution.c
+++ old-problems/3446/c/solution.c
@@ -1,6 +1,52 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
+int compareGreater(const void* a, const void* b) {
+  return *(int*)b - *(int*)a;
+}
+
 int** sortMatrix(
     int** grid, int gridSize, int* gridColSize,
     int* returnSize, int** returnColumnSizes) {
+  const int m = gridSize;
+  const int n = gridColSize[0];
+
+  int* arr = malloc((m <= n ? m : n) * sizeof(int));
+  for (int i = 0; i < m; ++i) {
+    int k = 0;
+    while (i + k < m && k < n) {
+      arr[k] = grid[i + k][k];
+      ++k;
+    }
+
+    qsort(arr, k, sizeof(int), compareGreater);
+
+    while (k > 0) {
+      --k;
+      grid[i + k][k] = arr[k];
+    }
+  }
+
+  for (int j = 1; j < n; ++j) {
+    int k = 0;
+    while (k < m && j + k < n) {
+      arr[k] = grid[k][j + k];
+      ++k;
+    }
+
+    qsort(arr, k, sizeof(int), compare);
+
+    while (k > 0) {
+      --k;
+      grid[k][j + k] = arr[k];
+    }
+  }
+
+  free(arr);
+
   *returnSize = gridSize;
   *returnColumnSizes = gridColSize;
   return grid;
