75215752905a4a69e6f0788b52653a3a392a1f9c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-01 17:14:27 +0700

    feat(problem-2733/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2733/solution.cpp
+++ old-problems/2733/solution.cpp
@@ -3,6 +3,24 @@
 class Solution {
  public:
   int findNonMinOrMax(std::vector<int>& nums) {
-    return nums.size();
+    int min{nums[0]}, max{nums[0]};
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] < min) {
+        if (min == max) {
+          min = nums[i];
+        } else {
+          return min;
+        }
+      } else if (nums[i] > max) {
+        if (min == max) {
+          max = nums[i];
+        } else {
+          return max;
+        }
+      } else if (nums[i] > min && nums[i] < max) {
+        return nums[i];
+      }
+    }
+    return -1;
   }
 };
