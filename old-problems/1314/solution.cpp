298e43a6e0c25e07791617e2e3e7faf4c7c51b2f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-02 15:03:13 +0700

    test: add test for problem 1314. Matrix Block Sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1314/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> matrixBlockSum(
+      std::vector<std::vector<int>>& mat, int k) {
+    (void)k;
+    return mat;
+  }
+};
