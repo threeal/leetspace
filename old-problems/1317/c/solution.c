d4b78c2a0646419195e8fea9a8c867f3f6be6f03
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-06 14:07:26 +0700

    feat(problem-1317/c): solve by using for-loop and while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1317/c/solution.c
+++ old-problems/1317/c/solution.c
@@ -1,5 +1,25 @@
+int output[2];
+
 int* getNoZeroIntegers(int n, int* returnSize) {
-  (void)n;
-  *returnSize = 0;
-  return 0;
+  for (int i = 1; i < n; ++i) {
+    int j = i;
+    while (j > 0 && j % 10 != 0) j /= 10;
+    if (j > 0) continue;
+
+    j = n - i;
+    while (j > 0 && j % 10 != 0) j /= 10;
+    if (j > 0) continue;
+
+    output[0] = i;
+    output[1] = n - i;
+
+    *returnSize = 2;
+    return output;
+  }
+
+  output[0] = 0;
+  output[1] = n;
+
+  *returnSize = 2;
+  return output;
 }
