5bd45e835ae68888411341ebacbf5390eb19e268
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-07 14:26:26 +0700

    feat(problem-3668/cpp): solve by storing friends in array map
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3668/solution.cpp
+++ old-problems/3668/solution.cpp
@@ -4,6 +4,15 @@ class Solution {
  public:
   std::vector<int> recoverOrder(
       std::vector<int>& order, std::vector<int>& friends) {
-    return order.size() > friends.size() ? order : friends;
+    std::vector<bool> isFriends(order.size() + 1, false);
+    for (const int id : friends) isFriends[id] = true;
+
+    std::size_t n{0};
+    for (std::size_t i{0}; i < order.size(); ++i) {
+      if (isFriends[order[i]]) order[n++] = order[i];
+    }
+
+    order.resize(n);
+    return order;
   }
 };
