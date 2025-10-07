a80168a81e3cb9db4d17fa120d0ae50d8adb100f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-07 11:06:42 +0700

    feat(problem-2428/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2428/solution.cpp
+++ old-problems/2428/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   int maxSum(std::vector<std::vector<int>>& grid) {
-    return grid.size();
+    int max{0};
+    for (std::size_t i{2}; i < grid.size(); ++i) {
+      for (std::size_t j{2}; j < grid[i].size(); ++j) {
+        const int sum{
+            grid[i - 2][j - 2] + grid[i - 2][j - 1] + grid[i - 2][j] +
+            grid[i - 1][j - 1] +
+            grid[i][j - 2] + grid[i][j - 1] + grid[i][j]};
+        if (sum > max) max = sum;
+      }
+    }
+    return max;
   }
 };
