e77ae4df7b871b1d53692a58cb8483db4612d7a3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-07 11:19:48 +0700

    test: add test for problem 3242. Design Neighbor Sum Service
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3242/solution.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+class NeighborSum {
+ public:
+  NeighborSum(std::vector<std::vector<int>>& grid) {
+    (void)grid;
+  }
+
+  int adjacentSum(int value) {
+    return value;
+  }
+
+  int diagonalSum(int value) {
+    return value;
+  }
+};
