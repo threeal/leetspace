47114919dd555507c4eca60a394bf5a9c4edc14b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-09 21:39:07 +0800

    feat(problem-2089/c): solve by counting less than or equal elements
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2089/c/solution.c
+++ old-problems/2089/c/solution.c
@@ -1,5 +1,18 @@
+#include <stdlib.h>
+
 int* targetIndices(int* nums, int numsSize, int target, int* returnSize) {
-  (void)target;
-  *returnSize = numsSize;
-  return nums;
+  int less = 0, equal = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] < target) {
+      ++less;
+    } else if (nums[i] == target) {
+      ++equal;
+    }
+  }
+
+  int* output = malloc(equal * sizeof(int));
+  for (int i = 0; i < equal; ++i) output[i] = less + i;
+
+  *returnSize = equal;
+  return output;
 }
