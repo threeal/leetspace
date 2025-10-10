04da872c4dc6da4e3c2c6bd730ba5e79bbc5edd3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-10 11:15:41 +0800

    feat(problem-3147/cpp): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3147/solution.cpp
+++ old-problems/3147/solution.cpp
@@ -1,8 +1,19 @@
+#include <limits>
 #include <vector>
 
 class Solution {
  public:
   int maximumEnergy(std::vector<int>& energy, int k) {
-    return energy.size() + k;
+    int maxEnergy{std::numeric_limits<int>::min()};
+    for (std::size_t i{energy.size() - k}; i < energy.size(); ++i) {
+      if (energy[i] > maxEnergy) maxEnergy = energy[i];
+    }
+
+    for (std::size_t i{energy.size() - k}; i > 0; --i) {
+      energy[i - 1] += energy[i + k - 1];
+      if (energy[i - 1] > maxEnergy) maxEnergy = energy[i - 1];
+    }
+
+    return maxEnergy;
   }
 };
