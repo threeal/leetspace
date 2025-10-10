4a2eacd68919363017acb962c3331e75daa214ab
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-10 17:30:48 +0800

    feat(problem-1935/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1935/c/solution.c
+++ old-problems/1935/c/solution.c
@@ -1,3 +1,22 @@
+#include <stdbool.h>
+#include <stdint.h>
+
 int canBeTypedWords(char* text, char* brokenLetters) {
-  return *text + *brokenLetters;
+  uint32_t isBrokenLetters = 0;
+  for (int i = 0; brokenLetters[i] != 0; ++i) {
+    isBrokenLetters |= 1 << (brokenLetters[i] - 'a');
+  }
+
+  int count = 0;
+  bool isBroken = false;
+  for (int i = 0; text[i] != 0; ++i) {
+    if (text[i] == ' ') {
+      if (!isBroken) ++count;
+      isBroken = false;
+    } else if (!isBroken && (isBrokenLetters & 1 << (text[i] - 'a')) != 0) {
+      isBroken = true;
+    }
+  }
+
+  return isBroken ? count : count + 1;
 }
