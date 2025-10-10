e9497d541effcc59b5696b4091c828befe171b9b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-10 11:28:40 +0800

    test(problem-3668): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3668/c/solution.c
@@ -0,0 +1,7 @@
+int* recoverOrder(
+    int* order, int orderSize, int* friends, int friendsSize, int* returnSize) {
+  (void)friends;
+  (void)friendsSize;
+  *returnSize = orderSize;
+  return order;
+}
