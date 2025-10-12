43b1fda4e424a361116acdbd2def299809d9d4eb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-12 20:44:14 +0800

    feat(problem-3025/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3025/c/solution.c
+++ old-problems/3025/c/solution.c
@@ -1,3 +1,27 @@
+#include <stdbool.h>
+
 int numberOfPairs(int** points, int pointsSize, int* pointsColSize) {
-  return points[pointsSize - 1][pointsColSize[pointsSize - 1] - 1];
+  (void)pointsColSize;
+  int count = 0;
+  for (int i = 0; i < pointsSize; ++i) {
+    for (int j = 0; j < pointsSize; ++j) {
+      if (i != j &&
+          points[i][0] <= points[j][0] &&
+          points[i][1] >= points[j][1]) {
+        bool valid = true;
+        for (int k = 0; k < pointsSize; ++k) {
+          if (k != i && k != j &&
+              points[k][0] >= points[i][0] &&
+              points[k][0] <= points[j][0] &&
+              points[k][1] <= points[i][1] &&
+              points[k][1] >= points[j][1]) {
+            valid = false;
+            break;
+          }
+        }
+        if (valid) ++count;
+      }
+    }
+  }
+  return count;
 }
