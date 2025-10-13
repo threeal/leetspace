601cb156791bbb16c86f3456822813d4d1de1440
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-13 18:46:56 +0800

    feat(problem-3461/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3461/solution.cpp
+++ old-problems/3461/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   bool hasSameDigits(std::string s) {
-    return s.empty();
+    for (char& c : s) c -= '0';
+
+    for (std::size_t n{s.size()}; n > 2; --n) {
+      for (std::size_t i{1}; i < n; ++i) {
+        s[i - 1] = (s[i - 1] + s[i]) % 10;
+      }
+    }
+
+    return s[0] == s[1];
   }
 };
