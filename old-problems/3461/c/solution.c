64d8920143ab57f825b057de86062aeeccd88f79
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-14 20:10:51 +0800

    feat(problem-3461/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3461/c/solution.c
+++ old-problems/3461/c/solution.c
@@ -1,5 +1,18 @@
 #include <stdbool.h>
 
 bool hasSameDigits(char* s) {
-  return *s;
+  int n = 0;
+  while (s[n] != 0) {
+    s[n] -= '0';
+    ++n;
+  }
+
+  while (n > 2) {
+    for (int i = 1; i < n; ++i) {
+      s[i - 1] = (s[i - 1] + s[i]) % 10;
+    }
+    --n;
+  }
+
+  return s[0] == s[1];
 }
