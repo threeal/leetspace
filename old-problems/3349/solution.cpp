9c445ed06dab3cb07ad540ce6f74829016763517
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-14 19:46:34 +0800

    feat(problem-3349/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3349/solution.cpp
+++ old-problems/3349/solution.cpp
@@ -3,6 +3,23 @@
 class Solution {
  public:
   bool hasIncreasingSubarrays(std::vector<int>& nums, int k) {
-    return nums.empty() || k;
+    const std::size_t kk = k;
+    bool prevIncreasing{false};
+    std::size_t l{0};
+    for (std::size_t r{1}; r < nums.size(); ++r) {
+      if (nums[r] <= nums[r - 1]) {
+        if (r - l >= kk) {
+          if (prevIncreasing || r - l >= kk * 2) {
+            return true;
+          } else {
+            prevIncreasing = true;
+          }
+        } else {
+          prevIncreasing = false;
+        }
+        l = r;
+      }
+    }
+    return prevIncreasing ? nums.size() - l >= kk : nums.size() - l >= kk * 2;
   }
 };
