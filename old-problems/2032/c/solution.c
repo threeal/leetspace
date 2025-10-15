b9e872dc0dbe5d9469581247dcfb35b0393bc4c5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-15 22:14:10 +0800

    test(problem-2032): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2032/c/solution.c
@@ -0,0 +1,10 @@
+int* twoOutOfThree(
+    int* nums1, int nums1Size, int* nums2, int nums2Size,
+    int* nums3, int nums3Size, int* returnSize) {
+  (void)nums2;
+  (void)nums2Size;
+  (void)nums3;
+  (void)nums3Size;
+  *returnSize = nums1Size;
+  return nums1;
+}
