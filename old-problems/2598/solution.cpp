e046050888aea430e71feee8229b01a9034790d4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-16 09:45:48 +0800

    feat(problem-2598/cpp): solve by counting modulo frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2598/solution.cpp
+++ old-problems/2598/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   int findSmallestInteger(std::vector<int>& nums, int value) {
-    return nums.size() + value;
+    std::vector<int> freqs(value, 0);
+    for (const int num : nums) {
+      ++freqs[(value + (num % value)) % value];
+    }
+
+    int candidate{0};
+    for (int i{1}; i < value; ++i) {
+      if (freqs[i] < freqs[candidate]) candidate = i;
+    }
+
+    return value * freqs[candidate] + candidate;
   }
 };
