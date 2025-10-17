787ddbf1e770c99e0b1d126be81969bb19019024
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-17 09:36:55 +0800

    feat(problem-213/c): solve by using a bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0213/c/solution.c
+++ old-problems/0213/c/solution.c
@@ -1,3 +1,26 @@
 int rob(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  if (numsSize == 1) return nums[0];
+
+  int a0 = nums[0], a1 = 0, b0 = 0, b1 = 0;
+  for (int i = 1; i + 1 < numsSize; ++i) {
+    b0 = a0 >= b0 + nums[i] ? a0 : b0 + nums[i];
+    b1 = a1 >= b1 + nums[i] ? a1 : b1 + nums[i];
+
+    if (++i + 1 >= numsSize) {
+      int max = a0;
+      if (a1 + nums[numsSize - 1] > max) max = a1 + nums[numsSize - 1];
+      if (b0 > max) max = b0;
+      if (b1 > max) max = b1;
+      return max;
+    }
+
+    a0 = b0 >= a0 + nums[i] ? b0 : a0 + nums[i];
+    a1 = b1 >= a1 + nums[i] ? b1 : a1 + nums[i];
+  }
+
+  int max = a0;
+  if (a1 > max) max = a1;
+  if (b0 > max) max = b0;
+  if (b1 + nums[numsSize - 1] > max) max = b1 + nums[numsSize - 1];
+  return max;
 }
