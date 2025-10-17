6360c8cce33312ed51a86b406d4a40cb10b18859
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-17 16:38:51 +0800

    feat(problem-3688/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3688/c/solution.c
+++ old-problems/3688/c/solution.c
@@ -1,3 +1,7 @@
 int evenNumberBitwiseORs(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int sum = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] % 2 == 0) sum |= nums[i];
+  }
+  return sum;
 }
