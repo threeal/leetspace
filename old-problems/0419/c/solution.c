fd758ed6e1f9e30e8721db0d3fcb500b204503ff
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-21 16:33:09 +0800

    feat(problem-419/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0419/c/solution.c
+++ old-problems/0419/c/solution.c
@@ -1,3 +1,22 @@
 int countBattleships(char** board, int boardSize, int* boardColSize) {
-  return board[boardSize - 1][boardColSize[boardSize - 1] - 1];
+  int count = 0;
+
+  if (board[0][0] == 'X') ++count;
+
+  for (int i = 1; i < boardColSize[0]; ++i) {
+    if (board[0][i] == 'X' && board[0][i - 1] == '.') ++count;
+  }
+
+  for (int i = 1; i < boardSize; ++i) {
+    if (board[i][0] == 'X' && board[i - 1][0] == '.') ++count;
+
+    for (int j = 1; j < boardColSize[i]; ++j) {
+      if (
+          board[i][j] == 'X' &&
+          board[i - 1][j] == '.' &&
+          board[i][j - 1] == '.') ++count;
+    }
+  }
+
+  return count;
 }
