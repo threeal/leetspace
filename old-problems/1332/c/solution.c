0daa18dff368e5e28b618dd38d9d77b0c30c3208
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-23 21:38:59 +0700

    feat(problem-1332/c): solve by checking polindrom
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1332/c/solution.c
+++ old-problems/1332/c/solution.c
@@ -1,3 +1,8 @@
+#include <string.h>
+
 int removePalindromeSub(char* s) {
-  return *s;
+  for (int l = 0, r = strlen(s) - 1; l < r; ++l, --r) {
+    if (s[l] != s[r]) return 2;
+  }
+  return 1;
 }
