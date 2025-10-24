0a1b3a079831a6d312f9f9e792c60843f4482d32
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-24 15:11:21 +0700

    test(problem-1733): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1733/c/solution.c
@@ -0,0 +1,11 @@
+int minimumTeachings(
+    int n, int** languages, int languagesSize, int* languagesColSize,
+    int** friendships, int friendshipsSize, int* friendshipsColSize) {
+  (void)languages;
+  (void)languagesSize;
+  (void)languagesColSize;
+  (void)friendships;
+  (void)friendshipsSize;
+  (void)friendshipsColSize;
+  return n;
+}
