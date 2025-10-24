8dca15c57a9d53f31b143c6dae59aea24e11c147
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-24 22:12:32 +0700

    feat(problem-2428/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2428/c/solution.c
+++ old-problems/2428/c/solution.c
@@ -1,3 +1,13 @@
 int maxSum(int** grid, int gridSize, int* gridColSize) {
-  return grid[gridSize - 1][gridColSize[gridSize - 1] - 1];
+  int max = 0;
+  for (int i = 2; i < gridSize; ++i) {
+    for (int j = 2; j < gridColSize[i]; ++j) {
+      const int sum =
+          grid[i - 2][j - 2] + grid[i - 2][j - 1] + grid[i - 2][j] +
+          grid[i - 1][j - 1] +
+          grid[i][j - 2] + grid[i][j - 1] + grid[i][j];
+      if (sum > max) max = sum;
+    }
+  }
+  return max;
 }
