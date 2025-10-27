74a92cf80fe4782765d9f7df035b110c53d970a4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-27 11:13:40 +0700

    feat(problem-930/c): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0930/c/solution.c
+++ old-problems/0930/c/solution.c
@@ -1,3 +1,21 @@
 int numSubarraysWithSum(int* nums, int numsSize, int goal) {
-  return nums[numsSize - 1] + goal;
+  int sum = 0, zeros = 0, count = 0;
+  for (int l = 0, r = 0; r < numsSize; ++r) {
+    sum += nums[r];
+    while (l < r) {
+      if (nums[l] == 0) {
+        ++zeros;
+        ++l;
+      } else if (sum > goal) {
+        zeros = 0;
+        ++l;
+        --sum;
+      } else {
+        break;
+      }
+    }
+
+    if (sum == goal) count += 1 + zeros;
+  }
+  return count;
 }
