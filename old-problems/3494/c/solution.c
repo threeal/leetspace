169c0fe4d88bd8df3d9a6cf7246d053047647feb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-28 11:50:23 +0700

    feat(problem-3494/c): solve by using prefix dynamic programming
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3494/c/solution.c
+++ old-problems/3494/c/solution.c
@@ -1,3 +1,22 @@
+#include <stdlib.h>
+
 long long minTime(int* skill, int skillSize, int* mana, int manaSize) {
-  return skill[skillSize - 1] + mana[manaSize - 1];
+  const int doneSize = skillSize + 1;
+  long long* done = malloc(doneSize * sizeof(long long));
+  for (int i = 0; i < doneSize; ++i) done[i] = 0;
+
+  for (int i = 0; i < manaSize; ++i) {
+    const long long m = mana[i];
+    for (int j = 0; j < skillSize; ++j) {
+      done[j + 1] = (done[j + 1] >= done[j] ? done[j + 1] : done[j]) + m * skill[j];
+    }
+    for (int j = skillSize - 1; j > 0; --j) {
+      done[j] = done[j + 1] - m * skill[j];
+    }
+  }
+
+  const long long res = done[skillSize];
+  free(done);
+
+  return res;
 }
