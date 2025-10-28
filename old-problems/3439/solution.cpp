fb36521151c547f161b77be387b4e82da3827324
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-28 14:19:50 +0700

    feat(problem-3439/cpp): solve by using queue for sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3439/solution.cpp
+++ old-problems/3439/solution.cpp
@@ -1,3 +1,4 @@
+#include <queue>
 #include <vector>
 
 class Solution {
@@ -5,6 +6,33 @@ class Solution {
   int maxFreeTime(
       int eventTime, int k, std::vector<int>& startTime,
       std::vector<int>& endTime) {
-    return eventTime + k + startTime[0] + endTime[0];
+    const std::size_t kk = k + 1;
+
+    int sum{startTime.front()};
+    int maxSum{sum};
+
+    std::queue<int> freeTimes{};
+    freeTimes.push(startTime.front());
+
+    for (std::size_t i{1}; i < endTime.size(); ++i) {
+      sum += startTime[i] - endTime[i - 1];
+      freeTimes.push(startTime[i] - endTime[i - 1]);
+      while (freeTimes.size() > kk) {
+        sum -= freeTimes.front();
+        freeTimes.pop();
+      }
+
+      if (sum > maxSum) maxSum = sum;
+    }
+
+    sum += eventTime - endTime.back();
+    freeTimes.push(eventTime - endTime.back());
+    while (freeTimes.size() > kk) {
+      sum -= freeTimes.front();
+      freeTimes.pop();
+    }
+
+    if (sum > maxSum) maxSum = sum;
+    return maxSum;
   }
 };
