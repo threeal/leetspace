bad18d8659bbe99992320bde681d6dc0cbaec3b6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-28 14:16:35 +0700

    feat(problem-3440/c): solve by checking gaps between events
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3440/c/solution.c
+++ old-problems/3440/c/solution.c
@@ -1,5 +1,53 @@
 int maxFreeTime(
     int eventTime, int* startTime, int startTimeSize,
     int* endTime, int endTimeSize) {
-  return eventTime + startTime[startTimeSize - 1] + endTime[endTimeSize - 1];
+  (void)endTimeSize;
+
+  const int n = startTimeSize;
+
+  int maxFree = 0, prevFree = startTime[0];
+  for (int i = 1; i < n; ++i) {
+    const int free = startTime[i] - endTime[i - 1];
+    if (free + prevFree > maxFree) maxFree = free + prevFree;
+    prevFree = free;
+  }
+
+  const int free = eventTime - endTime[n - 1];
+  if (free + prevFree > maxFree) maxFree = free + prevFree;
+
+  if (n >= 2) {
+    int maxEmpty = startTime[0];
+    for (int i = 2; i < n; ++i) {
+      if (endTime[i - 1] - startTime[i - 1] <= maxEmpty) {
+        const int free = startTime[i] - endTime[i - 2];
+        if (free > maxFree) maxFree = free;
+      }
+
+      const int empty = startTime[i - 1] - endTime[i - 2];
+      if (empty > maxEmpty) maxEmpty = empty;
+    }
+
+    if (endTime[n - 1] - startTime[n - 1] <= maxEmpty) {
+      const int free = eventTime - endTime[n - 2];
+      if (free > maxFree) maxFree = free;
+    }
+
+    maxEmpty = eventTime - endTime[n - 1];
+    for (int i = n - 1; i >= 2; --i) {
+      if (endTime[i - 1] - startTime[i - 1] <= maxEmpty) {
+        const int free = startTime[i] - endTime[i - 2];
+        if (free > maxFree) maxFree = free;
+      }
+
+      const int empty = startTime[i] - endTime[i - 1];
+      if (empty > maxEmpty) maxEmpty = empty;
+    }
+
+    if (endTime[0] - startTime[0] <= maxEmpty) {
+      const int free = startTime[1];
+      if (free > maxFree) maxFree = free;
+    }
+  }
+
+  return maxFree;
 }
