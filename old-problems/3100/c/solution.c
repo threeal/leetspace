84d307f330ccf18a1fcefcf7b3d42cff68304457
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-29 21:38:25 +0700

    feat: solve problem 3100. Water Bottles II in C (#4265)
    * test(problem-3100): add support to test in C
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(problem-3100/c): solve by using a while-loop
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3100/c/solution.c
@@ -0,0 +1,9 @@
+int maxBottlesDrunk(int numBottles, int numExchange) {
+  int totalDrink = numBottles;
+  while (numBottles >= numExchange) {
+    ++totalDrink;
+    numBottles += 1 - numExchange;
+    ++numExchange;
+  }
+  return totalDrink;
+}
