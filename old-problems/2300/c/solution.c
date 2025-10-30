ef3f5997289867906ec04622d4573dac38147ace
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-30 11:11:39 +0700

    feat(problem-2300/c): solve by using a binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2300/c/solution.c
+++ old-problems/2300/c/solution.c
@@ -1,9 +1,32 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int* successfulPairs(
     int* spells, int spellsSize, int* potions, int potionsSize,
     long long success, int* returnSize) {
-  (void)potions;
-  (void)potionsSize;
-  (void)success;
+  qsort(potions, potionsSize, sizeof(int), compare);
+
+  for (int i = 0; i < spellsSize; ++i) {
+    const long long spell = spells[i];
+
+    int low = 0, high = potionsSize - 1;
+    while (low < high) {
+      const int mid = low + (high - low) / 2;
+      if (spell * potions[mid] < success) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+
+    spells[i] = spell * potions[low] >= success
+        ? potionsSize - low
+        : 0;
+  }
+
   *returnSize = spellsSize;
   return spells;
 }
