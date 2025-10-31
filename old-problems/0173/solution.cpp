230df304e1cc0a51f6ce2a843d52b5f3c8118e1d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-31 11:03:08 +0700

    feat(problem-173/cpp): solve by using a stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0173/solution.cpp
+++ old-problems/0173/solution.cpp
@@ -1,14 +1,33 @@
+#include <stack>
+
 class BSTIterator {
+ private:
+  std::stack<TreeNode*> nodes{};
+
  public:
-  BSTIterator(TreeNode* root) {
-    (void)root;
+  BSTIterator(TreeNode* root) : nodes{} {
+    while (root != nullptr) {
+      nodes.push(root);
+      root = root->left;
+    }
   }
 
   int next() {
-    return 0;
+    TreeNode* node{nodes.top()};
+    nodes.pop();
+
+    const int val{node->val};
+
+    node = node->right;
+    while (node != nullptr) {
+      nodes.push(node);
+      node = node->left;
+    }
+
+    return val;
   }
 
   bool hasNext() {
-    return true;
+    return !nodes.empty();
   }
 };
