5aaadbe9cadb25df7055d22b1b61c49ffd81bb97
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-31 11:12:04 +0700

    feat(problem-2598/c): solve by counting modulo frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2598/c/solution.c
+++ old-problems/2598/c/solution.c
@@ -1,3 +1,17 @@
+#include <stdlib.h>
+
 int findSmallestInteger(int* nums, int numsSize, int value) {
-  return nums[numsSize - 1] + value;
+  int* freqs = malloc(value * sizeof(int));
+  for (int i = 0; i < value; ++i) freqs[i] = 0;
+
+  for (int i = 0; i < numsSize; ++i) {
+    ++freqs[(value + (nums[i] % value)) % value];
+  }
+
+  int candidate = 0;
+  for (int i = 1; i < value; ++i) {
+    if (freqs[i] < freqs[candidate]) candidate = i;
+  }
+
+  return value * freqs[candidate] + candidate;
 }
