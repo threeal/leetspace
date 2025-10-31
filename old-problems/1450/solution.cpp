2c128ad571ad511ebe496889d6bf578895126d96
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-31 10:42:34 +0700

    feat(problem-1450/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1450/solution.cpp
+++ old-problems/1450/solution.cpp
@@ -4,6 +4,10 @@ class Solution {
  public:
   int busyStudent(
       std::vector<int>& startTime, std::vector<int>& endTime, int queryTime) {
-    return startTime.size() + endTime.size() + queryTime;
+    int count{0};
+    for (std::size_t i{0}; i < startTime.size(); ++i) {
+      if (queryTime >= startTime[i] && queryTime <= endTime[i]) ++count;
+    }
+    return count;
   }
 };
