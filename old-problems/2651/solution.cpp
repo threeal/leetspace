57d29351c3ff90ee17753cea8e4918ae9fc4af36
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-10-31 10:32:46 +0700

    feat(problem-2651/cpp): solve by returning modulo of sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2651/solution.cpp
+++ old-problems/2651/solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
  public:
   int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
-    return arrivalTime + delayedTime;
+    return (arrivalTime + delayedTime) % 24;
   }
 };
