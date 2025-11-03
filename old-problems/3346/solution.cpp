7ee937abe9e349eb2ead51cbbcf24f5629c6c0ec
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-03 17:45:35 +0700

    feat(problem-3346/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3346/solution.cpp
+++ old-problems/3346/solution.cpp
@@ -1,8 +1,25 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maxFrequency(std::vector<int>& nums, int k, int numOperations) {
-    return nums.size() + k + numOperations;
+    std::sort(nums.begin(), nums.end());
+
+    std::size_t maxFreq{0}, l{0}, i{0}, r{0};
+    for (int num{nums.front()}; num <= nums.back(); ++num) {
+      while (nums[i] < num) ++i;
+
+      int equals{0};
+      while (i < nums.size() && nums[i] == num) ++equals, ++i;
+
+      while (nums[l] < num - k) ++l;
+      while (r < nums.size() && nums[r] <= num + k) ++r;
+
+      std::size_t freq{std::min<std::size_t>(r - l, equals + numOperations)};
+      if (freq > maxFreq) maxFreq = freq;
+    }
+
+    return maxFreq;
   }
 };
