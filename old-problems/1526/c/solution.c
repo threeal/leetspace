e86acf3d666f308d720f0c2fa87f60d49ae174b2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-03 17:58:28 +0700

    feat(problem-1526/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1526/c/solution.c
+++ old-problems/1526/c/solution.c
@@ -1,3 +1,9 @@
 int minNumberOperations(int* target, int targetSize) {
-  return target[targetSize - 1];
+  int count = target[0];
+  for (int i = 1; i < targetSize; ++i) {
+    if (target[i] - target[i - 1] > 0) {
+      count += target[i] - target[i - 1];
+    }
+  }
+  return count;
 }
