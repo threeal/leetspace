9b714f94b6cba0567140b22495e88c2c52a3ea49
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-05 09:58:11 +0700

    test: add test for problem 2788. Split Strings by Separator
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2788/solution.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::string> splitWordsBySeparator(
+      std::vector<std::string>& words, char separator) {
+    (void)separator;
+    return words;
+  }
+};
