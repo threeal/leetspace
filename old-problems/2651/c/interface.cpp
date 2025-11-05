e0820ecbdda716fdcb47935e8ea3c4f0df8d17aa
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-05 16:30:16 +0700

    test(problem-2651): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2651/c/interface.cpp
@@ -0,0 +1,7 @@
+extern "C" {
+int findDelayedArrivalTime(int arrivalTime, int delayedTime);
+}
+
+int solution_c(int arrivalTime, int delayedTime) {
+  return findDelayedArrivalTime(arrivalTime, delayedTime);
+}
