5a1f66fee6bba032c31eb342a0c17430e4a1fa18
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-05 11:11:04 +0700

    feat(problem-2932/cpp): solve by using a brute-force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2932/solution.cpp
+++ old-problems/2932/solution.cpp
@@ -1,8 +1,19 @@
+#include <algorithm>
+#include <cstdlib>
 #include <vector>
 
 class Solution {
  public:
   int maximumStrongPairXor(std::vector<int>& nums) {
-    return nums.size();
+    int max{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      for (std::size_t j{i}; j < nums.size(); ++j) {
+        if (std::abs(nums[j] - nums[i]) <= std::min(nums[i], nums[j]) &&
+            (nums[i] ^ nums[j]) > max) {
+          max = nums[i] ^ nums[j];
+        }
+      }
+    }
+    return max;
   }
 };
