87ec55a56da8318ac0238b265fcf1afefe1ea4c9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-06 20:55:43 +0700

    feat(problem-3349/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3349/c/solution.c
+++ old-problems/3349/c/solution.c
@@ -1,5 +1,21 @@
 #include <stdbool.h>
 
 bool hasIncreasingSubarrays(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] == k;
+  bool prevIncreasing = false;
+  int l = 0;
+  for (int r = 1; r < numsSize; ++r) {
+    if (nums[r] <= nums[r - 1]) {
+      if (r - l >= k) {
+        if (prevIncreasing || r - l >= k * 2) {
+          return true;
+        } else {
+          prevIncreasing = true;
+        }
+      } else {
+        prevIncreasing = false;
+      }
+      l = r;
+    }
+  }
+  return prevIncreasing ? numsSize - l >= k : numsSize - l >= k * 2;
 }
