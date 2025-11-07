81630fc1e056666720d88a1827ea558e8737a652
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-07 12:08:42 +0700

    feat(problem-3726/c): solve by using a while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3726/c/solution.c
+++ old-problems/3726/c/solution.c
@@ -1,3 +1,11 @@
 long long removeZeros(long long n) {
-  return n;
+  long long res = 0, exp = 1;
+  while (n != 0) {
+    if (n % 10 != 0) {
+      res += n % 10 * exp;
+      exp *= 10;
+    }
+    n /= 10;
+  }
+  return res;
 }
