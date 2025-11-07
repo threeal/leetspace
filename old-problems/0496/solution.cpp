23e808a5f8194cd38e1703f93629e8536a529649
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-07 09:32:43 +0700

    feat(problem-496/cpp): solve by using stack and map
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0496/solution.cpp
+++ old-problems/0496/solution.cpp
@@ -1,9 +1,26 @@
+#include <stack>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> nextGreaterElement(
       std::vector<int>& nums1, std::vector<int>& nums2) {
-    return nums1.size() >= nums2.size() ? nums1 : nums2;
+    std::unordered_map<int, std::size_t> indices{};
+    for (std::size_t i{0}; i < nums1.size(); ++i) {
+      indices.emplace(nums1[i], i);
+    }
+
+    std::stack<int> greaters{};
+    for (std::size_t i{nums2.size()}; i > 0; --i) {
+      const auto it = indices.find(nums2[i - 1]);
+      if (it != indices.end()) {
+        while (!greaters.empty() && greaters.top() <= it->first) greaters.pop();
+        nums1[it->second] = greaters.empty() ? -1 : greaters.top();
+      }
+      greaters.push(nums2[i - 1]);
+    }
+
+    return nums1;
   }
 };
