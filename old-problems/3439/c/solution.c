50596e3dc284cd19463e46a7f6402e6298b67e68
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-08 22:15:18 +0700

    feat(problem-3439/c): solve by counting gap between events
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3439/c/solution.c
+++ old-problems/3439/c/solution.c
@@ -1,6 +1,29 @@
 int maxFreeTime(
     int eventTime, int k, int* startTime, int startTimeSize,
     int* endTime, int endTimeSize) {
-  return eventTime + k + startTime[startTimeSize - 1] +
-      endTime[endTimeSize - 1];
+  (void)endTimeSize;
+  const int n = startTimeSize;
+
+  int eventsDuration = 0;
+  for (int i = 0; i < k; ++i) {
+    eventsDuration += endTime[i] - startTime[i];
+  }
+
+  if (k == n) return eventTime - eventsDuration;
+
+  int maxFreeDuration = startTime[k] - eventsDuration;
+  eventsDuration += (endTime[k] - startTime[k]) - (endTime[0] - startTime[0]);
+
+  for (int i = k + 1; i < n; ++i) {
+    const int freeDuration = startTime[i] - endTime[i - k - 1] - eventsDuration;
+    if (freeDuration > maxFreeDuration) maxFreeDuration = freeDuration;
+
+    eventsDuration += (endTime[i] - startTime[i]) -
+        (endTime[i - k] - startTime[i - k]);
+  }
+
+  const int freeDuration = eventTime - endTime[n - k - 1] - eventsDuration;
+  if (freeDuration > maxFreeDuration) maxFreeDuration = freeDuration;
+
+  return maxFreeDuration;
 }
