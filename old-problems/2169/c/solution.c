5b5b96879a4e98e95fe5f4fc31e1d0002aefcfb3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-09 21:40:03 +0700

    feat(problem-2169/c): solve by using a while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2169/c/solution.c
+++ old-problems/2169/c/solution.c
@@ -1,3 +1,12 @@
 int countOperations(int num1, int num2) {
-  return num1 + num2;
+  int count = 0;
+  while (num1 != 0 && num2 != 0) {
+    if (num1 >= num2) {
+      num1 -= num2;
+    } else {
+      num2 -= num1;
+    }
+    ++count;
+  }
+  return count;
 }
