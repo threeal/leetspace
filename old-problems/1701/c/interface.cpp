92756d723af8b486212d356294fd32d10262d1e6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-10 16:12:55 +0700

    ci: use self-hosted runner (#4378)
    * ci: use self-hosted runners in `ci` workflow
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * fix(problem-273/c): fix function declaration without prototype
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * test(problem-811): adjust output in `example_2`
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * test(problem-1701/c): fix return type in interface
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * ci: checkout without cleaning workspace
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1701/c/interface.cpp
+++ old-problems/1701/c/interface.cpp
@@ -5,7 +5,7 @@ double averageWaitingTime(
     int** customers, int customersSize, int* customersColSize);
 }
 
-int solution_c(std::vector<std::vector<int>> customers) {
+double solution_c(std::vector<std::vector<int>> customers) {
   std::vector<int*> customersDatas(customers.size());
   std::vector<int> customersSizes(customers.size());
   for (int i = customers.size() - 1; i >= 0; --i) {
