933e4df7940381574f26c9f64c6e3912fb73c5ba
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-10 13:23:44 +0700

    feat(problem-2788/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2788/c/solution.c
+++ old-problems/2788/c/solution.c
@@ -1,6 +1,38 @@
+#include <stdlib.h>
+
 char** splitWordsBySeparator(
     char** words, int wordsSize, char separator, int* returnSize) {
-  (void)separator;
-  *returnSize = wordsSize;
-  return words;
+  int maxOutputSize = wordsSize;
+  char** output = malloc(maxOutputSize * sizeof(char*));
+  int outputSize = 0;
+
+  for (int i = 0; i < wordsSize; ++i) {
+    int l = 0, r = 0;
+    while (words[i][r] != 0) {
+      if (words[i][r] == separator) {
+        if (r - l > 0) {
+          if (outputSize == maxOutputSize) {
+            maxOutputSize *= 2;
+            output = realloc(output, maxOutputSize * sizeof(char*));
+          }
+          output[outputSize++] = words[i] + l;
+          words[i][r] = 0;
+        }
+        l = r + 1;
+      }
+      ++r;
+    }
+
+    if (r - l > 0) {
+      if (outputSize == maxOutputSize) {
+        maxOutputSize *= 2;
+        output = realloc(output, maxOutputSize * sizeof(char*));
+      }
+      output[outputSize++] = words[i] + l;
+      words[i][r] = 0;
+    }
+  }
+
+  *returnSize = outputSize;
+  return output;
 }
