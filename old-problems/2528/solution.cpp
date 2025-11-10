e4ef336fdfb299eb8eff6f615a0610f3471199d8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-10 13:26:02 +0700

    feat(problem-2528/cpp): solve by using binary search and sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2528/solution.cpp
+++ old-problems/2528/solution.cpp
@@ -1,8 +1,59 @@
+#include <limits>
 #include <vector>
 
 class Solution {
  public:
   long long maxPower(std::vector<int>& stations, int r, int k) {
-    return stations.size() + r + k;
+    const std::size_t rr = r;
+
+    long long startSum{0};
+    for (int i{0}; i < r; ++i) startSum += stations[i];
+
+    long long sum{startSum};
+    long long low{std::numeric_limits<int>::max()};
+    long long high{std::numeric_limits<int>::min()};
+    for (std::size_t i{0}; i < stations.size(); ++i) {
+      if (i + rr < stations.size()) sum += stations[i + rr];
+      if (sum < low) low = sum;
+      if (sum > high) high = sum;
+      if (i >= rr) sum -= stations[i - rr];
+    }
+    high += k;
+
+    std::vector<int> powers(stations.size());
+
+    while (low < high) {
+      const long long mid{high - (high - low) / 2};
+
+      long long kk = k;
+      long long sum{startSum};
+      powers.assign(powers.size(), 0);
+
+      bool valid{true};
+      for (std::size_t i{0}; i < stations.size(); ++i) {
+        if (i + rr < stations.size()) sum += stations[i + rr];
+
+        if (sum < mid) {
+          if (mid - sum <= kk) {
+            powers[std::min(i + rr, stations.size() - 1)] = mid - sum;
+            kk -= mid - sum;
+            sum = mid;
+          } else {
+            valid = false;
+            break;
+          }
+        }
+
+        if (i >= rr) sum -= stations[i - rr] + powers[i - rr];
+      }
+
+      if (valid) {
+        low = mid;
+      } else {
+        high = mid - 1;
+      }
+    }
+
+    return low;
   }
 };
