88f0d4ec2b285ff7c66ee5904e5b86b5c66b76ba
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-10 19:20:00 +0700

    feat(problem-784/c): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0784/c/solution.c
+++ old-problems/0784/c/solution.c
@@ -1,5 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+
+static void explore(
+    char*** out, int* maxOutSize, int* outSize, char* s, int sLen, int i);
+
 char** letterCasePermutation(char* s, int* returnSize) {
-  (void)s;
-  *returnSize = 0;
-  return 0;
+  const int sLen = strlen(s);
+
+  int maxOutSize = sLen;
+  char** out = malloc(maxOutSize * sizeof(char*));
+  int outSize = 0;
+
+  explore(&out, &maxOutSize, &outSize, s, sLen, 0);
+
+  *returnSize = outSize;
+  return out;
+}
+
+static void explore(
+    char*** out, int* maxOutSize, int* outSize, char* s, int sLen, int i) {
+  if (s[i] == 0) {
+    if (*outSize == *maxOutSize) {
+      *maxOutSize *= 2;
+      *out = realloc(*out, *maxOutSize * sizeof(char*));
+    }
+    (*out)[(*outSize)++] = s;
+  } else {
+    explore(out, maxOutSize, outSize, s, sLen, i + 1);
+    if (s[i] > '9') {
+      char* ss = malloc((sLen + 1) * sizeof(char));
+      memcpy(ss, s, (sLen + 1) * sizeof(char));
+      ss[i] += s[i] <= 'Z' ? 'a' - 'A' : 'A' - 'a';
+      explore(out, maxOutSize, outSize, ss, sLen, i + 1);
+    }
+  }
 }
