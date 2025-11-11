1bad838f4f66bbfac30a3a744347b30e84a32343
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-11 22:17:08 +0700

    feat(problem-2023/c): solve by checking each pair
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2023/c/solution.c
+++ old-problems/2023/c/solution.c
@@ -1,5 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+
 int numOfPairs(char** nums, int numsSize, char* target) {
-  (void)nums;
-  (void)target;
-  return numsSize;
+  const int targetLen = strlen(target);
+
+  int* numsLen = malloc(numsSize * sizeof(int));
+  for (int i = 0; i < numsSize; ++i) numsLen[i] = strlen(nums[i]);
+
+  int count = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (numsLen[i] >= targetLen) continue;
+
+    int ii = 0;
+    while (ii < numsLen[i] && nums[i][ii] == target[ii]) ++ii;
+
+    if (ii < numsLen[i]) continue;
+
+    for (int j = 0; j < numsSize; ++j) {
+      if (i == j || ii + numsLen[j] != targetLen) continue;
+
+      int jj = 0;
+      while (jj < numsLen[j] && nums[j][jj] == target[ii + jj]) ++jj;
+
+      if (jj == numsLen[j]) ++count;
+    }
+  }
+
+  free(numsLen);
+  return count;
 }
