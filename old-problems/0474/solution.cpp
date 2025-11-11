2f4d07ddb3becd747519cc421c986760e4f2c46d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-11 08:57:27 +0700

    feat(problem-474/cpp): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0474/solution.cpp
+++ old-problems/0474/solution.cpp
@@ -4,6 +4,26 @@
 class Solution {
  public:
   int findMaxForm(std::vector<std::string>& strs, int m, int n) {
-    return strs.size() + m + n;
+    std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
+    for (const std::string& str : strs) {
+      int zeros{0}, ones{0};
+      for (const char c : str) {
+        if (c == '0') {
+          ++zeros;
+        } else {
+          ++ones;
+        }
+      }
+
+      for (int i{m}; i >= zeros; --i) {
+        for (int j{n}; j >= ones; --j) {
+          if (1 + dp[i - zeros][j - ones] > dp[i][j]) {
+            dp[i][j] = 1 + dp[i - zeros][j - ones];
+          }
+        }
+      }
+    }
+
+    return dp[m][n];
   }
 };
