23c93f2d9b9c6b52169e607b536b279f90ca8df7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-11 10:47:09 +0700

    feat(problem-1104/cpp): solve by using while-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1104/solution.cpp
+++ old-problems/1104/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   std::vector<int> pathInZigZagTree(int label) {
-    return {label};
+    int level{0};
+    while (1 << level <= label) ++level;
+
+    std::vector<int> output(level);
+    while (label > 0) {
+      output[level - 1] = label;
+      label = ((1 << level) - 1 - label + (1 << (level - 1))) / 2;
+      --level;
+    }
+
+    return output;
   }
 };
