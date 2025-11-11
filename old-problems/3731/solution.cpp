c14b9cc33774c080a47dae062fe7199785bb4060
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-11 11:01:51 +0700

    feat(problem-3731/cpp): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3731/solution.cpp
+++ old-problems/3731/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   std::vector<int> findMissingElements(std::vector<int>& nums) {
-    return nums;
+    std::sort(nums.begin(), nums.end());
+
+    std::vector<int> missings{};
+    missings.reserve(nums.back() + 1 - nums.front() - nums.size());
+
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      for (int num{nums[i - 1] + 1}; num < nums[i]; ++num) {
+        missings.push_back(num);
+      }
+    }
+
+    return missings;
   }
 };
