2c6ab4d2200e5acfdeddba90a7bfdc8f2505ea77
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-11 09:07:17 +0700

    feat(problem-2341/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2341/solution.cpp
+++ old-problems/2341/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   std::vector<int> numberOfPairs(std::vector<int>& nums) {
-    return nums;
+    int freqs[101]{0};
+    for (const int num : nums) ++freqs[num];
+
+    int pairs{0}, leftovers{0};
+    for (int num{0}; num <= 100; ++num) {
+      pairs += freqs[num] / 2;
+      leftovers += freqs[num] % 2;
+    }
+
+    return {pairs, leftovers};
   }
 };
