5c0fb75a7ae7926cea0529d095ef77bdfbc0c70a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-11 10:50:01 +0700

    feat(problem-3346/c): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3346/c/solution.c
+++ old-problems/3346/c/solution.c
@@ -1,3 +1,28 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int maxFrequency(int* nums, int numsSize, int k, int numOperations) {
-  return nums[numsSize - 1] + k + numOperations;
+  qsort(nums, numsSize, sizeof(int), compare);
+
+  int maxFreq = 0, l = 0, i = 0, r = 0;
+  for (int num = nums[0]; num <= nums[numsSize - 1]; ++num) {
+    while (nums[i] < num) ++i;
+
+    int equals = 0;
+    while (i < numsSize && nums[i] == num) ++equals, ++i;
+
+    while (nums[l] < num - k) ++l;
+    while (r < numsSize && nums[r] <= num + k) ++r;
+
+    const int freq = r - l < equals + numOperations
+        ? r - l
+        : equals + numOperations;
+
+    if (freq > maxFreq) maxFreq = freq;
+  }
+
+  return maxFreq;
 }
