ee689d5b381e402cb053d3a6570c980cb6acdf22
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-11 10:29:45 +0700

    feat(problem-2528/c): solve by using binary search and sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2528/c/solution.c
+++ old-problems/2528/c/solution.c
@@ -1,3 +1,59 @@
+#include <limits.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
 long long maxPower(int* stations, int stationsSize, int r, int k) {
-  return stations[stationsSize - 1] + r + k;
+  long long startSum = 0;
+  for (int i = 0; i < r; ++i) startSum += stations[i];
+
+  long long sum = startSum;
+  long long low = INT_MAX;
+  long long high = INT_MIN;
+  for (int i = 0; i < stationsSize; ++i) {
+    if (i + r < stationsSize) sum += stations[i + r];
+    if (sum < low) low = sum;
+    if (sum > high) high = sum;
+    if (i >= r) sum -= stations[i - r];
+  }
+  high += k;
+
+  int* powers = malloc(stationsSize * sizeof(int));
+  while (low < high) {
+    const long long mid = high - (high - low) / 2;
+
+    long long kk = k;
+    long long sum = startSum;
+    for (int i = 0; i < stationsSize; ++i) powers[i] = 0;
+
+    bool valid = true;
+    for (int i = 0; i < stationsSize; ++i) {
+      if (i + r < stationsSize) sum += stations[i + r];
+
+      if (sum < mid) {
+        if (mid - sum <= kk) {
+          const int idx = i + r <= stationsSize - 1
+              ? i + r
+              : stationsSize - 1;
+
+          powers[idx] = mid - sum;
+          kk -= mid - sum;
+          sum = mid;
+        } else {
+          valid = false;
+          break;
+        }
+      }
+
+      if (i >= r) sum -= stations[i - r] + powers[i - r];
+    }
+
+    if (valid) {
+      low = mid;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  free(powers);
+  return low;
 }
