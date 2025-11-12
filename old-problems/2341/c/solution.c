a9e3ad1d6da95259bc55a5417c57b3af9200cb61
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-12 21:54:35 +0700

    feat(problem-2341/c): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2341/c/solution.c
+++ old-problems/2341/c/solution.c
@@ -1,4 +1,18 @@
+int output[2];
+
 int* numberOfPairs(int* nums, int numsSize, int* returnSize) {
-  *returnSize = numsSize;
-  return nums;
+  int freqs[101] = {0};
+  for (int i = 0; i < numsSize; ++i) ++freqs[nums[i]];
+
+  int pairs = 0, leftovers = 0;
+  for (int num = 0; num <= 100; ++num) {
+    pairs += freqs[num] / 2;
+    leftovers += freqs[num] % 2;
+  }
+
+  output[0] = pairs;
+  output[1] = leftovers;
+
+  *returnSize = 2;
+  return output;
 }
