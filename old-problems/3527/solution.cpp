4d6e98bfe052f6bfaee7246a907b4e5f92239d16
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-12 11:34:24 +0700

    feat(problem-3527/cpp): solve by using hash map
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3527/solution.cpp
+++ old-problems/3527/solution.cpp
@@ -1,10 +1,35 @@
 #include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 class Solution {
  public:
   std::string findCommonResponse(
       std::vector<std::vector<std::string>>& responses) {
-    return responses.front().front();
+    std::unordered_map<std::string_view, int> freqs{};
+    for (const auto& responsesInDay : responses) {
+      std::unordered_set<std::string_view> visited{};
+      for (const auto& response : responsesInDay) {
+        if (!visited.contains(response)) {
+          visited.insert(response);
+          ++freqs[response];
+        }
+      }
+    }
+
+    std::string_view candidate{};
+    int candidateCount{0};
+
+    for (const auto& [response, count] : freqs) {
+      if (count > candidateCount ||
+          (count == candidateCount && response < candidate)) {
+        candidate = response;
+        candidateCount = count;
+      }
+    }
+
+    return std::string(candidate);
   }
 };
