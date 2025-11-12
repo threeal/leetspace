4f9221e58f87fd591e72cb40e5d65c6232295853
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-12 13:49:50 +0700

    feat(problem-1880/cpp): solve by converting each words into numeric representation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1880/solution.cpp
+++ old-problems/1880/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   bool isSumEqual(std::string firstWord, std::string secondWord, std::string targetWord) {
-    return firstWord == secondWord && secondWord == targetWord;
+    return convert(firstWord) + convert(secondWord) == convert(targetWord);
+  }
+
+ private:
+  int convert(const std::string& s) {
+    int num{0};
+    for (const char c : s) {
+      num = num * 10 + c - 'a';
+    }
+    return num;
   }
 };
