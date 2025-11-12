3acdb2bf49fb139903f7a4724cd1e6bd11b6e480
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-12 13:55:51 +0700

    feat(problem-3542/c): solve by using a monotonic stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3542/c/solution.c
+++ old-problems/3542/c/solution.c
@@ -1,3 +1,11 @@
 int minOperations(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int count = 0, n = -1;
+  for (int i = 0; i < numsSize; ++i) {
+    while (n >= 0 && nums[n] >= nums[i]) {
+      if (nums[n] > nums[i]) ++count;
+      --n;
+    }
+    if (nums[i] > 0) nums[++n] = nums[i];
+  }
+  return count + n + 1;
 }
