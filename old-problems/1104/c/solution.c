c83d84ed5d0b615d517cdcb970c8d066f16449cb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-13 09:37:34 +0700

    feat(problem-1104/c): solve by using while-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1104/c/solution.c
+++ old-problems/1104/c/solution.c
@@ -1,5 +1,17 @@
+#include <stdlib.h>
+
 int* pathInZigZagTree(int label, int* returnSize) {
-  (void)label;
-  *returnSize = 0;
-  return 0;
+  int level = 0;
+  while (1 << level <= label) ++level;
+
+  int* output = malloc(level * sizeof(int));
+  *returnSize = level;
+
+  while (label > 0) {
+    output[level - 1] = label;
+    label = ((1 << level) - 1 - label + (1 << (level - 1))) / 2;
+    --level;
+  }
+
+  return output;
 }
