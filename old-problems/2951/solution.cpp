7f89f4dd76a0d035a4f07e120720f417b8d830a5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-13 09:57:29 +0700

    feat(problem-2951/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2951/solution.cpp
+++ old-problems/2951/solution.cpp
@@ -3,6 +3,13 @@
 class Solution {
  public:
   std::vector<int> findPeaks(std::vector<int>& mountain) {
+    std::size_t n{0};
+    for (std::size_t i{2}; i < mountain.size(); ++i) {
+      if (mountain[i - 2] < mountain[i - 1] && mountain[i - 1] > mountain[i]) {
+        mountain[n++] = i - 1;
+      }
+    }
+    mountain.resize(n);
     return mountain;
   }
 };
