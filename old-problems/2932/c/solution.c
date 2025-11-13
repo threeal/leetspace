331f3d81ca10fa65aa1be0b87ac60d715168e07e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-13 11:09:01 +0700

    feat(problem-2932/c): solve by using a brute-force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2932/c/solution.c
+++ old-problems/2932/c/solution.c
@@ -1,3 +1,13 @@
 int maximumStrongPairXor(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int max = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    for (int j = i; j < numsSize; ++j) {
+      const int diff = nums[j] >= nums[i] ? nums[j] - nums[i] : nums[i] - nums[j];
+      const int min = nums[i] <= nums[j] ? nums[i] : nums[j];
+      if (diff <= min && (nums[i] ^ nums[j]) > max) {
+        max = nums[i] ^ nums[j];
+      }
+    }
+  }
+  return max;
 }
