58e74a98aab0189d15762f89831392cd72d3d15c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-13 08:59:56 +0700

    feat(problem-3328/cpp): solve by using while-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3228/solution.cpp
+++ old-problems/3228/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   int maxOperations(std::string s) {
-    return s.size();
+    int operations{0}, ones{0};
+
+    std::size_t i{0};
+    while (i < s.size()) {
+      while (i < s.size() && s[i] == '1') ++ones, ++i;
+      if (i == s.size()) break;
+
+      while (i < s.size() && s[i] == '0') ++i;
+      operations += ones;
+    }
+
+    return operations;
   }
 };
