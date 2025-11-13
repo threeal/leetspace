336b3b276ed99bc7e0aa22aadf7fd67dce82cb12
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-13 09:08:55 +0700

    feat(problem-944/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0944/solution.cpp
+++ old-problems/0944/solution.cpp
@@ -4,6 +4,15 @@
 class Solution {
  public:
   int minDeletionSize(std::vector<std::string>& strs) {
-    return strs.size();
+    int count{0};
+    for (std::size_t j{0}; j < strs[0].size(); ++j) {
+      for (std::size_t i{1}; i < strs.size(); ++i) {
+        if (strs[i][j] < strs[i - 1][j]) {
+          ++count;
+          break;
+        }
+      }
+    }
+    return count;
   }
 };
