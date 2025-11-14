1a5572dc393cb57b990886e7faa301b987d71188
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-14 10:09:32 +0700

    feat(problem-2654/c): solve by finding subarray with shortest GCD
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2654/c/solution.c
+++ old-problems/2654/c/solution.c
@@ -1,3 +1,33 @@
+static int findGcd(int a, int b) {
+  int tmp;
+  while (b) {
+    tmp = a % b;
+    a = b;
+    b = tmp;
+  }
+  return a < 0 ? -a : a;
+}
+
 int minOperations(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int ones = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] == 1) ++ones;
+  }
+
+  if (ones > 0) return numsSize - ones;
+
+  int minLen = numsSize + 1;
+  for (int i = 0; i < numsSize; ++i) {
+    int minNum = nums[i];
+    int maxJ = numsSize <= i + minLen ? numsSize : i + minLen;
+    for (int j = i + 1; j < maxJ; ++j) {
+      minNum = findGcd(minNum, nums[j]);
+      if (minNum == 1) {
+        minLen = j + 1 - i;
+        break;
+      }
+    }
+  }
+
+  return minLen <= numsSize ? numsSize + minLen - 2 : -1;
 }
