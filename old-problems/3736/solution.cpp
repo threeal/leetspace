efea6c19e996345dbeaf232aeab82a76c42498ef
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-14 10:03:42 +0700

    feat(problem-3736/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3736/solution.cpp
+++ old-problems/3736/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int minMoves(std::vector<int>& nums) {
-    return nums.size();
+    int moves{0}, max{nums[0]};
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] > max) {
+        moves += (nums[i] - max) * i;
+        max = nums[i];
+      } else {
+        moves += max - nums[i];
+      }
+    }
+    return moves;
   }
 };
