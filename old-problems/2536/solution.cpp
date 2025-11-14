f72cc87444b00842cfe78764b61cd9cf055a3f70
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-14 09:28:20 +0700

    test: add test for problem 2536. Increment Submatrices by One
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2536/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> rangeAddQueries(
+      int n, std::vector<std::vector<int>>& queries) {
+    (void)n;
+    return queries;
+  }
+};
