fca291fb6b81215fa09870d521a1e9df0689fe05
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-17 11:34:15 +0700

    feat(problem-883/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0883/c/solution.c
+++ old-problems/0883/c/solution.c
@@ -1,3 +1,25 @@
 int projectionArea(int** grid, int gridSize, int* gridColSize) {
-  return grid[gridSize - 1][gridColSize[gridSize - 1] - 1];
+  int area = 0;
+  for (int j = 0; j < gridColSize[0]; ++j) {
+    area += grid[0][j];
+  }
+
+  for (int i = 0; i < gridSize; ++i) {
+    int max = 0;
+    for (int j = 0; j < gridColSize[i]; ++j) {
+      if (grid[i][j] != 0) ++area;
+
+      if (grid[i][j] > max) {
+        area += grid[i][j] - max;
+        max = grid[i][j];
+      }
+
+      if (grid[i][j] > grid[0][j]) {
+        area += grid[i][j] - grid[0][j];
+        grid[0][j] = grid[i][j];
+      }
+    }
+  }
+
+  return area;
 }
