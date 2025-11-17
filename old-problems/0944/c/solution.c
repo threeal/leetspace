c2f9fa8b3560e297d5a3b6cc8c4ea494ff47f541
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-17 11:21:57 +0700

    feat(problem-944/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0944/c/solution.c
+++ old-problems/0944/c/solution.c
@@ -1,4 +1,14 @@
+#include <string.h>
+
 int minDeletionSize(char** strs, int strsSize) {
-  (void)strs;
-  return strsSize;
+  int count = 0, len = strlen(strs[0]);
+  for (int j = 0; j < len; ++j) {
+    for (int i = 1; i < strsSize; ++i) {
+      if (strs[i][j] < strs[i - 1][j]) {
+        ++count;
+        break;
+      }
+    }
+  }
+  return count;
 }
