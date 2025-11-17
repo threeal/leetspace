b65a8341fb24bb03d87f06c403e95d65c481f6c1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-17 19:25:31 +0700

    feat(problem-3239/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3239/c/solution.c
+++ old-problems/3239/c/solution.c
@@ -1,3 +1,17 @@
 int minFlips(int** grid, int gridSize, int* gridColSize) {
-  return grid[gridSize - 1][gridColSize[gridSize - 1] - 1];
+  int rowInvalids = 0;
+  for (int i = 0; i < gridSize; ++i) {
+    for (int l = 0, r = gridColSize[i] - 1; l < r; ++l, --r) {
+      if (grid[i][l] != grid[i][r]) ++rowInvalids;
+    }
+  }
+
+  int colInvalids = 0;
+  for (int i = 0; i < gridColSize[0]; ++i) {
+    for (int l = 0, r = gridSize - 1; l < r; ++l, --r) {
+      if (grid[l][i] != grid[r][i]) ++colInvalids;
+    }
+  }
+
+  return rowInvalids <= colInvalids ? rowInvalids : colInvalids;
 }
