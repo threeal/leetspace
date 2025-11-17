980917260f88a51c12eb4573f933a9535b7ffa4b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-17 14:17:51 +0700

    feat(problem-3079/cpp): solve by using loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3079/solution.cpp
+++ old-problems/3079/solution.cpp
@@ -3,6 +3,25 @@
 class Solution {
  public:
   int sumOfEncryptedInt(std::vector<int>& nums) {
-    return nums.size();
+    int sum{0};
+    for (int num : nums) {
+      int digits{0};
+      int maxDigit{0};
+      while (num > 0) {
+        if (num % 10 > maxDigit) maxDigit = num % 10;
+        num /= 10;
+        ++digits;
+      }
+
+      int encrypted{0};
+      while (digits > 0) {
+        encrypted = encrypted * 10 + maxDigit;
+        --digits;
+      }
+
+      sum += encrypted;
+    }
+
+    return sum;
   }
 };
