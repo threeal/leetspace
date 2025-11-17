0e43e60574b9e9a5e61de595a3c51c822bbbbe3a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-17 10:15:55 +0700

    feat(problem-3228/c): solve by using while-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3228/c/solution.c
+++ old-problems/3228/c/solution.c
@@ -1,3 +1,12 @@
 int maxOperations(char* s) {
-  return *s;
+  int operations = 0, ones = 0;
+  while (*s != 0) {
+    while (*s == '1') ++ones, ++s;
+    if (*s == 0) break;
+
+    while (*s == '0') ++s;
+    operations += ones;
+  }
+
+  return operations;
 }
