1576329ae14cf6154134fb0867a00bd91a096c9e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-18 13:02:20 +0700

    feat(problem-2643/cpp); solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2643/solution.cpp
+++ old-problems/2643/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   std::vector<int> rowAndMaximumOnes(std::vector<std::vector<int>>& mat) {
-    return mat[0];
+    int maxI{0}, maxCount{0};
+    for (std::size_t i{0}; i < mat.size(); ++i) {
+      int count{0};
+      for (std::size_t j{0}; j < mat[i].size(); ++j) {
+        if (mat[i][j] == 1) ++count;
+      }
+
+      if (count > maxCount) {
+        maxI = i;
+        maxCount = count;
+      }
+    }
+
+    return {maxI, maxCount};
   }
 };
