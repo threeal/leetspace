ca2243cc43a206d9bd1bfac8d964633f8162b5d2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-18 10:11:01 +0700

    feat(problem-1710/c): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1710/c/solution.c
+++ old-problems/1710/c/solution.c
@@ -1,5 +1,22 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return (*(int**)b)[1] - (*(int**)a)[1];
+}
+
 int maximumUnits(
     int** boxTypes, int boxTypesSize, int* boxTypesColSize, int truckSize) {
-  return boxTypes[boxTypesSize - 1][boxTypesColSize[boxTypesSize - 1] - 1] +
-      truckSize;
+  (void)boxTypesColSize;
+
+  qsort(boxTypes, boxTypesSize, sizeof(int*), compare);
+
+  int units = 0;
+  for (int i = 0; i < boxTypesSize; ++i) {
+    const int boxes = boxTypes[i][0] < truckSize ? boxTypes[i][0] : truckSize;
+    units += boxes * boxTypes[i][1];
+    truckSize -= boxes;
+    if (truckSize == 0) break;
+  }
+
+  return units;
 }
