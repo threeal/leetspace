9e1bceaff0165f4aeb04237241f96ed5b946d1fd
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-19 12:06:58 +0700

    feat(problem-3079/c): solve by using loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3079/c/solution.c
+++ old-problems/3079/c/solution.c
@@ -1,3 +1,23 @@
 int sumOfEncryptedInt(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int sum = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    int num = nums[i];
+    int digits = 0;
+    int maxDigit = 0;
+    while (num > 0) {
+      if (num % 10 > maxDigit) maxDigit = num % 10;
+      num /= 10;
+      ++digits;
+    }
+
+    int encrypted = 0;
+    while (digits > 0) {
+      encrypted = encrypted * 10 + maxDigit;
+      --digits;
+    }
+
+    sum += encrypted;
+  }
+
+  return sum;
 }
