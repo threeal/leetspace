aa4a9a60668401c7d7597d0773f44e41113691dd
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-19 21:35:34 +0700

    feat: solve problem 1742. Maximum Number of Balls in a Box in C (#4459)
    * test: add test for problem 1742. Maximum Number of Balls in a Box
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(problem-1742/c): solve by counting frequencies
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1742/c/solution.c
@@ -0,0 +1,17 @@
+int countBalls(int lowLimit, int highLimit) {
+  int freqs[46] = {0};
+  for (int num = lowLimit; num <= highLimit; ++num) {
+    const int sum =
+        (num % 10) + (num / 10 % 10) + (num / 100 % 10) + (num / 1000 % 10) +
+        (num / 10000 % 10) + (num / 100000);
+
+    ++freqs[sum];
+  }
+
+  int maxFreq = 0;
+  for (int num = 0; num < 46; ++num) {
+    if (freqs[num] > maxFreq) maxFreq = freqs[num];
+  }
+
+  return maxFreq;
+}
