fd461f20dfd79363407ef2e8d5c0a68590286e39
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-19 12:37:03 +0700

    feat(problem-3432/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3432/solution.cpp
+++ old-problems/3432/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   int countPartitions(std::vector<int>& nums) {
-    return nums.size();
+    int right{0};
+    for (const int num : nums) right += num;
+
+    nums.pop_back();
+
+    int count{0}, left{0};
+    for (const int num : nums) {
+      left += num;
+      right -= num;
+
+      if (left % 2 == right % 2) ++count;
+    }
+
+    return count;
   }
 };
