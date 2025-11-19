6c19c7563803d5fc54e731c8497d796cbb37e4f3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-19 14:12:38 +0700

    feat(problem-1833/cpp): solve by greedily pick the smallest cost
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1833/solution.cpp
+++ old-problems/1833/solution.cpp
@@ -3,6 +3,18 @@
 class Solution {
  public:
   int maxIceCream(std::vector<int>& costs, int coins) {
-    return costs.size() + coins;
+    std::sort(costs.begin(), costs.end());
+
+    int count{0};
+    for (const int cost : costs) {
+      coins -= cost;
+      if (coins >= 0) {
+        ++count;
+      } else {
+        break;
+      }
+    }
+
+    return count;
   }
 };
