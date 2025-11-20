4127697c071fcd6a6e29794634fd53f6b7f4922d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-20 09:08:07 +0700

    feat(problem-2255/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2255/solution.cpp
+++ old-problems/2255/solution.cpp
@@ -4,6 +4,19 @@
 class Solution {
  public:
   int countPrefixes(std::vector<std::string>& words, std::string s) {
-    return words.size() + s.size();
+    int count{0};
+    for (const std::string& word : words) {
+      if (word.size() <= s.size()) {
+        bool valid{true};
+        for (std::size_t i{0}; i < word.size(); ++i) {
+          if (word[i] != s[i]) {
+            valid = false;
+            break;
+          }
+        }
+        if (valid) ++count;
+      }
+    }
+    return count;
   }
 };
