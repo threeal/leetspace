80edf3d249f3403844192296a3362ecd22bc914e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-20 14:59:57 +0700

    feat(problem-717/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0717/c/solution.c
+++ old-problems/0717/c/solution.c
@@ -1,5 +1,7 @@
 #include <stdbool.h>
 
 bool isOneBitCharacter(int* bits, int bitsSize) {
-  return bits[bitsSize - 1] == 0;
+  int i = bitsSize - 2;
+  while (i >= 0 && bits[i] == 1) --i;
+  return (bitsSize - i) % 2 == 0;
 }
