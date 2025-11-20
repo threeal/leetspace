9fcdf878e3b571c2ef7d863b2ae2858e324f6a40
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-20 09:57:06 +0700

    feat(problem-757/cpp): solve by using greedy after sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0757/solution.cpp
+++ old-problems/0757/solution.cpp
@@ -1,8 +1,24 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int intersectionSizeTwo(std::vector<std::vector<int>>& intervals) {
-    return intervals.size();
+    std::sort(
+        intervals.begin(), intervals.end(), [](const auto& a, const auto& b) {
+          return a[1] == b[1] ? a[0] > b[0] : a[1] < b[1];
+        });
+
+    std::vector<int> nums{intervals[0][1] - 1, intervals[0][1]};
+    for (std::size_t i{1}; i < intervals.size(); ++i) {
+      if (intervals[i][0] > nums[nums.size() - 1]) {
+        nums.push_back(intervals[i][1] - 1);
+        nums.push_back(intervals[i][1]);
+      } else if (intervals[i][0] > nums[nums.size() - 2]) {
+        nums.push_back(intervals[i][1]);
+      }
+    }
+
+    return nums.size();
   }
 };
