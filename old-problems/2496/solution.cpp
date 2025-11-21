6e9a85c5bb0554e7fdab425dfe1d3cc76ea949a9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-21 13:55:23 +0700

    feat(problem-2496/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2496/solution.cpp
+++ old-problems/2496/solution.cpp
@@ -4,6 +4,19 @@
 class Solution {
  public:
   int maximumValue(std::vector<std::string>& strs) {
-    return strs.size();
+    int maxVal{0};
+    for (const std::string& str : strs) {
+      int val{0};
+      for (const char c : str) {
+        if (c >= '0' && c <= '9') {
+          val = val * 10 + c - '0';
+        } else {
+          val = str.size();
+          break;
+        }
+      }
+      if (val > maxVal) maxVal = val;
+    }
+    return maxVal;
   }
 };
