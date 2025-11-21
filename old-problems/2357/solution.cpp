1421e86b1aa8dfb520befec7001c28050e2fa4ff
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-21 16:25:20 +0700

    feat(problem-2357/cpp): solve by returning count of unique numbers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2357/solution.cpp
+++ old-problems/2357/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   int minimumOperations(std::vector<int>& nums) {
-    return nums.size();
+    std::vector<bool> exists(101, false);
+    exists[0] = true;
+
+    int uniques{0};
+    for (const int num : nums) {
+      if (!exists[num]) {
+        exists[num] = true;
+        ++uniques;
+      }
+    }
+
+    return uniques;
   }
 };
