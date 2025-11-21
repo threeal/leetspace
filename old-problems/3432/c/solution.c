6511987742e3ff6efeac074eb2c542a62afad62c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-21 17:25:13 +0700

    feat(problem-3432/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3432/c/solution.c
+++ old-problems/3432/c/solution.c
@@ -1,3 +1,16 @@
 int countPartitions(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int right = 0;
+  for (int i = 0; i < numsSize; ++i) right += nums[i];
+
+  --numsSize;
+
+  int count = 0, left = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    left += nums[i];
+    right -= nums[i];
+
+    if (left % 2 == right % 2) ++count;
+  }
+
+  return count;
 }
