ea51a3a1cd4416d895a2cf5759dbee5103ea4a1a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-21 14:10:14 +0700

    feat(problem-2154/c): solve by using while-loop after sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2154/c/solution.c
+++ old-problems/2154/c/solution.c
@@ -1,3 +1,21 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int findFinalValue(int* nums, int numsSize, int original) {
-  return nums[numsSize - 1] + original;
+  qsort(nums, numsSize, sizeof(int), compare);
+
+  int i = 0;
+  while (1) {
+    while (i < numsSize && nums[i] < original) ++i;
+    if (i < numsSize && nums[i] == original) {
+      original *= 2;
+    } else {
+      break;
+    }
+  }
+
+  return original;
 }
