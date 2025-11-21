84fabd39ee92fe4fb440e9583be8eaf8142af38d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-21 14:33:43 +0700

    feat(problem-1529/cpp): solve by using while loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1529/solution.cpp
+++ old-problems/1529/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   int minFlips(std::string target) {
-    return target.size();
+    std::size_t i{0}, count{0};
+    while (true) {
+      while (i < target.size() && target[i] == '0') ++i;
+      if (i == target.size()) break;
+      ++count;
+
+      while (i < target.size() && target[i] == '1') ++i;
+      if (i == target.size()) break;
+      ++count;
+    }
+    return count;
   }
 };
