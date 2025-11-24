fc0b78934a577814777065ec09df4c0a06ac65a5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-24 13:24:20 +0700

    feat(problem-2255/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2255/c/solution.c
+++ old-problems/2255/c/solution.c
@@ -1,3 +1,9 @@
 int countPrefixes(char** words, int wordsSize, char* s) {
-  return *words[wordsSize - 1] + *s;
+  int count = 0;
+  for (int i = 0; i < wordsSize; ++i) {
+    int j = 0;
+    while (words[i][j] != 0 && words[i][j] == s[j]) ++j;
+    if (words[i][j] == 0) ++count;
+  }
+  return count;
 }
