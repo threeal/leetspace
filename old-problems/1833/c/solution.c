2f56090d81b9b4b5d0e6aa9013b1c97b737ce521
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-24 13:09:42 +0700

    feat(problem-1833/c): solve by greedily pick the smallest cost
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1833/c/solution.c
+++ old-problems/1833/c/solution.c
@@ -1,3 +1,21 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int maxIceCream(int* costs, int costsSize, int coins) {
-  return costs[costsSize - 1] + coins;
+  qsort(costs, costsSize, sizeof(int), compare);
+
+  int count = 0;
+  for (int i = 0; i < costsSize; ++i) {
+    coins -= costs[i];
+    if (coins >= 0) {
+      ++count;
+    } else {
+      break;
+    }
+  }
+
+  return count;
 }
