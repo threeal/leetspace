d0c8192091bcc9dbfc1fcabee639e8a0f7152c56
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-24 22:21:47 +0700

    feat(problem-3684/cpp): solve by greedily pick distinct largest numbers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3684/solution.cpp
+++ old-problems/3684/solution.cpp
@@ -1,9 +1,17 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> maxKDistinct(std::vector<int>& nums, int k) {
-    (void)k;
+    std::sort(nums.begin(), nums.end(), std::greater<int>());
+
+    int n{1};
+    for (std::size_t i{1}; i < nums.size() && n < k; ++i) {
+      if (nums[i] != nums[i - 1]) nums[n++] = nums[i];
+    }
+
+    nums.resize(n);
     return nums;
   }
 };
