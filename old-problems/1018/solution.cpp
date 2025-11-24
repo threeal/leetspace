5f500d368bdf7226393a734d72c0c685e4ad6650
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-24 09:32:18 +0700

    feat(problem-1018/cpp): solve by keeping track of modulo while iterating array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1018/solution.cpp
+++ old-problems/1018/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   std::vector<bool> prefixesDivBy5(std::vector<int>& nums) {
-    return std::vector(nums.size(), false);
+    std::vector<bool> output(nums.size());
+
+    int ans{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      ans = ((ans << 1) + nums[i]) % 5;
+      output[i] = ans == 0;
+    }
+
+    return output;
   }
 };
