70d364720d041b23b3c6e50bef380bb4bb82fd45
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-26 09:59:04 +0700

    feat(problem-1529/c): solve by using while loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1529/c/solution.c
+++ old-problems/1529/c/solution.c
@@ -1,3 +1,13 @@
 int minFlips(char* target) {
-  return *target;
+  int i = 0, count = 0;
+  while (1) {
+    while (target[i] != 0 && target[i] == '0') ++i;
+    if (target[i] == 0) break;
+    ++count;
+
+    while (target[i] != 0 && target[i] == '1') ++i;
+    if (target[i] == 0) break;
+    ++count;
+  }
+  return count;
 }
