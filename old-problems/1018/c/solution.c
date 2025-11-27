0efab38d554799e91a80bdf4907079f935eefc3a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-27 13:39:41 +0700

    feat(problem-1018/c): solve by keeping track of modulo while iterating array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1018/c/solution.c
+++ old-problems/1018/c/solution.c
@@ -1,8 +1,15 @@
 #include <stdbool.h>
+#include <stdlib.h>
 
 bool* prefixesDivBy5(int* nums, int numsSize, int* returnSize) {
-  (void)nums;
-  (void)numsSize;
-  *returnSize = 0;
-  return 0;
+  bool* output = malloc(numsSize * sizeof(bool));
+
+  int ans = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    ans = ((ans << 1) + nums[i]) % 5;
+    output[i] = ans == 0;
+  }
+
+  *returnSize = numsSize;
+  return output;
 }
