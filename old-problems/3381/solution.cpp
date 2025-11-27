3b4a3a5f08e64f69ee4d5f8171c2888c9fddf410
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-27 13:16:39 +0700

    feat(problem-3381/cpp): solve by using prefix sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3381/solution.cpp
+++ old-problems/3381/solution.cpp
@@ -3,6 +3,21 @@
 class Solution {
  public:
   long long maxSubarraySum(std::vector<int>& nums, int k) {
-    return nums.size() + k;
+    long long sum{0};
+    for (int i{0}; i < k; ++i) sum += nums[i];
+
+    std::vector<long long> prefixes(nums.size(), 0);
+
+    prefixes[k - 1] = sum;
+    long long maxSum{sum};
+
+    for (std::size_t i = k; i < nums.size(); ++i) {
+      sum += nums[i] - nums[i - k];
+
+      prefixes[i] = prefixes[i - k] > 0 ? sum + prefixes[i - k] : sum;
+      if (prefixes[i] > maxSum) maxSum = prefixes[i];
+    }
+
+    return maxSum;
   }
 };
