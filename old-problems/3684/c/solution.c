c293116d0f42515d6f3aa21afc887f5ae62ed920
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-27 14:17:20 +0700

    feat(problem-3684/c): solve by greedily pick distinct largest numbers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3684/c/solution.c
+++ old-problems/3684/c/solution.c
@@ -1,5 +1,17 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)b - *(int*)a;
+}
+
 int* maxKDistinct(int* nums, int numsSize, int k, int* returnSize) {
-  (void)k;
-  *returnSize = numsSize;
+  qsort(nums, numsSize, sizeof(int), compare);
+
+  int n = 1;
+  for (int i = 1; i < numsSize && n < k; ++i) {
+    if (nums[i] != nums[i - 1]) nums[n++] = nums[i];
+  }
+
+  *returnSize = n;
   return nums;
 }
