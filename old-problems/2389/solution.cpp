0eea6d06e5048ec731f61dec81f639fcc70e2b5b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-28 12:50:28 +0700

    feat(problem-2389/cpp): solve by using greedy approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2389/solution.cpp
+++ old-problems/2389/solution.cpp
@@ -1,9 +1,31 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> answerQueries(
       std::vector<int>& nums, std::vector<int>& queries) {
-    return nums.size() >= queries.size() ? nums : queries;
+    std::vector<std::size_t> queryIndices(queries.size());
+    std::iota(queryIndices.begin(), queryIndices.end(), 0);
+
+    std::sort(nums.begin(), nums.end());
+
+    std::sort(
+        queryIndices.begin(), queryIndices.end(),
+        [&](const std::size_t a, const std::size_t b) {
+          return queries[a] < queries[b];
+        });
+
+    int sum{0};
+    std::size_t numsI{0};
+    for (const std::size_t i : queryIndices) {
+      while (numsI < nums.size() && sum + nums[numsI] <= queries[i]) {
+        sum += nums[numsI++];
+      }
+      queries[i] = numsI;
+    }
+
+    return queries;
   }
 };
