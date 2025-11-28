dc6d2a43655b76911f9c85aba364d79f90ee4626
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-28 13:21:48 +0700

    feat(problem-2283/cpp): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2283/solution.cpp
+++ old-problems/2283/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   bool digitCount(std::string num) {
-    return num.empty();
+    char freqs[10]{0};
+    for (const char digit : num) ++freqs[digit - '0'];
+    for (std::size_t i{0}; i < num.size(); ++i) {
+      if (num[i] - '0' != freqs[i]) return false;
+    }
+    return true;
   }
 };
