49f4116ce987fc6394c75e8208c6024c16f5a6de
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-28 09:50:14 +0700

    test: add test for problem 3159. Find Occurrences of an Element in an Array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3159/solution.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> occurrencesOfElement(
+      std::vector<int>& nums, std::vector<int>& queries, int x) {
+    (void)nums;
+    (void)x;
+    return queries;
+  }
+};
