5d3aaa8f9bc2be438a73b405e6db92718c37ecde
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-28 12:35:16 +0700

    feat(problem-2475/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2475/solution.cpp
+++ old-problems/2475/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int unequalTriplets(std::vector<int>& nums) {
-    return nums.size();
+    int count{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      for (std::size_t j{i + 1}; j < nums.size(); ++j) {
+        if (nums[i] == nums[j]) continue;
+        for (std::size_t k{j + 1}; k < nums.size(); ++k) {
+          if (nums[i] != nums[k] && nums[j] != nums[k]) ++count;
+        }
+      }
+    }
+    return count;
   }
 };
