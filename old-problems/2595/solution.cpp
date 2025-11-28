8171117d9a8653910afb76cb248e74f8f4a46d4c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-28 10:40:14 +0700

    feat(problem-2595/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2595/solution.cpp
+++ old-problems/2595/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   std::vector<int> evenOddBit(int n) {
-    return {n};
+    int evens{0}, odds{0};
+    for (int i{1}; i <= n; i <<= 1) {
+      if ((n & i) != 0) ++evens;
+      if ((i <<= 1) > n) break;
+      if ((n & i) != 0) ++odds;
+    }
+    return {evens, odds};
   }
 };
