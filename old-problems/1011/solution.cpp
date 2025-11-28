5d19ac815308f177487730488055e2b9dfd36379
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-28 10:14:34 +0700

    feat(problem-1011/cpp): solve by using a binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1011/solution.cpp
+++ old-problems/1011/solution.cpp
@@ -3,6 +3,32 @@
 class Solution {
  public:
   int shipWithinDays(std::vector<int>& weights, int days) {
-    return weights.size() + days;
+    int low{1}, high{0};
+    for (const int weight : weights) high += weight;
+
+    while (low < high) {
+      const int mid{low + (high - low) / 2};
+
+      int count{1}, sum{0};
+      for (const int weight : weights) {
+        if (sum + weight > mid) {
+          if (weight > mid || ++count > days) {
+            count = days + 1;
+            break;
+          }
+          sum = weight;
+        } else {
+          sum += weight;
+        }
+      }
+
+      if (count > days) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+
+    return low;
   }
 };
