3cbf5ca24ff84b3be912d1ea2d15eb1934df0993
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-28 12:37:38 +0700

    feat(problem-1015/cpp): solve by keeping track of modulo
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1015/solution.cpp
+++ old-problems/1015/solution.cpp
@@ -1,6 +1,20 @@
+#include <vector>
+
 class Solution {
  public:
   int smallestRepunitDivByK(int k) {
-    return k;
+    int val{1 % k}, len{1};
+
+    std::vector<bool> visited(k, false);
+    visited[val] = true;
+
+    while (val != 0) {
+      val = (val * 10 + 1) % k;
+      if (visited[val]) return -1;
+      visited[val] = true;
+      ++len;
+    }
+
+    return len;
   }
 };
