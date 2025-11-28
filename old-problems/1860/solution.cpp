ffd1ed4c59e3418a31bffa4bab032654f7030d6b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-11-28 18:49:02 +0700

    feat(problem-1860/cpp): solve by simulating the process
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1860/solution.cpp
+++ old-problems/1860/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   std::vector<int> memLeak(int memory1, int memory2) {
-    return {0, memory1, memory2};
+    int time{0};
+    while (true) {
+      ++time;
+      if (memory1 >= memory2) {
+        if (time > memory1) break;
+        memory1 -= time;
+      } else {
+        if (time > memory2) break;
+        memory2 -= time;
+      }
+    }
+    return {time, memory1, memory2};
   }
 };
