dfcd12c0d1bb76d4a7ff8a82980a100baa2e08ad
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-01 09:42:18 +0700

    feat(problem-2141/cpp): solve by using a binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2141/solution.cpp
+++ old-problems/2141/solution.cpp
@@ -1,8 +1,27 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   long long maxRunTime(int n, std::vector<int>& batteries) {
-    return n + batteries.size();
+    long long low{0}, high{0};
+    for (const int battery : batteries) high += battery;
+
+    while (low < high) {
+      const long long mid{high - (high - low) / 2};
+      long long extra{0};
+
+      for (const int battery : batteries) {
+        extra += std::min<long long>(battery, mid);
+      }
+
+      if (extra >= n * mid) {
+        low = mid;
+      } else {
+        high = mid - 1;
+      }
+    }
+
+    return low;
   }
 };
