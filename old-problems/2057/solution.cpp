1e80246e271d76f855f5cd7c91ecc2d3d0c7937c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-01 21:17:46 +0700

    feat(problem-2057/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2057/solution.cpp
+++ old-problems/2057/solution.cpp
@@ -3,6 +3,10 @@
 class Solution {
  public:
   int smallestEqual(std::vector<int>& nums) {
-    return nums.size();
+    const int n = nums.size();
+    for (int i{0}; i < n; ++i) {
+      if (i % 10 == nums[i]) return i;
+    }
+    return -1;
   }
 };
