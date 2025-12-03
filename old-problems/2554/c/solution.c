06f0c481789bc2b23a3b80d70c400e0fe7920912
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-03 09:42:58 +0700

    feat(problem-2554/c): solve by sorting banned nums and distribute smallest nums
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2554/c/solution.c
+++ old-problems/2554/c/solution.c
@@ -1,3 +1,22 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int maxCount(int* banned, int bannedSize, int n, int maxSum) {
-  return banned[bannedSize - 1] * n * maxSum;
+  qsort(banned, bannedSize, sizeof(int), compare);
+
+  int i = 0, count = 0;
+  for (int j = 1; j <= n && j <= maxSum; ++j) {
+    if (i < bannedSize && banned[i] == j) {
+      ++i;
+      while (i < bannedSize && banned[i] == j) ++i;
+    } else {
+      maxSum -= j;
+      ++count;
+    }
+  }
+
+  return count;
 }
