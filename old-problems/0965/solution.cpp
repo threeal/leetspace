f24e87bfc7a8396aa70b1e45e7aec03393a20ca4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-03 18:06:41 +0700

    feat(problem-965/cpp): solve by checking nodes recursively
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0965/solution.cpp
+++ old-problems/0965/solution.cpp
@@ -1,6 +1,16 @@
 class Solution {
  public:
   bool isUnivalTree(TreeNode* root) {
-    return root != nullptr;
+    if (root->left != nullptr &&
+        (root->val != root->left->val || !isUnivalTree(root->left))) {
+      return false;
+    }
+
+    if (root->right != nullptr &&
+        (root->val != root->right->val || !isUnivalTree(root->right))) {
+      return false;
+    }
+
+    return true;
   }
 };
