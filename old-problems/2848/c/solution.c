4093aa69db70d77601449f643824140bedfd1500
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-03 14:30:45 +0700

    feat(problem-2848/c): solve by using sorting and greedy
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2848/c/solution.c
+++ old-problems/2848/c/solution.c
@@ -1,3 +1,26 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  int* aa = *(int**)a;
+  int* bb = *(int**)b;
+  return aa[0] != bb[0] ? aa[0] - bb[0] : aa[1] - bb[1];
+}
+
 int numberOfPoints(int** nums, int numsSize, int* numsColSize) {
-  return nums[numsSize - 1][numsColSize[numsSize - 1] - 1];
+  (void)numsColSize;
+
+  qsort(nums, numsSize, sizeof(int*), compare);
+
+  int coverage = 0, min = nums[0][0], max = nums[0][1];
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i][0] > max) {
+      coverage += max - min + 1;
+      min = nums[i][0];
+      max = nums[i][1];
+    } else if (nums[i][1] > max) {
+      max = nums[i][1];
+    }
+  }
+
+  return coverage + max - min + 1;
 }
