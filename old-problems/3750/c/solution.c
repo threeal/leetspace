67e4c23b4711a2e2be3c6f194ccfcf2562336912
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-04 09:15:47 +0700

    feat(problem-3750/c): solve by iterating bits forward and backward
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3750/c/solution.c
+++ old-problems/3750/c/solution.c
@@ -1,3 +1,13 @@
 int minimumFlips(int n) {
-  return n;
+  int l = 1;
+  while ((l << 1) <= n) l <<= 1;
+
+  int flips = 0, r = 1;
+  while (l > r) {
+    if (((n & l) == 0) != ((n & r) == 0)) flips += 2;
+    l >>= 1;
+    r <<= 1;
+  }
+
+  return flips;
 }
