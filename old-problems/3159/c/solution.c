ebecbfa9d7d39c583b1beb402dcd3e4900b6752a
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-04 09:27:02 +0700

    feat(problem-3159/c): solve by keeping track of all x indices
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3159/c/solution.c
+++ old-problems/3159/c/solution.c
@@ -1,9 +1,15 @@
 int* occurrencesOfElement(
     int* nums, int numsSize, int* queries, int queriesSize, int x,
     int* returnSize) {
-  (void)queries;
-  (void)queriesSize;
-  (void)x;
-  *returnSize = numsSize;
-  return nums;
+  int n = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] == x) nums[n++] = i;
+  }
+
+  for (int i = 0; i < queriesSize; ++i) {
+    queries[i] = queries[i] <= n ? nums[queries[i] - 1] : -1;
+  }
+
+  *returnSize = queriesSize;
+  return queries;
 }
