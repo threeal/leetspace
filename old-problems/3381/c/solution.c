f1ec08c85316d03cba0ee965c4d770c6e951ea4b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-04 09:03:55 +0700

    feat(problem-3381/c): solve by using prefix sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3381/c/solution.c
+++ old-problems/3381/c/solution.c
@@ -1,3 +1,21 @@
+#include <stdlib.h>
+
 long long maxSubarraySum(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] + k;
+  long long sum = 0;
+  for (int i = 0; i < k; ++i) sum += nums[i];
+
+  long long* prefixes = malloc(numsSize * sizeof(long long));
+  for (int i = 0; i < numsSize; ++i) prefixes[i] = 0;
+
+  prefixes[k - 1] = sum;
+  long long maxSum = sum;
+
+  for (int i = k; i < numsSize; ++i) {
+    sum += nums[i] - nums[i - k];
+
+    prefixes[i] = prefixes[i - k] > 0 ? sum + prefixes[i - k] : sum;
+    if (prefixes[i] > maxSum) maxSum = prefixes[i];
+  }
+
+  return maxSum;
 }
