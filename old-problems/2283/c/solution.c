e1b5766cff83347f67df99dcb8f3772245c9a425
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-04 21:29:23 +0700

    feat(problem-2283/c): solve by counting frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2283/c/solution.c
+++ old-problems/2283/c/solution.c
@@ -1,5 +1,10 @@
 #include <stdbool.h>
 
 bool digitCount(char* num) {
-  return num == 0;
+  char freqs[10] = {0};
+  for (char* c = num; *c != 0; ++c) ++freqs[*c - '0'];
+  for (int i = 0; num[i] != 0; ++i) {
+    if (num[i] - '0' != freqs[i]) return false;
+  }
+  return true;
 }
