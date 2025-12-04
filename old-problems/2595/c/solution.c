1e25e052788fc1a5a6c949e9dfb9019d5d398c80
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-04 13:54:46 +0700

    feat(problem-2595/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2595/c/solution.c
+++ old-problems/2595/c/solution.c
@@ -1,5 +1,13 @@
+int output[2];
+
 int* evenOddBit(int n, int* returnSize) {
-  (void)n;
-  *returnSize = 0;
-  return 0;
+  output[0] = 0;
+  output[1] = 0;
+  for (int i = 1; i <= n; i <<= 1) {
+    if ((n & i) != 0) ++output[0];
+    if ((i <<= 1) > n) break;
+    if ((n & i) != 0) ++output[1];
+  }
+  *returnSize = 2;
+  return output;
 }
