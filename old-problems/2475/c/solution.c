4a4591725001e5546a8a0e3e131e3f03a8713f06
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-05 10:02:50 +0700

    feat(problem-2475/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2475/c/solution.c
+++ old-problems/2475/c/solution.c
@@ -1,3 +1,12 @@
 int unequalTriplets(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int count = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    for (int j = i + 1; j < numsSize; ++j) {
+      if (nums[i] == nums[j]) continue;
+      for (int k = j + 1; k < numsSize; ++k) {
+        if (nums[i] != nums[k] && nums[j] != nums[k]) ++count;
+      }
+    }
+  }
+  return count;
 }
