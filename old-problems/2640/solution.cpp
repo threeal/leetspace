9414cee5ce429be37a0628576ba4ccdc5f3bc733
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-05 13:53:52 +0700

    feat(problem-2640/cpp): solve by using a prefix sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2640/solution.cpp
+++ old-problems/2640/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   std::vector<long long> findPrefixScore(std::vector<int>& nums) {
-    return std::vector<long long>(nums.size(), 0);
+    std::vector<long long> score(nums.size());
+
+    int maxNum{0};
+    long long sum{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      if (nums[i] >= maxNum) maxNum = nums[i];
+      sum += nums[i] + maxNum;
+      score[i] = sum;
+    }
+
+    return score;
   }
 };
