cc4122ae74cadc31f026d77c796e41a8e002095c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-05 12:55:59 +0700

    feat(problem-821/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0821/solution.cpp
+++ old-problems/0821/solution.cpp
@@ -3,8 +3,19 @@
 class Solution {
  public:
   std::vector<int> shortestToChar(std::string s, char c) {
-    (void)s;
-    (void)c;
-    return {};
+    std::vector<int> distances(s.size(), 0);
+
+    int ci = s.size();
+    for (std::size_t i{0}; i < s.size(); ++i, ++ci) {
+      if (s[i] == c) ci = 0;
+      distances[i] = ci;
+    }
+
+    for (std::size_t i{s.size()}; i > 0; --i, ++ci) {
+      if (s[i - 1] == c) ci = 0;
+      if (ci < distances[i - 1]) distances[i - 1] = ci;
+    }
+
+    return distances;
   }
 };
