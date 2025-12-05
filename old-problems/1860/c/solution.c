16cbddd4e98ddfb6d3350d0348261b54e67be45b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-05 09:48:24 +0700

    feat(problem-1860/c): solve by simulating the process
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1860/c/solution.c
+++ old-problems/1860/c/solution.c
@@ -1,6 +1,22 @@
+int output[3];
+
 int* memLeak(int memory1, int memory2, int* returnSize) {
-  (void)memory1;
-  (void)memory2;
-  *returnSize = 0;
-  return 0;
+  int time = 0;
+  while (1) {
+    ++time;
+    if (memory1 >= memory2) {
+      if (time > memory1) break;
+      memory1 -= time;
+    } else {
+      if (time > memory2) break;
+      memory2 -= time;
+    }
+  }
+
+  output[0] = time;
+  output[1] = memory1;
+  output[2] = memory2;
+
+  *returnSize = 3;
+  return output;
 }
