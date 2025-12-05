31d000d70813d0d8af75fc892c64b16cb8214ef8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-05 20:33:11 +0700

    test(problem-2070): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2070/c/solution.c
@@ -0,0 +1,9 @@
+int* maximumBeauty(
+    int** items, int itemsSize, int* itemsColSize,
+    int* queries, int queriesSize, int* returnSize) {
+  (void)items;
+  (void)itemsSize;
+  (void)itemsColSize;
+  *returnSize = queriesSize;
+  return queries;
+}
