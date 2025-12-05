50e9a4c609dd905d09ba77d7e56248ab52f1caee
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-05 09:39:33 +0700

    feat(problem-12286/cpp): solve by keeping track of each index iterator
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1286/solution.cpp
+++ old-problems/1286/solution.cpp
@@ -1,17 +1,42 @@
 #include <string>
+#include <vector>
 
 class CombinationIterator {
+ private:
+  std::string characters;
+  std::vector<std::size_t> indices;
+  std::vector<std::size_t> maxIndices;
+
  public:
-  CombinationIterator(std::string characters, int combinationLength) {
-    (void)characters;
-    (void)combinationLength;
+  CombinationIterator(std::string characters, int combinationLength)
+      : characters{characters},
+        indices(combinationLength),
+        maxIndices(combinationLength) {
+    for (std::size_t i{0}; i < indices.size(); ++i) {
+      indices[i] = i;
+      maxIndices[i] = characters.size() - (indices.size() - i);
+    }
   }
 
   std::string next() {
-    return "";
+    std::string s(indices.size(), 0);
+    for (std::size_t i{0}; i < indices.size(); ++i) {
+      s[i] = characters[indices[i]];
+    }
+
+    std::size_t i{indices.size()};
+    while (i > 0 && ++indices[i - 1] > maxIndices[i - 1]) --i;
+    if (i > 0) {
+      while (i < indices.size()) {
+        indices[i] = indices[i - 1] + 1;
+        ++i;
+      }
+    }
+
+    return s;
   }
 
   bool hasNext() {
-    return false;
+    return indices[0] <= maxIndices[0];
   }
 };
