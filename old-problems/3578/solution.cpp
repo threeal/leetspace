3045f5683f228bcabfd5b1f16f627a9517d6f222
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-07 00:08:00 +0700

    feat(problem-3578/cpp): solve by using sliding window and DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3578/solution.cpp
+++ old-problems/3578/solution.cpp
@@ -1,8 +1,39 @@
+#include <queue>
 #include <vector>
 
 class Solution {
  public:
   int countPartitions(std::vector<int>& nums, int k) {
-    return nums.size() + k;
+    long long sum{0}, next{1};
+    std::priority_queue<std::tuple<int, std::size_t>> negMins{}, maxs{};
+    for (std::size_t l{0}, r{0}; r < nums.size(); ++r) {
+      negMins.push({-nums[r], r});
+      maxs.push({nums[r], r});
+
+      nums[r] = next;
+      sum = (sum + next) % 1000000007;
+
+      while (std::get<0>(maxs.top()) + std::get<0>(negMins.top()) > k) {
+        if (std::get<1>(maxs.top()) < std::get<1>(negMins.top())) {
+          const std::size_t ll{std::get<1>(maxs.top()) + 1};
+          for (std::size_t i{l}; i < ll; ++i) {
+            sum = (1000000007 + sum - nums[i]) % 1000000007;
+          }
+          l = ll;
+          while (std::get<1>(maxs.top()) < l) maxs.pop();
+        } else {
+          const std::size_t ll{std::get<1>(negMins.top()) + 1};
+          for (std::size_t i{l}; i < ll; ++i) {
+            sum = (1000000007 + sum - nums[i]) % 1000000007;
+          }
+          l = ll;
+          while (std::get<1>(negMins.top()) < l) negMins.pop();
+        }
+      }
+
+      next = sum;
+    }
+
+    return sum;
   }
 };
