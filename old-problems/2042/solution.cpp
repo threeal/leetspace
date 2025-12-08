e71fba1c1146123733ce75f0bf664c26a74c04bf
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-08 09:16:15 +0700

    feat(problem-2042/cpp): solve by properly parse each number
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2042/solution.cpp
+++ old-problems/2042/solution.cpp
@@ -3,6 +3,25 @@
 class Solution {
  public:
   bool areNumbersAscending(std::string s) {
-    return s.empty();
+    int prev{-1};
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      int num{0};
+      while (i < s.size() && s[i] != ' ') {
+        if (num != -1 && s[i] <= '9') {
+          num = num * 10 + s[i] - '0';
+        } else {
+          num = -1;
+        }
+        ++i;
+      }
+
+      if (num == -1) continue;
+      if (num > prev) {
+        prev = num;
+      } else {
+        return false;
+      }
+    }
+    return true;
   }
 };
