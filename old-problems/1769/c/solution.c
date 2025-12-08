32034bdee3c47674f27c3b0f20c3e377e8053dc0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-08 08:05:46 +0700

    feat(problem-1769/c): solve by using prefix-sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1769/c/solution.c
+++ old-problems/1769/c/solution.c
@@ -1,5 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+
 int* minOperations(char* boxes, int* returnSize) {
-  (void)boxes;
-  *returnSize = 0;
-  return 0;
+  const int boxesLen = strlen(boxes);
+  int* operations = malloc(boxesLen * sizeof(int));
+  memset(operations, 0, boxesLen * sizeof(int));
+
+  int n = 0, operation = 0;
+  for (int i = 0; i < boxesLen; ++i) {
+    operation += n;
+    operations[i] += operation;
+    if (boxes[i] == '1') ++n;
+  }
+
+  n = 0;
+  operation = 0;
+
+  for (int i = boxesLen - 1; i >= 0; --i) {
+    operation += n;
+    operations[i] += operation;
+    if (boxes[i] == '1') ++n;
+  }
+
+  *returnSize = boxesLen;
+  return operations;
 }
