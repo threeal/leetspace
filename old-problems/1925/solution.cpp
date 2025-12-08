b9c575584aad7e4460fe532530d5d96d9a1b9fea
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-08 08:21:32 +0700

    feat(problem-1925/cpp): solve by using a binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1925/solution.cpp
+++ old-problems/1925/solution.cpp
@@ -1,6 +1,21 @@
+#include <vector>
+
 class Solution {
  public:
   int countTriples(int n) {
-    return n;
+    std::vector<int> squares(n);
+    for (int i{0}; i < n; ++i) squares[i] = (i + 1) * (i + 1);
+
+    int count{0};
+    for (int a{1}; a <= n; ++a) {
+      const int a2{a * a};
+      for (int b{a + 1}; b <= n; ++b) {
+        const int c2{a2 + b * b};
+        const auto it = std::lower_bound(squares.begin(), squares.end(), c2);
+        if (it != squares.end() && *it == c2) ++count;
+      }
+    }
+
+    return count * 2;
   }
 };
