e9caa18cdab04fde1c44875e5a744441758c17b4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-08 09:06:29 +0700

    feat(problem-1925/cpp): solve by using two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1925/solution.cpp
+++ old-problems/1925/solution.cpp
@@ -1,21 +1,21 @@
-#include <vector>
-
 class Solution {
  public:
   int countTriples(int n) {
-    std::vector<int> squares(n);
-    for (int i{0}; i < n; ++i) squares[i] = (i + 1) * (i + 1);
-
     int count{0};
-    for (int a{1}; a <= n; ++a) {
-      const int a2{a * a};
-      for (int b{a + 1}; b <= n; ++b) {
-        const int c2{a2 + b * b};
-        const auto it = std::lower_bound(squares.begin(), squares.end(), c2);
-        if (it != squares.end() && *it == c2) ++count;
+    for (int c{5}; c <= n; ++c) {
+      const int c2{c * c};
+      int a{1}, b{c - 1};
+      while (a < b) {
+        const int a2b2 = a * a + b * b;
+        if (a2b2 == c2) {
+          ++count, ++a, --b;
+        } else if (a2b2 < c2) {
+          ++a;
+        } else {
+          --b;
+        }
       }
     }
-
     return count * 2;
   }
 };
