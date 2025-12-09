a89ed771c1e17acef2010e8c9a208bab816ddb3d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-09 20:05:42 +0700

    feat(problem-892/c): solve by checking neighboring cells
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0892/c/solution.c
+++ old-problems/0892/c/solution.c
@@ -1,3 +1,29 @@
+#include <stdlib.h>
+
 int surfaceArea(int** grid, int gridSize, int* gridColSize) {
-  return grid[gridSize - 1][gridColSize[gridSize - 1] - 1];
+  int area = 0;
+
+  if (grid[0][0] != 0) area += 2;
+  area += 2 * grid[0][0] +
+      grid[0][gridColSize[0] - 1] + grid[gridSize - 1][0];
+
+  for (int j = 1; j < gridColSize[0]; ++j) {
+    if (grid[0][j] != 0) area += 2;
+    area += grid[0][j] + grid[gridSize - 1][j] +
+        abs(grid[0][j] - grid[0][j - 1]);
+  }
+
+  for (int i = 1; i < gridSize; ++i) {
+    if (grid[i][0] != 0) area += 2;
+    area += grid[i][0] + grid[i][gridColSize[i] - 1] +
+        abs(grid[i][0] - grid[i - 1][0]);
+
+    for (int j = 1; j < gridColSize[i]; ++j) {
+      if (grid[i][j] != 0) area += 2;
+      area += abs(grid[i][j] - grid[i - 1][j]) +
+          abs(grid[i][j] - grid[i][j - 1]);
+    }
+  }
+
+  return area;
 }
