d26cf7964bd5ad25a0281f99a821eb1fe2589e85
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-09 20:48:54 +0700

    feat(problem-1217/c): solve by counting parity
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1217/c/solution.c
+++ old-problems/1217/c/solution.c
@@ -1,3 +1,7 @@
 int minCostToMoveChips(int* position, int positionSize) {
-  return position[positionSize - 1];
+  int evens = 0;
+  for (int i = 0; i < positionSize; ++i) {
+    if (position[i] % 2 == 0) ++evens;
+  }
+  return evens <= positionSize - evens ? evens : positionSize - evens;
 }
