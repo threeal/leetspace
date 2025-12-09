4dd3ff99364705e458173a2d485cde1ad9eeb063
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-09 20:42:56 +0700

    feat(problem-3402/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3402/solution.cpp
+++ old-problems/3402/solution.cpp
@@ -3,6 +3,18 @@
 class Solution {
  public:
   int minimumOperations(std::vector<std::vector<int>>& grid) {
-    return grid.size();
+    int operations{0};
+    for (std::size_t j{0}; j < grid[0].size(); ++j) {
+      int prev{grid[0][j]};
+      for (std::size_t i{1}; i < grid.size(); ++i) {
+        if (grid[i][j] > prev) {
+          prev = grid[i][j];
+        } else {
+          ++prev;
+          operations += prev - grid[i][j];
+        }
+      }
+    }
+    return operations;
   }
 };
