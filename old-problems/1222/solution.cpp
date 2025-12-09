f4b4325671920fe25aa268aaa41f5c68c545e121
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-09 09:33:01 +0700

    test: add test for problem 1222. Queens That Can Attack the King
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1222/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> queensAttacktheKing(
+      std::vector<std::vector<int>>& queens, std::vector<int>& king) {
+    (void)king;
+    return queens;
+  }
+};
