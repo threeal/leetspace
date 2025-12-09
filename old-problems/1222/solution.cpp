78b5a3324d89aab517f7b6fa5cb4de2a5825294d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-09 09:54:12 +0700

    feat(problem-1222/cpp): solve by mapping king and queens positions
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1222/solution.cpp
+++ old-problems/1222/solution.cpp
@@ -1,10 +1,73 @@
+#include <cstdlib>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::vector<int>> queensAttacktheKing(
       std::vector<std::vector<int>>& queens, std::vector<int>& king) {
-    (void)king;
+    std::vector<std::vector<bool>> board(8, std::vector<bool>(8, false));
+    for (const auto& queen : queens) {
+      board[queen[0]][queen[1]] = true;
+    }
+
+    std::size_t n{0};
+    for (int i{king[0]}, j{king[1]}; i < 8; ++i) {
+      if (board[i][j]) {
+        queens[n++] = {i, j};
+        break;
+      }
+    }
+
+    for (int i{king[0]}, j{king[1]}; i < 8 && j < 8; ++i, ++j) {
+      if (board[i][j]) {
+        queens[n++] = {i, j};
+        break;
+      }
+    }
+
+    for (int i{king[0]}, j{king[1]}; j < 8; ++j) {
+      if (board[i][j]) {
+        queens[n++] = {i, j};
+        break;
+      }
+    }
+
+    for (int i{king[0]}, j{king[1]}; i >= 0 && j < 8; --i, ++j) {
+      if (board[i][j]) {
+        queens[n++] = {i, j};
+        break;
+      }
+    }
+
+    for (int i{king[0]}, j{king[1]}; i >= 0; --i) {
+      if (board[i][j]) {
+        queens[n++] = {i, j};
+        break;
+      }
+    }
+
+    for (int i{king[0]}, j{king[1]}; i >= 0 && j >= 0; --i, --j) {
+      if (board[i][j]) {
+        queens[n++] = {i, j};
+        break;
+      }
+    }
+
+    for (int i{king[0]}, j{king[1]}; j >= 0; --j) {
+      if (board[i][j]) {
+        queens[n++] = {i, j};
+        break;
+      }
+    }
+
+    for (int i{king[0]}, j{king[1]}; i < 8 && j >= 0; ++i, --j) {
+      if (board[i][j]) {
+        queens[n++] = {i, j};
+        break;
+      }
+    }
+
+    queens.resize(n);
     return queens;
   }
 };
