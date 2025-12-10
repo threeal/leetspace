aa0ea1a4a3164a2ed46565d6c56c14c8c16c3f25
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-10 09:56:07 +0700

    feat(problem-1706/cpp): solve by process ball at each level
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1706/solution.cpp
+++ old-problems/1706/solution.cpp
@@ -1,8 +1,34 @@
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> findBall(std::vector<std::vector<int>>& grid) {
-    return grid.front();
+    const int n = grid[0].size();
+
+    std::vector<int> balls(n);
+    std::iota(balls.begin(), balls.end(), 0);
+
+    for (const std::vector<int>& level : grid) {
+      for (int& ball : balls) {
+        if (ball >= 0) {
+          if (level[ball] == 1) {
+            if (ball + 1 < n && level[ball + 1] == 1) {
+              ++ball;
+            } else {
+              ball = -1;
+            }
+          } else {
+            if (ball > 0 && level[ball - 1] != 1) {
+              --ball;
+            } else {
+              ball = -1;
+            }
+          }
+        }
+      }
+    }
+
+    return balls;
   }
 };
