5ce1b9ec0499d257d7c170657bdd69b4755fba22
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-10 11:07:58 +0700

    feat(problem-3274/cpp): solve by checking the parity of the given coordinates
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3274/solution.cpp
+++ old-problems/3274/solution.cpp
@@ -3,6 +3,7 @@
 class Solution {
  public:
   bool checkTwoChessboards(std::string coordinate1, std::string coordinate2) {
-    return coordinate1 == coordinate2;
+    return (coordinate1[0] + coordinate1[1]) % 2 ==
+        (coordinate2[0] + coordinate2[1]) % 2;
   }
 };
