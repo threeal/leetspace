21596fa3a1c9aba1997e09a94e7291bc43619f4c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-10 10:26:12 +0700

    feat(problem-3577/cpp): solve by checking minimum and returning factorial
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3577/solution.cpp
+++ old-problems/3577/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int countPermutations(std::vector<int>& complexity) {
-    return complexity.size();
+    for (std::size_t i{1}; i < complexity.size(); ++i) {
+      if (complexity[i] <= complexity[0]) return 0;
+    }
+
+    long long count{1};
+    for (long long i{2}, r = complexity.size(); i < r; ++i) {
+      count = (count * i) % 1000000007;
+    }
+
+    return count;
   }
 };
