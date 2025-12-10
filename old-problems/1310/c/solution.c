c22f99093c2870af39b0dc291f40ebf45e7c6589
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-10 09:45:47 +0700

    feat(problem-1310/c): solve by using a prefix XOR array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1310/c/solution.c
+++ old-problems/1310/c/solution.c
@@ -1,9 +1,21 @@
+#include <stdlib.h>
+
 int* xorQueries(
     int* arr, int arrSize,
     int** queries, int queriesSize, int* queriesColSize, int* returnSize) {
-  (void)queries;
-  (void)queriesSize;
   (void)queriesColSize;
-  *returnSize = arrSize;
-  return arr;
+
+  for (int i = 1; i < arrSize; ++i) arr[i] ^= arr[i - 1];
+
+  int* output = malloc(queriesSize * sizeof(int));
+  int outputSize = 0;
+
+  for (int i = 0; i < queriesSize; ++i) {
+    output[outputSize++] = queries[i][0] > 0
+        ? arr[queries[i][1]] ^ arr[queries[i][0] - 1]
+        : arr[queries[i][1]];
+  }
+
+  *returnSize = outputSize;
+  return output;
 }
