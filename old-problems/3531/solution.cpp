b7c9720dfe1ff9aeaf0454fe9a6da35a6e5a6134
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-11 11:37:54 +0700

    feat(problem-3531/cpp): solve by keeping track of coordinate ranges
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3531/solution.cpp
+++ old-problems/3531/solution.cpp
@@ -3,6 +3,24 @@
 class Solution {
  public:
   int countCoveredBuildings(int n, std::vector<std::vector<int>>& buildings) {
-    return n + buildings.size();
+    std::vector<int> xMins(n + 1, std::numeric_limits<int>::max());
+    std::vector<int> xMaxs(n + 1, std::numeric_limits<int>::min());
+    std::vector<int> yMins(n + 1, std::numeric_limits<int>::max());
+    std::vector<int> yMaxs(n + 1, std::numeric_limits<int>::min());
+
+    for (const auto& b : buildings) {
+      if (b[0] < yMins[b[1]]) yMins[b[1]] = b[0];
+      if (b[0] > yMaxs[b[1]]) yMaxs[b[1]] = b[0];
+      if (b[1] < xMins[b[0]]) xMins[b[0]] = b[1];
+      if (b[1] > xMaxs[b[0]]) xMaxs[b[0]] = b[1];
+    }
+
+    int count{0};
+    for (const auto& b : buildings) {
+      if (b[0] > yMins[b[1]] && b[0] < yMaxs[b[1]] &&
+          b[1] > xMins[b[0]] && b[1] < xMaxs[b[0]]) ++count;
+    }
+
+    return count;
   }
 };
