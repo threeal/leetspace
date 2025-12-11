4f7f94beab5d62d7812bd9f479dd35552658e1b9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-11 10:38:04 +0700

    style: fix formatting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0141/test.cpp
+++ old-problems/0141/test.cpp
@@ -3,7 +3,7 @@
 
 struct ListNode {
   int val;
-  ListNode *next;
+  ListNode* next;
   ListNode(int x) : val(x), next(NULL) {}
 };
 
--- old-problems/0719/c/solution.c
+++ old-problems/0719/c/solution.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 
-int compare(const void *a, const void *b) {
-  return *(int *)a - *(int *)b;
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
 }
 
-int smallestDistancePair(int *nums, int numsSize, int k) {
+int smallestDistancePair(int* nums, int numsSize, int k) {
   qsort(nums, numsSize, sizeof(int), compare);
 
   int low = 0;
