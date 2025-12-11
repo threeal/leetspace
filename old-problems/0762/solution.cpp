678db1546a724349dbc2f8b823bc52996720c718
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-11 11:39:46 +0700

    feat(problem-762/cpp): solve by using `std::popcount`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0762/solution.cpp
+++ old-problems/0762/solution.cpp
@@ -1,6 +1,22 @@
+#include <bit>
+
 class Solution {
  public:
   int countPrimeSetBits(int left, int right) {
-    return right + 1 - left;
+    int count{0};
+    for (int num{left}; num <= right; ++num) {
+      switch (std::popcount<unsigned int>(num)) {
+        case 2:
+        case 3:
+        case 5:
+        case 7:
+        case 11:
+        case 13:
+        case 17:
+        case 19:
+          ++count;
+      }
+    }
+    return count;
   }
 };
