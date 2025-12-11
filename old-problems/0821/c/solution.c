c630f40e5b092a1190e3e649119b50682f0cc615
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-11 11:49:25 +0700

    feat(problem-821/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0821/c/solution.c
+++ old-problems/0821/c/solution.c
@@ -1,6 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+
 int* shortestToChar(char* s, char c, int* returnSize) {
-  (void)s;
-  (void)c;
-  *returnSize = 0;
-  return 0;
+  const int sLen = strlen(s);
+  int* distances = malloc(sLen * sizeof(int));
+  for (int i = 0; i < sLen; ++i) distances[i] = 0;
+
+  int ci = sLen;
+  for (int i = 0; i < sLen; ++i, ++ci) {
+    if (s[i] == c) ci = 0;
+    distances[i] = ci;
+  }
+
+  for (int i = sLen - 1; i >= 0; --i, ++ci) {
+    if (s[i] == c) ci = 0;
+    if (ci < distances[i]) distances[i] = ci;
+  }
+
+  *returnSize = sLen;
+  return distances;
 }
