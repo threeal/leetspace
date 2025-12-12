d8cac16345465820c3b430b86ba7d1df0c80add9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-12 09:48:38 +0700

    test: add test for problem 3433. Count Mentions Per User
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3433/solution.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> countMentions(
+      int numberOfUsers, std::vector<std::vector<std::string>>& events) {
+    (void)numberOfUsers;
+    (void)events;
+    return {};
+  }
+};
