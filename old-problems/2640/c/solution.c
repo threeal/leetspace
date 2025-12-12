920c6f7fc188c2c040ce87d428b5686b07ff06d4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-12 09:28:35 +0700

    feat(problem-2640/c): solve by using a prefix sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2640/c/solution.c
+++ old-problems/2640/c/solution.c
@@ -1,6 +1,15 @@
+#include <stdlib.h>
+
 long long* findPrefixScore(int* nums, int numsSize, int* returnSize) {
-  (void)nums;
-  (void)numsSize;
-  *returnSize = 0;
-  return 0;
+  long long* score = malloc(numsSize * sizeof(long long));
+
+  long long sum = 0;
+  for (int maxNum = 0, i = 0; i < numsSize; ++i) {
+    if (nums[i] >= maxNum) maxNum = nums[i];
+    sum += nums[i] + maxNum;
+    score[i] = sum;
+  }
+
+  *returnSize = numsSize;
+  return score;
 }
