6b0ceeca4a24b0cf8675d3af2eb68c47340374de
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-12 10:53:20 +0700

    feat(problem-1111/cpp): solve by keeping track of parentheses depth
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1111/solution.cpp
+++ old-problems/1111/solution.cpp
@@ -4,6 +4,11 @@
 class Solution {
  public:
   std::vector<int> maxDepthAfterSplit(std::string seq) {
-    return std::vector<int>(seq.size());
+    int depth{0};
+    std::vector<int> output(seq.size());
+    for (std::size_t i{0}; i < seq.size(); ++i) {
+      output[i] = seq[i] == '(' ? ++depth % 2 : depth-- % 2;
+    }
+    return output;
   }
 };
