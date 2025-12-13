bbeea1e4e8991306e55daa7adaba0cd0c9e4b942
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-13 10:10:04 +0700

    test: add test for problem 3606. Coupon Code Validator
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3606/solution.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::string> validateCoupons(
+      std::vector<std::string>& code,
+      std::vector<std::string>& businessLine,
+      std::vector<bool>& isActive) {
+    (void)businessLine;
+    (void)isActive;
+    return code;
+  }
+};
