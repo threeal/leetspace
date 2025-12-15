74d08339c8ef153838369f131200afac5016d0d2
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-15 12:00:51 +0700

    feat(problem-2562/cpp): solve by using two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2562/solution.cpp
+++ old-problems/2562/solution.cpp
@@ -3,6 +3,18 @@
 class Solution {
  public:
   long long findTheArrayConcVal(std::vector<int>& nums) {
-    return nums.size();
+    long long sum{0};
+
+    std::size_t l{0}, r{nums.size() - 1};
+    while (l < r) {
+      long long exp{10}, num = nums[r];
+      while (exp <= num) exp *= 10;
+      sum += exp * nums[l] + num;
+      ++l, --r;
+    }
+
+    if (l == r) sum += nums[l];
+
+    return sum;
   }
 };
