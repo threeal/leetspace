55d735e0e857639d4d36b173b2733eb4258fbf1c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-15 09:09:39 +0700

    feat(problem-2110/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2110/solution.cpp
+++ old-problems/2110/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   long long getDescentPeriods(std::vector<int>& prices) {
-    return prices.size();
+    std::size_t sum{1};
+    for (std::size_t l{0}, r{1}; r < prices.size(); ++r) {
+      if (prices[r - 1] - prices[r] != 1) l = r;
+      sum += r - l + 1;
+    }
+    return sum;
   }
 };
