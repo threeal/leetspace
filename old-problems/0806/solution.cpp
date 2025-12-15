61c1572047ec75303c26fa33e7b4b6e64b69574e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-15 09:33:00 +0700

    feat(problem-806/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0806/solution.cpp
+++ old-problems/0806/solution.cpp
@@ -4,7 +4,14 @@
 class Solution {
  public:
   std::vector<int> numberOfLines(std::vector<int>& widths, std::string s) {
-    (void)s;
-    return widths;
+    int lines{1}, width{0};
+    for (char c : s) {
+      if (width + widths[c - 'a'] > 100) {
+        ++lines;
+        width = 0;
+      }
+      width += widths[c - 'a'];
+    }
+    return {lines, width};
   }
 };
