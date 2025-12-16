9814043425c1786a8d267604fc4317cfc38bb578
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-16 10:46:13 +0700

    DONT MERGE

--- old-problems/3562/solution.cpp
+++ old-problems/3562/solution.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <limits>
 #include <vector>
 
 class Solution {
@@ -5,6 +7,43 @@ class Solution {
   int maxProfit(
       int n, std::vector<int>& present, std::vector<int>& future,
       std::vector<std::vector<int>>& hierarchy, int budget) {
-    return n + present[0] + future[0] + hierarchy[0][0] + budget;
+    std::vector<std::vector<int>> children(n);
+    for (std::size_t i{0}; i < hierarchy.size(); ++i) {
+      children[hierarchy[i][0]].push_back(hierarchy[i][1]);
+    }
+
+    std::vector<std::vector<std::vector<int>>> caches(
+        2, std::vector<std::vector<int>>(n));
+
+    return std::max(
+        findMaxProfit(caches, children, present, future, budget, 0, 0),
+        findMaxProfit(caches, children, present, future, budget, 1, 0));
+  }
+
+ private:
+  static int findMaxProfit(
+      std::vector<std::vector<std::vector<int>>>& caches,
+      const std::vector<std::vector<int>>& children,
+      const std::vector<int>& present,
+      const std::vector<int>& future,
+      int budget,
+      int parentBought,
+      int employee) {
+    auto& cache = caches[parentBought][employee];
+    if (cache.empty()) {
+      const int cost{parentBought ? present[employee] / 2 : present[employee]};
+
+      int takeProfit{future[employee] - cost};
+      int skipProfit{0};
+
+      for (int child : children[employee]) {
+        takeProfit += findMaxProfit(cache, children, present, future, 1, child, budget - cost);
+        skipProfit += findMaxProfit(cache, children, present, future, 0, child, budget);
+      }
+
+      cache[parentBought][employee][budget] = std::max(takeProfit, skipProfit);
+    }
+
+    return *std::max_element(cache.begin(), cache.end());
   }
 };
