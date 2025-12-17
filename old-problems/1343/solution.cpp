d61179726be351bf4a00fae74acbe254c37be0b4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-17 15:44:15 +0700

    feat(problem-1343/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1343/solution.cpp
+++ old-problems/1343/solution.cpp
@@ -1,8 +1,17 @@
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   int numOfSubarrays(std::vector<int>& arr, int k, int threshold) {
-    return arr.size() + k + threshold;
+    threshold *= k;
+    int sum{std::accumulate(arr.begin(), arr.begin() + k, 0)};
+    int count{sum >= threshold ? 1 : 0};
+    for (std::size_t i = k; i < arr.size(); ++i) {
+      sum -= arr[i - k];
+      sum += arr[i];
+      if (sum >= threshold) ++count;
+    }
+    return count;
   }
 };
