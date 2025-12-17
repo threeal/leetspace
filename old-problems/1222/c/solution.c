a5553a60e3dc415a19dc37c2660e7faca6e60e73
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-17 15:21:32 +0700

    feat(problem-1222/c): solve by mapping king and queens positions
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1222/c/solution.c
+++ old-problems/1222/c/solution.c
@@ -1,9 +1,81 @@
+#include <stdint.h>
+
 int** queensAttacktheKing(
     int** queens, int queensSize, int* queensColSize, int* king, int kingSize,
     int* returnSize, int** returnColumnSizes) {
-  (void)king;
   (void)kingSize;
-  *returnSize = queensSize;
+
+  uint8_t board[8] = {0};
+  for (int i = 0; i < queensSize; ++i) {
+    board[queens[i][0]] |= 1 << queens[i][1];
+  }
+
+  int n = 0;
+  for (int i = king[0], j = king[1]; i < 8; ++i) {
+    if ((board[i] & (1 << j)) != 0) {
+      queens[n][0] = i;
+      queens[n++][1] = j;
+      break;
+    }
+  }
+
+  for (int i = king[0], j = king[1]; i < 8 && j < 8; ++i, ++j) {
+    if ((board[i] & (1 << j)) != 0) {
+      queens[n][0] = i;
+      queens[n++][1] = j;
+      break;
+    }
+  }
+
+  for (int i = king[0], j = king[1]; j < 8; ++j) {
+    if ((board[i] & (1 << j)) != 0) {
+      queens[n][0] = i;
+      queens[n++][1] = j;
+      break;
+    }
+  }
+
+  for (int i = king[0], j = king[1]; i >= 0 && j < 8; --i, ++j) {
+    if ((board[i] & (1 << j)) != 0) {
+      queens[n][0] = i;
+      queens[n++][1] = j;
+      break;
+    }
+  }
+
+  for (int i = king[0], j = king[1]; i >= 0; --i) {
+    if ((board[i] & (1 << j)) != 0) {
+      queens[n][0] = i;
+      queens[n++][1] = j;
+      break;
+    }
+  }
+
+  for (int i = king[0], j = king[1]; i >= 0 && j >= 0; --i, --j) {
+    if ((board[i] & (1 << j)) != 0) {
+      queens[n][0] = i;
+      queens[n++][1] = j;
+      break;
+    }
+  }
+
+  for (int i = king[0], j = king[1]; j >= 0; --j) {
+    if ((board[i] & (1 << j)) != 0) {
+      queens[n][0] = i;
+      queens[n++][1] = j;
+      break;
+    }
+  }
+
+  for (int i = king[0], j = king[1]; i < 8 && j >= 0; ++i, --j) {
+    if ((board[i] & (1 << j)) != 0) {
+      queens[n][0] = i;
+      queens[n++][1] = j;
+      break;
+    }
+  }
+
+  *returnSize = n;
   *returnColumnSizes = queensColSize;
   return queens;
 }
