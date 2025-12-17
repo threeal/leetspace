35eb1216f2717595b7b7d6107f80670ed8dbbdcb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-17 21:27:04 +0700

    feat(problem-841/c): solve by using BFS
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0841/c/solution.c
+++ old-problems/0841/c/solution.c
@@ -1,8 +1,32 @@
 #include <stdbool.h>
+#include <stdlib.h>
 
 bool canVisitAllRooms(int** rooms, int roomsSize, int* roomsColSize) {
-  (void)rooms;
-  (void)roomsSize;
-  (void)roomsColSize;
-  return false;
+  int* queue = malloc(roomsSize * sizeof(int));
+  int queueLeft = 0, queueRight = 0;
+
+  queue[queueRight++] = 0;
+
+  bool* visited = malloc(roomsSize * sizeof(bool));
+  for (int i = 0; i < roomsSize; ++i) visited[i] = false;
+
+  visited[0] = true;
+
+  int unvisited = roomsSize - 1;
+  while (queueLeft < queueRight) {
+    const int room = queue[queueLeft++];
+    for (int i = 0; i < roomsColSize[room]; ++i) {
+      const int key = rooms[room][i];
+      if (!visited[key]) {
+        queue[queueRight++] = key;
+        visited[key] = true;
+        --unvisited;
+      }
+    }
+  }
+
+  free(queue);
+  free(visited);
+
+  return unvisited == 0;
 }
