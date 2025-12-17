476b79add8c0a26d2aaa0988415823217eb704cf
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-17 15:53:46 +0700

    feat(problem-2399/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2399/solution.cpp
+++ old-problems/2399/solution.cpp
@@ -4,6 +4,13 @@
 class Solution {
  public:
   bool checkDistances(std::string s, std::vector<int>& distance) {
-    return s.empty() && distance.empty();
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      if (distance[s[i] - 'a'] != -1) {
+        const std::size_t j{i + distance[s[i] - 'a'] + 1};
+        if (j >= s.size() || s[i] != s[j]) return false;
+        distance[s[i] - 'a'] = -1;
+      }
+    }
+    return true;
   }
 };
