3704198f781655be9273d533901404c96d6400fc
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-17 14:58:37 +0700

    feat(problem-2062/c): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2062/c/solution.c
+++ old-problems/2062/c/solution.c
@@ -1,3 +1,31 @@
 int countVowelSubstrings(char* word) {
-  return *word;
+  int total = 0;
+  char n = 0, freqs[26] = {0};
+  for (int ll = 0, l = 0, r = 0; word[r] != 0; ++r) {
+    switch (word[r]) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+        if (++freqs[word[r] - 'a'] == 1) ++n;
+        if (n == 5) {
+          while (freqs[word[l] - 'a'] > 1) {
+            --freqs[word[l] - 'a'];
+            ++l;
+          }
+          total += l + 1 - ll;
+        }
+        break;
+
+      default:
+        n = 0;
+        while (l <= r) {
+          --freqs[word[l] - 'a'];
+          ++l;
+        }
+        ll = l;
+    }
+  }
+  return total;
 }
