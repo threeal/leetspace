d4b984389f092cc5c029e72e76158c0a92ab9a8b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-18 16:56:58 +0700

    test: add test for problem 538. Convert BST to Greater Tree
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0538/solution.cpp
@@ -0,0 +1,6 @@
+class Solution {
+ public:
+  TreeNode* convertBST(TreeNode* root) {
+    return root;
+  }
+};
---
+++ old-problems/0538/test.cpp
@@ -0,0 +1,87 @@
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+// clang-format off
+#include "solution.cpp"
+// clang-format on
+
+#include <catch2/catch_test_macros.hpp>
+#include <optional>
+#include <queue>
+#include <vector>
+
+const auto null = std::nullopt;
+
+TreeNode* toTreeNode(const std::vector<std::optional<int>>& list) {
+  if (list.empty()) return nullptr;
+
+  auto root = new TreeNode(*list[0]);
+
+  std::queue<TreeNode**> children{};
+  children.push(&(root->left));
+  children.push(&(root->right));
+
+  for (std::size_t i{1}; i < list.size(); ++i) {
+    if (list[i]) {
+      auto node = new TreeNode(*list[i]);
+      *children.front() = node;
+      children.push(&(node->left));
+      children.push(&(node->right));
+    }
+    children.pop();
+  }
+
+  return root;
+}
+
+std::vector<std::optional<int>> fromTreeNode(TreeNode* root) {
+  if (root == nullptr) return {};
+
+  std::vector<std::optional<int>> list{};
+
+  std::queue<TreeNode*> nodes{};
+  nodes.push(root);
+
+  while (!nodes.empty()) {
+    if (nodes.front() == nullptr) {
+      list.push_back(null);
+    } else {
+      list.push_back(nodes.front()->val);
+      nodes.push(nodes.front()->left);
+      nodes.push(nodes.front()->right);
+    }
+    nodes.pop();
+  }
+
+  while (!list.back()) list.pop_back();
+  return list;
+}
+
+TEST_CASE("538. Convert BST to Greater Tree") {
+  SECTION("Example 1") {
+    TreeNode* root{
+        toTreeNode(
+            {4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8})};
+
+    TreeNode* actual{Solution{}.convertBST(root)};
+    std::vector<std::optional<int>> expected{
+        30, 36, 21, 36, 35, 26, 15, null, null, null, 33, null, null, null, 8};
+
+    REQUIRE(fromTreeNode(actual) == expected);
+  }
+
+  SECTION("Example 2") {
+    TreeNode* root{toTreeNode({0, null, 1})};
+    TreeNode* actual{Solution{}.convertBST(root)};
+    std::vector<std::optional<int>> expected{1, null, 1};
+    REQUIRE(fromTreeNode(actual) == expected);
+  }
+}
