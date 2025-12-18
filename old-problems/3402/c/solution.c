b2718be65dfc32cb542468dd75b8caee2938ff8d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-18 10:58:30 +0700

    feat(problem-3402/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3402/c/solution.c
+++ old-problems/3402/c/solution.c
@@ -1,3 +1,15 @@
 int minimumOperations(int** grid, int gridSize, int* gridColSize) {
-  return grid[gridSize - 1][gridColSize[gridSize - 1] - 1];
+  int operations = 0;
+  for (int j = 0; j < gridColSize[0]; ++j) {
+    int prev = grid[0][j];
+    for (int i = 1; i < gridSize; ++i) {
+      if (grid[i][j] > prev) {
+        prev = grid[i][j];
+      } else {
+        ++prev;
+        operations += prev - grid[i][j];
+      }
+    }
+  }
+  return operations;
 }
