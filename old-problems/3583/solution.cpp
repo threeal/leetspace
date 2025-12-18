e247b1e03002c816022e81cbb13fd2dc3ff28076
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-18 10:51:32 +0700

    feat(problem-3583/cpp): solve by counting frequencies to the left and right
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3583/solution.cpp
+++ old-problems/3583/solution.cpp
@@ -1,8 +1,28 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int specialTriplets(std::vector<int>& nums) {
-    return nums.size();
+    std::unordered_map<int, long long> rFreqs{};
+    for (const int num : nums) ++rFreqs[num];
+
+    long long triplets{0};
+    std::unordered_map<int, long long> lFreqs{};
+    for (const int num : nums) {
+      --rFreqs[num];
+
+      const auto lIt = lFreqs.find(2 * num);
+      if (lIt != lFreqs.end()) {
+        const auto rIt = rFreqs.find(2 * num);
+        if (rIt != rFreqs.end()) {
+          triplets = (triplets + (lIt->second * rIt->second)) % 1000000007;
+        }
+      }
+
+      ++lFreqs[num];
+    }
+
+    return triplets;
   }
 };
