e7194a80b6a0412562d9c741f0225c2dfadd4a00
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-18 17:12:08 +0700

    feat(problem-538/cpp): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0538/solution.cpp
+++ old-problems/0538/solution.cpp
@@ -1,6 +1,23 @@
 class Solution {
  public:
   TreeNode* convertBST(TreeNode* root) {
+    if (root != nullptr) explore(root, 0);
     return root;
+  }
+
+ private:
+  static int explore(TreeNode* node, int sum) {
+    if (node->right != nullptr) {
+      sum = explore(node->right, sum);
+    }
+
+    sum += node->val;
+    node->val = sum;
+
+    if (node->left != nullptr) {
+      sum = explore(node->left, sum);
+    }
+
+    return sum;
   }
 };
