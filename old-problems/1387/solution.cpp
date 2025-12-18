8b706de5b9abbc7113f74d6ae23fd896e18549d1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-18 12:08:57 +0700

    feat(problem-1387/cpp): solve by using partial sort
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1387/solution.cpp
+++ old-problems/1387/solution.cpp
@@ -1,6 +1,21 @@
+#include <algorithm>
+#include <vector>
+
 class Solution {
  public:
   int getKth(int lo, int hi, int k) {
-    return lo + hi + k;
+    std::vector<std::tuple<int, int>> nums(hi + 1 - lo);
+    for (int num{lo}; num <= hi; ++num) {
+      int steps{0}, val{num};
+      while (val != 1) {
+        val = val % 2 == 0 ? val / 2 : val * 3 + 1;
+        ++steps;
+      }
+      nums[num - lo] = {steps, num};
+    }
+
+    std::partial_sort(nums.begin(), nums.begin() + k, nums.end());
+
+    return std::get<1>(nums[k - 1]);
   }
 };
