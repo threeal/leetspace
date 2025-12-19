1fb2db999290f1d224de253858ed73b6a57abf78
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-19 09:42:22 +0700

    feat(problem-732/cpp): solve by using a binary search tree map
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0732/solution.cpp
+++ old-problems/0732/solution.cpp
@@ -1,8 +1,29 @@
+#include <map>
+
 class MyCalendarThree {
+ private:
+  std::map<int, int> tree;
+  int maxCount;
+
  public:
-  MyCalendarThree() {}
+  MyCalendarThree() : tree{{-1, 0}}, maxCount{0} {}
 
   int book(int startTime, int endTime) {
-    return startTime + endTime;
+    auto startIt = --tree.upper_bound(startTime);
+    if (startIt->first != startTime) {
+      startIt = tree.emplace(startTime, startIt->second).first;
+    }
+
+    auto endIt = --tree.upper_bound(endTime);
+    if (endIt->first != endTime) {
+      endIt = tree.emplace(endTime, endIt->second).first;
+    }
+
+    for (auto it = startIt; it != endIt; ++it) {
+      ++it->second;
+      if (it->second > maxCount) maxCount = it->second;
+    }
+
+    return maxCount;
   }
 };
