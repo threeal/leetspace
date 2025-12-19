98ed881ff0de8020a7ca4b25fc7220bf6fc926d7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-19 10:06:13 +0700

    feat(problem-1706/c): solve by process ball at each level
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1706/c/solution.c
+++ old-problems/1706/c/solution.c
@@ -1,4 +1,30 @@
+#include <stdlib.h>
+
 int* findBall(int** grid, int gridSize, int* gridColSize, int* returnSize) {
-  *returnSize = gridColSize[gridSize - 1];
-  return grid[gridSize - 1];
+  const int ballsSize = gridColSize[0];
+  int* balls = malloc(ballsSize * sizeof(int));
+  for (int i = 0; i < ballsSize; ++i) balls[i] = i;
+
+  for (int i = 0; i < gridSize; ++i) {
+    for (int j = 0; j < ballsSize; ++j) {
+      if (balls[j] >= 0) {
+        if (grid[i][balls[j]] == 1) {
+          if (balls[j] + 1 < ballsSize && grid[i][balls[j] + 1] == 1) {
+            ++balls[j];
+          } else {
+            balls[j] = -1;
+          }
+        } else {
+          if (balls[j] > 0 && grid[i][balls[j] - 1] != 1) {
+            --balls[j];
+          } else {
+            balls[j] = -1;
+          }
+        }
+      }
+    }
+  }
+
+  *returnSize = ballsSize;
+  return balls;
 }
