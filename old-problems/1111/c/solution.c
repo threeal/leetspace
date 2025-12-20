0197dec509c7d897101c0b05c2fa2dab1b49af6e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-20 14:27:21 +0700

    feat(problem-1111/c): solve by keeping track of parentheses depth
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1111/c/solution.c
+++ old-problems/1111/c/solution.c
@@ -1,5 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+
 int* maxDepthAfterSplit(char* seq, int* returnSize) {
-  (void)seq;
-  *returnSize = 0;
-  return 0;
+  int depth = 0;
+  const int seqLen = strlen(seq);
+  int* output = malloc(seqLen * sizeof(int));
+  for (int i = 0; i < seqLen; ++i) {
+    output[i] = seq[i] == '(' ? ++depth % 2 : depth-- % 2;
+  }
+  *returnSize = seqLen;
+  return output;
 }
