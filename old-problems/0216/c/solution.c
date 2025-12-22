608396ba5aeeb1c3f929df50047ddd8620c1367e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-22 10:03:59 +0700

    feat(problem-216/c): solve by using a backtracking
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0216/c/solution.c
+++ old-problems/0216/c/solution.c
@@ -1,7 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void find(
+    int* maxOutputSize, int*** output, int** outputColSizes, int* outputSize,
+    int* nums, int* numsSize, int k, int n);
+
 int** combinationSum3(int k, int n, int* returnSize, int** returnColumnSizes) {
-  (void)k;
-  (void)n;
-  *returnSize = 0;
-  *returnColumnSizes = 0;
-  return 0;
+  int maxOutputSize = 32;
+  int** output = malloc(maxOutputSize * sizeof(int*));
+  int* outputColSizes = malloc(maxOutputSize * sizeof(int));
+  int outputSize = 0;
+
+  int* nums = malloc(9 * sizeof(int));
+  int numsSize = 0;
+
+  const int max = n < 9 ? n : 9;
+  for (int num = 1; num <= max; ++num) {
+    nums[numsSize++] = num;
+    find(&maxOutputSize, &output, &outputColSizes, &outputSize,
+         nums, &numsSize, k - 1, n - num);
+
+    --numsSize;
+  }
+
+  free(nums);
+
+  *returnSize = outputSize;
+  *returnColumnSizes = outputColSizes;
+  return output;
+}
+
+static void find(
+    int* maxOutputSize, int*** output, int** outputColSizes, int* outputSize,
+    int* nums, int* numsSize, int k, int n) {
+  if (k == 1) {
+    if (n <= 9 && n > nums[*numsSize - 1]) {
+      nums[(*numsSize)++] = n;
+
+      if (*outputSize >= *maxOutputSize) {
+        *maxOutputSize *= 4;
+        *output = realloc(*output, *maxOutputSize * sizeof(int*));
+        *outputColSizes = realloc(*outputColSizes, *maxOutputSize * sizeof(int));
+      }
+
+      (*output)[*outputSize] = malloc(*numsSize * sizeof(int));
+      memcpy((*output)[*outputSize], nums, *numsSize * sizeof(int));
+      (*outputColSizes)[(*outputSize)++] = *numsSize;
+
+      --(*numsSize);
+    }
+  } else {
+    const int max = n < 9 ? n : 9;
+    for (int num = nums[*numsSize - 1] + 1; num <= max; ++num) {
+      nums[(*numsSize)++] = num;
+      find(maxOutputSize, output, outputColSizes, outputSize,
+           nums, numsSize, k - 1, n - num);
+      --(*numsSize);
+    }
+  }
 }
