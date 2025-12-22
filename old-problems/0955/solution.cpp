4efd2f242ed633016d3ab2bd0c7bb8b82f405b04
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-22 08:48:35 +0700

    feat(problem-955/cpp): solve by greedily check each column

--- old-problems/0955/solution.cpp
+++ old-problems/0955/solution.cpp
@@ -4,6 +4,27 @@
 class Solution {
  public:
   int minDeletionSize(std::vector<std::string>& strs) {
-    return strs.size();
+    int deletions{0};
+    std::vector<bool> isValids(strs[0].size(), false);
+    std::size_t validLefts{strs.size() - 1};
+
+    for (std::size_t i{0}; i < strs[0].size() && validLefts > 0; ++i) {
+      std::size_t j{1};
+      while (j < strs.size() &&
+             (isValids[j] || (strs[j - 1][i] <= strs[j][i]))) ++j;
+
+      if (j == strs.size()) {
+        for (std::size_t j{1}; j < strs.size(); ++j) {
+          if (!isValids[j] && strs[j - 1][i] < strs[j][i]) {
+            isValids[j] = true;
+            --validLefts;
+          }
+        }
+      } else {
+        ++deletions;
+      }
+    }
+
+    return deletions;
   }
 };
