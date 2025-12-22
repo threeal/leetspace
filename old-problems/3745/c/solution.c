a654b0fec705b5b712fcd17393ea17c86b9683b3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-22 08:57:05 +0700

    feat(problem-3745/c): solve by keeping two largest numbers and a smallest number
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3745/c/solution.c
+++ old-problems/3745/c/solution.c
@@ -1,3 +1,25 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int maximizeExpressionOfThree(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  qsort(nums, 3, sizeof(int), compare);
+
+  int c = nums[0], b = nums[1], a = nums[2];
+  for (int i = 3; i < numsSize; ++i) {
+    if (nums[i] > b) {
+      if (nums[i] > a) {
+        b = a;
+        a = nums[i];
+      } else {
+        b = nums[i];
+      }
+    } else if (nums[i] < c) {
+      c = nums[i];
+    }
+  }
+
+  return a + b - c;
 }
