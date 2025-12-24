0e759aa77ec2d72d64ae100cf06b37a7bf35266f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-24 20:07:29 +0700

    feat(problem-2042/c): solve by properly parse each number
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2042/c/solution.c
+++ old-problems/2042/c/solution.c
@@ -1,5 +1,26 @@
 #include <stdbool.h>
+#include <string.h>
 
 bool areNumbersAscending(char* s) {
-  return s == 0;
+  int prev = -1;
+  const int sLen = strlen(s);
+  for (int i = 0; i < sLen; ++i) {
+    int num = 0;
+    while (i < sLen && s[i] != ' ') {
+      if (num != -1 && s[i] <= '9') {
+        num = num * 10 + s[i] - '0';
+      } else {
+        num = -1;
+      }
+      ++i;
+    }
+
+    if (num == -1) continue;
+    if (num > prev) {
+      prev = num;
+    } else {
+      return false;
+    }
+  }
+  return true;
 }
