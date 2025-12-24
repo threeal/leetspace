6bf2f2a6a3d1aaf42ebc78534b9bdd6b4e91571b
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-24 19:53:58 +0700

    feat(problem-3074/cpp): solve by using greedy approach after sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3074/solution.cpp
+++ old-problems/3074/solution.cpp
@@ -1,8 +1,19 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   int minimumBoxes(std::vector<int>& apple, std::vector<int>& capacity) {
-    return apple.size() + capacity.size();
+    int sum{std::accumulate(apple.begin(), apple.end(), 0)};
+    std::sort(capacity.begin(), capacity.end(), std::greater<int>());
+
+    std::size_t i{0};
+    while (sum > 0) {
+      sum -= capacity[i];
+      ++i;
+    }
+
+    return i;
   }
 };
