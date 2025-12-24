6d149b71180ffbc5d195e3adbe0750570b0d3297
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-24 20:04:10 +0700

    feat(problem-2110/c): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2110/c/solution.c
+++ old-problems/2110/c/solution.c
@@ -1,3 +1,8 @@
 long long getDescentPeriods(int* prices, int pricesSize) {
-  return prices[pricesSize - 1];
+  long long sum = 1;
+  for (int l = 0, r = 1; r < pricesSize; ++r) {
+    if (prices[r - 1] - prices[r] != 1) l = r;
+    sum += r - l + 1;
+  }
+  return sum;
 }
