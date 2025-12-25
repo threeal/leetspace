9a05ba7fe436d8e5e90ea4036c16973500d9a016
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-25 08:33:15 +0700

    feat(problem-1200/cpp): solve by keeping track of min diff of pairs
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1200/solution.cpp
+++ old-problems/1200/solution.cpp
@@ -1,8 +1,24 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::vector<int>> minimumAbsDifference(std::vector<int>& arr) {
-    return {arr};
+    std::sort(arr.begin(), arr.end());
+
+    int minDiff{arr[1] - arr[0]};
+    std::vector<std::vector<int>> output{{arr[0], arr[1]}};
+
+    for (std::size_t i{2}; i < arr.size(); ++i) {
+      const int diff{arr[i] - arr[i - 1]};
+      if (diff > minDiff) continue;
+      if (diff < minDiff) {
+        minDiff = diff;
+        output.clear();
+      }
+      output.push_back({arr[i - 1], arr[i]});
+    }
+
+    return output;
   }
 };
