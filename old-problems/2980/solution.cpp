e0514462fe2a2170b65147d36e2f5e56ef084497
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-25 21:22:27 +0700

    feat(problem-2980/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2980/solution.cpp
+++ old-problems/2980/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   bool hasTrailingZeros(std::vector<int>& nums) {
-    return nums.empty();
+    int evens{0};
+    for (int num : nums) {
+      if (num % 2 == 0) {
+        if (++evens == 2) return true;
+      }
+    }
+    return false;
   }
 };
