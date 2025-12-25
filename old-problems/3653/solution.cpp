24ecd41ccf2820c6f2fcc3644e364b25c8f2457f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-25 13:44:38 +0700

    feat(problem-3653/cpp): solve by processing queries
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3653/solution.cpp
+++ old-problems/3653/solution.cpp
@@ -4,6 +4,14 @@ class Solution {
  public:
   int xorAfterQueries(
       std::vector<int>& nums, std::vector<std::vector<int>>& queries) {
-    return nums.size() + queries.size();
+    for (const auto& query : queries) {
+      for (int i{query[0]}; i <= query[1]; i += query[2]) {
+        nums[i] = (static_cast<long long>(nums[i]) * query[3]) % 1000000007;
+      }
+    }
+
+    int ans{0};
+    for (int num : nums) ans ^= num;
+    return ans;
   }
 };
