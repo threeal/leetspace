6db4ef35eec0ce4be2a45a639692c5391a073298
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-25 08:14:30 +0700

    feat(problem-999/cpp): solve by checking cells in each direction
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0999/solution.cpp
+++ old-problems/0999/solution.cpp
@@ -3,6 +3,64 @@
 class Solution {
  public:
   int numRookCaptures(std::vector<std::vector<char>>& board) {
-    return board.front().size();
+    std::size_t ry{0}, rx{0};
+    while (ry < board.size()) {
+      rx = 0;
+      while (rx < board[ry].size() && board[ry][rx] != 'R') ++rx;
+
+      if (rx < board[ry].size()) {
+        break;
+      } else {
+        ++ry;
+      }
+    }
+
+    int captures{0};
+
+    for (std::size_t y{ry + 1}; y < board.size(); ++y) {
+      switch (board[y][rx]) {
+        case 'p':
+          ++captures;
+
+        case 'B':
+          y = board.size();
+          break;
+      }
+    }
+
+    for (std::size_t y{ry}; y > 0; --y) {
+      switch (board[y - 1][rx]) {
+        case 'p':
+          ++captures;
+
+        case 'B':
+          y = 1;
+          break;
+      }
+    }
+
+    for (std::size_t x{rx + 1}; x < board[ry].size(); ++x) {
+      switch (board[ry][x]) {
+        case 'p':
+          ++captures;
+
+        case 'B':
+          x = board[ry].size();
+          break;
+      }
+    }
+
+    for (std::size_t x{rx}; x > 0; --x) {
+      switch (board[ry][x - 1]) {
+        case 'p':
+          ++captures;
+
+        case 'B':
+          x = 1;
+          break;
+      }
+    }
+
+    return captures;
   }
 };
