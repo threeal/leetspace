08a603859e1504c0e281e1e0b2e3b63888df3544
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-25 18:47:32 +0700

    feat(problem-806/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0806/c/solution.c
+++ old-problems/0806/c/solution.c
@@ -1,5 +1,20 @@
+static int output[2];
+
 int* numberOfLines(int* widths, int widthsSize, char* s, int* returnSize) {
-  (void)s;
-  *returnSize = widthsSize;
-  return widths;
+  (void)widthsSize;
+
+  int lines = 1, width = 0;
+  for (char* c = s; *c != 0; ++c) {
+    if (width + widths[*c - 'a'] > 100) {
+      ++lines;
+      width = 0;
+    }
+    width += widths[*c - 'a'];
+  }
+
+  output[0] = lines;
+  output[1] = width;
+
+  *returnSize = 2;
+  return output;
 }
