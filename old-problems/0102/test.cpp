a2b132a161dafa652b1351eee407424c5cd34b45
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-25 15:35:18 +0800

    feat: solve problem 102. Binary Tree Level Order Traversal (#4737)
    * test: add test for problem 102. Binary Tree Level Order Traversal
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(problem-102/cpp): solve by using BFS
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0102/test.cpp
@@ -0,0 +1,63 @@
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+// clang-format off
+#include "solution.cpp"
+// clang-format on
+
+#include <catch2/catch_test_macros.hpp>
+#include <optional>
+#include <queue>
+#include <vector>
+
+const auto null = std::nullopt;
+
+TreeNode* toTreeNode(const std::vector<std::optional<int>>& list) {
+  if (list.empty()) return nullptr;
+
+  auto root = new TreeNode(*list[0]);
+
+  std::queue<TreeNode**> children{};
+  children.push(&(root->left));
+  children.push(&(root->right));
+
+  for (std::size_t i{1}; i < list.size(); ++i) {
+    if (list[i]) {
+      auto node = new TreeNode(*list[i]);
+      *children.front() = node;
+      children.push(&(node->left));
+      children.push(&(node->right));
+    }
+    children.pop();
+  }
+
+  return root;
+}
+
+TEST_CASE("102. Binary Tree Level Order Traversal") {
+  SECTION("Example 1") {
+    TreeNode* root{toTreeNode({3, 9, 20, null, null, 15, 7})};
+    std::vector<std::vector<int>> expected{{3}, {9, 20}, {15, 7}};
+    REQUIRE(Solution{}.levelOrder(root) == expected);
+  }
+
+  SECTION("Example 2") {
+    TreeNode* root{toTreeNode({1})};
+    std::vector<std::vector<int>> expected{{1}};
+    REQUIRE(Solution{}.levelOrder(root) == expected);
+  }
+
+  SECTION("Example 3") {
+    TreeNode* root{toTreeNode({})};
+    std::vector<std::vector<int>> expected{};
+    REQUIRE(Solution{}.levelOrder(root) == expected);
+  }
+}
