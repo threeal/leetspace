0ddd026e47b3aca75e5c23b2c299d160dc66f295
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-26 10:02:01 +0700

    feat(problem-2309/c): solve by using bit manipulation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2309/c/solution.c
+++ old-problems/2309/c/solution.c
@@ -1,3 +1,20 @@
+#include <stdint.h>
+
 char* greatestLetter(char* s) {
+  uint32_t upper = 0, lower = 0;
+  for (char* c = s; *c != 0; ++c) {
+    if (*c >= 'a') {
+      lower |= 1 << (*c - 'a');
+    } else {
+      upper |= 1 << (*c - 'A');
+    }
+  }
+
+  upper &= lower;
+  if (upper == 0) return "";
+
+  s[0] = 'A' + 31 - __builtin_clz(upper);
+  s[1] = 0;
+
   return s;
 }
