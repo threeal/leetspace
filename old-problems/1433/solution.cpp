fd141749e9c61f9f88db7392a41c847c6796d407
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-26 09:08:16 +0700

    feat(problem-1433/cpp): solve by counting character frequencies and compare each
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1433/solution.cpp
+++ old-problems/1433/solution.cpp
@@ -3,6 +3,25 @@
 class Solution {
  public:
   bool checkIfCanBreak(std::string s1, std::string s2) {
-    return s1 == s2;
+    int freqs1[26]{0}, freqs2[26]{0};
+    for (std::size_t i{0}; i < s1.size(); ++i) {
+      ++freqs1[s1[i] - 'a'];
+      ++freqs2[s2[i] - 'a'];
+    }
+
+    int diff{0};
+    for (int i{0}; i < 26; ++i) {
+      diff += freqs1[i] - freqs2[i];
+      if (diff < 0) {
+        diff = 0;
+        for (int j{0}; j < 26; ++j) {
+          diff += freqs2[j] - freqs1[j];
+          if (diff < 0) return false;
+        }
+        return true;
+      }
+    }
+
+    return true;
   }
 };
