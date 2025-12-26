3fe3797692b6d97aae94f1ab014f93c2e9f41949
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-26 10:55:07 +0700

    feat(problem-2353/cpp): solve by storing equal row frequencies in a map
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2352/solution.cpp
+++ old-problems/2352/solution.cpp
@@ -1,8 +1,23 @@
+#include <map>
 #include <vector>
 
 class Solution {
  public:
   int equalPairs(std::vector<std::vector<int>>& grid) {
-    return grid.size();
+    std::map<std::vector<int>, int> freqs{};
+    for (const auto& row : grid) ++freqs[row];
+
+    int count{0};
+    std::vector<int> column(grid.size());
+    for (std::size_t i{0}; i < grid.size(); ++i) {
+      for (std::size_t j{0}; j < grid.size(); ++j) {
+        column[j] = grid[j][i];
+      }
+
+      const auto it = freqs.find(column);
+      if (it != freqs.end()) count += it->second;
+    }
+
+    return count;
   }
 };
