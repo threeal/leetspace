819fac09b336630c12f00c6396b85a25383a1a9d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-26 19:45:52 +0700

    feat(problem-3583/c): solve by counting frequencies to the left and right
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3583/c/solution.c
+++ old-problems/3583/c/solution.c
@@ -1,3 +1,17 @@
 int specialTriplets(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  long long rFreqs[200001] = {0};
+  for (int i = 0; i < numsSize; ++i) ++rFreqs[nums[i]];
+
+  long long triplets = 0;
+  long long lFreqs[200001] = {0};
+  for (int i = 0; i < numsSize; ++i) {
+    --rFreqs[nums[i]];
+    const int num2 = nums[i] * 2;
+    if (lFreqs[num2] != 0 && rFreqs[num2] != 0) {
+      triplets = (triplets + (lFreqs[num2] * rFreqs[num2])) % 1000000007;
+    }
+    ++lFreqs[nums[i]];
+  }
+
+  return triplets;
 }
