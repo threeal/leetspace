d1b4c38fdcdd0e38152d477081b505c25602f36d
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-26 19:53:23 +0700

    feat(problem-2562/c): solve by using two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2562/c/solution.c
+++ old-problems/2562/c/solution.c
@@ -1,3 +1,15 @@
 long long findTheArrayConcVal(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  long long sum = 0;
+
+  int l = 0, r = numsSize - 1;
+  while (l < r) {
+    long long exp = 10, num = nums[r];
+    while (exp <= num) exp *= 10;
+    sum += exp * nums[l] + num;
+    ++l, --r;
+  }
+
+  if (l == r) sum += nums[l];
+
+  return sum;
 }
