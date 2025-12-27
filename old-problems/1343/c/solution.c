d5ab11b9e6f28336c1ab44a15b4937e24424ef65
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-27 20:42:59 +0700

    feat(problem-1343/c): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1343/c/solution.c
+++ old-problems/1343/c/solution.c
@@ -1,3 +1,15 @@
 int numOfSubarrays(int* arr, int arrSize, int k, int threshold) {
-  return arr[arrSize - 1] + k + threshold;
+  threshold *= k;
+
+  int sum = 0;
+  for (int i = 0; i < k; ++i) sum += arr[i];
+
+  int count = sum >= threshold ? 1 : 0;
+  for (int i = k; i < arrSize; ++i) {
+    sum -= arr[i - k];
+    sum += arr[i];
+    if (sum >= threshold) ++count;
+  }
+
+  return count;
 }
