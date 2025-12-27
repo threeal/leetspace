21a2856ad11cf7fb92fc969845db9fa5393e6521
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-27 16:07:43 +0700

    feat(problem-3577/c): solve by checking minimum and returning factorial
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3577/c/solution.c
+++ old-problems/3577/c/solution.c
@@ -1,3 +1,12 @@
 int countPermutations(int* complexity, int complexitySize) {
-  return complexity[complexitySize - 1];
+  for (int i = 1; i < complexitySize; ++i) {
+    if (complexity[i] <= complexity[0]) return 0;
+  }
+
+  long long count = 1;
+  for (long long i = 2, r = complexitySize; i < r; ++i) {
+    count = (count * i) % 1000000007;
+  }
+
+  return count;
 }
