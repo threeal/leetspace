3472e6b832992c312a1388c12590d22221655a81
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-29 09:58:25 +0700

    feat(problem-2399/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2399/c/solution.c
+++ old-problems/2399/c/solution.c
@@ -1,5 +1,15 @@
 #include <stdbool.h>
+#include <string.h>
 
 bool checkDistances(char* s, int* distance, int distanceSize) {
-  return *s == distance[distanceSize - 1];
+  (void)distanceSize;
+  const int sLen = strlen(s);
+  for (int i = 0; i < sLen; ++i) {
+    if (distance[s[i] - 'a'] != -1) {
+      const int j = i + distance[s[i] - 'a'] + 1;
+      if (j >= sLen || s[i] != s[j]) return false;
+      distance[s[i] - 'a'] = -1;
+    }
+  }
+  return true;
 }
