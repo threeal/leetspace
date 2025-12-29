05f8da31a188b04762d903080ff18b7ac05f2cdb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-29 12:31:21 +0700

    feat(problem-2895/cpp): solve by using sorting and greedily match processor and task times
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2895/solution.cpp
+++ old-problems/2895/solution.cpp
@@ -1,9 +1,21 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int minProcessingTime(
       std::vector<int>& processorTime, std::vector<int>& tasks) {
-    return processorTime.size() + tasks.size();
+    std::sort(processorTime.begin(), processorTime.end());
+    std::sort(tasks.begin(), tasks.end(), std::greater<int>());
+
+    int maxTime{0};
+    for (std::size_t i{0}; i < processorTime.size(); ++i) {
+      for (std::size_t j{0}; j < 4; ++j) {
+        const int time{processorTime[i] + tasks[i * 4 + j]};
+        if (time > maxTime) maxTime = time;
+      }
+    }
+
+    return maxTime;
   }
 };
