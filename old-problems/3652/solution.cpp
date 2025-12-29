e0510282462a8461adb1c36f6ebf6a0afca674d9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-29 12:34:32 +0700

    feat(problem-3652/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3652/solution.cpp
+++ old-problems/3652/solution.cpp
@@ -4,6 +4,33 @@ class Solution {
  public:
   long long maxProfit(
       std::vector<int>& prices, std::vector<int>& strategy, int k) {
-    return prices.size() + strategy.size() + k;
+    std::size_t kk = k, kk2{kk / 2};
+
+    long long profit{0};
+    for (std::size_t i{0}; i < prices.size(); ++i) {
+      profit += prices[i] * strategy[i];
+    }
+
+    long long maxProfit{profit};
+
+    for (std::size_t i{0}; i < kk2; ++i) {
+      profit -= prices[i] * strategy[i];
+    }
+
+    for (std::size_t i{kk2}; i < kk; ++i) {
+      if (strategy[i] != 1) profit += prices[i] * (1 - strategy[i]);
+    }
+
+    if (profit > maxProfit) maxProfit = profit;
+
+    for (std::size_t i{kk}; i < prices.size(); ++i) {
+      if (strategy[i] != 1) profit += prices[i] * (1 - strategy[i]);
+      profit -= prices[i - kk2];
+      profit += prices[i - kk] * strategy[i - kk];
+
+      if (profit > maxProfit) maxProfit = profit;
+    }
+
+    return maxProfit;
   }
 };
