29768473735366e3dab60aa9d1a63c09cb3e34ce
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-30 10:38:40 +0700

    feat(problem-1502/cpp): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1502/solution.cpp
+++ old-problems/1502/solution.cpp
@@ -1,8 +1,14 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   bool canMakeArithmeticProgression(std::vector<int>& arr) {
-    return arr.empty();
+    std::sort(arr.begin(), arr.end());
+    const int diff{arr[1] - arr[0]};
+    for (std::size_t i{2}; i < arr.size(); ++i) {
+      if (arr[i] - arr[i - 1] != diff) return false;
+    }
+    return true;
   }
 };
