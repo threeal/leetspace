bb04600de7438e7bb13f527560e5b4074af5aef4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-30 08:42:07 +0700

    feat(problem-1619/c): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1619/c/solution.c
+++ old-problems/1619/c/solution.c
@@ -1,3 +1,19 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 double trimMean(int* arr, int arrSize) {
-  return arr[arrSize - 1];
+  qsort(arr, arrSize, sizeof(int), compare);
+
+  const int fifth = arrSize / 20;
+  const int ninetyFifth = arrSize - fifth;
+
+  int sum = 0;
+  for (int i = fifth; i < ninetyFifth; ++i) {
+    sum += arr[i];
+  }
+
+  return (double)sum / (ninetyFifth - fifth);
 }
