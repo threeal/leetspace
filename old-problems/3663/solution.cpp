b4d83a5b978ba553d1afa9dfcc67697edebfca74
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-30 08:26:11 +0700

    feat(problem-3663/cpp): solve by dividing number to count digits
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3663/solution.cpp
+++ old-problems/3663/solution.cpp
@@ -1,6 +1,19 @@
 class Solution {
  public:
   int getLeastFrequentDigit(int n) {
-    return n;
+    int freqs[10]{0};
+    while (n != 0) {
+      ++freqs[n % 10];
+      n /= 10;
+    }
+
+    int least{0};
+    while (freqs[least] == 0) ++least;
+
+    for (int digit{least + 1}; digit < 10; ++digit) {
+      if (freqs[digit] != 0 && freqs[digit] < freqs[least]) least = digit;
+    }
+
+    return least;
   }
 };
