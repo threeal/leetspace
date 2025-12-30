d0fc182c2e3cdfec56d61937c94141788c0a8a08
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-30 10:05:58 +0700

    feat(problem-766/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0766/solution.cpp
+++ old-problems/0766/solution.cpp
@@ -3,6 +3,22 @@
 class Solution {
  public:
   bool isToeplitzMatrix(std::vector<std::vector<int>>& matrix) {
-    return matrix.empty();
+    for (std::size_t i{0}; i < matrix.size(); ++i) {
+      std::size_t ii{i + 1}, jj{1};
+      while (ii < matrix.size() && jj < matrix[ii].size()) {
+        if (matrix[ii][jj] != matrix[i][0]) return false;
+        ++ii, ++jj;
+      }
+    }
+
+    for (std::size_t j{1}; j < matrix[0].size(); ++j) {
+      std::size_t ii{1}, jj{j + 1};
+      while (ii < matrix.size() && jj < matrix[ii].size()) {
+        if (matrix[ii][jj] != matrix[0][j]) return false;
+        ++ii, ++jj;
+      }
+    }
+
+    return true;
   }
 };
