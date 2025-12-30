e05f87c78fc9084cc52a353984c44751cd04ad75
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-30 13:25:28 +0700

    feat(problem-2180/cpp): solve by checking all numbers below `num` which digit sum is even
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2180/solution.cpp
+++ old-problems/2180/solution.cpp
@@ -1,6 +1,15 @@
 class Solution {
  public:
   int countEven(int num) {
-    return num;
+    int count{0};
+    for (int i{1}; i <= num; ++i) {
+      int ii{i}, sum{0};
+      while (ii != 0) {
+        sum += ii % 10;
+        ii /= 10;
+      }
+      if (sum % 2 == 0) ++count;
+    }
+    return count;
   }
 };
