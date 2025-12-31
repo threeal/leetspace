35521a304f16b09aed4136ed9fe0a4dd2bbed156
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-31 09:11:53 +0700

    feat(problem-2024/cpp): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2024/solution.cpp
+++ old-problems/2024/solution.cpp
@@ -3,6 +3,27 @@
 class Solution {
  public:
   int maxConsecutiveAnswers(std::string answerKey, int k) {
-    return answerKey.size() + k;
+    std::size_t maxConsecutive{0};
+    std::size_t lt{0}, lf{0};
+    int kt{k}, kf{k};
+    for (std::size_t r{0}; r < answerKey.size(); ++r) {
+      if (answerKey[r] == 'F') {
+        --kt;
+        while (kt < 0) {
+          if (answerKey[lt] == 'F') ++kt;
+          ++lt;
+        }
+      } else {
+        --kf;
+        while (kf < 0) {
+          if (answerKey[lf] == 'T') ++kf;
+          ++lf;
+        }
+      }
+
+      const std::size_t consecutive{r - (lt <= lf ? lt : lf) + 1};
+      if (consecutive > maxConsecutive) maxConsecutive = consecutive;
+    }
+    return maxConsecutive;
   }
 };
