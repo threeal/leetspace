abc339eda32a3aacece4e5d923262bf4760efdd5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-31 11:41:16 +0700

    feat(problem-3536/cpp): solve by returning the product of two largest digits
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3536/solution.cpp
+++ old-problems/3536/solution.cpp
@@ -1,6 +1,16 @@
 class Solution {
  public:
   int maxProduct(int n) {
-    return n;
+    int a{0}, b{0};
+    while (n != 0) {
+      if (n % 10 >= a) {
+        b = a;
+        a = n % 10;
+      } else if (n % 10 > b) {
+        b = n % 10;
+      }
+      n /= 10;
+    }
+    return a * b;
   }
 };
