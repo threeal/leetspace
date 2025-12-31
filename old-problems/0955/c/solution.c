bc138b906a4d8ee9ed0a558c30d22e31a8c924d0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-31 08:20:14 +0700

    feat(problem-955/c): solve by greedily check each column
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0955/c/solution.c
+++ old-problems/0955/c/solution.c
@@ -1,3 +1,31 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
 int minDeletionSize(char** strs, int strsSize) {
-  return *strs[strsSize - 1];
+  int deletions = 0;
+
+  bool* isValids = malloc(strsSize * sizeof(bool));
+  for (int i = 0; i < strsSize; ++i) isValids[i] = false;
+
+  int validLefts = strsSize - 1;
+
+  const int strs0Len = strlen(strs[0]);
+  for (int i = 0; i < strs0Len && validLefts > 0; ++i) {
+    int j = 1;
+    while (j < strsSize && (isValids[j] || (strs[j - 1][i] <= strs[j][i]))) ++j;
+
+    if (j == strsSize) {
+      for (int j = 1; j < strsSize; ++j) {
+        if (!isValids[j] && strs[j - 1][i] < strs[j][i]) {
+          isValids[j] = true;
+          --validLefts;
+        }
+      }
+    } else {
+      ++deletions;
+    }
+  }
+
+  return deletions;
 }
