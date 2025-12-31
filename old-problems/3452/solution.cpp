55c748ef64332db16906584dd7a42b85e57903da
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-31 11:35:15 +0700

    feat(problem-3452/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3452/solution.cpp
+++ old-problems/3452/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int sumOfGoodNumbers(std::vector<int>& nums, int k) {
-    return nums.size() + k;
+    int sum{0};
+    const int n = nums.size();
+    for (int i{0}; i < n; ++i) {
+      if ((i - k < 0 || nums[i] > nums[i - k]) &&
+          (i + k >= n || nums[i] > nums[i + k])) {
+        sum += nums[i];
+      }
+    }
+    return sum;
   }
 };
