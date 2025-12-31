883de52f4d5066a65e92f22be5b9cc1ef32498f0
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-31 10:48:26 +0700

    feat(problem-3074/c): solve by using greedy approach after sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3074/c/solution.c
+++ old-problems/3074/c/solution.c
@@ -1,3 +1,20 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)b - *(int*)a;
+}
+
 int minimumBoxes(int* apple, int appleSize, int* capacity, int capacitySize) {
-  return apple[appleSize - 1] + capacity[capacitySize - 1];
+  int sum = 0;
+  for (int i = 0; i < appleSize; ++i) sum += apple[i];
+
+  qsort(capacity, capacitySize, sizeof(int), compare);
+
+  int i = 0;
+  while (sum > 0) {
+    sum -= capacity[i];
+    ++i;
+  }
+
+  return i;
 }
