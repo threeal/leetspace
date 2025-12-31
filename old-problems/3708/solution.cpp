ea455bb1a6be1a0083c95360017f2fe343df2cea
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-31 11:59:41 +0700

    feat(problem-3708/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3708/solution.cpp
+++ old-problems/3708/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int longestSubarray(std::vector<int>& nums) {
-    return nums.size();
+    int length{2}, maxLength{2};
+    for (std::size_t i{2}; i < nums.size(); ++i) {
+      if (nums[i] == nums[i - 1] + nums[i - 2]) {
+        if (++length > maxLength) maxLength = length;
+      } else {
+        length = 2;
+      }
+    }
+    return maxLength;
   }
 };
