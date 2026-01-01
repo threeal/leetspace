04bf72fb916bb5bd5ec0f5b30bca42a7c02bcb56
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-01 21:32:38 +0700

    feat(problem-1338/cpp): solve by greedily pick number with large frequency
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1338/solution.cpp
+++ old-problems/1338/solution.cpp
@@ -1,8 +1,25 @@
+#include <algorithm>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int minSetSize(std::vector<int>& arr) {
-    return arr.size();
+    std::unordered_map<int, int> numFreqs{};
+    for (int num : arr) ++numFreqs[num];
+
+    std::vector<int> freqs{};
+    freqs.reserve(numFreqs.size());
+
+    for (const auto [num, freq] : numFreqs) {
+      freqs.push_back(freq);
+    }
+
+    std::sort(freqs.begin(), freqs.end(), std::greater<int>());
+
+    int i{0}, left = arr.size() / 2;
+    while (left > 0) left -= freqs[i++];
+
+    return i;
   }
 };
