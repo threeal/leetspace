666d221bc32fe1c04fa281e1c1698099b8888deb
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-01 21:30:16 +0700

    feat(problem-2980/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2980/c/solution.c
+++ old-problems/2980/c/solution.c
@@ -1,5 +1,11 @@
 #include <stdbool.h>
 
 bool hasTrailingZeros(int* nums, int numsSize) {
-  return nums[numsSize - 1] == 0;
+  int evens = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] % 2 == 0) {
+      if (++evens == 2) return true;
+    }
+  }
+  return false;
 }
