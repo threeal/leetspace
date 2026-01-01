025eb6d15e709b6c13f3ceb2bfb68024d44b9704
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-01 21:17:24 +0700

    test(problem-1200): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1200/c/solution.c
@@ -0,0 +1,8 @@
+int** minimumAbsDifference(
+    int* arr, int arrSize, int* returnSize, int** returnColumnSizes) {
+  (void)arr;
+  (void)arrSize;
+  *returnSize = 0;
+  *returnColumnSizes = 0;
+  return 0;
+}
