c7e392a7aee7fb3184bb3069821b91bdb19b513f
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-01 17:06:49 +0700

    feat(problem-227/cpp): solve by using a stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0227/solution.cpp
+++ old-problems/0227/solution.cpp
@@ -1,8 +1,73 @@
+#include <stack>
 #include <string>
+#include <tuple>
 
 class Solution {
  public:
   int calculate(std::string s) {
-    return s.size();
+    std::stack<std::tuple<int, char>> operations;
+    std::size_t i{0};
+    while (true) {
+      while (i < s.size() && s[i] == ' ') ++i;
+
+      int num{0};
+      while (s[i] >= '0') {
+        num = num * 10 + (s[i] - '0');
+        ++i;
+      }
+
+      while (i < s.size() && s[i] == ' ') ++i;
+      if (i != s.size()) {
+        while (!operations.empty() &&
+               priority(std::get<1>(operations.top())) <= priority(s[i])) {
+          const auto [prevNum, op] = operations.top();
+          operations.pop();
+
+          num = calculate(prevNum, op, num);
+        }
+
+        operations.push({num, s[i]});
+        ++i;
+      } else {
+        while (!operations.empty()) {
+          const auto [prevNum, op] = operations.top();
+          operations.pop();
+
+          num = calculate(prevNum, op, num);
+        }
+
+        return num;
+      }
+    }
+
+    return -1;
+  }
+
+ private:
+  static char priority(char op) {
+    switch (op) {
+      case '*':
+      case '/':
+        return 0;
+    }
+    return 1;
+  }
+
+  static int calculate(int a, char op, int b) {
+    switch (op) {
+      case '*':
+        return a * b;
+
+      case '+':
+        return a + b;
+
+      case '-':
+        return a - b;
+
+      case '/':
+        return a / b;
+    }
+
+    return 0;
   }
 };
