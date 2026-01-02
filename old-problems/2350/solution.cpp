287986487b0e38cab7dea2e4d67212c6a1cd66c6
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-02 14:22:26 +0700

    feat(problem-2350/cpp): solve by count window that contains all dice sides
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2350/solution.cpp
+++ old-problems/2350/solution.cpp
@@ -3,6 +3,22 @@
 class Solution {
  public:
   int shortestSequence(std::vector<int>& rolls, int k) {
-    return rolls.size() + k;
+    std::vector<bool> exists(k + 1, false);
+    int existCount{0};
+
+    int shortest{1};
+    for (std::size_t i{0}; i < rolls.size(); ++i) {
+      if (!exists[rolls[i]]) {
+        if (++existCount == k) {
+          ++shortest;
+          exists.assign(k + 1, false);
+          existCount = 0;
+        } else {
+          exists[rolls[i]] = true;
+        }
+      }
+    }
+
+    return shortest;
   }
 };
