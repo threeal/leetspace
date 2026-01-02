106f020f725a18dd9bdff6604e557d89dc741da5
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-02 15:38:40 +0700

    feat(problem-1721/cpp): solve by using while loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1721/solution.cpp
+++ old-problems/1721/solution.cpp
@@ -1,7 +1,23 @@
+#include <algorithm>
+
 class Solution {
  public:
   ListNode* swapNodes(ListNode* head, int k) {
-    head->val = k;
+    ListNode* left{head};
+    while (k > 1) {
+      left = left->next;
+      --k;
+    }
+
+    ListNode* right{head};
+    ListNode* tail{left};
+    while (tail->next != nullptr) {
+      right = right->next;
+      tail = tail->next;
+    }
+
+    std::swap(left->val, right->val);
+
     return head;
   }
 };
