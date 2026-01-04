8c3228e18300b361231be5df1586899cac4e0069
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-04 21:31:41 +0700

    feat(problem-1390/cpp): solve by checking all possible factors of each number
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1390/solution.cpp
+++ old-problems/1390/solution.cpp
@@ -3,6 +3,21 @@
 class Solution {
  public:
   int sumFourDivisors(std::vector<int>& nums) {
-    return nums.size();
+    int sum{0};
+    for (const int num : nums) {
+      int a{2};
+      while (a * a < num && num % a != 0) ++a;
+
+      if (a * a < num) {
+        int b{a + 1};
+        while (b * b < num && num % b != 0) ++b;
+
+        if (b * b > num) {
+          sum += 1 + a + num / a + num;
+        }
+      }
+    }
+
+    return sum;
   }
 };
