a499299a482e968eec6af474955268682ee382de
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-04 22:08:55 +0700

    feat(problem-1385/c): solve by using sorting and binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1385/c/solution.c
+++ old-problems/1385/c/solution.c
@@ -1,4 +1,37 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int findTheDistanceValue(
     int* arr1, int arr1Size, int* arr2, int arr2Size, int d) {
-  return arr1[arr1Size] + arr2[arr2Size] + d;
+  qsort(arr2, arr2Size, sizeof(int), compare);
+
+  int count = 0;
+  for (int i = 0; i < arr1Size; ++i) {
+    int low1 = 0, high1 = arr2Size;
+    while (low1 < high1) {
+      const int mid = low1 + (high1 - low1) / 2;
+      if (arr2[mid] < arr1[i] - d) {
+        low1 = mid + 1;
+      } else {
+        high1 = mid;
+      }
+    }
+
+    int low2 = 0, high2 = arr2Size;
+    while (low2 < high2) {
+      const int mid = low2 + (high2 - low2) / 2;
+      if (arr2[mid] <= arr1[i] + d) {
+        low2 = mid + 1;
+      } else {
+        high2 = mid;
+      }
+    }
+
+    if (low1 == low2) ++count;
+  }
+
+  return count;
 }
