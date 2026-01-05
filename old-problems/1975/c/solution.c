d487bcd9c1258aa0937ece09125b7495d8845b56
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-05 09:23:13 +0700

    feat(problem-1975/c): solve by sum absolute values
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1975/c/solution.c
+++ old-problems/1975/c/solution.c
@@ -1,3 +1,24 @@
+#include <stdbool.h>
+
 long long maxMatrixSum(int** matrix, int matrixSize, int* matrixColSize) {
-  return matrix[matrixSize - 1][matrixColSize[matrixSize - 1] - 1];
+  long long sum = 0;
+  int negCount = 0, minVal = 100000;
+  bool hasZero = false;
+  for (int i = 0; i < matrixSize; ++i) {
+    for (int j = 0; j < matrixColSize[i]; ++j) {
+      const int val = matrix[i][j];
+      if (val == 0) {
+        hasZero = true;
+      } else if (val < 0) {
+        ++negCount;
+        sum += -val;
+        if (-val < minVal) minVal = -val;
+      } else {
+        sum += val;
+        if (val < minVal) minVal = val;
+      }
+    }
+  }
+  if (!hasZero && negCount % 2 == 1) sum -= 2 * minVal;
+  return sum;
 }
