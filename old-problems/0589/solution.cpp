0f4f0e2b1f9105bf89f0fdaf404e7f33ac11976f
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-05 18:11:08 +0700

    feat(problem-589/cpp): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0589/solution.cpp
+++ old-problems/0589/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   std::vector<int> preorder(Node* root) {
-    return {root->val};
+    std::vector<int> output{};
+    if (root != nullptr) traverse(output, root);
+    return output;
+  }
+
+ private:
+  void traverse(std::vector<int>& output, Node* node) {
+    output.push_back(node->val);
+    for (Node* child : node->children) {
+      traverse(output, child);
+    }
   }
 };
