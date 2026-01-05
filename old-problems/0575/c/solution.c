ddfe85fdd6cddd6049d7c9112df3d466436e8b13
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-05 20:33:12 +0700

    feat(problem-575/c): solve by returning the minimum between unique numbers and `n / 2`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0575/c/solution.c
+++ old-problems/0575/c/solution.c
@@ -1,3 +1,23 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int distributeCandies(int* candyType, int candyTypeSize) {
-  return candyType[candyTypeSize - 1];
+  if (candyTypeSize == 2) return 1;
+
+  qsort(candyType, candyTypeSize, sizeof(int), compare);
+
+  int uniques = 1;
+  const int half = candyTypeSize / 2;
+
+  for (int i = 1; i < candyTypeSize; ++i) {
+    if (candyType[i] != candyType[i - 1]) {
+      ++uniques;
+      if (uniques == half) break;
+    }
+  }
+
+  return uniques;
 }
