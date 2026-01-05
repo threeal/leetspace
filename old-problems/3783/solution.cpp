33016dc2fc81c5c9899b6075b0b36aeb2e0dcfa8
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-05 10:48:39 +0700

    feat(problem-3783/cpp): solve by using modulo and division to reverse digits
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3783/solution.cpp
+++ old-problems/3783/solution.cpp
@@ -1,6 +1,14 @@
+#include <cstdlib>
+
 class Solution {
  public:
   int mirrorDistance(int n) {
-    return n;
+    const int original{n};
+    int mirror{0};
+    while (n != 0) {
+      mirror = mirror * 10 + n % 10;
+      n /= 10;
+    }
+    return std::abs(original - mirror);
   }
 };
