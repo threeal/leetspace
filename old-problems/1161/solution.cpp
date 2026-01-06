03edf19e81db6e7e910f1f1939610b0c5c53fc1c
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-06 09:35:32 +0700

    feat(problem-1161/cpp): solve by using breadth-first search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1161/solution.cpp
+++ old-problems/1161/solution.cpp
@@ -1,6 +1,32 @@
+#include <queue>
+
 class Solution {
  public:
   int maxLevelSum(TreeNode* root) {
-    return root->val;
+    int maxSum{root->val}, maxSumLevel{1};
+
+    std::queue<TreeNode*> nodes{};
+    if (root->left) nodes.push(root->left);
+    if (root->right) nodes.push(root->right);
+
+    for (int level{2}; !nodes.empty(); ++level) {
+      int sum{0};
+
+      for (std::size_t n{nodes.size()}; n != 0; --n) {
+        TreeNode* node{nodes.front()};
+        nodes.pop();
+
+        sum += node->val;
+        if (node->left) nodes.push(node->left);
+        if (node->right) nodes.push(node->right);
+      }
+
+      if (sum > maxSum) {
+        maxSum = sum;
+        maxSumLevel = level;
+      }
+    }
+
+    return maxSumLevel;
   }
 };
