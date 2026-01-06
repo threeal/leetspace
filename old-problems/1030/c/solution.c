9ae36925b983b68028dd2488958872cde6530460
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-06 13:40:40 +0700

    test(problem-1030): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1030/c/solution.c
@@ -0,0 +1,11 @@
+int** allCellsDistOrder(
+    int rows, int cols, int rCenter, int cCenter,
+    int* returnSize, int** returnColumnSizes) {
+  (void)rows;
+  (void)cols;
+  (void)rCenter;
+  (void)cCenter;
+  *returnSize = 0;
+  *returnColumnSizes = 0;
+  return 0;
+}
