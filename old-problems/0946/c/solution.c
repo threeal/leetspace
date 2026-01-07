8515e5bec98b3b9db8a739b737fe53fab8e7b62c
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-07 21:10:40 +0700

    feat(problem-946/c): solve by simulating the stack process
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0946/c/solution.c
+++ old-problems/0946/c/solution.c
@@ -2,5 +2,16 @@
 
 bool validateStackSequences(
     int* pushed, int pushedSize, int* popped, int poppedSize) {
-  return pushed[pushedSize - 1] == popped[poppedSize - 1];
+  int l = 1, r = 1, i = 0;
+  while (true) {
+    if (l == 0 || pushed[l - 1] != popped[i]) {
+      if (r == pushedSize) break;
+      pushed[l++] = pushed[r++];
+    } else {
+      --l;
+      ++i;
+      if (i == poppedSize) break;
+    }
+  }
+  return i == poppedSize;
 }
