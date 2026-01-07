8a96aeef040188f95778fc9f184bcf475264f6c1
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-07 09:50:37 +0700

    feat(problem-1339/cpp): solve by using a depth-first search approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1339/solution.cpp
+++ old-problems/1339/solution.cpp
@@ -1,6 +1,44 @@
 class Solution {
  public:
   int maxProduct(TreeNode* root) {
-    return root->val;
+    searchSum(root);
+    return searchMaxProduct(root, 0) % 1000000007;
+  }
+
+ private:
+  static void searchSum(TreeNode* node) {
+    if (node->left != nullptr) {
+      searchSum(node->left);
+      node->val += node->left->val;
+    }
+
+    if (node->right != nullptr) {
+      searchSum(node->right);
+      node->val += node->right->val;
+    }
+  }
+
+  static long long searchMaxProduct(TreeNode* node, int prevSum) {
+    long long maxProduct{0};
+
+    if (node->left != nullptr) {
+      const long long rightSum{prevSum + node->val - node->left->val};
+      long long product{(node->left->val * rightSum)};
+      if (product > maxProduct) maxProduct = product;
+
+      product = searchMaxProduct(node->left, rightSum);
+      if (product > maxProduct) maxProduct = product;
+    }
+
+    if (node->right != nullptr) {
+      const long long leftSum{prevSum + node->val - node->right->val};
+      long long product{(node->right->val * leftSum)};
+      if (product > maxProduct) maxProduct = product;
+
+      product = searchMaxProduct(node->right, leftSum);
+      if (product > maxProduct) maxProduct = product;
+    }
+
+    return maxProduct;
   }
 };
