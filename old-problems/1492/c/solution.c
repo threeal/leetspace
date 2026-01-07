dd4bd6ec2a4f8acd3ae5bc6e1f93e42161dc60fb
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-07 14:01:38 +0700

    feat(problem-1492/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1492/c/solution.c
+++ old-problems/1492/c/solution.c
@@ -1,3 +1,11 @@
 int kthFactor(int n, int k) {
-  return n + k;
+  const int n2 = n / 2;
+  for (int i = 1; i <= n2; ++i) {
+    if (n % i == 0) {
+      if (--k == 0) {
+        return i;
+      }
+    }
+  }
+  return k == 1 ? n : -1;
 }
