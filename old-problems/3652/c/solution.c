b32eea3c76f7aca107b30ca93b7f1eaddb3da332
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-08 21:39:24 +0700

    feat(problem-3652/c): solve by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3652/c/solution.c
+++ old-problems/3652/c/solution.c
@@ -1,4 +1,33 @@
 long long maxProfit(
     int* prices, int pricesSize, int* strategy, int strategySize, int k) {
-  return prices[pricesSize - 1] + strategy[strategySize - 1] + k;
+  (void)strategySize;
+
+  const int k2 = k / 2;
+
+  long long profit = 0;
+  for (int i = 0; i < pricesSize; ++i) {
+    profit += prices[i] * strategy[i];
+  }
+
+  long long maxProfit = profit;
+
+  for (int i = 0; i < k2; ++i) {
+    profit -= prices[i] * strategy[i];
+  }
+
+  for (int i = k2; i < k; ++i) {
+    if (strategy[i] != 1) profit += prices[i] * (1 - strategy[i]);
+  }
+
+  if (profit > maxProfit) maxProfit = profit;
+
+  for (int i = k; i < pricesSize; ++i) {
+    if (strategy[i] != 1) profit += prices[i] * (1 - strategy[i]);
+    profit -= prices[i - k2];
+    profit += prices[i - k] * strategy[i - k];
+
+    if (profit > maxProfit) maxProfit = profit;
+  }
+
+  return maxProfit;
 }
