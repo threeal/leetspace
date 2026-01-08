66c2c53d048603a8604857751c862e3fbe65094b
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-08 12:26:04 +0700

    feat(problem-429/cpp): solve by using breadth-first search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0429/solution.cpp
+++ old-problems/0429/solution.cpp
@@ -1,8 +1,30 @@
+#include <queue>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::vector<int>> levelOrder(Node* root) {
-    return {{root->val}};
+    if (root == nullptr) return {};
+
+    std::vector<std::vector<int>> vals{};
+    std::queue<Node*> nodes{};
+    nodes.push(root);
+
+    while (!nodes.empty()) {
+      const std::size_t n{nodes.size()};
+      vals.push_back(std::vector<int>(n));
+
+      for (std::size_t i{0}; i < n; ++i) {
+        Node* node{nodes.front()};
+        nodes.pop();
+
+        vals.back()[i] = node->val;
+        for (Node* child : node->children) {
+          nodes.push(child);
+        }
+      }
+    }
+
+    return vals;
   }
 };
