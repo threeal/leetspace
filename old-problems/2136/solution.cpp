da6328791674fd6e7131072a41385e290466b52a
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-08 13:09:12 +0700

    feat(problem-2136/cpp): solve by greedily plant flowers with longest growth time
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2136/solution.cpp
+++ old-problems/2136/solution.cpp
@@ -1,9 +1,29 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   int earliestFullBloom(
       std::vector<int>& plantTime, std::vector<int>& growTime) {
-    return plantTime.size() + growTime.size();
+    std::vector<std::size_t> indices(plantTime.size());
+    std::iota(indices.begin(), indices.end(), 0);
+
+    std::sort(
+        indices.begin(), indices.end(),
+        [&](std::size_t i, std::size_t j) {
+          return growTime[i] > growTime[j];
+        });
+
+    int day{0};
+    int maxBloomDay{0};
+    for (std::size_t i : indices) {
+      day += plantTime[i];
+      if (day + growTime[i] > maxBloomDay) {
+        maxBloomDay = day + growTime[i];
+      }
+    }
+
+    return maxBloomDay;
   }
 };
