6c586ab0ac1e881b68eef9c99460e81678dfdb56
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-08 21:11:40 +0700

    feat(problem-3798/cpp): solve by using a while-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3798/solution.cpp
+++ old-problems/3798/solution.cpp
@@ -3,6 +3,8 @@
 class Solution {
  public:
   std::string largestEven(std::string s) {
-    return s;
+    std::size_t n{s.size()};
+    while (n != 0 && s[n - 1] == '1') --n;
+    return s.substr(0, n);
   }
 };
