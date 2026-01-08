c03efb8ddc7bb5131a1d313999c5a4f612f20719
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-08 12:02:10 +0700

    feat(problem-3537/cpp): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3537/solution.cpp
+++ old-problems/3537/solution.cpp
@@ -3,6 +3,36 @@
 class Solution {
  public:
   std::vector<std::vector<int>> specialGrid(int n) {
-    return std::vector<std::vector<int>>(n, std::vector<int>(n, 0));
+    if (n == 0) return {{0}};
+
+    std::size_t width{1};
+    while (n > 0) {
+      width *= 2;
+      --n;
+    }
+
+    std::vector<std::vector<int>> grid(width, std::vector<int>(width));
+    fill(grid, 0, 0, width, 0);
+
+    return grid;
+  }
+
+ private:
+  static void fill(
+      std::vector<std::vector<int>>& grid,
+      std::size_t y, std::size_t x, std::size_t width, int min) {
+    if (width == 2) {
+      grid[y][x + 1] = min;
+      grid[y + 1][x + 1] = min + 1;
+      grid[y + 1][x] = min + 2;
+      grid[y][x] = min + 3;
+    } else {
+      const std::size_t half{width / 2};
+      const std::size_t area{half * half};
+      fill(grid, y, x + half, half, min);
+      fill(grid, y + half, x + half, half, min + area);
+      fill(grid, y + half, x, half, min + 2 * area);
+      fill(grid, y, x, half, min + 3 * area);
+    }
   }
 };
