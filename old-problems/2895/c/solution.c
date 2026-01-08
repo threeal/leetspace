e3b97dc2b53561b89b4a3fd94bea83776b174871
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-08 09:36:17 +0700

    feat(problem-2895/c): solve by using sorting and greedily match processor and task times
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2895/c/solution.c
+++ old-problems/2895/c/solution.c
@@ -1,4 +1,25 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
+int compareGreater(const void* a, const void* b) {
+  return *(int*)b - *(int*)a;
+}
+
 int minProcessingTime(
     int* processorTime, int processorTimeSize, int* tasks, int tasksSize) {
-  return processorTime[processorTimeSize - 1] + tasks[tasksSize - 1];
+  qsort(processorTime, processorTimeSize, sizeof(int), compare);
+  qsort(tasks, tasksSize, sizeof(int), compareGreater);
+
+  int maxTime = 0;
+  for (int i = 0; i < processorTimeSize; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      const int time = processorTime[i] + tasks[i * 4 + j];
+      if (time > maxTime) maxTime = time;
+    }
+  }
+
+  return maxTime;
 }
