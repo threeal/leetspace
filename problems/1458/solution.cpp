58a547550f06f7917939edb015ff34d280453da6
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-08 11:55:49 +0700

    perf(problem-1458/cpp): modify solution to tuse bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- problems/1458/solution.cpp
+++ problems/1458/solution.cpp
@@ -1,31 +1,27 @@
 #include <algorithm>
-#include <functional>
-#include <limits>
 #include <vector>
 
 class Solution {
  public:
   int maxDotProduct(std::vector<int>& nums1, std::vector<int>& nums2) {
-    static const int min_val = std::numeric_limits<int>::min();
+    std::vector<std::vector<int>> dp(
+        nums1.size(), std::vector<int>(nums2.size()));
 
-    const int n1 = nums1.size();
-    const int n2 = nums2.size();
+    dp[0][0] = nums1[0] * nums2[0];
+    for (std::size_t j{1}; j < nums2.size(); ++j) {
+      dp[0][j] = std::max(nums1[0] * nums2[j], dp[0][j - 1]);
+    }
 
-    auto cache = std::vector(n1, std::vector(n2, min_val));
+    for (std::size_t i{1}; i < nums1.size(); ++i) {
+      dp[i][0] = std::max(nums1[i] * nums2[0], dp[i - 1][0]);
 
-    const std::function<int(int, int)> fn = [&](int i1, int i2) -> int {
-      if (i1 >= n1 || i2 >= n2) return min_val;
+      for (std::size_t j{1}; j < nums2.size(); ++j) {
+        dp[i][j] = std::max(
+            std::max(dp[i - 1][j - 1], 0) + nums1[i] * nums2[j],
+            std::max(dp[i - 1][j], dp[i][j - 1]));
+      }
+    }
 
-      if (cache[i1][i2] > min_val) return cache[i1][i2];
-
-      cache[i1][i2] = std::max({nums1[i1] * nums2[i2] + std::max(fn(i1 + 1, i2 + 1), 0),
-                                fn(i1 + 1, i2),
-                                fn(i1, i2 + 1),
-                                fn(i1 + 1, i2 + 1)});
-
-      return cache[i1][i2];
-    };
-
-    return fn(0, 0);
+    return dp[nums1.size() - 1][nums2.size() - 1];
   }
 };
