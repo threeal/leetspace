bbe7a3c96c6a35f5c8bc9544a016bff320197515
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-09 17:27:51 +0700

    feat(problem-1991/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1991/solution.cpp
+++ old-problems/1991/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   int findMiddleIndex(std::vector<int>& nums) {
-    return nums.size();
+    int rightSum{0};
+    for (int num : nums) rightSum += num;
+
+    int leftSum{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      rightSum -= nums[i];
+      if (leftSum == rightSum) return i;
+      leftSum += nums[i];
+    }
+
+    return -1;
   }
 };
