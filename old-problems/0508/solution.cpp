0fbaa33ea45bc291552a51b7a2496fea0b87da43
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-09 17:35:08 +0700

    test: add test for problem 508. Most Frequent Subtree Sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0508/solution.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> findFrequentTreeSum(TreeNode* root) {
+    return {root->val};
+  }
+};
---
+++ old-problems/0508/test.cpp
@@ -0,0 +1,53 @@
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+// clang-format off
+#include "solution.cpp"
+// clang-format on
+
+#include <catch2/catch_test_macros.hpp>
+#include <optional>
+#include <queue>
+#include <vector>
+
+TreeNode* toTreeNode(const std::vector<std::optional<int>>& list) {
+  if (list.empty()) return nullptr;
+
+  TreeNode* root{new TreeNode(*list[0])};
+
+  std::queue<TreeNode**> children{};
+  children.push(&(root->left));
+  children.push(&(root->right));
+
+  for (std::size_t i{1}; i < list.size(); ++i) {
+    if (list[i]) {
+      TreeNode* node{new TreeNode(*list[i])};
+      *children.front() = node;
+      children.push(&(node->left));
+      children.push(&(node->right));
+    }
+    children.pop();
+  }
+
+  return root;
+}
+
+TEST_CASE("508. Most Frequent Subtree Sum") {
+  SECTION("Example 1") {
+    TreeNode* root{toTreeNode({5, 2, -3})};
+    REQUIRE(Solution{}.findFrequentTreeSum(root) == std::vector<int>{2, -3, 4});
+  }
+
+  SECTION("Example 2") {
+    TreeNode* root{toTreeNode({5, 2, -5})};
+    REQUIRE(Solution{}.findFrequentTreeSum(root) == std::vector<int>{2});
+  }
+}
