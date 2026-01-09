af2e7d2aef15d27a9964e44ecde2f87624534540
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-09 14:20:00 +0700

    test: add test for problem 3607. Power Grid Maintenance
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3607/solution.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> processQueries(
+      int c, std::vector<std::vector<int>>& connections,
+      std::vector<std::vector<int>>& queries) {
+    (void)c;
+    (void)connections;
+    (void)queries;
+    return {};
+  }
+};
