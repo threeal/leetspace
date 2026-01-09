13f477d86827e6da159ec0c45044896d31d5e29c
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-09 17:00:29 +0700

    feat(problem-3033/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3033/solution.cpp
+++ old-problems/3033/solution.cpp
@@ -4,6 +4,16 @@ class Solution {
  public:
   std::vector<std::vector<int>> modifiedMatrix(
       std::vector<std::vector<int>>& matrix) {
+    for (std::size_t i{0}; i < matrix[0].size(); ++i) {
+      int maxValue{0};
+      for (std::size_t j{0}; j < matrix.size(); ++j) {
+        if (matrix[j][i] > maxValue) maxValue = matrix[j][i];
+      }
+
+      for (std::size_t j{0}; j < matrix.size(); ++j) {
+        if (matrix[j][i] == -1) matrix[j][i] = maxValue;
+      }
+    }
     return matrix;
   }
 };
