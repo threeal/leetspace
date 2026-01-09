cf449ef839393712674ea2ab72b5738826c2846f
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-09 14:44:59 +0700

    test: add test for problem 225. Implement Stack using Queues
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0225/solution.cpp
@@ -0,0 +1,20 @@
+class MyStack {
+ public:
+  MyStack() {}
+
+  void push(int x) {
+    (void)x;
+  }
+
+  int pop() {
+    return 0;
+  }
+
+  int top() {
+    return 0;
+  }
+
+  bool empty() {
+    return false;
+  }
+};
