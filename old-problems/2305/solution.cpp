7915d4c566cb175a05b243baa28bf620a5bd6ab4
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-12 20:04:17 +0700

    feat(problem-2305/cpp): solve by using DFS
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2305/solution.cpp
+++ old-problems/2305/solution.cpp
@@ -1,8 +1,31 @@
+#include <limits>
 #include <vector>
 
 class Solution {
  public:
   int distributeCookies(std::vector<int>& cookies, int k) {
-    return cookies.size() + k;
+    std::vector<int> children(k, 0);
+    return search(cookies, 0, children);
+  }
+
+ private:
+  int search(const std::vector<int>& cookies, std::size_t i, std::vector<int>& children) {
+    if (i == cookies.size()) {
+      int maxChild{0};
+      for (const int child : children) {
+        if (child > maxChild) maxChild = child;
+      }
+      return maxChild;
+    }
+
+    int minMaxChild{std::numeric_limits<int>::max()};
+    for (int& child : children) {
+      child += cookies[i];
+      const int maxChild{search(cookies, i + 1, children)};
+      if (maxChild < minMaxChild) minMaxChild = maxChild;
+      child -= cookies[i];
+    }
+
+    return minMaxChild;
   }
 };
