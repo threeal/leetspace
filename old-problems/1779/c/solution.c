3b6e8c2dbe8b8931df002a9e24fc910c32b91388
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-12 19:57:30 +0700

    feat(problem-1779/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1779/c/solution.c
+++ old-problems/1779/c/solution.c
@@ -1,4 +1,24 @@
+#include <limits.h>
+#include <stdlib.h>
+
 int nearestValidPoint(
     int x, int y, int** points, int pointsSize, int* pointsColSize) {
-  return x + y + points[pointsSize - 1][pointsColSize[pointsSize - 1] - 1];
+  (void)pointsColSize;
+
+  int nearest = -1, distance = INT_MAX;
+  for (int i = 0; i < pointsSize; ++i) {
+    if (points[i][0] == x) {
+      if (abs(points[i][1] - y) < distance) {
+        nearest = i;
+        distance = abs(points[i][1] - y);
+      }
+    } else if (points[i][1] == y) {
+      if (abs(points[i][0] - x) < distance) {
+        nearest = i;
+        distance = abs(points[i][0] - x);
+      }
+    }
+  }
+
+  return nearest;
 }
