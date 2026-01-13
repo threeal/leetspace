524e7202c0609a0eb10f6736153e0e9239f6800c
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-13 12:35:58 +0700

    feat(problem-529/cpp): solve by using detph-first search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0529/solution.cpp
+++ old-problems/0529/solution.cpp
@@ -4,7 +4,61 @@ class Solution {
  public:
   std::vector<std::vector<char>> updateBoard(
       std::vector<std::vector<char>>& board, std::vector<int>& click) {
-    (void)click;
+    if (board[click[0]][click[1]] == 'M') {
+      board[click[0]][click[1]] = 'X';
+    } else {
+      if (board[click[0]][click[1]] == 'E') {
+        reveal(board, click[0], click[1]);
+      }
+    }
     return board;
+  }
+
+ private:
+  static void reveal(
+      std::vector<std::vector<char>>& board, std::size_t y, std::size_t x) {
+    int mines{0};
+
+    if (y > 0) {
+      if (x > 0 && board[y - 1][x - 1] == 'M') ++mines;
+      if (board[y - 1][x] == 'M') ++mines;
+      if (x + 1 < board[y - 1].size() && board[y - 1][x + 1] == 'M') ++mines;
+    }
+
+    if (x > 0 && board[y][x - 1] == 'M') ++mines;
+    if (x + 1 < board[y].size() && board[y][x + 1] == 'M') ++mines;
+
+    if (y + 1 < board.size()) {
+      if (x > 0 && board[y + 1][x - 1] == 'M') ++mines;
+      if (board[y + 1][x] == 'M') ++mines;
+      if (x + 1 < board[y + 1].size() && board[y + 1][x + 1] == 'M') ++mines;
+    }
+
+    if (mines != 0) {
+      board[y][x] = '0' + mines;
+    } else {
+      board[y][x] = 'B';
+
+      if (y > 0) {
+        if (x > 0 && board[y - 1][x - 1] == 'E') reveal(board, y - 1, x - 1);
+        if (board[y - 1][x] == 'E') reveal(board, y - 1, x);
+        if (x + 1 < board[y - 1].size() && board[y - 1][x + 1] == 'E') {
+          reveal(board, y - 1, x + 1);
+        }
+      }
+
+      if (x > 0 && board[y][x - 1] == 'E') reveal(board, y, x - 1);
+      if (x + 1 < board[y].size() && board[y][x + 1] == 'E') {
+        reveal(board, y, x + 1);
+      }
+
+      if (y + 1 < board.size()) {
+        if (x > 0 && board[y + 1][x - 1] == 'E') reveal(board, y + 1, x - 1);
+        if (board[y + 1][x] == 'E') reveal(board, y + 1, x);
+        if (x + 1 < board[y + 1].size() && board[y + 1][x + 1] == 'E') {
+          reveal(board, y + 1, x + 1);
+        }
+      }
+    }
   }
 };
