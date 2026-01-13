afcb19a0496e900daea4ff084428da2fad8e0ab4
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-13 12:09:31 +0700

    feat(problem-973/cpp): solve by using a partial sort
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0973/solution.cpp
+++ old-problems/0973/solution.cpp
@@ -1,9 +1,16 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::vector<int>> kClosest(
       std::vector<std::vector<int>>& points, int k) {
+    std::partial_sort(
+        points.begin(), points.begin() + k, points.end(),
+        [](const auto& a, const auto& b) {
+          return (a[0] * a[0] + a[1] * a[1]) < (b[0] * b[0] + b[1] * b[1]);
+        });
+
     points.resize(k);
     return points;
   }
