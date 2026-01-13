4cf069a988c45cd145de8a4d26b9e48d53cad347
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-13 11:43:50 +0700

    feat(problem-1899/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1899/solution.cpp
+++ old-problems/1899/solution.cpp
@@ -4,6 +4,16 @@ class Solution {
  public:
   bool mergeTriplets(
       std::vector<std::vector<int>>& triplets, std::vector<int>& target) {
-    return triplets[0] == target;
+    int flag{0};
+    for (const auto& triplet : triplets) {
+      if (triplet[0] <= target[0] && triplet[1] <= target[1] &&
+          triplet[2] <= target[2]) {
+        if (triplet[0] == target[0]) flag |= 1;
+        if (triplet[1] == target[1]) flag |= 2;
+        if (triplet[2] == target[2]) flag |= 4;
+        if (flag == 7) return true;
+      }
+    }
+    return false;
   }
 };
