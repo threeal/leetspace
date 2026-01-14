4900968b0f27e64fc6f0c436bbc57faa8cfa541b
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-14 12:15:36 +0700

    feat(problem-1525/cpp): solve by counting character frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1525/solution.cpp
+++ old-problems/1525/solution.cpp
@@ -3,6 +3,18 @@
 class Solution {
  public:
   int numSplits(std::string s) {
-    return s.size();
+    int rFreqs[26]{0}, rDistinct{0};
+    for (char c : s) {
+      if (++rFreqs[c - 'a'] == 1) ++rDistinct;
+    }
+
+    int lFreqs[26]{0}, lDistinct{0}, splits{0};
+    for (char c : s) {
+      if (++lFreqs[c - 'a'] == 1) ++lDistinct;
+      if (--rFreqs[c - 'a'] == 0) --rDistinct;
+      if (lDistinct == rDistinct) ++splits;
+    }
+
+    return splits;
   }
 };
