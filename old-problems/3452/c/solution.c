603d31046863953e5ddf54718d1b945dfc4439b2
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-14 20:57:16 +0700

    feat(problem-3452/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3452/c/solution.c
+++ old-problems/3452/c/solution.c
@@ -1,3 +1,10 @@
 int sumOfGoodNumbers(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] + k;
+  int sum = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if ((i - k < 0 || nums[i] > nums[i - k]) &&
+        (i + k >= numsSize || nums[i] > nums[i + k])) {
+      sum += nums[i];
+    }
+  }
+  return sum;
 }
