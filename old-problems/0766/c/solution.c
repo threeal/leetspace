5120e2fb8377966dab636df57de83b5d1bd44fb0
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-14 14:12:10 +0700

    feat(problem-766/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0766/c/solution.c
+++ old-problems/0766/c/solution.c
@@ -1,5 +1,21 @@
 #include <stdbool.h>
 
 bool isToeplitzMatrix(int** matrix, int matrixSize, int* matrixColSize) {
-  return matrix[matrixSize - 1][matrixColSize[matrixSize - 1] - 1] == 0;
+  for (int i = 0; i < matrixSize; ++i) {
+    int ii = i + 1, jj = 1;
+    while (ii < matrixSize && jj < matrixColSize[ii]) {
+      if (matrix[ii][jj] != matrix[i][0]) return false;
+      ++ii, ++jj;
+    }
+  }
+
+  for (int j = 1; j < matrixColSize[0]; ++j) {
+    int ii = 1, jj = j + 1;
+    while (ii < matrixSize && jj < matrixColSize[ii]) {
+      if (matrix[ii][jj] != matrix[0][j]) return false;
+      ++ii, ++jj;
+    }
+  }
+
+  return true;
 }
