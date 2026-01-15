d434d1f1604e585390248e0e49ffa5b37d97e06d
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-15 16:01:06 +0700

    feat(problem-257/cpp): solve by using DFS and backtracking
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0257/solution.cpp
+++ old-problems/0257/solution.cpp
@@ -4,7 +4,33 @@
 class Solution {
  public:
   std::vector<std::string> binaryTreePaths(TreeNode* root) {
-    (void)root;
-    return {};
+    std::vector<std::string> paths{};
+    std::string path{""};
+    explore(paths, path, root);
+    return paths;
+  }
+
+ private:
+  static void explore(
+      std::vector<std::string>& paths, std::string& path, TreeNode* node) {
+    const std::size_t prevSize{path.size()};
+    path += std::to_string(node->val);
+
+    if (node->left != nullptr) {
+      path += "->";
+      explore(paths, path, node->left);
+      if (node->right != nullptr) {
+        explore(paths, path, node->right);
+      }
+    } else {
+      if (node->right != nullptr) {
+        path += "->";
+        explore(paths, path, node->right);
+      } else {
+        paths.push_back(path);
+      }
+    }
+
+    path.resize(prevSize);
   }
 };
