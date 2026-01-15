c3ddaddf039021d666e3c97931beff804f8ade1c
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-15 10:31:18 +0700

    feat(problem-3708/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3708/c/solution.c
+++ old-problems/3708/c/solution.c
@@ -1,3 +1,11 @@
 int longestSubarray(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int length = 2, maxLength = 2;
+  for (int i = 2; i < numsSize; ++i) {
+    if (nums[i] == nums[i - 1] + nums[i - 2]) {
+      if (++length > maxLength) maxLength = length;
+    } else {
+      length = 2;
+    }
+  }
+  return maxLength;
 }
