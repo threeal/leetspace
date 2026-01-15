f092ae99078c3ca1b4ab26763e01002ffbf7ac73
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-15 10:09:27 +0700

    feat(problem-2711/cpp): solve by iterating diagonally and counting freqs
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2711/solution.cpp
+++ old-problems/2711/solution.cpp
@@ -1,9 +1,48 @@
+#include <cstdlib>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::vector<int>> differenceOfDistinctValues(
       std::vector<std::vector<int>>& grid) {
+    for (std::size_t i{0}; i < grid.size(); ++i) {
+      int brFreqs[51]{0}, brDistinct{0};
+      std::size_t ii{i}, j{0};
+      while (ii < grid.size() && j < grid[ii].size()) {
+        if (++brFreqs[grid[ii][j]] == 1) ++brDistinct;
+        ++ii, ++j;
+      }
+
+      int tlFreqs[51]{0}, tlDistinct{0};
+      ii = i, j = 0;
+      while (ii < grid.size() && j < grid[ii].size()) {
+        const int num{grid[ii][j]};
+        if (--brFreqs[num] == 0) --brDistinct;
+        grid[ii][j] = std::abs(tlDistinct - brDistinct);
+        if (++tlFreqs[num] == 1) ++tlDistinct;
+        ++ii, ++j;
+      }
+    }
+
+    for (std::size_t j{1}; j < grid[0].size(); ++j) {
+      int brFreqs[51]{0}, brDistinct{0};
+      std::size_t i{0}, jj{j};
+      while (i < grid.size() && jj < grid[i].size()) {
+        if (++brFreqs[grid[i][jj]] == 1) ++brDistinct;
+        ++i, ++jj;
+      }
+
+      int tlFreqs[51]{0}, tlDistinct{0};
+      i = 0, jj = j;
+      while (i < grid.size() && jj < grid[i].size()) {
+        const int num{grid[i][jj]};
+        if (--brFreqs[num] == 0) --brDistinct;
+        grid[i][jj] = std::abs(tlDistinct - brDistinct);
+        if (++tlFreqs[num] == 1) ++tlDistinct;
+        ++i, ++jj;
+      }
+    }
+
     return grid;
   }
 };
