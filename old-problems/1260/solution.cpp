16063610cb0b88663f5b9e872669c639c91295ee
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-15 15:43:54 +0700

    feat(problem-1260/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1260/solution.cpp
+++ old-problems/1260/solution.cpp
@@ -4,7 +4,15 @@ class Solution {
  public:
   std::vector<std::vector<int>> shiftGrid(
       std::vector<std::vector<int>>& grid, int k) {
-    (void)k;
-    return grid;
+    std::vector<std::vector<int>> out{grid};
+
+    const std::size_t n{grid[0].size()};
+    const std::size_t mn{grid.size() * n};
+    for (std::size_t i{0}; i < mn; ++i) {
+      const std::size_t j{(i + k) % mn};
+      out[j / n][j % n] = grid[i / n][i % n];
+    }
+
+    return out;
   }
 };
