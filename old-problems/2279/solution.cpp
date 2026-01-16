6df90fd8139cf644de531a56ce0bb51a3876f8c7
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-16 16:16:34 +0700

    feat(problem-2279/cpp): solve by greedily pick bag with smallest capacity to full
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2279/solution.cpp
+++ old-problems/2279/solution.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
@@ -5,6 +6,15 @@ class Solution {
   int maximumBags(
       std::vector<int>& capacity, std::vector<int>& rocks,
       int additionalRocks) {
-    return capacity.size() + rocks.size() + additionalRocks;
+    for (std::size_t i{0}; i < capacity.size(); ++i) capacity[i] -= rocks[i];
+    std::sort(capacity.begin(), capacity.end());
+
+    std::size_t i{0};
+    while (i < capacity.size() && capacity[i] <= additionalRocks) {
+      additionalRocks -= capacity[i];
+      ++i;
+    }
+
+    return i;
   }
 };
