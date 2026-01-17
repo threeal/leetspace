e1299c6d72362c6a238e0b18b60fd4d3af4f807a
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-17 22:46:46 +0700

    feat(problem-917/cpp): solve by using two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0917/solution.cpp
+++ old-problems/0917/solution.cpp
@@ -1,8 +1,29 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   std::string reverseOnlyLetters(std::string s) {
+    std::size_t l{0}, r{s.size() - 1};
+    while (l < r) {
+      while (l < r && !isLetter(s[l])) ++l;
+      if (l >= r) break;
+
+      while (l < r && !isLetter(s[r])) --r;
+      if (l >= r) break;
+
+      std::swap(s[l], s[r]);
+      ++l, --r;
+    }
     return s;
+  }
+
+ private:
+  static bool isLetter(char c) {
+    if (c < 'A') return false;
+    if (c <= 'Z') return true;
+    if (c < 'a') return false;
+    if (c <= 'z') return true;
+    return false;
   }
 };
