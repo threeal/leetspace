521325168bc76cfc6b7fbb8b41b26cd55be8fa48
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-17 22:55:12 +0700

    feat(problem-901/cpp): solve by using a monotonic stack
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0901/solution.cpp
+++ old-problems/0901/solution.cpp
@@ -1,8 +1,24 @@
+#include <stack>
+
+struct Stock {
+  int price;
+  int count;
+};
+
 class StockSpanner {
+ private:
+  std::stack<Stock> stocks;
+
  public:
-  StockSpanner() {}
+  StockSpanner() : stocks{} {}
 
   int next(int price) {
-    return price;
+    int count{1};
+    while (!stocks.empty() && stocks.top().price <= price) {
+      count += stocks.top().count;
+      stocks.pop();
+    }
+    stocks.push({.price = price, .count = count});
+    return count;
   }
 };
