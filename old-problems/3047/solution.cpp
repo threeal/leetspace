895ca67ba58c73ee2136d4beb6aa6fc25c87e967
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-17 22:20:01 +0700

    feat(problem-3047/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3047/solution.cpp
+++ old-problems/3047/solution.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
@@ -5,6 +6,21 @@ class Solution {
   long long largestSquareArea(
       std::vector<std::vector<int>>& bottomLeft,
       std::vector<std::vector<int>>& topRight) {
-    return bottomLeft.size() + topRight.size();
+    int maxSide{0};
+    for (std::size_t i{0}; i < bottomLeft.size(); ++i) {
+      for (std::size_t j{i + 1}; j < topRight.size(); ++j) {
+        const int width{
+            std::min(topRight[i][0], topRight[j][0]) -
+            std::max(bottomLeft[i][0], bottomLeft[j][0])};
+
+        const int height{
+            std::min(topRight[i][1], topRight[j][1]) -
+            std::max(bottomLeft[i][1], bottomLeft[j][1])};
+
+        const int side{std::min(width, height)};
+        if (side > maxSide) maxSide = side;
+      }
+    }
+    return static_cast<long long>(maxSide) * maxSide;
   }
 };
