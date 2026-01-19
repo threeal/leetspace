0321f370fc51b312fc7b95c01f4fc076b15256fd
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-19 14:04:31 +0700

    feat(problem-2350/c): solve by count window that contains all dice sides
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2350/c/solution.c
+++ old-problems/2350/c/solution.c
@@ -1,3 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+
 int shortestSequence(int* rolls, int rollsSize, int k) {
-  return rolls[rollsSize - 1] + k;
+  const int existsSize = (k + 1) / 8 + 1;
+  char* exists = malloc(existsSize * sizeof(char));
+  memset(exists, 0, existsSize * sizeof(char));
+
+  int existCount = 0;
+  int shortest = 1;
+  for (int i = 0; i < rollsSize; ++i) {
+    if ((exists[rolls[i] / 8] & (1 << (rolls[i] % 8))) == 0) {
+      if (++existCount == k) {
+        ++shortest;
+        memset(exists, 0, existsSize * sizeof(char));
+        existCount = 0;
+      } else {
+        exists[rolls[i] / 8] |= 1 << (rolls[i] % 8);
+      }
+    }
+  }
+
+  return shortest;
 }
