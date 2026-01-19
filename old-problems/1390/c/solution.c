c063b427d4efc744b7cc5a870fff03803bb7194c
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-19 20:00:40 +0700

    feat(problem-1390/c): solve by checking all possible factors of each number
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1390/c/solution.c
+++ old-problems/1390/c/solution.c
@@ -1,3 +1,18 @@
 int sumFourDivisors(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int sum = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    int a = 2;
+    while (a * a < nums[i] && nums[i] % a != 0) ++a;
+
+    if (a * a < nums[i]) {
+      int b = a + 1;
+      while (b * b < nums[i] && nums[i] % b != 0) ++b;
+
+      if (b * b > nums[i]) {
+        sum += 1 + a + nums[i] / a + nums[i];
+      }
+    }
+  }
+
+  return sum;
 }
