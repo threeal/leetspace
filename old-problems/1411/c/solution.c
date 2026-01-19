4dee2c02b8a8dd2b354841f35b2af19f4160d1b6
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-19 13:15:28 +0700

    feat(problem-1411/c): solve by using a bottom up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1411/c/solution.c
+++ old-problems/1411/c/solution.c
@@ -1,3 +1,9 @@
 int numOfWays(int n) {
-  return n;
+  long long a = 6, b = 6;
+  while (--n != 0) {
+    const long long prevA = a, prevB = b;
+    a = (2 * prevA + 2 * prevB) % 1000000007;
+    b = (2 * prevA + 3 * prevB) % 1000000007;
+  }
+  return (a + b) % 1000000007;
 }
