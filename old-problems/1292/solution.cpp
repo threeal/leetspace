bbfd9fa98b1ea9f6727407b7ad2b297a3a37111b
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-19 14:23:18 +0700

    feat(problem-1292/cpp): solve by using binary search and prefix sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1292/solution.cpp
+++ old-problems/1292/solution.cpp
@@ -10,10 +10,16 @@ class Solution {
       }
     }
 
+    for (std::size_t i{1}; i < mat.size(); ++i) {
+      for (std::size_t j{0}; j < mat[i].size(); ++j) {
+        mat[i][j] += mat[i - 1][j];
+      }
+    }
+
     std::size_t low{0}, high{std::min(mat.size(), mat[0].size())};
     while (low < high) {
-      const std::size_t mid{high - (high - mid) / 2};
-      if (checkSideLength(mat, threshold, mid)) {
+      const std::size_t mid{high - (high - low) / 2};
+      if (mid == 0 || checkSideLength(mat, threshold, mid)) {
         low = mid;
       } else {
         high = mid - 1;
@@ -26,5 +32,20 @@ class Solution {
  private:
   static bool checkSideLength(
       const std::vector<std::vector<int>>& mat, int threshold, std::size_t n) {
+    if (mat[n - 1][n - 1] <= threshold) return true;
+    for (std::size_t i{n}; i < mat[0].size(); ++i) {
+      if (mat[n - 1][i] - mat[n - 1][i - n] <= threshold) return true;
+    }
+
+    for (std::size_t i{n}; i < mat.size(); ++i) {
+      if (mat[i][n - 1] - mat[i - n][n - 1] <= threshold) return true;
+      for (std::size_t j{n}; j < mat[i].size(); ++j) {
+        const int sum{
+            mat[i][j] - mat[i - n][j] - mat[i][j - n] + mat[i - n][j - n]};
+        if (sum <= threshold) return true;
+      }
+    }
+
+    return false;
   }
 };
