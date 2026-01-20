1ff2fb381f42d0cc75f7d1743abc0ca774ff71bf
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-20 15:46:13 +0700

    feat(problem-3746/c): solve by comparing frequencies of `a` and `b`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3746/c/solution.c
+++ old-problems/3746/c/solution.c
@@ -1,3 +1,9 @@
 int minLengthAfterRemovals(char* s) {
-  return *s;
+  int a = 0, n = 0;
+  while (s[n] != 0) {
+    if (s[n] == 'a') ++a;
+    ++n;
+  }
+  a *= 2;
+  return a >= n ? a - n : n - a;
 }
