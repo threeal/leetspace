83cc860a931b7ea656ba74c64e74659c27e3442f
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-20 12:05:52 +0700

    test(problem-3653): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3653/c/solution.c
@@ -0,0 +1,6 @@
+int xorAfterQueries(
+    int* nums, int numsSize,
+    int** queries, int queriesSize, int* queriesColSize) {
+  return nums[numsSize - 1] +
+      queries[queriesSize - 1][queriesColSize[queriesSize - 1] - 1];
+}
