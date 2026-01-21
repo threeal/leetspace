86c8713d7ebcf3c9e68f79f799b7909c538e2f37
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-21 17:27:44 +0700

    feat(problem-3536/c): solve by returning the product of two largest digits
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3536/c/solution.c
+++ old-problems/3536/c/solution.c
@@ -1,3 +1,13 @@
 int maxProduct(int n) {
-  return n;
+  int a = 0, b = 0;
+  while (n != 0) {
+    if (n % 10 >= a) {
+      b = a;
+      a = n % 10;
+    } else if (n % 10 > b) {
+      b = n % 10;
+    }
+    n /= 10;
+  }
+  return a * b;
 }
