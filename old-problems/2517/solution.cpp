83227571f200d90dc5ea9fada904ff254f99656c
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-21 16:28:15 +0700

    feat(problem-2517/cpp): solve by using sorting and binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2517/solution.cpp
+++ old-problems/2517/solution.cpp
@@ -1,8 +1,32 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maximumTastiness(std::vector<int>& price, int k) {
-    return price.size() + k;
+    std::sort(price.begin(), price.end());
+
+    int low{0}, high{price.back() - price.front()};
+    while (low < high) {
+      const int mid{high - (high - low) / 2};
+
+      int kk{1};
+      std::size_t i{0};
+      while (kk < k) {
+        std::size_t j{i + 1};
+        while (j < price.size() && price[j] - price[i] < mid) ++j;
+        if (j == price.size()) break;
+        i = j;
+        ++kk;
+      }
+
+      if (kk == k) {
+        low = mid;
+      } else {
+        high = mid - 1;
+      }
+    }
+
+    return low;
   }
 };
