0b7b159bc3bc7db5f23c80a57e09557fb694422c
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-22 13:23:00 +0800

    feat: solve problem 1161. Maximum Level Sum of a Binary Tree (#4962)
    * test: add test for problem 1161. Maximum Level Sum of a Binary Tree
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * test(problem-1161): add test case 38
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(problem-1161/cpp): solve by using breadth-first search
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1161/test.cpp
@@ -0,0 +1,60 @@
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+// clang-format off
+#include "solution.cpp"
+// clang-format on
+
+#include <catch2/catch_test_macros.hpp>
+#include <optional>
+#include <queue>
+#include <vector>
+
+const auto null = std::nullopt;
+
+TreeNode* toTreeNode(const std::vector<std::optional<int>>& list) {
+  if (list.empty()) return nullptr;
+
+  TreeNode* root{new TreeNode(*list[0])};
+
+  std::queue<TreeNode**> children{};
+  children.push(&(root->left));
+  children.push(&(root->right));
+
+  for (std::size_t i{1}; i < list.size(); ++i) {
+    if (list[i]) {
+      TreeNode* node{new TreeNode(*list[i])};
+      *children.front() = node;
+      children.push(&(node->left));
+      children.push(&(node->right));
+    }
+    children.pop();
+  }
+
+  return root;
+}
+
+TEST_CASE("1161. Maximum Level Sum of a Binary Tree") {
+  SECTION("Example 1") {
+    TreeNode* root{toTreeNode({1, 7, 0, 7, -8, null, null})};
+    REQUIRE(Solution{}.maxLevelSum(root) == 2);
+  }
+
+  SECTION("Example 2") {
+    TreeNode* root{toTreeNode({989, null, 10250, 98693, -89388, null, null, null, -32127})};
+    REQUIRE(Solution{}.maxLevelSum(root) == 2);
+  }
+
+  SECTION("Test Case 38") {
+    TreeNode* root{toTreeNode({-100, -200, -300, -20, -5, -10, null})};
+    REQUIRE(Solution{}.maxLevelSum(root) == 3);
+  }
+}
