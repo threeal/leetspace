716242dbc7443de52668e94d59b5af91982798ba
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-22 17:47:11 +0700

    feat(problem-2136/c): solve by greedily plant flowers with longest growth time
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2136/c/solution.c
+++ old-problems/2136/c/solution.c
@@ -1,4 +1,28 @@
+#include <stdlib.h>
+
+int* gGrowTime;
+int compare(const void* a, const void* b) {
+  return gGrowTime[*(int*)b] - gGrowTime[*(int*)a];
+}
+
 int earliestFullBloom(
     int* plantTime, int plantTimeSize, int* growTime, int growTimeSize) {
-  return plantTime[plantTimeSize - 1] + growTime[growTimeSize - 1];
+  (void)growTimeSize;
+
+  int* indices = malloc(plantTimeSize * sizeof(int));
+  for (int i = 0; i < plantTimeSize; ++i) indices[i] = i;
+
+  gGrowTime = growTime;
+  qsort(indices, plantTimeSize, sizeof(int), compare);
+
+  int day = 0;
+  int maxBloomDay = 0;
+  for (int i = 0; i < plantTimeSize; ++i) {
+    day += plantTime[indices[i]];
+    if (day + growTime[indices[i]] > maxBloomDay) {
+      maxBloomDay = day + growTime[indices[i]];
+    }
+  }
+
+  return maxBloomDay;
 }
