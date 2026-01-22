d93618fc6f3ce62e7826d4491e5e52ae16500690
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-22 12:20:33 +0700

    feat(problem-3567/cpp): solve by using while-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3507/solution.cpp
+++ old-problems/3507/solution.cpp
@@ -1,8 +1,35 @@
+#include <limits>
 #include <vector>
 
 class Solution {
  public:
   int minimumPairRemoval(std::vector<int>& nums) {
-    return nums.size();
+    const std::size_t prevSize{nums.size()};
+    while (nums.size() > 1) {
+      int minSum{std::numeric_limits<int>::max()};
+      std::size_t i{1}, minI{0};
+      while (i < nums.size() && nums[i] >= nums[i - 1]) {
+        if (nums[i] + nums[i - 1] < minSum) {
+          minSum = nums[i] + nums[i - 1];
+          minI = i;
+        }
+        ++i;
+      }
+
+      if (i == nums.size()) break;
+
+      while (i < nums.size()) {
+        if (nums[i] + nums[i - 1] < minSum) {
+          minSum = nums[i] + nums[i - 1];
+          minI = i;
+        }
+        ++i;
+      }
+
+      nums[minI - 1] = minSum;
+      nums.erase(nums.begin() + minI);
+    }
+
+    return prevSize - nums.size();
   }
 };
