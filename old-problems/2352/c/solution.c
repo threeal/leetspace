bc5ca1810c5e30a95a39226a9fe3048216820fea
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-23 10:34:24 +0700

    feat(problem-2452/cpp): solve by using sorting and binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2352/c/solution.c
+++ old-problems/2352/c/solution.c
@@ -1,3 +1,65 @@
+#include <stdlib.h>
+
+int gColSize;
+int compare(const void* a, const void* b) {
+  int* aa = *(int**)a;
+  int* bb = *(int**)b;
+  for (int i = 0; i < gColSize; ++i) {
+    if (aa[i] != bb[i]) return aa[i] - bb[i];
+  }
+  return 0;
+}
+
 int equalPairs(int** grid, int gridSize, int* gridColSize) {
-  return grid[gridSize - 1][gridColSize[gridSize - 1] - 1];
+  int** rows = malloc(gridSize * sizeof(int*));
+  for (int i = 0; i < gridSize; ++i) rows[i] = grid[i];
+
+  gColSize = gridColSize[0];
+  qsort(rows, gridSize, sizeof(int*), compare);
+
+  int rowsSize = 1;
+  int* rowFreqs = malloc(gridSize * sizeof(int));
+  rowFreqs[0] = 1;
+
+  for (int i = 1; i < gridSize; ++i) {
+    if (compare(rows + rowsSize - 1, rows + i) == 0) {
+      ++rowFreqs[rowsSize - 1];
+    } else {
+      rows[rowsSize] = rows[i];
+      rowFreqs[rowsSize] = 1;
+      ++rowsSize;
+    }
+  }
+
+  int count = 0;
+  int* column = malloc(gridSize * sizeof(int));
+  for (int i = 0; i < gridSize; ++i) {
+    for (int j = 0; j < gridSize; ++j) {
+      column[j] = grid[j][i];
+    }
+
+    int low = 0, high = rowsSize - 1;
+    while (low < high) {
+      const int mid = low + (high - low) / 2;
+      int res = compare(rows + mid, &column);
+      if (res < 0) {
+        low = mid + 1;
+      } else if (res > 0) {
+        high = mid;
+      } else {
+        count += rowFreqs[mid];
+        break;
+      }
+    }
+
+    if (low >= high && compare(rows + low, &column) == 0) {
+      count += rowFreqs[low];
+    }
+  }
+
+  free(rows);
+  free(rowFreqs);
+  free(column);
+
+  return count;
 }
