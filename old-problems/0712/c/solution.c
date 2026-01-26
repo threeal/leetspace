0958e163a68dd4e59536276bcd0581d413513b71
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-26 21:04:17 +0700

    feat(problem-712/c): solve by using a bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0712/c/solution.c
+++ old-problems/0712/c/solution.c
@@ -1,3 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+
 int minimumDeleteSum(char* s1, char* s2) {
-  return *s1 + *s2;
+  const int s1len = strlen(s1);
+  const int s2len = strlen(s2);
+
+  int* dp = malloc((s2len + 1) * sizeof(int));
+  memset(dp, 0, (s2len + 1) * sizeof(int));
+
+  for (int i = 0; i < s1len; ++i) {
+    int prevTl = 0;
+    for (int j = 0; j < s2len; ++j) {
+      if (s1[i] == s2[j]) {
+        const int tempPrevTl = prevTl;
+        prevTl = dp[j + 1];
+        dp[j + 1] = tempPrevTl + s1[i];
+      } else {
+        prevTl = dp[j + 1];
+        if (dp[j] > dp[j + 1]) dp[j + 1] = dp[j];
+      }
+    }
+  }
+
+  int sum = 0;
+  for (int i = 0; i < s1len; ++i) sum += s1[i];
+  for (int i = 0; i < s2len; ++i) sum += s2[i];
+
+  return sum - 2 * dp[s2len];
 }
