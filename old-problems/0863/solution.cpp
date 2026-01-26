debbf6577cf4f815ada65e7020c35b0761198194
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-26 11:06:09 +0700

    test: add test for problem 863. All Nodes Distance K in Binary Tree
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0863/solution.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+    (void)root;
+    (void)target;
+    (void)k;
+    return {};
+  }
+};
