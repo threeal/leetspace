96038341e8d03c57d0e26b6f5893d529b488b096
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-26 12:07:21 +0700

    feat(problem-669/cpp): solve by using depth-first search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0669/solution.cpp
+++ old-problems/0669/solution.cpp
@@ -1,8 +1,26 @@
 class Solution {
  public:
   TreeNode* trimBST(TreeNode* root, int low, int high) {
-    (void)low;
-    (void)high;
-    return root;
+    if (root->val >= low) {
+      if (root->val <= high) {
+        if (root->left != nullptr) {
+          root->left = trimBST(root->left, low, high);
+        }
+
+        if (root->right != nullptr) {
+          root->right = trimBST(root->right, low, high);
+        }
+
+        return root;
+      } else {
+        return root->left != nullptr
+            ? trimBST(root->left, low, high)
+            : nullptr;
+      }
+    } else {
+      return root->right != nullptr
+          ? trimBST(root->right, low, high)
+          : nullptr;
+    }
   }
 };
