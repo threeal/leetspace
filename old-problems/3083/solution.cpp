62d7ef477bbfd05ac0336daa4e612c5c26078b3d
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-26 14:54:11 +0700

    feat(problem-3083/cpp): solve by using map of bit flags
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3083/solution.cpp
+++ old-problems/3083/solution.cpp
@@ -1,8 +1,18 @@
+#include <cstdint>
 #include <string>
 
 class Solution {
  public:
   bool isSubstringPresent(std::string s) {
-    return s.empty();
+    std::uint32_t exists[26]{0};
+    for (std::size_t i{1}; i < s.size(); ++i) {
+      exists[s[i] - 'a'] |= 1 << (s[i - 1] - 'a');
+    }
+
+    for (std::size_t i{1}; i < s.size(); ++i) {
+      if ((exists[s[i - 1] - 'a'] & (1 << (s[i] - 'a'))) != 0) return true;
+    }
+
+    return false;
   }
 };
