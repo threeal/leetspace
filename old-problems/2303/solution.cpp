cb7557de5408ed59d5850d2372bcf6a3dddae516
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-26 21:07:26 +0700

    feat(problem-2303/cpp): solve by calculating each bracket
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2303/solution.cpp
+++ old-problems/2303/solution.cpp
@@ -3,6 +3,26 @@
 class Solution {
  public:
   double calculateTax(std::vector<std::vector<int>>& brackets, int income) {
-    return brackets.size() + income;
+    double tax{0};
+
+    if (income > 0) {
+      if (income <= brackets[0][0]) {
+        tax += static_cast<double>(income) * brackets[0][1] / 100;
+      } else {
+        tax += static_cast<double>(brackets[0][0]) * brackets[0][1] / 100;
+
+        std::size_t i{1};
+        while (brackets[i][0] < income) {
+          tax += static_cast<double>(brackets[i][0] - brackets[i - 1][0]) *
+              brackets[i][1] / 100;
+          ++i;
+        }
+
+        tax += static_cast<double>(income - brackets[i - 1][0]) *
+            brackets[i][1] / 100;
+      }
+    }
+
+    return tax;
   }
 };
