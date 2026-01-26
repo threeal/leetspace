c8437064348fdfca669755e4d648fc0a835c758f
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-26 21:09:05 +0700

    test: add test for problem 985. Sum of Even Numbers After Queries
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0985/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> sumEvenAfterQueries(
+      std::vector<int>& nums, std::vector<std::vector<int>>& queries) {
+    (void)queries;
+    return nums;
+  }
+};
