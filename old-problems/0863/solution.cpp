d35e03a26b6edf8eb850d092f8b2ee8ab06c9bf8
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-26 11:25:33 +0700

    feat(problem-863/cpp): solve by using depth-first search approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0863/solution.cpp
+++ old-problems/0863/solution.cpp
@@ -3,9 +3,53 @@
 class Solution {
  public:
   std::vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-    (void)root;
-    (void)target;
-    (void)k;
-    return {};
+    std::vector<int> output{};
+    findTarget(output, root, target, k);
+    return output;
+  }
+
+ private:
+  int findTarget(
+      std::vector<int>& output, TreeNode* node, TreeNode* target, int k) {
+    if (node == target) {
+      findNodes(output, node, k);
+      return k - 1;
+    } else {
+      if (node->left != nullptr) {
+        const int res{findTarget(output, node->left, target, k)};
+        if (res != -1) {
+          if (res == 0) {
+            output.push_back(node->val);
+          } else if (node->right != nullptr) {
+            findNodes(output, node->right, res - 1);
+          }
+          return res - 1;
+        }
+      }
+
+      if (node->right != nullptr) {
+        const int res{findTarget(output, node->right, target, k)};
+        if (res != -1) {
+          if (res == 0) {
+            output.push_back(node->val);
+          } else if (node->left != nullptr) {
+            findNodes(output, node->left, res - 1);
+          }
+          return res - 1;
+        }
+      }
+
+      return -1;
+    }
+  }
+
+ private:
+  void findNodes(std::vector<int>& output, TreeNode* node, int k) {
+    if (k == 0) {
+      output.push_back(node->val);
+    } else {
+      if (node->left != nullptr) findNodes(output, node->left, k - 1);
+      if (node->right != nullptr) findNodes(output, node->right, k - 1);
+    }
   }
 };
