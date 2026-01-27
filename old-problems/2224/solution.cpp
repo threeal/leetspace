6cbe7dae76c2be154623760e5c1bb819d9df83c3
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-27 09:46:02 +0700

    feat(problem-2224/cpp): solve by parsing then using math
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2224/solution.cpp
+++ old-problems/2224/solution.cpp
@@ -3,6 +3,9 @@
 class Solution {
  public:
   int convertTime(std::string current, std::string correct) {
-    return current.size() + correct.size();
+    const int seconds{
+        (correct[0] * 600 + correct[1] * 60 + correct[3] * 10 + correct[4]) -
+        (current[0] * 600 + current[1] * 60 + current[3] * 10 + current[4])};
+    return seconds / 60 + seconds % 60 / 15 + seconds % 15 / 5 + seconds % 5;
   }
 };
