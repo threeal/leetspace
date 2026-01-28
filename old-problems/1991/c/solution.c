c682298bd46e90617155a5c51193205b5f0793c8
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-28 13:53:27 +0700

    feat(problem-1991/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1991/c/solution.c
+++ old-problems/1991/c/solution.c
@@ -1,3 +1,13 @@
 int findMiddleIndex(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int rightSum = 0;
+  for (int i = 0; i < numsSize; ++i) rightSum += nums[i];
+
+  int leftSum = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    rightSum -= nums[i];
+    if (leftSum == rightSum) return i;
+    leftSum += nums[i];
+  }
+
+  return -1;
 }
