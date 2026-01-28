ff89e7d355fb7278a2baf3e3c291801bdebf42aa
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-28 13:34:14 +0700

    feat(problem-701/cpp): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0701/solution.cpp
+++ old-problems/0701/solution.cpp
@@ -1,7 +1,25 @@
 class Solution {
  public:
   TreeNode* insertIntoBST(TreeNode* root, int val) {
-    root->val = val;
+    if (root == nullptr) return new TreeNode(val);
+    insert(root, val);
     return root;
+  }
+
+ private:
+  void insert(TreeNode* node, int val) {
+    if (val < node->val) {
+      if (node->left != nullptr) {
+        insert(node->left, val);
+      } else {
+        node->left = new TreeNode(val);
+      }
+    } else {
+      if (node->right != nullptr) {
+        insert(node->right, val);
+      } else {
+        node->right = new TreeNode(val);
+      }
+    }
   }
 };
