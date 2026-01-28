78afdd848cc08d0c8ab09187e75c94baef2164af
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-28 13:46:01 +0700

    feat(problem-3033/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3033/c/solution.c
+++ old-problems/3033/c/solution.c
@@ -1,6 +1,17 @@
 int** modifiedMatrix(
     int** matrix, int matrixSize, int* matrixColSize,
     int* returnSize, int** returnColumnSizes) {
+  for (int i = 0; i < matrixColSize[0]; ++i) {
+    int maxValue = 0;
+    for (int j = 0; j < matrixSize; ++j) {
+      if (matrix[j][i] > maxValue) maxValue = matrix[j][i];
+    }
+
+    for (int j = 0; j < matrixSize; ++j) {
+      if (matrix[j][i] == -1) matrix[j][i] = maxValue;
+    }
+  }
+
   *returnSize = matrixSize;
   *returnColumnSizes = matrixColSize;
   return matrix;
