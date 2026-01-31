7fa37515e57947d90ea5c8a2e37f5aa77a250305
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-01-31 20:59:25 +0700

    feat(problem-744/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0744/solution.cpp
+++ old-problems/0744/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   char nextGreatestLetter(std::vector<char>& letters, char target) {
-    return letters.front() + target;
+    char res{127};
+    for (char c : letters) {
+      if (c > target && c < res) res = c;
+    }
+
+    return res == 127 ? letters[0] : res;
   }
 };
