e0b5c464c8f632efae6adfe8cef1e40ebf2ad155
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-01 20:45:17 +0700

    feat(problem-3010/cpp): solve by finding two smallest elements after the first element
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3010/solution.cpp
+++ old-problems/3010/solution.cpp
@@ -3,6 +3,16 @@
 class Solution {
  public:
   int minimumCost(std::vector<int>& nums) {
-    return nums.size();
+    int a{nums[1] < nums[2] ? nums[1] : nums[2]};
+    int b{nums[1] < nums[2] ? nums[2] : nums[1]};
+    for (std::size_t i{3}; i < nums.size(); ++i) {
+      if (nums[i] < a) {
+        b = a;
+        a = nums[i];
+      } else if (nums[i] < b) {
+        b = nums[i];
+      }
+    }
+    return nums[0] + a + b;
   }
 };
