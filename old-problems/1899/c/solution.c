a4792b4427ddddabe50f30d9500bda3ab9b57db3
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-04 11:32:32 +0700

    feat(problem-1899/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1899/c/solution.c
+++ old-problems/1899/c/solution.c
@@ -3,6 +3,17 @@
 bool mergeTriplets(
     int** triplets, int tripletsSize, int* tripletsColSize,
     int* target, int targetSize) {
-  return triplets[tripletsSize - 1][tripletsColSize[tripletsSize - 1] - 1] ==
-      target[targetSize - 1];
+  (void)tripletsColSize;
+  (void)targetSize;
+  int flag = 0;
+  for (int i = 0; i < tripletsSize; ++i) {
+    if (triplets[i][0] <= target[0] && triplets[i][1] <= target[1] &&
+        triplets[i][2] <= target[2]) {
+      if (triplets[i][0] == target[0]) flag |= 1;
+      if (triplets[i][1] == target[1]) flag |= 2;
+      if (triplets[i][2] == target[2]) flag |= 4;
+      if (flag == 7) return true;
+    }
+  }
+  return false;
 }
