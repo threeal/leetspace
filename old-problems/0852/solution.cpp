3ac928a6d75a06c407ddb5e32013aa66f3316d05
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-05 12:47:37 +0700

    feat(problem-852/cpp): solve by using a binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0852/solution.cpp
+++ old-problems/0852/solution.cpp
@@ -3,6 +3,15 @@
 class Solution {
  public:
   int peakIndexInMountainArray(std::vector<int>& arr) {
-    return arr.size();
+    std::size_t low{0}, high{arr.size() - 2};
+    while (low < high) {
+      const std::size_t mid{low + (high - low) / 2};
+      if (arr[mid] < arr[mid + 1]) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+    return low;
   }
 };
