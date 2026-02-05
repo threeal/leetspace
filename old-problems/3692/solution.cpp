b98fa44c97245d7896d304faf897f6195f1b7036
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-05 12:09:57 +0700

    feat(problem-3692/cpp): solve by counting frequency and return majority
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3692/solution.cpp
+++ old-problems/3692/solution.cpp
@@ -3,6 +3,26 @@
 class Solution {
  public:
   std::string majorityFrequencyGroup(std::string s) {
+    int freqs[26]{0};
+    for (char c : s) ++freqs[c - 'a'];
+
+    int freqMembers[101]{0};
+    int majorFreq{0};
+    for (int i{0}; i < 26; ++i) {
+      if (freqs[i] != 0) {
+        ++freqMembers[freqs[i]];
+        if (freqMembers[freqs[i]] > freqMembers[majorFreq] ||
+            (freqMembers[freqs[i]] == freqMembers[majorFreq] && freqs[i] > majorFreq)) {
+          majorFreq = freqs[i];
+        }
+      }
+    }
+
+    s.clear();
+    for (int i{0}; i < 26; ++i) {
+      if (freqs[i] == majorFreq) s += 'a' + i;
+    }
+
     return s;
   }
 };
