baad06a92fcb87e570c9ccca28325d051818f391
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-05 11:54:21 +0700

    feat(problem-171/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0171/solution.cpp
+++ old-problems/0171/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   int titleToNumber(std::string columnTitle) {
-    return columnTitle.size();
+    int num{columnTitle[columnTitle.size() - 1] + 1 - 'A'}, pow{1};
+    for (std::size_t i{1}; i < columnTitle.size(); ++i) {
+      pow *= 26;
+      num += (columnTitle[columnTitle.size() - i - 1] + 1 - 'A') * pow;
+    }
+    return num;
   }
 };
