e102bddadab9e4fdedd677dfd0140af32bd60e93
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-05 10:18:31 +0700

    feat(problem-3379/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3379/solution.cpp
+++ old-problems/3379/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   std::vector<int> constructTransformedArray(std::vector<int>& nums) {
-    return nums;
+    const int n = nums.size();
+    std::vector<int> output(n);
+    for (int i{0}; i < n; ++i) {
+      output[i] = nums[(n + (i + nums[i]) % n) % n];
+    }
+    return output;
   }
 };
