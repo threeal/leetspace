4dd2f6be1a9065451b3ff61e6693a44cc481bf55
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-05 10:29:00 +0700

    test(problem-3643): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3643/c/solution.c
@@ -0,0 +1,10 @@
+int** reverseSubmatrix(
+    int** grid, int gridSize, int* gridColSize, int x, int y, int k,
+    int* returnSize, int** returnColumnSizes) {
+  (void)x;
+  (void)y;
+  (void)k;
+  *returnSize = gridSize;
+  *returnColumnSizes = gridColSize;
+  return grid;
+}
