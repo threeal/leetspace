1e2e68dcad721affc4f864b2a9eb1417ed11f6aa
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-05 12:38:19 +0700

    feat(problem-2908/cpp): solve by keeping track of min numbers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2908/solution.cpp
+++ old-problems/2908/solution.cpp
@@ -1,8 +1,23 @@
+#include <limits>
 #include <vector>
 
 class Solution {
  public:
   int minimumSum(std::vector<int>& nums) {
-    return nums.size();
+    std::vector<int> minNums{nums};
+    for (std::size_t i{nums.size() - 1}; i > 0; --i) {
+      if (minNums[i] < minNums[i - 1]) minNums[i - 1] = minNums[i];
+    }
+
+    int minSum{std::numeric_limits<int>::max()}, minNum{nums[0]};
+    for (std::size_t i{2}; i < nums.size(); ++i) {
+      if (nums[i - 1] > minNum && nums[i - 1] > minNums[i]) {
+        const int sum = minNum + nums[i - 1] + minNums[i];
+        if (sum < minSum) minSum = sum;
+      }
+      if (nums[i - 1] < minNum) minNum = nums[i - 1];
+    }
+
+    return minSum == std::numeric_limits<int>::max() ? -1 : minSum;
   }
 };
