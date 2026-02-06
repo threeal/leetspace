c84c26cc6ea7c3f49051fae43f66927457395382
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-06 22:46:11 +0700

    feat(problem-2279/c): solve by greedily pick bag with smallest capacity to full
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2279/c/solution.c
+++ old-problems/2279/c/solution.c
@@ -1,5 +1,22 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int maximumBags(
     int* capacity, int capacitySize, int* rocks, int rocksSize,
     int additionalRocks) {
-  return capacity[capacitySize - 1] + rocks[rocksSize - 1] + additionalRocks;
+  (void)rocksSize;
+
+  for (int i = 0; i < capacitySize; ++i) capacity[i] -= rocks[i];
+  qsort(capacity, capacitySize, sizeof(int), compare);
+
+  int i = 0;
+  while (i < capacitySize && capacity[i] <= additionalRocks) {
+    additionalRocks -= capacity[i];
+    ++i;
+  }
+
+  return i;
 }
