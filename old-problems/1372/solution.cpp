b32189aa3722c0451e53b9481c5dfe88fe595d37
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-06 11:21:48 +0700

    feat(problem-1372/cpp): solve by using a recursive function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1372/solution.cpp
+++ old-problems/1372/solution.cpp
@@ -1,6 +1,26 @@
+#include <algorithm>
+
 class Solution {
  public:
   int longestZigZag(TreeNode* root) {
-    return root->val;
+    return explore(root, 0, 0);
+  }
+
+  int explore(TreeNode* node, int left, int right) {
+    if (node->left != nullptr) {
+      if (node->right != nullptr) {
+        return std::max(
+            explore(node->left, 1 + right, 0),
+            explore(node->right, 0, 1 + left));
+      } else {
+        return std::max(left, explore(node->left, 1 + right, 0));
+      }
+    } else {
+      if (node->right != nullptr) {
+        return std::max(right, explore(node->right, 0, 1 + left));
+      } else {
+        return std::max(left, right);
+      }
+    }
   }
 };
