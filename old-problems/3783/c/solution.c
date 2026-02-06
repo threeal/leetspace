0a499590506c53de36fd3131fed2b68c4d369d03
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-06 22:37:59 +0700

    feat(problem-3783/c): solve by using modulo and division to reverse digits
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3783/c/solution.c
+++ old-problems/3783/c/solution.c
@@ -1,3 +1,11 @@
+#include <stdlib.h>
+
 int mirrorDistance(int n) {
-  return n;
+  const int original = n;
+  int mirror = 0;
+  while (n != 0) {
+    mirror = mirror * 10 + n % 10;
+    n /= 10;
+  }
+  return abs(original - mirror);
 }
