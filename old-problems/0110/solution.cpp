af2c59896ab6140b0b589730169ad258e1d4ec26
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-08 23:04:02 +0700

    feat(problem-110/cpp): solve by using depth-first search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0110/solution.cpp
+++ old-problems/0110/solution.cpp
@@ -1,6 +1,36 @@
+#include <limits>
+
 class Solution {
  public:
   bool isBalanced(TreeNode* root) {
-    return root != nullptr;
+    if (root == nullptr) return true;
+    return explore(root, 1) != -1;
+  }
+
+ private:
+  static int explore(TreeNode* node, int depth) {
+    if (node->left != nullptr) {
+      if (node->right != nullptr) {
+        const int leftDepth{explore(node->left, depth + 1)};
+        if (leftDepth == -1) return -1;
+
+        const int rightDepth{explore(node->right, depth + 1)};
+        if (rightDepth == -1) return -1;
+
+        return leftDepth > rightDepth
+            ? (leftDepth - rightDepth <= 1 ? leftDepth : -1)
+            : (rightDepth - leftDepth <= 1 ? rightDepth : -1);
+      } else {
+        const int leftDepth{explore(node->left, depth + 1)};
+        return leftDepth != -1 && leftDepth - depth <= 1 ? leftDepth : -1;
+      }
+    } else {
+      if (node->right != nullptr) {
+        const int rightDepth{explore(node->right, depth + 1)};
+        return rightDepth != -1 && rightDepth - depth <= 1 ? rightDepth : -1;
+      } else {
+        return depth;
+      }
+    }
   }
 };
