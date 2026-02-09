d5847377c31c8eb90120a1dc726579420421fad7
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-09 11:33:08 +0700

    feat(problem-973/cpp): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0973/c/solution.c
+++ old-problems/0973/c/solution.c
@@ -1,8 +1,17 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  int* aa = *(int**)a;
+  int* bb = *(int**)b;
+  return (aa[0] * aa[0] + aa[1] * aa[1]) - (bb[0] * bb[0] + bb[1] * bb[1]);
+}
+
 int** kClosest(
     int** points, int pointsSize, int* pointsColSize, int k,
     int* returnSize, int** returnColumnSizes) {
-  (void)k;
-  *returnSize = pointsSize;
+  qsort(points, pointsSize, sizeof(int*), compare);
+
+  *returnSize = k;
   *returnColumnSizes = pointsColSize;
   return points;
 }
