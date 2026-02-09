8cfb3d257c98f92885098aec85537a5a700b7667
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-09 11:30:08 +0700

    test(problem-973): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0973/c/solution.c
@@ -0,0 +1,8 @@
+int** kClosest(
+    int** points, int pointsSize, int* pointsColSize, int k,
+    int* returnSize, int** returnColumnSizes) {
+  (void)k;
+  *returnSize = pointsSize;
+  *returnColumnSizes = pointsColSize;
+  return points;
+}
