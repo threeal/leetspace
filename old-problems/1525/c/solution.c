4413ba61c562a4606eb34360314d68fe91f2615a
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-10 11:20:53 +0700

    feat(problem-1525/c): solve by counting character frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1525/c/solution.c
+++ old-problems/1525/c/solution.c
@@ -1,3 +1,15 @@
 int numSplits(char* s) {
-  return *s;
+  int rFreqs[26] = {0}, rDistinct = 0;
+  for (char* c = s; *c != 0; ++c) {
+    if (++rFreqs[*c - 'a'] == 1) ++rDistinct;
+  }
+
+  int lFreqs[26] = {0}, lDistinct = 0, splits = 0;
+  for (char* c = s; *c != 0; ++c) {
+    if (++lFreqs[*c - 'a'] == 1) ++lDistinct;
+    if (--rFreqs[*c - 'a'] == 0) --rDistinct;
+    if (lDistinct == rDistinct) ++splits;
+  }
+
+  return splits;
 }
