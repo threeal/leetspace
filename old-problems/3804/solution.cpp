817d12180d33bc2399e8cdfe9afdc66814226aad
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-10 19:47:38 +0700

    feat(problem-3804/cpp): solve by using brute-force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3804/solution.cpp
+++ old-problems/3804/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   int centeredSubarrays(std::vector<int>& nums) {
-    return nums.size();
+    int count{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      int sum{0};
+      for (std::size_t j{i}; j < nums.size(); ++j) {
+        sum += nums[j];
+        for (std::size_t k{i}; k <= j; ++k) {
+          if (sum == nums[k]) {
+            ++count;
+            break;
+          }
+        }
+      }
+    }
+    return count;
   }
 };
