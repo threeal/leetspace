1cb634f57dd65948814ca5c9d000554a59aa8e0a
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-10 19:34:32 +0700

    feat(problem-107/cpp): solve by using breadth-first search approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0107/solution.cpp
+++ old-problems/0107/solution.cpp
@@ -1,9 +1,31 @@
+#include <algorithm>
+#include <queue>
 #include <vector>
 
 class Solution {
  public:
   std::vector<std::vector<int>> levelOrderBottom(TreeNode* root) {
-    (void)root;
-    return {};
+    if (root == nullptr) return {};
+
+    std::queue<TreeNode*> nodes{};
+    nodes.push(root);
+
+    std::vector<std::vector<int>> output{};
+    while (!nodes.empty()) {
+      output.push_back({});
+      output.back().reserve(nodes.size());
+      for (std::size_t n{nodes.size()}; n != 0; --n) {
+        TreeNode* node{nodes.front()};
+        nodes.pop();
+
+        if (node->left != nullptr) nodes.push(node->left);
+        if (node->right != nullptr) nodes.push(node->right);
+
+        output.back().push_back(node->val);
+      }
+    }
+
+    std::reverse(output.begin(), output.end());
+    return output;
   }
 };
