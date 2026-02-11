24989b3774c5e6475c1f47fce99cb73429fc9185
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-11 23:02:27 +0700

    test(problem-985): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0985/c/solution.c
@@ -0,0 +1,10 @@
+int* sumEvenAfterQueries(
+    int* nums, int numsSize,
+    int** queries, int queriesSize, int* queriesColSize,
+    int* returnSize) {
+  (void)queries;
+  (void)queriesSize;
+  (void)queriesColSize;
+  *returnSize = numsSize;
+  return nums;
+}
