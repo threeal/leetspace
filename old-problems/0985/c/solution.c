24bd88ae383f733497135a411beb4db2a03fd0a3
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-11 23:03:47 +0700

    feat(problem-985/c): solve by keeping track of the sum while processing each query
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0985/c/solution.c
+++ old-problems/0985/c/solution.c
@@ -1,10 +1,24 @@
+#include <stdlib.h>
+
 int* sumEvenAfterQueries(
     int* nums, int numsSize,
     int** queries, int queriesSize, int* queriesColSize,
     int* returnSize) {
-  (void)queries;
-  (void)queriesSize;
   (void)queriesColSize;
-  *returnSize = numsSize;
-  return nums;
+
+  int sum = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] % 2 == 0) sum += nums[i];
+  }
+
+  int* output = malloc(queriesSize * sizeof(int));
+  for (int i = 0; i < queriesSize; ++i) {
+    if (nums[queries[i][1]] % 2 == 0) sum -= nums[queries[i][1]];
+    nums[queries[i][1]] += queries[i][0];
+    if (nums[queries[i][1]] % 2 == 0) sum += nums[queries[i][1]];
+    output[i] = sum;
+  }
+
+  *returnSize = queriesSize;
+  return output;
 }
