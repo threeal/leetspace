7383b33cd734a5d7a7de4bc941e8d88273680ed5
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-11 22:53:43 +0700

    feat(problem-3622/cpp): solve by iterating digits of the given number
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3622/solution.cpp
+++ old-problems/3622/solution.cpp
@@ -1,6 +1,11 @@
 class Solution {
  public:
   bool checkDivisibility(int n) {
-    return n == 0;
+    int sum{0}, product{1};
+    for (int nn{n}; nn != 0; nn /= 10) {
+      sum += nn % 10;
+      product *= nn % 10;
+    }
+    return n % (sum + product) == 0;
   }
 };
