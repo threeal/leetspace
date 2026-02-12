2d487bcd3c0cde44fe2df8e065b4f3274a0ff556
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-12 14:29:18 +0700

    feat(problem-3453/c): solve by using a binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3453/c/solution.c
+++ old-problems/3453/c/solution.c
@@ -1,3 +1,38 @@
 double separateSquares(int** squares, int squaresSize, int* squaresColSize) {
-  return squares[squaresSize - 1][squaresColSize[squaresSize - 1] - 1];
+  (void)squaresColSize;
+
+  double lowY = squares[0][1];
+  double highY = squares[0][1] + squares[0][2];
+  for (int i = 1; i < squaresSize; ++i) {
+    double low = squares[i][1];
+    if (low < lowY) lowY = low;
+
+    double high = squares[i][1] + squares[i][2];
+    if (high > highY) highY = high;
+  }
+
+  while (highY - lowY >= 0.00001) {
+    const double midY = lowY + (highY - lowY) / 2;
+
+    double lowSum = 0, highSum = 0;
+    for (int i = 0; i < squaresSize; ++i) {
+      const double y = squares[i][1], l = squares[i][2];
+      if (y >= midY) {
+        highSum += l * l;
+      } else if (y + l <= midY) {
+        lowSum += l * l;
+      } else {
+        lowSum += (midY - y) * l;
+        highSum += (y + l - midY) * l;
+      }
+    }
+
+    if (lowSum >= highSum) {
+      highY = midY;
+    } else {
+      lowY = midY;
+    }
+  }
+
+  return lowY;
 }
