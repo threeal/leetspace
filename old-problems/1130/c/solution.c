e7d1b5ea47583db544f9e9d2d579a2d2fa37d0c7
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-15 22:07:57 +0700

    feat(problem-1130/cpp): solve by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1130/c/solution.c
+++ old-problems/1130/c/solution.c
@@ -1,3 +1,46 @@
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct val_cost_t {
+  int val;
+  int cost;
+};
+
 int mctFromLeafValues(int* arr, int arrSize) {
-  return arr[arrSize - 1];
+  struct val_cost_t** values = malloc(arrSize * sizeof(struct val_cost_t*));
+
+  values[0] = malloc(arrSize * sizeof(struct val_cost_t));
+  for (int i = 0; i < arrSize; ++i) {
+    values[0][i].val = arr[i];
+    values[0][i].cost = 0;
+  }
+
+  for (int i = 1; i < arrSize; ++i) {
+    const int n = arrSize - i;
+    values[i] = malloc(n * sizeof(struct val_cost_t));
+    for (int j = 0; j < n; ++j) {
+      int maxVal = 0, minCost = INT_MAX;
+      for (int k = 0; k < i; ++k) {
+        const struct val_cost_t left = values[k][j];
+        const struct val_cost_t right = values[i - k - 1][j + k + 1];
+
+        const int val = left.val >= right.val ? left.val : right.val;
+        if (val > maxVal) maxVal = val;
+
+        const int cost = left.cost + right.cost + left.val * right.val;
+        if (cost < minCost) minCost = cost;
+      }
+
+      values[i][j].val = maxVal;
+      values[i][j].cost = minCost;
+    }
+  }
+
+  const int cost = values[arrSize - 1][0].cost;
+
+  for (int i = 0; i < arrSize; ++i) free(values[i]);
+  free(values);
+
+  return cost;
 }
