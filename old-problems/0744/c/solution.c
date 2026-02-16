7302868afa27e5b4161ef858d0f0cbc224721153
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-16 21:45:36 +0700

    feat(problem-744/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0744/c/solution.c
+++ old-problems/0744/c/solution.c
@@ -1,3 +1,8 @@
 char nextGreatestLetter(char* letters, int lettersSize, char target) {
-  return letters[lettersSize - 1] + target;
+  char res = 127;
+  for (int i = 0; i < lettersSize; ++i) {
+    if (letters[i] > target && letters[i] < res) res = letters[i];
+  }
+
+  return res == 127 ? letters[0] : res;
 }
