e1d2032753661790d444ad0eb399c9b636f26ba2
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-16 08:34:56 +0700

    feat(problem-2517): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2517/c/solution.c
+++ old-problems/2517/c/solution.c
@@ -1,3 +1,31 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int maximumTastiness(int* price, int priceSize, int k) {
-  return price[priceSize - 1] + k;
+  qsort(price, priceSize, sizeof(int), compare);
+
+  int low = 0, high = price[priceSize - 1] - price[0];
+  while (low < high) {
+    const int mid = high - (high - low) / 2;
+
+    int kk = 1, i = 0;
+    while (kk < k) {
+      int j = i + 1;
+      while (j < priceSize && price[j] - price[i] < mid) ++j;
+      if (j == priceSize) break;
+      i = j;
+      ++kk;
+    }
+
+    if (kk == k) {
+      low = mid;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  return low;
 }
