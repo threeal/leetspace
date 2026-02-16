a540c2301d639f8527b4969dafeafc0eb0eb8f87
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-16 09:36:20 +0700

    feat(problem-3823/cpp): solve by using two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3823/solution.cpp
+++ old-problems/3823/solution.cpp
@@ -1,8 +1,27 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   std::string reverseByType(std::string s) {
+    std::size_t l{0}, rc{s.size() - 1}, rs{s.size() - 1};
+    while (l < rc || l < rs) {
+      if (s[l] >= 'a') {
+        while (l < rc && s[rc] < 'a') --rc;
+        if (l < rc) {
+          std::swap(s[l], s[rc]);
+          --rc;
+        }
+      } else {
+        while (l < rs && s[rs] >= 'a') --rs;
+        if (l < rs) {
+          std::swap(s[l], s[rs]);
+          --rs;
+        }
+      }
+      ++l;
+    }
+
     return s;
   }
 };
