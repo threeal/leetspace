7d0d3c92086e6bd6a090b2d56563266b15bf34ae
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-18 09:41:22 +0700

    feat(problem-693/cpp): solve by iterating bits
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0693/solution.cpp
+++ old-problems/0693/solution.cpp
@@ -1,6 +1,9 @@
 class Solution {
  public:
   bool hasAlternatingBits(int n) {
-    return n == 0;
+    for (int i{1}; i < 32 && (1 << i) <= n; ++i) {
+      if (((n >> i) & 1) == ((n >> (i - 1)) & 1)) return false;
+    }
+    return true;
   }
 };
