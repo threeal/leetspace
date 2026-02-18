c02f3bd68c83f19b77b1c95a7e095c4c7c3f0f5c
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-18 13:38:18 +0700

    feat(problem-2943/c): solve by using sorting and counting consecutive numbers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2943/c/solution.c
+++ old-problems/2943/c/solution.c
@@ -1,4 +1,35 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int maximizeSquareHoleArea(
     int n, int m, int* hBars, int hBarsSize, int* vBars, int vBarsSize) {
-  return n + m + hBars[hBarsSize - 1] + vBars[vBarsSize - 1];
+  (void)n;
+  (void)m;
+
+  qsort(hBars, hBarsSize, sizeof(int), compare);
+  qsort(vBars, vBarsSize, sizeof(int), compare);
+
+  int hMaxCount = 1, hCount = 1;
+  for (int i = 1; i < hBarsSize; ++i) {
+    if (hBars[i] == hBars[i - 1] + 1) {
+      if (++hCount > hMaxCount) hMaxCount = hCount;
+    } else {
+      hCount = 1;
+    }
+  }
+
+  int vMaxCount = 1, vCount = 1;
+  for (int i = 1; i < vBarsSize; ++i) {
+    if (vBars[i] == vBars[i - 1] + 1) {
+      if (++vCount > vMaxCount) vMaxCount = vCount;
+    } else {
+      vCount = 1;
+    }
+  }
+
+  int maxCount = 1 + (hMaxCount <= vMaxCount ? hMaxCount : vMaxCount);
+  return maxCount * maxCount;
 }
