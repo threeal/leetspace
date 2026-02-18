f0e9755605733bd910150c462d56c04dc12b9075
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-18 09:27:29 +0700

    feat(problem-401/cpp): solve by iterating permutation of bits
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0401/solution.cpp
+++ old-problems/0401/solution.cpp
@@ -4,6 +4,33 @@
 class Solution {
  public:
   std::vector<std::string> readBinaryWatch(int turnedOn) {
-    return std::vector<std::string>(turnedOn);
+    if (turnedOn == 0) return {"0:00"};
+    if (turnedOn > 8) return {};
+
+    std::vector<std::string> out{};
+    int q{(1 << turnedOn) - 1};
+    const int maxQ{1 << 10};
+    while (q < maxQ) {
+      const int h{q >> 6};
+      const int m{q & 0b111111};
+      if (h < 12 && m < 60) {
+        std::string str(h >= 10 ? 5 : 4, ' ');
+
+        std::size_t i{0};
+        if (h >= 10) str[i++] = '0' + h / 10;
+        str[i++] = '0' + h % 10;
+        str[i++] = ':';
+        str[i++] = '0' + m / 10;
+        str[i++] = '0' + m % 10;
+
+        out.push_back(std::move(str));
+      }
+
+      const int r{q & -q};
+      const int n{q + r};
+      q = (((n ^ q) >> 2) / r) | n;
+    }
+
+    return out;
   }
 };
