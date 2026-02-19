fc355722762bda492319a4be0b09e4b3d1bfcad3
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-19 17:09:11 +0700

    feat(problem-2129/c): solve by parsing words from the given string
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2129/c/solution.c
+++ old-problems/2129/c/solution.c
@@ -1,3 +1,20 @@
 char* capitalizeTitle(char* title) {
+  int r = 0;
+  while (1) {
+    const int l = r++;
+    while (title[r] != 0 && title[r] != ' ') {
+      if (title[r] < 'a') title[r] += 'a' - 'A';
+      ++r;
+    }
+
+    if (r - l > 2) {
+      if (title[l] >= 'a') title[l] -= 'a' - 'A';
+    } else {
+      if (title[l] < 'a') title[l] += 'a' - 'A';
+    }
+
+    if (title[r] == 0) break;
+    ++r;
+  }
   return title;
 }
