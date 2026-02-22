127602d264b2802880a3920a0f79b4fba43d8c91
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-22 22:40:11 +0700

    feat(problem-868/cpp): solve by using bit manipulation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0868/solution.cpp
+++ old-problems/0868/solution.cpp
@@ -1,6 +1,18 @@
+#include <bit>
+
 class Solution {
  public:
   int binaryGap(int n) {
-    return n;
+    unsigned int nn = n;
+    nn >>= std::countr_zero(nn) + 1;
+
+    int maxGap{0};
+    while (nn != 0) {
+      const int gap{std::countr_zero(nn) + 1};
+      if (gap >= maxGap) maxGap = gap;
+      nn >>= gap;
+    }
+
+    return maxGap;
   }
 };
