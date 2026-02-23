ae27ada745ef0f8ab9783cfc84a2ef4d8d0ac815
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-23 22:36:40 +0700

    feat(problem-171/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0171/c/solution.c
+++ old-problems/0171/c/solution.c
@@ -1,3 +1,11 @@
+#include <string.h>
+
 int titleToNumber(char* columnTitle) {
-  return *columnTitle;
+  const int columnTitleLen = strlen(columnTitle);
+  int num = columnTitle[columnTitleLen - 1] + 1 - 'A', pow = 1;
+  for (int i = 1; i < columnTitleLen; ++i) {
+    pow *= 26;
+    num += (columnTitle[columnTitleLen - i - 1] + 1 - 'A') * pow;
+  }
+  return num;
 }
