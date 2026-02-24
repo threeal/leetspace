cf278650182ebcfccfe9ba95d915f8ec2b3c8cf4
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-24 17:23:08 +0700

    feat(problem-932/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0932/solution.cpp
+++ old-problems/0932/solution.cpp
@@ -1,8 +1,29 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> beautifulArray(int n) {
-    return std::vector<int>(n);
+    std::vector<int> res{};
+    res.reserve(n);
+    res.push_back(1);
+
+    std::vector<int> prev{};
+    prev.reserve(n);
+
+    for (int i{2}; i <= n; ++i) {
+      std::swap(prev, res);
+      res.clear();
+
+      for (int num : prev) {
+        if (num * 2 - 1 <= i) res.push_back(num * 2 - 1);
+      }
+
+      for (int num : prev) {
+        if (num * 2 <= i) res.push_back(num * 2);
+      }
+    }
+
+    return res;
   }
 };
