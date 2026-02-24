64ba72741a4455923a43f2f90ea4ee85338012f3
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-24 17:34:39 +0700

    feat(problem-3637/c): solve by using while-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3637/c/solution.c
+++ old-problems/3637/c/solution.c
@@ -1,5 +1,15 @@
 #include <stdbool.h>
 
 bool isTrionic(int* nums, int numsSize) {
-  return nums[numsSize - 1] == 0;
+  if (nums[1] <= nums[0]) return false;
+
+  int i = 2;
+  while (i < numsSize && nums[i - 1] < nums[i]) ++i;
+  if (i == numsSize || nums[i - 1] == nums[i]) return false;
+
+  while (i < numsSize && nums[i - 1] > nums[i]) ++i;
+  if (i == numsSize || nums[i - 1] == nums[i]) return false;
+
+  while (i < numsSize && nums[i - 1] < nums[i]) ++i;
+  return i == numsSize;
 }
