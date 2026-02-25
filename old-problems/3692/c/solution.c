15bee99fb69c00e873abc60584a23d6587490376
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-25 15:43:57 +0700

    feat(problem-3692/c): solve by counting frequency and return majority
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3692/c/solution.c
+++ old-problems/3692/c/solution.c
@@ -1,3 +1,24 @@
 char* majorityFrequencyGroup(char* s) {
+  int freqs[26] = {0};
+  for (char* c = s; *c != 0; ++c) ++freqs[*c - 'a'];
+
+  int freqMembers[101] = {0};
+  int majorFreq = 0;
+  for (int i = 0; i < 26; ++i) {
+    if (freqs[i] != 0) {
+      ++freqMembers[freqs[i]];
+      if (freqMembers[freqs[i]] > freqMembers[majorFreq] ||
+          (freqMembers[freqs[i]] == freqMembers[majorFreq] && freqs[i] > majorFreq)) {
+        majorFreq = freqs[i];
+      }
+    }
+  }
+
+  int n = 0;
+  for (int i = 0; i < 26; ++i) {
+    if (freqs[i] == majorFreq) s[n++] = 'a' + i;
+  }
+
+  s[n] = 0;
   return s;
 }
