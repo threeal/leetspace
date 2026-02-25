d4332355d89e06a8c18457f50b68bb46c707ddb0
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-25 15:36:14 +0700

    feat(problem-1356/c): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1356/c/solution.c
+++ old-problems/1356/c/solution.c
@@ -1,4 +1,15 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  const int aa = *(int*)a;
+  const int bb = *(int*)b;
+  const int ai = __builtin_popcount(aa);
+  const int bi = __builtin_popcount(bb);
+  return ai == bi ? aa - bb : ai - bi;
+}
+
 int* sortByBits(int* arr, int arrSize, int* returnSize) {
+  qsort(arr, arrSize, sizeof(int), compare);
   *returnSize = arrSize;
   return arr;
 }
