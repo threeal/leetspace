807b1957fc28e9b070a202cabdbb93e758052a86
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-26 14:01:25 +0700

    feat(problem-3849/cpp): solve by greedily put ones in front
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3849/solution.cpp
+++ old-problems/3849/solution.cpp
@@ -3,6 +3,24 @@
 class Solution {
  public:
   std::string maximumXor(std::string s, std::string t) {
-    return s + t;
+    std::size_t zeros{0};
+    for (char c : t) {
+      if (c == '0') ++zeros;
+    }
+
+    if (zeros < s.size()) {
+      for (std::size_t i{0}; i < zeros; ++i) {
+        if (s[i] == '0') {
+          s[i] = '1';
+          if (++zeros == s.size()) return s;
+        }
+      }
+
+      for (std::size_t i{zeros}; i < s.size(); ++i) {
+        s[i] = s[i] == '0' ? '1' : '0';
+      }
+    }
+
+    return s;
   }
 };
