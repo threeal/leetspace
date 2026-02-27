9558dc0b1f9aa66cbec5a3a421bca2f7eb465f64
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-27 13:59:42 +0700

    feat(problem-3843/cpp): solve by counting frequency of frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3843/solution.cpp
+++ old-problems/3843/solution.cpp
@@ -1,8 +1,19 @@
+#include <unordered_map>
 #include <vector>
 
 class Solution {
  public:
   int firstUniqueFreq(std::vector<int>& nums) {
-    return nums.size();
+    std::unordered_map<int, int> freqs{};
+    for (int num : nums) ++freqs[num];
+
+    std::vector<int> freqFreqs(nums.size() + 1, 0);
+    for (const auto [num, freq] : freqs) ++freqFreqs[freq];
+
+    for (int num : nums) {
+      if (freqFreqs[freqs[num]] == 1) return num;
+    }
+
+    return -1;
   }
 };
