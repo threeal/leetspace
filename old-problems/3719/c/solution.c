f44b31eb74792585eeb77ff6bae78d3c1424ad9d
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-27 14:13:45 +0700

    feat(problem-3719/c): solve by using brute-force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3719/c/solution.c
+++ old-problems/3719/c/solution.c
@@ -1,3 +1,46 @@
 int longestBalanced(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int freqs[100001] = {0};
+  int evens = 0, odds = 0;
+
+  int l = 0, r = 0, maxLength = 0;
+  while (1) {
+    while (r < numsSize) {
+      if (++freqs[nums[r]] == 1) {
+        if (nums[r] % 2 == 0) {
+          ++evens;
+        } else {
+          ++odds;
+        }
+      }
+
+      if (evens == odds) {
+        maxLength = r + 1 - l;
+      }
+
+      ++r;
+    }
+
+    if (--freqs[nums[l]] == 0) {
+      if (nums[l] % 2 == 0) {
+        --evens;
+      } else {
+        --odds;
+      }
+    }
+
+    if (++l == numsSize) break;
+
+    while (r - l > maxLength) {
+      --r;
+      if (--freqs[nums[r]] == 0) {
+        if (nums[r] % 2 == 0) {
+          --evens;
+        } else {
+          --odds;
+        }
+      }
+    }
+  }
+
+  return maxLength;
 }
