8aa149d2983bd6b24d741fec3c6155f72c17f175
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-28 08:24:10 +0700

    feat(problem-3010/c): solve by finding two smallest elements after the first element
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3010/c/solution.c
+++ old-problems/3010/c/solution.c
@@ -1,3 +1,13 @@
 int minimumCost(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int a = nums[1] < nums[2] ? nums[1] : nums[2];
+  int b = nums[1] < nums[2] ? nums[2] : nums[1];
+  for (int i = 3; i < numsSize; ++i) {
+    if (nums[i] < a) {
+      b = a;
+      a = nums[i];
+    } else if (nums[i] < b) {
+      b = nums[i];
+    }
+  }
+  return nums[0] + a + b;
 }
