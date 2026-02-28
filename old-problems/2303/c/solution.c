3fbcd718a5816f3ab0ef93098d8563503b1815bc
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-28 08:20:21 +0700

    feat(problem-2303/c): solve by calculating each bracket
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2303/c/solution.c
+++ old-problems/2303/c/solution.c
@@ -1,5 +1,26 @@
 double calculateTax(
     int** brackets, int bracketsSize, int* bracketsColSize, int income) {
-  return brackets[bracketsSize - 1][bracketsColSize[bracketsSize - 1] - 1] +
-      income;
+  (void)bracketsSize;
+  (void)bracketsColSize;
+
+  double tax = 0;
+  if (income > 0) {
+    if (income <= brackets[0][0]) {
+      tax += (double)income * brackets[0][1] / 100;
+    } else {
+      tax += (double)brackets[0][0] * brackets[0][1] / 100;
+
+      int i = 1;
+      while (brackets[i][0] < income) {
+        tax += (double)(brackets[i][0] - brackets[i - 1][0]) *
+            brackets[i][1] / 100;
+        ++i;
+      }
+
+      tax += (double)(income - brackets[i - 1][0]) *
+          brackets[i][1] / 100;
+    }
+  }
+
+  return tax;
 }
