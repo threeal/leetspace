7ed0065d384a628172fb45b518b27564a2dcb3c9
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-02-28 08:35:19 +0700

    feat(problem-1680/cpp): solve by using for-loop and bit manipulation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1680/solution.cpp
+++ old-problems/1680/solution.cpp
@@ -1,6 +1,12 @@
+#include <bit>
+
 class Solution {
  public:
   int concatenatedBinary(int n) {
-    return n;
+    long long ans{0};
+    for (int i{1}; i <= n; ++i) {
+      ans = ((ans << (32 - std::countl_zero<unsigned int>(i))) | i) % 1000000007;
+    }
+    return ans;
   }
 };
