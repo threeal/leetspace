0555c3987c0fe9b07a38f71f02979faedb0e6054
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-03 22:10:02 +0800

    feat(problem-2908/c): solve by keeping track of min numbers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2908/c/solution.c
+++ old-problems/2908/c/solution.c
@@ -1,3 +1,24 @@
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+
 int minimumSum(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int* minNums = malloc(numsSize * sizeof(int));
+  memcpy(minNums, nums, numsSize * sizeof(int));
+
+  for (int i = numsSize - 1; i > 0; --i) {
+    if (minNums[i] < minNums[i - 1]) minNums[i - 1] = minNums[i];
+  }
+
+  int minSum = INT_MAX, minNum = nums[0];
+  for (int i = 2; i < numsSize; ++i) {
+    if (nums[i - 1] > minNum && nums[i - 1] > minNums[i]) {
+      const int sum = minNum + nums[i - 1] + minNums[i];
+      if (sum < minSum) minSum = sum;
+    }
+    if (nums[i - 1] < minNum) minNum = nums[i - 1];
+  }
+
+  free(minNums);
+  return minSum == INT_MAX ? -1 : minSum;
 }
