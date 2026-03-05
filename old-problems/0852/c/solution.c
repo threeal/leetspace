e8b8b804631340465640c1467deba6cee5bfb7e5
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-05 11:03:42 +0800

    feat(problem-852/c): solve by using a binary search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0852/c/solution.c
+++ old-problems/0852/c/solution.c
@@ -1,3 +1,12 @@
 int peakIndexInMountainArray(int* arr, int arrSize) {
-  return arr[arrSize - 1];
+  int low = 0, high = arrSize - 2;
+  while (low < high) {
+    const int mid = low + (high - low) / 2;
+    if (arr[mid] < arr[mid + 1]) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+  }
+  return low;
 }
