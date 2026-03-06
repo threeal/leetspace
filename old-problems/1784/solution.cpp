3316eb0a13b8ab46f5dfa27c0dcc75acb9bd8f50
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-06 23:20:57 +0800

    feat(problem-1784/cpp): solve by using while-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1784/solution.cpp
+++ old-problems/1784/solution.cpp
@@ -3,6 +3,9 @@
 class Solution {
  public:
   bool checkOnesSegment(std::string s) {
-    return s.empty();
+    std::size_t i{0};
+    while (i < s.size() && s[i] == '1') ++i;
+    while (i < s.size() && s[i] == '0') ++i;
+    return i == s.size();
   }
 };
