9a1c3778201df555082495cb72e1f68403107aed
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-07 23:38:51 +0800

    feat(problem-3842/cpp): solve by using sorting and counting frequency
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3842/solution.cpp
+++ old-problems/3842/solution.cpp
@@ -1,8 +1,24 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> toggleLightBulbs(std::vector<int>& bulbs) {
+    std::sort(bulbs.begin(), bulbs.end());
+
+    std::size_t n{0}, freqs{1};
+    for (std::size_t i{1}; i < bulbs.size(); ++i) {
+      if (bulbs[i] == bulbs[i - 1]) {
+        ++freqs;
+      } else {
+        if (freqs % 2 == 1) bulbs[n++] = bulbs[i - 1];
+        freqs = 1;
+      }
+    }
+
+    if (freqs % 2 == 1) bulbs[n++] = bulbs.back();
+
+    bulbs.resize(n);
     return bulbs;
   }
 };
