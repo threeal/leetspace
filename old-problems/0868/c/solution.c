f6f6d25eca8ea5c3af6a418733fb5800c28a40fb
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-08 23:45:00 +0800

    feat(problem-868/c): solve by using bit manipulation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0868/c/solution.c
+++ old-problems/0868/c/solution.c
@@ -1,3 +1,13 @@
 int binaryGap(int n) {
-  return n;
+  unsigned int nn = n;
+  nn >>= __builtin_ctz(nn) + 1;
+
+  int maxGap = 0;
+  while (nn != 0) {
+    const int gap = __builtin_ctz(nn) + 1;
+    if (gap >= maxGap) maxGap = gap;
+    nn >>= gap;
+  }
+
+  return maxGap;
 }
