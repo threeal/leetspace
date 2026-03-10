f817dd435174218b601ed79a8942142230ea08e0
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-10 23:07:10 +0800

    feat(problem-3014/c): solve by counting frequency of each letter
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3014/c/solution.c
+++ old-problems/3014/c/solution.c
@@ -1,3 +1,19 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)b - *(int*)a;
+}
+
 int minimumPushes(char* word) {
-  return *word;
+  int freqs[26] = {0};
+  for (char* c = word; *c != 0; ++c) ++freqs[*c - 'a'];
+
+  qsort(freqs, 26, sizeof(int), compare);
+
+  int sum = 0;
+  for (int i = 0; i < 26 && freqs[i] != 0; ++i) {
+    sum += (i / 8 + 1) * freqs[i];
+  }
+
+  return sum;
 }
