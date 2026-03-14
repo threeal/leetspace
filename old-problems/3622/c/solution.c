8f8c2dfd40ad3125079650ac399ffc9a48fc38e1
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-14 22:14:28 +0700

    feat(problem-3622/c): solve by iterating digits of the given number
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3622/c/solution.c
+++ old-problems/3622/c/solution.c
@@ -1,5 +1,10 @@
 #include <stdbool.h>
 
 bool checkDivisibility(int n) {
-  return n == 0;
+  int sum = 0, product = 1;
+  for (int nn = n; nn != 0; nn /= 10) {
+    sum += nn % 10;
+    product *= nn % 10;
+  }
+  return n % (sum + product) == 0;
 }
