ddbcb0ad7513f27b40d875d72f8a030d6686dc46
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-16 22:07:42 +0700

    feat(problem-3120/c): solve by using a bit flags
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3120/c/solution.c
+++ old-problems/3120/c/solution.c
@@ -1,3 +1,12 @@
 int numberOfSpecialChars(char* word) {
-  return *word;
+  unsigned int lower = 0, upper = 0;
+  while (*word != 0) {
+    if (*word >= 'a') {
+      lower |= 1 << (*word - 'a');
+    } else {
+      upper |= 1 << (*word - 'A');
+    }
+    ++word;
+  }
+  return __builtin_popcount(lower & upper);
 }
