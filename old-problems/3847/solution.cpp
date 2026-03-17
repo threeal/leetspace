e0fa9af9a05c383e79e3bdab42c8f6c181a34e3b
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-17 13:36:31 +0700

    feat(problem-3847/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3847/solution.cpp
+++ old-problems/3847/solution.cpp
@@ -3,6 +3,13 @@
 class Solution {
  public:
   int scoreDifference(std::vector<int>& nums) {
-    return nums.size();
+    int diff{0};
+    bool isActive{true};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      if (nums[i] % 2 == 1) isActive = !isActive;
+      if ((i + 1) % 6 == 0) isActive = !isActive;
+      diff += isActive ? nums[i] : -nums[i];
+    }
+    return diff;
   }
 };
