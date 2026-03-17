59c7b37d892d1dee6b468b1926cfeeee4b5ac7a3
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-17 10:22:45 +0700

    feat(problem-3083/c): solve by using map of bit flags
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3083/c/solution.c
+++ old-problems/3083/c/solution.c
@@ -1,5 +1,15 @@
 #include <stdbool.h>
+#include <stdint.h>
 
 bool isSubstringPresent(char* s) {
-  return *s == 0;
+  uint32_t exists[26] = {0};
+  for (int i = 1; s[i] != 0; ++i) {
+    exists[s[i] - 'a'] |= 1 << (s[i - 1] - 'a');
+  }
+
+  for (int i = 1; s[i] != 0; ++i) {
+    if ((exists[s[i - 1] - 'a'] & (1 << (s[i] - 'a'))) != 0) return true;
+  }
+
+  return false;
 }
