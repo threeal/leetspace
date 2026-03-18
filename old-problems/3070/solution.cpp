ac1c63db13b7726ab3a81769466d1ee0d2437372
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-18 22:33:21 +0700

    feat(problem-3070/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3070/solution.cpp
+++ old-problems/3070/solution.cpp
@@ -3,7 +3,35 @@
 class Solution {
  public:
   int countSubmatrices(std::vector<std::vector<int>>& grid, int k) {
-    (void)k;
-    return grid.size();
+    int count{0};
+    if (grid[0][0] <= k) ++count;
+
+    for (std::size_t i{1}; i < grid[0].size(); ++i) {
+      grid[0][i] += grid[0][i - 1];
+      if (grid[0][i] <= k) {
+        ++count;
+      } else {
+        break;
+      }
+    }
+
+    for (std::size_t i{1}; i < grid.size(); ++i) {
+      grid[i][0] += grid[i - 1][0];
+      if (grid[i][0] <= k) {
+        ++count;
+        for (std::size_t j{1}; j < grid[i].size(); ++j) {
+          grid[i][j] += grid[i - 1][j] + grid[i][j - 1] - grid[i - 1][j - 1];
+          if (grid[i][j] <= k) {
+            ++count;
+          } else {
+            break;
+          }
+        }
+      } else {
+        break;
+      }
+    }
+
+    return count;
   }
 };
