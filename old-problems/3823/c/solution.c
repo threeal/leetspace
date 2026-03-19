28ff25e5b72a91302efd2e00e53c56c731b85a72
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-19 19:01:41 +0700

    feat(problem-3823/c): solve by using two pointers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3823/c/solution.c
+++ old-problems/3823/c/solution.c
@@ -1,3 +1,28 @@
+#include <string.h>
+
 char* reverseByType(char* s) {
+  const int sLen = strlen(s);
+  int l = 0, rc = sLen - 1, rs = sLen - 1;
+  while (l < rc || l < rs) {
+    if (s[l] >= 'a') {
+      while (l < rc && s[rc] < 'a') --rc;
+      if (l < rc) {
+        const char temp = s[l];
+        s[l] = s[rc];
+        s[rc] = temp;
+        --rc;
+      }
+    } else {
+      while (l < rs && s[rs] >= 'a') --rs;
+      if (l < rs) {
+        const char temp = s[l];
+        s[l] = s[rs];
+        s[rs] = temp;
+        --rs;
+      }
+    }
+    ++l;
+  }
+
   return s;
 }
