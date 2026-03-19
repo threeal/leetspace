af69388374e52aa698e900e898aa4f0c5f3d937b
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-19 20:02:07 +0800

    feat: solve problem 693. Binary Number with Alternating Bits in C (#5359)
    * test(problem-693): add support to test in C
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(problem-693/c): solve by iterating bits
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0693/c/solution.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+
+bool hasAlternatingBits(int n) {
+  for (int i = 1; i < 32 && (1 << i) <= n; ++i) {
+    if (((n >> i) & 1) == ((n >> (i - 1)) & 1)) return false;
+  }
+  return true;
+}
