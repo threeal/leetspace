78bde9ae8634da16c4ab222985d93563afbb16a9
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-21 21:47:32 +0700

    feat(problem-3210/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3210/c/solution.c
+++ old-problems/3210/c/solution.c
@@ -1,4 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+
 char* getEncryptedString(char* s, int k) {
-  (void)k;
-  return s;
+  const int sLen = strlen(s);
+  char* out = malloc((sLen + 1) * sizeof(char));
+  memset(out, ' ', (sLen + 1) * sizeof(char));
+  for (int i = 0; i < sLen; ++i) {
+    out[i] = s[(i + k) % sLen];
+  }
+  out[sLen] = 0;
+  return out;
 }
