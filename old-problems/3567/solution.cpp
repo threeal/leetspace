65176c1b391afca159774f71c5515bed029343c9
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-21 21:39:59 +0700

    test: add test for problem 3567. Minimum Absolute Difference in Sliding Submatrix
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3567/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> minAbsDiff(
+      std::vector<std::vector<int>>& grid, int k) {
+    (void)k;
+    return grid;
+  }
+};
