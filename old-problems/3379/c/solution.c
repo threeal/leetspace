60f7623fe2ef2a3c9e8b1140c3d23a2b94706b36
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-26 21:53:57 +0700

    feat(problem-3379/c): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3379/c/solution.c
+++ old-problems/3379/c/solution.c
@@ -1,4 +1,10 @@
+#include <stdlib.h>
+
 int* constructTransformedArray(int* nums, int numsSize, int* returnSize) {
+  int* output = malloc(numsSize * sizeof(int));
+  for (int i = 0; i < numsSize; ++i) {
+    output[i] = nums[(numsSize + (i + nums[i]) % numsSize) % numsSize];
+  }
   *returnSize = numsSize;
-  return nums;
+  return output;
 }
