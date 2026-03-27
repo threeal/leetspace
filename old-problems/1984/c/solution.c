79111c06133076ac9e87f8da3189d00b783ca7bd
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-27 09:07:45 +0700

    feat(problem-1984/c): solve by using sorting and sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1984/c/solution.c
+++ old-problems/1984/c/solution.c
@@ -1,3 +1,19 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
+
 int minimumDifference(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] + k;
+  if (k == 1) return 0;
+
+  qsort(nums, numsSize, sizeof(int), compare);
+
+  int minDiff = nums[k - 1] - nums[0];
+  for (int i = k + 1; i <= numsSize; ++i) {
+    const int diff = nums[i - 1] - nums[i - k];
+    if (diff < minDiff) minDiff = diff;
+  }
+
+  return minDiff;
 }
