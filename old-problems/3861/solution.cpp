f606e423fd03d5f0e150a340c9c2787eb5bb6446
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-27 09:00:49 +0700

    feat(problem-3861/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3861/solution.cpp
+++ old-problems/3861/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   int minimumIndex(std::vector<int>& capacity, int itemSize) {
-    return capacity.size() + itemSize;
+    int idx{-1};
+    for (std::size_t i{0}; i < capacity.size(); ++i) {
+      if (capacity[i] >= itemSize &&
+          (idx == -1 || capacity[i] < capacity[idx])) idx = i;
+    }
+    return idx;
   }
 };
