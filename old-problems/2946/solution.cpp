97e6f5919ecc5c50153664d48e6adb5adfd11374
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-27 08:59:01 +0700

    feat(problem-2946/cpp): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2946/solution.cpp
+++ old-problems/2946/solution.cpp
@@ -3,6 +3,18 @@
 class Solution {
  public:
   bool areSimilar(std::vector<std::vector<int>>& mat, int k) {
-    return mat.empty() && k == 0;
+    const std::size_t n{mat[0].size()};
+    for (std::size_t i{0}; i < mat.size(); ++i) {
+      for (std::size_t j{0}; j < n; ++j) {
+        if (mat[i][j] != mat[i][(j + k) % n]) return false;
+      }
+
+      if (++i >= mat.size()) break;
+
+      for (std::size_t j{0}; j < n; ++j) {
+        if (mat[i][j] != mat[i][(n * k + j - k) % n]) return false;
+      }
+    }
+    return true;
   }
 };
