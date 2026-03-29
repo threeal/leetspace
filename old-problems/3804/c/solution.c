4c09b1249b40aae924efa29fda01b6d3f46d85c0
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-29 21:15:02 +0700

    feat(problem-3804/c): solve by using brute-force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3804/c/solution.c
+++ old-problems/3804/c/solution.c
@@ -1,3 +1,16 @@
 int centeredSubarrays(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int count = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    int sum = 0;
+    for (int j = i; j < numsSize; ++j) {
+      sum += nums[j];
+      for (int k = i; k <= j; ++k) {
+        if (sum == nums[k]) {
+          ++count;
+          break;
+        }
+      }
+    }
+  }
+  return count;
 }
