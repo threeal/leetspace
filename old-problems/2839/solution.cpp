ad523bffdb6c700479ceb45a8dd9d8e305076583
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-29 21:11:34 +0700

    feat(problem-2839/cpp): solve by checking all possibilities
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2839/solution.cpp
+++ old-problems/2839/solution.cpp
@@ -3,6 +3,7 @@
 class Solution {
  public:
   bool canBeEqual(std::string s1, std::string s2) {
-    return s1.size() + s2.size();
+    return ((s1[0] == s2[0] && s1[2] == s2[2]) || (s1[0] == s2[2] && s1[2] == s2[0])) &&
+        ((s1[1] == s2[1] && s1[3] == s2[3]) || (s1[1] == s2[3] && s1[3] == s2[1]));
   }
 };
