530bbbe1b3c5a213b466c3bf7ca85e8903867bd4
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-31 09:13:58 +0700

    feat(problem-2395/c): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2395/c/solution.c
+++ old-problems/2395/c/solution.c
@@ -1,5 +1,19 @@
 #include <stdbool.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return (long long)*(int*)a - (long long)*(int*)b;
+}
 
 bool findSubarrays(int* nums, int numsSize) {
-  return nums[numsSize - 1] == 0;
+  for (int i = 1; i < numsSize; ++i) nums[i - 1] += nums[i];
+  --numsSize;
+
+  qsort(nums, numsSize, sizeof(int), compare);
+
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i - 1] == nums[i]) return true;
+  }
+
+  return false;
 }
