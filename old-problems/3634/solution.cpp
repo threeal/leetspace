b0a49b210cb8ba19af356ce693da9befa1452dee
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-03-31 08:58:22 +0700

    feat(problem-3634/cpp): solve by using sorting and sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3634/solution.cpp
+++ old-problems/3634/solution.cpp
@@ -1,8 +1,15 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int minRemoval(std::vector<int>& nums, int k) {
-    return nums.size() + k;
+    std::sort(nums.begin(), nums.end());
+    std::size_t maxSize{0};
+    for (std::size_t l{0}, r{0}; r < nums.size(); ++r) {
+      while (nums[l] < (nums[r] + k - 1) / k) ++l;
+      if (r + 1 - l > maxSize) maxSize = r + 1 - l;
+    }
+    return nums.size() - maxSize;
   }
 };
