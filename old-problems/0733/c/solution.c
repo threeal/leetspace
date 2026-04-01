3d2f7e100c481066befdcb84c4547ad7c6427630
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-01 09:48:14 +0700

    test(problem-733): add support to test in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0733/c/solution.c
@@ -0,0 +1,10 @@
+int** floodFill(
+    int** image, int imageSize, int* imageColSize, int sr, int sc, int color,
+    int* returnSize, int** returnColumnSizes) {
+  (void)sr;
+  (void)sc;
+  (void)color;
+  *returnSize = imageSize;
+  *returnColumnSizes = imageColSize;
+  return image;
+}
