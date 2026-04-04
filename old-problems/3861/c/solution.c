e686eb136386220fc78f89256c0ef58f04fe78c0
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-04 22:35:01 +0700

    feat(problem-3861/c): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3861/c/solution.c
+++ old-problems/3861/c/solution.c
@@ -1,3 +1,8 @@
 int minimumIndex(int* capacity, int capacitySize, int itemSize) {
-  return capacity[capacitySize - 1] + itemSize;
+  int idx = -1;
+  for (int i = 0; i < capacitySize; ++i) {
+    if (capacity[i] >= itemSize &&
+        (idx == -1 || capacity[i] < capacity[idx])) idx = i;
+  }
+  return idx;
 }
