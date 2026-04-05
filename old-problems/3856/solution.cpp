1c0d412c3934eba8ac0b606be85dc17a3f563419
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-05 22:41:40 +0700

    feat(problem-3856/cpp): solve by iterating backward
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3856/solution.cpp
+++ old-problems/3856/solution.cpp
@@ -3,6 +3,25 @@
 class Solution {
  public:
   std::string trimTrailingVowels(std::string s) {
+    bool done{false};
+    std::size_t n{s.size()};
+    while (!done) {
+      switch (s[n - 1]) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+          if (--n == 0) {
+            done = true;
+          }
+          break;
+
+        default:
+          done = true;
+      }
+    }
+    s.resize(n);
     return s;
   }
 };
