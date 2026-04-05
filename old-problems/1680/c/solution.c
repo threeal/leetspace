7fa6aa3ee0c166834cabe784ea08f4c0bbcf074f
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-05 22:51:51 +0700

    feat(problem-1680/c): solve by using for-loop and bit manipulation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1680/c/solution.c
+++ old-problems/1680/c/solution.c
@@ -1,3 +1,7 @@
 int concatenatedBinary(int n) {
-  return n;
+  long long ans = 0;
+  for (int i = 1; i <= n; ++i) {
+    ans = ((ans << (32 - __builtin_clz(i))) | i) % 1000000007;
+  }
+  return ans;
 }
