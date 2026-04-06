4ca02464c0518874f38704a23cf04ce61f048464
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-06 21:32:48 +0700

    feat(problem-932/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0932/c/solution.c
+++ old-problems/0932/c/solution.c
@@ -1,5 +1,30 @@
+#include <stdlib.h>
+
 int* beautifulArray(int n, int* returnSize) {
-  (void)n;
-  *returnSize = 0;
-  return 0;
+  int* res = malloc(n * sizeof(int));
+  int resSize = 0;
+  res[resSize++] = 1;
+
+  int* prev = malloc(n * sizeof(int));
+  int prevSize = 0;
+
+  for (int i = 2; i <= n; ++i) {
+    int* temp = res;
+    res = prev;
+    prev = temp;
+
+    prevSize = resSize;
+    resSize = 0;
+
+    for (int j = 0; j < prevSize; ++j) {
+      if (prev[j] * 2 - 1 <= i) res[resSize++] = prev[j] * 2 - 1;
+    }
+
+    for (int j = 0; j < prevSize; ++j) {
+      if (prev[j] * 2 <= i) res[resSize++] = prev[j] * 2;
+    }
+  }
+
+  *returnSize = resSize;
+  return res;
 }
