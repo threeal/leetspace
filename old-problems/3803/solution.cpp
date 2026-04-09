320d93e7397203039b935580249ea6ad603cc11e
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-09 11:06:43 +0700

    feat(problem-3803/cpp): solve by keeping track of distinct characters
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3803/solution.cpp
+++ old-problems/3803/solution.cpp
@@ -1,8 +1,15 @@
+#include <bit>
 #include <string>
 
 class Solution {
  public:
   int residuePrefixes(std::string s) {
-    return s.size();
+    int count{0};
+    std::uint32_t distincts{0};
+    for (std::size_t i{0}; i < s.size(); ++i) {
+      distincts |= 1 << (s[i] - 'a');
+      if (std::popcount(distincts) == (i + 1) % 3) ++count;
+    }
+    return count;
   }
 };
