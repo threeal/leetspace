2b38fe592490f69615031b9e02679ca02f61ae92
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-10 14:14:28 +0700

    feat(problem-3740/cpp): solve by using brute-force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3740/solution.cpp
+++ old-problems/3740/solution.cpp
@@ -3,6 +3,19 @@
 class Solution {
  public:
   int minimumDistance(std::vector<int>& nums) {
-    return nums.size();
+    std::size_t minDist{nums.size() * 3};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      for (std::size_t j{i + 1}; j < nums.size(); ++j) {
+        if (nums[i] == nums[j]) {
+          for (std::size_t k{j + 1}; k < nums.size(); ++k) {
+            if (nums[j] == nums[k]) {
+              const std::size_t dist{2 * (k - i)};
+              if (dist < minDist) minDist = dist;
+            }
+          }
+        }
+      }
+    }
+    return minDist == nums.size() * 3 ? -1 : minDist;
   }
 };
