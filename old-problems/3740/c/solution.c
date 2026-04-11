f4afb88699c8809c42ef13e53d0ad27452d32018
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-11 22:31:03 +0700

    feat(problem-3740/c): solve by using brute-force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3740/c/solution.c
+++ old-problems/3740/c/solution.c
@@ -1,3 +1,16 @@
 int minimumDistance(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int minDist = numsSize * 3;
+  for (int i = 0; i < numsSize; ++i) {
+    for (int j = i + 1; j < numsSize; ++j) {
+      if (nums[i] == nums[j]) {
+        for (int k = j + 1; k < numsSize; ++k) {
+          if (nums[j] == nums[k]) {
+            const int dist = 2 * (k - i);
+            if (dist < minDist) minDist = dist;
+          }
+        }
+      }
+    }
+  }
+  return minDist == numsSize * 3 ? -1 : minDist;
 }
