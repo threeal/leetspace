3a2f4c63092f7244b8eb3100ea83da413e215b3d
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-12 21:13:25 +0700

    feat(problem-2839/c): solve by checking all possibilities
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2839/c/solution.c
+++ old-problems/2839/c/solution.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 
 bool canBeEqual(char* s1, char* s2) {
-  return *s1 == *s2;
+  return ((s1[0] == s2[0] && s1[2] == s2[2]) || (s1[0] == s2[2] && s1[2] == s2[0])) &&
+      ((s1[1] == s2[1] && s1[3] == s2[3]) || (s1[1] == s2[3] && s1[3] == s2[1]));
 }
