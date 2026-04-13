be5c34daf370a96ec515922078bb29ef555fc9f5
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-13 09:47:39 +0700

    feat(problem-1848/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1848/solution.cpp
+++ old-problems/1848/solution.cpp
@@ -3,6 +3,14 @@
 class Solution {
  public:
   int getMinDistance(std::vector<int>& nums, int target, int start) {
-    return nums.size() + target + start;
+    const int n = nums.size();
+    int minDist{n};
+    for (int i{0}; i < n; ++i) {
+      if (nums[i] == target) {
+        const int dist{std::abs(start - i)};
+        if (dist < minDist) minDist = dist;
+      }
+    }
+    return minDist;
   }
 };
