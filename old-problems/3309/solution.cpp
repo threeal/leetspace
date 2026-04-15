7763ab957c6fa30fa5b31dee9d7bd84d2ac4fe95
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-15 12:18:54 +0700

    feat(problem-3309/cpp): solve by checking all possibilities
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3309/solution.cpp
+++ old-problems/3309/solution.cpp
@@ -1,8 +1,17 @@
+#include <algorithm>
+#include <bit>
 #include <vector>
 
 class Solution {
  public:
   int maxGoodNumber(std::vector<int>& nums) {
-    return nums.size();
+    const int a{nums[0]}, an{32 - std::countl_zero<unsigned int>(a)};
+    const int b{nums[1]}, bn{32 - std::countl_zero<unsigned int>(b)};
+    const int c{nums[2]}, cn{32 - std::countl_zero<unsigned int>(c)};
+    return std::max(
+        std::max(
+            a << (bn + cn) | std::max(b << cn | c, c << bn | b),
+            b << (an + cn) | std::max(a << cn | c, c << an | a)),
+        c << (an + bn) | std::max(a << bn | b, b << an | a));
   }
 };
