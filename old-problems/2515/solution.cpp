7620b7b2d384e3e625167bb0ebde22c6a86c1972
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-15 11:58:07 +0700

    feat(problem-2515/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2515/solution.cpp
+++ old-problems/2515/solution.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <string>
 #include <vector>
 
@@ -5,6 +6,15 @@ class Solution {
  public:
   int closestTarget(
       std::vector<std::string>& words, std::string target, int startIndex) {
-    return words.size() + target.size() + startIndex;
+    const int n = words.size();
+    int minDistance = n;
+    for (int i = 0; i < n; ++i) {
+      if (words[i] == target) {
+        const int distance{
+            std::min((n + startIndex - i) % n, (n - startIndex + i) % n)};
+        if (distance < minDistance) minDistance = distance;
+      }
+    }
+    return minDistance == n ? -1 : minDistance;
   }
 };
