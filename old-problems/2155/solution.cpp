a6aadd7a6f603f6d223743c25c7737e8ed0beaad
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-20 11:55:40 +0700

    feat(problem-2155/cpp): solve by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2155/solution.cpp
+++ old-problems/2155/solution.cpp
@@ -3,6 +3,30 @@
 class Solution {
  public:
   std::vector<int> maxScoreIndices(std::vector<int>& nums) {
-    return nums;
+    int score{0};
+    for (const int num : nums) {
+      if (num != 0) ++score;
+    }
+
+    int maxScore{score};
+    std::vector<int> indices{0};
+
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      if (nums[i] == 0) {
+        ++score;
+      } else {
+        --score;
+      }
+
+      if (score >= maxScore) {
+        if (score > maxScore) {
+          maxScore = score;
+          indices.clear();
+        }
+        indices.push_back(i + 1);
+      }
+    }
+
+    return indices;
   }
 };
