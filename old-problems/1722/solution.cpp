ad7fe0c68e3f8505977976baeb56bc60e5aef4fb
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-21 20:58:36 +0700

    feat(problem-1722/cpp): solve by using DSU
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1722/solution.cpp
+++ old-problems/1722/solution.cpp
@@ -1,3 +1,4 @@
+#include <numeric>
 #include <vector>
 
 class Solution {
@@ -6,6 +7,64 @@ class Solution {
       std::vector<int>& source,
       std::vector<int>& target,
       std::vector<std::vector<int>>& allowedSwaps) {
-    return source.size() + target.size() + allowedSwaps.size();
+    std::vector<std::size_t> roots(source.size());
+    std::iota(roots.begin(), roots.end(), 0);
+
+    for (const auto& swap : allowedSwaps) {
+      const std::size_t rootA{getRoots(roots, swap[0])};
+      const std::size_t rootB{getRoots(roots, swap[1])};
+      if (rootA != rootB) {
+        if (rootA < rootB) {
+          roots[rootB] = rootA;
+        } else {
+          roots[rootA] = rootB;
+        }
+      }
+    }
+
+    std::vector<std::size_t> indices(source.size());
+    std::iota(indices.begin(), indices.end(), 0);
+
+    std::sort(
+        indices.begin(), indices.end(),
+        [&roots](std::size_t a, std::size_t b) {
+          return getRoots(roots, a) < getRoots(roots, b);
+        });
+
+    int hammingDistance{0};
+    std::size_t l{0};
+    for (std::size_t r{1}; r < source.size(); ++r) {
+      if (getRoots(roots, indices[l]) != getRoots(roots, indices[r])) {
+        std::unordered_map<int, int> freqs{};
+        for (std::size_t i{l}; i < r; ++i) ++freqs[source[indices[i]]];
+        for (std::size_t i{l}; i < r; ++i) {
+          auto it = freqs.find(target[indices[i]]);
+          if (it != freqs.end() && it->second != 0) {
+            --it->second;
+          } else {
+            ++hammingDistance;
+          }
+        }
+        l = r;
+      }
+    }
+
+    std::unordered_map<int, int> freqs{};
+    for (std::size_t i{l}; i < source.size(); ++i) ++freqs[source[indices[i]]];
+    for (std::size_t i{l}; i < source.size(); ++i) {
+      auto it = freqs.find(target[indices[i]]);
+      if (it != freqs.end() && it->second != 0) {
+        --it->second;
+      } else {
+        ++hammingDistance;
+      }
+    }
+
+    return hammingDistance;
+  }
+
+ private:
+  static std::size_t getRoots(std::vector<std::size_t>& roots, std::size_t i) {
+    return roots[i] != i ? (roots[i] = getRoots(roots, roots[i])) : i;
   }
 };
