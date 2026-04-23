ea0ccd9c84588502018f316eb6d5a5a7acca8ce8
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-23 13:07:33 +0700

    feat(problem-2946/c): solve by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2946/c/solution.c
+++ old-problems/2946/c/solution.c
@@ -1,5 +1,17 @@
 #include <stdbool.h>
 
 bool areSimilar(int** mat, int matSize, int* matColSize, int k) {
-  return mat[matSize - 1][matColSize[matSize - 1]] == k;
+  const int n = matColSize[0];
+  for (int i = 0; i < matSize; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (mat[i][j] != mat[i][(j + k) % n]) return false;
+    }
+
+    if (++i >= matSize) break;
+
+    for (int j = 0; j < n; ++j) {
+      if (mat[i][j] != mat[i][(n * k + j - k) % n]) return false;
+    }
+  }
+  return true;
 }
