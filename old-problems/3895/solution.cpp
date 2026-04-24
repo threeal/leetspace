e40e0f72c3e029d2169c08c56c2a1d2911025c81
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-24 10:59:27 +0700

    feat(problem-3895/cpp): solve by using modulo to count digit
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3895/solution.cpp
+++ old-problems/3895/solution.cpp
@@ -3,6 +3,13 @@
 class Solution {
  public:
   int countDigitOccurrences(std::vector<int>& nums, int digit) {
-    return nums.size() + digit;
+    int count{0};
+    for (int num : nums) {
+      while (num != 0) {
+        if (num % 10 == digit) ++count;
+        num /= 10;
+      }
+    }
+    return count;
   }
 };
