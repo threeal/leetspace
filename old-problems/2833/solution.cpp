31766c8feacab572ca2f2f54ebc529d42fd902a3
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-24 11:12:40 +0700

    feat(problem-2833/cpp): solve by counting frequency of each character
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2833/solution.cpp
+++ old-problems/2833/solution.cpp
@@ -3,6 +3,20 @@
 class Solution {
  public:
   int furthestDistanceFromOrigin(std::string moves) {
-    return moves.size();
+    int l{0}, r{0}, _{0};
+    for (const char move : moves) {
+      switch (move) {
+        case 'L':
+          ++l;
+          break;
+        case 'R':
+          ++r;
+          break;
+        case '_':
+          ++_;
+          break;
+      }
+    }
+    return l > r ? l + _ - r : r + _ - l;
   }
 };
