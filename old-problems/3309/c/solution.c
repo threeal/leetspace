74498e0a8ea80c6b485905e27c4719880ca85c23
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-26 23:05:56 +0700

    feat(problem-3309/c): solve by checking all possibilities
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3309/c/solution.c
+++ old-problems/3309/c/solution.c
@@ -1,3 +1,19 @@
 int maxGoodNumber(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  (void)numsSize;
+
+  const int a = nums[0], an = 32 - __builtin_clz(a);
+  const int b = nums[1], bn = 32 - __builtin_clz(b);
+  const int c = nums[2], cn = 32 - __builtin_clz(c);
+
+  const int bc = b << cn | c, cb = c << bn | b;
+  const int aa = a << (bn + cn) | (bc > cb ? bc : cb);
+
+  const int ac = a << cn | c, ca = c << an | a;
+  const int bb = b << (an + cn) | (ac > ca ? ac : ca);
+
+  const int aabb = aa > bb ? aa : bb;
+  const int ab = a << bn | b, ba = b << an | a;
+  const int cc = c << (an + bn) | (ab > ba ? ab : ba);
+
+  return aabb > cc ? aabb : cc;
 }
