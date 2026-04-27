82bb4cd7eeec68d48f61a696ec1554cf9a745a59
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-27 13:41:05 +0700

    feat(problem-2078/c): solve by using while-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2078/c/solution.c
+++ old-problems/2078/c/solution.c
@@ -1,3 +1,9 @@
 int maxDistance(int* colors, int colorsSize) {
-  return colors[colorsSize - 1];
+  if (colors[0] != colors[colorsSize - 1]) return colorsSize - 1;
+
+  int l = 0, r = colorsSize - 1;
+  while (colors[l] == colors[0]) ++l;
+  while (colors[r] == colors[0]) --r;
+
+  return r > colorsSize - l - 1 ? r : colorsSize - l - 1;
 }
