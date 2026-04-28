0d184bbb68c95e0f6bfad9d9c2a8e381639a304a
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-04-28 09:20:31 +0700

    feat(problem-3741/cpp): solve by using sorting
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3741/solution.cpp
+++ old-problems/3741/solution.cpp
@@ -1,8 +1,26 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   int minimumDistance(std::vector<int>& nums) {
-    return nums.size();
+    std::vector<std::size_t> idxs(nums.size());
+    std::iota(idxs.begin(), idxs.end(), 0);
+
+    std::sort(
+        idxs.begin(), idxs.end(), [&nums](std::size_t a, std::size_t b) {
+          return nums[a] == nums[b] ? a < b : nums[a] < nums[b];
+        });
+
+    std::size_t minDist{nums.size()};
+    for (std::size_t i{2}; i < idxs.size(); ++i) {
+      if (nums[idxs[i - 2]] == nums[idxs[i]] &&
+          idxs[i] - idxs[i - 2] < minDist) {
+        minDist = idxs[i] - idxs[i - 2];
+      }
+    }
+
+    return minDist == nums.size() ? -1 : minDist * 2;
   }
 };
