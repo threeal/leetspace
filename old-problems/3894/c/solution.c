1b231b04f99b97ae9cae33c39917a83db7ea16e6
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-02 22:51:19 +0700

    feat(problem-3894/c): solve by using if conditions
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3894/c/solution.c
+++ old-problems/3894/c/solution.c
@@ -1,4 +1,6 @@
 char* trafficSignal(int timer) {
-  (void)timer;
-  return "";
+  if (timer == 0) return "Green";
+  if (timer == 30) return "Orange";
+  if (timer > 30 && timer <= 90) return "Red";
+  return "Invalid";
 }
