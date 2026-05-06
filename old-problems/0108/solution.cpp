6fd03a1aa2e11c85eb17f79379cd9c7c3d4ccc6c
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-06 10:04:52 +0700

    feat(problem-108/cpp): solve by using a divide and conquer approach
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/0108/solution.cpp
+++ old-problems/0108/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   TreeNode* sortedArrayToBST(std::vector<int>& nums) {
-    return new TreeNode(nums.size());
+    return divideAndConquer(nums.data(), 0, nums.size() - 1);
+  }
+
+ private:
+  TreeNode* divideAndConquer(int* nums, std::size_t l, std::size_t r) {
+    const std::size_t mid{r - (r - l) / 2};
+    TreeNode* node{new TreeNode(nums[mid])};
+
+    if (l < mid) node->left = divideAndConquer(nums, l, mid - 1);
+    if (mid < r) node->right = divideAndConquer(nums, mid + 1, r);
+
+    return node;
   }
 };
