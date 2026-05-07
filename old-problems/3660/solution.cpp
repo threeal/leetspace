e122bb93874462184506a34aa24afc3a1d8e9b09
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-07 10:06:49 +0700

    feat(problem-3660/cpp): solve by using prefix-max and suffix-min
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3660/solution.cpp
+++ old-problems/3660/solution.cpp
@@ -1,8 +1,26 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   std::vector<int> maxValue(std::vector<int>& nums) {
+    const std::size_t n{nums.size()};
+    std::vector<int> maxs(n);
+    std::vector<int> mins(n);
+
+    maxs[0] = nums[0];
+    for (std::size_t i{1}; i < n; ++i) {
+      maxs[i] = std::max(maxs[i - 1], nums[i]);
+    }
+
+    mins[n - 1] = nums[n - 1];
+    nums[n - 1] = maxs[n - 1];
+
+    for (std::size_t i{n - 1}; i > 0; --i) {
+      mins[i - 1] = std::min(mins[i], nums[i - 1]);
+      nums[i - 1] = maxs[i - 1] > mins[i] ? nums[i] : maxs[i - 1];
+    }
+
     return nums;
   }
 };
