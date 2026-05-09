d10017c8fa5439cf1c8bde86bdaa30e4d3e34bc2
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-09 22:18:53 +0700

    test: add test for problem 1914. Cyclically Rotating a Grid
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/1914/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> rotateGrid(
+      std::vector<std::vector<int>>& grid, int k) {
+    (void)k;
+    return grid;
+  }
+};
