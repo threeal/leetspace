823b455b5525354d830c234ed852541926a1a68e
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-09 22:28:42 +0700

    DONT MERGE

--- old-problems/1914/solution.cpp
+++ old-problems/1914/solution.cpp
@@ -4,7 +4,15 @@ class Solution {
  public:
   std::vector<std::vector<int>> rotateGrid(
       std::vector<std::vector<int>>& grid, int k) {
-    (void)k;
-    return grid;
+    const std::size_t m{grid.size()};
+    const std::size_t n{grid[0].size()};
+
+    std::vector<std::vector<int>> out(m, std::vector<int>(n));
+    for (std::size_t i{0}; i * 2 < m && i * 2 < n; ++i) {
+      const std::size_t
+
+    }
+
+    return out;
   }
 };
