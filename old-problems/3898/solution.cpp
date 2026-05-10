00e56f63a3fe01c1912007e2cfd8ca0b11b4b968
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-10 20:44:27 +0700

    feat(problem-3898/cpp): solve by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3898/solution.cpp
+++ old-problems/3898/solution.cpp
@@ -3,6 +3,12 @@
 class Solution {
  public:
   std::vector<int> findDegrees(std::vector<std::vector<int>>& matrix) {
-    return matrix[0];
+    std::vector<int> degrees(matrix.size(), 0);
+    for (std::size_t i{0}; i < matrix.size(); ++i) {
+      for (const int val : matrix[i]) {
+        if (val == 1) ++degrees[i];
+      }
+    }
+    return degrees;
   }
 };
