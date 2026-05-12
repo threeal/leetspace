1fbc67cfebdc4ed247d5f81bba015e8f9a3a4e60
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-12 09:53:02 +0700

    feat(problem-1665/cpp): solve by using binary search and greedy
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1665/solution.cpp
+++ old-problems/1665/solution.cpp
@@ -3,6 +3,39 @@
 class Solution {
  public:
   int minimumEffort(std::vector<std::vector<int>>& tasks) {
-    return tasks.size();
+    int low{0}, high{0};
+    for (const auto& task : tasks) {
+      low += task[0];
+      high += task[1];
+    }
+
+    std::sort(
+        tasks.begin(), tasks.end(),
+        [](const auto& a, const auto& b) {
+          const int aa{a[1] - a[0]};
+          const int bb{b[1] - b[0]};
+          return aa == bb ? a[1] > b[1] : aa > bb;
+        });
+
+    while (low < high) {
+      const int mid{low + (high - low) / 2};
+
+      int energy{mid};
+      for (const auto& task : tasks) {
+        if (task[1] > energy || task[0] > energy) {
+          energy = -1;
+          break;
+        }
+        energy -= task[0];
+      }
+
+      if (energy >= 0) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return low;
   }
 };
