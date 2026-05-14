35ca7414b9c44f14ef34c7514a9351060ac950e2
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-14 10:41:39 +0700

    solve problem 2784 by using boolean array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2784/solution.cpp
+++ old-problems/2784/solution.cpp
@@ -3,6 +3,18 @@
 class Solution {
  public:
   bool isGood(std::vector<int>& nums) {
-    return nums.empty();
+    const int n = nums.size() - 1;
+    std::vector<bool> exists(n + 1, false);
+    int nCount{0};
+    for (const int num : nums) {
+      if (num > n) return false;
+      if (num < n) {
+        if (exists[num]) return false;
+        exists[num] = true;
+      } else {
+        if (++nCount == 3) return false;
+      }
+    }
+    return true;
   }
 };
