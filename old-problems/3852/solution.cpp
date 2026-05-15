2f863558bfd4dcc7cf8d63c5877caee00ad10c11
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-15 12:41:07 +0700

    solve problem 3852 in C++ by counting frequency
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3852/solution.cpp
+++ old-problems/3852/solution.cpp
@@ -3,6 +3,20 @@
 class Solution {
  public:
   std::vector<int> minDistinctFreqPair(std::vector<int>& nums) {
-    return nums;
+    int freqs[101]{0};
+    for (const int num : nums) ++freqs[num];
+
+    for (int x{0}; x <= 100; ++x) {
+      if (freqs[x] != 0) {
+        for (int y{x + 1}; y <= 100; ++y) {
+          if (freqs[y] != 0 && freqs[x] != freqs[y]) {
+            return {x, y};
+          }
+        }
+        break;
+      }
+    }
+
+    return {-1, -1};
   }
 };
