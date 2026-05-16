d39503cc43be3338f1d5e4ea1500424d72cc082b
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-16 22:53:46 +0700

    solve problem 3660 in C by using prefix-max and suffix-min
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3660/c/solution.c
+++ old-problems/3660/c/solution.c
@@ -1,4 +1,25 @@
+#include <stdlib.h>
+
 int* maxValue(int* nums, int numsSize, int* returnSize) {
+  int* maxs = malloc(numsSize * sizeof(int));
+  int* mins = malloc(numsSize * sizeof(int));
+
+  maxs[0] = nums[0];
+  for (int i = 1; i < numsSize; ++i) {
+    maxs[i] = maxs[i - 1] >= nums[i] ? maxs[i - 1] : nums[i];
+  }
+
+  mins[numsSize - 1] = nums[numsSize - 1];
+  nums[numsSize - 1] = maxs[numsSize - 1];
+
+  for (int i = numsSize - 1; i > 0; --i) {
+    mins[i - 1] = mins[i] <= nums[i - 1] ? mins[i] : nums[i - 1];
+    nums[i - 1] = maxs[i - 1] > mins[i] ? nums[i] : maxs[i - 1];
+  }
+
+  free(maxs);
+  free(mins);
+
   *returnSize = numsSize;
   return nums;
 }
