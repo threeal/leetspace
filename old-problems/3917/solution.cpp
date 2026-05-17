bdcc64cf217f5add43c3ff58a68bc9bd43dda08b
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-17 21:48:54 +0700

    solve problem 3917 in C++ by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3917/solution.cpp
+++ old-problems/3917/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   std::vector<int> countOppositeParity(std::vector<int>& nums) {
+    int even{0}, odd{0};
+    for (std::size_t i{nums.size()}; i > 0;) {
+      --i;
+      if (nums[i] % 2 == 0) {
+        nums[i] = odd;
+        ++even;
+      } else {
+        nums[i] = even;
+        ++odd;
+      }
+    }
     return nums;
   }
 };
