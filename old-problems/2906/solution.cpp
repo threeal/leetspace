469b9a35821a72823c004546e3577a3713c135b8
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-18 12:12:56 +0700

    solve problem 2906 in C++ using prefix and suffix products
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2906/solution.cpp
+++ old-problems/2906/solution.cpp
@@ -4,6 +4,28 @@ class Solution {
  public:
   std::vector<std::vector<int>> constructProductMatrix(
       std::vector<std::vector<int>>& grid) {
-    return grid;
+    std::vector<std::vector<int>> output(
+        grid.size(), std::vector<int>(grid[0].size()));
+
+    int prefix{1};
+    for (std::size_t i{0}; i < grid.size(); ++i) {
+      for (std::size_t j{0}; j < grid[0].size(); ++j) {
+        grid[i][j] %= 12345;
+        output[i][j] = prefix;
+        prefix = (prefix * grid[i][j]) % 12345;
+      }
+    }
+
+    int suffix{1};
+    for (std::size_t i{grid.size()}; i > 0;) {
+      --i;
+      for (std::size_t j{grid[i].size()}; j > 0;) {
+        --j;
+        output[i][j] = (output[i][j] * suffix) % 12345;
+        suffix = (suffix * grid[i][j]) % 12345;
+      }
+    }
+
+    return output;
   }
 };
