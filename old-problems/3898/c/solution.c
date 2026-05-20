c3d8b33094f7fe171024b1b022d74638504cfb80
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-20 14:39:22 +0700

    solve problem 3898 in C by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3898/c/solution.c
+++ old-problems/3898/c/solution.c
@@ -1,5 +1,14 @@
+#include <stdlib.h>
+
 int* findDegrees(
     int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
-  *returnSize = matrixColSize[matrixSize - 1];
-  return matrix[matrixSize - 1];
+  int* degrees = malloc(matrixSize * sizeof(int));
+  for (int i = 0; i < matrixSize; ++i) {
+    degrees[i] = 0;
+    for (int j = 0; j < matrixColSize[i]; ++j) {
+      if (matrix[i][j] == 1) ++degrees[i];
+    }
+  }
+  *returnSize = matrixSize;
+  return degrees;
 }
