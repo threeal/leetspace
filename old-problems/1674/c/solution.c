71a8d01c46183df167b13530ba521b67c84874e0
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-20 14:50:15 +0700

    solve problem 1674 in C using prefix sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1674/c/solution.c
+++ old-problems/1674/c/solution.c
@@ -1,3 +1,25 @@
+#include <stdlib.h>
+
 int minMoves(int* nums, int numsSize, int limit) {
-  return nums[numsSize - 1] + limit;
+  const int limit2 = 2 * limit;
+  const int prefixMovesSize = limit2 + 2;
+  int* prefixMoves = malloc(prefixMovesSize * sizeof(int));
+  for (int i = 0; i < prefixMovesSize; ++i) prefixMoves[i] = 0;
+
+  prefixMoves[2] += numsSize;
+  for (int l = 0, r = numsSize - 1; l < r; ++l, --r) {
+    --prefixMoves[(nums[l] < nums[r] ? nums[l] : nums[r]) + 1];
+    --prefixMoves[nums[l] + nums[r]];
+    ++prefixMoves[nums[l] + nums[r] + 1];
+    ++prefixMoves[(nums[l] > nums[r] ? nums[l] : nums[r]) + limit + 1];
+  }
+
+  int moves = prefixMoves[2], minMoves = moves;
+  for (int i = 3; i <= limit2; ++i) {
+    moves += prefixMoves[i];
+    if (moves < minMoves) minMoves = moves;
+  }
+
+  free(prefixMoves);
+  return minMoves;
 }
