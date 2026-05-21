0134d8d3c14e9dd1dea6ff2317a744390782264e
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-21 19:21:32 +0700

    solve problem 3904 in C++ by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3904/solution.cpp
+++ old-problems/3904/solution.cpp
@@ -1,8 +1,22 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int firstStableIndex(std::vector<int>& nums, int k) {
-    return nums.size() + k;
+    std::vector<int> mins(nums.size());
+
+    mins[nums.size() - 1] = nums[nums.size() - 1];
+    for (std::size_t i{nums.size() - 1}; i > 0; --i) {
+      mins[i - 1] = std::min(nums[i - 1], mins[i]);
+    }
+
+    int max{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      if (nums[i] > max) max = nums[i];
+      if (max - mins[i] <= k) return i;
+    }
+
+    return -1;
   }
 };
