a9a652a905351d312d3aca90ef9c0d282d70a3c3
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-22 09:13:17 +0700

    solve problem 3904 in C by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3904/c/solution.c
+++ old-problems/3904/c/solution.c
@@ -1,3 +1,22 @@
+#include <stdlib.h>
+
 int firstStableIndex(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] + k;
+  int* mins = malloc(numsSize * sizeof(int));
+
+  mins[numsSize - 1] = nums[numsSize - 1];
+  for (int i = numsSize - 1; i > 0; --i) {
+    mins[i - 1] = nums[i - 1] < mins[i] ? nums[i - 1] : mins[i];
+  }
+
+  int max = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] > max) max = nums[i];
+    if (max - mins[i] <= k) {
+      free(mins);
+      return i;
+    }
+  }
+
+  free(mins);
+  return -1;
 }
