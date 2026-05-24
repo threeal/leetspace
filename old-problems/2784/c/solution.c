dc785c070ae202ee065038037791f710fa03ba8d
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-24 17:53:28 +0700

    solve problem 2784 in C by using boolean array
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2784/c/solution.c
+++ old-problems/2784/c/solution.c
@@ -1,5 +1,36 @@
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 bool isGood(int* nums, int numsSize) {
-  return nums[numsSize - 1] == 0;
+  const int n = numsSize - 1;
+
+  const int existsSize = n / 8 + 1;
+  uint8_t* exists = malloc(existsSize * sizeof(uint8_t));
+  memset(exists, 0, existsSize * sizeof(uint8_t));
+
+  int nCount = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    if (nums[i] > n) {
+      free(exists);
+      return false;
+    }
+
+    if (nums[i] < n) {
+      if ((exists[nums[i] / 8] & (1 << (nums[i] % 8))) != 0) {
+        free(exists);
+        return false;
+      }
+      exists[nums[i] / 8] |= 1 << (nums[i] % 8);
+    } else {
+      if (++nCount == 3) {
+        free(exists);
+        return false;
+      }
+    }
+  }
+
+  free(exists);
+  return true;
 }
