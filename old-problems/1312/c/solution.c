9637cdef6cb7786dd717d3586b0db775160b9ec9
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-26 10:37:32 +0700

    solve problem 1312 in C using a bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1312/c/solution.c
+++ old-problems/1312/c/solution.c
@@ -1,3 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+
 int minInsertions(char* s) {
-  return *s;
+  const int sLen = strlen(s);
+  int* dp = malloc((sLen + 1) * sizeof(int));
+  memset(dp, 0, (sLen + 1) * sizeof(int));
+  for (int i = 0; i < sLen; ++i) {
+    int prev = dp[0];
+    for (int j = 0; j < sLen; ++j) {
+      const int temp = dp[j + 1];
+      if (s[i] == s[sLen - j - 1]) {
+        dp[j + 1] = prev + 1;
+      } else {
+        if (dp[j] > dp[j + 1]) dp[j + 1] = dp[j];
+      }
+      prev = temp;
+    }
+  }
+  return sLen - dp[sLen];
 }
