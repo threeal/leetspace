b1f5f77ce7b42c19a9bf03e3c2849c8ea869bf11
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-28 19:34:11 +0700

    solve problem 2304 in C++ by using a bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2304/solution.cpp
+++ old-problems/2304/solution.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
@@ -5,6 +6,18 @@ class Solution {
   int minPathCost(
       std::vector<std::vector<int>>& grid,
       std::vector<std::vector<int>>& moveCost) {
-    return grid.size() + moveCost.size();
+    std::vector<int> now{grid[0]}, prev(now.size());
+    for (std::size_t i{1}; i < grid.size(); ++i) {
+      std::swap(now, prev);
+      for (std::size_t j{0}; j < grid[i].size(); ++j) {
+        int minCost{prev[0] + moveCost[grid[i - 1][0]][j]};
+        for (std::size_t k{1}; k < grid[i].size(); ++k) {
+          const int cost{prev[k] + moveCost[grid[i - 1][k]][j]};
+          if (cost < minCost) minCost = cost;
+        }
+        now[j] = grid[i][j] + minCost;
+      }
+    }
+    return *std::min_element(now.begin(), now.end());
   }
 };
