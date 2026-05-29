04d7c8f9c24a60ad838d68c0d83c5ae488189ea0
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-29 14:15:09 +0700

    add support to test problem 3093 in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3093/c/solution.c
@@ -0,0 +1,10 @@
+int* stringIndices(
+    char** wordsContainer, int wordsContainerSize,
+    char** wordsQuery, int wordsQuerySize, int* returnSize) {
+  (void)wordsContainer;
+  (void)wordsContainerSize;
+  (void)wordsQuery;
+  (void)wordsQuerySize;
+  *returnSize = 0;
+  return 0;
+}
