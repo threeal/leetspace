6a9ff38b17583890a310f5b48ab052e2cd1023ec
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-29 14:05:35 +0700

    solve problem 3300 in C by finding min digit sum
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3300/c/solution.c
+++ old-problems/3300/c/solution.c
@@ -1,3 +1,12 @@
 int minElement(int* nums, int numsSize) {
-  return nums[numsSize - 1];
+  int minSum = nums[0];
+  for (int i = 0; i < numsSize; ++i) {
+    int sum = 0;
+    while (nums[i] > 0) {
+      sum += nums[i] % 10;
+      nums[i] /= 10;
+    }
+    if (sum < minSum) minSum = sum;
+  }
+  return minSum;
 }
