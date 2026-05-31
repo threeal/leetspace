ed07fb07c381dce77d16bbdc84813e921e35ff4e
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-31 12:56:58 +0700

    solve problem 3925 in C++ by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3925/solution.cpp
+++ old-problems/3925/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   std::vector<int> concatWithReverse(std::vector<int>& nums) {
+    const std::size_t n{nums.size()};
+    nums.resize(n * 2);
+    for (std::size_t i{0}; i < n; ++i) {
+      nums[nums.size() - i - 1] = nums[i];
+    }
     return nums;
   }
 };
