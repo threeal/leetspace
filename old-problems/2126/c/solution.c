54e8be659f966c158d72b2a8b3b1e8e8ff99a0df
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-07 21:08:50 +0700

    solve problem 2126 in C by using sorting and for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2126/c/solution.c
+++ old-problems/2126/c/solution.c
@@ -1,5 +1,16 @@
 #include <stdbool.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+  return *(int*)a - *(int*)b;
+}
 
 bool asteroidsDestroyed(int mass, int* asteroids, int asteroidsSize) {
-  return mass + asteroids[asteroidsSize - 1];
+  qsort(asteroids, asteroidsSize, sizeof(int), compare);
+  long long llmass = mass;
+  for (int i = 0; i < asteroidsSize; ++i) {
+    if (asteroids[i] > llmass) return false;
+    llmass += asteroids[i];
+  }
+  return true;
 }
