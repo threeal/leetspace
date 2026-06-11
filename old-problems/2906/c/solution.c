899ebe1a5aa393f1618c6796f4a39f475495caf6
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-11 10:05:34 +0700

    solve problem 2906 in C using prefix and suffix products
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2906/c/solution.c
+++ old-problems/2906/c/solution.c
@@ -1,7 +1,29 @@
+#include <stdlib.h>
+
 int** constructProductMatrix(
     int** grid, int gridSize, int* gridColSize,
     int* returnSize, int** returnColumnSizes) {
+  int** output = malloc(gridSize * sizeof(int*));
+
+  int prefix = 1;
+  for (int i = 0; i < gridSize; ++i) {
+    output[i] = malloc(gridColSize[i] * sizeof(int));
+    for (int j = 0; j < gridColSize[0]; ++j) {
+      grid[i][j] %= 12345;
+      output[i][j] = prefix;
+      prefix = (prefix * grid[i][j]) % 12345;
+    }
+  }
+
+  int suffix = 1;
+  for (int i = gridSize - 1; i >= 0; --i) {
+    for (int j = gridColSize[i] - 1; j >= 0; --j) {
+      output[i][j] = (output[i][j] * suffix) % 12345;
+      suffix = (suffix * grid[i][j]) % 12345;
+    }
+  }
+
   *returnSize = gridSize;
   *returnColumnSizes = gridColSize;
-  return grid;
+  return output;
 }
