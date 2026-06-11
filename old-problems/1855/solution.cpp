d4201e94b5e612abf890a7989a0daf85622abb86
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-11 09:48:18 +0700

    solve problem 1855 in C++ by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1855/solution.cpp
+++ old-problems/1855/solution.cpp
@@ -3,6 +3,11 @@
 class Solution {
  public:
   int maxDistance(std::vector<int>& nums1, std::vector<int>& nums2) {
-    return nums1.size() + nums2.size();
+    std::size_t i{0}, j{1};
+    while (i < nums1.size() && j < nums2.size()) {
+      if (nums1[i] > nums2[j]) ++i;
+      ++j;
+    }
+    return j - i - 1;
   }
 };
