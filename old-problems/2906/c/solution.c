007ed36fec70154b16f0f98e57bb42ebdf115082
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-11 10:02:59 +0700

    add support to test problem 2906 in C
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/2906/c/solution.c
@@ -0,0 +1,7 @@
+int** constructProductMatrix(
+    int** grid, int gridSize, int* gridColSize,
+    int* returnSize, int** returnColumnSizes) {
+  *returnSize = gridSize;
+  *returnColumnSizes = gridColSize;
+  return grid;
+}
