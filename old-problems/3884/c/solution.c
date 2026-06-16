9249237506edfc7a6ed7a0196f6a0929a6240852
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-16 21:12:05 +0700

    solve problem 3884 in C by using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3884/c/solution.c
+++ old-problems/3884/c/solution.c
@@ -1,3 +1,9 @@
+#include <string.h>
+
 int firstMatchingIndex(char* s) {
-  return *s;
+  const int sLen = strlen(s);
+  for (int l = 0, r = sLen - 1; l < r; ++l, --r) {
+    if (s[l] == s[r]) return l;
+  }
+  return sLen % 2 == 0 ? -1 : sLen / 2;
 }
