a68724a3971302b0ed946728ebaa9ae0c9dfba22
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-16 21:03:10 +0700

    solve problem 3612 in C++ by simulating the operation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3612/solution.cpp
+++ old-problems/3612/solution.cpp
@@ -1,8 +1,28 @@
+#include <algorithm>
 #include <string>
 
 class Solution {
  public:
   std::string processStr(std::string s) {
-    return s;
+    std::string res{};
+    for (char c : s) {
+      switch (c) {
+        case '*':
+          if (!res.empty()) res.pop_back();
+          break;
+
+        case '#':
+          res += res;
+          break;
+
+        case '%':
+          if (!res.empty()) std::reverse(res.begin(), res.end());
+          break;
+
+        default:
+          res += c;
+      }
+    }
+    return res;
   }
 };
