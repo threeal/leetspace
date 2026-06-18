f33c4b77c8f662f1feaf2bd58228c581935d4da2
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-18 16:59:45 +0700

    solve problem 1344 in C++ by using math
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1344/solution.cpp
+++ old-problems/1344/solution.cpp
@@ -1,6 +1,10 @@
+#include <cmath>
+
 class Solution {
  public:
   double angleClock(int hour, int minutes) {
-    return hour + minutes;
+    const double h = hour, m = minutes;
+    const double angle{std::abs((h + m / 60.0) / 12.0 - m / 60.0)};
+    return (angle < 0.5 ? angle : 1.0 - angle) * 360.0;
   }
 };
