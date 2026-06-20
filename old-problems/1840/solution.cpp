380d8ec16fbc0dcc0841ad02ce6ef7055177b663
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-20 21:28:19 +0700

    DONT MERGE

--- old-problems/1840/solution.cpp
+++ old-problems/1840/solution.cpp
@@ -1,9 +1,18 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
  public:
   int maxBuilding(
       int n, std::vector<std::vector<int>>& restrictions) {
-    return n + restrictions.size();
+    if (restrictions.empty()) return n - 1;
+
+    std::sort(
+        restrictions.begin(), restrictions.end(),
+        [](const auto& a, const auto& b) {
+          return a[0] < b[0];
+        });
+
+    return restrictions[0][0];
   }
 };
