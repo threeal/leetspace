3f5c1e3cbee5da2e8f1d7b6e43b244914a90b011
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-22 19:28:58 +0800

    solve problem 1189 in C++ by counting character frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1189/solution.cpp
+++ old-problems/1189/solution.cpp
@@ -3,6 +3,37 @@
 class Solution {
  public:
   int maxNumberOfBalloons(std::string text) {
-    return text.size();
+    int b{0}, a{0}, l{0}, o{0}, n{0};
+    for (char c : text) {
+      switch (c) {
+        case 'b':
+          ++b;
+          break;
+
+        case 'a':
+          ++a;
+          break;
+
+        case 'l':
+          ++l;
+          break;
+
+        case 'o':
+          ++o;
+          break;
+
+        case 'n':
+          ++n;
+          break;
+      }
+    }
+
+    int max{b};
+    if (a < max) max = a;
+    if (l / 2 < max) max = l / 2;
+    if (o / 2 < max) max = o / 2;
+    if (n < max) max = n;
+
+    return max;
   }
 };
