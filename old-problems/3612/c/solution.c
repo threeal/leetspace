89e62a66a10f38587e0fd1785ff2cbb8ae71b6b5
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-22 19:47:22 +0800

    solve problem 3612 in C by simulating the operation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3612/c/solution.c
+++ old-problems/3612/c/solution.c
@@ -1,3 +1,44 @@
+#include <stdlib.h>
+
 char* processStr(char* s) {
-  return s;
+  int resLen = 0;
+  int maxResLen = 128;
+  char* res = malloc(maxResLen * sizeof(char));
+
+  for (char* c = s; *c != 0; ++c) {
+    switch (*c) {
+      case '*':
+        if (resLen > 0) --resLen;
+        break;
+
+      case '#':
+        if (resLen * 2 >= maxResLen) {
+          maxResLen *= 4;
+          res = realloc(res, maxResLen * sizeof(char));
+        }
+        for (int i = 0; i < resLen; ++i) {
+          res[resLen + i] = res[i];
+        }
+        resLen *= 2;
+        break;
+
+      case '%':
+        for (int l = 0, r = resLen - 1; l < r; ++l, --r) {
+          const int temp = res[l];
+          res[l] = res[r];
+          res[r] = temp;
+        }
+        break;
+
+      default:
+        res[resLen++] = *c;
+        if (resLen >= maxResLen) {
+          maxResLen *= 4;
+          res = realloc(res, maxResLen * sizeof(char));
+        }
+    }
+  }
+
+  res[resLen] = 0;
+  return res;
 }
