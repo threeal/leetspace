b8f9ceb2d5f6c2412b0d1b5a600748ce23588958
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-24 20:52:58 +0800

    solve problem 3633 in C by using brute force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3633/c/solution.c
+++ old-problems/3633/c/solution.c
@@ -1,10 +1,26 @@
+#include <limits.h>
+
 int earliestFinishTime(
     int* landStartTime, int landStartTimeSize,
     int* landDuration, int landDurationSize,
     int* waterStartTime, int waterStartTimeSize,
     int* waterDuration, int waterDurationSize) {
-  return landStartTime[landStartTimeSize - 1] +
-      landDuration[landDurationSize - 1] +
-      waterStartTime[waterStartTimeSize - 1] +
-      waterDuration[waterDurationSize - 1];
+  (void)landDurationSize;
+  (void)waterDurationSize;
+  int minTime = INT_MAX;
+  for (int i = 0; i < landStartTimeSize; ++i) {
+    for (int j = 0; j < waterStartTimeSize; ++j) {
+      int startTime = landStartTime[i] + landDuration[i];
+      if (waterStartTime[j] > startTime) startTime = waterStartTime[j];
+      const int waterEndTime = startTime + waterDuration[j];
+
+      startTime = waterStartTime[j] + waterDuration[j];
+      if (landStartTime[i] > startTime) startTime = landStartTime[i];
+      const int landEndTime = startTime + landDuration[i];
+
+      const int time = waterEndTime < landEndTime ? waterEndTime : landEndTime;
+      if (time < minTime) minTime = time;
+    }
+  }
+  return minTime;
 }
