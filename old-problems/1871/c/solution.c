b91365c3a05851c939c151072c9bda6ed1c366d1
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-26 15:09:47 +0800

    solve problem 1871 in C by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1871/c/solution.c
+++ old-problems/1871/c/solution.c
@@ -1,5 +1,30 @@
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 bool canReach(char* s, int minJump, int maxJump) {
-  return *s == 0 && minJump == 0 && maxJump == 0;
+  const int sLen = strlen(s);
+
+  if (s[sLen - 1] == '1') return false;
+
+  int* jumps = malloc(sLen * sizeof(int));
+  int front = 0, back = 0, laterBack = 0;
+  jumps[laterBack++] = 0;
+
+  for (int i = 1; i < sLen; ++i) {
+    while (back < laterBack && jumps[back] + minJump <= i) ++back;
+    while (front < back && jumps[front] + maxJump < i) ++front;
+
+    if (front == back) {
+      if (back == laterBack) {
+        free(jumps);
+        return false;
+      }
+    } else {
+      if (s[i] == '0') jumps[laterBack++] = i;
+    }
+  }
+
+  free(jumps);
+  return front < back;
 }
