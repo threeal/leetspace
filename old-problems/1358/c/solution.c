0ab13f4a4e8c3ce35a129b5cc05b6faa1f8c7f9d
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-06-30 22:56:34 +0800

    solve problem 1358 in C by using sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1358/c/solution.c
+++ old-problems/1358/c/solution.c
@@ -1,3 +1,73 @@
+#include <stdbool.h>
+
 int numberOfSubstrings(char* s) {
-  return *s;
+  int r = 0, a = 0, b = 0, c = 0, valid = 0;
+  while (s[r] != 0 && valid < 3) {
+    switch (s[r]) {
+      case 'a':
+        if (++a == 1) ++valid;
+        break;
+      case 'b':
+        if (++b == 1) ++valid;
+        break;
+      case 'c':
+        if (++c == 1) ++valid;
+        break;
+    }
+    ++r;
+  }
+
+  if (valid < 3) return 0;
+
+  int l = 0, count = 0;
+  while (true) {
+    bool stop = false;
+    while (!stop) {
+      switch (s[l]) {
+        case 'a':
+          if (a > 1) {
+            ++l;
+            --a;
+          } else {
+            stop = true;
+          }
+          break;
+        case 'b':
+          if (b > 1) {
+            ++l;
+            --b;
+          } else {
+            stop = true;
+          }
+          break;
+        case 'c':
+          if (c > 1) {
+            ++l;
+            --c;
+          } else {
+            stop = true;
+          }
+          break;
+      }
+    }
+    count += l + 1;
+
+    if (s[r] == 0) break;
+
+    switch (s[r]) {
+      case 'a':
+        ++a;
+        break;
+      case 'b':
+        ++b;
+        break;
+      case 'c':
+        ++c;
+        break;
+    }
+
+    ++r;
+  }
+
+  return count;
 }
