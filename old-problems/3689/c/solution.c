37d8a144744d8a8e01ea5d27a437e57c8214781b
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-07-06 21:48:28 +0700

    solve problem 3689 in C by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3689/c/solution.c
+++ old-problems/3689/c/solution.c
@@ -1,3 +1,11 @@
 long long maxTotalValue(int* nums, int numsSize, int k) {
-  return nums[numsSize - 1] + k;
+  int min = nums[0], max = nums[0];
+  for (int i = 1; i < numsSize; ++i) {
+    if (nums[i] < min) {
+      min = nums[i];
+    } else if (nums[i] > max) {
+      max = nums[i];
+    }
+  }
+  return (long long)(max - min) * k;
 }
