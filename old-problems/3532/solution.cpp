903cee861a4ff2b6129c9295a49bb4303250a7a3
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-07-09 22:24:43 +0700

    add test for problem 3532. Path Existence Queries in a Graph I
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3532/solution.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<bool> pathExistenceQueries(
+      int n, std::vector<int>& nums, int maxDiff,
+      std::vector<std::vector<int>>& queries) {
+    (void)n;
+    (void)nums;
+    (void)maxDiff;
+    (void)queries;
+    return {};
+  }
+};
