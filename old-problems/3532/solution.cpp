2c953e858ca2f9a42b05fdf15e825aad1a17d2d5
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-07-09 22:28:45 +0700

    solve problem 3532 in C++ by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3532/solution.cpp
+++ old-problems/3532/solution.cpp
@@ -6,9 +6,21 @@ class Solution {
       int n, std::vector<int>& nums, int maxDiff,
       std::vector<std::vector<int>>& queries) {
     (void)n;
-    (void)nums;
-    (void)maxDiff;
-    (void)queries;
-    return {};
+
+    int prev{nums[0]}, group{0};
+    nums[0] = group;
+
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] - prev > maxDiff) ++group;
+      prev = nums[i];
+      nums[i] = group;
+    }
+
+    std::vector<bool> results(queries.size());
+    for (std::size_t i{0}; i < queries.size(); ++i) {
+      results[i] = nums[queries[i][0]] == nums[queries[i][1]];
+    }
+
+    return results;
   }
 };
