720a1aaea54a4af74afeac57236f43ddfea69ec9
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-08-05 17:56:09 +0800

    DONT MERGE

--- old-problems/3310/solution.cpp
+++ old-problems/3310/solution.cpp
@@ -4,9 +4,31 @@ class Solution {
  public:
   std::vector<int> remainingMethods(
       int n, int k, std::vector<std::vector<int>>& invocations) {
+    std::vector<std::vector<int>> methodInvocations(n);
+    for (const auto& invocation : invocations) {
+      methodInvocations[invocation[i][0]].push_back(invocation[i][1]);
+    }
+
+    std::vector<bool> isMethodSuspicious(n);
+    markSuspiciousMethods(methodInvocations, isMethodSuspicious, k);
+
     (void)n;
     (void)k;
     (void)invocations;
     return {};
+  }
+
+ private:
+  void markSuspiciousMethods(
+      const std::vector<std::vector<int>>& methodInvocations,
+      std::vector<bool>& isMethodSuspicious,
+      int method) {
+    isMethodSuspicious[method] = true;
+    for (const auto invokedMethod : methodInvocations[method]) {
+      if (!isMethodSuspicious[invokedMethod]) {
+        markSuspiciousMethods(
+            methodInvocations, isMethodSuspicious, invokedMethod);
+      }
+    }
   }
 };
