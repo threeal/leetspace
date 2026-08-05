2cd8610f004fa7ee73fbaf53a607f905afa1fdaa
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-08-05 17:38:16 +0800

    add test for problem 3310. Remove Methods From Project
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/3310/solution.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> remainingMethods(
+      int n, int k, std::vector<std::vector<int>>& invocations) {
+    (void)n;
+    (void)k;
+    (void)invocations;
+    return {};
+  }
+};
