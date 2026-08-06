5a1f1765ad55da617f1b8e6d24dd203a277b3cab
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-08-06 10:21:12 +0800

    solve problem 3345 in C++ by using brute-force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3345/solution.cpp
+++ old-problems/3345/solution.cpp
@@ -1,6 +1,15 @@
 class Solution {
  public:
   int smallestNumber(int n, int t) {
-    return n + t;
+    while (true) {
+      if (n >= 100) {
+        if (((n / 100) * ((n / 10) % 10) * (n % 10)) % t == 0) return n;
+      } else if (n >= 10) {
+        if (((n / 10) * (n % 10)) % t == 0) return n;
+      } else {
+        if (n % t == 0) return n;
+      }
+      ++n;
+    }
   }
 };
