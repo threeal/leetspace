dcf7de6f1bdceeaf28ca09e4e14ebabf26fb514b
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-08-10 10:57:57 +0800

    solve problem 1510 in C++ by using a bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1510/solution.cpp
+++ old-problems/1510/solution.cpp
@@ -1,6 +1,17 @@
+#include <vector>
+
 class Solution {
  public:
   bool winnerSquareGame(int n) {
-    return n == 0;
+    std::vector<bool> wins(n + 1, false);
+    for (int i{1}; i <= n; ++i) {
+      for (int j{1}; j * j <= i; ++j) {
+        if (!wins[i - j * j]) {
+          wins[i] = true;
+          break;
+        }
+      }
+    }
+    return wins[n];
   }
 };
