fcef6a48287c54feeae35323acfff3a60ea68411
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-08-11 23:39:05 +0800

    solve problem 2996 in C++ using while loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2996/solution.cpp
+++ old-problems/2996/solution.cpp
@@ -3,6 +3,25 @@
 class Solution {
  public:
   int missingInteger(std::vector<int>& nums) {
-    return nums.size();
+    int sum{nums[0]};
+    std::uint64_t flag{1ull << nums[0]};
+    std::size_t i{1};
+
+    while (i < nums.size() && nums[i] == nums[i - 1] + 1) {
+      sum += nums[i];
+      flag |= 1ull << nums[i];
+      ++i;
+    }
+
+    while (i < nums.size()) {
+      flag |= 1ull << nums[i];
+      ++i;
+    }
+
+    if (sum <= 50) {
+      while ((flag & 1ull << sum) != 0) ++sum;
+    }
+
+    return sum;
   }
 };
