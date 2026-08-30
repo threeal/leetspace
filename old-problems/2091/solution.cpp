ec5eada71d06d4533e4993ba5a2e5cf46f8a06bb
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-08-30 11:39:22 +0800

    solve problem 2091 by finding min-max
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/2091/solution.cpp
+++ old-problems/2091/solution.cpp
@@ -1,8 +1,22 @@
+#include <utility>
 #include <vector>
 
 class Solution {
  public:
   int minimumDeletions(std::vector<int>& nums) {
-    return nums.size();
+    std::size_t min{0}, max{0};
+    for (std::size_t i{1}; i < nums.size(); ++i) {
+      if (nums[i] < nums[min]) {
+        min = i;
+      } else if (nums[i] > nums[max]) {
+        max = i;
+      }
+    }
+
+    if (min > max) std::swap(min, max);
+
+    return std::min(
+        std::min(max + 1, nums.size() - min),
+        min + 1 + nums.size() - max);
   }
 };
