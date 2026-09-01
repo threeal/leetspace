a0d439522af31bce799e8a67b9b40b841f3504d9
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-09-01 12:48:17 +0800

    solve problem 1855 in C by using a sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1855/c/solution.c
+++ old-problems/1855/c/solution.c
@@ -1,3 +1,8 @@
 int maxDistance(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-  return nums1[nums1Size - 1] + nums2[nums2Size - 1];
+  int i = 0, j = 1;
+  while (i < nums1Size && j < nums2Size) {
+    if (nums1[i] > nums2[j]) ++i;
+    ++j;
+  }
+  return j - i - 1;
 }
