0e95bbee6e08218d8f9e3e8b3fa700c7251cab7b
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-09-04 03:28:48 +0000

    solve problem 3635 in C++ by using for-loops
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3635/solution.cpp
+++ old-problems/3635/solution.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <numeric>
 #include <vector>
 
 class Solution {
@@ -5,7 +7,33 @@ class Solution {
   int earliestFinishTime(
       std::vector<int>& landStartTime, std::vector<int>& landDuration,
       std::vector<int>& waterStartTime, std::vector<int>& waterDuration) {
-    return landStartTime.size() + landDuration.size() +
-        waterStartTime.size() + waterDuration.size();
+    int minLandFinish{std::numeric_limits<int>::max()};
+    for (std::size_t i{0}; i < landStartTime.size(); ++i) {
+      if (landStartTime[i] + landDuration[i] < minLandFinish) {
+        minLandFinish = landStartTime[i] + landDuration[i];
+      }
+    }
+
+    int minFinish{std::numeric_limits<int>::max()};
+    int minWaterFinish{std::numeric_limits<int>::max()};
+    for (std::size_t i{0}; i < waterStartTime.size(); ++i) {
+      const int maxStartTime{std::max(minLandFinish, waterStartTime[i])};
+      if (maxStartTime + waterDuration[i] < minFinish) {
+        minFinish = maxStartTime + waterDuration[i];
+      }
+
+      if (waterStartTime[i] + waterDuration[i] < minWaterFinish) {
+        minWaterFinish = waterStartTime[i] + waterDuration[i];
+      }
+    }
+
+    for (std::size_t i{0}; i < landStartTime.size(); ++i) {
+      const int maxStartTime{std::max(minWaterFinish, landStartTime[i])};
+      if (maxStartTime + landDuration[i] < minFinish) {
+        minFinish = maxStartTime + landDuration[i];
+      }
+    }
+
+    return minFinish;
   }
 };
