6ed4445b4686aacdec045aa0e19883174cbcd2fd
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-09-04 03:31:18 +0000

    solve problem 3931 in C++ using a for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3931/solution.cpp
+++ old-problems/3931/solution.cpp
@@ -3,6 +3,13 @@
 class Solution {
  public:
   bool isAdjacentDiffAtMostTwo(std::string s) {
-    return s.empty();
+    for (std::size_t i{1}; i < s.size(); ++i) {
+      if (s[i] > s[i - 1]) {
+        if (s[i] - s[i - 1] > 2) return false;
+      } else if (s[i - 1] - s[i] > 2) {
+        return false;
+      }
+    }
+    return true;
   }
 };
