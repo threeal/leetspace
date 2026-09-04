6041ab885a150ddff94ff29250d08523545eecf4
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-09-04 03:07:05 +0000

    solve problem 3903 in C++ by precomputing suffix minimums
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3903/solution.cpp
+++ old-problems/3903/solution.cpp
@@ -3,6 +3,17 @@
 class Solution {
  public:
   int firstStableIndex(std::vector<int>& nums, int k) {
-    return static_cast<int>(nums.size()) + k;
+    std::vector<int> mins{nums};
+    for (std::size_t i{mins.size() - 1}; i > 0; --i) {
+      if (mins[i] < mins[i - 1]) mins[i - 1] = mins[i];
+    }
+
+    int max{0};
+    for (std::size_t i{0}; i < nums.size(); ++i) {
+      if (nums[i] > max) max = nums[i];
+      if (max - mins[i] <= k) return i;
+    }
+
+    return -1;
   }
 };
