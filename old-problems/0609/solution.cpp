05282fd944849b8d003b17f4fcdb2f271eebaf6a
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-09-13 12:58:58 +0800

    test: add test for problem 609. Find Duplicate File in System
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ old-problems/0609/solution.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<std::string>> findDuplicate(
+      std::vector<std::string>& paths) {
+    return {paths};
+  }
+};
