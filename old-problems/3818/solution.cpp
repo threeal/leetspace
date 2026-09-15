d65e389e759d6ad6260bbfdf9e68ea09d59415c9
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-09-15 14:52:35 +0000

    feat(problem-3818/cpp): solve by finding the first non-increasing index from the right
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/3818/solution.cpp
+++ old-problems/3818/solution.cpp
@@ -3,6 +3,8 @@
 class Solution {
  public:
   int minimumPrefixLength(std::vector<int>& nums) {
-    return nums.size();
+    std::size_t i{nums.size() - 1};
+    while (i > 0 && nums[i - 1] < nums[i]) --i;
+    return i;
   }
 };
