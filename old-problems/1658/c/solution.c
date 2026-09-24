55d6c69cd729a3118b24c9daf503d8642933a316
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-09-24 10:10:00 +0800

    solve problem 1658 in C by using a two-pointer sliding window
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- old-problems/1658/c/solution.c
+++ old-problems/1658/c/solution.c
@@ -1,3 +1,17 @@
 int minOperations(int* nums, int numsSize, int x) {
-  return nums[numsSize - 1] + x;
+  int l = 0;
+  while (l < numsSize && x > 0) x -= nums[l++];
+  if (l == numsSize) return x == 0 ? numsSize : -1;
+
+  int min = x == 0 ? l : numsSize + 1;
+  int r = numsSize;
+  while (l > 0) {
+    x += nums[--l];
+    while (x > 0) x -= nums[--r];
+    if (x == 0 && l + numsSize - r < min) {
+      min = l + numsSize - r;
+    }
+  }
+
+  return min <= numsSize ? min : -1;
 }
